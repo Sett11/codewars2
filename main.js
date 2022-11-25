5699cb0e2f4194beac98fb3d50469274c9424838
Jhonny <gonzodzen@mail.ru>
2022-11-25 17:46:43 +0300

    Even numbers in an array from codewars

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-function sortGiftCode(code){
-    return code.split('').sort().join('')
+function evenNumbers(array, number) {
+    return array.filter(el => el%2===0).reverse().slice(0,  number).reverse()
   }
+  console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
