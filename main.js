b39e739a015b3062893cdfb05658d7b8137dd7eb
Jhonny <gonzodzen@mail.ru>
2022-11-30 01:41:36 +0300

    Switcheroo from codewars

--- main.js
+++ main.js
@@ -1,37 +1,4 @@
-let v1 = 50;
- let   v2 = 100;
-  let  v3 = 150;
-  let  v4 = 200;
-  let  v5 = 2;
-  let  v6 = 250;
-
-function equal1(){
-  let a = v4,
-   b = v2
-  return a - b;
+function switcheroo(x){
+  return x.split('').map(el=> el==='a'?el='b':el==='b'?el='a':el).join('')
 }
-
-//Please refer to the example above to complete the following functions
-function equal2(){
-  let a =v4  , //set number value to a
-      b = v2 ; //set number value to b
-  return a - b;
-}
-
-function equal3(){
-  let a =v1  , //set number value to a
-      b =v5  ; //set number value to b
-  return a * b;
-}
-
-function equal4(){
-  let a =v4  , //set number value to a
-      b = v5 ; //set number value to b
-  return a / b;
-}
-
-function equal5(){
-  let a =  v2, //set number value to a
-      b =  v4; //set number value to b
-  return a % b;
-}
+console.log(switcheroo('abc'))
