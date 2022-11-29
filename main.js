1285f617a033187ec4d436c8b7aafc0c38d54eee
Jhonny <gonzodzen@mail.ru>
2022-11-30 01:25:38 +0300

    Training JS #2: Basic data types--Number from codewars

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
+let v1 = 50;
+ let   v2 = 100;
+  let  v3 = 150;
+  let  v4 = 200;
+  let  v5 = 2;
+  let  v6 = 250;
 
-function animal(obj){
-  let [values1,values2, values3] = [...Object.values(obj)]
-  return `This ${values3} ${values1} has ${values2} legs.`
+function equal1(){
+  let a = v4,
+   b = v2
+  return a - b;
 }
-console.log(animal({name:"dog",legs:4,color:"white"}))
+
+//Please refer to the example above to complete the following functions
+function equal2(){
+  let a =v4  , //set number value to a
+      b = v2 ; //set number value to b
+  return a - b;
+}
+
+function equal3(){
+  let a =v1  , //set number value to a
+      b =v5  ; //set number value to b
+  return a * b;
+}
+
+function equal4(){
+  let a =v4  , //set number value to a
+      b = v5 ; //set number value to b
+  return a / b;
+}
+
+function equal5(){
+  let a =  v2, //set number value to a
+      b =  v4; //set number value to b
+  return a % b;
+}
