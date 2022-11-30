c67761d965127df88cacd8c4e13d42497a524c03
Jhonny <gonzodzen@mail.ru>
2022-12-01 02:53:31 +0300

    Calculating with Functions actual solution!

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
-function noOdds( values ){
-  return values.filter(el => el%2===0)
+function zero(callback) {
+  return callback?callback(0):0
 }
-console.log(noOdds([0,1,2,3]))
+function one(callback) {
+  return callback?callback(1):1
+}
+function two(callback) {
+  return callback?callback(2):2
+}
+function three(callback) {
+  return callback?callback(3):3
+}
+function four(callback) {
+  return callback?callback(4):4
+}
+function five(callback) {
+  return callback?callback(5):5
+}
+function six(callback) {
+  return callback?callback(6):6
+}
+function seven(callback) {
+  return callback?callback(7):7
+}
+function eight(callback) {
+  return callback?callback(8):8
+}
+function nine(callback) {
+  return callback?callback(9):9
+}
+
+function plus(a) {
+  return (b)=>a+b
+}
+function minus(a) {
+  return (b)=>b-a
+}
+function times(a) {
+  return (b)=>a*b
+}
+function dividedBy(a) {
+  return (b)=>Math.floor(b/a)
+}
