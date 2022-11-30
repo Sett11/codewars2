c0029febb318fe4fbf7139d7f78aefe72c920d24
Jhonny <gonzodzen@mail.ru>
2022-12-01 01:16:12 +0300

    callback algs

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
-const cubeChecker = function(volume, side){
-  return side<=0||volume<=0?false: side*side*side===volume?true:false
+function one(callback){
+  return callback?callback(1):1
 }
+function two(callback){
+  return callback?callback(2):2
+}
+function free(callback){
+  return callback?callback(3):3
+}
+function four(callback){
+  return callback?callback(4):4
+}
+function five(callback){
+  return callback?callback(5):5
+}
+function six(callback){
+  return callback?callback(6):6
+}
+function seven(callback){
+  return callback?callback(7):7
+}
+function eight(callback){
+  return callback?callback(8):8
+}
+function nine(callback){
+  return callback?callback(9):9
+}
+
+function plus(a){
+  return (b)=> a+b
+}
+function minus(a){
+  return (b)=> a-b 
+}
+function divide(a){
+  return (b)=> a*b
+}
+function mult(a){
+  return (b)=> a/b
+}
+
+console.log(two(plus(five())))
