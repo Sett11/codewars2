a7360a477447e67836b1ffa59e525e9433eb56bb
Jhonny <gonzodzen@mail.ru>
2022-11-30 18:54:26 +0300

    Hoare's sort

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const array = [0,3,2,5,6,8,1,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32]
 let count = 0
 const sortArray = (arr)=>{
-  for(let i = 0; i< arr.length;i++){
-    let indexMin = i
-    for(let j = i + 1;j<arr.length; j++){
-      if(arr[j] < arr[indexMin]){
-        indexMin = j
-      }
-      count += 1
-    }
-    let wow = arr[i]
-    arr[i] = arr[indexMin]
-    arr[indexMin] = wow
+  if(arr.length <= 1){
+    return arr
   }
-  return arr
+  let basesElem = Math.floor(arr.length/2)
+  let bases = arr[basesElem]
+  let little = []
+  let big = []
+  for(let i = 0; i < arr.length; i++){
+    count+=1
+    if(i===basesElem){
+      continue
+    }
+    if(arr[i] < bases){
+      little.push(arr[i])
+    }
+    else{
+      big.push(arr[i])
+    }
+  }
+  return [...sortArray(little), basesElem, ...sortArray(big)]
 }
 console.log(sortArray(array))
 console.log(count)
