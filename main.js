1f5a6ddf4e557854259b781ef7c68f7fa832fbee
Jhonny <gonzodzen@mail.ru>
2022-11-30 17:58:24 +0300

    binarySearch

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const fib = (n) =>{
-  if(n===1||n===2){
-    return 1
+const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let count = 0
+const binarySearch = (arr, item)=>{
+  let start = 0
+  let end = arr.length
+  let middle;
+  let found = false
+  let position = -1
+  while(found ===false && start <=end){
+    count+=1
+    middle = Math.floor((start+end)/2)
+    if(array[middle] === item){
+      found === true
+      position = middle
+      return position
+    }
+    if(item<array[middle]){
+      end = middle-1
+    }
+    else{
+      start = middle + 1
+    }
   }
-  return fib(n-1) + fib(n-2)
+  return position
 }
-console.log(fib(8))
+console.log(binarySearch(array, 4))
+console.log(count)
