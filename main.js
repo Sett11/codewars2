20ecb061bcad0c896f00bb073d9c97ac255747eb
Jhonny <gonzodzen@mail.ru>
2022-11-30 22:12:43 +0300

    Class LinkedList.

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-const cashFunc = (fn) =>{
-  const cash = {}
-  return function (n) {
-    if(cash[n]){
-       console.log('From cash', cash[n])
-       return cash[n]
+class LinkedList {
+  constructor(){
+    this.size = 0
+    this.root = null
+  }
+  add(value){
+    if(this.size === 0){
+      this.root = new Node(value)
+      this.size += 1
+      return true
     }
-    let result = fn(n)
-    console.log('From function', result)
-    cash[n] = result
-    return result
+    let node = this.root
+    while(node.next){
+      node = node.next
+    }
+    let newNode = new Node(value)
+    node.next = newNode
+    this.size += 1
+  }
+  getSize(){
+    return this.size
+  }
+  print(){
+    let result = []
+    let node = this.root
+    while(node){
+      result.push(node.value)
+      node = node.next
+    }
+    console.log(result)
   }
 }
 
-const fact = (n) => {
-  if(n===1){
-    return 1
+class Node {
+  constructor(value){
+    this.value = value
+    this.next = null
   }
-  return n *= fact(n-1)
 }
 
-const cashFact = cashFunc(fact)
-cashFact(5)
-cashFact(4)
-cashFact(4)
-cashFact(3)
-cashFact(2)
-cashFact(5)
+const list = new LinkedList()
+
+list.add(5)
+list.add(3)
+list.add(2)
+list.add(5)
+list.add(7)
+
+list.print()
