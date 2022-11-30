cd3d1addc1fdaf601b48366ab3e9a94fb30cccff
Jhonny <gonzodzen@mail.ru>
2022-11-30 18:24:10 +0300

    Sort.

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+const array = [0,3,2,5,6,8,1,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32]
 let count = 0
-const binarySearch = (arr, item)=>{
-  let start = 0
-  let end = arr.length
-  let middle;
-  let found = false
-  let position = -1
-  while(found ===false && start <=end){
-    count+=1
-    middle = Math.floor((start+end)/2)
-    if(array[middle] === item){
-      found === true
-      position = middle
-      return position
+const sortArray = (arr)=>{
+  for(let i = 0; i< arr.length;i++){
+    let indexMin = i
+    for(let j = i + 1;j<arr.length; j++){
+      if(arr[j] < arr[indexMin]){
+        indexMin = j
+      }
+      count += 1
     }
-    if(item<array[middle]){
-      end = middle-1
-    }
-    else{
-      start = middle + 1
-    }
+    let wow = arr[i]
+    arr[i] = arr[indexMin]
+    arr[indexMin] = wow
   }
-  return position
+  return arr
 }
-console.log(binarySearch(array, 4))
+console.log(sortArray(array))
 console.log(count)
