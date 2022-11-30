e0905515cd099890d3e115c1d4de9b721c0b89b8
Jhonny <gonzodzen@mail.ru>
2022-11-30 12:26:02 +0300

    Sum of the Fibonacci Sequence Using Recursion.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const f = (n) => {
-  if(n===1){
+const fib = (n) =>{
+  if(n===1||n===2){
     return 1
   }
-  return n * f(n-1)
+  return fib(n-1) + fib(n-2)
 }
-console.log(f(5))
+console.log(fib(8))
