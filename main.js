86aa8551f3d7ae14447f3f83964261841d0f80dc
Jhonny <gonzodzen@mail.ru>
2022-12-01 03:24:41 +0300

    Who ate the cookie? from codewars

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
-function parseF(s) {
-  return /\d/.test(s)?+s:null
+function cookie(x){
+  return  typeof x ==='string' ? "Who ate the last cookie? It was Zach!": Number.isFinite(x)?
+    "Who ate the last cookie? It was Monica!":
+    "Who ate the last cookie? It was the dog!"
 }
+console.log(cookie(3))
