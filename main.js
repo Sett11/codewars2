4552acf48c295f599a0d7c189fd0e9eb287419c9
Jhonny <gonzodzen@mail.ru>
2022-12-01 03:15:26 +0300

    Parse float from codewars.

--- main.js
+++ main.js
@@ -1,43 +1,4 @@
-function zero(callback) {
-  return callback?callback(0):0
-}
-function one(callback) {
-  return callback?callback(1):1
-}
-function two(callback) {
-  return callback?callback(2):2
-}
-function three(callback) {
-  return callback?callback(3):3
-}
-function four(callback) {
-  return callback?callback(4):4
-}
-function five(callback) {
-  return callback?callback(5):5
-}
-function six(callback) {
-  return callback?callback(6):6
-}
-function seven(callback) {
-  return callback?callback(7):7
-}
-function eight(callback) {
-  return callback?callback(8):8
-}
-function nine(callback) {
-  return callback?callback(9):9
-}
 
-function plus(a) {
-  return (b)=>a+b
-}
-function minus(a) {
-  return (b)=>b-a
-}
-function times(a) {
-  return (b)=>a*b
-}
-function dividedBy(a) {
-  return (b)=>Math.floor(b/a)
+function parseF(s) {
+  return /\d/.test(s)?+s:null
 }
