af72232b87e6671e2d614b7d3d54905c9b559b04
Jhonny <gonzodzen@mail.ru>
2022-12-05 01:07:12 +0300

    Most digits

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function _if(bool, func1, func2) {
-    return bool?func1():func2()
+function findLongest(array){
+    let newArr = []
+     array.map(el => newArr.push([el]))
+     let s = newArr.map(el => String(el))
+      s.sort((a,b) => b.length - a.length)
+      return Number(s[0])
   }
+  console.log(findLongest([8, 9000, 800]))
