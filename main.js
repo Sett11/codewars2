b4d111128bb346e606e1c8b748ff30ac81418c6f
Jhonny <gonzodzen@mail.ru>
2022-12-05 01:32:02 +0300

    Power of two

--- main.js
+++ main.js
@@ -1,15 +1,2 @@
-function cubeOdd(arr) {
-    let newArr = []
-    for( let i = 0; i < arr.length; i++){
-        if(arr[i] !== Number(arr[i])){
-            return undefined
-        } else{
-            newArr.push(arr[i])
-        }
-    }
-    return newArr
-    .filter(el => el % 2 !== 0)
-    .map(el => el**3)
-    .reduce((acc, cur) => acc + cur, 0)
-}
-console.log(cubeOdd([1, 2, 3, 4]))
+const isPowerOfTwo = n => n===0 ?  false : (n & (n - 1)) === 0
+console.log(isPowerOfTwo(0))
