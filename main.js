21293050b47e62d118eb6302c256c3aaca3bcf83
Jhonny <gonzodzen@mail.ru>
2022-12-05 01:22:11 +0300

    Sum of Odd Cubed Numbers

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function findLongest(array){
+function cubeOdd(arr) {
     let newArr = []
-     array.map(el => newArr.push([el]))
-     let s = newArr.map(el => String(el))
-      s.sort((a,b) => b.length - a.length)
-      return Number(s[0])
-  }
-  console.log(findLongest([8, 9000, 800]))
+    for( let i = 0; i < arr.length; i++){
+        if(arr[i] !== Number(arr[i])){
+            return undefined
+        } else{
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+    .filter(el => el % 2 !== 0)
+    .map(el => el**3)
+    .reduce((acc, cur) => acc + cur, 0)
+}
+console.log(cubeOdd([1, 2, 3, 4]))
