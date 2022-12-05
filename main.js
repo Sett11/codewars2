50d5b94b813052074dfedd60aee6f79ced9ace07
Jhonny <gonzodzen@mail.ru>
2022-12-05 21:41:05 +0300

    Type of sum

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-function isPangram(string){
-    let arr = []
-    let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let newString = string.replace(/[\W\.]/g, '').toLowerCase().split('')
-     for(let i of arr_en){
-        let counter = newString.length
-        while(counter){
-             arr.push(newString.includes(i))
-             counter--
-        }
-     }
-     return arr.includes(false) === false ? true : false
+function typeOfSum(a, b) {
+    return typeof (a+b)
   }
-  console.log(isPangram("This is not a pangram."))
