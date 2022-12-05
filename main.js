e93d20feb4371add548b4b9c8388e510549be0d6
Jhonny <gonzodzen@mail.ru>
2022-12-05 18:44:47 +0300

    Leonardo Dicaprio and Oscars

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-function Ship(draft,crew) {
-    this.draft = draft;
-    this.crew = crew;
-    this.isWorthIt = function(){
-        return (this.draft - (this.crew * 1.5)) > 20? true : false
-    }
-   }
-   let aWorthyShip = new Ship(100,20)
-   console.log(aWorthyShip.isWorthIt())
-   
+function leo(oscar){
+    return oscar === 88 ? "Leo finally won the oscar! Leo is happy":
+    oscar === 86 ? "Not even for Wolf of wallstreet?!":
+    oscar !== 88 && oscar !== 86 && oscar < 88 ? "When will you give Leo an Oscar?":
+    oscar > 88 ? "Leo got one already!": null
+}
+console.log(leo(88))
