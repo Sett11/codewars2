f732bcdade1b33e5d0e237776704089065e7a9af
Jhonny <gonzodzen@mail.ru>
2022-12-05 21:28:42 +0300

    Detect Pangram

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-function expressionMatter(a, b, c) {
-    let firstSum = a+b+c
-    let sum = a*(b+c)
-    let sum1 = a*b*c
-    let sum2 = a+(b*c)
-    let sum3 = (a+b)*c
-    return Math.max(firstSum, sum, sum1, sum2, sum3)
+function isPangram(string){
+    let arr = []
+    let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let newString = string.replace(/[\W\.]/g, '').toLowerCase().split('')
+     for(let i of arr_en){
+        let counter = newString.length
+        while(counter){
+             arr.push(newString.includes(i))
+             counter--
+        }
+     }
+     return arr.includes(false) === false ? true : false
   }
-  console.log(expressionMatter(1,1,1))
+  console.log(isPangram("This is not a pangram."))
