f4cecbd5db6982f176504f39437dba14301802c0
Jhonny <gonzodzen@mail.ru>
2022-12-05 22:03:18 +0300

    Enumerable Magic #1 - True for All?

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-function typeOfSum(a, b) {
-    return typeof (a+b)
-  }
+function all(arr, fun) {
+    if(arr.length === 0){
+        return true
+    }
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr.push(fun(arr[i]));
+//   }
+//   return newArr.includes(false) ? false : true
+return [...arr.filter(fun)].length !== 0 ? true : false
+}
+console.log(
+  all([1, 2, 3, 4, 5], function (v) {
+    return v > 9;
+  })
+);
