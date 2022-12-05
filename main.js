2175315a88973a9949d35beaa8de1a37dd7acf94
Jhonny <gonzodzen@mail.ru>
2022-12-05 22:51:20 +0300

    Simple validation of a username with regex

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-function all(arr, fun) {
-    if(arr.length === 0){
-        return true
-    }
-//   const newArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     newArr.push(fun(arr[i]));
-//   }
-//   return newArr.includes(false) ? false : true
-return [...arr.filter(fun)].length !== 0 ? true : false
-}
-console.log(
-  all([1, 2, 3, 4, 5], function (v) {
-    return v > 9;
-  })
-);
+function validateUsr(username) {
+    const result = /^[a-z0-9_]{4,16}$/.test(username)
+    return result
+  }
