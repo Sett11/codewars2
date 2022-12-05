de117f4bf6e36eef7b60c66ca98b3b53d96c8cf3
Jhonny <gonzodzen@mail.ru>
2022-12-05 20:34:48 +0300

    Thinkful - Number Drills: Blue and red marbles

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-function array(arr){
-    let newArr = arr.split(/,/)
-    if(newArr.length <= 2){
-        return null
-    }
-    newArr.shift()
-    newArr.pop()
-    return newArr.join(' ')
-}
-console.log(array('1,2,3,4'))
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    return  ((blueStart - bluePulled) / (blueStart - bluePulled + redStart - redPulled))
+  }
+  console.log(guessBlue(12, 18, 4, 6))
