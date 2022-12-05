847c196f289c74eec69a82472680ce0892a95b13
Jhonny <gonzodzen@mail.ru>
2022-12-05 19:30:39 +0300

    Sum of Multiples

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function trueOrFalse(val){
-    if (val){
-      return 'true'
-    } else{
-      return 'false'
-    }
+function sumMul(n, m) {
+  if (m < n || m <= 0) {
+    return "INVALID";
   }
+  let arr = [];
+  for (let i = n; i < m; i += n) {
+    arr.push(i);
+  }
+  return arr.reduce((x,y) => x + y, 0)
+}
+console.log(sumMul(4, 123));
