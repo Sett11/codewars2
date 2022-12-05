4e56c843cc8bf4eb5457b5f9c30877eb0173be66
Jhonny <gonzodzen@mail.ru>
2022-12-05 20:53:26 +0300

    Simple beads count

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function guessBlue(blueStart, redStart, bluePulled, redPulled) {
-    return  ((blueStart - bluePulled) / (blueStart - bluePulled + redStart - redPulled))
+function countRedBeads(n) {
+    if(n < 2){
+        return 0
+    }
+    let count = 0
+    for(let i = 0; i < n; i++){
+        count += 2
+    }
+    return count -2
   }
-  console.log(guessBlue(12, 18, 4, 6))
+  console.log(countRedBeads(3))
