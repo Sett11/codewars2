51deabbdf68c3f0c970ede86dec5a862e777e139
Jhonny <gonzodzen@mail.ru>
2022-12-05 18:29:51 +0300

    Meeting

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-let TempleStrings = function(obj, feature) {
-    return `${obj} are ${feature}`
-  }
+function meeting(s) {
+    return s.split(/\;/)
+    .map(el => el.split(':').reverse().join(', ').toUpperCase())
+    .sort().map(el => '('+el+')').join('')
+}
+console.log(meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"))
