3c4ddc3a3bf2641ad71c3c7dd023f3f37ab93981
Jhonny <gonzodzen@mail.ru>
2022-12-05 20:56:56 +0300

    Expressions Matter

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-function countRedBeads(n) {
-    if(n < 2){
-        return 0
-    }
-    let count = 0
-    for(let i = 0; i < n; i++){
-        count += 2
-    }
-    return count -2
+function expressionMatter(a, b, c) {
+    let firstSum = a+b+c
+    let sum = a*(b+c)
+    let sum1 = a*b*c
+    let sum2 = a+(b*c)
+    let sum3 = (a+b)*c
+    return Math.max(firstSum, sum, sum1, sum2, sum3)
   }
-  console.log(countRedBeads(3))
+  console.log(expressionMatter(1,1,1))
