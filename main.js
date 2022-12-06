69169aa54d26f8f497c4f5d45ffacb79df1ee6bf
Jhonny <gonzodzen@mail.ru>
2022-12-06 22:23:22 +0300

    UEFA EURO 2016

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function first(arr, n = 1) {
-     return arr.slice(0, n)
+function uefaEuro2016(teams, scores){
+    let res = ''
+    if(scores[0] > scores[1]){
+        res = `${teams[0]} won!`
+    }
+    if(scores[1] > scores[0]){
+        res = `${teams[1]} won!`
+    }
+    if(scores[0] === scores[1]){
+        res = `teams played draw.`
+    }
+    return `At match ${teams[0]} - ${teams[1]}, ${res}`
   }
-  console.log(first(['a', 'b', 'c', 'd', 'e']))
+  console.log(uefaEuro2016(['Portugal', 'Iceland'],[1, 1]))
