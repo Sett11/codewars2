4465ba23d6249f326de168aeab51c3d47c06a434
Jhonny <gonzodzen@mail.ru>
2022-12-06 22:13:52 +0300

    pick a set of first elements

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function getSum( a,b ){
-    let newA = Math.max(a,b)
-    let newB = Math.min(a,b)
-    let result = []
-    let count = newA - newB
-    while(count>=0){
-        result.push(newA--)
-        count--
-    }
-    return result.reduce((acc, curr) => acc + curr, 0)
- }
- console.log(getSum(-1,2))
+function first(arr, n = 1) {
+     return arr.slice(0, n)
+  }
+  console.log(first(['a', 'b', 'c', 'd', 'e']))
