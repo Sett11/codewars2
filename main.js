244f9cfb5286f3abdc9edeae02abe2a3863e3a4e
Jhonny <gonzodzen@mail.ru>
2022-12-06 14:27:00 +0300

    Surface Area and Volume of a Box

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-var encryptThis = function (text) {
-  const first = text.split(" ");
-  return first.map((el) => {
-    if(el.length === 2){
-        const nt = el.charCodeAt(0)
-        const nnt = el.slice(1)
-        return nt + nnt
-    }
-    const newText = el.charCodeAt(0)
-    const nextNewText = el.slice(1);
-    let lastNewText = nextNewText.slice(1, nextNewText.length - 1)
-    const firstS = nextNewText.charAt(0)
-    const lastS = nextNewText.charAt(nextNewText.length - 1)
-    return newText + lastS + lastNewText + firstS
-  }).join(' ')
-};
-console.log(encryptThis("The less he spoke the more he heard"));
+function getSize(width, height, depth){
+  return [(2 * ((width*height) + (height*depth) + (width*depth))), (width * height * depth)]
+}
+console.log(getSize(10, 10, 10))
