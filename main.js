e302afc29a2e14cfa8e163cd073b6b2ec70962f5
Jhonny <gonzodzen@mail.ru>
2022-12-06 22:38:06 +0300

    Convert to Binary

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-function uefaEuro2016(teams, scores){
-    let res = ''
-    if(scores[0] > scores[1]){
-        res = `${teams[0]} won!`
+
+    function toBinary (n) {
+        let bin = (n%2).toString()
+        for(; n > 1; ){
+            n = parseInt(n/2)
+            bin = (n%2) + (bin)
+        }
+        return bin
     }
-    if(scores[1] > scores[0]){
-        res = `${teams[1]} won!`
-    }
-    if(scores[0] === scores[1]){
-        res = `teams played draw.`
-    }
-    return `At match ${teams[0]} - ${teams[1]}, ${res}`
-  }
-  console.log(uefaEuro2016(['Portugal', 'Iceland'],[1, 1]))
+  console.log(toBinary(11))
