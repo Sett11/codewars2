d57dfd5e844e5ae536aae1cc356b936c8deeda38
Jhonny <gonzodzen@mail.ru>
2022-12-06 18:36:29 +0300

    Minimize Sum Of Array (Array Series #1)

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-    return Math.abs(dadYearsOld - sonYearsOld * 2)
+function minSum(arr) {
+    const arr2 = arr.sort((a,b) => a - b)
+    let s = 0
+    for(let i = 0; i < arr.length / 2; i++){
+     s += arr2[i] * arr2[arr2.length - 1 - i]
+    }
+    return s
   }
-  console.log(twiceAsOld(42, 21))
+  console.log(minSum([5,4,2,3]))
