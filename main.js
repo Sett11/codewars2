335892e7b06cf30a220bda711a23079ad5299431
Jhonny <gonzodzen@mail.ru>
2022-12-06 20:36:25 +0300

    Counting Duplicates

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function tidyNumber(n){
-  return n === Number((''+n).split('').map(Number).sort((a,b) => a - b).join(''))
-}
-console.log(tidyNumber(9672))
+function duplicateCount(text){
+    const count = text.toLowerCase().split('').reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1
+        return acc
+    }, {})
+    return [...Object.values(count)].filter(el => el !== 1).length  
+  }
+  console.log(duplicateCount("ABBA"))
