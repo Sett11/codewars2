47ce95c1a8343515c619785a9514d5a4b7487657
Jhonny <gonzodzen@mail.ru>
2022-12-06 11:35:10 +0300

    Love vs friendship

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let greet = function(name) {
-const first = name.charAt(0).toUpperCase()
-const rest = name.slice(1).toLowerCase()
-const result = first + rest
-    return `Hello ${result}!`
-}
-console.log(greet("JACK" ))
+function wordsToMarks(string){
+    let res = 0
+    let arr = string.split('')
+    let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for(let i = 0; i < arr.length; i++){
+        res += arr_en.indexOf(arr[i]) + 1
+    }
+    return res
+  }
+  console.log(wordsToMarks("friends"))
