bc2fb0bb64a54404d998e9aeb04575040efade94
Jhonny <gonzodzen@mail.ru>
2022-12-07 01:46:17 +0300

    Basic Training: Add item to an Array

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-function menFromBoys(arr){
-    const arr1 = []
-    const arr2 = []
-    const set = [...new Set(arr)]
-    for( let i = 0; i < set.length; i++){
-        if(set[i] % 2 === 0){
-            arr1.push(set[i])
-        } else{
-            arr2.push(set[i])
-        }
-    }
-    arr1.sort((a,b) => a-b)
-    arr2.sort((a,b) => b-a)
-    return arr1.concat(arr2)
-}
-console.log(menFromBoys([82,91,72,76,76,100,85]))
+// add the value "codewars" to the already defined websites array
+websites.push('codewars')
