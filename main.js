1293e2473dd4cd4f0687d057c60b46fe81cdb953
Jhonny <gonzodzen@mail.ru>
2022-12-06 15:39:05 +0300

    Find the odd int

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function getSize(width, height, depth){
-  return [(2 * ((width*height) + (height*depth) + (width*depth))), (width * height * depth)]
-}
-console.log(getSize(10, 10, 10))
+function findOdd(A) {
+    const res = A.reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1
+        return acc
+    }, {})
+    const val = [...Object.values(res)]
+    const fil = val.filter(el => el % 2 !== 0)
+    const ind = val.indexOf(fil[0])
+    const key = [...Object.keys(res)]
+    return Number(key[ind])
+  }
+  console.log(findOdd([0,1,0,1,0]))
