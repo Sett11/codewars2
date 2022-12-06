59b73da521864f2a771829352e6c36608e84ce53
Jhonny <gonzodzen@mail.ru>
2022-12-06 13:56:34 +0300

    Encrypt this!

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-function spacify(str) {
-    return str.split('').join(' ')
-  }
-  console.log(spacify('hello world'))
+var encryptThis = function (text) {
+  const first = text.split(" ");
+  return first.map((el) => {
+    if(el.length === 2){
+        const nt = el.charCodeAt(0)
+        const nnt = el.slice(1)
+        return nt + nnt
+    }
+    const newText = el.charCodeAt(0)
+    const nextNewText = el.slice(1);
+    let lastNewText = nextNewText.slice(1, nextNewText.length - 1)
+    const firstS = nextNewText.charAt(0)
+    const lastS = nextNewText.charAt(nextNewText.length - 1)
+    return newText + lastS + lastNewText + firstS
+  }).join(' ')
+};
+console.log(encryptThis("The less he spoke the more he heard"));
