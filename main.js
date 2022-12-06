7c74edaf3842d828331fbe9232c99c7d394c1e89
Jhonny <gonzodzen@mail.ru>
2022-12-06 23:43:00 +0300

    Filter the number

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-function solve(arr) {
-    const newArr = []
-    for(let i = 0; i < arr.length; i++){
-        newArr.push(arr.lastIndexOf(arr[i]))
-    }
-    const set = [...new Set(newArr)].sort((a,b) => a-b)
-    const res = []
-    for(let i = 0; i < set.length; i++){
-        res.push(arr[set[i]])
-    }
-    return res
+var filterString = function(value) {
+    return Number(value.replace(/\D/g, ''))
   }
-  console.log(solve([1,1,4,5,1,2,1]))
+  console.log(filterString("aa1bb2cc3dd"))
