c2ece9a11e98b68c5b9327548fe055b24c5cbb4b
Jhonny <gonzodzen@mail.ru>
2022-12-07 01:30:49 +0300

    Balanced Number (Special Numbers Series #1 )

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-function kebabize(str) {
-    let newStr = str.replace(/\W/g, '').replace(/[A-Z]/g, ' $&').toLowerCase().replace(/\s/g, '-').replace(/\d/g, '').split('')
-     if(newStr[0] === '-'){
-        newStr.splice(0, 1)
-     }
-     return newStr.join('')
-  }
-  console.log(kebabize('-Qny'))
+function balancedNum(number){
+    const newNum = (''+number).split('').map(Number)
+    if(newNum.length <= 2){
+        return "Balanced"
+    }
+    if(newNum.length === 3){
+        if(newNum[0] === newNum[newNum.length-1]){
+            return "Balanced"
+        } else{
+            "Not Balanced"
+        }
+    }
+    if(newNum.length === 5){
+        if(newNum[0]+newNum[1] === newNum[newNum.length-1] + newNum[newNum.length-2]){
+            return "Balanced"
+        } else{
+            "Not Balanced"
+        }
+    }
+    
+    const start = Math.floor(newNum.length / 2) - 1
+    const end = Math.floor(newNum.length / 2) + 1
+    const arr1 = newNum.slice(0, start)
+    const arr2 = newNum.slice(end, newNum.length)
+    return arr1.reduce((x,y) => x+y, 0) === arr2.reduce((x,y) => x+y, 0) ?
+    "Balanced" : 'Not Balanced'
+
+}
+console.log(balancedNum(56239814))
