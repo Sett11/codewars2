b0a765101d00ed225ecf406c8650843117cc992d
Jhonny <gonzodzen@mail.ru>
2022-12-06 15:41:55 +0300

    Grasshopper - Order of operations

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function findOdd(A) {
-    const res = A.reduce((acc, curr) => {
-        acc[curr] = (acc[curr] || 0) + 1
-        return acc
-    }, {})
-    const val = [...Object.values(res)]
-    const fil = val.filter(el => el % 2 !== 0)
-    const ind = val.indexOf(fil[0])
-    const key = [...Object.keys(res)]
-    return Number(key[ind])
+function orderOperations () {
+    return (2 + 2) * (2 + 2) * 2
   }
-  console.log(findOdd([0,1,0,1,0]))
+  console.log(orderOperations())
