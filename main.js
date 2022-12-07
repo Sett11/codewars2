247c53eb1bf7023c15c20e2211c002122e28b820
Jhonny <gonzodzen@mail.ru>
2022-12-07 17:20:05 +0300

    Alternate case

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-// add the value "codewars" to the already defined websites array
-websites.push('codewars')
+function alternateCase(s) {
+    return s.split('').map(el => {
+        if(el === el.toUpperCase()){
+            el = el.toLowerCase()
+            return el
+        } else{
+            return el.toUpperCase()
+        }
+    }).join('')
+  }
+  console.log(alternateCase("ABC"))
