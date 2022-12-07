6cd2a67e967e5d0f0b376f2e53664187bd249152
Jhonny <gonzodzen@mail.ru>
2022-12-07 19:34:57 +0300

    Product Of Maximums Of Array (Array Series #2)

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-const numberToPower = (number, power) => power > 0 ? number * numberToPower(number ,power - 1) : 1
-  console.log(numberToPower(10,0))
+function maxProduct(numbers, size){
+    return numbers.sort((a,b) => b - a).slice(0, size)
+    .reduce((acc, curr) => acc * curr)
+  }
+  console.log(maxProduct([10,3,-27,-1], 3))
