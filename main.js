1a692fbd570ff08bc3525d825efb47bcfd8e3cfc
Jhonny <gonzodzen@mail.ru>
2022-12-07 23:09:03 +0300

    Odd-Even String Sort

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function evil(n) {
-    let bin = (n%2).toString()
-    for(; n > 1; ){
-        n = parseInt(n/2)
-        bin = (n%2) + (bin)
-    }
-    return bin.replace(/0/g, '').length % 2 === 0 ? "It's Evil!" : "It's Odious!"
+function sortMyString(S) {
+    const newS = S.split('')
+    const arr1 = []
+    const arr2 = []
+    newS.map((el, i, newS) => {
+        if(i % 2 === 0){
+            arr1.push(el)
+        } else{
+            arr2.push(el)
+        }
+    })
+    arr1.push(' ')
+    return arr1.concat(arr2).join('')
 }
-console.log(evil(20))
+console.log(sortMyString("YCOLUE'VREER"))
