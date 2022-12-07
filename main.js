89d588cfe801719210c3d8a6685e2779b6952dbf
Jhonny <gonzodzen@mail.ru>
2022-12-08 00:27:45 +0300

    Simple Comparison?

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-function toFreud(string) {
-    return string.length === 0 || string === ' ' ? '' 
-    : string.split(/\s/g).map(el => el = 'sex').join(' ')
-    }
-    console.log(toFreud("You're becoming a true freudian expert"))
+function add(a, b){
+	return a == b
+}
