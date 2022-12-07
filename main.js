233488fb541064beed519538e672cdb0da6905b0
Jhonny <gonzodzen@mail.ru>
2022-12-08 00:56:11 +0300

    Highest Scoring Word

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function add(a, b){
-	return a == b
+function high(x){
+    const arr = x.split(' ').map(el => [...el].map(el => (parseInt(el, 36)- 9)).reduce((acc,curr) => acc + curr))
+    const ind = arr.indexOf(Math.max(...arr))
+    const res = x.split(' ')
+    return res[ind]
 }
