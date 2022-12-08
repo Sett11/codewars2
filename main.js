f518316cc28441c69504adac5a175b336ca9dfaa
Jhonny <gonzodzen@mail.ru>
2022-12-08 12:34:49 +0300

    Fuel Calculator

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-function automorphic(n){
-  const q = n ** 2
-  const newN = (''+n).split('').map(Number)
-  const newQ = (''+q).split('').map(Number)
-  let total = 0
-  if(newQ.length > newN.length){
-    total = newQ.length - newN.length
+function fuelPrice(litres, pricePerLitre) {
+  let count = pricePerLitre
+  if(litres < 2){
+    count = pricePerLitre
+    return Number(((litres * count)).toFixed(2))
   }
-  return newQ.slice(total).join('') === newN.join('') ? 'Automorphic' : 'Not!!'
+  if(litres >= 2 && litres < 4){
+    count = count - 0.05
+    return Number(((litres * count)).toFixed(2))
+  }
+  else if(litres >= 4 && litres < 6){
+    count = count - 0.10
+    return Number(((litres * count)).toFixed(2))
+  }
+  else if(litres >= 6 && litres < 8){
+    count = count - 0.15
+    return Number(((litres * count)).toFixed(2))
+  }
+  else if(litres >= 8 && litres < 10){
+    count = count - 0.2
+    return Number(((litres * count)).toFixed(2))
+  } else{
+    count = count - 0.25
+    return Number(((litres * count)).toFixed(2))
+  }
 }
-console.log(automorphic(9))
+console.log(fuelPrice(5, 1.23))
