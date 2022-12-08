baf54b2ea3c8fb116b109c7ad17f23c958984a9d
Jhonny <gonzodzen@mail.ru>
2022-12-08 16:44:29 +0300

    Pythagorean Triple

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function squares(x, n) {
- return Array.from({length: n}, (_, index) => index == 0 ? x : x*=x)
+function isPythagoreanTriple(integers) {
+  return (integers[0]**2 + integers[1]**2) === integers[2]**2 ? true 
+  : (integers[1]**2 + integers[2]**2) === integers[0]**2 ? true :
+  (integers[2]**2 + integers[0]**2) === integers[1]**2 ? true : false
 }
-console.log(squares(3, 3));
+console.log(isPythagoreanTriple([72, 78, 30]))
+console.log(78**2)
