656622d33c4059d583cf67aeef31733c037bd6cd
Jhonny <gonzodzen@mail.ru>
2022-12-08 20:13:14 +0300

    Maximum Product

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-function divCon(x){
-  const arr1 = []
-  const arr2 = []
-  for( let i = 0; i < x.length; i++){
-    if(typeof x[i] === 'string'){
-      arr1.push(x[i])
-    } else{
-      arr2.push(x[i])
-    }
-  }
-  return arr2.reduce((acc, curr) => acc + curr, 0) - arr1.reduce((acc, curr) => (+acc) + (+curr), 0)
+function adjacentElementsProduct(array) {
+  let arr = []
+for(i=0;i<array.length;i++){
+  arr.push(array[i] * array[i+1])
 }
-console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+for(i=array.length;i>0;i--){
+  arr.push(array[i-1] * array[i])
+}
+let result = arr.sort((a,b)=>b-a)
+return result[0]
+}
+console.log(adjacentElementsProduct([4, 12, 3, 1, 5]))
