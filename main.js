6a9a7b8a7a364d5a7f1fd9c78c9051bf03222214
Jhonny <gonzodzen@mail.ru>
2022-12-08 11:04:15 +0300

    Reverse every other word in the string

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-function outed(meet, boss){
-  const val = Object.values(meet)
-  const key = Object.keys(meet)
-  const searchInd = key.indexOf(boss)
-  val[searchInd] = val[searchInd] * 2
-  const res = val.reduce((acc, curr) => acc + curr)
-  return res / key.length <= 5 ? 'Get Out Now!' : 'Nice Work Champ!'
+function reverse(str){
+  return str.trim().split(' ').map((el,i) => {
+    if(i % 2 !== 0){
+      return el.split('').reverse().join('')
+    } else{
+      return el
+    }
+  }).join(' ')
 }
-console.log(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie'))
+console.log(reverse("Reverse this string, please!"))
