f564db8cb89efb68808c4a32f4a26b11c2f3b29f
Jhonny <gonzodzen@mail.ru>
2022-12-08 13:17:23 +0300

    Alphabet symmetry

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function isVow(a){
-  return a.map((el) => {
-    const transform = String.fromCharCode(el)
-    if(transform === 'a' || transform === 'e' || transform === 'i' || transform === 'o' || transform === 'u'){
-      el = transform
-      return el
-    } else{
-      return el
-    }
-  })
+function solve(arr) {
+  return arr.map((el) =>
+    [...el]
+      .map((el, i) => {
+        if (i === parseInt(el, 36) - 10) {
+          el = 1;
+          return el;
+        } else {
+          el = 2;
+          return el;
+        }
+      })
+      .filter((el) => el !== 2)
+      .reduce((acc, curr) => acc + curr, 0)
+  );
 }
-console.log(isVow([118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]))
+console.log(solve(["encode", "abc", "xyzD", "ABmD"]));
