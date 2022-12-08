d82446eaa4201165ec34703aaaa01615ccf03bcf
Jhonny <gonzodzen@mail.ru>
2022-12-08 22:53:25 +0300

    Word a10n (abbreviation)

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-function sumTriangularNumbers(n) {
-  return n < 0 ? 0: n * (n + 1) * (n + 2) / 6
+function abbreviate(string) {
+  return string.replace(/\w{4,}/g, function(w) { return w[0] + (w.length - 2) + w[w.length - 1] });
 }
