039435b2e721eb54f4b593094719bebe4f068960
Jhonny <gonzodzen@mail.ru>
2022-12-08 12:07:08 +0300

    Automorphic Number (Special Numbers Series #6)

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const multiplyAll = arr => num => arr.map(el => el * num)
-
-console.log(multiplyAll([1,2,3])(2))
+function automorphic(n){
+  const q = n ** 2
+  const newN = (''+n).split('').map(Number)
+  const newQ = (''+q).split('').map(Number)
+  let total = 0
+  if(newQ.length > newN.length){
+    total = newQ.length - newN.length
+  }
+  return newQ.slice(total).join('') === newN.join('') ? 'Automorphic' : 'Not!!'
+}
+console.log(automorphic(9))
