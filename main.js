41b9fbc9a4f85da0984237be3140680494116322
Jhonny <gonzodzen@mail.ru>
2022-12-08 19:59:39 +0300

    Divide and Conquer

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function explode(s) {
-  return s.split('').map((el, i) => {
-   return el.toString().repeat(el)
-  }).filter(el => el !== '').join('')
+function divCon(x){
+  const arr1 = []
+  const arr2 = []
+  for( let i = 0; i < x.length; i++){
+    if(typeof x[i] === 'string'){
+      arr1.push(x[i])
+    } else{
+      arr2.push(x[i])
+    }
+  }
+  return arr2.reduce((acc, curr) => acc + curr, 0) - arr1.reduce((acc, curr) => (+acc) + (+curr), 0)
 }
-console.log(explode("102269"))
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
