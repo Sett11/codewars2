6b71e84f3ba232635abb87877bdadaed8b306393
Jhonny <gonzodzen@mail.ru>
2022-12-09 13:16:23 +0300

    Primorial Of a Number

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-function extraPerfect(n){
-  const arr = []
-  for(let i = 1; i <= n; i += 2){
-    arr.push(i)
+function numPrimorial(n) {
+  let arr = [];
+  let x = 1;
+  while (x < 10000) {
+    arr.push(x);
+    x++;
   }
-  return arr
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 2; j < arr[i]; j++) {
+      if (arr[i] % j === 0) {
+        arr[i] = null;
+      } else {
+        arr[i] = arr[i];
+      }
+    }
+  }
+  const newArr = arr.filter((el) => el !== null && el !== 1);
+  const nextArr = newArr.slice(0, n);
+  return nextArr.reduce((acc, curr) => acc * curr);
 }
-console.log(extraPerfect(7))
+console.log(numPrimorial(3));
