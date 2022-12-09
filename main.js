213e4fffe15f605a6f02fab8bdd8e65c43625e7b
Jhonny <gonzodzen@mail.ru>
2022-12-09 11:55:02 +0300

    Special Number (Special Numbers Series #5)

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function Hero (name = 'Hero') {
-  this.name = name,
-  this.position = '00',
-  this.health = 100,
-  this.damage = 5,
-  this.experience = 0
+function specialNumber(n) {
+  return ("" + n)
+    .split("")
+    .map(Number)
+    .filter((el) => el <= 5 && el >= 0).length === ("" + n)
+    .split("")
+    .map(Number).length
+    ? "Special!!"
+    : "NOT!!";
 }
+console.log(specialNumber(2553));
