989a0650bad2a73baada187ac163e7f3cd3a3468
Jhonny <gonzodzen@mail.ru>
2022-12-10 20:51:36 +0300

    The Vowel Code

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
-function getASCII(c){
-  return c.charCodeAt()
+function encode(string) {
+  return string.split('').map((el) => {
+    if(el=== 'a'){
+      el = '1'
+      return el
+    }
+    if(el=== 'e'){
+      el = '2'
+      return el
+    }
+    if(el=== 'i'){
+      el = '3'
+      return el
+    }
+    if(el=== 'o'){
+      el = '4'
+      return el
+    }
+    if(el=== 'u'){
+      el = '5'
+      return el
+    } else{
+      return el
+    }
+  }).join('')
 }
-console.log(getASCII('A'))
+
+function decode(string) {
+  return string.split('').map((el) => {
+    if(el=== '1'){
+      el = 'a'
+      return el
+    }
+    if(el=== '2'){
+      el = 'e'
+      return el
+    }
+    if(el=== '3'){
+      el = 'i'
+      return el
+    }
+    if(el=== '4'){
+      el = 'o'
+      return el
+    }
+    if(el=== '5'){
+      el = 'u'
+      return el
+    } else{
+      return el
+    }
+  }).join('')
+}
