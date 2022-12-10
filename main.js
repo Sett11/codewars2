a9930b3ec1a4392a4a88454ccb1282f6b96dfda3
Jhonny <gonzodzen@mail.ru>
2022-12-10 13:28:22 +0300

    Recursion #1 - Factorial

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
-function numPrimorial(n) {
-  let arr = [];
-  let x = 1;
-  while (x < 10000) {
-    arr.push(x);
-    x++;
-  }
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 2; j < arr[i]; j++) {
-      if (arr[i] % j === 0) {
-        arr[i] = null;
-      } else {
-        arr[i] = arr[i];
-      }
-    }
-  }
-  const newArr = arr.filter((el) => el !== null && el !== 1);
-  const nextArr = newArr.slice(0, n);
-  return nextArr.reduce((acc, curr) => acc * curr);
-}
-console.log(numPrimorial(3));
+const factorial = n => n <= 0 ? 1 : n * factorial(n-1)
+console.log(factorial(5))
