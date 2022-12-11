3dfef924ec83bf0349d5598889d2134c07b7d69b
Jhonny <gonzodzen@mail.ru>
2022-12-11 18:36:34 +0300

    Transform To Prime

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-function maxTriSum(numbers){
-  const arr = [...new Set(numbers.sort((a,b) => b - a))]
-  return arr[0] + arr[1] + arr[2]
+function minimumNumber(numbers){
+  const arr = []
+  const sum = numbers.reduce((acc, curr) => acc + curr)
+  let counter = 2
+  while(counter < sum+100){
+    arr.push(counter)
+    counter++
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 2; j < arr[i]; j++) {
+      if (arr[i] % j === 0) {
+        arr[i] = null;
+      } else {
+        arr[i] = arr[i];
+      }
+    }
+  }
+ const newArr = arr.filter(el => el !== null)
+ let res = 0
+ for(let i = 0; i < newArr.length; i++){
+  if(newArr[i] < sum){
+    continue
+  } else{
+    res = newArr[i]
+    break
+  }
+ }
+ return res-sum
 }
-console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]))
+console.log(minimumNumber([2,12,8,4,6]))
