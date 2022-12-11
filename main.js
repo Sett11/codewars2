1a9333ed8cae63a56ff6b8be1d5d8e2e149a970d
Jhonny <gonzodzen@mail.ru>
2022-12-11 17:38:27 +0300

    Maximum Triplet Sum (Array Series #7)

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-function maxGap (numbers){
-  const arr = numbers.sort((a, b) => a - b)
-  const arr2 = []
-  for(let i = 0; i < arr.length; i++){
-    arr2.push(Math.abs(arr[i] - arr[i + 1]))
-  }
-  const res = arr2.sort((a, b) => b - a)
-  return res[0]
+function maxTriSum(numbers){
+  const arr = [...new Set(numbers.sort((a,b) => b - a))]
+  return arr[0] + arr[1] + arr[2]
 }
-console.log(maxGap([-7,-42,-809,-14,-12]))
+console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]))
