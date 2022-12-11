f97c9f511891fc4c217033ac2226aa8e51b9c16c
Jhonny <gonzodzen@mail.ru>
2022-12-11 13:35:39 +0300

    Basic Calculator

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-function encode(str,  n){
-  const arr = str.split('').map((el) =>{
-    el = parseInt(el, 36) - 9
-    return el
-  })
-  const newN = n.toString().repeat(arr.length)
-  const nextNewN = (''+newN).split('').map(Number)
-  for(let i = 0; i < arr.length; i++){
-    arr[i] += nextNewN[i]
-  }
-  return arr
-}
-
-console.log(encode("masterpiece",1939))
+function calculate(num1, operation, num2) {
+  return operation === '+' ? num1+num2 :
+   operation === '-' ? num1-num2 :
+   operation === '*' ? num1*num2 : 
+   operation === '/' && num2 === 0 ? null :
+   operation === '/' ? num1/num2 : null
+ }
+ console.log(calculate(3.2,"/", 0))
