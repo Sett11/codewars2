7baa201940249f83b79cfdf18beea61518490b91
Jhonny <gonzodzen@mail.ru>
2022-12-11 13:29:18 +0300

    Digital cypher

--- main.js
+++ main.js
@@ -1,47 +1,14 @@
-function correctPolishLetters (string) {
-  return string.split('').map((el) => {
-    if(el === 'ą'){
-      el = 'a'
-      return el
-    }
-    if(el === 'ą'){
-      el = 'a'
-      return el
-    }
-    if(el === 'ć'){
-      el = 'c'
-      return el
-    }
-    if(el === 'ę'){
-      el = 'e'
-      return el
-    }
-    if(el === 'ł'){
-      el = 'l'
-      return el
-    }
-    if(el === 'ń'){
-      el = 'n'
-      return el
-    }
-    if(el === 'ó'){
-      el = 'o'
-      return el
-    }
-    if(el === 'ś'){
-      el = 's'
-      return el
-    }
-    if(el === 'ź'){
-      el = 'z'
-      return el
-    }
-    if(el === 'ż'){
-      el = 'z'
-      return el
-    } else{
-      return el
-    }
-  }).join('')
+function encode(str,  n){
+  const arr = str.split('').map((el) =>{
+    el = parseInt(el, 36) - 9
+    return el
+  })
+  const newN = n.toString().repeat(arr.length)
+  const nextNewN = (''+newN).split('').map(Number)
+  for(let i = 0; i < arr.length; i++){
+    arr[i] += nextNewN[i]
+  }
+  return arr
 }
-console.log(correctPolishLetters("Jędrzej Błądziński"))
+
+console.log(encode("masterpiece",1939))
