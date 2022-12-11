12e101cf56cc047984e2c3fbc599230c57fa6dd4
Jhonny <gonzodzen@mail.ru>
2022-12-11 16:07:27 +0300

    Maximum Gap (Array Series #4)

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-function add(x, y) {
-  if(x === 0){
-    return y
+function maxGap (numbers){
+  const arr = numbers.sort((a, b) => a - b)
+  const arr2 = []
+  for(let i = 0; i < arr.length; i++){
+    arr2.push(Math.abs(arr[i] - arr[i + 1]))
   }
-  else if(y === 0){
-    return x
-  } else{
-    const sum = x^y
-  const count = (x & y) << 1
-  return add(sum, count)
-  }
+  const res = arr2.sort((a, b) => b - a)
+  return res[0]
 }
-console.log(add(23, 17))
+console.log(maxGap([-7,-42,-809,-14,-12]))
