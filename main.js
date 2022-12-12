e72bec63373e9ab21ef2cdb393ab584cb8bfcc18
Jhonny <gonzodzen@mail.ru>
2022-12-13 00:04:18 +0300

    Sum of Pairs

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const splitInParts = function(s, partLength){
-  const arr = []
-  const newS = s.split('')
-  for(let i = 0; i < Math.ceil(newS.length/partLength); i++){
-    arr[i] = newS.slice(i * partLength, i * partLength + partLength)
+function sumPairs(ints, s) {
+  const newS = new Set();
+  for (let i = 0; i < ints.length; i++) {
+    let curr = ints[i];
+    if (newS.has(s - curr)) return [s - curr, curr];
+    else newS.add(curr);
   }
-  return arr.map(el => el.join('')).join(' ')
 }
-console.log(splitInParts("supercalifragilisticexpialidocious", 3))
+console.log(sumPairs([10, 5, 2, 3, 7, 5], 10));
