1c9a79b9f5cfd3d4326874ed059db9ca2e39e802
Jhonny <gonzodzen@mail.ru>
2022-12-13 02:03:59 +0300

    Average Scores

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-function decipherThis(str) {
-  const first = str.split(' ')
-  return first.map((el) => {
-    if(el.length === 2){
-      const ns = String.fromCharCode(el[0])
-      const nns = el.slice(1)
-      return ns + nns
-    }
-    const newStr = String.fromCharCode(el[0])
-    const nextNewStr = el.slice(1)
-    let lastNewStr = nextNewStr.slice(1, nextNewStr.length - 1)
-    const first = nextNewStr.charAt(0)
-    const last = nextNewStr.charAt(nextNewStr.length - 1)
-    return newStr + last + lastNewStr + first
-  }).join(' ')
-  }; 
-console.log(decipherThis('72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o'));
+function average(scores) {
+  const s = scores.reduce((acc, curr) => acc + curr) / scores.length
+  return s < Math.ceil(s) - 0.5 ? Math.floor(s): Math.ceil(s)
+}
+console.log(average([90,98,89,100,100,86,94]))
