e0dcb4e4829d1de5f4cae9e936ebd06e518abc09
Jhonny <gonzodzen@mail.ru>
2022-12-12 20:56:14 +0300

    All Star Code Challenge #22

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function twoHighest(arr) {
-  const arr1 =  [...new Set(arr)].sort((a,b) => b-a)
-  return [arr1[0], arr1[1]].filter(el => el !== undefined)
-}
-console.log(twoHighest([15]))
+function toTime(seconds) {
+  const min = (seconds / 60).toFixed(2) % 60
+  const hour = (seconds / 3600).toFixed(2)
+  return `${Math.floor(hour)} hour(s) and ${Math.floor(min)} minute(s)`
+  }
+console.log(toTime(0))
