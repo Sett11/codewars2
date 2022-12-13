5475efbe39f4716020d98673e1c777f709ace0e3
Jhonny <gonzodzen@mail.ru>
2022-12-14 01:27:56 +0300

    The old switcheroo

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const sumOfIntegersInString = (s) => s.replace(/\D/g, '-').split('-').filter(el => el !== '').map(Number).reduce((acc, curr) => acc+curr,0)
-
-console.log(sumOfIntegersInString("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"))
+function vowel2index(str) {
+  return str.split('').map((el,i) => {
+    if(el.toLowerCase() === 'a' || el.toLowerCase() === 'u' || el.toLowerCase() === 'i' || el.toLowerCase() === 'e' || el.toLowerCase() === 'o'){
+      el = i + 1
+      return el
+    } else{
+      return el
+    }
+  }).join('')
+}
+console.log(vowel2index('this is my string'))
