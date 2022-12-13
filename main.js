889cf6885207a553eb2b387e84e134a8057f2c30
Jhonny <gonzodzen@mail.ru>
2022-12-13 20:44:58 +0300

    Tribonacci Sequence

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-var ArrowFunc = function(arr) {
-  return arr.map(el => String.fromCharCode(el)).join(''); //Complete this function
+function tribonacci(signature,n){
+  if(n <=0){
+    return[]
+  }
+  if(n === 1){
+    return [signature[0]]
+  }
+  if(n === 2){
+    return [signature[0], signature[1]]
+  }
+  signature.push(signature.reduce((acc, curr) => acc+curr))
+  for(let i = 1; i < n-3; i++){
+    signature.push(signature.slice(i, signature.length).reduce((acc, curr) => acc + curr))
+  }
+  return signature
 }
+console.log(tribonacci([13, 16, 3], 2))
