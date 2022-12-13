69f2bc3281134dcaae30fa12d928094f7639b681
Jhonny <gonzodzen@mail.ru>
2022-12-13 20:11:19 +0300

    Word values

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const lostSheep = (friday,saturday,total) => total - friday.concat(saturday).reduce((acc, curr) => acc + curr, 0)
-
-console.log(lostSheep([2,7,13,17],[23,56,44,12,1,2,1,],255))
+function wordValue(a) {
+  return a.map((el) => {
+    if(el){
+     return el.replace(/\s/g, '').split('').map((el) => {
+      return parseInt(el, 36) - 9
+     }).map(el => Number.isNaN(el) ? el = 0: el = el).reduce((acc, curr) => acc+curr,0)
+    }
+  }).map(el => !!el === false ? el = 0: el = el).map((el, ind ) => el * (ind + 1) )
+}
+console.log(wordValue(["abc abc","abc abc","abc","abc"]))
