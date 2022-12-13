05d7035df908e2ff9722fc91fecb1d035468d4b8
Jhonny <gonzodzen@mail.ru>
2022-12-14 00:32:33 +0300

    Sum of integers in string

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function pairs(ar){
-  let arr = []
-  for(let i = 0; i < Math.ceil(ar.length/2); i++){
-    arr.push(ar.slice(i * 2, (i*2) + 2))
-  }
-  let counter = 0
-  for(let i = 0; i < arr.length; i++){
-    for(let j = 0; j < arr[i].length; j++){
-      if(arr[i][1] - arr[i][0] === 1 || arr[i][0] - arr[i][1] === 1){
-        counter += 1
-      }
-    }
-  }
-  return counter/2
- }
- console.log(pairs([81, 44, 80, 26, 12, 27, -34, 37, -35]))
+const sumOfIntegersInString = (s) => s.replace(/\D/g, '-').split('-').filter(el => el !== '').map(Number).reduce((acc, curr) => acc+curr,0)
+
+console.log(sumOfIntegersInString("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"))
