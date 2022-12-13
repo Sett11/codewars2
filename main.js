36a0cb6158af0169f6fe0eaa31254281a6e41738
Jhonny <gonzodzen@mail.ru>
2022-12-13 20:18:59 +0300

    Take an Arrow to the knee, Functionally

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function wordValue(a) {
-  return a.map((el) => {
-    if(el){
-     return el.replace(/\s/g, '').split('').map((el) => {
-      return parseInt(el, 36) - 9
-     }).map(el => Number.isNaN(el) ? el = 0: el = el).reduce((acc, curr) => acc+curr,0)
-    }
-  }).map(el => !!el === false ? el = 0: el = el).map((el, ind ) => el * (ind + 1) )
+var ArrowFunc = function(arr) {
+  return arr.map(el => String.fromCharCode(el)).join(''); //Complete this function
 }
-console.log(wordValue(["abc abc","abc abc","abc","abc"]))
