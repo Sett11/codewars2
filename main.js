7cf65e36cfc26f55b4a7c07f9a00a0378e5f8bb6
Jhonny <gonzodzen@mail.ru>
2022-12-15 00:35:39 +0300

    Math Issues

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function remove (string) {
-  const res = string.split('').map(el => el === '!' ? el = '&': el=el).filter(el => el !== '&')
-  res.push('!')
-  return res.join('')
-}
-console.log(remove("Hi"))
+Math.round1 = function(number) {
+  return (number ^ 0) === number ? number: (number + 0.5) > Number(number.toFixed(0)) ? Number(number.toFixed(0)):
+  (number + 0.5) === Number(number.toFixed(0)) ? number + 0.5 : null
+};
+
+Math.ceil1 = function(number) {
+  return number === 0 ? 0: (number ^ 0) === number ? number : Number(number.toString().replace(/\..+/g, '')) + 1
+};
+
+Math.floor1 = function(number) {
+  return (number ^ 0) === number ? number: Number(number.toString().replace(/\..+/g, ''))
+};
+console.log(Math.round1(1))
+ console.log(Math.ceil1(1))
+ console.log(Math.floor1(1))
