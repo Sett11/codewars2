41e901722ef1ff2f776880f53cec633d339a89ae
Jhonny <gonzodzen@mail.ru>
2022-12-15 00:47:11 +0300

    By 3, or not by 3? That is the question . . .

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-Math.round1 = function(number) {
-  return (number ^ 0) === number ? number: (number + 0.5) > Number(number.toFixed(0)) ? Number(number.toFixed(0)):
-  (number + 0.5) === Number(number.toFixed(0)) ? number + 0.5 : null
-};
-
-Math.ceil1 = function(number) {
-  return number === 0 ? 0: (number ^ 0) === number ? number : Number(number.toString().replace(/\..+/g, '')) + 1
-};
-
-Math.floor1 = function(number) {
-  return (number ^ 0) === number ? number: Number(number.toString().replace(/\..+/g, ''))
-};
-console.log(Math.round1(1))
- console.log(Math.ceil1(1))
- console.log(Math.floor1(1))
+function divisibleByThree(str){
+  const n = str.split('').map(Number).reduce((acc, curr) => acc + curr, 0) / 3
+  return Number.isInteger(n)
+}
+console.log(divisibleByThree('123'))
