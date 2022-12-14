54742e84fd08712d1fc0d9cf7471bff358336866
Jhonny <gonzodzen@mail.ru>
2022-12-14 23:41:37 +0300

    Basic Math (Add or Subtract)

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-class Cat extends Animal {
-  speak() {
-    return `${this.name} meows.`
-  }
-}
+const calculate = (str) => eval(str.replace(/plus/gi, '+').replace(/minus/gi, '-')).toString()
+  
+  console.log(calculate("1minus2minus3minus4"))
