1e2ea4c46b4863b1c277f8d22525d0a9af5984ab
Jhonny <gonzodzen@mail.ru>
2022-12-15 02:06:54 +0300

    Equal Sides Of An Array

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-var filterLucky = (x) =>
-  x.map((el) => (el = [el].join("").split("").map(Number))).filter(el => el.includes(7)).map(el => Number(el.join('')))
-console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17]));
+function findEvenIndex(arr) {
+  if(arr.reduce((acc, curr) => acc + curr, 0) - arr[0] === 0){
+    return 0
+  }
+    let res = 0;
+    let left = 0;
+    let rigth = 0;
+    for (let i = 1; i < arr.length; i++) {
+      left = arr.slice(0, i).reduce((acc, curr) => acc + curr, 0);
+      rigth = arr.slice(i + 1, arr.length).reduce((acc, curr) => acc + curr, 0);
+      if (left === rigth) {
+        res = i;
+        return res
+      }
+    }
+    return -1
+}
+console.log(findEvenIndex([20,10,-80,10,10,15,35]))
