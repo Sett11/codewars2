10b7c3a405b1b5d38f733b610362b6ad14aa076f
Jhonny <gonzodzen@mail.ru>
2022-12-14 22:31:18 +0300

    String Merge!

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
-function decipherThis(str) {
-  const first = str.split(" ");
-  return first
-    .map((el) => {
-      if (el.replace(/\D/gi, "") == Number(el)) {
-        el = String.fromCharCode(el);
-        return el;
-      }
-      if (el.length === 2) {
-        const ns = String.fromCharCode(el.replace(/\D/gi, ""));
-        const nns = el[1];
-        return ns + nns;
-      }
-      if (el.replace(/\d/g, "").length === 1) {
-        return (
-          String.fromCharCode(el.replace(/\D/gi, "")) + el.replace(/\d/g, "")
-        );
-      }
-      const newStr = String.fromCharCode(el.replace(/\D/gi, ""));
-      let elem = el.replace(/\d/g, "");
-      const nextNewStr = elem.charAt(0);
-      const slicEl = elem.slice(1, elem.length - 1);
-      const last = elem.slice(elem.length - 1);
-      return newStr + last + slicEl + nextNewStr;
-    })
-    .join(" ");
+function stringMerge(string1, string2, letter){
+  const s1 = string1.split('')
+  const s2 = string2.split('')
+  const ind1 = s1.indexOf(letter)
+  const ind2 = s2.indexOf(letter)
+  return s1.slice(0, ind1).concat(s2.slice(ind2, s2.length)).join('')
 }
-console.log(
-  decipherThis("72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o")
-);
-//'Have a go at this and see how you do'
+console.log(stringMerge("coding", "anywhere", "n"))
