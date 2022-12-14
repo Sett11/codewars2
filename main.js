1a4d100acc9f6e38f66a00f739077cf2e4dac496
Jhonny <gonzodzen@mail.ru>
2022-12-15 02:50:45 +0300

    Changing letters

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function checkThreeAndTwo(array) {
-  const arr = array.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1
-    return acc
-  }, {})
-  const res = Object.values(arr)
-  return res[0] === 2 && res[1] === 3 || res[1] === 2 && res[0] === 3 ? true : false
+function swap(string) {
+  return string
+    .replace(/a/g, "A")
+    .replace(/u/g, "U")
+    .replace(/o/g, "O")
+    .replace(/i/g, "I")
+    .replace(/e/g, "E");
 }
-console.log(checkThreeAndTwo(["a", "a", "a", "a", "b"]))
+console.log(swap("HelloWorld!"));
