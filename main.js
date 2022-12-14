27535038affd80290f84458ab8e4207d0292c515
Jhonny <gonzodzen@mail.ru>
2022-12-15 01:12:31 +0300

    Find the lucky numbers

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-function SafeInteger(n) {
-  return Number.isSafeInteger(n)
-}
+var filterLucky = (x) =>
+  x.map((el) => (el = [el].join("").split("").map(Number))).filter(el => el.includes(7)).map(el => Number(el.join('')))
+console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17]));
