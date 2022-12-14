649b9f6951d2fd2c525d8d004dddd8c44bf4d767
Jhonny <gonzodzen@mail.ru>
2022-12-15 02:46:32 +0300

    Check three and two

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-function findEvenIndex(arr) {
-  if(arr.reduce((acc, curr) => acc + curr, 0) - arr[0] === 0){
-    return 0
-  }
-    let res = 0;
-    let left = 0;
-    let rigth = 0;
-    for (let i = 1; i < arr.length; i++) {
-      left = arr.slice(0, i).reduce((acc, curr) => acc + curr, 0);
-      rigth = arr.slice(i + 1, arr.length).reduce((acc, curr) => acc + curr, 0);
-      if (left === rigth) {
-        res = i;
-        return res
-      }
-    }
-    return -1
+function checkThreeAndTwo(array) {
+  const arr = array.reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1
+    return acc
+  }, {})
+  const res = Object.values(arr)
+  return res[0] === 2 && res[1] === 3 || res[1] === 2 && res[0] === 3 ? true : false
 }
-console.log(findEvenIndex([20,10,-80,10,10,15,35]))
+console.log(checkThreeAndTwo(["a", "a", "a", "a", "b"]))
