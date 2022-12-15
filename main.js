178a961ad6b9a2303ffbce7a9c77d27749e02b6c
Jhonny <gonzodzen@mail.ru>
2022-12-15 20:05:20 +0300

    Interview Question (easy)

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-function evensAndOdds(num){
-	return num % 2 === 0 ? num.toString(2): num.toString(16)
+function getStrings(city) {
+  const obj = city
+    .toLowerCase()
+    .replace(/\s/g, "")
+    .replace(/\d/g, "")
+    .split("")
+    .reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, {});
+  return Object.entries(obj)
+    .flat()
+    .map((el) => {
+      if (el === " ") {
+        el = 0;
+        return el;
+      }
+      if (typeof el === "number") {
+        el = ":" + "*".repeat(el) + ",";
+        return el;
+      } else {
+        return el;
+      }
+    })
+    .join("")
+    .replace(/,$/, "");
 }
-console.log(evensAndOdds(13))
+console.log(getStrings("L1as V0egas"));
