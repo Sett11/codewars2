d5821f619abe7b8ecba23a4c4e21026cb1652660
Jhonny <gonzodzen@mail.ru>
2022-12-15 22:22:08 +0300

    Count the smiley faces!

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-function bingo(ticket, win) {
-  const arr = ticket.map((el) => {
-    return [el[0].split(""), el[1]];
-  });
-  let counter = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i][0].length; j++) {
-      arr[i][0][j] = arr[i][0][j].charCodeAt(arr[i][0][j]);
-      if (arr[i][0][j] === arr[i][0][j]) {
-      }
-      if (arr[i][0][j] === arr[i][1]) {
-        counter += 1;
-        break;
-      }
-    }
-  }
-  return counter < win ? "Loser!" : "Winner!";
+function countSmileys(arr) {
+  return arr
+    .join(" ")
+    .split(/\s/g)
+    .filter((el) => {
+      return (
+        el === ":D" ||
+        el === ":~)" ||
+        el === ";~D" ||
+        el === ":)" ||
+        el === "-D" ||
+        el === ":~)" ||
+        el === ";-D" ||
+        el === ";D" ||
+        el === ";)" ||
+        el === ":~)" ||
+        el === ":-)" ||
+        el === ":-D" ||
+        el === ":~D" ||
+        el === ":)" ||
+        el === ";-)" ||
+        el === ":~)" ||
+        el === ":)" ||
+        el === ";~)"
+      );
+    }).length;
 }
-console.log(
-  bingo(
-    [
-      ["BGRWIML", 71],
-      ["ZYHHZOO", 80],
-      ["WKZVG", 79],
-      ["YZLNDWO", 87],
-      ["IXNBE", 69],
-      ["CMDCHWIC", 67],
-      ["XPNI", 65],
-      ["DBSHG", 80],
-      ["RBD", 77],
-      ["WQXJRE", 85],
-    ],
-    5
-  )
-);
