f0ca9eb2b1297cb48a406b5dd7f56e49f1b6d58f
Jhonny <gonzodzen@mail.ru>
2022-12-15 12:45:20 +0300

    Where is my parent!?(cry)

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-function remove(s,n){
-  const arr = s.split('')
-  const store = []
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] === '!'){
-      arr[i] = 0
-      store.push(arr[i])
-    }
-    if(store.length === n){
-      break
-    }
+function findChildren(dancingBrigade) {
+  let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	const arr = dancingBrigade.toLowerCase().split('').sort()
+  const arr2 = []
+  for(let i = 0; i < arr_en.length; i++){
+    if(arr.includes(arr_en[i]) !== -1)
+    arr2.push(arr.indexOf(arr_en[i]))
   }
-  return arr.filter(el => el !== 0).join('')
+  const res = arr2.filter(el => el!== -1)
+ for(let i = 0; i < arr.length; i++){
+  arr[res[i]] = arr[res[i]].toUpperCase()
+  if(res[i] === res[res.length - 1]){
+    break
+  }
+ }
+  return arr.join('')
 }
-console.log(remove("!!!Hi !!hi!!! !hi",1))
+console.log(findChildren("uwwWUueEe"))
