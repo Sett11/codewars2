ace4ecae5bc9d70da80e14889507b18e80f2d474
Jhonny <gonzodzen@mail.ru>
2022-12-15 23:37:39 +0300

    New Cashier Does Not Know About Space or Shift

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-function countSmileys(arr) {
-  return arr
-    .join(" ")
-    .split(/\s/g)
-    .filter((el) => {
-      return (
-        el === ":D" ||
-        el === ":~)" ||
-        el === ";~D" ||
-        el === ":)" ||
-        el === "-D" ||
-        el === ":~)" ||
-        el === ";-D" ||
-        el === ";D" ||
-        el === ";)" ||
-        el === ":~)" ||
-        el === ":-)" ||
-        el === ":-D" ||
-        el === ":~D" ||
-        el === ":)" ||
-        el === ";-)" ||
-        el === ":~)" ||
-        el === ":)" ||
-        el === ";~)"
-      );
-    }).length;
+function getOrder(input) {
+  const menu = [
+    "burger",
+    "fries",
+    "chicken",
+    "pizza",
+    "sandwich",
+    "onionrings",
+    "milkshake",
+    "coke",
+  ];
+  const arr = [];
+  for (let el of menu) {
+    arr.push(input.match(new RegExp(el, "gi")));
+  }
+  return []
+    .concat(...arr)
+    .filter((el) => el)
+    .map((el) => el.charAt(0).toUpperCase() + el.slice(1).toLowerCase())
+    .join(" ");
 }
