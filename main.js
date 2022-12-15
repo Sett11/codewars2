c01fa1c96bef631484aa771c7d7e08320f71c7e2
Jhonny <gonzodzen@mail.ru>
2022-12-15 10:40:09 +0300

    Sum of a sequence

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-function makeString(s) {
-  return s
-    .split(" ")
-    .map((el) => {
-      const first = el.charAt(0);
-      return first;
-    })
-    .join("");
+const sequenceSum = (begin, end, step) => {
+  const arr = []
+  for(let i = begin; i <= end; i+=step){
+    arr.push(i)
+  }
+  return arr.reduce((acc, curr) => acc + curr, 0)
 }
-
-console.log(makeString("brown eyes are nice"));
+console.log(sequenceSum(2,2,2))
