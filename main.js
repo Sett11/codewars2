6cce57b58c46d46d30dece7d58e52f9dc8eee119
Jhonny <gonzodzen@mail.ru>
2022-12-17 14:40:11 +0300

    Compare Strings by Sum of Chars

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const sameCase = (a, b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)): -1
+function compare(s1, s2) {
+  if(s1 === null){
+    s1 = ''
+  }
+  if(s2 === null){
+    s2 = ''
+  }
+  if(s1.replace(/[A-Za-z]/g, '').length > 0){
+    s1 = ''
+  }
+  if(s2.replace(/[A-Za-z]/g, '').length > 0){
+    s2 = ''
+  }
+  let newS1 = s1.toUpperCase().split('').map((el,i) => el.charCodeAt(el))
+  let newS2 = s2.toUpperCase().split('').map((el,i) => el.charCodeAt(el))
+  return  newS1.reduce((acc, curr) => acc+curr, 0) === newS2.reduce((acc, curr) => acc+curr, 0)
+ }
+ console.log(compare(null, ""))
