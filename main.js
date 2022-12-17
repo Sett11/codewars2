9c97d6b86f0b6bfcb55cc081b7c38b4b690a1ae2
Jhonny <gonzodzen@mail.ru>
2022-12-17 14:57:08 +0300

    Product Array (Array Series #5)

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-function compare(s1, s2) {
-  if(s1 === null){
-    s1 = ''
+function productArray(numbers){
+  const arr = []
+  let counter = 0
+  while(counter < numbers.length){
+    arr.push(numbers.reduce((acc, curr) => acc * curr) / numbers[counter])
+    counter++
   }
-  if(s2 === null){
-    s2 = ''
-  }
-  if(s1.replace(/[A-Za-z]/g, '').length > 0){
-    s1 = ''
-  }
-  if(s2.replace(/[A-Za-z]/g, '').length > 0){
-    s2 = ''
-  }
-  let newS1 = s1.toUpperCase().split('').map((el,i) => el.charCodeAt(el))
-  let newS2 = s2.toUpperCase().split('').map((el,i) => el.charCodeAt(el))
-  return  newS1.reduce((acc, curr) => acc+curr, 0) === newS2.reduce((acc, curr) => acc+curr, 0)
- }
- console.log(compare(null, ""))
+  return arr
+}
+console.log(productArray([12,20]))
