cac890ffd108ff286df1b4581bb922d1802b9a70
Jhonny <gonzodzen@mail.ru>
2022-12-17 21:06:44 +0300

    Insert dashes

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function periodIsLate(last, today, cycleLength){
-  return Date.parse(today) -  Date.parse(last) > cycleLength * ((24*3600)*1000)
+function insertDash(num) {
+  const z = num.toString().split('')
+  for(let i = 0; i < z.length; i++){
+    if(z[i] % 2 !== 0 && z[i+1]%2!==0){
+      z[i] = z[i] + '-'
+    }
+  }
+  const res = z.toString().split('')
+  if(res[res.length-1] === '-'){
+    res[res.length-1] = '&'
+  }
+  return res.filter(el => el !== '&').join('').split(',').join('')
 }
-console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35))
+console.log(insertDash(454793));
