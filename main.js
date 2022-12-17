a55c32e2b8ad7324aeb0c82f716bc0bc50069578
Jhonny <gonzodzen@mail.ru>
2022-12-17 16:08:49 +0300

    The Poet And The Pendulum

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-function productArray(numbers){
-  const arr = []
-  let counter = 0
-  while(counter < numbers.length){
-    arr.push(numbers.reduce((acc, curr) => acc * curr) / numbers[counter])
-    counter++
-  }
-  return arr
+function pendulum(values) {
+  const [arr1, arr2] = [[], []]
+  values.sort((a,b) => a - b).map((el,i) => (i % 2 === 0 ? arr1 : arr2).push(el))
+  return arr1.reverse().concat(arr2)
 }
-console.log(productArray([12,20]))
+console.log(pendulum([9, 4, 6, 4, 10, 5]));
