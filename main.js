aa1794cc22cf3834c406a4283e6a69f747365c59
Jhonny <gonzodzen@mail.ru>
2022-12-17 16:47:57 +0300

    Directions Reduction

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-function maxMultiple(divisor, bound) {
-  let res = null;
-  for (let i = 1; i <= bound; i++) {
-    if (i % divisor === 0) {
-      res = i
+function dirReduc(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "NORTH" && arr[i + 1] === "SOUTH") {
+      arr[i] = 0;
+      arr[i + 1] = 0;
+    } else if (arr[i] === "SOUTH" && arr[i + 1] === "NORTH") {
+      arr[i] = 0;
+      arr[i + 1] = 0;
+    } else if (arr[i] === "EAST" && arr[i + 1] === "WEST") {
+      arr[i] = 0;
+      arr[i + 1] = 0;
+    } else if (arr[i] === "WEST" && arr[i + 1] === "EAST") {
+      arr[i] = 0;
+      arr[i + 1] = 0;
+    } else {
+      newArr.push(arr[i]);
     }
   }
-  return res
+  const res = newArr.filter((el) => el !== 0);
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === "NORTH" && res[i + 1] === "SOUTH") {
+      res[i] = 0;
+      res[i + 1] = 0;
+    } else if (res[i] === "SOUTH" && res[i + 1] === "NORTH") {
+      res[i] = 0;
+      res[i + 1] = 0;
+    } else if (res[i] === "EAST" && res[i + 1] === "WEST") {
+      res[i] = 0;
+      res[i + 1] = 0;
+    } else if (res[i] === "WEST" && res[i + 1] === "EAST") {
+      res[i] = 0;
+      res[i + 1] = 0;
+    }
+  }
+  return res.filter((el) => el !== 0);
 }
-console.log(maxMultiple(2, 7));
+console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]));
