6dad5b95d82a4256dd57279fe8993f7a528b91af
Jhonny <gonzodzen@mail.ru>
2022-12-17 14:00:12 +0300

    Linked_list

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
-function greet(name) {
-  return name ? `hello ${name}!`: null
+class LinkedList{
+  constructor(){
+    this.size = 0
+    this.root = null
+  }
+  add(value){
+    if(this.size === 0){
+      this.root = new Node(value)
+      this.size += 1
+      return true
+    }
+    let node = this.root
+    while(node.next){
+      node = node.next
+    }
+    let newNode = new Node(value)
+    node.next = newNode
+    this.size += 1
+  }
+
+  getSize(){
+    return this.size
+  }
+
+  print(){
+    let res = []
+    let node = this.root
+    while(node){
+      res.push(node.value)
+      node = node.next
+    }
+    console.log(res)
+  }
 }
+class Node{
+  constructor(value){
+    this.value = value
+    this.next = null
+  }
+}
+
+const list = new LinkedList()
+list.add(4)
+list.add(2)
+list.add(9)
+list.add(7)
+
+list.print()
