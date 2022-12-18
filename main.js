dad9794ee9426308cab23ec87d33afe297e1659b
Jhonny <gonzodzen@mail.ru>
2022-12-18 14:26:18 +0300

    Limit string length - 1

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-function bingo(a) {
-  let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  for(let i = 0; i < arr_en.length; i++){
-    a[i] = arr_en[a[i]-1]
-  }
-  for(let i = 0; i < a.length; i++){
-    if(a[i] !== 'b' && a[i] !== 'i' && a[i] !== 'n' && a[i] !== 'g' && a[i] !== 'o'){
-      a[i] = undefined
+function solution(string,limit){
+    if(string.length <= limit){
+      return string
     }
-  }
-  const res = a.filter(el => el !== undefined)
-  const set = [...new Set(res)]
-  if(set.length < 5){
-    return 'LOSE'
-  }
-  const arr = []
-  for(let i = 0; i < set.length; i++){
-    arr.push(set.includes('b'))
-    set.includes('i')
-    set.includes('n')
-    set.includes('g')
-    set.includes('o')
-  }
-  return arr.every(el => el === true) ? 'WIN': 'LOSE'
+    const str = string.split('')
+    const arr = []
+    for(let i = 0; i < limit; i++){
+      arr.push(str[i])
+    }
+    arr.push('...')
+    return arr.join('')
 }
-console.log(bingo([21,13,2,7,5,14,7,15,9,10]))
+console.log(solution('Test', 8))
