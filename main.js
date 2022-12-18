91b4cb5ec1c541b60ba51e0ab6ec5968d54e5158
Jhonny <gonzodzen@mail.ru>
2022-12-18 22:36:08 +0300

    Basic subclasses - Adam and Eve

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function any(arr, fun){
-  return arr.some(fun)
+class Human{
+
+}
+class Man extends Human{
+
+}
+class Woman extends Human{
+
+}
+
+class God{
+  /**
+   * @returns Human[]
+   */
+    static create(){
+      return [new Man(), new Woman()]
+    }
 }
