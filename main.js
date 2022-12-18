c2bb637abf6532655ac766866fc128637148250e
Jhonny <gonzodzen@mail.ru>
2022-12-18 22:31:50 +0300

    Enumerable Magic #2 - True for Any?

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function solve(arr) {
-  const arr1 = arr.slice().sort((a, b) => a - b);
-  const arr2 = arr.slice().sort((a, b) => b - a);
-  const res = [];
-  for (let i = 0; i < arr1.length; i++) {
-    res.push(arr2[i]);
-    res.push(arr1[i]);
-  }
-  res.length = arr.length;
-  return res;
+function any(arr, fun){
+  return arr.some(fun)
 }
-console.log(solve([15, 11, 10, 7, 12]));
