b04061015ace00020007ab5ab9119d5c1a6c2828
Jhonny <gonzodzen@mail.ru>
2022-12-18 23:00:22 +0300

    Substituting Variables Into Strings: Padded Numbers

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-class Human{
-
-}
-class Man extends Human{
-
-}
-class Woman extends Human{
-
-}
-
-class God{
-  /**
-   * @returns Human[]
-   */
-    static create(){
-      return [new Man(), new Woman()]
+function solution(value) {
+  const arr = [0, 0, 0, 0, 0];
+  const nextArr = arr.slice();
+  value = value.toString();
+  for (let i = 0; i < value.length; i++) {
+    if (value) {
+      nextArr.push(value);
+      break;
     }
+  }
+  let total = value.length;
+  while (total) {
+    nextArr.shift();
+    total--;
+  }
+  return `Value is ${nextArr.join("")}`;
 }
+console.log(solution(1109));
