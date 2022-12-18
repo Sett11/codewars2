4caa3e0182a131fba12fc8590658461231c71924
Jhonny <gonzodzen@mail.ru>
2022-12-18 22:30:19 +0300

    Max-min arrays

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function getAges(sum,difference){
-  if (difference<0||sum<0) return null;
-  sum=sum/2
-  difference/=2
-  if (sum+difference<0||sum-difference<0) return null;
-  return [sum+difference,sum-difference]
-};
-console.log(getAges(40, 13));
+function solve(arr) {
+  const arr1 = arr.slice().sort((a, b) => a - b);
+  const arr2 = arr.slice().sort((a, b) => b - a);
+  const res = [];
+  for (let i = 0; i < arr1.length; i++) {
+    res.push(arr2[i]);
+    res.push(arr1[i]);
+  }
+  res.length = arr.length;
+  return res;
+}
+console.log(solve([15, 11, 10, 7, 12]));
