600a1f75bb4038beccd480c2c3f6a0d6d31af378
Jhonny <gonzodzen@mail.ru>
2022-12-18 17:59:04 +0300

    Enumerable Magic #20 - Cascading Subsets

--- main.js
+++ main.js
@@ -1,68 +1,14 @@
-const whosOnline = (friends) => {
-  const obj = {};
-  friends.map((el) => {
-    if (!obj.online) {
-      if (el.status === "online" && el.lastActivity <= 10) {
-        obj.online = [el.username];
-      }
-    } else {
-      if (el.status === "online" && el.lastActivity <= 10) {
-        obj.online.push(el.username);
-      }
+function eachCons(array, n) {
+  const arr = []
+  for(let i = 0; i < array.length; i++){
+    let currArr = []
+    for(let j = 0; j < n; j++){
+      let ind = i + j
+      if(ind >= array.length) return arr
+      currArr.push(array[ind])
     }
-    if (!obj.offline) {
-      if (el.status === "offline") {
-        obj.offline = [el.username];
-      }
-    } else {
-      if (el.status === "offline") {
-        obj.offline.push(el.username);
-      }
-    }
-    if (!obj.away) {
-      if (el.status === "online" && el.lastActivity > 10) {
-        obj.away = [el.username];
-      }
-    } else{
-      if (el.status === "online" && el.lastActivity > 10) {
-        obj.away.push(el.username)
-      }
-    }
-  });
-  return obj;
-};
-
-console.log(
-  whosOnline([
-    {
-      username: "David",
-      status: "online",
-      lastActivity: 10,
-    },
-    {
-      username: "Vasia",
-      status: "online",
-      lastActivity: 5,
-    },
-    {
-      username: "w",
-      status: "offline",
-      lastActivity: 22,
-    },
-    {
-      username: "d",
-      status: "offline",
-      lastActivity: 22,
-    },
-    {
-      username: "Lucy",
-      status: "offline",
-      lastActivity: 22,
-    },
-    {
-      username: "Bob",
-      status: "online",
-      lastActivity: 104,
-    },
-  ])
-);
+    arr.push(currArr)
+  }
+  return arr;
+}
+console.log(eachCons([3, 5, 8, 13], 2));
