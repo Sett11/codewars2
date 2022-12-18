40e6ebc88a6430b60a411406196c194b38c284b4
Jhonny <gonzodzen@mail.ru>
2022-12-18 18:14:24 +0300

    Is it a number?

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-function eachCons(array, n) {
-  const arr = []
-  for(let i = 0; i < array.length; i++){
-    let currArr = []
-    for(let j = 0; j < n; j++){
-      let ind = i + j
-      if(ind >= array.length) return arr
-      currArr.push(array[ind])
-    }
-    arr.push(currArr)
-  }
-  return arr;
-}
-console.log(eachCons([3, 5, 8, 13], 2));
+const isDigit = (s) => s == parseFloat(s)
