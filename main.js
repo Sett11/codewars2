0e0f0b1d48757b25953ab8347584663ff9372d06
Jhonny <gonzodzen@mail.ru>
2022-12-19 01:05:11 +0300

    Potenciation

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function findMissingNumber(sequence) {
-  if (sequence == "") return 0;
-  sequence = sequence.split(" ").map(Number);
-  if(sequence.length === 0) return 0
-  if(sequence.some(el => Number.isNaN(Number(el)))){
-    return 1
-  }
-  for (let i = 1; i <= sequence.length; i++) {
-    if (i !== sequence[i - 1]) return i;
-  }
-  return 0;
+function power(x,y){
+  return x < 1 || y < 1 ? 1: x**y
 }
+console.log(power(0,3))
