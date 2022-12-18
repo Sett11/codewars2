ba04f90608b67a76df71a90486db1e7fb2aec975
Jhonny <gonzodzen@mail.ru>
2022-12-18 16:09:21 +0300

    Initialize my name

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const none = (arr, fun) => arr.some(fun) === true ? false : true
-
-console.log(none([1,2,3,4,5],function(item){ return item > 5 }))
+function initializeNames(name){
+  const arr = name.split(' ')
+  if(arr.length === 1){
+    return arr[0]
+  }
+  if(arr.length === 2){
+    return [arr[0] + ' ' + arr[1]].join(' ')
+  }
+  return arr.map((el, i, arr) => {
+    if(i === 0 || i === arr.length-1){
+      return el
+    }
+    el = el.charAt(0) + '.'
+    return el
+  }).join(' ')
+}
+console.log(initializeNames('Alice Betty Catherine Davis'))
