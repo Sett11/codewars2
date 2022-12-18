89be9c857a30a7aa7ac3d97c3edaf72e4e1faad5
Jhonny <gonzodzen@mail.ru>
2022-12-19 01:31:38 +0300

    Fun with ES6 Classes #2 - Animals and Inheritance

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
-function power(x,y){
-  return x < 1 || y < 1 ? 1: x**y
+class Animal{
+
 }
-console.log(power(0,3))
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name
+    this.age = age
+    this.status = status
+    this.species = 'shark'
+    this.legs = 0
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name
+    this.age = age
+    this.status = status
+    this.species = 'cat'
+    this.legs = 4
+  }
+  
+  introduce(){
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super();
+    this.name = name
+    this.age = age
+    this.status = status
+    this.master = master
+    this.species = 'dog'
+    this.legs = 4
+  }
+  introduce(){
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+  }
+  greetMaster(){
+    return `Hello ${this.master}`
+  }
+}
+const example = new Cat("Example", 10, "Happy");
+console.log(example.species)
