88e5e82832547ae7358908a643aaee291de7fe39
Jhonny <gonzodzen@mail.ru>
2022-12-18 18:58:45 +0300

    Computer problem series #1: Fill the Hard Disk Drive

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const isDigit = (s) => s == parseFloat(s)
+function save(sizes, hd) {
+  let sum = 0;
+  for (let i = 0; i < sizes.length; i++) {
+    sum += sizes[i];
+    if (sum > hd) {
+      sum = i;
+      break
+    }
+    if (i === sizes.length-1) {
+      sum = sizes.length;
+      break
+    }
+  }
+  return sum;
+}
+console.log(save([4, 8, 15, 16, 23, 42], 108));
