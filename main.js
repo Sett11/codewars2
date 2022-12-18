020250605234dc65bb004715d22df77302387ebe
Jhonny <gonzodzen@mail.ru>
2022-12-18 19:43:55 +0300

    Is he gonna survive?

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-function diamond(n) {
-  if (n < 3 || n % 2 === 0) {
-    return null;
-  }
-  let midll = parseInt((n + 1) / 2);
-  let first,
-    last = "";
-  for (let i = 1; i <= n; i++) {
-    first = i <= midll ? i : n - i + 1;
-    last += " ".repeat(midll - first) + "*".repeat(2 * first - 1) + "\n";
-  }
-  return last;
+function hero(bullets, dragons) {
+  return (dragons*2)<=bullets
 }
-console.log(diamond(5));
+
+
+console.log(hero(10,5))
