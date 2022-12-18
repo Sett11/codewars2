1f40b4dc373f66b23341d5591a118c4a0ba230b5
Jhonny <gonzodzen@mail.ru>
2022-12-19 02:09:15 +0300

    Dictionary from two lists

--- main.js
+++ main.js
@@ -1,49 +1,8 @@
-class Animal{
-
+function createDict(keys, values) {
+  const obj = {};
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = values[i] != undefined ? values[i] : null;
+  }
+  return obj;
 }
-
-class Shark extends Animal {
-  constructor(name, age, status) {
-    super();
-    this.name = name
-    this.age = age
-    this.status = status
-    this.species = 'shark'
-    this.legs = 0
-  }
-}
-
-class Cat extends Animal {
-  constructor(name, age, status) {
-    super();
-    this.name = name
-    this.age = age
-    this.status = status
-    this.species = 'cat'
-    this.legs = 4
-  }
-  
-  introduce(){
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
-  }
-}
-
-class Dog extends Animal {
-  constructor(name, age, status, master) {
-    super();
-    this.name = name
-    this.age = age
-    this.status = status
-    this.master = master
-    this.species = 'dog'
-    this.legs = 4
-  }
-  introduce(){
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.`
-  }
-  greetMaster(){
-    return `Hello ${this.master}`
-  }
-}
-const example = new Cat("Example", 10, "Happy");
-console.log(example.species)
+console.log(createDict(["a", "b", "c", "d"], [1, 2, 3]));
