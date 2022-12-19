50b85b56e82b3aa1cf7d4f6ae3c14888ac7448d1
Jhonny <gonzodzen@mail.ru>
2022-12-19 18:31:28 +0300

    Even or Odd - Which is Greater?

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-function SeriesSum(n){
-  let sum = 0
-  for(let i = 1, j = 1; i <= n*3; i+=3){
-    sum += j/i
+function evenOrOdd(str) {
+  str = str.split("").map(Number);
+  const arr1 = [];
+  const arr2 = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] % 2 === 0) {
+      arr1.push(str[i]);
+    } else {
+      arr2.push(str[i]);
+    }
   }
-  return sum.toFixed(2)
+  return arr1.reduce((acc, curr) => acc + curr,0) >
+    arr2.reduce((acc, curr) => acc + curr,0)
+    ? "Even is greater than Odd"
+    : arr1.reduce((acc, curr) => acc + curr,0) <
+      arr2.reduce((acc, curr) => acc + curr,0)
+    ? "Odd is greater than Even"
+    : "Even and Odd are the same";
 }
-console.log(SeriesSum(2))
+console.log(evenOrOdd("123"));
