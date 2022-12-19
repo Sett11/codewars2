66ae62ca4814334966322e76c70eed7fb9611a9a
Jhonny <gonzodzen@mail.ru>
2022-12-19 22:19:52 +0300

    A Rule of Divisibility by 13

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-function solution(pairs) {
-  return Object.entries(pairs)
-    .join("= ")
+function thirt(n) {
+  const basic = [
+    1, 10, 9, 12, 3, 4, 1, 10, 9, 12, 3, 4, 1, 10, 9, 12, 3, 4, 1, 10, 9, 12, 3,
+    4,
+  ];
+  const total = ("" + n).split("").map(Number).reverse();
+  const arr = [];
+  for (let i = 0; i < total.length; i++) {
+    arr.push(total[i] * basic[i]);
+  }
+  let wow = ("" + arr.reduce((acc, curr) => acc + curr, 0))
     .split("")
-    .map((el) => {
-      if (el === ",") {
-        el = "=";
-        return el;
-      }
-      if (el === "=") {
-        el = ",";
-        return el;
-      } else {
-        return el;
-      }
-    })
-    .join(" ")
-    .split(" ")
-    .join("")
-    .replace(/\=/g, " $& ");
+    .map(Number)
+    .reverse();
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res.push(wow[i] * basic[i]);
+  }
+  if (res.length === 2) {
+    return res
+      .filter((el) => Number.isFinite(el))
+      .reduce((acc, curr) => acc + curr, 0);
+  }
+  res = res
+    .filter((el) => Number.isFinite(el))
+    .reduce((acc, curr) => acc + curr, 0);
+  let Oh = ("" + res).split("").map(Number).reverse();
+  let finiteRes = [];
+  for (let i = 0; i < arr.length; i++) {
+    finiteRes.push(Oh[i] * basic[i]);
+  }
+  return finiteRes
+    .filter((el) => Number.isFinite(el))
+    .reduce((acc, curr) => acc + curr, 0);
 }
-console.log(solution({ a: 1, b: "2", c: 3, d: "4" }));
+console.log(thirt(987663637354321));
