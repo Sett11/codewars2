9c7fcc181ed4cc5f8ea82f3211cbdae913fb7465
Jhonny <gonzodzen@mail.ru>
2022-12-19 22:30:06 +0300

    How many arguments

--- main.js
+++ main.js
@@ -1,36 +1,4 @@
-function thirt(n) {
-  const basic = [
-    1, 10, 9, 12, 3, 4, 1, 10, 9, 12, 3, 4, 1, 10, 9, 12, 3, 4, 1, 10, 9, 12, 3,
-    4,
-  ];
-  const total = ("" + n).split("").map(Number).reverse();
-  const arr = [];
-  for (let i = 0; i < total.length; i++) {
-    arr.push(total[i] * basic[i]);
-  }
-  let wow = ("" + arr.reduce((acc, curr) => acc + curr, 0))
-    .split("")
-    .map(Number)
-    .reverse();
-  let res = [];
-  for (let i = 0; i < arr.length; i++) {
-    res.push(wow[i] * basic[i]);
-  }
-  if (res.length === 2) {
-    return res
-      .filter((el) => Number.isFinite(el))
-      .reduce((acc, curr) => acc + curr, 0);
-  }
-  res = res
-    .filter((el) => Number.isFinite(el))
-    .reduce((acc, curr) => acc + curr, 0);
-  let Oh = ("" + res).split("").map(Number).reverse();
-  let finiteRes = [];
-  for (let i = 0; i < arr.length; i++) {
-    finiteRes.push(Oh[i] * basic[i]);
-  }
-  return finiteRes
-    .filter((el) => Number.isFinite(el))
-    .reduce((acc, curr) => acc + curr, 0);
+function args_count () {
+  return arguments.length
 }
-console.log(thirt(987663637354321));
+console.log(args_count(1,2,3))
