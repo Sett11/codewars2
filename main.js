2eb5acb77b877076c40232d8efdd75e35837e1d7
Jhonny <gonzodzen@mail.ru>
2022-12-20 00:06:40 +0300

    Array Deep Count

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-function sumOfN(n) {
-  if (n > 0) {
-    if (n === 1) {
-      return [0, 1];
-    }
-    const arr = [];
-    const newArr = [];
-    for (let i = 0; i < n + 5; i++) {
-      arr.push(i);
-      newArr.push(arr.reduce((acc, curr) => acc + curr, 0));
-    }
-    newArr.length = n + 1;
-    return newArr;
-  }
-  if (n === 0) {
-    return [0];
-  }
-  if (n < 0) {
-    if (n === -1) {
-      return [0, -1];
-    }
-    const arr = [];
-    const newArr = [];
-    for (let i = 0; i > n - 5; i--) {
-      arr.push(i);
-      newArr.push(arr.reduce((acc, curr) => acc + curr, 0));
-    }
-    newArr.length = Math.abs(n) + 1;
-    return newArr;
-  }
-}
-console.log(sumOfN(-5));
+const deepCount = (a) =>  a.reduce((acc,curr) => Array.isArray(curr) ? acc += deepCount(curr) : acc, a.length)
+
+console.log(deepCount([[[[[]], 2, 24]], 10, [[3, 10, [[]]]]]));
