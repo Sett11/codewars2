c09cfb7a64bbf42d90ae65d62c0cb21b0cc3ec5c
Jhonny <gonzodzen@mail.ru>
2022-12-19 21:10:47 +0300

    Training JS #13: Number object and its properties

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-class Person {
-  constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male'){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.gender = gender
-  }
-  sayFullName(){
-    return `${firstName} ${lastName}`
-  }
-  static greetExtraTerrestrials(raceName){
-    return `Welcome to Planet Earth ${raceName}`
-  }
+function whatNumberIsIt(n) {
+  return n === Number.MAX_VALUE
+    ? "Input number is Number.MAX_VALUE"
+    : n === Number.MIN_VALUE
+    ? "Input number is Number.MIN_VALUE"
+    : Number.isNaN(n)
+    ? "Input number is Number.NaN"
+    : n === Number.NEGATIVE_INFINITY
+    ? "Input number is Number.NEGATIVE_INFINITY"
+    : n === Number.POSITIVE_INFINITY
+    ? "Input number is Number.POSITIVE_INFINITY"
+    : `Input number is ${n}`;
 }
+console.log(whatNumberIsIt(10));
