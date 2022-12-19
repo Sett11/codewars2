d4f08befaf5342d61f1123507099f082f00e606e
Jhonny <gonzodzen@mail.ru>
2022-12-19 22:58:48 +0300

    Basic Sequence Practice

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-function args_count () {
-  return arguments.length
+function sumOfN(n) {
+  if (n > 0) {
+    if (n === 1) {
+      return [0, 1];
+    }
+    const arr = [];
+    const newArr = [];
+    for (let i = 0; i < n + 5; i++) {
+      arr.push(i);
+      newArr.push(arr.reduce((acc, curr) => acc + curr, 0));
+    }
+    newArr.length = n + 1;
+    return newArr;
+  }
+  if (n === 0) {
+    return [0];
+  }
+  if (n < 0) {
+    if (n === -1) {
+      return [0, -1];
+    }
+    const arr = [];
+    const newArr = [];
+    for (let i = 0; i > n - 5; i--) {
+      arr.push(i);
+      newArr.push(arr.reduce((acc, curr) => acc + curr, 0));
+    }
+    newArr.length = Math.abs(n) + 1;
+    return newArr;
+  }
 }
-console.log(args_count(1,2,3))
+console.log(sumOfN(-5));
