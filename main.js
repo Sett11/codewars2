a6dbc46f37d0fccb307ad795dac6ded590586e24
Jhonny <gonzodzen@mail.ru>
2022-12-20 02:04:59 +0300

    "Very Even" Numbers.

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
-String.prototype.digit = function() {
-  return /^\d$/.test(this)
-};
+function isVeryEvenNumber(n) {
+  if (/\^d$/.test(n)) {
+    if (n % 2 === 0) {
+      return true;
+    }
+  } else {
+    let res = ("" + n)
+      .split("")
+      .map(Number)
+      .reduce((acc, curr) => acc + curr, 0);
+    if (/\^d$/.test(res)) {
+      return res % 2 === 0;
+    } else {
+      if (/\^d$/.test(res)) {
+        return (
+          ("" + res)
+            .split("")
+            .map(Number)
+            .reduce((acc, curr) => acc + curr, 0) %
+            2 ===
+          0
+        );
+      } else {
+        if (/\^d$/.test(res)) {
+          return res % 2 === 0;
+        } else {
+          res = ("" + res)
+            .split("")
+            .map(Number)
+            .reduce((acc, curr) => acc + curr, 0);
+          return (
+            ("" + res)
+              .split("")
+              .map(Number)
+              .reduce((acc, curr) => acc + curr, 0) %
+              2 ===
+            0
+          );
+        }
+      }
+    }
+  }
+}
+
