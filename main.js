6bbc26e93b52f5b6c43a3248e92d2e6302c6c8db
Jhonny <gonzodzen@mail.ru>
2022-12-20 23:36:12 +0300

    The Deaf Rats of Hamelin

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const rowSumOddNumbers = n => n**3
-
-console.log(rowSumOddNumbers(5))
+const countDeafRats = (town) => {
+  if(town != null){
+    [left,right]=town.split('P');
+        const a=left+right.split('').reverse().join('');
+        const b=(a.match(/O~|~O/gi)||[]).filter(v=>v=='O~').length;
+         return b;
+  }
+  return 0;   
+}
+console.log(countDeafRats('PO~O~O~O~  O~  O~~O~OO~O~O~  O~O~O~O~O~~O~O  O~O~O~O~O~O~O~O~O~O~'));
