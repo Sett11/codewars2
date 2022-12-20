3353145844a3cf466c44dacc85069f2b96366e43
Jhonny <gonzodzen@mail.ru>
2022-12-20 21:05:49 +0300

    Unscrambled eggs

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-const head = a => a.length === 1 ? [] : a[0]
-const teil = a => a.length === 1 ? [] : a[a.length - 1]
-const init = a => a.slice(0, a.length - 1)
-const last = a => a[a.length - 1]
+const unscrambleEggs = word => word.replace(/egg/gi, '')
+console.log(unscrambleEggs("ceggodegge heggeregge"))
