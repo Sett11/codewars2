dd75cc44415d325191b31837283bfe1cf2074208
Jhonny <gonzodzen@mail.ru>
2022-12-21 01:23:41 +0300

    Find the Capitals

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function addExtra( listOfNumbers ){
-  listOfNumbers = listOfNumbers.slice()
-   listOfNumbers.push('1')
-  return listOfNumbers
+function capital(capitals) {
+  return capitals
+    .map((el) => {
+      return Object.values(el);
+    })
+    .map((ul) => {
+      ul = `The capital of ${ul[0]} is ${ul[1]}`;
+      return ul;
+    });
 }
+console.log(
+  capital([
+    { state: "Maine", capital: "Augusta" },
+    { country: "Spain", capital: "Madrid" },
+  ])
+);
