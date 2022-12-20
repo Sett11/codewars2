416e1080e51502ffe893049b0dd99885b13bea17
Jhonny <gonzodzen@mail.ru>
2022-12-21 01:08:47 +0300

    Sum The Tree

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function roundIt(n){
-  const arr = n.toString().split('.').map(el => el.split(''))
-  return arr[0].length > arr[1].length ? Math.floor(n) :
-  arr[1].length > arr[0].length ? Math.ceil(n) : Math.round(n)
-}
-console.log(roundIt(34.56))
+const sumTheTreeValues = (root) =>
+  !root
+    ? 0
+    : root.value + sumTheTreeValues(root.left) + sumTheTreeValues(root.right);
+console.log(
+  sumTheTreeValues({
+    value: 11,
+    left: { value: 0, left: null, right: null },
+    right: {
+      value: 0,
+      left: null,
+      right: { value: 1, left: null, right: null },
+    },
+  })
+);
