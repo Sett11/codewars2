14487980ae07f32122f8b640a01cb9c838c78766
Jhonny <gonzodzen@mail.ru>
2022-12-21 01:37:00 +0300

    Function Composition

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-function capital(capitals) {
-  return capitals
-    .map((el) => {
-      return Object.values(el);
-    })
-    .map((ul) => {
-      ul = `The capital of ${ul[0]} is ${ul[1]}`;
-      return ul;
-    });
-}
-console.log(
-  capital([
-    { state: "Maine", capital: "Augusta" },
-    { country: "Spain", capital: "Madrid" },
-  ])
-);
+const compose = (f,g) => (...args) => (f(g(...args)))
