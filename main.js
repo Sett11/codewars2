aff01ed224b8a88c679a5d6cc73b5c34cad38003
Jhonny <gonzodzen@mail.ru>
2022-12-21 01:11:58 +0300

    Improving Math.round(x)

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const sumTheTreeValues = (root) =>
-  !root
-    ? 0
-    : root.value + sumTheTreeValues(root.left) + sumTheTreeValues(root.right);
-console.log(
-  sumTheTreeValues({
-    value: 11,
-    left: { value: 0, left: null, right: null },
-    right: {
-      value: 0,
-      left: null,
-      right: { value: 1, left: null, right: null },
-    },
-  })
-);
+Math.roundTo = function (number, precision) {
+  return Number(number.toFixed(precision))
+}
