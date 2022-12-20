724578ca990e982e21d4bd67c38635d916716d32
Jhonny <gonzodzen@mail.ru>
2022-12-20 19:51:33 +0300

    Be Concise III - Sum Squares

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const seven = (m) => {
-  let counter = 0;
-  while (("" + m).length > 2) {
-    counter++;
-    m = Math.floor(m / 10) - (m % 10);
-  }
-  return [m, counter];
-};
-console.log(seven(1021));
+const sumSquares = a => a.reduce((acc, curr) => acc += curr**2, 0)
+
+console.log(sumSquares([7,3,9,6,5]));
