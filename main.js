cbc61858872f5d1ede9c43156fe4b205f3080ebf
Jhonny <gonzodzen@mail.ru>
2022-12-22 00:28:07 +0300

    Show multiples of 2 numbers within a range

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-sortme = function( names ){
-    return names.sort()
+function multiples(s1, s2, s3) {
+  const arr = [];
+  for (let i = 1; i < s3; i++) {
+    arr.push(i);
   }
+  return arr.filter((el) => el % s1 === 0 && el % s2 === 0);
+}
+console.log(multiples(13, 5, 800));
