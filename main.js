e11da943f88fda082ab90d6f34cdd5d71d3e6e40
Jhonny <gonzodzen@mail.ru>
2022-12-22 00:11:32 +0300

    Sort arrays - 1

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const findOddNames = list => list.filter(el => el.firstName.split``.reduce((acc,curr) => acc + curr.charCodeAt(), 0) % 2 !== 0)
-
-console.log(
-  findOddNames([{"firstName":"Harry","lastName":"K.","country":"Brazil","continent":"Americas","age":19,"language":"Python"}])
-);
+sortme = function( names ){
+    return names.sort()
+  }
