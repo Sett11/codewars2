a6bef0d5e0efcc786dc309a30f54b4af8f32a431
Jhonny <gonzodzen@mail.ru>
2022-12-21 23:33:54 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function uniqueSum(lst){
-    return [...new Set(lst)].reduce((acc, curr) => acc + curr, null)
-  }
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((el) =>
+      el === el.toUpperCase()
+        ? (el = el.toLowerCase())
+        : (el = el.toUpperCase())
+    )
+    .join("");
+};
