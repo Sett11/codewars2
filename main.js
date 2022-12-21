5c13cf0c8ed1e52ef08aba090465506a118386bd
Jhonny <gonzodzen@mail.ru>
2022-12-21 20:57:22 +0300

    last digits of a number

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-const howManyTimes = (annualPrice, individualPrice) =>  Math.ceil(annualPrice/individualPrice)
-console.log(howManyTimes(80, 15));
+const lastDigit = (n, d) =>
+  d <= 0 ? [] : ("" + n).split("").map(Number).reverse().slice(0, d).reverse();
+console.log(lastDigit(34625647867585, 10));
