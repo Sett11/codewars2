b77bc7bfc5db615e6c2ffcdd38287500df746fa7
Jhonny <gonzodzen@mail.ru>
2022-12-22 01:01:56 +0300

    Is Integer Array?

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function multiples(s1, s2, s3) {
-  const arr = [];
-  for (let i = 1; i < s3; i++) {
-    arr.push(i);
-  }
-  return arr.filter((el) => el % s1 === 0 && el % s2 === 0);
+function isIntArray(arr) {
+  return !arr
+    ? false
+    : arr.length === 0
+    ? true
+    : arr.every(
+        (el) =>
+          !Number.isNaN(el) &&
+          !!el === true &&
+          Number.isFinite(el) &&
+          Number.isSafeInteger(el)&&
+          el >-1 && Number.isInteger(el)
+      )
+    ? true
+    : false;
 }
-console.log(multiples(13, 5, 800));
+console.log(isIntArray([1.2, 1.8, 3]));
