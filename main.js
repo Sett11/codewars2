24815b3eb4a95aa2619e464acb8effef99e0aeb3
Jhonny <gonzodzen@mail.ru>
2022-12-21 20:51:21 +0300

    How many times should I go?

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-function binaryToString(binary) {
-  const bin = binary.split("");
-  const arr = [];
-  for (let i = 0; i < bin.length; i += 8) {
-    arr.push(bin.slice(i, i + 8));
-  }
-  return arr
-    .map((el) => el.join(""))
-    .map((el) => String.fromCharCode(parseInt(el, 2)))
-    .join("");
-}
-console.log(
-  binaryToString("01001011010101000100100001011000010000100101100101000101")
-);
+const howManyTimes = (annualPrice, individualPrice) =>  Math.ceil(annualPrice/individualPrice)
+console.log(howManyTimes(80, 15));
