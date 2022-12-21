464426cf5242a4a722b028acdd4d190f982d2a01
Jhonny <gonzodzen@mail.ru>
2022-12-21 21:18:55 +0300

    Vowel one

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-function reverseList(arr) {
-    return arr.reverse()
-  }
+function vowelOne(s) {
+  return s
+    .split("")
+    .map((el) => {
+        el = el.toLowerCase()
+      if (el === "a" || el === "u" || el === "e" || el === "i" || el === "o") {
+        el = 1;
+        return el;
+      } else {
+        el = 0;
+        return el;
+      }
+    })
+    .join("");
+}
+console.log(vowelOne("aeiou, abc"));
