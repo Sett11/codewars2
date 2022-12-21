ad10b2a5e947c875b4f40b11b14a3117706d7467
Jhonny <gonzodzen@mail.ru>
2022-12-21 20:00:44 +0300

    Frequency sequence

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-const arrCheck = value => value.every(el => Array.isArray(el))
-console.log(arrCheck([[1],[2], {}]))
+function freqSeq(str, sep) {
+  const arr = str.split("").reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {});
+  const s = str.split("");
+  const obj = Object.entries(arr).flat(2);
+  for (let i = 0; i < s.length; i++) {
+    const gam = obj.indexOf(s[i]);
+    if (gam != -1) {
+      s[i] = obj[gam + 1];
+    }
+  }
+  return s.join(sep);
+}
+console.log(freqSeq("hello world", "-"));
