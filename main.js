f729f563ef55a20275c056b5d2a227231245303d
Jhonny <gonzodzen@mail.ru>
2022-12-21 22:18:32 +0300

    Nickname Generator

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-String.prototype.vowel = function() {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-      return (/^[aeiouAEIOU]$/g).test(this);
-  };
+function nicknameGenerator(name) {
+  return name.length < 4
+    ? "Error: Name too short"
+    : /[a|i|o|u|e]/i.test(name[2])
+    ? name.slice(0, 4)
+    : name.slice(0, 3);
+}
+console.log(nicknameGenerator("Samantha"));
