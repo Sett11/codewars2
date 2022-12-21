ff5a4e4df39c708cfced6ab483b5a009a4c9a302
Jhonny <gonzodzen@mail.ru>
2022-12-21 22:49:55 +0300

    Who is the killer?

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-String.prototype.isLetter = function() {
-    return /^[a-z]$/i.test(this)
-  }
+function killer(suspectInfo, dead) {
+  return Object.entries(suspectInfo).filter((el) => {
+    if (el[1].includes(...dead)) {
+      return el[0];
+    }
+  })[0][0];
+}
+console.log(killer({ Brad: [], Megan: ["Ben", "Kevin"], Finn: [] }, ["Ben"]));
