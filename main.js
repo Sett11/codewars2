1e1f42724890c24e71e1a25706323f477d89fb33
Jhonny <gonzodzen@mail.ru>
2022-12-21 22:20:42 +0300

    Regexp Basics - is it a letter?

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function nicknameGenerator(name) {
-  return name.length < 4
-    ? "Error: Name too short"
-    : /[a|i|o|u|e]/i.test(name[2])
-    ? name.slice(0, 4)
-    : name.slice(0, 3);
-}
-console.log(nicknameGenerator("Samantha"));
+String.prototype.isLetter = function() {
+    return /^[a-z]$/i.test(this)
+  }
