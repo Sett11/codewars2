f6bb0901ccd765fd92c7525ce5c0f968c848ef03
Jhonny <gonzodzen@mail.ru>
2022-12-22 18:13:44 +0300

    Find min and max

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-function pattern(n) {
-  const arr = [];
-  for (let i = 1; i <= n; i++) {
-    arr.push(i);
+function getMinMax(arr){
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    return [min, max]
   }
-  let res = "";
-  for (let i = 1; i < arr.length; i++) {
-    res += 1 + "*".repeat(i) + arr[i] + "\n";
-  }
-  return "1\n" + res.slice(0, res.length-1)
-}
-console.log(pattern(20));
