90bcd9180f6b2f94f09bcee3b3855d5da9ca01a5
Jhonny <gonzodzen@mail.ru>
2022-12-22 21:41:51 +0300

    Loose Change

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function countWords(str) {
-    return str.replace(/\./g, '').replace(/﻿/g, ' ').trim().split(' ').filter(el => el !== ' ' && el !== '').length
+function looseChange(cents) {
+  if (cents < 0) {
+    cents = 0;
   }
-  console.log(countWords("﻿Hello﻿World "))
+  const obj = { Nickels: 0, Pennies: 0, Dimes: 0, Quarters: 0 };
+  const q = Math.floor(cents / 25);
+  obj.Quarters = q;
+  cents -= q * 25;
+  const d = Math.floor(cents / 10);
+  obj.Dimes = d;
+  cents -= 10 * d;
+  const n = Math.floor(cents / 5);
+  obj.Nickels = n;
+  cents -= n * 5;
+  const p = Math.floor(cents / 1);
+  obj.Pennies = p;
+  cents -= p * 1;
+  return obj;
+}
+console.log(looseChange(-3));
