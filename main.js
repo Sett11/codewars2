8e0bd505e2c527f83510de50ca94f9aa7a3a41a0
Jhonny <gonzodzen@mail.ru>
2022-12-22 18:52:44 +0300

    Consecutive letters

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function isNegativeZero(n) {
-    return n === 0 && (1 / n) === -Infinity ? true : false
+function solve(s) {
+  const arr = s.split``.map((el) => parseInt(el, 36) - 10).sort((a,b) => a - b)
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] + 1 === arr[i + 1]) {
+      continue;
+    }
+    if (i === arr.length - 1 && arr[i-1] === arr[i]-1) {
+      break;
+    }
+    return false;
   }
+  return arr.every((el, i, arr) => arr.indexOf(el) === arr.lastIndexOf(el));
+}
+console.log(solve("gjmhkfil"));
