6d8287f70a1122f11d7c01b6a916778534c403d4
Jhonny <gonzodzen@mail.ru>
2022-12-22 23:34:36 +0300

    Factorial decomposition

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const solve = (s,g) => s%g === 0 ? [g, s-g] : -1
-console.log(solve(12, 5))
+function decomp(n) {
+  let obj = {};
+  for (let i = 2; i <= n; i++) {
+    let count = i;
+    for (let j = 2; count > 1; j++) {
+      while (count % j === 0) {
+        count /= j;
+        obj[j] = obj[j] + 1 || 1;
+      }
+    }
+  }
+  return Object.keys(obj)
+    .map((el) => {
+      let res = obj[el];
+      return el + (res > 1 ? "^" + res : "");
+    })
+    .join(" * ");
+}
+
+console.log(decomp(22));
