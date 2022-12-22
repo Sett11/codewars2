f51d87f7e86bebbe57a14029c982785451bb135b
Jhonny <gonzodzen@mail.ru>
2022-12-22 20:50:10 +0300

    Minimum Steps (Array Series #6)

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const firstLetter = str => str[0].toUpperCase()
-
-const isValidName = name => /[a-z]/gi.test(name)
-
-const aliasGen = (fName, sName) =>
-  isValidName(fName[0]) && isValidName(sName[0])
-    ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(sName)]}`
-    : 'Your name must start with a letter from A - Z.'
+function minimumSteps(numbers, value) {
+  numbers = numbers.sort((a, b) => a - b);
+  for (let i = 1; i <= numbers.length; i++) {
+    if (numbers.slice(0, i).reduce((acc, curr) => acc + curr, 0) >= value)
+      return i - 1;
+  }
+}
+console.log(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464));
