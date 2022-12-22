785dc8b3fa69832106a3eff5150ebce40e5423f1
Jhonny <gonzodzen@mail.ru>
2022-12-22 19:47:08 +0300

    Help Suzuki rake his garden!

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-function evenChars(string) {
-  if (string.length < 2 && string.length > 100) {
-    return "invalid string";
-  }
-  const str = string.split``;
-  const arr = [];
-  for (let i = 0; i < str.length; i++) {
-    if (i % 2 !== 0) {
-      arr.push(str[i]);
-    }
-  }
-  return arr;
+function rakeGarden(garden) {
+  return garden.split` `
+    .map((el) => {
+      if (el !== "gravel" && el !== "rock") {
+        el = "gravel";
+        return el;
+      }
+      return el;
+    })
+    .join(" ");
 }
-console.log(evenChars("abcdefghijklm"));
+console.log(
+  rakeGarden(
+    "slug spider rock gravel gravel gravel gravel gravel gravel gravel"
+  )
+);
