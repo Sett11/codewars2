5e3240185e963fdd96b386181ed07622ae71072e
Jhonny <gonzodzen@mail.ru>
2022-12-22 19:01:09 +0300

    Last

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-function solve(s) {
-  const arr = s.split``.map((el) => parseInt(el, 36) - 10).sort((a,b) => a - b)
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] + 1 === arr[i + 1]) {
-      continue;
-    }
-    if (i === arr.length - 1 && arr[i-1] === arr[i]-1) {
-      break;
-    }
-    return false;
-  }
-  return arr.every((el, i, arr) => arr.indexOf(el) === arr.lastIndexOf(el));
+function last(list) {
+  const arg = arguments[arguments.length - 1];
+  const res = list[list.length - 1];
+  return arguments.length > 1 ? arg : res || arg;
 }
-console.log(solve("gjmhkfil"));
+console.log(last(1, "b", 3, "d", 5));
