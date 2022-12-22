143636b41ca01f64186db58e0712343593209c30
Jhonny <gonzodzen@mail.ru>
2022-12-22 21:46:50 +0300

    Find the calculation type

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-function looseChange(cents) {
-  if (cents < 0) {
-    cents = 0;
-  }
-  const obj = { Nickels: 0, Pennies: 0, Dimes: 0, Quarters: 0 };
-  const q = Math.floor(cents / 25);
-  obj.Quarters = q;
-  cents -= q * 25;
-  const d = Math.floor(cents / 10);
-  obj.Dimes = d;
-  cents -= 10 * d;
-  const n = Math.floor(cents / 5);
-  obj.Nickels = n;
-  cents -= n * 5;
-  const p = Math.floor(cents / 1);
-  obj.Pennies = p;
-  cents -= p * 1;
-  return obj;
+function calcType(a, b, res) {
+  return a + b === res
+    ? "addition"
+    : a * b === res
+    ? "multiplication"
+    : a - b === res
+    ? "subtraction"
+    : a / b === res
+    ? "division"
+    : null;
 }
-console.log(looseChange(-3));
