1cdb70defc5d1c2b58e7257b5136d405c01ca5b7
Jhonny <gonzodzen@mail.ru>
2022-12-22 21:25:28 +0300

    Count words

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-function minimumSteps(numbers, value) {
-  numbers = numbers.sort((a, b) => a - b);
-  for (let i = 1; i <= numbers.length; i++) {
-    if (numbers.slice(0, i).reduce((acc, curr) => acc + curr, 0) >= value)
-      return i - 1;
+function countWords(str) {
+    return str.replace(/\./g, '').replace(/﻿/g, ' ').trim().split(' ').filter(el => el !== ' ' && el !== '').length
   }
-}
-console.log(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464));
+  console.log(countWords("﻿Hello﻿World "))
