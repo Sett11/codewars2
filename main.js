bf21abd63f4bdeea9f534994a812c23fee74576d
Jhonny <gonzodzen@mail.ru>
2022-12-22 18:34:59 +0300

    Is It Negative Zero (-0)?

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-function consecutive(arr) {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const res = [];
-  for (let i = min; i < max; i++) {
-    if (arr[i] !== i) {
-      res.push(i);
-    }
+function isNegativeZero(n) {
+    return n === 0 && (1 / n) === -Infinity ? true : false
   }
-  return res.filter((el) => !arr.includes(el)).length;
-}
-
-console.log(consecutive([1, 2, 3, 4]));
