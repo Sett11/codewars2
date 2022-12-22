fb19fd0b05ad1b1b38d02b3f50a0dfa42ee5d58d
Jhonny <gonzodzen@mail.ru>
2022-12-22 18:25:47 +0300

    How many consecutive numbers are needed?

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function getMinMax(arr){
-    const min = Math.min(...arr)
-    const max = Math.max(...arr)
-    return [min, max]
+function consecutive(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const res = [];
+  for (let i = min; i < max; i++) {
+    if (arr[i] !== i) {
+      res.push(i);
+    }
   }
+  return res.filter((el) => !arr.includes(el)).length;
+}
+
+console.log(consecutive([1, 2, 3, 4]));
