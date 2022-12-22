aba16d9207d18e7ebe7c431bd4a2879cecfad268
Jhonny <gonzodzen@mail.ru>
2022-12-22 20:02:06 +0300

    Is n divisible by (...)?

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-function modifyMultiply(str, loc, num) {
-  const res = str.split` `[loc].split` `;
-  const arr = Array(num).fill(res);
-  return arr.join("-");
+function isDivisible() {
+  const arg = [...arguments];
+  if (arg.length === 1) {
+    return true;
+  }
+  const first = arg.shift();
+  return arg.every((el) => first % el === 0);
 }
-console.log(
-  modifyMultiply(
-    "Creativity is the process of having original ideas that have value. It is a process; it's not random.",
-    8,
-    10
-  )
-);
+console.log(isDivisible(12, 3, 4, 5));
