e27102fa877954e39de594618c6ff17d355f57ac
Jhonny <gonzodzen@mail.ru>
2022-12-23 21:21:01 +0300

    Odder Than the Rest

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-const scrollingText = text => [...text.toUpperCase()].map((el, i) => text.slice(i).toUpperCase() + text.slice(0, i).toUpperCase())
-console.log(scrollingText('codewars'))
+function oddOne(arr) {
+  const ar = []
+  for(let i = 0 ; i < arr.length; i++){
+    if(arr[i] % 2 !== 0){
+      ar.push(i)
+    }
+  }
+  return ar[0] === undefined ? -1 : ar[0]
+}
+console.log(oddOne([2,4,6,7,10]))
