5dec15ef63c8d9f6fe0c694d3bc63c7955fb0214
Jhonny <gonzodzen@mail.ru>
2022-12-23 19:13:56 +0300

    Sum up the random string

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-const root = (x, n) => Math.pow(x, 1/n)
+function sumFromString(str){
+  return str.replace(/\D/gi, '&').split('&').filter(el => el !== '').map(el => Number(el)).reduce((acc,curr) => acc+curr,0)
+}
+console.log(sumFromString("In 2015, I want to know how much does iPhone 6+ cost?"))
