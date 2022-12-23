e1fff907de2539d822c1643813398ed576993129
Jhonny <gonzodzen@mail.ru>
2022-12-23 19:52:23 +0300

    Scrolling Text

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-function findStrangeValue() {
-  return NaN 
-}
+const scrollingText = text => [...text.toUpperCase()].map((el, i) => text.slice(i).toUpperCase() + text.slice(0, i).toUpperCase())
+console.log(scrollingText('codewars'))
