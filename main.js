27c293c7f64eebe79b65434925dd22ad25a0246d
Jhonny <gonzodzen@mail.ru>
2022-12-24 16:04:54 +0300

    The Office VI - Sabbatical

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-const vampire_test = (a, b) => {
-  const sum = (a * b).toString();
-  const str = (a.toString() + b.toString()).split``;
-  const obj1 = str.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-  }, {});
-  const obj2 = sum.split``.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-  }, {});
-  return (
-    Object.entries(obj1).join``.replace(/,/g, "") ===
-    Object.entries(obj2).join``.replace(/,/g, "")
-  );
-};
-console.log(vampire_test(204, 615));
+function sabb(s, val, happiness){
+  return s.replace(/[^sabticl]/g, '').length + val + happiness > 22 ? 'Sabbatical! Boom!' : 'Back to your desk, boy.'
+}
+console.log(sabb('Can I have a sabbatical?', 5, 5))
