137d3e161b2e467c6258faef07ccf7cd695143b5
Jhonny <gonzodzen@mail.ru>
2022-12-25 23:13:01 +0300

    Hungarian Vowel Harmony (easy)

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-function Node(data, next = null) {
-  this.data = data;
-  this.next = next;
+function dative(word) {
+  const res = word.replace(/[^eéiíöőüűaáoóuú]/g, "");
+  const lett = res[res.length - 1];
+  return lett === "a" ||
+    lett === "á" ||
+    lett === "o" ||
+    lett === "ó" ||
+    lett === "u" ||
+    lett === "ú"
+    ? word + "nak"
+    : word + "nek";
 }
-
-function reduce(head, f, init) {
-  let count = 0, arr = [], tmp = head
-  while(tmp !== null){
-    arr.push(tmp.data)
-    tmp = tmp.next
-    count++
-  }
-  return arr.reduce(f,init)
-}
-console.log(
-  reduce(
-    {
-      data: 1,
-      next: { data: 2, next: { data: 3, next: { data: 3, next: null } } },
-    },
-    (a,b) => a + b, 0
-  )
-);
+console.log(dative("ablak"));
