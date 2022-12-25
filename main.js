709396f0afc027121b5c3ca5b2093c1a2d4a8fce
Jhonny <gonzodzen@mail.ru>
2022-12-26 00:48:43 +0300

    Partial Word Searching

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-function splitTheBill(x) {
-  const arr = Object.values(x);
-  const total = (arr.reduce((acc, curr) => acc + curr, 0) / arr.length).toFixed(2)
-  for (let k in x) {
-    x[k] = (x[k] - total).toFixed(2)
-  }
-  return x;
+function wordSearch(query, seq){
+  const r = new RegExp(`${query}`, 'i')
+  const res = seq.filter(el => r.test(el))
+  return res.length === 0 ? ['Empty'] : res
 }
-console.log(splitTheBill({ A: 40, B: 25, X: 10 }));
+console.log(wordSearch("ab", ["za", "aB", "Abc", "zAB", "zbc"]));
