89c70d778ca21b9bd0489d392c51d14a3120e751
Jhonny <gonzodzen@mail.ru>
2022-12-25 19:41:26 +0300

    Reverser

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-var rooms = {
-  1: {
-    name: "F",
-    description: "ooo",
-    completed: "kkk",
-  },
-  2: {
-    name: "F",
-    description: "ooo",
-    completed: "kkk",
-  },
-  3: {
-    name: "F",
-    description: "ooo",
-    completed: "kkk",
-  },
-};
+function reverse(n) {
+  let counter = 0;
+  while (n) {
+    counter = counter * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return counter;
+}
+console.log(reverse(112));
