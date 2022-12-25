a7b31178184fe52ebd57945694b6c7b305ea450e
Jhonny <gonzodzen@mail.ru>
2022-12-25 19:25:07 +0300

    Grasshopper - Create the rooms

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const isMonotone = (arr) => {
-  if(arr.length === 0 || arr.length ===1){
-    return true
-  }
-  const ar = [];
-  for (let i = 0; i < arr.length; i++) {
-    ar.push(arr[i] <= arr[i + 1]);
-  }
-  ar.length = arr.length - 1;
-  return ar.every((el) => el === true);
+var rooms = {
+  1: {
+    name: "F",
+    description: "ooo",
+    completed: "kkk",
+  },
+  2: {
+    name: "F",
+    description: "ooo",
+    completed: "kkk",
+  },
+  3: {
+    name: "F",
+    description: "ooo",
+    completed: "kkk",
+  },
 };
-console.log(isMonotone([1, 2, 3]));
