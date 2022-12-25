2f83bd0a0754c93d92c85bd916ab02c1c6406428
Jhonny <gonzodzen@mail.ru>
2022-12-25 18:41:24 +0300

    Santa's Naughty List

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const flatten = a => a.flat()
+function findChildren(santasList, children) {
+  const arr = []
+  for(let i = 0; i < santasList.length + children.length; i++){
+    if(santasList.includes(children[i])){
+      arr.push(children[i])
+    }
+  }
+  return [...new Set(arr)].sort()
+}
+console.log(findChildren(["Jason", "James", "Johnson"], ["Jason", "James", "JJ"]))
