f865470323dfbc892db7a64eaa43f039da3ca4d2
Jhonny <gonzodzen@mail.ru>
2022-12-25 16:25:33 +0300

    Dot Calculator

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-function remove(string) {
-  const arr = string.split``;
-  const res = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "!") {
-      res.push(arr[i]);
+function dotCalculator(e) {
+  const basic = e.split``.filter((el) => el !== " ");
+  let ind = null;
+  for (let i = 0; i < basic.length; i++) {
+    if (basic[i] !== ".") {
+      ind = i;
     }
   }
-  const str = arr.filter((el) => el !== "!");
-  return str.concat(res).join("");
+  if (basic[ind] === "+") {
+    return (basic.slice(0, ind) + basic.slice(ind + 1, basic.length)).replace(
+      /,/g,
+      ""
+    );
+  }
+  if (basic[ind] === "-") {
+    const res =
+      basic.slice(0, ind).length - basic.slice(ind + 1, basic.length).length;
+    return basic
+      .slice(0, ind - basic.slice(ind + 1, basic.length).length)
+      .join("");
+  }
+  if (basic[ind] === "*") {
+    return e.slice(0, ind).repeat(basic.slice(ind + 1, basic.length).length);
+  }
+  if (basic[ind] === "/") {
+    return e.slice(
+      0,
+      Math.floor(
+        (e.slice(0, ind).length - 1) / e.slice(ind + 3, e.length).length
+      )
+    );
+  }
 }
-console.log(remove("Hi! Hi! Hi!"));
+console.log(dotCalculator(". // .."));
