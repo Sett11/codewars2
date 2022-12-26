ac50b3862c17f215247527638c8dc151eb122c00
Jhonny <gonzodzen@mail.ru>
2022-12-26 14:51:37 +0300

    The Pony Express. Refactoring.

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function riders(stations) {
-  return Math.ceil(stations.reduce((acc,curr) => acc+curr,0)/100)
-}
+const riders = stations => 
+  stations.reduce((acc,curr) => {
+    acc.totalDistance += curr
+    if(acc.totalDistance > 100){
+      acc.totalDistance = curr
+      acc.totalRiders++
+    }
+    return acc
+  },
+  { totalRiders: 1, totalDistance: 0 }
+  ).totalRiders
+
 console.log(riders([6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49]))
