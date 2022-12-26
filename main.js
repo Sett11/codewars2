5b7b6a31c9750fa284239f8cdb1648c9e7485f97
Jhonny <gonzodzen@mail.ru>
2022-12-27 02:01:17 +0300

    Replace all items

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function smallEnough(a, limit){
-  return a.every(el => el <= limit)
+function replaceAll(seq, find, replace) {
+  if(typeof seq === 'string'){
+    seq = seq.split('')
+    return seq.map((el) => (el === find ? (el = replace) : (el = el))).join('')
+  }
+  return seq.map((el) => (el === find ? (el = replace) : (el = el)));
 }
+console.log(replaceAll([1, 2, 2], 1, 2));
