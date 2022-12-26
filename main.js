f19e4f6d751a00c612fd2634efcd555ac8c3bccf
Jhonny <gonzodzen@mail.ru>
2022-12-27 01:21:47 +0300

    Clocky Mc Clock-Face

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-function allNonConsecutive(arr) {
-  const ar = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i + 1] - arr[i] !== 1) {
-      ar.push([arr.indexOf(arr[i + 1]), arr[i + 1]]);
-    }
+const whatTimeIsIt = (a) => {
+  if(a===0 || a === 360){
+    return `12:00`
   }
-  return ar
-    .map((el, i) => {
-      if (el[0] !== undefined && el[1] !== undefined) {
-        el = { i: el[0], n: el[1] };
-        return el;
-      }
-    })
-    .filter((el) => el);
-}
-console.log(allNonConsecutive([1, 2, 3, 4, 6, 7, 8, 10]));
+    const sum = a * 2
+    let hours = Math.floor(sum / 60)
+    let min = Math.floor(sum - hours*60)
+    const control = (''+min).split('').map(Number)
+    const control2 = (''+hours).split('').map(Number)
+    if(control.length == 1){
+      control.unshift(0)
+      min = control.join('')
+    }
+    if(control2.length == 1){
+      control2.unshift(0)
+      hours = control2.join('')
+    }
+    return `${hours == 00? 12: hours}:${String(min)}`
+  }
+console.log(whatTimeIsIt(82.72141264404617))
