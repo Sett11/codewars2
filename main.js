ccb99e99e4dd66be55eb87082baf172db899b5d5
Jhonny <gonzodzen@mail.ru>
2022-12-26 19:18:11 +0300

    Round by 0.5 steps

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function closeCompare(a, b, m = 0){ 
- return m >= Math.abs(a-b) ? 0 : a < b ? -1 : a > b ? 1: null
+function solution(n) {
+  const arr = String(n.toFixed(1)).split`.`;
+  const lastElem = Number(arr[arr.length - 1]);
+  return lastElem === 1 || lastElem === 2
+    ? Math.floor(n)
+    : lastElem === 3 || lastElem === 4
+    ? Math.floor(n) + 0.5
+    : lastElem === 6 || lastElem === 7
+    ? Math.floor(n) + 0.5
+    : lastElem === 8 || lastElem === 9
+    ? Math.ceil(n)
+    : n
 }
-console.log(closeCompare(4,5))
+console.log(solution(720));
