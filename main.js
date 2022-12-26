b6d49131eb912de2aa02560acc7392e2e4fd0327
Jhonny <gonzodzen@mail.ru>
2022-12-27 01:52:04 +0300

    No Loops 1 - Small enough?

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-function alternate(n, firstValue, secondValue){
-  const arr = Array(n).fill(firstValue)
-  for(let i = 0; i < arr.length; i++){
-    if(i % 2 !== 0){
-      arr[i] = secondValue
-    }
-  }
-  return arr
+function smallEnough(a, limit){
+  return a.every(el => el <= limit)
 }
