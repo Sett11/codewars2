14286c2d9d124e6b30cb1124223607e4a3fe9651
Jhonny <gonzodzen@mail.ru>
2022-12-26 22:16:41 +0300

    The Lazy Startup Office

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function splitAndMerge(str,sp){
-  let words = str;
-  return words.split(" ").map(el => el.split("").join(sp)).join(" ");
- }
+function binRota(arr) {
+  return arr.map((el,i) => {
+    if(i % 2 !== 0){
+      el = el.reverse()
+      return el
+    }
+    return el
+  }).flat(1)
+}
+console.log(
+  binRota([
+    ["Sam", "Nina", "Tim", "Helen", "Gurpreet", "Edward", "Holly", "Eliza"],
+    ["Billy", "Megan", "Aki", "Arun", "Joy", "Anish", "Lee", "Maryan"],
+    ["Nick", "Josh", "Pete", "Kavita", "Daisy", "Francesca", "Alfie", "Macy"],
+  ])
+);
