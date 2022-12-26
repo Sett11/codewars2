41d67ba0079ea919a33b77bb98f51682a8787e6b
Jhonny <gonzodzen@mail.ru>
2022-12-26 22:23:52 +0300

    Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-function binRota(arr) {
-  return arr.map((el,i) => {
-    if(i % 2 !== 0){
-      el = el.reverse()
-      return el
-    }
-    return el
-  }).flat(1)
+function alienLanguage(str) {
+  return str.split(" ").map((el) => {
+    el = `${el.slice(0, el.length - 1).toUpperCase()}${el
+      .charAt(el.length - 1)
+      .toLowerCase()}`;
+    return el;
+  }).join` `;
 }
-console.log(
-  binRota([
-    ["Sam", "Nina", "Tim", "Helen", "Gurpreet", "Edward", "Holly", "Eliza"],
-    ["Billy", "Megan", "Aki", "Arun", "Joy", "Anish", "Lee", "Maryan"],
-    ["Nick", "Josh", "Pete", "Kavita", "Daisy", "Francesca", "Alfie", "Macy"],
-  ])
-);
+console.log(alienLanguage("My name is John"));
