c9bc5d3fc38238930e4aa94c156dd09a6cc81826
Jhonny <gonzodzen@mail.ru>
2022-12-27 01:50:14 +0300

    Length and two values.

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-const whatTimeIsIt = (a) => {
-  if(a===0 || a === 360){
-    return `12:00`
-  }
-    const sum = a * 2
-    let hours = Math.floor(sum / 60)
-    let min = Math.floor(sum - hours*60)
-    const control = (''+min).split('').map(Number)
-    const control2 = (''+hours).split('').map(Number)
-    if(control.length == 1){
-      control.unshift(0)
-      min = control.join('')
+function alternate(n, firstValue, secondValue){
+  const arr = Array(n).fill(firstValue)
+  for(let i = 0; i < arr.length; i++){
+    if(i % 2 !== 0){
+      arr[i] = secondValue
     }
-    if(control2.length == 1){
-      control2.unshift(0)
-      hours = control2.join('')
-    }
-    return `${hours == 00? 12: hours}:${String(min)}`
   }
-console.log(whatTimeIsIt(82.72141264404617))
+  return arr
+}
