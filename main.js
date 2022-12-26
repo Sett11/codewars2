ffa8842cd0a5e93a17cda928173e10921a1c7788
Jhonny <gonzodzen@mail.ru>
2022-12-26 19:31:14 +0300

    Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-function solution(n) {
-  const arr = String(n.toFixed(1)).split`.`;
-  const lastElem = Number(arr[arr.length - 1]);
-  return lastElem === 1 || lastElem === 2
-    ? Math.floor(n)
-    : lastElem === 3 || lastElem === 4
-    ? Math.floor(n) + 0.5
-    : lastElem === 6 || lastElem === 7
-    ? Math.floor(n) + 0.5
-    : lastElem === 8 || lastElem === 9
-    ? Math.ceil(n)
-    : n
+function remove(s) {
+  return s.split` `
+    .map((el) =>
+      el.indexOf("!") === el.lastIndexOf("!") && el.includes("!")
+        ? el === "&"
+        : el
+    )
+    .filter((el) => el !== false)
+    .join(" ");
 }
-console.log(solution(720));
+console.log(remove("Hi! Hi Hi!! Hi!"));
