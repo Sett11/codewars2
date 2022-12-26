adba951e474e71d6d2500c8983b306482674cf6c
Jhonny <gonzodzen@mail.ru>
2022-12-26 22:10:25 +0300

    Training JS #18: Methods of String object--concat() split() and its good friend join()

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function makeValley(arr) {
-  let db = arr.slice().sort((a, b) => a - b);
-  let res = [], res2 = [];
-  for (let i = 0; i < arr.length; i++) {
-    res.push(db.pop());
-    res2.push(db.pop());
-  }
-  return (res
-    .concat(res2.sort((a, b) => a - b))
-    .filter((el) => el !== undefined))
-}
-console.log(makeValley([20, 7, 6, 2]));
+function splitAndMerge(str,sp){
+  let words = str;
+  return words.split(" ").map(el => el.split("").join(sp)).join(" ");
+ }
