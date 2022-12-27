1f906c97d81255e5259655bb57f03e8a3df5248f
Jhonny <gonzodzen@mail.ru>
2022-12-28 00:53:36 +0300

    Password maker

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const sortme = names =>  names.sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-
-console.log(sortme(["Hello","there","I'm","fine"]))
+function makePassword(phrase) {
+  return phrase.split` `.map((el) => (el = el.charAt(0))).join``
+    .replace(/i/gi, "1")
+    .replace(/o/gi, "0")
+    .replace(/s/gi, "5");
+}
+console.log(makePassword("Keep Calm and Carry On"));
