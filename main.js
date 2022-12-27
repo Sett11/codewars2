93a90c149a6921c1712993ddeab571bdf47cb56f
Jhonny <gonzodzen@mail.ru>
2022-12-27 20:15:06 +0300

    Help the bookseller !

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-function howMuchWater(water, load, clothes){
-  if(load * 2 < clothes){
-    return 'Too much clothes'
+function stockList(listOfArt, listOfCat){
+  if(listOfArt.length === 0 || listOfCat.length === 0){
+    return ''
   }
-  if(clothes < load){
-    return 'Not enough clothes'
+  const arr = listOfArt.map((el) => {
+    el = [el.charAt(0), Number(el.replace(/\D/g, ''))]
+    return el
+  })
+  let nextArr = listOfCat.map(el => [el])
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < nextArr.length; j++){
+      if(arr[i][0] === nextArr[j][0]){
+        nextArr[j].push(arr[i][1])
+      }
+    }
   }
-  return Number((water * 1.1 ** (clothes-load)).toFixed(2))
+  nextArr = nextArr.map(el => el.flat(1))
+  for(let i = 0; i < nextArr.length; i++){
+    let tmp = []
+    for(let j = 0; j < nextArr[i].length; j++){
+      if(typeof nextArr[i][j] === 'number'){
+        tmp.push(nextArr[i][j])
+      }
+    }
+    tmp = tmp.reduce((acc,curr) => acc+curr,0)
+        nextArr[i].push(tmp)
+  }
+  return nextArr.map((el) => {
+    el = `(${el[0]} : ${el[el.length-1]})`
+    return el
+  }).join(' - ')
 }
-console.log(howMuchWater(50,15,29))
+console.log(stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"], ["A", "B", "C", "D"]))
