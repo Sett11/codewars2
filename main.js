2e9c204405ed94b365edd24c07bc5b0c0b7a79a0
Jhonny <gonzodzen@mail.ru>
2022-12-27 14:25:47 +0300

    We Have Liftoff

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-const paintLetterboxes = (start, end) => {
-  const arr = [];
-  for (let i = start; i <= end; i++) {
-    arr.push(i);
-  }
-  const newArr = arr.map((el) => (el = String(el))).join("").split``
-    .sort((a, b) => a - b)
-    .map(Number);
-  const res = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((el) => [el]);
-  for (let i = 0; i < newArr.length; i++) {
-    for (let j = 0; j < res.length; j++) {
-      if (res[j][0] === newArr[i]) {
-        res[j].push(newArr[i]);
-      }
-    }
-  }
-  return res.map((el) => (el = el.length - 1));
-};
-console.log(paintLetterboxes(125, 132));
+function liftoff(i){
+  const res = i.sort((a, b) => b - a)
+  res.push('liftoff!')
+  return res.join` `
+}
+console.log(liftoff([2, 8, 10, 9, 1, 3, 4, 7, 6, 5]))
