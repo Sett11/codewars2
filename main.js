9838fcffb681074640f0207ec2c11b0f606b0ef8
Jhonny <gonzodzen@mail.ru>
2022-12-28 01:39:00 +0300

    Odd Ones Out!

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-function makePassword(phrase) {
-  return phrase.split` `.map((el) => (el = el.charAt(0))).join``
-    .replace(/i/gi, "1")
-    .replace(/o/gi, "0")
-    .replace(/s/gi, "5");
+function oddOnesOut(nums) {
+  const res = Object.entries(
+    nums.reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, {})
+  )
+    .filter((el) => el[1] % 2 !== 0)
+    .map((el) => Number(el[0]));
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < res.length; j++) {
+      if (nums[i] === res[j]) {
+        nums[i] = "&";
+      }
+    }
+  }
+  return nums.filter((el) => el !== "&");
 }
-console.log(makePassword("Keep Calm and Carry On"));
+console.log(oddOnesOut([100, 100, 5, 5, 100, 50, 68, 50, 68, 50, 68, 5, 100]));
