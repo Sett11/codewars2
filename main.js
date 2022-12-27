020d75c453c4a8d29b6e53857acb240c6bae51ec
Jhonny <gonzodzen@mail.ru>
2022-12-27 21:33:42 +0300

    Scrabble Score

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-function sortVowels(s) {
-  return typeof s !== "string" || !s
-    ? ""
-    : s
-        .replace(/[^aiuoe]/gi, "$&|\n")
-        .replace(/[aieou]/gi, "|$&\n")
-        .replace(/\n$/, '');
+function scrabbleScore(str) {
+  return str
+    .replace(/[aeioulnrst]/gi, " 1 ")
+    .replace(/[dg]/gi, " 2 ")
+    .replace(/[bcmp]/gi, " 3 ")
+    .replace(/[fhvwy]/gi, " 4 ")
+    .replace(/[k]/gi, " 5 ")
+    .replace(/[jx]/gi, " 8 ")
+    .replace(/[qz]/gi, " 10 ").split` `
+    .filter((el) => el !== "")
+    .map(Number)
+    .reduce((acc, curr) => acc + curr, 0);
 }
-console.log(sortVowels("Codewars"));
-("C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|");
+console.log(scrabbleScore("street"));
