170adb1b5be466883ae19c362fccab112c82f090
Jhonny <gonzodzen@mail.ru>
2022-12-27 14:18:14 +0300

    Letterbox Paint-Squad

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-function doubleEveryOther(a) {
-  return a.map((el,i) => {
-    if(Number.isInteger(el) && i % 2 !== 0){
-      el = el * 2
-      return el
-    } else {
-      return el
+const paintLetterboxes = (start, end) => {
+  const arr = [];
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  const newArr = arr.map((el) => (el = String(el))).join("").split``
+    .sort((a, b) => a - b)
+    .map(Number);
+  const res = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((el) => [el]);
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < res.length; j++) {
+      if (res[j][0] === newArr[i]) {
+        res[j].push(newArr[i]);
+      }
     }
-  })
-}
-console.log(doubleEveryOther([1,2,3,4]))
+  }
+  return res.map((el) => (el = el.length - 1));
+};
+console.log(paintLetterboxes(125, 132));
