abdf4f748cf7fc39b51bec2e6632405085c3915a
Jhonny <gonzodzen@mail.ru>
2022-12-27 18:53:43 +0300

    Number Of Occurrences

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-function decode  (code, n) {
-  const arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const num = String(n).repeat(20).split``.map(Number)
-  for(let i = 0; i < code.length; i++){
-    code[i] -= num[i]
+Array.prototype.numberOfOccurrences = function(el) {
+  const arr = []
+  for(let i = 0; i < this.length; i++){
+    if(this[i] === el){
+      arr.push(this[i])
+    }
   }
-  for(let i = 0; i <= arr_en.length + 20; i++){
-    code[i] = arr_en[code[i]-1]
-  }
-  return code.filter(el => el).join``
+  return arr.length
 }
-console.log(decode([ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8], 1939))
