88756800f8cd789dc41c4cef2ebfac4302e49531
Jhonny <gonzodzen@mail.ru>
2022-12-27 13:23:30 +0300

    Elevator Distance

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function ghostBusters(building) {
-  return building.indexOf(' ') === -1 ? "You just wanted my autograph didn't you?" : building.replace(/\s/g, '')
+function elevatorDistance(array) {
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+    arr.push(Math.abs(array[i] - array[i + 1]));
+  }
+  return arr.filter((el) => el).reduce((acc, curr) => acc + curr, 0);
 }
+console.log(elevatorDistance([7, 1, 7, 1]));
