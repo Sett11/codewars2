5038f5ec4ccf6095a3c9c48999580077d2f39528
Jhonny <gonzodzen@mail.ru>
2022-12-28 16:23:49 +0300

    What a "Classy" Song

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-const isSquare = (arr) =>
-  arr.length === 0 ? undefined : arr.map((el) => Number.isInteger(Math.sqrt(el))).every((el) => el === true);
-console.log(isSquare([1, 4, 9, 16, 25, 36]));
+class Song{
+  title = ''
+  artist = ''
+  store = []
+  constructor(title, artist){
+    this.title = title
+    this.artist = artist
+  }
+  howMany(n){
+    n = [...new Set(n.map(el => el.toLowerCase()))]
+    let cou = 0
+    n.map(el => this.store.includes(el) ? cou+=0:cou+=1)
+    this.store = [...new Set([...this.store, ...n])]
+    return cou
+  }
+}
+const son = new Song('Mount Moose', 'The Snazzy Moose')
+console.log(son.howMany(['JoHn', 'Luke', 'AmAndA']))
