a7077f36e6641f6219a93b3cc0e8aea6b17d9618
Jhonny <gonzodzen@mail.ru>
2022-12-28 16:32:17 +0300

    Monkey Tennis - The Aftermath

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-class Song{
-  title = ''
-  artist = ''
-  store = []
-  constructor(title, artist){
-    this.title = title
-    this.artist = artist
-  }
-  howMany(n){
-    n = [...new Set(n.map(el => el.toLowerCase()))]
-    let cou = 0
-    n.map(el => this.store.includes(el) ? cou+=0:cou+=1)
-    this.store = [...new Set([...this.store, ...n])]
-    return cou
-  }
+const ballCollector = (detritus) => {
+  return {weight: detritus
+     .filter((el) => el === 58)
+     .reduce((acc, curr) => acc + curr, 0)}
 }
-const son = new Song('Mount Moose', 'The Snazzy Moose')
-console.log(son.howMany(['JoHn', 'Luke', 'AmAndA']))
+
+console.log(ballCollector([58, 68, 62, 69, 58]));
