9235db4e88a6f19d54fe3b7baeb1217af7452986
Jhonny <gonzodzen@mail.ru>
2022-12-29 01:47:41 +0300

    Write Number in Expanded Form

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-function magNumber(i) {
-  return i[0] === "PT92"
-    ? Math.ceil((i[1] * 3) / 17)
-    : i[0] === "M4A1" || i[0] === "M16A2"
-    ? Math.ceil((i[1] * 3) / 30)
-    : Math.ceil((i[1] * 3) / 5);
+function expandedForm(num) {
+  let arr = [];
+  let indArr = [];
+  const w = ("" + num).split("").map(Number);
+  w.map((el, i) => {
+      arr.push(el);
+      indArr.push(i);
+  });
+  indArr = indArr.reverse()
+  arr = arr.map(el => [el])
+  for(let i = 0; i < arr.length; i++){
+    arr[i].push(`${'0'.repeat(indArr[i])}`)
+  }
+  return arr.filter(el => el[0] !== 0).map(el => el.reduce((acc,curr) => acc+curr,'')).join` + `
 }
-
-console.log(magNumber(["PSG1", 31]));
+console.log(expandedForm(70304));
