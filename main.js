c896a44fd3f36a712b8bf9e1d3c920797f8032e7
Jhonny <gonzodzen@mail.ru>
2022-12-28 14:32:33 +0300

    Are they square?

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function oddOnesOut(nums) {
-  const res = Object.entries(
-    nums.reduce((acc, curr) => {
-      acc[curr] = (acc[curr] || 0) + 1;
-      return acc;
-    }, {})
-  )
-    .filter((el) => el[1] % 2 !== 0)
-    .map((el) => Number(el[0]));
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < res.length; j++) {
-      if (nums[i] === res[j]) {
-        nums[i] = "&";
-      }
-    }
-  }
-  return nums.filter((el) => el !== "&");
-}
-console.log(oddOnesOut([100, 100, 5, 5, 100, 50, 68, 50, 68, 50, 68, 5, 100]));
+const isSquare = (arr) =>
+  arr.length === 0 ? undefined : arr.map((el) => Number.isInteger(Math.sqrt(el))).every((el) => el === true);
+console.log(isSquare([1, 4, 9, 16, 25, 36]));
