00d0b34c6e1f07db26e5f28cc2d3e2ba88536681
Jhonny <gonzodzen@mail.ru>
2022-12-29 01:15:41 +0300

    Difference between years. (Level 1)

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-function determineTime (d) {
-  const arr = d.map(el => el.split(':'))
-  const res = Array(3).fill(0).map(el => [el])
-  for(let i = 0; i < arr.length; i++){
-    res[0].push(arr[i][0])
-    res[1].push(arr[i][1])
-    res[2].push(arr[i][2])
-  }
-  const nextRes = res.map(el => el.map(Number).reduce((acc,curr) => acc+curr,0))
-  if(nextRes[2] > 60){
-    let tmp = Math.floor(nextRes[2] / 60)
-    nextRes[2] = nextRes[2] % 60
-    nextRes[1] = nextRes[1] + tmp
-  }
-  if(nextRes[1] > 60){
-    let tmp = Math.floor(nextRes[1] / 60)
-    nextRes[1] = nextRes[1] % 60
-    nextRes[0] = nextRes[0] + tmp
-  }
-  return nextRes[0] < 24 || nextRes[0] === 24 && nextRes[1] === 0 && nextRes[2] === 0 ? true : false
+const howManyYears = (date1, date2) => {
+  const res = [date1, date2].map(el => [el.slice(0, 4)].map(Number))
+  return Math.abs(res[0] - res[1])
 }
-console.log(determineTime(["12:00:00", "12:00:01"]))
+console.log(howManyYears('1997/10/10', '2015/10/10'))
