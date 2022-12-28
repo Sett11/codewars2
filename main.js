2b4813f56568142c5b4e39d30ff5db24b1336355
Jhonny <gonzodzen@mail.ru>
2022-12-29 01:28:21 +0300

    Help the Elite Squad of Brazilian forces BOPE

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const howManyYears = (date1, date2) => {
-  const res = [date1, date2].map(el => [el.slice(0, 4)].map(Number))
-  return Math.abs(res[0] - res[1])
+function magNumber(i) {
+  return i[0] === "PT92"
+    ? Math.ceil((i[1] * 3) / 17)
+    : i[0] === "M4A1" || i[0] === "M16A2"
+    ? Math.ceil((i[1] * 3) / 30)
+    : Math.ceil((i[1] * 3) / 5);
 }
-console.log(howManyYears('1997/10/10', '2015/10/10'))
+
+console.log(magNumber(["PSG1", 31]));
