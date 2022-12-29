fa4de08ea64dcc12a96b783ead30c9cc2d97be25
Jhonny <gonzodzen@mail.ru>
2022-12-30 00:03:48 +0300

    All Star Code Challenge #1

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const orderedCount = (text) => {
-  text = text.split``
-  const set = [...new Set(text)];
-  let arr = [];
-  for (let i = 0; i < set.length; i++) {
-    arr.push(set[i]);
-  }
-  arr = arr.map((el) => [el]);
-  for (let i = 0; i < text.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (text[i] === arr[j][0]) {
-        arr[j].push(text[i]);
-      }
-    }
-  }
-  return arr.map((el) => [String(el.shift()), el.length]);
-};
-console.log(orderedCount("abracadabra"));
+function sumPPG(playerOne, playerTwo){
+  return playerOne.ppg + playerTwo.ppg
+}
