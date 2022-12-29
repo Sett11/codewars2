e7005da393c7addffe36748a1a284f1a103cf0fc
Jhonny <gonzodzen@mail.ru>
2022-12-30 00:01:45 +0300

    Ordered Count of Characters

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
-function timeCorrect(ts) {
-  if (!ts) {
-    return ts;
+const orderedCount = (text) => {
+  text = text.split``
+  const set = [...new Set(text)];
+  let arr = [];
+  for (let i = 0; i < set.length; i++) {
+    arr.push(set[i]);
   }
-  if (!/^\d{2,2}:{1,1}\d{2,2}:{1,1}\d{2,2}$/.test(ts)) {
-    return null;
+  arr = arr.map((el) => [el]);
+  for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (text[i] === arr[j][0]) {
+        arr[j].push(text[i]);
+      }
+    }
   }
-  ts = ts.split(":");
-  if(Number(ts[0]) < 24 && Number(ts[1]) <60 && Number(ts[2]) < 60){
-    return ts.join(":");
-  }
-  const reduceSec = Math.floor(Number(ts[2]) / 60);
-  if (Number(ts[2]) > 59 || ts[2].length !== 2) {
-    ts[1] = String(reduceSec + Number(ts[1]));
-    ts[2] = "0" + String(ts[2] % 60);
-  }
-  const reduceMin = Math.floor(Number(ts[1]) / 60);
-  if (Number(ts[1]) > 59 || ts[1].length !== 2) {
-    ts[0] = "0" + String(reduceMin + Number(ts[0]));
-    ts[1] = "0" + String(Number(ts[1]) % 60);
-  }
-  if (Number(ts[0]) > 24) {
-    ts[0] = "0" + String(Number(ts[0]) % 24);
-  }
-  if (ts[0].length > 2) {
-    ts[0] = ts[0].slice(1);
-  }
-  if (ts[1].length > 2) {
-    ts[1] = ts[1].slice(1);
-  }
-  if (ts[2].length > 2) {
-    ts[2] = ts[2].slice(1);
-  }
-  if(ts[0] === '24'){
-    ts[0] = '00'
-  }
-  return ts.join(":");
-}
-console.log(timeCorrect("11:70:10"));
+  return arr.map((el) => [String(el.shift()), el.length]);
+};
+console.log(orderedCount("abracadabra"));
