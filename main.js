ae47e5682a1ff06bde71838a4b1386606443eb78
Jhonny <gonzodzen@mail.ru>
2022-12-29 21:28:11 +0300

    Did she say hallo?

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-function multiple(x) {
-  return x % 3 === 0 && x % 5 === 0 ? 'BangBoom' :
-  x % 3 === 0 && x % 5 !== 0 ? 'Bang' :
-  x % 5 === 0 && x % 3 !== 0 ? 'Boom': 'Miss'
+function validateHello(greetings) {
+  res =  /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings) 
+  return res
 }
-console.log(multiple(30))
