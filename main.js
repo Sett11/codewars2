a07f06a674797c08d4b17678f5ee8a34be9afc33
Jhonny <gonzodzen@mail.ru>
2022-12-30 00:22:44 +0300

    Make acronym

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-function paul(x) {
-  const res = x
-    .map((el) => {
-      return el === "kata"
-        ? 5
-        : el === "Petes kata"
-        ? 10
-        : el === "eating"
-        ? 1
-        : 0;
-    })
-    .reduce((acc, curr) => acc + curr, 0);
-  return res < 40
-    ? "Super happy!"
-    : res >= 40 && res < 70
-    ? "Happy!"
-    : res >= 70 && res < 100
-    ? "Sad!"
-    : "Miserable!";
+function toAcronym(inp){
+  return inp.split` `.map(el => el[0].toUpperCase()).join``
 }
-console.log(paul(["life", "Petes kata", "Petes kata", "Petes kata", "eating"]));
+console.log(toAcronym("PHP: Hypertext Preprocessor"))
