7a49400829282843e8035bc344bb6ab0de99d1d0
Jhonny <gonzodzen@mail.ru>
2022-12-30 00:17:43 +0300

    Paul's Misery

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function sumPPG(playerOne, playerTwo){
-  return playerOne.ppg + playerTwo.ppg
+function paul(x) {
+  const res = x
+    .map((el) => {
+      return el === "kata"
+        ? 5
+        : el === "Petes kata"
+        ? 10
+        : el === "eating"
+        ? 1
+        : 0;
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+  return res < 40
+    ? "Super happy!"
+    : res >= 40 && res < 70
+    ? "Happy!"
+    : res >= 70 && res < 100
+    ? "Sad!"
+    : "Miserable!";
 }
+console.log(paul(["life", "Petes kata", "Petes kata", "Petes kata", "eating"]));
