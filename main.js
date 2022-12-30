e54e68b8fc47ea2b8cadc1fdb12d46a3e1cc2f9f
Jhonny <gonzodzen@mail.ru>
2022-12-30 21:35:00 +0300

    Find Screen Size

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-function scoreboard(s) {
-  s = s
-    .replace(/nil/g, "0")
-    .replace(/one/g, "1")
-    .replace(/two/g, "2")
-    .replace(/three/g, "3")
-    .replace(/four/g, "4")
-    .replace(/five/g, "5")
-    .replace(/six/g, "6")
-    .replace(/seven/g, "7")
-    .replace(/eight/g, "8")
-    .replace(/nine/g, "9")
-    .replace(/\D/g, "");
-  return s.split``.map(Number);
+function findScreenHeight(width, ratio) {
+    ratio = ratio.split`:`
+    return `${width}x${width / ratio[0] * ratio[1]}`
 }
-console.log(scoreboard("The score is four nil"));
+console.log(findScreenHeight(1024, '16:9'))
