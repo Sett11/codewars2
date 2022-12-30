612b085d9fda5916f76fcc81ad8a66ffb9939366
Jhonny <gonzodzen@mail.ru>
2022-12-30 13:48:10 +0300

    Function Cache

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function copyList(l){
-  return l.slice()
+function cache(func) {
+  const cash = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (!(key in cash)) {
+      const res = func.apply(null, args);
+      cash[key] = res;
+    }
+    return cash[key];
+  };
 }
+console.log(cache());
