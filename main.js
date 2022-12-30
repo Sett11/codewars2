306cfaa9304d891c6f5fec956ae8a5510ff73d03
Jhonny <gonzodzen@mail.ru>
2022-12-30 20:35:05 +0300

    Geometric Progression Sequence

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-Array.prototype.square = function(){
-    return this.slice().map(el => el**2)
+function geometricSequenceElements(a, r, n) {
+  const arr = [];
+  arr.push(a);
+  arr.push(a * r);
+  while (arr.length < n) {
+    arr.push(arr.reduce((acc) => acc * r, a));
   }
-  Array.prototype.cube = function(){
-    return this.slice().map(el => el**3)
-  }
-  Array.prototype.average = function(){
-    if(this.length === 0) return NaN
-    return this.slice().reduce((acc,curr) => acc + curr) / this.length
-  }
-  Array.prototype.sum = function(){
-    return this.slice().reduce((acc,curr) => acc + curr, 0)
-  }
-  Array.prototype.even = function(){
-    return this.slice().filter(el => el % 2 === 0)
-  }
-  Array.prototype.odd = function(){
-    return this.slice().filter(el => el % 2 !== 0)
-  }
+  return arr.join(", ");
+}
+console.log(geometricSequenceElements(1, -2, 10));
