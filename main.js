be4e596325dbecc03a46ab096c26dac795688908
Jhonny <gonzodzen@mail.ru>
2022-12-30 19:34:26 +0300

    Easy Time Convert

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-const last = xs => xs.length > 0 ? xs[xs.length - 1] : null
+function timeConvert(num) {
+    if(num < 1) return '00:00'
+    const hours = Math.floor(num / 60)
+    const min = num % 60
+    return `${hours < 10 ? '0' + hours : hours}:${min < 10 ? '0' + min : min}`
+}
+console.log(timeConvert(8))
