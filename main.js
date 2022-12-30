d7504b91f78cf9354c25ecf834fa4e75c876f5cb
Jhonny <gonzodzen@mail.ru>
2022-12-30 10:26:45 +0300

    Making Copies

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function repeats(arr){
-    let sum = null
-    arr.map((el,i,arr) => arr.indexOf(el) === arr.lastIndexOf(el) ? sum += el: null)
-    return sum
+function copyList(l){
+  return l.slice()
 }
-console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]))
