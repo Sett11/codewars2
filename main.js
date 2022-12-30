1bb0c394a366a22481e8a1c4d05e0bfd48bc1a05
Jhonny <gonzodzen@mail.ru>
2022-12-30 18:11:51 +0300

    Recursion 101

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-function testResult(a) {
-  const sum = a.reduce((acc, curr) => acc + curr, 0) / a.length,
-    arr = [],
-    arr1 = [],
-    arr2 = [];
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] > 8) {
-      arr.push(a[i]);
-    }
-    if (a[i] <= 8 && a[i] > 6) {
-      arr1.push(a[i]);
-    }
-    if (a[i] <= 6) {
-      arr2.push(a[i]);
+function solve(a, b) {
+  while(a >= (b*2) || b >= (a*2)){
+    if(a === 0 || b === 0){
+        return [a,b]
+    } else if(a >= b*2){
+        a = a%(b*2)
+    } else if(b >= (a*2)){
+        b  =b% (a*2)
     }
   }
-  return arr1.length === 0 && arr2.length === 0
-    ? [parseFloat(sum.toFixed(3)), { h: arr.length, a: arr1.length, l: arr2.length }, "They did well"]
-    : [parseFloat(sum.toFixed(3)), { h: arr.length, a: arr1.length, l: arr2.length }];
+  return [a,b]
 }
-console.log(testResult([5, 6, 4, 8, 9, 8, 9, 10, 10, 10]));
+
+console.log(solve(2, 10));
