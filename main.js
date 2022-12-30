fc343f7baf4290d7c58004cb8cdddb96ff48201f
Jhonny <gonzodzen@mail.ru>
2022-12-30 19:27:09 +0300

    99 Problems, #1: last in list

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-function reverseByCenter(s){
-    const len = Math.floor(s.length / 2)
-    if(s.length % 2 !== 0){
-        return s.slice(len+1, s.length) + s[len] + s.slice(0, len)
-    }
-    return s.slice(len, s.length) + s.slice(0, len)
-  }
-  console.log(reverseByCenter('secret'))
+const last = xs => xs.length > 0 ? xs[xs.length - 1] : null
