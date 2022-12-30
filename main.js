739fcbe19070b994fc78782d8afa87aa4f16bd1a
Jhonny <gonzodzen@mail.ru>
2022-12-30 22:15:10 +0300

    Harshad or Niven numbers

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-function findScreenHeight(width, ratio) {
-    ratio = ratio.split`:`
-    return `${width}x${width / ratio[0] * ratio[1]}`
-}
-console.log(findScreenHeight(1024, '16:9'))
+const Harshad = ( function() {
+    'use strict';
+  
+    return {
+      
+      isValid: function( number ) {
+       return number % (String(number).split('').reduce((acc,curr) => acc+curr*1, 0)) === 0
+      },
+     
+      getNext: function( number ) {
+       for(let i = number+1; ;i++){
+        if(i % (String(i).split('').reduce((acc,curr) => acc+curr*1, 0)) === 0){
+            return i
+        }
+       }
+      },
+     
+      getSerie: function( count, start ) {
+        const arr = []
+   for(let i = start + 1 || 0; ; i++ ){
+        if(i % (String(i).split('').reduce((acc,curr) => acc+curr*1, 0)) === 0){
+            arr.push(i)
+        }
+     if(arr.length === count){
+       break
+     }
+    }
+    return arr
+      }
+    };
+  
+  } () );
