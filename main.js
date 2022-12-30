ad0b2a2b3bf86f9d75e0e9edce0134c54981cac6
Jhonny <gonzodzen@mail.ru>
2022-12-30 22:47:56 +0300

    Training JS #16: Methods of String object--slice(), substring() and substr()

--- main.js
+++ main.js
@@ -1,32 +1,6 @@
-const Harshad = ( function() {
-    'use strict';
-  
-    return {
-      
-      isValid: function( number ) {
-       return number % (String(number).split('').reduce((acc,curr) => acc+curr*1, 0)) === 0
-      },
-     
-      getNext: function( number ) {
-       for(let i = number+1; ;i++){
-        if(i % (String(i).split('').reduce((acc,curr) => acc+curr*1, 0)) === 0){
-            return i
-        }
-       }
-      },
-     
-      getSerie: function( count, start ) {
-        const arr = []
-   for(let i = start + 1 || 0; ; i++ ){
-        if(i % (String(i).split('').reduce((acc,curr) => acc+curr*1, 0)) === 0){
-            arr.push(i)
-        }
-     if(arr.length === count){
-       break
-     }
-    }
-    return arr
-      }
-    };
-  
-  } () );
+function cutIt(arr){
+    const res = []
+     arr.map(el => res.push(el.length))
+     return arr.map(el => el.slice(0, Math.min(...res)))
+  }
+  console.log(cutIt(["abc","defgh","ijklmn"]))
