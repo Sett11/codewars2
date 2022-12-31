06354e99eba8a764225da7605860c999baacd20b
Jhonny <gonzodzen@mail.ru>
2022-12-31 19:54:44 +0300

    Arguments to Binary addition

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-class Ghost {
-  constructor(){
-    const arr = ["white", "yellow", "purple", "red"]
-    this.color = arr[Math.floor(Math.random() * 5)]
-  }
-};
+const arr2bin = arr => (arr.map(el => typeof el !== 'number' || el !== el ? 0 : el).reduce((acc,curr) => acc+curr,0)).toString(2)
+
+console.log(arr2bin([1,2,3,4,5]))
