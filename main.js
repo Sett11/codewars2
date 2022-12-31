61cc869a4523a9922548f32b4ee2180f635828da
Jhonny <gonzodzen@mail.ru>
2022-12-31 13:37:54 +0300

    Billiards triangle

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-function orderFood(list) {
-  const obj = {}
-  list.map(el => obj[el.meal] = 0)
-  for(let i = 0; i < list.length; i++){
-    if(list[i].meal){
-      obj[list[i].meal] += 1
+function pyramid(balls) {
+  if(balls === 1){
+    return 1
+  }
+  const arr = [], arr2 = []
+  let cou = 1
+  while(cou <= balls){
+    arr.push(cou)
+    cou++
+  }
+  for(let i = 0; i < arr.length; i++){
+    arr2.push(arr.slice(arr.length - i))
+    if(arr2.flat().length >= balls){
+      break
     }
   }
-  return obj
+  if(arr2.flat().length > balls){
+    return arr2.filter(el => el.length !== 0).length - 1
+  }
+  return arr2.filter(el => el.length !== 0).length
 }
-
-console.log(orderFood([
-  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
-    meal: 'vegetarian' },
-  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
-    meal: 'standard' },
-  { firstName: 'Vasia', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
-    meal: 'standard' },
-  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
-    meal: 'vegan' },
-  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
-    meal: 'vegetarian' },
-]))
+console.log(pyramid(9999))
