78a41ff2cdceb76975a538e0f961110b37e51dfc
Jhonny <gonzodzen@mail.ru>
2022-12-31 14:02:15 +0300

    Interactive Dictionary

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-function pyramid(balls) {
-  if(balls === 1){
-    return 1
+class Dictionary {
+  constructor() {
+    this.obj = {};
   }
-  const arr = [], arr2 = []
-  let cou = 1
-  while(cou <= balls){
-    arr.push(cou)
-    cou++
+
+  newEntry(key, value) {
+    return this.obj[key] = value;
   }
-  for(let i = 0; i < arr.length; i++){
-    arr2.push(arr.slice(arr.length - i))
-    if(arr2.flat().length >= balls){
-      break
-    }
+
+  look(key) {
+    return this.obj[key] || `Can\'t find entry for ${key}`;
   }
-  if(arr2.flat().length > balls){
-    return arr2.filter(el => el.length !== 0).length - 1
-  }
-  return arr2.filter(el => el.length !== 0).length
 }
-console.log(pyramid(9999))
