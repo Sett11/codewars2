8b51ad8bac99fcb7a73ddcc51f87697d44536b4b
Jhonny <gonzodzen@mail.ru>
2022-12-31 19:58:36 +0300

    Fruit string calculator

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const arr2bin = arr => (arr.map(el => typeof el !== 'number' || el !== el ? 0 : el).reduce((acc,curr) => acc+curr,0)).toString(2)
-
-console.log(arr2bin([1,2,3,4,5]))
+function calculate(s) {
+  return eval(s.replace(/loses/g, '-').replace(/gains/g, '+').replace(/[^0-9-+]/g, ''))
+}
+console.log(calculate("Panda has 48 apples and loses 4"))
