04cab6119a09814c3d938bba855fa18b43000f31
Jhonny <gonzodzen@mail.ru>
2022-12-31 12:38:36 +0300

    Coding Meetup #14 - Higher-Order Functions Series - Order the food

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-function pattern(n){
-    if(n === q){
-        return '1'
+function orderFood(list) {
+  const obj = {}
+  list.map(el => obj[el.meal] = 0)
+  for(let i = 0; i < list.length; i++){
+    if(list[i].meal){
+      obj[list[i].meal] += 1
     }
-    const arr = [], res = []
-    let cou = n
-    while(cou > 0){
-        arr.push(cou)
-        cou--
-    }
-    for(let i = 1; i <= arr.length; i++){
-        res.push(arr.slice(0, i) + '\n')
-    }
-    return res.map(el => el.split``.reverse().join``).reverse().join('').replace(/,/g, '').replace(/\n$/,'')
+  }
+  return obj
 }
-console.log(pattern(5))
+
+console.log(orderFood([
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Vasia', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+    meal: 'vegan' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+    meal: 'vegetarian' },
+]))
