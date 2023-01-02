b7ff6c84105b834fdc1bae0d3e2d4f3c5f90a2ed
Jhonny <gonzodzen@mail.ru>
2023-01-02 16:22:45 +0300

    Ultimate Array Reverser

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const myParseInt = str => str.trim().match(/[\D]/) ? NaN : +str
-
-console.log(myParseInt('99'))
+const ultimateReverse = (s) => {
+  const arr = [];
+  const z = s.join``.split``.reverse();
+  for (let i = 0; i < s.length; i++) {
+    arr.push(z.splice(0, s[i].length).join``);
+  }
+  return arr;
+};
+console.log(
+  ultimateReverse(["I", "like", "big", "butts", "and", "I", "cannot", "lie!"])
+);
