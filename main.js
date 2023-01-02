7d18c49e1e710a09f0805dbf68fd0a37a9de75be
Jhonny <gonzodzen@mail.ru>
2023-01-02 15:56:13 +0300

    String to integer conversion

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function moveVowel(inp) {
-  const arr = [];
-  inp.split``.map((el) =>
-    el === "a" || el === "i" || el === "e" || el === "o" || el === "u"
-      ? arr.push(el)
-      : el
-  );
-  inp = inp.replace(/a|i|e|o|u/g, "");
-  return inp + arr.join``;
-}
-console.log(moveVowel("apple"));
+const myParseInt = str => str.trim().match(/[\D]/) ? NaN : +str
+
+console.log(myParseInt('99'))
