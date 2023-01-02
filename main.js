675ea1ea9460246765d11bc93e59a7f45acf1127
Jhonny <gonzodzen@mail.ru>
2023-01-02 12:18:03 +0300

    Arithmetic sequence - sum of n elements

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-function sumOrProduct(a, n) {
-  const arr1 = [],
-    arr2 = [];
-  a = a.sort((a, b) => a - b);
-  for (let i = 0; i < n; i++) {
-    arr1.push(a[i]);
-    arr2.push(a[a.length - i-1]);
+function ArithmeticSequenceSum(a, r, n) {
+  const arr = [a];
+  while (arr.length < n) {
+    arr.push(arr.reduce((acc, curr) => acc + r, a));
   }
-  const sum1 = arr1.reduce((acc, curr) => acc * curr)
-  const sum2 = arr2.reduce((acc, curr) => acc + curr)
-  return sum1 > sum2 ? 'product' : sum1 < sum2 ? 'sum' : 'same'
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
-console.log(sumOrProduct([10, 20, 3, 30, 5, 4], 3));
+console.log(ArithmeticSequenceSum(2, 2, 10));
