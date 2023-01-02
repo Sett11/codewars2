8d786f209a44e2004f231b3a8b7e768027045716
Jhonny <gonzodzen@mail.ru>
2023-01-02 16:44:00 +0300

    Find the next perfect square!

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-const ultimateReverse = (s) => {
-  const arr = [];
-  const z = s.join``.split``.reverse();
-  for (let i = 0; i < s.length; i++) {
-    arr.push(z.splice(0, s[i].length).join``);
-  }
-  return arr;
-};
-console.log(
-  ultimateReverse(["I", "like", "big", "butts", "and", "I", "cannot", "lie!"])
-);
+function findNextSquare(sq) {
+  let min = Math.sqrt(sq)
+  if(!Number.isInteger(min)) return -1
+  return (min+1)**2 
+}
+console.log(findNextSquare(625))
