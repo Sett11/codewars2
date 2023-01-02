4bfefbf7fa7d9828359c5db8e0bb5111c56b4968
Jhonny <gonzodzen@mail.ru>
2023-01-02 21:51:18 +0300

    Piano Kata, Part 1

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
-function changer(str) {
-  let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  str = str.split``
-  for(let i = 0; i < str.length; i++){
-    if(str[i] === ' ' || str[i] === '0' || str[i] === '1' || str[i] === '2' || str[i] === '3' || str[i] === '4' || str[i] === '5' || str[i] === '6' || str[i] === '7' || str[i] === '8' || str[i] === '9'){
-      continue
-    }
-      str[i] = (parseInt(str[i], 36) - 9)
+function blackOrWhiteKey(k) {
+  if(k > 88){
+    k = k % 88
   }
-  return str
-  .map(el => {
-    if(el === ' ' || el === '0' || el === '1' || el === '2' || el === '3' || el === '4' || el === '5' || el === '6' || el === '7' || el === '8' || el === '9'){
-      return el
-    } else{
-
-    }
-    el = arr[el]
-    if(el === 'a' || el === 'u' || el ==='o' || el === 'i' || el === 'e'){
-      return el.toUpperCase()
-    }
-    else{
-      return el.toLowerCase()
-    }
-  }).join``
+  if(k === 2 || k === 5 || k === 7 || k === 10 || k === 12 || k === 14 || k === 17 || k === 19 || k === 22 || k === 24 || k === 26 || k === 29 || k === 31 || k === 34 || k === 36 || k === 38 || k === 41 || k === 43 || k === 46 || k === 48 || k === 50 || k === 53 || k === 55 || k === 58 || k === 60 || k === 62 ||k === 65 || k === 67 || k === 70 || k === 72 || k === 74 || k === 77 || k === 79 || k === 82 || k === 84 || k === 86){
+    return 'black'
+  }
+  return 'white'
 }
-
-console.log(changer('ZZ'))
+console.log(blackOrWhiteKey(2017))
