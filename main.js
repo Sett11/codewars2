95c5f213070575d0d98b707134ae7ead9a913852
Jhonny <gonzodzen@mail.ru>
2023-01-03 15:35:20 +0300

    Capitals first!

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-function createSecretHolder(secret) {
-    return {
-        getSecret: function(){
-            return secret
-        },
-        setSecret: function(arg){
-            secret = arg
+function capitalsFirst(str){
+    str = str.split` `
+    const arr = [], arr2 = []
+    for(let i = 0; i < str.length; i++){
+        if(!str[i][0].match(/[a-zA-Z]/)){
+            continue
+        }
+        if(str[i][0].match(/[a-zA-Z]/) && str[i][0] === str[i][0].toUpperCase()){
+            arr.push(str[i])
+        }
+        if(str[i][0].match(/[a-zA-Z]/) && str[i][0] === str[i][0].toLowerCase()){
+            arr2.push(str[i])
         }
     }
-}
-const obj = createSecretHolder(5)
-obj.setSecret(7)
-console.log(obj.getSecret())
+    return arr.length !== 0 && arr2.length !== 0 ? arr.join` ` + ' ' + arr2.join` ` :
+    arr.length === 0 ? arr2.join` ` : arr2.length === 0 ? arr.join` ` : ''
+  }
+  console.log(capitalsFirst("hey You, Sort me Already!"))
