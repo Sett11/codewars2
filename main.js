60491fdebc0c36c0163dc67c179edee0089521d8
Jhonny <gonzodzen@mail.ru>
2023-01-03 15:59:50 +0300

    Area of a Circle

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-function capitalsFirst(str){
-    str = str.split` `
-    const arr = [], arr2 = []
-    for(let i = 0; i < str.length; i++){
-        if(!str[i][0].match(/[a-zA-Z]/)){
-            continue
-        }
-        if(str[i][0].match(/[a-zA-Z]/) && str[i][0] === str[i][0].toUpperCase()){
-            arr.push(str[i])
-        }
-        if(str[i][0].match(/[a-zA-Z]/) && str[i][0] === str[i][0].toLowerCase()){
-            arr2.push(str[i])
-        }
+const circleArea = (r) => {
+    const res = Number((Math.PI * r**2).toFixed(2))
+      return r <= 0 || typeof r !== 'number' || r !== r? false : res
     }
-    return arr.length !== 0 && arr2.length !== 0 ? arr.join` ` + ' ' + arr2.join` ` :
-    arr.length === 0 ? arr2.join` ` : arr2.length === 0 ? arr.join` ` : ''
-  }
-  console.log(capitalsFirst("hey You, Sort me Already!"))
+console.log(circleArea('lll'))
