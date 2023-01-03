7cffa783d567cd853bc8ca74f7803c90457b54ef
Jhonny <gonzodzen@mail.ru>
2023-01-04 02:50:33 +0300

    How many days are we represented in a foreign country?

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-function minimumSum(values, n) {
-    if(values.length === 0||n===0){
-      return 0
-    }
-       const arr = values.slice().sort((a,b) => a - b)
-       arr.length = n
-       return arr.reduce((acc,curr) => acc+curr,0)
-    }
-    
-    function maximumSum(values, n) {
-       if(values.length === 0||n===0){
-      return 0
-    }
-      const arr = values.slice().sort((a,b) => b - a)
-       arr.length = n
-       return arr.reduce((acc,curr) => acc+curr,0)
-    }
-  console.log(minimumSum([5, 4, 3, 2, 1], 2))
-  console.log(maximumSum([5, 4, 3, 2, 1], 8))
+function daysRepresented(trips) {
+    const res = []
+  const arr = trips
+    .map((el) => [el, []])
+    .map((el) => {
+      if (el[0][1] > el[0][0]) {
+        for (let i = el[0][0]; i <= el[0][1]; i++) {
+          el[1].push(i);
+        }
+      }
+      return el;
+    })
+    .map((el) => el[1]).map(el=>{
+        for(let i = 0; i < el.length; i++){
+            res.push(el[i])
+        }
+    })
+  return [...new Set(res)].length
+}
+
+console.log(
+  daysRepresented([ [ 54, 93 ],
+    [ 8, 43 ],
+    [ 101, 128 ],
+    [ 17, 27 ],
+    [ 171, 180 ],
+    [ 210, 220 ] ])
+);
