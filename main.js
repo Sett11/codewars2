75fbc18d8d3d4563afa34bbd07e88bd9d7b3857e
Jhonny <gonzodzen@mail.ru>
2023-01-03 16:20:06 +0300

    String Reversing, Changing case, etc.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const circleArea = (r) => {
-    const res = Number((Math.PI * r**2).toFixed(2))
-      return r <= 0 || typeof r !== 'number' || r !== r? false : res
-    }
-console.log(circleArea('lll'))
+function reverseAndMirror(s1,s2) {
+    s2 = s2.split``.reverse().map(el => el === el.toUpperCase() ? el.toLowerCase(): el.toUpperCase()).join``
+    s1 = s1.split``.reverse().map(el => el === el.toUpperCase() ? el.toLowerCase(): el.toUpperCase()).join``
+    return `${s2}@@@${s1}${s1.split``.reverse().join``}`
+  }
+  console.log(reverseAndMirror("FizZ", "buZZ"))
