4e6de2a843b51df49b72680303b7b426e7d27ef2
Jhonny <gonzodzen@mail.ru>
2023-01-04 03:24:47 +0300

    Basic JS - Calculating averages

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-function solve(a, b) {
-  let totalA = 0, totalB = 0
-  for(let i = 0; i < a.length; i++){
-    if(a[i] > b[i]){
-        totalA+=1
+var Calculator = {
+    average: function() {
+      if(arguments.length === 0) return 0
+        const arr = []
+        for(let i = 0; i < arguments.length; i++){
+           arr.push(arguments[i])
+        }
+        return arr.reduce((acc,curr)=>acc+curr,0) / arr.length
     }
-    if(b[i] > a[i]){
-        totalB+=1
-    }
-  }
-  const res = `${totalA}, ${totalB}`
-  return totalA === totalB ? `${res}: that looks like a "draw"! Rock on!` :
-  totalA > totalB ? `${res}: Alice made "Kurt" proud!` : `${res}: Bob made "Jeff" proud!`
-}
-console.log(solve([25, 50, 22], [34, 49, 50]))
+   };
