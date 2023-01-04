7662453a6dfa9d66c15b76c876e070fb03d31d0b
Jhonny <gonzodzen@mail.ru>
2023-01-04 21:00:05 +0300

    IPv4 в int32

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function password(str) {
-  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(str)
+function ipToInt32(ip){
+  return ip.split`.`.reduce((acc,curr)=>acc*256 + +curr)
 }
-console.log(password("abcd123a"))
+console.log(ipToInt32("128.32.10.1"))
