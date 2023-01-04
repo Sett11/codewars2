3d06d5621f8b1f4e957678bd6b71b87f77a691ed
Jhonny <gonzodzen@mail.ru>
2023-01-04 13:50:42 +0300

    Thinkful - List and Loop Drills: Inverse Slicer

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function onesComplement(n) {
-  return n.toString(2).split``.map(el => el === '0' ? el = '1' : el = '0').join``
-};
+function inverseSlice(items, a, b) {
+  const str = items.slice(a,b).join` `
+  const r = new RegExp(`${str}`)
+  return items.join` `.replace(r, '').split` `.filter(el => el !== '').map(el => Number(el) === Number(el) ? Number(el) : el)
+}
+console.log(inverseSlice(['Intuition', 'is', 'a', 'poor', 'guide', 'when', 'facing', 'probabilistic', 'evidence'], 5, 13))
