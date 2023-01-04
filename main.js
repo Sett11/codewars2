a98135db917e07f1505461c8ab1800035532a5bd
Jhonny <gonzodzen@mail.ru>
2023-01-04 12:30:16 +0300

    Ones' Complement

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function catMouse(x, j) {
-  if (x.indexOf("C") === -1||x.indexOf("m") === -1||x.indexOf("D") === -1)return "boring without all three";
-  const cat = x.indexOf('C')
-  const mouse = x.indexOf('m')
-  const dog = x.indexOf('D')
-  if(Math.abs(cat - mouse) > j)return 'Escaped!'
- 
- if(x.replace(/\./g,'') === 'CDm' || x.replace(/\./g,'') === 'mDC')return 'Protected!'
- return "Caught!"
-}
-console.log(catMouse('m.D.C.',3));
+function onesComplement(n) {
+  return n.toString(2).split``.map(el => el === '0' ? el = '1' : el = '0').join``
+};
