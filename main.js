a8b678511f013741118bdfee05846ded9f3be53b
Jhonny <gonzodzen@mail.ru>
2023-01-05 00:53:44 +0300

    Simple Encryption #1 - Alternating Split

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-function closest(strng) {
-  if(strng.length === 0){
-    return []
-  }
-  const arr = [], arr2 = [], db = strng.split` `.map((el) => [el, [], []]).map((el,i) => {
-    el[2].push(el[0])
-    el[1].push(i)
-    el[0] = el[0].split``.map(Number).reduce((acc,curr)=>acc+curr,0)
-    el[0] = [el[0]]
-    return el
-  }).sort((a,b)=>a[0] - b[0] || a[1] - b[1]).map((el) => {
-    arr.push(el[0][0])
-    return el
-  })
-  for(let i = 0; i < arr.length; i++){
-    arr2.push(arr[i+1] - arr[i])
-  }
-  const res = arr2.map(el => Number.isNaN(el) || el !== el ? '&' : el).filter(el => el !=='&')
-  const min = Math.min(...res)
-  const ind = res.indexOf(min)
-  const F = db.slice(ind, ind + 2)
-  return [[F[0][0][0], F[0][1][0], Number(F[0][2][0])], [F[1][0][0], F[1][1][0], Number(F[1][2][0])]]
+function encrypt(text, n) {
+  if(text===''||!text||n<1)return text
+  const even = [], odd = []
+ text.split``.map((el,i) => i % 2 === 0 ? even.push(el):odd.push(el))
+ const res = odd.concat(even).join``
+ if(n===1){
+  return res
+ }else{
+  return encrypt(res, n-1)
+ }
 }
-console.log(closest("456899 50 11992 176 272293 163 389128 96 290193 85 52"))
+
+function decrypt(encryptedText, n) {
+  if(encryptedText===''||!encryptedText||n<1)return encryptedText
+  const first = encryptedText.slice(Math.floor(encryptedText.length / 2)).split``
+  const last = encryptedText.slice(0, Math.floor(encryptedText.length / 2)).split``
+  const arr = []
+  for(let i = 0; i < first.length + last.length; i++){
+    arr.push(first[i])
+    arr.push(last[i])
+  }
+  const res = arr.join``
+ if(n===1){
+  return res
+ }else{
+  return decrypt(res, n-1)
+ }
+}
+console.log(decrypt(" Tah itse sits!", 3))
