cc4db213ab0b8a78c0c20f80b75ba0a7e678adbe
Jhonny <gonzodzen@mail.ru>
2023-01-04 12:27:25 +0300

    Cat and Mouse - Harder Version

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-var Calculator = {
-    average: function() {
-      if(arguments.length === 0) return 0
-        const arr = []
-        for(let i = 0; i < arguments.length; i++){
-           arr.push(arguments[i])
-        }
-        return arr.reduce((acc,curr)=>acc+curr,0) / arr.length
-    }
-   };
+function catMouse(x, j) {
+  if (x.indexOf("C") === -1||x.indexOf("m") === -1||x.indexOf("D") === -1)return "boring without all three";
+  const cat = x.indexOf('C')
+  const mouse = x.indexOf('m')
+  const dog = x.indexOf('D')
+  if(Math.abs(cat - mouse) > j)return 'Escaped!'
+ 
+ if(x.replace(/\./g,'') === 'CDm' || x.replace(/\./g,'') === 'mDC')return 'Protected!'
+ return "Caught!"
+}
+console.log(catMouse('m.D.C.',3));
