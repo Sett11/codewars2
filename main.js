81c79bba092e09c7391dcb5e199e8f40e72eeedb
Jhonny <gonzodzen@mail.ru>
2023-01-05 01:13:43 +0300

    Inside Out Strings

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-function encrypt(text, n) {
-  if(text===''||!text||n<1)return text
-  const even = [], odd = []
- text.split``.map((el,i) => i % 2 === 0 ? even.push(el):odd.push(el))
- const res = odd.concat(even).join``
- if(n===1){
-  return res
- }else{
-  return encrypt(res, n-1)
- }
+function insideOut(x){
+  return x.split` `.map(el => el.split``).map((el) => {
+    if(el.length < 4){
+      return el
+    }
+    if(el.length % 2 === 0 && el.length > 3){
+      el = el.slice(0, el.length/2).reverse().concat(el.slice(el.length/2).reverse())
+      return el
+    }
+    if(el.length % 2 !== 0 && el.length > 3){
+      el = el.slice(0, Math.floor(el.length/2)).reverse() + el.join``.charAt(Math.floor(el.length/2)) + el.slice(Math.ceil(el.length/2)).reverse()
+      return el
+    }
+  }).join` `.replace(/,/g, '')
 }
-
-function decrypt(encryptedText, n) {
-  if(encryptedText===''||!encryptedText||n<1)return encryptedText
-  const first = encryptedText.slice(Math.floor(encryptedText.length / 2)).split``
-  const last = encryptedText.slice(0, Math.floor(encryptedText.length / 2)).split``
-  const arr = []
-  for(let i = 0; i < first.length + last.length; i++){
-    arr.push(first[i])
-    arr.push(last[i])
-  }
-  const res = arr.join``
- if(n===1){
-  return res
- }else{
-  return decrypt(res, n-1)
- }
-}
-console.log(decrypt(" Tah itse sits!", 3))
+console.log(insideOut('take me to semynak'))
