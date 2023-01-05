faabdf8c31ed0410a94d8765f554f2e70f1b01ac
Jhonny <gonzodzen@mail.ru>
2023-01-05 22:50:56 +0300

    Hidden "Cubic" numbers

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function isolateIt(arr){
-  return arr.map((el)=>{
-    if(el.length % 2 === 0){
-      el = el.slice(0, ~~el.length/2)+'|'+el.slice(~~el.length/2)
+function isSumOfCubes(s){
+  s = s.replace(/\D/g, ' ').split` `.filter(el=>el!=='').map((el)=>{
+    if(el.length<4){
       return el
-    }else{
-      el = el.slice(0,~~el.length/2)+'|'+el.slice(Math.ceil(el.length/2))
-      return el
+    } else{
+      const arr = []
+      for(let i = 0; i < el.length; i+=3){
+        arr.push(el.slice(i,i+3))
+      }
+      return arr
     }
-  })
+  }).join` `.replace(/,/g, ' ').split` `
+  const db = s.map((el)=>{
+    return [el, el.split``.map(Number).map(ul=>ul**3).reduce((acc,curr)=>acc+curr,0)]
+  }).filter(el=>el[0]==el[1]).map(el=>el[1])
+  return db.length === 0 ? 'Unlucky' : `${db.join` `} ${db.reduce((acc,curr)=>acc+curr,0)} Lucky`
 }
-console.log(isolateIt(["abcde","fghij", 'kkkk']))
+console.log(isSumOfCubes("&z371 upon 407298a --- dreary, ###100.153 I thought, 9926315strong and weary -127&() 1"))
