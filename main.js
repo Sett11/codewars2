3b5d2be5111a7233422d4d1226cc324183ee759b
Jhonny <gonzodzen@mail.ru>
2023-01-05 19:59:52 +0300

    Training JS #23: methods of arrayObject---push(), pop(), shift() and unshift()

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const shuffleIt = (arr,...arg) => {
-  for(let i = 0; i < arg.length; i++){
-    [arr[arg[i][0]], arr[arg[i][1]]] = [arr[arg[i][1]], arr[arg[i][0]]]
+function infiniteLoop(arr,d,n){
+  if(d==='left'){
+    while(n > 0){
+      arr[2].push(arr[0].shift())
+      arr[1].push(arr[2].shift())
+      arr[0].push(arr[1].shift())
+      n--
+    }
+  }
+  if(d==='right'){
+    while(n > 0){
+      arr[0].unshift(arr[2].pop())
+      arr[2].unshift(arr[1].pop())
+      arr[1].unshift(arr[0].pop())
+      n--
+    }
   }
   return arr
 }
-console.log(shuffleIt([1,2,3,4,5],[1,2],[3,4],[2,3]))
+console.log(infiniteLoop([[1,2,3],[4,5,6],[7,8,9]],"right",1))
