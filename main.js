128e6feec859513b7576c4671bc4394fd55a6f63
Jhonny <gonzodzen@mail.ru>
2023-01-05 20:58:27 +0300

    Training JS #25: methods of arrayObject---reverse() and sort()

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-function threeInOne(arr){
-  arr=arr.slice()
-  const res = []
-  for(let i = 0; i < arr.length; i+=3){
-    res.push(arr.slice(i, i+3))
-  }
-  return res.map(el=>el.reduce((acc,curr)=>acc+curr,0))
+function sortIt(arr){
+  return arr.slice().sort((a,b)=>{
+    const first = arr.filter(el=>el===a).length
+    const last = arr.filter(el=>el===b).length
+    if(first===last){return b-a}
+    if(first>last){return 1}
+     return -1
+  })
 }
-console.log(threeInOne([1,3,5,2,4,6,7,7,7]))
+console.log(sortIt([1,2,3,4,4,5,5,6,6]))
