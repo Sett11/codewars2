d378adaa9628794e4ed0a8632f8168b6fd1ec6df
Jhonny <gonzodzen@mail.ru>
2023-01-06 20:26:25 +0300

    Simple Simple Simple String Expansion

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function climb(n) {
-  const arr = []
-  while(n>=1){
-    arr.push(n)
-    n = ~~(n/2)
+function stringExpansion(s) {
+  if(!s.match(/[\d]/)){
+    return s
   }
-  return arr.reverse()
+  s=1+s
+  const arr = s.match(/\d[a-zA-Z]+/g)
+  if(arr===null){
+    return ''
+  }
+  let str = ''
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 1; j < arr[i].length; j++){
+      str+=arr[i][j].repeat(arr[i][0])
+    }
+  }
+  return str
 }
-console.log(climb(10));
+console.log(stringExpansion('0d0a0v0t0y'))
