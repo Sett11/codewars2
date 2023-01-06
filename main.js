3af4ef9c9fad3aa942c0b134309e6f83fa7959e6
Jhonny <gonzodzen@mail.ru>
2023-01-06 03:14:01 +0300

    Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function factory(x){
-  return function(arr){
-    return arr.map(el=>el*x)
-  }
+function product (s) {
+  const str = s.match(/!/g), nextStr = s.match(/\?/g)
+  return !str||!nextStr?0: nextStr.length*str.length
 }
+console.log(product("!????!!!?"))
