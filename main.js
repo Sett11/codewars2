2a8588062fbc299c0b1ec2a9c5696c33363056e9
Jhonny <gonzodzen@mail.ru>
2023-01-06 22:32:50 +0300

    Arithmetic Sequence!

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function convertHashToArray(hash){
-  return Object.entries(hash).sort()
-}
-console.log(convertHashToArray({name: "Jeremy", age: 24}))
+const nthterm = (f, n, c) => {
+  const arr = [f]
+  while(arr.length < n+1){
+    arr.push(arr.reduce((acc)=>acc+=c,f))
+  }
+  return arr.reverse()[0]
+};
+console.log(nthterm(-50,10,20));
