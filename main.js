3abef6702231d89e98b498595f15df3fd863f038
Jhonny <gonzodzen@mail.ru>
2023-01-07 13:57:19 +0300

    Most valuable character

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-function maxSum(arr,range){
-  const res = []
-  for(let i = 0, j = 0; i < arr.length, j < range.length; i++, j++){
-    res.push(arr.slice(range[j][0], range[j][1]+1))
-  }
-  return Math.max(...res.map(el=>el.reduce((acc,curr)=>acc+curr,0)))
+function solve(st) {
+  return st.split``.map((el,i,arr)=>{
+    el = [el, Math.abs(arr.indexOf(el) - arr.lastIndexOf(el))]
+    return el
+  }).sort((a,b)=>b[1]-a[1]||a[0].localeCompare(b[0]))[0][0]
 }
-console.log(maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3],[0,4],[6,8]]))
+console.log(solve("axyzxyz"))
