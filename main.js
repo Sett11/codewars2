147b20905981dbc8023f4770b4c541857cdfdf6a
Jhonny <gonzodzen@mail.ru>
2023-01-07 16:12:30 +0300

    Errors : histogram

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function orderWord(s){
-  return s? s.split``.sort().join``: 'Invalid String!'
+function hist(s) {
+  s = s.replace(/[^w+|u+|z+|x+]/gi, '').split``.sort().join``.replace(/w+|u+|z+|x+/gi, ' $&').split` `.filter(el=>el!=='')
+  let str = ''
+  for(let i = 0; i < s.length; i++){
+    str += `${s[i][0]}  ${s[i].length}     ${'*'.repeat(s[i].length)}${'\n'}   `
+  }
+  return str.slice(0, str.length) 
 }
-console.log(orderWord("Hello, World!"))
+console.log(hist("tpwaemuqxdmwqbqrjbeosjnejqorxdozsxnrgpgqkeihqwybzyymqeazfkyiucesxwutgszbenzvgxibxrlvmzihcb"))
