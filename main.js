ce83d9890d2e242aa15d3a4e6336b3812bf62c19
Jhonny <gonzodzen@mail.ru>
2023-01-15 22:41:32 +0300

    Find within array

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function printerError(s) {
-  return `${s.replace(/[^n-z]/gi,'').length}/${s.length}`
+const trueIfValueEqualsIndex = function(v, i) { return v === i; }
+
+const findInArray = (array, iterator) => {
+  const arr = []
+  for(let i = 0; i < array.length; i++){
+    if(iterator(array[i], i)){
+      arr.push(i)
+    }
+  }
+  return arr.length===0?-1:arr[0]
 }
-console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
+console.log(findInArray([13,5,3,1,4,5], trueIfValueEqualsIndex))
