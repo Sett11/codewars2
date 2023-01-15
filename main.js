4c0f40c718df143229b82b2c0e019a1379f92937
Jhonny <gonzodzen@mail.ru>
2023-01-15 19:44:56 +0300

    Scaling Squared Strings

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-function reject(array, predicate) {
-  const newArr = array.slice().filter(predicate)
-  for(let i = 0; i <array.length; i++){
-    if(newArr.indexOf(array[i])!==-1){
-      array[i] = '&'
+function scale(s, k, n) {
+  if(s==='')return ''
+  const arr = s.split`\n`.map(el=>el.split``)
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[i].length; j++){
+      arr[i][j] = arr[i][j].repeat(k)
     }
+    arr[i] = arr[i].join``
+    let str = ''
+    str = arr[i] + '\n'
+    arr[i] = str
+    arr[i] = arr[i].repeat(n)
   }
-  return array.filter(el=>el!=='&')
+  const res = arr.join``
+  return res.slice(0,res.length-1)
 }
-console.log(reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0))
+console.log(scale("abcd\nefgh\nijkl\nmnop", 2, 3))
