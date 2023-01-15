77af04e30ed5775e8a9d98b4bf04ee2ba01cfce5
Jhonny <gonzodzen@mail.ru>
2023-01-15 23:58:57 +0300

    Moves in squared strings (I)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function lowestProduct(input) {
-  if(input.length < 4) return "Number is too small"
-  let arr = []
-  for(let i = 0; i < input.length; i++){
-    arr.push(input.slice(i, i+4))
-  }
-  arr = arr.filter(el=>el.length>=4)
-  return Math.min(...arr.map(el=>el.split``.map(Number).reduce((acc,curr)=>acc*curr)))
+function vertMirror(s) {
+  return s.split`\n`.map(el=>el.split``.reverse().join``).join`\n`
 }
-console.log(lowestProduct('1234111'))
+function horMirror(s) {
+  return s.split`\n`.reverse().join`\n`
+}
+function oper(fct, s) {
+  return fct(s)
+}
+console.log(oper(horMirror, "abcd\nefgh\nijkl\nmnop"))
