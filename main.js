d1732a29e2d74e0eb3da2ef69249c6ce63e2a622
Jhonny <gonzodzen@mail.ru>
2023-01-15 21:53:07 +0300

    Count consonants

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function elapsedSeconds(s, e){
-  return (Date.parse(e) - Date.parse(s))/1000
+function consonantCount(str) {
+  return str.replace(/a|u|e|i|o|_/gi,'').replace(/[^a-zA-Z]/gi,'').length
 }
-console.log(elapsedSeconds(new Date(2013, 1, 1, 0, 0, 1), new Date(2013, 1, 1, 0, 0, 20)))
+console.log(consonantCount("0123456789"))
