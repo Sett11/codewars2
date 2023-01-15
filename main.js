14a0b2e7a808eaec20a154636e6754dffb69672a
Jhonny <gonzodzen@mail.ru>
2023-01-16 00:14:38 +0300

    Moves in squared strings (II)

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-function vertMirror(s) {
-  return s.split`\n`.map(el=>el.split``.reverse().join``).join`\n`
+
+function rot(s) {
+  return s.split`\n`.map(el=>el.split``.reverse().join``).reverse().join`\n`
 }
-function horMirror(s) {
-  return s.split`\n`.reverse().join`\n`
+function selfieAndRot(s) {
+  const res1 = s.split`\n`.map(el=>{
+    el = el + '.'.repeat(el.length) + '\n'
+    return el
+  }).join``
+  const res2 = s.split`\n`.map(el=>el.split``.reverse().join``).reverse().map(el=>{
+    el = '\n' + '.'.repeat(el.length) + el
+    return el
+  }).join``
+  return res1.slice(0, res1.length-1) + res2
 }
 function oper(fct, s) {
   return fct(s)
 }
-console.log(oper(horMirror, "abcd\nefgh\nijkl\nmnop"))
+console.log(selfieAndRot("abcd\nefgh\nijkl\nmnop"))
