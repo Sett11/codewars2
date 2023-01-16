82732c73e9940f84ab1e39ceaa8d128758ef1df5
Jhonny <gonzodzen@mail.ru>
2023-01-16 10:29:05 +0300

    Sorting Dictionaries

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function generateName(){
-  const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-  const arr = []
-  while(arr.length<6){
-    arr.push(str[Math.floor(Math.random()*52)])
-  }
-  return photoManager.nameExists(arr.join``) ? generateName() : arr.join``
+function sortDict(dict) {
+  return Object.entries(dict)
+  .map(el=>el.map(ul=>typeof ul !== 'number' && Number(ul)===Number(ul)? Number(ul):ul))
+  .sort((a,b)=>b[1]-a[1])
 }
+console.log(sortDict({'a':6,'b':2,'c':4}))
