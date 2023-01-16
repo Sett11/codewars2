8abb48c7f683d103cc516a735cd219e000985e95
Jhonny <gonzodzen@mail.ru>
2023-01-16 20:53:06 +0300

    Sentences should start with capital letters.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const unique = arr => arr.filter((el,i,arr)=>{
- return arr.indexOf(el)==i
-})
-
-console.log(unique([1,2,2,3]))
+function fix(p){
+  return p.split`.`.map((el,i)=>{
+    if(el[0]!==' '){
+      el = el.charAt(0).toUpperCase() + el.slice(1)
+      return el
+    }
+    if(el[0]===' '){
+      el = el.charAt(0) + el.charAt(1).toUpperCase() + el.slice(2)
+      return el
+    }
+  }).join`.`
+}
+console.log(fix("hi. my name is inigo montoya. you killed my father. prepare to die."))
