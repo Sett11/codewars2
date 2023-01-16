a07df893893fdcb753e6aed2f4a1efad7bae9c46
Jhonny <gonzodzen@mail.ru>
2023-01-16 17:17:31 +0300

    Basics 03: Strings, Numbers and Calculation

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-function spacey(array){
-  const arr = []
-  for(let i = 0; i <= array.length; i++){
-    arr.push(array.slice(0,i))
-  }
-  return arr.map(el=>el.join``).filter(el=>el!=='')
+function calculateString(st){
+  return (eval(st.replace(/[^0-9.+-/*-]/g,''))).toFixed(0)
 }
-console.log(spacey(['this','cheese','has','no','holes']))
+console.log(calculateString("a1a2b3c.c0c/a1a0b.cc00c"))
