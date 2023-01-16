71f3f9b5995c75009ed607d3f4a60c932b04f474
Jhonny <gonzodzen@mail.ru>
2023-01-16 09:45:44 +0300

    Get key/value pairs as arrays

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-
-function rot(s) {
-  return s.split`\n`.map(el=>el.split``.reverse().join``).reverse().join`\n`
+function keysAndValues(data){
+  return [Object.keys(data), Object.values(data)]
 }
-function selfieAndRot(s) {
-  const res1 = s.split`\n`.map(el=>{
-    el = el + '.'.repeat(el.length) + '\n'
-    return el
-  }).join``
-  const res2 = s.split`\n`.map(el=>el.split``.reverse().join``).reverse().map(el=>{
-    el = '\n' + '.'.repeat(el.length) + el
-    return el
-  }).join``
-  return res1.slice(0, res1.length-1) + res2
-}
-function oper(fct, s) {
-  return fct(s)
-}
-console.log(selfieAndRot("abcd\nefgh\nijkl\nmnop"))
+console.log(keysAndValues({a: 1, b: 2, c: 3}))
