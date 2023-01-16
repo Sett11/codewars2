066aa3800f109db27cdf5b29169e0904e9707434
Jhonny <gonzodzen@mail.ru>
2023-01-16 21:23:39 +0300

    The old switcheroo 2

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-function fix(p){
-  return p.split`.`.map((el,i)=>{
-    if(el[0]!==' '){
-      el = el.charAt(0).toUpperCase() + el.slice(1)
-      return el
+function encode(str) {
+  let arr = 'abcdefghijklmnopqrstuvwxyz'
+  let arr2 = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase()
+  return str.split``.map(el=>{
+    for(let i = 0; i < arr.length; i++){
+      if(el===arr[i]){
+        el = i+1
+      }
+      else if(el===arr2[i]){
+        el = i+1
+      }
+      continue
     }
-    if(el[0]===' '){
-      el = el.charAt(0) + el.charAt(1).toUpperCase() + el.slice(2)
-      return el
-    }
-  }).join`.`
-}
-console.log(fix("hi. my name is inigo montoya. you killed my father. prepare to die."))
+    return el
+  }).join``
+ }
+ console.log(encode('abc-#@5'))
