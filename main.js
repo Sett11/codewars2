daeb7b6a9aa15423a466bcb096ebace557d3dc7e
Jhonny <gonzodzen@mail.ru>
2023-01-16 14:09:56 +0300

    Use map() to double the values in an array

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-function isItANum(str) {
-  const res = str.replace(/[^0-9]/g,'')
-  return res.length === 11 && res[0] === '0' ? res : "Not a phone number"
+function double(array) {
+  return array.map(el=>el*2)
 }
-  console.log(isItANum("S:)0207ERGQREG88349F82!efRF)"))
