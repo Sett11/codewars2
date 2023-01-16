365184c3d1d9aad64856664a92bd62aa537b70e0
Jhonny <gonzodzen@mail.ru>
2023-01-16 23:48:21 +0300

    All, None & Any

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-function encode(str) {
-  let arr = 'abcdefghijklmnopqrstuvwxyz'
-  let arr2 = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase()
-  return str.split``.map(el=>{
-    for(let i = 0; i < arr.length; i++){
-      if(el===arr[i]){
-        el = i+1
-      }
-      else if(el===arr2[i]){
-        el = i+1
-      }
-      continue
+Array.prototype.all = function (p) {
+  for(let i = 0; i < this.length; i++){
+    if(!p(this[i])){
+      return false
     }
-    return el
-  }).join``
- }
- console.log(encode('abc-#@5'))
+  }
+  return true
+};
+
+Array.prototype.none = function (p) {
+  for(let i = 0; i < this.length; i++){
+    if(p(this[i])){
+      return false
+    }
+  }
+  return true
+};
+
+Array.prototype.any = function (p) {
+  for(let i = 0; i < this.length; i++){
+    if(p(this[i])){
+      return true
+    }
+  }
+  return fakse
+};
