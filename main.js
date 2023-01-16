552b521aa6d374b22f8d2ebf23b0f354f01f6ff0
Jhonny <gonzodzen@mail.ru>
2023-01-16 19:13:20 +0300

    Coding Meetup #16 - Higher-Order Functions Series - Ask for missing details

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function calculateString(st){
-  return (eval(st.replace(/[^0-9.+-/*-]/g,''))).toFixed(0)
+function askForMissingDetails(list) {
+  return list.map(el=>{
+    for(let i in el){
+      if(el[i]===null){
+        return el
+      }
+    }
+  }).map(el=>{
+    for(let i in el){
+      if(el[i]===null){
+        el.question = `Hi, could you please provide your ${i}.`
+      }
+    }
+    return el
+  }).filter(el=>el!==undefined)
 }
-console.log(calculateString("a1a2b3c.c0c/a1a0b.cc00c"))
+
+console.log(askForMissingDetails([
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+]))
