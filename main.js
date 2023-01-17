351087ec8e0447e0babf47813cebe86325640fb8
Jhonny <gonzodzen@mail.ru>
2023-01-17 11:04:10 +0300

    Coding Meetup #17 - Higher-Order Functions Series - Sort by programming language

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
-Array.prototype.all = function (p) {
-  for(let i = 0; i < this.length; i++){
-    if(!p(this[i])){
-      return false
-    }
-  }
-  return true
-};
-
-Array.prototype.none = function (p) {
-  for(let i = 0; i < this.length; i++){
-    if(p(this[i])){
-      return false
-    }
-  }
-  return true
-};
-
-Array.prototype.any = function (p) {
-  for(let i = 0; i < this.length; i++){
-    if(p(this[i])){
-      return true
-    }
-  }
-  return fakse
-};
+function sortByLanguage(list) {
+  return list.sort((a,b)=> a.language.localeCompare(b.language)||a.firstName.localeCompare(b.firstName))
+}
+console.log(sortByLanguage([  
+  { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' },
+  { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
+  { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' }
+]))
