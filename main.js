231a69f52fed676f1ab49add1584f47707eac23e
Jhonny <gonzodzen@mail.ru>
2023-01-17 11:07:17 +0300

    letters only, please!

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-function sortByLanguage(list) {
-  return list.sort((a,b)=> a.language.localeCompare(b.language)||a.firstName.localeCompare(b.firstName))
+function removeChars(s) {
+  return s.replace(/[^a-zA-Z\s]/g,'')
 }
-console.log(sortByLanguage([  
-  { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' },
-  { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
-  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
-  { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' }
-]))
