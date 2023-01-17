0ca6f0f0daf1e82f56348572d85345cc5af58884
Jhonny <gonzodzen@mail.ru>
2023-01-17 16:33:07 +0300

    Integer depth

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function removeChars(s) {
-  return s.replace(/[^a-zA-Z\s]/g,'')
+function computeDepth (x){
+  const arr = []
+  let sum = 0
+  for(let i = 1; i < 50; i++){
+    sum = String(x*i).split``
+    for(let j = 0; j < sum.length; j++){
+      if(arr.indexOf(sum[j])===-1){
+        arr.push(sum[j])
+      }
+    }
+    if(arr.length===10){
+      return i
+    }
+  }
+  return arr
 }
+console.log(computeDepth(1))
