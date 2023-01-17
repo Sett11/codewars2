548617f80bce0c2ef7e591c0912d16f89bd2af6a
Jhonny <gonzodzen@mail.ru>
2023-01-17 18:55:48 +0300

    The maximum and minimum difference -- Simple version

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
-function solve(z) {
-  const notPrimes = (n) => {
-    if (n === 1) {
-      return true
+function maxAndMin(arr1,arr2){
+  const arr = []
+  arr1.map(el=>{
+    for(let i = 0; i < arr2.length; i++){
+      arr.push(Math.abs(el - arr2[i]))
     }
-    if (n === 2 || n === 3) {
-      return false
-    }
-    const num = String(n).split``.map(Number);
-    if (num.includes(2) ||
-      num.includes(3) ||
-      num.includes(5) ||
-      num.includes(7)) {
-      return false;
-    }
-    for (let i = 2; i < n; i++) {
-      if (n % i === 0) {
-        return true;
-      }
-    }
-    return false;
-  }
-  const arr = [];
-    let counter = 1;
-    while (arr.length <= z) {
-      if (notPrimes(counter)) {
-        arr.push(counter);
-      }
-      counter++;
-    }
-    return arr[z]
-  }
-  console.log(solve(10))
+  })
+  return [Math.max(...arr), Math.min(...arr)]
+}
+console.log(maxAndMin([3,10,5],[20,7,15,8]))
