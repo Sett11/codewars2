e513f578bda3033842453f3e7077ba88da0cadf1
Jhonny <gonzodzen@mail.ru>
2023-01-18 23:04:41 +0300

    Next Palindromic Number.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function bears(x, s) {
-  const res = !s.match(/B8|8B/g) ? "" : s.match(/B8|8B/g).join``;
-  return [res, res.length / 2 >= x];
+function nextPal(val) {
+  let v = true,
+    res = "";
+  while (v) {
+    val++;
+    res = String(val).split``;
+    if (res.join`` === res.reverse().join``) {
+      v = false;
+    }
+  }
+  return Number(res.join``);
 }
-console.log(bears(4, "8B8B8BB"));
+console.log(nextPal(2541));
