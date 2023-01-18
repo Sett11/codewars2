997aacdb3330c0901561c81211d728535f4d2553
Jhonny <gonzodzen@mail.ru>
2023-01-18 19:43:56 +0300

    Sushi-go-round (Beginner's)

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-function bestMatch(ALAHLYGoals, zamalekGoals) {
-    const arr = []
-    for(let i = 0; i < ALAHLYGoals.length; i++){
-        arr.push([Math.abs(ALAHLYGoals[i] - zamalekGoals[i]), zamalekGoals[i], i])
-    }
-    const min = Math.min(...arr)
-    const ind = arr.indexOf(min)
-    return arr.sort((a,b)=>a[0] - b[0] || b[1] - a[1])[0][2]
+function totalBill(str) {
+    return (str.replace(/\s/g, '').length * 2) - Math.floor(str.replace(/\s/g, '').length / 5) * 2
 }
-  console.log(bestMatch([13,7,19,5,14,7,8,9,7,2],[9,2,9,2,10,6,6,7,2,1]))
+  console.log(totalBill('rrrrrrrrrrrrrrrrrr   rr r'))
