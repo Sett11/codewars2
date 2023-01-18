77717b7df22220d725b7f77f69b26cd1b709f758
Jhonny <gonzodzen@mail.ru>
2023-01-18 17:24:14 +0300

    Average Array

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-function countAdjacentPairs(s) {
-  s = s.split` `.map(el=>el.toLowerCase())
-  let counter = 0
-  let cash = ''
-  for(let i = 0; i < s.length; i++){
-    if(s[i] === s[i+1]&&s[i]!==cash){
-      counter++
-      cash = s[i]
+function avgArray(ar) {
+    let arr = [], res = []
+    ar.map(el=>el.map((ul,i)=>[ul, i])).map(el=>el.map(ul=>arr.push(ul)))
+    arr.sort((a,b)=>a[1]-b[1])
+    for(let i = 0; i < arr.length; i+=ar.length){
+        res.push(arr.slice(i, i + ar.length))
     }
-    if(s[i]!==cash){
-      cash = ''
-    }
-  }
-  return counter
+    return res.map(el=>el.map(ul=>ul[0]).reduce((acc,curr)=>acc+curr,0)/ar.length)
 }
-console.log(countAdjacentPairs("dog dog DOG dog dog dog" ))
+console.log(avgArray([[1.2, 8.521, 0.4, 3.14, 1.9], [2, 4.5, 3.75, 0.987, 1.0]]))
