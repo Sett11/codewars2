c4ed5eaa74b0dba3554556a2019252c09a964ded
Jhonny <gonzodzen@mail.ru>
2023-01-18 23:37:05 +0300

    Milk and Cookies for Santa

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-const AmIAfraid = function (d, n){
-    return d==='Monday'&&n===12 ? true :
-    d==='Tuesday'&&n>95 ? true :
-    d==='Wednesday'&&n===34 ? true :
-    d==='Thursday'&&n===0 ? true :
-    d==='Friday'&&n%2===0 ?true :
-    d==='Saturday'&&n===56 ? true :
-    d==='Sunday'&&n===666||d==='Sunday'&&n===-666 ? true : false
-  }
-  console.log(AmIAfraid("Monday", 13))
+function timeForMilkAndCookies(d){
+    return (d).toString().slice(4,10) === 'Dec 24'
+}
+  console.log(timeForMilkAndCookies(new Date(2013, 11, 24)))
