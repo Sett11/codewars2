c160acbb04d8ea889367b62065ffaa6995b15927
Jhonny <gonzodzen@mail.ru>
2023-01-19 18:03:58 +0300

    Mispelled word

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-class Animal{
-    constructor(name, type){
-        this.name = name
-        this.type = type
-        this.toString = function(){
-            return `${this.name} is a ${this.type}`
+var mispelled = function(word1, word2){
+    if(word1===word2){
+        return true
+    }
+    let counter = 0
+    for(let i = 0; i < word1.length + 2; i++){
+        if(word1[i]!==word2[i]){
+            counter+=1
         }
     }
+    return word1.replace(word2, '').length===1||word2.replace(word1,'').length===1||counter===1 ? true : false
 }
-let dog = new Animal('Max', 'dog')
-console.log(dog.name)
-dog.name = 'G'
-console.log(dog.name)
+console.log(mispelled('1versed', 'versed'))
