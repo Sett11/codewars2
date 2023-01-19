5732f51770791d751cd1a13eb29021b6d15336eb
Jhonny <gonzodzen@mail.ru>
2023-01-19 18:38:36 +0300

    Colour Association

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-var mispelled = function(word1, word2){
-    if(word1===word2){
-        return true
-    }
-    let counter = 0
-    for(let i = 0; i < word1.length + 2; i++){
-        if(word1[i]!==word2[i]){
-            counter+=1
-        }
-    }
-    return word1.replace(word2, '').length===1||word2.replace(word1,'').length===1||counter===1 ? true : false
+function colourAssociation(array){
+    return array.map(el=>{
+        let obj = {}
+        obj[el[0]] = el[1]
+        return obj
+    })
 }
-console.log(mispelled('1versed', 'versed'))
+  console.log(colourAssociation([["white", "goodness"], ["blue", "tranquility"]]))
