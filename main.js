d92abc8e2ba35585056376410c146c7f34289aac
Jhonny <gonzodzen@mail.ru>
2023-01-19 19:14:38 +0300

    Sort My Animals

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function colourAssociation(array){
-    return array.map(el=>{
-        let obj = {}
-        obj[el[0]] = el[1]
-        return obj
-    })
-}
-  console.log(colourAssociation([["white", "goodness"], ["blue", "tranquility"]]))
+function sortAnimal(animal) {
+    if(animal===null){
+          return null
+        }
+        if(animal.length===0){
+          return []
+        }
+        return animal.sort((a,b)=>a.numberOfLegs - b.numberOfLegs|| a.name.localeCompare(b.name))
+    }
+
+  console.log(sortAnimal([
+    { name: "Cat", numberOfLegs: 4 }, 
+    { name: "Snake", numberOfLegs: 0 }, 
+    { name: "Dog", numberOfLegs: 4 },
+    { name: "Pig", numberOfLegs: 4 },
+    { name: "Human", numberOfLegs: 2 },
+    { name: "Bird", numberOfLegs: 2 }]))
