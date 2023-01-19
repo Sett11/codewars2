78c6643e45c5bd05a7ef678a0190f1d62fbb3a0d
Jhonny <gonzodzen@mail.ru>
2023-01-19 16:58:39 +0300

    Invalid Input - Error Handling #1

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-function getDecimal(n) {
-    return Math.abs(n - Math.trunc(n))
-  }
-console.log(getDecimal(4.5));
+function getCount(words) {
+  const arr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const obj = { vowels: 0, consonants: 0 };
+  if (!words||typeof words !=='string') {
+    return obj
+}
+  words.split``.map((el) => {
+    if (arr.includes(el) && el.match(/a|e|u|o|i/gi)!==null) {
+      obj.vowels += 1;
+    }
+    if (arr.includes(el)&& el.match(/a|e|u|o|i/gi)===null) {
+      obj.consonants += 1;
+    }
+  });
+  return obj;
+}
+console.log(getCount("   "));
