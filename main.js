d32d5a85a40ca8e17e5a3375589f728fe2edeabc
Jhonny <gonzodzen@mail.ru>
2023-01-19 23:31:52 +0300

    Pizza Payments

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-
-const Converter = {
-    toAscii: function (hex) {
-        const arr = []
-        for(let i = 0; i < hex.length; i+=2){
-            arr.push(hex.slice(i, i+2))
-        }
-        return arr.map(el=> String.fromCharCode(parseInt(el,16))).join``
-    },
-    toHex: function (ascii) {
-      return ascii.split``.map((el,i)=> (el.charCodeAt()).toString(16)).join``
+function michaelPays(c) {
+    if(c<5){
+        return Number((c).toFixed(2))
     }
-  }
+    let K = c / 3
+    if(K>10){
+        K = 10
+    }
+    return Number((c - K).toFixed(2))
+}
 
-  
-  console.log(Converter.toAscii("4c6f6f6b206d6f6d2c206e6f2068616e6473"))
+console.log(michaelPays(28.789))
