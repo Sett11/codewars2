09f4d9261f6be9405e484fe43e1921e4a846a1f8
Jhonny <gonzodzen@mail.ru>
2023-01-19 08:33:03 +0300

    String subpattern recognition I

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function timeForMilkAndCookies(d){
-    return (d).toString().slice(4,10) === 'Dec 24'
+function hasSubpattern(s){
+    if(s.length===2&&s[0]===s[1])return true
+    const arr = []
+    for(let i = 1; i <= s.length/2; i++){
+        arr.push(s.slice(0,i))
+    }
+    return arr
+     .map(el=>[el,s.length/el.length])
+     .filter(el=>el[0].repeat(el[1])===s).length !== 0
 }
-  console.log(timeForMilkAndCookies(new Date(2013, 11, 24)))
+console.log(hasSubpattern('abbaabbaabba'))
