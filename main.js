e3f2902d7dba6c88f138cb0e55c64006cae7b01b
Jhonny <gonzodzen@mail.ru>
2023-01-19 16:21:38 +0300

    Get decimal part of the given number

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-function arraySum(ar) {
-  const arr = [];
-  ar.flat(100).map((el) => (typeof el === "number" ? arr.push(el) : null));
-  return arr.reduce((acc,curr)=>acc+curr,0)
-}
-
-console.log(arraySum([1, 2, { t: 9 }, 4, 5, [1, 2, [3], 9, { a: 8 }]]));
+function getDecimal(n) {
+    return Math.abs(n - Math.trunc(n))
+  }
+console.log(getDecimal(4.5));
