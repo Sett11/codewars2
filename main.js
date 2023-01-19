81e726b2b4e0a95b5bff4c2fed76ed43426ddef3
Jhonny <gonzodzen@mail.ru>
2023-01-19 17:47:21 +0300

    JavaScript class-like objects

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-function getCount(words) {
-  const arr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const obj = { vowels: 0, consonants: 0 };
-  if (!words||typeof words !=='string') {
-    return obj
-}
-  words.split``.map((el) => {
-    if (arr.includes(el) && el.match(/a|e|u|o|i/gi)!==null) {
-      obj.vowels += 1;
+class Animal{
+    constructor(name, type){
+        this.name = name
+        this.type = type
+        this.toString = function(){
+            return `${this.name} is a ${this.type}`
+        }
     }
-    if (arr.includes(el)&& el.match(/a|e|u|o|i/gi)===null) {
-      obj.consonants += 1;
-    }
-  });
-  return obj;
 }
-console.log(getCount("   "));
+let dog = new Animal('Max', 'dog')
+console.log(dog.name)
+dog.name = 'G'
+console.log(dog.name)
