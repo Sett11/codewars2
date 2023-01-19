86444991fd8f07bf358bc8922b6c8ad23320029e
Jhonny <gonzodzen@mail.ru>
2023-01-19 23:10:23 +0300

    ASCII hex converter

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-function getNiceNames(people){
-    return people.map(el=>el.wasNice ? el.name : null).filter(el=>el!==null)
-  }
-  
-  function getNaughtyNames(people){
-    return people.map(el=>!el.wasNice ? el.name : null).filter(el=>el!==null)
+
+const Converter = {
+    toAscii: function (hex) {
+        const arr = []
+        for(let i = 0; i < hex.length; i+=2){
+            arr.push(hex.slice(i, i+2))
+        }
+        return arr.map(el=> String.fromCharCode(parseInt(el,16))).join``
+    },
+    toHex: function (ascii) {
+      return ascii.split``.map((el,i)=> (el.charCodeAt()).toString(16)).join``
+    }
   }
 
-  console.log(getNaughtyNames([
-    { name: 'Warrior reading this kata', wasNice: true },
-    { name: 'xDranik', wasNice: false },
-    { name: 'Santa', wasNice: true }
-]))
+  
+  console.log(Converter.toAscii("4c6f6f6b206d6f6d2c206e6f2068616e6473"))
