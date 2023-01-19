eaddf4f5ee7d00f3a6ed0bacbefd5704d198c250
Jhonny <gonzodzen@mail.ru>
2023-01-19 20:34:19 +0300

    ASCII letters from Number

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function solution(fullText, searchText){
-    if(fullText.length===0){
-      return 0
+function convert(number){
+    const arr = []
+    for(let i = 0; i < number.length; i+= 2){
+        arr.push(number.slice(i, i+2))
     }
-      const r = new RegExp(`${searchText}`, 'g')
-      return fullText.match(r)? fullText.match(r).length : 0
-  }
-console.log(solution('abbc','bb'))
+    return arr.map(el=>String.fromCharCode(el)).join``
+}
+  console.log(convert("656667"))
