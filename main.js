b6b96a2cc5496484c30386ecbc30ac2fd4455225
Jhonny <gonzodzen@mail.ru>
2023-01-20 19:39:42 +0300

    Ч4113нг3

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-function isSantaClausable(obj) {
-    return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(el=>{
-       return typeof obj[el] ==='function'
-    })
-  }
-  console.log(isSantaClausable({
-    sayHoHoHo: function() { console.log('Ho Ho Ho!') },
-    distributeGifts: function() { console.log('Gifts for all!'); },
-    goDownTheChimney: function() { console.log('*whoosh*'); }
-}))
+function nerdify(txt){
+    return txt.replace(/a/gi,'4').replace(/e/gi,'3').replace(/l/g,'1')
+}
+console.log(nerdify("Fund4m3nt41s"))
