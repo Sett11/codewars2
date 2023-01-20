475f17455304106ccedfecbb50be68662146d8c9
Jhonny <gonzodzen@mail.ru>
2023-01-20 16:22:21 +0300

    Alan Partridge III - London

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-function zeroPlentiful(ar){
-    const res = ar.map(el=>{
-        let str = String(el).split``
-        if(str[str.length-1]==='0'&&el!==0){
-            el = '&'
-        }
-        return el
-    }).join``.replace(/0+/g,' $& ').replace(/[^0\s]/g,'').split` `.filter(el=>el!=='')
-    return res.every(el=>el.length>=4) ? res.length : 0
+function alan(x){
+    return [...new Set(x.filter(el=>{
+        return el==='Rejection'||el==='Disappointment'||el==='Backstabbing Central'||el==='Shattered Dreams Parkway'?true:false
+    }))].length===4 ? 'Smell my cheese you mother!' : 'No, seriously, run. You will miss it.'
 }
-  console.log(zeroPlentiful([0, 0, 0, 0, 0, 0]))
+console.log(alan(["Backstabbing Central","Disappointment","the Moon","Rejection","the Moon","Spiksworth","Norwich","Rejection"]))
