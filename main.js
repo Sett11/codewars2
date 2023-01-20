11d141dd24a68b07c3139b64e2a9416abb7b7dcf
Jhonny <gonzodzen@mail.ru>
2023-01-20 12:18:42 +0300

    String Reordering

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-class Cuboid {
-    constructor(length, width, height){
-        this.length = length
-        this.width = width
-        this.height = height
-    }
-   get surfaceArea(){
-        return 2 *(this.length*this.height + this.height*this.width + this.length*this.width)
-    }
-   get volume(){
-        return this.length * this.width * this.height
-    }
-  }
-  class Cube extends Cuboid {
-    constructor(length){
-        super(length,length,length)
-    }
-  }
-
-const r = new Cuboid(1,2,3)
-r.length = 4
-
-const c = new Cube(1)
-
-console.log(r.surfaceArea)
+function sentence(l) {
+    return l.map(el=>Object.entries(el)[0]).map(el=>[Number(el[0]), el[1]]).sort((a,b)=>a[0]-b[0]).map(el=>el[1]).join` `
+}
+  console.log(sentence([
+    {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
+    {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
+   ]))
