6f99dd41ac47757d3d843574dafe05fe2ad72ccd
Jhonny <gonzodzen@mail.ru>
2023-01-20 08:37:43 +0300

    Converting integer to currency format

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function giveChange(amount) {
-  const arr = [100,50,20,10,5,1]
-  return arr.map((el)=>[el,amount]=[Math.floor(amount/el), amount%el]).map(el=>el[0]).reverse()
-}
-console.log(giveChange(367))
+function toCurrency(p){
+    p = String(p).split``
+    if(p.length===7){
+        return `${p[0]},${p[1]}${p[2]}${p[3]},${p[4]}${p[5]}${p[6]}`
+    }
+    if(p.length===5){
+      return `${p[0]}${p[1]},${p[2]}${p[3]}${p[4]}`
+    }
+    if(p.length===4){
+      return `${p[0]},${p[1]}${p[2]}${p[3]}`
+    }
+    const arr = []
+    for(let i = 0; i < p.length; i+=3){
+      arr.push(p.slice(i,i+3))
+    }
+    return arr.map(el=>el.join``).join`,`
+  }
+  console.log(toCurrency(12))
