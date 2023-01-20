bc799a828f978aadf40228f07c372fc5690b1c73
Jhonny <gonzodzen@mail.ru>
2023-01-20 20:10:04 +0300

    Numerical Palindrome #1.5

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function nerdify(txt){
-    return txt.replace(/a/gi,'4').replace(/e/gi,'3').replace(/l/g,'1')
+function palindrome(num,s) {
+    if(num<0||s<0||!Number.isInteger(num)||!Number.isInteger(s)||typeof num!=='number'||typeof s !=='number'){
+        return "Not valid"
+    }
+    const arr = []
+    if(String(num).split``.reverse().join``===String(num)&&String(num).split``.length > 1){
+        arr.push(num)
+    }
+    for(let i = num; ; i++){
+        if(String(i).split``.reverse().join``===String(i)&&String(i).split``.length > 1&&arr.indexOf(i)===-1){
+            arr.push(i)
+        }
+        if(arr.length===s){
+            break
+        }
+    }
+    return arr
 }
-console.log(nerdify("Fund4m3nt41s"))
+  console.log(palindrome(493,0))
