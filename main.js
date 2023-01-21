ba27726e185ebbb700287908587cbd546cb3e4d1
Jhonny <gonzodzen@mail.ru>
2023-01-21 15:16:34 +0300

    Moves in squared strings (IV)

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-
-function scoreTest(str, right, omit, wrong){
-  let arr0 = [], arr1 = [], arr2 = []
-  for(let i = 0; i < str.length; i++){
-    if(str[i]===0){
-      arr0.push(str[i])
+function diag2Sym(s) {
+  s = s.split`\n`.map(el=>el.split``.reverse().join``)
+  const arr = []
+  for(let i = 0; i < s.length; i++){
+    let str = ''
+    for(let j = 0; j < s.length; j++){
+      str+=s[j][i]
     }
-    if(str[i]===1){
-      arr1.push(str[i])
-    }
-    if(str[i]===2){
-      arr2.push(str[i])
-    }
+    arr.push(str)
   }
-  arr0 = arr0.map(el=>el=right).reduce((acc,curr)=>acc+curr,0)
-  arr1 = arr1.map(el=>el=omit).reduce((acc,curr)=>acc+curr,0)
-  arr2 = arr2.map(el=>el = wrong).reduce((acc,curr)=>acc+curr,0)
-  return arr0 + arr1 - arr2
+  return arr.map(el=>el.split``.reverse().join``).join`\n`
 }
-console.log(scoreTest([0, 1, 0, 0, 2, 1, 0, 2, 2, 1], 3, -1, 2))
+function rot90Counter(s) {
+  return diag2Sym(s).split`\n`.map(el=>el.split``.reverse().join``).join`\n`
+}
+function selfieDiag2Counterclock(s) {
+  const newS = s.split`\n`, s1 = diag2Sym(s).split`\n`, s2 = rot90Counter(s).split`\n`, arr = []
+  for(let i = 0; i < newS.length; i++){
+    let str = ''
+      str = newS[i] + '|' + s1[i] + '|' + s2[i]
+      arr.push(str)
+  }
+  return arr.join`\n`
+}
+function oper(fct, s) {
+  return fct(s)
+}
+
+console.log(oper(selfieDiag2Counterclock ,"abcd\nefgh\nijkl\nmnop"))
