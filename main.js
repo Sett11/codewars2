3135f36503575a6eebfd40cf2e238d59d64ddd72
Jhonny <gonzodzen@mail.ru>
2023-01-21 22:07:17 +0300

    isEven? - Bitwise Series

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-function solve(x, y) {
-  const arr = []
-  for(let i = x; i < y; i++){
-    if((i).toString().match(/2|3|4|5|7/g)){
-      continue
-    }
-    arr.push(i+[])
-  }
-  return arr.filter(el=>el===el.replace(/6|9/g, ul=>ul==='6'?'9':'6').split``.reverse().join``).length
-}
- console.log(solve(10, 100))
+const isEven = n => n&1?false:true 
