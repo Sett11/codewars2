2506e9eec8450750c50590dafd3c52889ddc48f4
Jhonny <gonzodzen@mail.ru>
2023-01-21 14:46:20 +0300

    Scoring Tests

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-function makeParts(ar, chunkSize) {
-  const arr = []
-  for(let i = 0; i < ar.length; i+=chunkSize){
-    arr.push(ar.slice(i, i + chunkSize))
+
+function scoreTest(str, right, omit, wrong){
+  let arr0 = [], arr1 = [], arr2 = []
+  for(let i = 0; i < str.length; i++){
+    if(str[i]===0){
+      arr0.push(str[i])
+    }
+    if(str[i]===1){
+      arr1.push(str[i])
+    }
+    if(str[i]===2){
+      arr2.push(str[i])
+    }
   }
-  return arr
+  arr0 = arr0.map(el=>el=right).reduce((acc,curr)=>acc+curr,0)
+  arr1 = arr1.map(el=>el=omit).reduce((acc,curr)=>acc+curr,0)
+  arr2 = arr2.map(el=>el = wrong).reduce((acc,curr)=>acc+curr,0)
+  return arr0 + arr1 - arr2
 }
-console.log(makeParts([1,2,3,4,5], 10))
+console.log(scoreTest([0, 1, 0, 0, 2, 1, 0, 2, 2, 1], 3, -1, 2))
