d9ef4e5240f9c9cda03852ab5c0cf6a6863a7f9a
Jhonny <gonzodzen@mail.ru>
2023-01-21 14:28:52 +0300

    Cut array into smaller parts

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-function bowlingPins(arr){
-  let str = '7 8 9 10\n 4 5 6 \n  2 3  \n   1   '
-   arr = arr.map(el=>String(el))
-  str = str.split``
-  for(let i = 0; i < str.length; i++){
-    if(str[i]==='1'&&str[i+1]==='0'){
-      str[i] = '10'
-      str[i+1] = '&'
-    }
+function makeParts(ar, chunkSize) {
+  const arr = []
+  for(let i = 0; i < ar.length; i+=chunkSize){
+    arr.push(ar.slice(i, i + chunkSize))
   }
-  str = str.filter(el=>el!=='&')
-  return str.map(el=>{
-    if(arr.indexOf(el)!==-1){
-      el = ' '
-      return el
-    }
-    if(arr.indexOf(el)===-1&&el!=='\n'&&el!==' '){
-      el = 'I'
-      return el
-    }
-    return el
-  }).join``
+  return arr
 }
-console.log(bowlingPins([3,5,9]))
+console.log(makeParts([1,2,3,4,5], 10))
