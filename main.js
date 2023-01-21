42730d3756d05edbe10afa23cca405c3ea44bba5
Jhonny <gonzodzen@mail.ru>
2023-01-21 15:33:00 +0300

    Pairs of integers from 0 to n

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
-function diag2Sym(s) {
-  s = s.split`\n`.map(el=>el.split``.reverse().join``)
+function generatePairs(n) {
   const arr = []
-  for(let i = 0; i < s.length; i++){
-    let str = ''
-    for(let j = 0; j < s.length; j++){
-      str+=s[j][i]
+  for(let i = 0; i <= n; i++){
+    for(let j = 0; j <= n; j++){
+      j>=i?arr.push([i,j]):null
     }
-    arr.push(str)
   }
-  return arr.map(el=>el.split``.reverse().join``).join`\n`
+  return arr
 }
-function rot90Counter(s) {
-  return diag2Sym(s).split`\n`.map(el=>el.split``.reverse().join``).join`\n`
-}
-function selfieDiag2Counterclock(s) {
-  const newS = s.split`\n`, s1 = diag2Sym(s).split`\n`, s2 = rot90Counter(s).split`\n`, arr = []
-  for(let i = 0; i < newS.length; i++){
-    let str = ''
-      str = newS[i] + '|' + s1[i] + '|' + s2[i]
-      arr.push(str)
-  }
-  return arr.join`\n`
-}
-function oper(fct, s) {
-  return fct(s)
-}
-
-console.log(oper(selfieDiag2Counterclock ,"abcd\nefgh\nijkl\nmnop"))
+console.log(generatePairs(2))
