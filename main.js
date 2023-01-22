8f469a66df1bd7ee17873fe9acc777d1dfcb889e
Jhonny <gonzodzen@mail.ru>
2023-01-22 20:57:44 +0300

    Required Data I

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-function squaresToOdd(sqr1, sqr2){
-  const counter = sqr1-sqr2, sum = sqr1**2 - sqr2**2, arr = [], res = []
-  if(counter===1){
-    return `${sqr1}^2 - ${sqr2}^2 = ${sum} = ${sum}`
-  }
-  for(let i = 0; i <= sum; i++){
-      if(i%2!==0){
-        arr.push(i)
-      }
-  }
-  for(let i = 0, j = counter; i < arr.length, j < arr.length; i+=1, j+=1){
-    if(arr.slice(i, j).reduce((acc,curr)=>acc+curr,0)===sum){
-      res.push(arr.slice(i,j))
+function countSel(lst) {
+  let len = lst.length, counter = [...new Set(lst)].length, unic = 0, tmp =
+  lst.map((el,i,arr)=>{
+    if(arr.indexOf(el)===arr.lastIndexOf(el)){
+      unic+=1
+    }
+  })
+  const res = Object.entries(lst.reduce((acc,curr)=>{
+    acc[curr] = (acc[curr]||0)+1
+    return acc
+  }, {})).map(el=>{
+    el[0] = Number(el[0])
+    return el
+  }).sort((a,b)=>b[1]-a[1])
+  for(let i = 1; i < res.length; i++){
+     tmp = res[0][1]
+    if(res[i][1]!==tmp){
+      res[i] = '&'
+    }else{
+      continue
     }
   }
-  return `${sqr1}^2 - ${sqr2}^2 = ${String(res).replace(/,/g,' + ')} = ${sum}`
+  const finRes = res.filter(el=>el!=='&').map(el=>el[0]).sort((a,b) => a - b)
+  return [len, counter, unic, [finRes, tmp]]
 }
-console.log(squaresToOdd(100,99))
+console.log(countSel([5, -1, 1, -1, -2, 5, 0, -2, -5, 3]))
