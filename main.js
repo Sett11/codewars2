f57a9d0055da97e04e77774932ec17750cb8e8ac
Jhonny <gonzodzen@mail.ru>
2023-01-22 18:57:37 +0300

    The Sum and The Rest of Certain Pairs of Numbers have to be Perfect Squares

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-const midpointSum=(n)=>{
-  for(let i = 1; i < n.length-1; i++){
-    let tmp = n[i]
-    let first = n.slice(0,i).reduce((acc,curr)=>acc+curr,0)
-    let last = n.slice(i+1).reduce((acc,curr)=>acc+curr,0)
-    if(first===last){
-      return i
-      
+function closestPairTonum(n) {
+  for(let i = n-1; i > 0; i--){
+    let tmp = i
+    for(let j = n-2; j > 0; j--){
+      let tmp2 = j
+      if(Number.isInteger(Math.sqrt(tmp + tmp2))&&Number.isInteger(Math.sqrt(tmp-tmp2))&&tmp>tmp2){
+        return [tmp, tmp2]
+      }
     }
   }
-  return undefined
 }
-console.log(midpointSum([0,0,1]))
+console.log(closestPairTonum(50))
