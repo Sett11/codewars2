fd172b40bae93998dd82db9f9c8db114f4c0d47c
Jhonny <gonzodzen@mail.ru>
2023-01-22 16:20:42 +0300

    Buddy Pairs

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-const isEven = n => n&1?false:true 
+function buddy(start, limit) {
+  const arr = []
+  while (start < limit) {
+    let sum = 1, sum2 = 1, check = 0
+    for(let i = 2; i < Math.sqrt(start); i++){
+      if(start%i===0){
+        sum+=i
+        sum+=start/i
+      }
+    }
+    if(sum>start){
+      check = sum - 1
+     for(let j = 2; j < Math.sqrt(check); j++){
+      if(check%j===0){
+        sum2+=j
+        sum2+=check/j
+      }
+     }
+    if (start+1 === sum2 && check+1 === sum) {
+      if (start > check) {
+        return "Nothing";
+      }
+      return [start, check];
+    } else {
+      start++;
+    }
+    }else{
+      start++
+    }
+  }
+  return "Nothing";
+}
+console.log(buddy(381, 4318));
