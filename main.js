31db27bd8e66b931f8a24a83a565637e32886c66
Jhonny <gonzodzen@mail.ru>
2023-01-22 19:04:29 +0300

    The Sum and The Rest of Certain Pairs of Numbers have to be Perfect Squares (more Challenging)

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function closestPairTonum(n) {
+function nClosestPairsTonum(n, k) {
+  const arr = []
   for(let i = n-1; i > 0; i--){
     let tmp = i
     for(let j = n-2; j > 0; j--){
       let tmp2 = j
       if(Number.isInteger(Math.sqrt(tmp + tmp2))&&Number.isInteger(Math.sqrt(tmp-tmp2))&&tmp>tmp2){
-        return [tmp, tmp2]
+        arr.push([tmp, tmp2])
+      }
+      if(arr.length===k){
+        break
       }
     }
   }
+  return arr
 }
-console.log(closestPairTonum(50))
+console.log(nClosestPairsTonum(10000, 8))
