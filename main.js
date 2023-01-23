fd0fa6ccb395ed225931ca9174f2d369690768a1
Jhonny <gonzodzen@mail.ru>
2023-01-23 14:27:26 +0300

    1/n- Cycle

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function digitalRoot(n) {
-  n = (n+'').split``.map(Number).reduce((acc,curr)=>acc+curr,0)
-  return (n+'').split``.length === 1 ? Number((n+'').split``) : digitalRoot(n)
+function cycle(n) {
+  if(n%2===0||n%5===0)return -1
+  let i = 0, val = 1
+  while(++i){
+    val = val * 10%n
+    if(val===1)return i
+  }
 }
-console.log(digitalRoot(456))
+console.log(cycle(7))
