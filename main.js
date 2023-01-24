8fe72488a15a165d5fd68a566e5fded60cfbe8e7
Jhonny <gonzodzen@mail.ru>
2023-01-24 22:10:06 +0300

    Minimum to multiple

--- main.js
+++ main.js
@@ -1,61 +1,12 @@
-function lastSurvivors(strs, nums) {
-  if (strs.length === 0 || nums.length === 0) return "";
-  if(strs.length===1&&nums.length===1&&nums[0]===0)return strs[0]
-  if (strs.length === 1) {
-    strs = [...strs[0].split``];
-    let arr = [];
-    for (let i = 0; i <= strs.length; i++) {
-      let str = [];
-      for (let j = 0; j < strs.length; j++) {
-        if (strs.length !== 1) {
-          if (strs[j][i] !== undefined && strs[j][i] !== "") {
-            str.push(strs[j][i]);
-          }
-        }
-      }
-      arr.push(str.reverse());
+function minimum(a, x) {
+  if(a===x)return 0
+  let counter = 1
+  while(a%x!==0){
+    if((a+counter)%x===0||(a-counter)%x===0){
+      break
     }
-    arr = arr.filter((el) => el.length !== 0);
-    if (arr.length === 1) {
-      arr = [...arr[0]].reverse();
-    }
-    for (let i = 0; i < nums.length; i++) {
-      arr[i] = [arr[i], nums[i]];
-    }
-    return arr
-      .map((el) => {
-        el = el[0].slice(el[1]);
-        return el;
-      })
-      .filter((el) => el.length !== 0).join``.replace(/,/g, "").split``.sort()
-      .join``.trim()
+    counter++
   }
-  let arr = [];
-  for (let i = 0; i < strs[0].length; i++) {
-    let str = [];
-    for (let j = 0; j < strs.length; j++) {
-      if (i === strs.length && i < strs[0].length) {
-      }
-      if (strs[j][i] !== undefined && strs[j][i] !== " ") {
-        str.push(strs[j][i]);
-      }
-    }
-    arr.push(str.reverse());
-  }
-  for (let i = 0; i < nums.length; i++) {
-    arr[i] = [arr[i], nums[i]];
-  }
-  return arr
-    .map((el) => {
-      el = el[0].slice(el[1]);
-      return el;
-    })
-    .filter((el) => el.length !== 0).join``.replace(/,/g, "").split``.sort()
-    .join``.trim()
+  return counter
 }
-
-console.log(
-  lastSurvivors(
-    ["xgwcd   er "], [0,7,3,4,1,4,2,0,1,3,0]
-  )
-)
+console.log(minimum(10,6))
