e22f0e0363eba97433b6146aa3f2d9013fbdc3d6
Jhonny <gonzodzen@mail.ru>
2023-01-25 10:11:54 +0300

    Find The Duplicated Number in a Consecutive Unsorted List

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function alternateSqSum(arr){
-  return arr.map((el,i)=>i%2!==0?el**2:el).reduce((acc,curr)=>acc+curr,0)
+function findDup( arr ){
+return arr.filter((el,i,arr)=>arr.indexOf(el)!==arr.lastIndexOf(el))[0]
 }
-console.log(alternateSqSum([1,2,3,4,5]))
+console.log(findDup([1,2,2,3]))
