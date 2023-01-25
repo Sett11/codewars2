2bd66d3c7ed756443c66202eb4adda6ea0921ee2
Jhonny <gonzodzen@mail.ru>
2023-01-25 16:32:02 +0300

    Find the unknown digit

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-var findDup=function(arr){
-  return arr.filter((el,i,arr)=>arr.indexOf(el)!==arr.lastIndexOf(el))[0]
- }
-console.log(findDup([1,2,2,3]))
+function solveExpression(exp) {
+  console.log(exp)
+  if(exp.match(/^0/)||exp.match(/\*\*/)||exp.match(/\+\+/)||!exp.match(/\d/))return -1
+  let counter = 0
+  while(counter<10){
+    let check = exp.replace(/\?/g,counter).replace(/=/, '$&=').replace(/--/, ' - -')
+    if(eval(check)&&exp.match(/\d/g).map(Number).indexOf(counter)===-1&&!check.match(/=+00+$/g)&&!check.match(/^00+/g)){
+      return counter
+    }
+    counter++
+  }
+   return -1
+}
+console.log(solveExpression('?*123?45=?'))
