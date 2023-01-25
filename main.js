b27be8250de6abde652b0c5db2d9768881c0ae15
Jhonny <gonzodzen@mail.ru>
2023-01-25 17:18:46 +0300

    For the sake of argument

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-function solveExpression(exp) {
-  console.log(exp)
-  if(exp.match(/^0/)||exp.match(/\*\*/)||exp.match(/\+\+/)||!exp.match(/\d/))return -1
-  let counter = 0
-  while(counter<10){
-    let check = exp.replace(/\?/g,counter).replace(/=/, '$&=').replace(/--/, ' - -')
-    if(eval(check)&&exp.match(/\d/g).map(Number).indexOf(counter)===-1&&!check.match(/=+00+$/g)&&!check.match(/^00+/g)){
-      return counter
-    }
-    counter++
-  }
-   return -1
+function numbers () {
+  return [...arguments].every(el=>typeof el === 'number')
 }
-console.log(solveExpression('?*123?45=?'))
+console.log(numbers(1,2,3,4,5,NaN))
