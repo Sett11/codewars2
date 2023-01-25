e56a0449ccb60afbf8b09c5c1d6df8bb2f6ec4fa
Jhonny <gonzodzen@mail.ru>
2023-01-25 18:18:51 +0300

    Array combinations

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function numbers () {
-  return [...arguments].every(el=>typeof el === 'number')
-}
-console.log(numbers(1,2,3,4,5,NaN))
+function solve(arr) {
+  return arr.map(el=>[...new Set(el)].length).reduce((a,c)=>a*c)
+};
+console.log(solve([[1,2],[3,4],[5,6]]))
