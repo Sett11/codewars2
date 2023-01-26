0140a9271b205beb23d5e9faa51425fdb9d42470
Jhonny <gonzodzen@mail.ru>
2023-01-26 10:34:17 +0300

    Last Survivors Ep.2

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-function stantonMeasure (array) {
-  const check = array.filter(el=>el===1)
-  return array.filter(el=>el===check.length).length
+function lastSurvivors(str) {
+  const letters = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+  let newStr = str.split``.map((el) => parseInt(el, 36) - 10);
+  for (let i = 0; i < newStr.length; i++) {
+    let ind1 = newStr.indexOf(newStr[i]),
+      ind2 = newStr.lastIndexOf(newStr[i]);
+    if (ind1 !== ind2) {
+      newStr[ind1] += 1;
+      newStr[ind2] = "&";
+      newStr = newStr.filter((el) => el !== "&");
+      if (newStr[i] > 25) {
+        newStr[i] = (newStr[i] % 25) - 1;
+      }
+      if (newStr.length > [...new Set(newStr)].length) {
+        i = 0;
+      }
+      if (newStr[i] > 25) {
+        newStr[i] = (newStr[i] % 25) - 1;
+      }
+      if (newStr.length > [...new Set(newStr)].length) {
+        i = 0;
+      }
+    }
+  }
+  return newStr.map((el) => letters[el]).sort().join``;
 }
-console.log(stantonMeasure([1, 4, 3, 2, 1, 2, 3, 2]))
+console.log(lastSurvivors("lxxoygparcpklltwnkoywtcnhgsqpxe"));
