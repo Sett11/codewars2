73d130dc8cfdd98c8816e7d5d108e1f788e4c5e4
Jhonny <gonzodzen@mail.ru>
2023-01-26 17:35:42 +0300

    Search The 0 Sums Combinations in an Array

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-function chooseBestSum(t, k, l) {
+function findZeroSumGroups(ar, n){
+  console.log(ar,n)
+  if(ar.length===0)return "No elements to combine"
   const F = (arr,length) => {
     const total = []
     if(length===1){
       total.push(...arr.map(el=>[el]))
-    }
-    else{
+    }else{
       for(let i = 0; i < arr.length; i++){
-        total.push(...F(arr.slice(i+1),length-1).map(el=>[arr[i], ...el]))
+        total.push(...F(arr.slice(i+1), length-1).map(el=>[arr[i], ...el]))
       }
     }
     return total
   }
-  const arr = F(l,k).map(el=>el.reduce((a,c)=>a+c,0))
-  const finRes = [...new Set(arr)].filter(el=>el<=t).sort((a,b)=>b-a)[0]
-  return finRes===undefined ? null : finRes
-}
-console.log(chooseBestSum(230, 4, [
-  100,  76, 56,  44,  89,
-   73,  68, 56,  64, 123,
- 2333, 144, 50, 132, 123,
-   34,  89
-]))
+  const res = [...new Set(F(ar,n).map(el=>{
+    if(el.reduce((a,c)=>a+c,0)===0){
+      return el.sort((a,b)=>a-b)
+    }
+  }))].filter(el=>el!==undefined)
+  const finRes = [...new Set(res.map(el=>el.join`,`))].map(el=>el.split`,`.map(Number)).sort((a,b)=>a[0]-b[0]||a[1]-b[1]||a[2]-b[2]||a[3]-b[3])
+  .filter(el=>el.length === [...new Set(el)].length)
+  return finRes.length===0 ? "No combinations" : finRes.length===1 ? finRes[0] : finRes
+}    
+console.log(findZeroSumGroups([
+  6,  2,  6,   0,  4, -3, -10,
+ -3, 10, 10,   0,  6,  2,   6,
+  0,  4, -3, -10, -3, 10,  10,
+  0
+], 2))
+
+
+'[...new Set(res.map(el=>el.join`,`))].map(el=>el.split`,`.map(Number))'
