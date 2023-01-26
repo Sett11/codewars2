2314a85a7a0ef829f4f1ddc85e616f6f11c78483
Jhonny <gonzodzen@mail.ru>
2023-01-26 20:00:25 +0300

    longest_palindrome

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-function isAllPossibilities(x){
-  console.log(x)
-  if(!x.includes(0)||x.length===0)return false
-  if(x[x.length-1]===0)return true
-  const arr = [],res = []
-  let counter = 1
-  while(counter<x[x.length-1]){
-    arr.push(counter)
-    counter++
+longestPalindrome=function(s){
+  console.log(s)
+  if(s.length===0)return 0
+  if(s.length===1)return 1
+  if(s.length===2&&s[0]===s[1])return 2
+  if(s.length===3&&s[0]===s[1]||s.length===3&&s[1]===s[2])return 2
+  const arr = []
+  for(let i = 0; i < s.length; i++){
+    for(let j = 1; j < s.length; j++){
+      if(s.slice(i,j)!==''){
+        arr.push(s.slice(i,j))
+      }
+    }
   }
   for(let i = 0; i < arr.length; i++){
-    res.push(x.includes(arr[i]))
+    let tmp = arr[i].slice(0, Math.floor(arr[i].length/2))
+    let tmp2 = arr[i].slice(Math.ceil(arr[i].length/2))
+    for(let j = 0; j < tmp.length; j++){
+      if(tmp[j]!==tmp2[tmp2.length-j-1]){
+        arr[i] = 0
+      }
+    }
   }
-  return res.length===0 ? false : res.every(el=>el===true)
+  const finRes = arr.filter(el=>el!==0).sort((a,b)=>b.length-a.length)[0]
+  return finRes.length
 }
-console.log(isAllPossibilities([ 3, 2, 1, 0 ]))
+console.log(longestPalindrome('a'))
