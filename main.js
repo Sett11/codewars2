72d71977b507d0b5a7b770363a3cb0fb71bb9066
Jhonny <gonzodzen@mail.ru>
2023-01-26 20:14:18 +0300

    Map over a list of lists

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-function duplicateSandwich(a) {
-  const arr = []
-  for(let i = 0; i < a.length; i++){
-    if(a.indexOf(a[i])!==a.lastIndexOf(a[i])){
-      arr.push(a.indexOf(a[i]), a.lastIndexOf(a[i]))
-      break
-    }
-  }
-  return a.slice(arr[0]+1, arr[1])
+function gridMap(fn,a) {
+  return a.map(el=>el.map(fn))
 }
-console.log(duplicateSandwich([0, 1, 2, 3, 4, 5, 6, 1, 7, 8]))
+console.log(gridMap(x => x+1, [ [1,2,3,4]
+  , [5,6,7,8,9]
+  , [0,2,4]
+  ]))
