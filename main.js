135b6ef7d6f11865e218930f1baa9b9ff456daae
Jhonny <gonzodzen@mail.ru>
2023-01-26 15:54:26 +0300

    Best travel

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-var nato = (function() {
-  var letters =  {
-    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
-    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
-    "G": "Golf",   "H": "Hotel",   "I": "India",
-    "J": "Juliett","K": "Kilo",    "L": "Lima",
-    "M": "Mike",   "N": "November","O": "Oscar",
-    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
-    "S": "Sierra", "T": "Tango",   "U": "Uniform",
-    "V": "Victor", "W": "Whiskey", "X": "X-ray",
-    "Y": "Yankee", "Z": "Zulu"
+function chooseBestSum(t, k, l) {
+  const F = (arr,length) => {
+    const total = []
+    if(length===1){
+      total.push(...arr.map(el=>[el]))
+    }
+    else{
+      for(let i = 0; i < arr.length; i++){
+        total.push(...F(arr.slice(i+1),length-1).map(el=>[arr[i], ...el]))
+      }
+    }
+    return total
   }
-  
-  return function(word) {
-    return word.split``.map(el=>letters[el.charAt(0).toUpperCase()]).join` `
-  }
-})()
+  const arr = F(l,k).map(el=>el.reduce((a,c)=>a+c,0))
+  const finRes = [...new Set(arr)].filter(el=>el<=t).sort((a,b)=>b-a)[0]
+  return finRes===undefined ? null : finRes
+}
+console.log(chooseBestSum(230, 4, [
+  100,  76, 56,  44,  89,
+   73,  68, 56,  64, 123,
+ 2333, 144, 50, 132, 123,
+   34,  89
+]))
