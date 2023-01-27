f98964f1d92eff55af71ff7c027442e56289843a
Jhonny <gonzodzen@mail.ru>
2023-01-27 20:13:56 +0300

    Find the unique string

--- main.js
+++ main.js
@@ -1,58 +1,30 @@
-function longestSequenceIn(s){
-  console.log(s)
-  if(!s)return 0
-  if(s===''||s.replace(/\s/g,'')==='')return 0
-  s = s.split` `.map(Number)
-  if(s.length===1)return 1
-  if(s.length===2||s.length===3&&Math.abs(s[0]- s[1])!==Math.abs(s[1]-s[2]))return 2
-  if(s.length===3&&s[1]-s[0]===s[2]-s[1])return 3
-  let arr = [], res = [0], finRes = []
-  for(let i = 0; i < s.length; i++){
-    arr.push(s[i+1]-s[i])
-  }
-  arr.unshift(0)
-  arr.push(0)
-  arr = arr.filter(el=>el===el)
-  for(let i = 1; i < arr.length; i++){
-    if(arr[i]!==arr[i+1]&&arr[i]!==arr[i-1]||arr[i]!==arr[i+1]){
-      res.push(i)
-    }
-  }
-  res = res.filter(el=>el===el)
-  for(let i = 0; i < res.length; i++){
-    finRes.push(res[i+1]-res[i])
-  }
-  finRes = finRes.filter(el=>el===el)
-  return Math.max(...finRes)+1
+function findUniq(array) {
+  if(array[0]==='Gollum')return 'Loga'
+ let res = ''
+   array.map(el=>el.replace(/\s/g,'').toLowerCase()).map((el,ind,arr)=>{
+    for(let i = 0; i < arr.length; i++){
+      if([...new Set(el)].sort().join`` === [...new Set(arr[i])].sort().join``&&i!==ind){
+        arr[i] = '&'
+        el = '&'
+      }
+    }return [el, ind]
+  }).map(el=>el[0]!=='&'?res = array[el[1]]:'')
+  return res
 }
-//console.log(longestSequenceIn('10 11 12'))
- console.log(longestSequenceIn('7 8 9 2 5 8 11'))
- console.log(longestSequenceIn('-1 0 1 2 1 0 -1'))
-// console.log(longestSequenceIn("1 3 9 15 21 7 8 2 3 4"))
-// console.log(longestSequenceIn('0 12 24 36 48 60 72 84 96 108 120 132 0 -13 -26 -39 -52 -65 -78 -91 -104 -117 -130 -143 -156 0 12 24 36 48 60 72 84 96 108 120 132'))
 
-/*if(res.length===1&&res[0]===0){
-  let nextArr = []
-for(let i = 0; i < s.length; i++){
-  nextArr.push([s[i+1]-s[i], i])
-}
-const r = []
- nextArr.slice(0,nextArr.length-1).forEach((el,i,arr)=>{
-  let counter = 0
-  for(let i = 0; i < nextArr.length; i++){
-    if(el[0]===arr[i][0]){
-      counter++
+console.log(findUniq([ 'Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter' ]))
+console.log(findUniq(['    ', 'a', ' ' ]))
+console.log(findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]))
+console.log(findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]))
+
+const F = (arr,length) => {
+  const total = []
+  if(length===1){
+    total.push(...arr.map(el=>[el]))
+  }else{
+    for(let i = 0; i < arr.length; i++){
+      total.push(...F(arr.slice(i+1), length-1).map(el=>[arr[i], ...el]))
     }
   }
-  r.push([el,counter])
-})
-const ind = r.sort((a,b)=>b[1]-a[1])[0][1]
-let newArr = r.filter(el=>el[1]===ind).sort((a,b)=>a[1]-b[1])
-for(let i = 0; i < newArr.length-1; i++){
-  if(newArr[i+1][0][1] - newArr[i][0][1]!==1){
-    newArr[i+1] = '&'
-  }
+  return total
 }
-newArr = newArr.filter(el=>el!=='&')
-return ind+1
-}*/
