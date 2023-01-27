0bc2d84db39a53a4374c58661f14904e8ed114f4
Jhonny <gonzodzen@mail.ru>
2023-01-27 20:57:37 +0300

    Create Phone Number

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-function prefixSumsToSuffixSums(a) {
-  const arr = []
-  arr.push(a.pop())
-  for(let i = 0; i < a.length; i++){
-    arr.push(arr[0] - a[i])
-  } return arr
+function createPhoneNumber(numbers){
+  return `(${numbers.slice(0,3).join``}) ${numbers.slice(3,6).join``}-${numbers.slice(6).join``}`
 }
-console.log(prefixSumsToSuffixSums([-1, -2, -3, -4, -5]))
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
