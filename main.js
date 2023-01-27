1e74d682846f265deaba756602c700df57253a57
Jhonny <gonzodzen@mail.ru>
2023-01-27 20:37:57 +0300

    Simple Fun #237: Suffix Sums

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
-function findUniq(array) {
-  if(array[0]==='Gollum')return 'Loga'
- let res = ''
-   array.map(el=>el.replace(/\s/g,'').toLowerCase()).map((el,ind,arr)=>{
-    for(let i = 0; i < arr.length; i++){
-      if([...new Set(el)].sort().join`` === [...new Set(arr[i])].sort().join``&&i!==ind){
-        arr[i] = '&'
-        el = '&'
-      }
-    }return [el, ind]
-  }).map(el=>el[0]!=='&'?res = array[el[1]]:'')
-  return res
-}
-
-console.log(findUniq([ 'Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter' ]))
-console.log(findUniq(['    ', 'a', ' ' ]))
-console.log(findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]))
-console.log(findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]))
-
-const F = (arr,length) => {
-  const total = []
-  if(length===1){
-    total.push(...arr.map(el=>[el]))
-  }else{
-    for(let i = 0; i < arr.length; i++){
-      total.push(...F(arr.slice(i+1), length-1).map(el=>[arr[i], ...el]))
-    }
+function suffixSums(a) {
+  const arr = []
+  for(let i = 0; i < a.length; i++){
+    arr.push(a.slice(i).reduce((a,c)=>a+c))
   }
-  return total
+  return arr
 }
+console.log(suffixSums([1, 2, 3, -6]))
