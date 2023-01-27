70a899983043756b3d1c609649bb8595eab41755
Jhonny <gonzodzen@mail.ru>
2023-01-27 20:47:23 +0300

    Simple Fun #250: Prefix Sums To Suffix Sums

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function suffixSums(a) {
+function prefixSumsToSuffixSums(a) {
   const arr = []
+  arr.push(a.pop())
   for(let i = 0; i < a.length; i++){
-    arr.push(a.slice(i).reduce((a,c)=>a+c))
-  }
-  return arr
+    arr.push(arr[0] - a[i])
+  } return arr
 }
-console.log(suffixSums([1, 2, 3, -6]))
+console.log(prefixSumsToSuffixSums([-1, -2, -3, -4, -5]))
