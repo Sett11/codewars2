b76dda0b08709bd5ac6815cadc8af5ed97a83271
Jhonny <gonzodzen@mail.ru>
2023-01-28 23:19:44 +0300

    What's A Name In?

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-function nthSmallest() {
-  const arr = [...arguments].flat(), check = arr.pop()
-  return arr.sort((a,b)=>a-b)[check-1]
+function nameInStr(str, name){
+  str = str.toLowerCase(),name = name.toLowerCase()
+  const arr = Array.from({length:str.length}, _ => Array(name.length))
+  return F(str,name,0,0,arr)===name?true:false
 }
-console.log(nthSmallest([1,5], [2], [4,8,9], 4))
+function F (x,y,i,j,arr){
+  if(i===x.length||j===y.length)return ''
+  if(arr[i][j]!==undefined){
+    return arr[i][j]
+  }
+  if(x[i]===y[j]){
+    arr[i][j] = x[i] + F(x,y,i+1,j+1,arr)
+    return arr[i][j]
+  }
+  const a = F(x,y,i+1,j,arr), b = F(x,y,i,j+1,arr)
+  arr[i][j] = a.length>b.length?a:b
+  return arr[i][j]
+}
+console.log(nameInStr('Next to a lake', 'chris'))
