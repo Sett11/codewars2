c12e4a714ae98ba4db4dfa640ebd1e646a058dec
Jhonny <gonzodzen@mail.ru>
2023-01-28 14:00:26 +0300

    Truncate a string!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function sortItOut(array){
-  const arr1 = [], arr2 = []
-   array.map(el=>el&1?arr2.push(el):arr1.push(el))
-   return arr2.sort((a,b)=>a-b).concat(arr1.sort((a,b)=>b-a))
+function truncateString(str, num) {
+  if(str.length===num)return str
+  if(str.length<num)return str
+  if(num<=3)return str.slice(0,num) + '...'
+  return str.slice(0, num - 3) + '...'
 }
-  console.log(sortItOut([11,22,33,44,55,55,90.4,4,78]))
+console.log(truncateString('A-tisket a-tasket A green and yellow basket', 43))
