afeb55849441a2c69ec6e08b695abe137e92097e
Jhonny <gonzodzen@mail.ru>
2023-01-29 01:24:18 +0300

    No ifs no buts

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-Plugboard = function(wires){
-  console.log(wires)
-  let arr = []
-  if(wires===undefined){
-    arr = []
-  }
-  else if([...new Set(wires)].length!==wires.length||wires.length===1||wires.length%2!==0||wires.length>20){
-    throw Error("Invalid translation")
-  }else{
-     if(wires.length!==0){
-    wires = wires.split``
-  }
-   arr = []
-  for(let i = 0; i < wires.length; i+=2){
-    arr.push(wires.slice(i,i+2))
-  }
-    }
-  return {
-     process(wire){
-       console.log(wire)
-      for(let i = 0; i < arr.length; i++){
-        if(wire===arr[i][0]){
-          wire = arr[i][1]
-          return wire
-        }
-        if(wire===arr[i][1]){
-          wire = arr[i][0]
-          return wire
-        }
-      }
-      return wire
-    }
-  }
+const noIfsNoButs = function (a,b) {
+  const res1 = (a-b)<0&&`${a} is smaller than ${b}`
+  const res2 = (a-b)>0&&`${a} is greater than ${b}`
+  const res3 = (a-b)===0&&`${a} is equal to ${b}`
+  return res1||res2||res3
 }
-
-const R = Plugboard('ABCDEFGHIJKLMNOPQRST')
-console.log(R.process('T'))
+console.log(noIfsNoButs(100,100))
