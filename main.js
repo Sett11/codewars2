a079b943c85bb26b8b9b0183b380aa3ce978039e
Jhonny <gonzodzen@mail.ru>
2023-01-28 23:40:34 +0300

    Alternating between three values

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-function nameInStr(str, name){
-  str = str.toLowerCase(),name = name.toLowerCase()
-  const arr = Array.from({length:str.length}, _ => Array(name.length))
-  return F(str,name,0,0,arr)===name?true:false
-}
-function F (x,y,i,j,arr){
-  if(i===x.length||j===y.length)return ''
-  if(arr[i][j]!==undefined){
-    return arr[i][j]
+function f(x, cc) {
+  cc = Object.values(cc)
+  for(let i = 0; i < cc.length; i++){
+    if(x===cc[i]){
+      x = cc[i+1]
+      return x
+    }
+    if(x===cc[cc.length-1]){
+      x = cc[0]
+      return x
+    }
   }
-  if(x[i]===y[j]){
-    arr[i][j] = x[i] + F(x,y,i+1,j+1,arr)
-    return arr[i][j]
-  }
-  const a = F(x,y,i+1,j,arr), b = F(x,y,i,j+1,arr)
-  arr[i][j] = a.length>b.length?a:b
-  return arr[i][j]
 }
-console.log(nameInStr('Next to a lake', 'chris'))
+console.log(f(5, { a:3, b:4, c:5 } ))
