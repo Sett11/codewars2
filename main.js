496ce019ce7f985a307cfac402b374bbf55cc72b
Jhonny <gonzodzen@mail.ru>
2023-01-28 23:56:53 +0300

    Simple Sentences

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-function f(x, cc) {
-  cc = Object.values(cc)
-  for(let i = 0; i < cc.length; i++){
-    if(x===cc[i]){
-      x = cc[i+1]
-      return x
-    }
-    if(x===cc[cc.length-1]){
-      x = cc[0]
-      return x
-    }
-  }
+function makeSentence(parts) {
+  return parts.join` `.replace(/\s,/g,',').replace(/\./g,'').trim() + '.'
 }
-console.log(f(5, { a:3, b:4, c:5 } ))
+console.log(makeSentence([ 'hello', 'world', '.', '.', '.' ]))
