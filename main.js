6cf71e599243f4744e8be60e01a99c58dd0639b0
Jhonny <gonzodzen@mail.ru>
2023-01-29 17:00:35 +0300

    Coding 3min: Collatz Array(Split or merge)

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-function dup(s) {
-  return s.map(el=>{
-    el = el.split``
-   for(let i = 0; i < el.length; i++){
-    if(el[i]===el[i+1]){
-      el.splice(i+1,1)
-      i--
+function sc(arr,n){
+  if(n===0)return arr
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i]%2!==0){
+      arr[i] = ((arr[i]*3)+1)
+      if(i!==arr.length-1){
+        arr[i] += arr[i+1]
+        arr[i+1] = '&'
+      }
+      i++
     }
-   }
-   return el.join``
-  })
+    if(arr[i]%2===0){
+      arr[i] = [arr[i]/2, arr[i]/2]
+    }
+  }
+  arr = arr.flat().filter(el=>el!=='&')
+  return sc(arr,n-1)
 }
-console.log(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]))
+console.log(sc([7,7,8,8], 1))
+
+if (!Array.prototype.flat) {
+	Array.prototype.flat = function(depth) {
+		if (depth === undefined) {
+			depth = 1;
+		}
+		const flatten = function (arr, depth) {
+			if (depth < 1) {
+				return arr.slice();
+			}
+			return arr.reduce(function (acc, val) {
+				return acc.concat(Array.isArray(val) ? flatten(val, depth - 1) : val);
+			}, []);
+
+		}
+		return flatten(this, depth);
+	}
+}
