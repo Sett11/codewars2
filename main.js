cdd3538817f908adc28b38d9bb9bf9280c9a66ad
Jhonny <gonzodzen@mail.ru>
2023-01-29 19:51:59 +0300

    Shortest Code: Symmetric Sort

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-sc = (a) =>
-  Math.max(
-    (a = a.sort((z, v) => v - z))[0] + a[1],
-    a[0] * a[1],
-    a[0] - Math.min(...a),
-    a[0] * Math.max(...a.map((t) => (t == 0 ? 0 : 1 / t)))
-  )
-console.log(sc([-1, 2]));
+ sc=a=>{
+  a=a.sort((a,b)=>a-b),z=a.slice()
+  return z.filter((t,i)=>i%2==0).concat(a.filter((t,i)=>i&1).reverse())
+}
+console.log(sc([1,2,3,4,5,6,7,8,9]))
