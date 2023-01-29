3e6797ead1fdbb053f90c2c15d673e537cb7d8b9
Jhonny <gonzodzen@mail.ru>
2023-01-29 19:53:46 +0300

    Coding 3min : Symmetric Sort

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
- sc=a=>{
-  a=a.sort((a,b)=>a-b),z=a.slice()
+ sc=(a)=>{
+  a=a.sort((a,b)=>a-b),z=[]
+  for(i = 0; i < a.length; i++){
+    z.push(a[i])
+  }
   return z.filter((t,i)=>i%2==0).concat(a.filter((t,i)=>i&1).reverse())
 }
 console.log(sc([1,2,3,4,5,6,7,8,9]))
