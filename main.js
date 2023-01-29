e569cf6be9c9bb2468cf7d7c2dfeebfcb819b35c
Jhonny <gonzodzen@mail.ru>
2023-01-29 18:15:30 +0300

    Coding 3min : Max Value

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
- sc =r=> {
-  r=typeof r=='object'?r:(r+'').split``
-return r.join``==r.reverse().join``
+function sc(array){
+  const arr = []
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < array.length; j++){
+      if(i!==j){
+        arr.push(array[i]+array[j], array[i]-array[j], array[i]*array[j], array[i]/array[j])
+      }
+    }
+  }return Math.max(...arr.filter(el=>el==el&&el!==Infinity&&el!==-Infinity))
 }
-
-
-console.log(sc([11,12,13,12,11]))
+console.log(sc([1,0]))
