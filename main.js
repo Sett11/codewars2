76c138cd70ec369efae8e49abe7054ec78cf0b3a
Jhonny <gonzodzen@mail.ru>
2023-01-29 17:50:36 +0300

    Shortest Code : Are they symmetrical?

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-function sc(arr,n){
-  if(n===0)return arr
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i]%2!==0){
-      arr[i] = ((arr[i]*3)+1)
-      if(i!==arr.length-1){
-        arr[i] += arr[i+1]
-        arr[i+1] = '&'
-      }
-      i++
-    }
-    if(arr[i]%2===0){
-      arr[i] = [arr[i]/2, arr[i]/2]
-    }
-  }
-  arr = arr.flat().filter(el=>el!=='&')
-  return sc(arr,n-1)
+ sc =r=> {
+  r=typeof r=='object'?r:(r+'').split``
+return r.join``==r.reverse().join``
 }
-console.log(sc([7,7,8,8], 1))
 
-if (!Array.prototype.flat) {
-	Array.prototype.flat = function(depth) {
-		if (depth === undefined) {
-			depth = 1;
-		}
-		const flatten = function (arr, depth) {
-			if (depth < 1) {
-				return arr.slice();
-			}
-			return arr.reduce(function (acc, val) {
-				return acc.concat(Array.isArray(val) ? flatten(val, depth - 1) : val);
-			}, []);
 
-		}
-		return flatten(this, depth);
-	}
-}
+console.log(sc([11,12,13,12,11]))
