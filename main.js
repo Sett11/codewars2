9a8523bda0fd587ff025a46e9bfb6b95186baf77
Jhonny <gonzodzen@mail.ru>
2023-01-30 00:02:22 +0300

    Coding 3min : Jumping Dutch act

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-sc=w=>{
-  w = w.replace(/[^a-zA-Z\s]/g,'').split` `
-   o = {}
-   c = 0
-  for( i of w){
-    i=i.toLowerCase()
-    if(!o[i]){
-      o[i]=i
-      c+=i.length
-    }else{
-      c+=1
-    }
-  }
-  return c
+function sc(floor){
+  if(floor<=1)return ''
+  let res = `${'Aa~ '.repeat(floor-1)}${'Pa!'}`
+  return floor<7?res + ' ' + 'Aa!':res
 }
-console.log(sc("Hello World!"))
+console.log(sc(1))
