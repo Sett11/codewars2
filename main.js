b01d388539cdf6ae0d51b544f32006f31584c15d
Jhonny <gonzodzen@mail.ru>
2023-01-30 23:05:13 +0300

    WeIrD StRiNg CaSe

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-function changeCase(w, t){
-  console.log(w)
-  if(w==='')return ''
-  if(t!=='kebab'&&t!=='camel'&&t!=='snake'||w.match(/-/g)&&w.match(/_/g))return undefined
-  if(w.match(/[A-Z]/g)&&w.match(/_/g)||w.match(/[A-Z]/g)&&w.match(/-/g))return undefined
-  if(t=='kebab'){
-    w = w.replace(/-|_/g, ' ').replace(/[A-Z]/g,' $&').split` `
-    return w.map(el=>el.toLowerCase()).join`-`
-  }
-  if(t=='snake'){
-    w = w.replace(/-|_/g, ' ').replace(/[A-Z]/g,' $&').split` `
-    return w.map(el=>el.toLowerCase()).join`_`
-  }
-  if(t=='camel'){
-    w = w.replace(/-|_/g, ' ').replace(/[A-Z]/g,' $&').split` `
-    return w.shift() + w.map(el=>el.charAt(0).toUpperCase() + el.slice(1)).join``
-  }
+function toWeirdCase(s){
+ return s.split` `.map(el=>el.split``.map((ul,i)=>i%2===0?ul.toUpperCase():ul.toLowerCase()).join``).join` `
 }
-console.log(changeCase("map_to_all", "camel"))
-console.log(changeCase("snakeCamel_case", "snake"))
+console.log(toWeirdCase("Weird string case"))
+console.log(toWeirdCase('String'))
