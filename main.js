1f4dcf7f650e581b09966b627e2799adf39febc3
Jhonny <gonzodzen@mail.ru>
2023-01-30 21:53:00 +0300

    Numbers that are a power of their sum of digits

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function eqSumPowdig(hMax, exp) {
-  let arr = [], counter = 2
-  while(counter<=hMax){
-    if((counter+'').split``.map(Number).map(el=>el**exp).reduce((a,c)=>a+c,0)==counter){
-      arr.push(counter)
+function powerSumDigTerm(n) {
+  const res = [81,512,2401,4913,5832,17576,19683,234256,390625,614656,1679616,17210368]
+  //if(n<res.length)return res[n]
+  let counter = res[res.length-1]+1
+  while(res.length<n){
+    for(let i = 2; i < 10; i++){
+      if((counter+'').split``.map(Number).reduce((a,c)=>a+c,0)**i==counter){
+        res.push(counter)
+        break
+      }
     }
     counter++
   }
-  return arr
+  return counter
 }
-console.log(eqSumPowdig(400,3))
+console.log(powerSumDigTerm(13))
