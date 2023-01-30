b3eea62dc37576f6d066b643ed0c33337f626536
Jhonny <gonzodzen@mail.ru>
2023-01-31 00:32:09 +0300

    Split Strings

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-function sort(s) {
-  s=s.map(el=>Object.entries(el))
-  .sort((a,b)=>b[1][1]-a[1][1]||a[2][1].match(/ .+/g)[0][1].localeCompare(b[2][1].match(/ .+/g)[0][1])||a[0][1]-b[0][1])
-  let str = ''
-  for(let i = 0; i < s.length; i++){
-    str+=`${s[i][2][1]},`
-  }
-  return str.slice(0,str.length-1)
+function solution(str){
+   const arr = []
+   for(let i = 0; i < str.length; i+=2){
+    arr.push(str.slice(i,i+2))
+   }
+   return str.length%2===0? arr : arr.map(el=>el.length===2?el:el+'_')
 }
-
-
-class Student {
-  constructor(age, gpa, fullName) {
-    this.age = age;
-    this.gpa = gpa;
-    this.fullName = fullName;
-  };
-};
-console.log(sort([new Student(23, 88, "David Goodman"), 
-new Student(25, 82, "Mark Rose"), 
-new Student(22, 90, "Jane Doe"),
-new Student(25, 90, "Jane Dane")]))
+console.log(solution(''))
