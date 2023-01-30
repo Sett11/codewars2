f27a49454c026815573e9e48e67ccfae3b2226bd
Jhonny <gonzodzen@mail.ru>
2023-01-31 00:59:24 +0300

    Find the discounted prices

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function radLadies(name){
-  return name.replace(/[^a-zA-Z\s!]/g,'').toUpperCase()
+function findDiscounted(p){
+  return p=='0 0'?'0':p.split` `.map(Number).map((el,_,arr)=>{
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i]===el+(el/3)){
+        arr[i] = undefined
+        return el
+      }
+    }
+  }).filter(el=>el).join` `
 }
+console.log(findDiscounted("9 9 12 12 12 15 16 20"))
+console.log(findDiscounted("15 20 60 75 80 100"))
