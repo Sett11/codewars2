9223e12314ffcb1df850eab174ab01498411c50b
Jhonny <gonzodzen@mail.ru>
2023-01-30 22:15:48 +0300

    Convert string to camel case

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-function powerSumDigTerm(n) {
-  const res = [81,512,2401,4913,5832,17576,19683,234256,390625,614656,1679616,17210368]
-  //if(n<res.length)return res[n]
-  let counter = res[res.length-1]+1
-  while(res.length<n){
-    for(let i = 2; i < 10; i++){
-      if((counter+'').split``.map(Number).reduce((a,c)=>a+c,0)**i==counter){
-        res.push(counter)
-        break
-      }
-    }
-    counter++
-  }
-  return counter
+function toCamelCase(str){
+  str = str.replace(/-|_/g, ' ').split` `
+  return str.shift() + str.map(el=>el.charAt(0).toUpperCase() + el.slice(1)).join``
 }
-console.log(powerSumDigTerm(13))
+console.log(toCamelCase("The_Stealth_Warrior"))
