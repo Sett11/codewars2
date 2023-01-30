10776a9924b2d6b36c247ad72091874763cae766
Jhonny <gonzodzen@mail.ru>
2023-01-31 00:45:23 +0300

    The Ladies of ENIAC

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function solution(str){
-   const arr = []
-   for(let i = 0; i < str.length; i+=2){
-    arr.push(str.slice(i,i+2))
-   }
-   return str.length%2===0? arr : arr.map(el=>el.length===2?el:el+'_')
+function radLadies(name){
+  return name.replace(/[^a-zA-Z\s!]/g,'').toUpperCase()
 }
-console.log(solution(''))
