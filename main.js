b9304fbf2a14516950f8c69c5f7c98db4fd7ad68
Jhonny <gonzodzen@mail.ru>
2023-01-31 21:55:35 +0300

    Check for prime numbers

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function isPrime(n) {
-  if (n < 2 || n === 4||n===9) return false;
+  if (n < 2) return false;
   if (n === 2) return true;
   for (let i = 2; i < Math.sqrt(n)+1; i++) {
     if (n % i === 0) return false;
