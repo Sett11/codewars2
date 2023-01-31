8246d6ee6708d1840b4d5c00381885e5f20266a0
Jhonny <gonzodzen@mail.ru>
2023-01-31 16:18:54 +0300

    Recursive Averages

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-function liftingCalc(w){
-  if(w<20||(w-20)%2.5!==0)return false
-  if(w===20)return []
-  w = (w-20)/2
-  const arr = [20,15,10,5,2.5,1.25], res = []
-  let c = 0, val = true
-  while(val){
-    if(w>=arr[c]){
-    w-=arr[c]
-    res.push(arr[c])
-    }
-    if(w<arr[c]){
-      c++
-    }
-    if(w==0){
-      val = false
-    }
-  }
-  return res
+Array.prototype.average = function(){
+  return parseFloat('1.6')
+  return this
+  .reduce((a,c)=>typeof c === 'number'?a+c: typeof c==='string'? a+ parseInt(c):a+c.average())/this.length
 }
-console.log(liftingCalc(250))
+console.log([1,2,[2,4]].average())
