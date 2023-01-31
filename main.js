abfc601c63788d8c648704a413f4a9c6fc3da101
Jhonny <gonzodzen@mail.ru>
2023-01-31 07:35:39 +0300

    Take a picture !

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-function findDiscounted(p){
-  return p=='0 0'?'0':p.split` `.map(Number).map((el,_,arr)=>{
-    for(let i = 0; i < arr.length; i++){
-      if(arr[i]===el+(el/3)){
-        arr[i] = undefined
-        return el
-      }
+function sortPhotos(p){
+  const res = p
+  .map(el=>el.split`.`)
+  .sort((a,b)=>Number(a[0])-Number(b[0])||a[1].replace(/\D/g, '')-b[1].replace(/\D/g, ''))
+  .map(el=>el.join`.`)
+  if(res.length>5){
+    while(res.length>5){
+      res.shift()
     }
-  }).filter(el=>el).join` `
+  }
+  const num = Number(res[res.length-1].match(/\d+$/g,''))+1
+  res.push(res[res.length-1].replace(/\d+$/g,num))
+  return res
 }
-console.log(findDiscounted("9 9 12 12 12 15 16 20"))
-console.log(findDiscounted("15 20 60 75 80 100"))
+console.log(sortPhotos(["2016.img1","2013.img3","2016.img2","2015.img3","2012.img7","2016.img90","2013.img"]))
