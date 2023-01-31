f1a23a7dc1c7a46205e798ac3ec11e144bd8de18
Jhonny <gonzodzen@mail.ru>
2023-01-31 07:54:38 +0300

    replaceAll(input, find, replace)

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-function sortPhotos(p){
-  const res = p
-  .map(el=>el.split`.`)
-  .sort((a,b)=>Number(a[0])-Number(b[0])||a[1].replace(/\D/g, '')-b[1].replace(/\D/g, ''))
-  .map(el=>el.join`.`)
-  if(res.length>5){
-    while(res.length>5){
-      res.shift()
-    }
-  }
-  const num = Number(res[res.length-1].match(/\d+$/g,''))+1
-  res.push(res[res.length-1].replace(/\d+$/g,num))
-  return res
+function replaceAll(i, f, r) {
+  if(f.match(/\/|\^|\&/))return i.replace(f, r)
+  return i.replace(new RegExp(`${f}`,'g'), r)
 }
-console.log(sortPhotos(["2016.img1","2013.img3","2016.img2","2015.img3","2012.img7","2016.img90","2013.img"]))
+console.log(replaceAll('123\^$.|?*+()[]{}', '\^$.|?*+()[]{}', '-'))
