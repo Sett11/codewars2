276a45c314d07099dc1f457ee8512d91d393994f
Jhonny <gonzodzen@mail.ru>
2023-01-31 21:48:15 +0300

    Is a number prime?

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-avg = a => a.reduce((a,c)=>a+c,0)/a.length
+function isPrime(n) {
+  if (n < 2 || n === 4||n===9) return false;
+  if (n === 2) return true;
+  for (let i = 2; i < Math.sqrt(n)+1; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
