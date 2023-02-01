ed78e12dbbab3a0305f9c7599b36112453e58eb0
Jhonny <gonzodzen@mail.ru>
2023-02-01 12:08:38 +0300

    N-th Fibonacci

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-function sumConsecutives(s) {
-  for(let i = 0; i < s.length;i++){
-   if(s[i]===s[i+1]){
-    for(let j = i; j < s.length; j++){
-      if(s[j]!==s[j+1]){
-       s = s.slice(0,i).concat(s.slice(i,j+1).reduce((a,c)=>a+c,0)).concat(s.slice(j+1))
-       break
-      }
+function nthFibo(n) {
+    const arr = [0,1]
+    for(let i = 0; i <= n; i++){
+      arr.push(arr[i] + arr[i+1])
     }
-   }
+    return arr[n-1]
   }
-  return s
-}
-console.log(sumConsecutives([1,4,4,4,0,4,3,3,1]))
+  console.log(nthFibo(4))
