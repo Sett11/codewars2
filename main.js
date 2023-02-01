7e978ee2c76dd2c7a9f222f05c49a05a04ea410d
Jhonny <gonzodzen@mail.ru>
2023-02-01 12:41:30 +0300

    Even Fibonacci Sum

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function fibonacci(n) {
     arr = [0, 1];
     for (let i = 0; i <= n; i++) {
-      arr.push(arr[i] + arr[i + 1]);
+        if(arr[i]+arr[i+1]>=n){
+            break
+          }
+      arr.push(arr[i] + arr[i + 1])
     }
-    return arr.slice(0,n)
-  }
-console.log(fibonacci(5))
+    return arr.filter(el=>el%2===0).reduce((a,c)=>a+c)
+}
+console.log(fibonacci(1000000000))
