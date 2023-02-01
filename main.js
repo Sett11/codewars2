0b6b7989484ff5d3c2ed1b9abdf6d4aa4de7279b
Jhonny <gonzodzen@mail.ru>
2023-02-01 12:14:11 +0300

    Fibonacci Reloaded

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-function nthFibo(n) {
-    const arr = [0,1]
-    for(let i = 0; i <= n; i++){
-      arr.push(arr[i] + arr[i+1])
-    }
-    return arr[n-1]
+function fib(n) {
+  arr = [0, 1];
+  for (let i = 0; i <= n; i++) {
+    arr.push(arr[i] + arr[i + 1]);
   }
-  console.log(nthFibo(4))
+  return arr[n - 1];
+}
