9f664ad752e5d267c0404c33271212130f2c4990
Jhonny <gonzodzen@mail.ru>
2023-02-02 17:06:53 +0300

    Maze Runner

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-const peakHeight = (m) => {
-    if(m.length===0)return 0
-    m=m.map(el=>el.map(ul=>ul===' '?0:1))
-    let c = 1, val = true
-    while(val){
-        val=false
-         nm = m.map(el=>el.slice())
-        for(let i = c; i < m.length-c; i++){
-            for(let j = c; j < m[i].length-c; j++){
-                if(m[i][j]>0&&m[i][j]===m[i][j+1]&&m[i][j]===m[i][j-1]&&m[i][j]===m[i+1][j]&&m[i][j]===m[i-1][j]){
-                    nm[i][j]+=1
-                    val=true
-                }
-            }
-        }
-        c++
-        m=nm
-    }return Math.max(...nm.flat())
+function mazeRunner(m, d) {
+  let ind = []
+  m.map((el, i) => el.map((ul, x) => (ul === 2 ? ind.push(i, x) : null)));
+  let oneI = ind[0],twoI = ind[1], arr = [], res = []
+    for(let i = 0; i < d.length; i++){
+        if (d[i] === "N") {
+            arr.push([oneI-1, twoI])
+            oneI-=1
+          }
+          if (d[i] === "S") {
+            arr.push([oneI+1, twoI])
+            oneI+=1
+          }
+          if (d[i] === "E") {
+            arr.push([oneI, twoI+1])
+            twoI+=1
+          }
+          if (d[i] === "W") {
+            arr.push([oneI, twoI-1])
+            twoI-=1
+          }
+    }
+    arr.map(el=>el[0]<0||el[1]<0||el[0]>m[0].length-1||el[1]>m[0].length-1?res.push(1): res.push(m[el[0]][el[1]]))
+  return !res.some(el=>el===1)&&res.some(el=>el===3)?'Finish':res.some(el=>el===1)?'Dead':'Lost'
 }
-console.log(peakHeight([
-    "^^^^^^        ".split(''),
-    " ^^^^^^^^     ".split(''),
-    "  ^^^^^^^     ".split(''),
-    "  ^^^^^       ".split(''),
-    "  ^^^^^^^^^^^ ".split(''),
-    "  ^^^^^^      ".split(''),
-    "  ^^^^        ".split('')
-  ]))
+console.log(
+  mazeRunner(
+    [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 3],
+      [1, 0, 1, 0, 1, 0, 1],
+      [0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 1],
+      [1, 2, 1, 0, 1, 0, 1],
+    ],
+    ["N","E","E","E","E"]
+  )
+);
