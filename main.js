04bc6f6cd01ff1d9fd0f0372ea61399a266b5921
Jhonny <gonzodzen@mail.ru>
2023-02-02 14:32:10 +0300

    Bird Mountain

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-const A = [
-    'aardvark',   'alligator', 'armadillo',   'antelope',   'baboon',
-    'bear',       'bobcat',    'butterfly',   'cat',        'camel',
-    'cow',        'chameleon', 'dog',         'dolphin',    'duck',
-    'dragonfly',  'eagle',     'elephant',    'emu',        'echidna',
-    'fish',       'frog',      'flamingo',    'fox',        'goat',
-    'giraffe',    'gibbon',    'gecko',       'hyena',      'hippopotamus',
-    'horse',      'hamster',   'insect',      'impala',     'iguana',
-    'ibis',       'jackal',    'jaguar',      'jellyfish',  'kangaroo',
-    'kiwi',       'koala',     'killerwhale', 'lemur',      'leopard',
-    'llama',      'lion',      'monkey',      'mouse',      'moose',
-    'meercat',    'numbat',    'newt',        'ostrich',    'otter',
-    'octopus',    'orangutan', 'penguin',     'panther',    'parrot',
-    'pig',        'quail',     'quokka',      'quoll',      'rat',
-    'rhinoceros', 'racoon',    'reindeer',    'rabbit',     'snake',
-    'squirrel',   'sheep',     'seal',        'turtle',     'tiger',
-    'turkey',     'tapir',     'unicorn',     'vampirebat', 'vulture',
-    'wombat',     'walrus',    'wildebeast',  'wallaby',    'yak',
-    'zebra'
-  ]
-
-const roadKill = function(p) {
-    if(p==='==a======a=a=a=lig===a=t====o=r=r=r=r==')return '??'
-  console.log(p)
-    const arr = A.slice().map(el=>[...new Set(el)].join``)
-    newP=[...new Set(p.replace(/=/g,''))].join``, nextP=[...new Set(p.replace(/=/g,'').split``.reverse().join``)].join``
-    const ind = arr.indexOf(newP), ind2 = arr.indexOf(nextP)
-    return A[ind]||A[ind2]||'??'
-  }
-
-
-console.log(roadKill("=====k====r=a=vvvv==d=d=d=d=r==a=a======="))
-console.log(roadKill("=======r==ro=o=ot=t==t==a=g=gg=i=l==ll=l===la=a====="))
-console.log(roadKill("======pe====nnnnnn=======================n=n=ng====u==iiii=iii==nn========================n="))
+const peakHeight = (m) => {
+    if(m.length===0)return 0
+    m=m.map(el=>el.map(ul=>ul===' '?0:1))
+    let c = 1, val = true
+    while(val){
+        val=false
+         nm = m.map(el=>el.slice())
+        for(let i = c; i < m.length-c; i++){
+            for(let j = c; j < m[i].length-c; j++){
+                if(m[i][j]>0&&m[i][j]===m[i][j+1]&&m[i][j]===m[i][j-1]&&m[i][j]===m[i+1][j]&&m[i][j]===m[i-1][j]){
+                    nm[i][j]+=1
+                    val=true
+                }
+            }
+        }
+        c++
+        m=nm
+    }return Math.max(...nm.flat())
+}
+console.log(peakHeight([
+    "^^^^^^        ".split(''),
+    " ^^^^^^^^     ".split(''),
+    "  ^^^^^^^     ".split(''),
+    "  ^^^^^       ".split(''),
+    "  ^^^^^^^^^^^ ".split(''),
+    "  ^^^^^^      ".split(''),
+    "  ^^^^        ".split('')
+  ]))
