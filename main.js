f4a3afecf48506abbf42a9c818248b7dbec2c0c9
Jhonny <gonzodzen@mail.ru>
2023-02-03 20:56:14 +0300

    Trim a String

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-const searchArray = (a, q) => {
-    if(a.some(el=>typeof el!=='object')||typeof q!=='object'||q.length!==2||!a.every(el=>el.length===2))throw Error
-    return a.map((el,i)=>[el,i]).filter(el=>el[0].join``===q.join``).map(el=>el[1])[0]||-1
+String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g,'')
   }
-console.log(
-  searchArray([
-    [2, 3],
-    [7, 2],
-    [9, 20],
-    [1, 2],
-    [7, 2],
-    [45, 4],
-    [7, 87],
-    [4, 5],
-    [2, 7],
-    [6, 32],
-  ], [9,20])
-);
+  console.log('     aaa     '.trim())
