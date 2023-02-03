1fdb6f16099bafe7f2b7609c4623a776f73e324d
Jhonny <gonzodzen@mail.ru>
2023-02-03 20:06:02 +0300

    IndexOf Array in Array

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-function pairOfShoes(s) {
-	const arrNull = [], arrOne = []
-    s.forEach(el=>el[0]===0?arrNull.push(el[0]):arrOne.push(el[0]))
-    if(arrNull.length!==arrOne.length)return false
-    s=Object.values(s.map(el=>el[1]).sort((a,b)=>b-a).reduce((a,c)=>{
-        a[c]=(a[c]||0)+1
-        return a
-    },{}))
-    return s.every(el=>el%2===0)
-}
-console.log(pairOfShoes([[0, 21], 
-    [1, 23], 
-    [1, 21], 
-    [1, 23]]))
+const searchArray = (a, q) => {
+    if(a.some(el=>typeof el!=='object')||typeof q!=='object'||q.length!==2||!a.every(el=>el.length===2))throw Error
+    return a.map((el,i)=>[el,i]).filter(el=>el[0].join``===q.join``).map(el=>el[1])[0]||-1
+  }
+console.log(
+  searchArray([
+    [2, 3],
+    [7, 2],
+    [9, 20],
+    [1, 2],
+    [7, 2],
+    [45, 4],
+    [7, 87],
+    [4, 5],
+    [2, 7],
+    [6, 32],
+  ], [9,20])
+);
