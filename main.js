19b5342bc97e4dde175fea7da3fa27cdba73a9fe
Jhonny <gonzodzen@mail.ru>
2023-02-03 19:05:56 +0300

    Simple Fun #52: Pair Of Shoes

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function pairwise(arr, n) {
-    const res = []
-    for(let i = 0; i<arr.length-1;i++){
-      for(let j =i+1; j<arr.length;j++){
-        if(arr[i]+arr[j]===n&&!res.includes(i)&&!res.includes(j)){
-          res.push(i,j)
-          break
-          }
-      }
-    }
-    return res.reduce((a,c)=>a+c,0)
-  }
+function pairOfShoes(s) {
+	const arrNull = [], arrOne = []
+    s.forEach(el=>el[0]===0?arrNull.push(el[0]):arrOne.push(el[0]))
+    if(arrNull.length!==arrOne.length)return false
+    s=Object.values(s.map(el=>el[1]).sort((a,b)=>b-a).reduce((a,c)=>{
+        a[c]=(a[c]||0)+1
+        return a
+    },{}))
+    return s.every(el=>el%2===0)
+}
+console.log(pairOfShoes([[0, 21], 
+    [1, 23], 
+    [1, 21], 
+    [1, 23]]))
