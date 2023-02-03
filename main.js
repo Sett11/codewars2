672bbc52aac39bc419339d351682a142a9d1be42
Jhonny <gonzodzen@mail.ru>
2023-02-03 18:38:03 +0300

    Simple Fun #162: Pair Wise

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function compoundArray(a, b) {
-    arr=[]
-    for(i=0;i<Math.max(a.length,b.length);i++){
-    arr.push(a[i])
-      arr.push(b[i])
+function pairwise(arr, n) {
+    const res = []
+    for(let i = 0; i<arr.length-1;i++){
+      for(let j =i+1; j<arr.length;j++){
+        if(arr[i]+arr[j]===n&&!res.includes(i)&&!res.includes(j)){
+          res.push(i,j)
+          break
+          }
+      }
     }
-    return arr.filter(el=>el!==undefined)
+    return res.reduce((a,c)=>a+c,0)
   }
