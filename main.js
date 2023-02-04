6dd35bbc513bd6220546fc35417066f0ea83a03d
Jhonny <gonzodzen@mail.ru>
2023-02-04 17:00:33 +0300

    Pete's inappropriate speech

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-String.prototype.trim = function() {
-    return this.replace(/^\s+|\s+$/g,'')
+function peteTalk(s,o=[]){
+  s=s.replace(/\W+/g,' $&').split` `.filter(e=>e!==''), o=o.map(e=>e.toLowerCase())
+  for(let i = 0; i < s.length; i++){
+    if(s[i].replace(/\W_/g,'').length<3||o.includes(s[i].toLowerCase())){
+      s[i]=s[i].toLowerCase()
+      continue
+    }
+    if(i===0&&o.includes(s[i].toLowerCase())&&s[i].match(/\*/g)){
+      s[i]=s[i].charAt(0).toUpperCase()+s.slice(1).toLowerCase()
+    }
+    if(i===0&&!o.includes(s[i].toLowerCase())&&s[i].replace(/\W_/g,'').length>2){
+      s[i]=s[i].charAt(0).toUpperCase()+'*'.repeat(s[i].length-2)+s[i].charAt(s[i].length-1)
+    }
+    if(i!==0&&!o.includes(s[i].toLowerCase())&&s[i].replace(/\W_/g,'').length>2&&s[i-1]!=='.'||s[i-1]!=='?'||s[i-1]!=='!'){
+      s[i]=s[i].charAt(0).toLowerCase()+'*'.repeat(s[i].length-2)+s[i].charAt(s[i].length-1).toLowerCase()
+    }
+    if(i!==0&&!o.includes(s[i].toLowerCase())&&s[i].replace(/\W_/g,'').length>2&&s[i-1]=='.'||s[i-1]=='?'||s[i-1]=='!'){
+      s[i]=s[i].charAt(0).toUpperCase()+'*'.repeat(s[i].length-2)+s[i].charAt(s[i].length-1).toLowerCase()
+    }
   }
-  console.log('     aaa     '.trim())
+  for(let i = 1; i < s.length; i++){
+    if(s[i]===s[i].toLowerCase()&&s[i-1]==='.'||s[i-1]==='?'||s[i-1]==='!'){
+      s[i]=s[i].charAt(0).toUpperCase()+s[i].slice(1)
+    }
+  }
+  s=s.join` `.replace(/ (\W)/g,'$1')
+  return s.charAt(0).toUpperCase()+s.slice(1)
+}
+console.log(peteTalk('FROM couch ALCOHOL eat over:'))
+console.log(peteTalk("I want to punch someone in the face",["someone","face"]))
+console.log(peteTalk('What the hell am I doing here? And where is my wallet? PETE SMASH!'))
