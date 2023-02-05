27204d6a41fba3b60dfae5ff017c00e688aa7f2f
Jhonny <gonzodzen@mail.ru>
2023-02-06 01:36:00 +0300

    Deep comparison of objects

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-Array.prototype.each = function(fn){
-  for(let i = 0; i < this.length; i++){
-    if(fn(this[i],i))break
-  }
+const deepCompare=(o1, o2)=>{
+  console.log(o1,o2)
+  if(o1===null||o2===null)return o1===o2
+  if(typeof o1!=='object'||typeof o2!=='object'||typeof o1==='boolean'||typeof o2==='boolean')return o1===o2
+  if(typeof o1==='object'&&o1.length||typeof o2==='object'&&o2.length)return o1.join``===o2.join``
+  const one = Object.entries(o1).flat().sort().join``, two = Object.entries(o2).flat().sort().join``
+  return one===two
 }
+console.log(deepCompare({ name: 'Joe', surname: 'Smith' }, { surname: 'Smith', name: 'Joe' }))
