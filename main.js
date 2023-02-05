7d24f16188b3cd5a108c7041bdbdd3d55a895aaf
Jhonny <gonzodzen@mail.ru>
2023-02-05 19:10:53 +0300

    [Code Golf] Get the Square of a Number without ** or * or pow()

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
-class FileMaster {
-  constructor(filepath) {
-    this.filepath=filepath
-  }
-  
-  extension() {
-    return this.filepath.match(/\..+$/g)[0].replace(/\./,'')
-  }
-  
-  filename() {
-    return this.filepath.replace(/\..+$/,'').replace(/.+\//,'')
-  }
-  
-  dirpath() {
-    return this.filepath.replace(/\..+$/,'').replace(/.+\//,'$& ').split` `[0]
-  }
-}
-const r = new FileMaster('/Users/person1/Pictures/house.png')
-console.log(r.dirpath())
+square=n=>n/(1/n)
+console.log(square(9))
