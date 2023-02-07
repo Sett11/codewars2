2fbe298b23075101c2e094684d8f6c35852476ff
Jhonny <gonzodzen@mail.ru>
2023-02-07 21:38:55 +0300

    Robotic Tattoo Removal

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function encryptor (k, m) {
-    if(k===0)return m
-    const letters = 'abcdefghijklmnopqrstuvwxyz'
-	return m.split``.map(e=>!letters.includes(e.toLowerCase())?e:e===e.toUpperCase()?[((parseInt(e,36)-10)+k)%26,'U']:[((parseInt(e,36)-10)+k)%26,'L'])
-     .map(el=>{
-         if(el[0]<0)el[0]=26+el[0]
-         if(el[1]==='U')el=letters[el[0]].toUpperCase()
-         if(el[1]==='L')el=letters[el[0]]
-         return el
-     }).join``
+function robot(s) {
+    return s.map(el=>el.map(ul=>ul==='X'?ul='*':ul))
 }
-console.log(encryptor(-5, 'Hello World!'))
