7b3380d8267644650380f388e430c0fe1e04a6b0
Jhonny <gonzodzen@mail.ru>
2023-02-08 17:19:23 +0300

    Create Four Letter Birding Codes from Bird Names

--- main.js
+++ main.js
@@ -1,60 +1,18 @@
-function wordMesh(a) {
-  console.log(a);
-  const arr = [];
-  for (let i = 0; i < a.length - 1; i++) {
-    if (a[i] === a[i + 1]) {
-        arr.push(a[i]);
-        i+=1
-      }
-    for (let j = 0; j < a[i].length; j++) {
-      if (a[i + 1].slice(0, a[i + 1].length / 2 + 1).match(a[i].slice(j))) {
-        if (a[i].slice(j).length>1&&a[i].slice(j)[0] === a[i + 1][0] &&a[i].slice(j)[1] === a[i + 1][1]){
-            arr.push(a[i].slice(j))
-            break
-        }
-          if(a[i].slice(j).length===1&&a[i].slice(j)===a[i+1][0]){
-            arr.push(a[i].slice(j))
-            break
-          }
-      }
-    }
-  }return arr.length === a.length - 1 ? arr.join`` : "failed to mesh";
+function birdCode(arr){
+    return arr.map(e=>{
+        e=e.replace(/-/g,' ').split` `
+        if(e.length===1)e=(e[0].slice(0,4)+'').toUpperCase()
+        if(e.length===2)e=(e[0].slice(0,2)+e[1].slice(0,2)).toUpperCase()
+        if(e.length===3)e=(e[0].charAt(0)+e[1].charAt(0)+e[2].slice(0,2)).toUpperCase()
+        if(e.length===4)e=(e[0].charAt(0)+e[1].charAt(0)+e[2].charAt(0)+e[3].charAt(0)).toUpperCase()
+        return e
+    })
 }
-console.log(wordMesh(["abandon", "donation", "onion", "ongoing"]));
-console.log(
-  wordMesh([
-    "chlorine",
-    "brinemore",
-    "oregon",
-    "gonzaga",
-    "gambino",
-    "inoculate",
-  ])
-);
-console.log(
-  wordMesh([
-    "bookpencil",
-    "pencilpapertypewritercomputer",
-    "computersoftwarehardware",
-    "warehousestoragecrates",
-    "ratespricesdiscount",
-    "ratespricesdiscount",
-    "counterintuitive",
-    "vertigofallinghighup",
-  ])
-);
-console.log(
-  wordMesh([
-    "pneumonia",
-    "niacin",
-    "cinnamon",
-    "monotheist",
-    "sisterhood",
-    "oddity",
-    "typecast",
-    "castle",
-    "lenient",
-    "entrapment",
-  ])
-);
-console.log(wordMesh(["golden", "nebula", "landfall", "llama", "amanda"]));
+console.log(birdCode(["Common Tern", "Black-Capped Chickadee"]))
+console.log(birdCode([ 'American Redstart',
+'Northern Cardinal',
+'Pine Grosbeak',
+'Barred Owl',
+'Starling',
+'Cooper\'s Hawk',
+'Pigeon' ]))
