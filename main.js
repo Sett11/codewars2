632cb13035ed1d5da54551876c748c39baf4b644
Jhonny <gonzodzen@mail.ru>
2023-02-08 17:01:21 +0300

    Word Mesh

--- main.js
+++ main.js
@@ -1,148 +1,60 @@
-function connectFour(b) {
-    console.log(b)
-    b=b.map(e=>{
-        e.push(0,0,0,0)
-        e.unshift(0,0,0,0)
-        return e
-    })
-    b.push(Array(b[0].length+5).fill(0)),b.push(Array(b[0].length+5).fill(0)),b.push(Array(b[0].length+5).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0))
-    for(let i = 0; i < b.length; i++){
-        for(let j = 0; j < b[i].length; j++){
-                if(b[i][j]==='R'&&b[i][j+1]==='R'&&b[i][j+2]==='R'&&b[i][j+3]==='R'){
-                    return 'R'
-                }
-                if(b[i][j]==='R'&&b[i+1][j]==='R'&&b[i+2][j]==='R'&&b[i+3][j]==='R'){
-                    return 'R'
-                }
-                if(b[i][j]==='R'&&b[i+1][j+1]==='R'&&b[i+2][j+2]==='R'&&b[i+3][j+3]==='R'){
-                    return 'R'
-                }
-                if(b[i][j]==='R'&&b[i-1][j-1]==='R'&&b[i-2][j-2]==='R'&&b[i-3][j-3]==='R'){
-                    return 'R'
-                }
-                if(b[i][j]==='R'&&b[i-1][j+1]==='R'&&b[i-2][j+2]==='R'&&b[i-3][j+3]==='R'){
-                    return 'R'
-                }
-
-                if(b[i][j]==='Y'&&b[i][j+1]==='Y'&&b[i][j+2]==='Y'&&b[i][j+3]==='Y'){
-                    return 'Y'
-                }
-                if(b[i][j]==='Y'&&b[i+1][j]==='Y'&&b[i+2][j]==='Y'&&b[i+3][j]==='Y'){
-                    return 'Y'
-                }
-                if(b[i][j]==='Y'&&b[i+1][j+1]==='Y'&&b[i+2][j+2]==='Y'&&b[i+3][j+3]==='Y'){
-                    return 'Y'
-                }
-                if(b[i][j]==='Y'&&b[i-1][j-1]==='Y'&&b[i-2][j-2]==='Y'&&b[i-3][j-3]==='Y'){
-                    return 'Y'
-                }
-                if(b[i][j]==='Y'&&b[i-1][j+1]==='Y'&&b[i-2][j+2]==='Y'&&b[i-3][j+3]==='Y'){
-                    return 'Y'
-                }
+function wordMesh(a) {
+  console.log(a);
+  const arr = [];
+  for (let i = 0; i < a.length - 1; i++) {
+    if (a[i] === a[i + 1]) {
+        arr.push(a[i]);
+        i+=1
+      }
+    for (let j = 0; j < a[i].length; j++) {
+      if (a[i + 1].slice(0, a[i + 1].length / 2 + 1).match(a[i].slice(j))) {
+        if (a[i].slice(j).length>1&&a[i].slice(j)[0] === a[i + 1][0] &&a[i].slice(j)[1] === a[i + 1][1]){
+            arr.push(a[i].slice(j))
+            break
         }
+          if(a[i].slice(j).length===1&&a[i].slice(j)===a[i+1][0]){
+            arr.push(a[i].slice(j))
+            break
+          }
+      }
     }
-    return b.flat().includes('-')?'in progress':'draw'
+  }return arr.length === a.length - 1 ? arr.join`` : "failed to mesh";
 }
-  console.log(connectFour(
- [['-','-','-','-','-','-','-'],
-  ['-','-','-','-','-','-','-'],
-  ['-','-','-','R','R','R','R'],
-  ['-','-','-','Y','Y','R','Y'],
-  ['-','-','-','Y','R','Y','Y'],
-  ['-','-','Y','Y','R','R','R']]))
-  console.log(connectFour( [
-    [
-      '-', '-', '-',
-      '-', '-', '-',
-      '-'
-    ],
-    [
-      '-', '-', '-',
-      '-', '-', '-',
-      '-'
-    ],
-    [
-      '-', '-', '-',
-      '-', 'Y', '-',
-      '-'
-    ],
-    [
-      '-', '-', '-',
-      'Y', 'R', '-',
-      '-'
-    ],
-    [
-      '-', 'R', 'Y',
-      'R', 'Y', '-',
-      '-'
-    ],
-    [
-      'R', 'Y', 'R',
-      'Y', 'R', 'Y',
-      'R'
-    ]
-  ]))
-  console.log(connectFour( [
-    [
-      'Y', 'R', 'Y',
-      'R', 'Y', 'R',
-      'Y'
-    ],
-    [
-      'R', 'Y', 'R',
-      'R', 'Y', 'R',
-      'Y'
-    ],
-    [
-      'Y', 'Y', 'R',
-      'R', 'R', 'Y',
-      'R'
-    ],
-    [
-      'R', 'R', 'Y',
-      'Y', 'Y', 'R',
-      'Y'
-    ],
-    [
-      'Y', 'Y', 'Y',
-      'R', 'Y', 'R',
-      'Y'
-    ],
-    [
-      'R', 'Y', 'R',
-      'R', 'R', 'Y',
-      'R'
-    ]
-  ]))
-  console.log(connectFour([
-    [
-      '-', '-', '-',
-      '-', '-', '-',
-      '-'
-    ],
-    [
-      '-', '-', '-',
-      '-', '-', '-',
-      '-'
-    ],
-    [
-      '-', 'R', '-',
-      '-', '-', '-',
-      '-'
-    ],
-    [
-      '-', 'R', 'Y',
-      '-', '-', '-',
-      '-'
-    ],
-    [
-      '-', 'R', 'Y',
-      'Y', 'Y', 'Y',
-      '-'
-    ],
-    [
-      '-', 'Y', 'R',
-      'R', 'Y', 'R',
-      '-'
-    ]
-  ]))
+console.log(wordMesh(["abandon", "donation", "onion", "ongoing"]));
+console.log(
+  wordMesh([
+    "chlorine",
+    "brinemore",
+    "oregon",
+    "gonzaga",
+    "gambino",
+    "inoculate",
+  ])
+);
+console.log(
+  wordMesh([
+    "bookpencil",
+    "pencilpapertypewritercomputer",
+    "computersoftwarehardware",
+    "warehousestoragecrates",
+    "ratespricesdiscount",
+    "ratespricesdiscount",
+    "counterintuitive",
+    "vertigofallinghighup",
+  ])
+);
+console.log(
+  wordMesh([
+    "pneumonia",
+    "niacin",
+    "cinnamon",
+    "monotheist",
+    "sisterhood",
+    "oddity",
+    "typecast",
+    "castle",
+    "lenient",
+    "entrapment",
+  ])
+);
+console.log(wordMesh(["golden", "nebula", "landfall", "llama", "amanda"]));
