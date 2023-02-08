6c34cae46d32bbf862dbbd7d45261d95df72933c
Jhonny <gonzodzen@mail.ru>
2023-02-08 12:58:52 +0300

    Connect Four: Who Won?

--- main.js
+++ main.js
@@ -1,15 +1,148 @@
-function findRarestPepe(p) {
-  p=Object.entries(p.reduce((a,c)=>{
-    a[c]=(a[c]||0)+1
-    return a
-  },{})).sort((a,b)=>a[1]-b[1]),unic=0
-  uniq=p[0][1],res=p.filter(e=>e[1]===uniq).map(e=>e[0]).sort()
-  return uniq<5&&res.length===1?res.join``:uniq<5&&res.length>1?res:'No rare pepes!'
+function connectFour(b) {
+    console.log(b)
+    b=b.map(e=>{
+        e.push(0,0,0,0)
+        e.unshift(0,0,0,0)
+        return e
+    })
+    b.push(Array(b[0].length+5).fill(0)),b.push(Array(b[0].length+5).fill(0)),b.push(Array(b[0].length+5).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0)),b.unshift(Array(b[b.length-1].length).fill(0))
+    for(let i = 0; i < b.length; i++){
+        for(let j = 0; j < b[i].length; j++){
+                if(b[i][j]==='R'&&b[i][j+1]==='R'&&b[i][j+2]==='R'&&b[i][j+3]==='R'){
+                    return 'R'
+                }
+                if(b[i][j]==='R'&&b[i+1][j]==='R'&&b[i+2][j]==='R'&&b[i+3][j]==='R'){
+                    return 'R'
+                }
+                if(b[i][j]==='R'&&b[i+1][j+1]==='R'&&b[i+2][j+2]==='R'&&b[i+3][j+3]==='R'){
+                    return 'R'
+                }
+                if(b[i][j]==='R'&&b[i-1][j-1]==='R'&&b[i-2][j-2]==='R'&&b[i-3][j-3]==='R'){
+                    return 'R'
+                }
+                if(b[i][j]==='R'&&b[i-1][j+1]==='R'&&b[i-2][j+2]==='R'&&b[i-3][j+3]==='R'){
+                    return 'R'
+                }
+
+                if(b[i][j]==='Y'&&b[i][j+1]==='Y'&&b[i][j+2]==='Y'&&b[i][j+3]==='Y'){
+                    return 'Y'
+                }
+                if(b[i][j]==='Y'&&b[i+1][j]==='Y'&&b[i+2][j]==='Y'&&b[i+3][j]==='Y'){
+                    return 'Y'
+                }
+                if(b[i][j]==='Y'&&b[i+1][j+1]==='Y'&&b[i+2][j+2]==='Y'&&b[i+3][j+3]==='Y'){
+                    return 'Y'
+                }
+                if(b[i][j]==='Y'&&b[i-1][j-1]==='Y'&&b[i-2][j-2]==='Y'&&b[i-3][j-3]==='Y'){
+                    return 'Y'
+                }
+                if(b[i][j]==='Y'&&b[i-1][j+1]==='Y'&&b[i-2][j+2]==='Y'&&b[i-3][j+3]==='Y'){
+                    return 'Y'
+                }
+        }
+    }
+    return b.flat().includes('-')?'in progress':'draw'
 }
-console.log(findRarestPepe(['Donald Trump Pepe',
-'Melania Trump Pepe',
-'Melania Trump Pepe',
-'Clown Pepe',
-'Clown Pepe',
-'Clown Pepe',
-]))
+  console.log(connectFour(
+ [['-','-','-','-','-','-','-'],
+  ['-','-','-','-','-','-','-'],
+  ['-','-','-','R','R','R','R'],
+  ['-','-','-','Y','Y','R','Y'],
+  ['-','-','-','Y','R','Y','Y'],
+  ['-','-','Y','Y','R','R','R']]))
+  console.log(connectFour( [
+    [
+      '-', '-', '-',
+      '-', '-', '-',
+      '-'
+    ],
+    [
+      '-', '-', '-',
+      '-', '-', '-',
+      '-'
+    ],
+    [
+      '-', '-', '-',
+      '-', 'Y', '-',
+      '-'
+    ],
+    [
+      '-', '-', '-',
+      'Y', 'R', '-',
+      '-'
+    ],
+    [
+      '-', 'R', 'Y',
+      'R', 'Y', '-',
+      '-'
+    ],
+    [
+      'R', 'Y', 'R',
+      'Y', 'R', 'Y',
+      'R'
+    ]
+  ]))
+  console.log(connectFour( [
+    [
+      'Y', 'R', 'Y',
+      'R', 'Y', 'R',
+      'Y'
+    ],
+    [
+      'R', 'Y', 'R',
+      'R', 'Y', 'R',
+      'Y'
+    ],
+    [
+      'Y', 'Y', 'R',
+      'R', 'R', 'Y',
+      'R'
+    ],
+    [
+      'R', 'R', 'Y',
+      'Y', 'Y', 'R',
+      'Y'
+    ],
+    [
+      'Y', 'Y', 'Y',
+      'R', 'Y', 'R',
+      'Y'
+    ],
+    [
+      'R', 'Y', 'R',
+      'R', 'R', 'Y',
+      'R'
+    ]
+  ]))
+  console.log(connectFour([
+    [
+      '-', '-', '-',
+      '-', '-', '-',
+      '-'
+    ],
+    [
+      '-', '-', '-',
+      '-', '-', '-',
+      '-'
+    ],
+    [
+      '-', 'R', '-',
+      '-', '-', '-',
+      '-'
+    ],
+    [
+      '-', 'R', 'Y',
+      '-', '-', '-',
+      '-'
+    ],
+    [
+      '-', 'R', 'Y',
+      'Y', 'Y', 'Y',
+      '-'
+    ],
+    [
+      '-', 'Y', 'R',
+      'R', 'Y', 'R',
+      '-'
+    ]
+  ]))
