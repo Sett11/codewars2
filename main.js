49541badaf5f5f7d9e9990bf2302c8f5ade88bfb
Jhonny <gonzodzen@mail.ru>
2023-02-14 21:35:53 +0300

    Array Mash

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function minDistance(n){
-    console.log(n)
-      const arr=[],res=[]
-      for(let i = 1; i < n/2+1; i++){
-          if(n%i===0)arr.push(i)
-      }
-      arr.push(n)
-      for(let i = 0; i < arr.length-1; i++){
-          res.push(arr[i+1]-arr[i])
-      }return Math.min(...res)
-  }
-console.log(minDistance(17))
+function arrayMash (a1, a2, arr=[]) {
+   for(let i = 0; i < a1.length; i++){arr.push(a1[i]);arr.push(a2[i])}return arr
+}
+  console.log(arrayMash([1,2,3],['a','b','c'] ))
