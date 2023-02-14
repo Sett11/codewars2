c6b38f961a5a68fa38a563eb85f45e7f29ebdb46
Jhonny <gonzodzen@mail.ru>
2023-02-15 02:54:22 +0300

    Caesar Cipher Helper

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-function isSolved(b) {
-    let count=3
-   while(count){
-    for(let i = 0; i < b.length-2; i++){
-        for(let j = 0; j < b.length-2; j++){
-            if(b[i][j]===0&&b[i][j+1]===1&&b[i][j+2]===1||b[i][j]===0&&b[i+1][j]===1&&b[i+2][j]===1||b[i][j]===0&&b[i+1][j+1]===1&&b[i+2][j+2]===1 || b[i][j]===0&&b[i][j+1]===2&&b[i][j+2]===2||b[i][j]===0&&b[i+1][j]===2&&b[i+2][j]===2||b[i][j]===0&&b[i+1][j+1]===2&&b[i+2][j+2]===2)return -1
-            if(b[i][j]===1&&b[i][j+1]===1&&b[i][j+2]===1||b[i][j]===1&&b[i+1][j]===1&&b[i+2][j]===1||b[i][j]===1&&b[i+1][j+1]===1&&b[i+2][j+2]===1)return 1
-            if(b[i][j]===2&&b[i][j+1]===2&&b[i][j+2]===2||b[i][j]===2&&b[i+1][j]===2&&b[i+2][j]===2||b[i][j]===2&&b[i+1][j+1]===2&&b[i+2][j+2]===2)return 2
+class CaesarCipher{
+    constructor(n,smLett='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz',bgLett='ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'){
+        this.n=n
+        this.a=smLett
+        this.b=bgLett
+    }
+        encode(s){
+            return s.split``.map(e=>this.a.includes(e)?this.a[this.a.indexOf(e)+this.n]:this.b.includes(e)?this.b[this.b.indexOf(e)+this.n]:e).join``.toUpperCase()
         }
-    }b=b.map(e=>e.reverse()).reverse()
-    count--
-   }return b.some(e=>e.some(u=>u===0))?-1:0
-}
-  console.log(isSolved(
-   [[0,0,1],
-    [0,1,2],
-    [2,1,0]]))
+        decode(s){
+            return s.split``.map(e=>this.a.includes(e)?this.a[this.a.lastIndexOf(e)-this.n]:this.b.includes(e)?this.b[this.b.lastIndexOf(e)-this.n]:e).join``.toUpperCase()
+        }
+  }
+  console.log(new CaesarCipher(5).decode('BFKKQJX'))
