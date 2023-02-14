a0ff7364a70cf902204173942cf2a6c535f8ffe8
Jhonny <gonzodzen@mail.ru>
2023-02-14 17:01:07 +0300

    Fizz Buzz

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-f=
-''
-[
-'\
-t\
-r\
-i\
-m'
-][
-'\
-b\
-i\
-n\
-d'
-]`
-H\
-e\
-l\
-l\
-o\
-,\
- \
-w\
-o\
-r\
-l\
-d\
-!`
+function fizzbuzz(n){
+    const arr=[]
+    for(let i = 1; i <= n; i++){
+    arr.push(i)
+   }
+   return arr.map(e=>e%3===0&&e%5===0?'FizzBuzz':e%3===0?'Fizz':e%5===0?'Buzz':e)
+}
+console.log(fizzbuzz(10))
