a2cf4aac566d073fe484e039349b5bff8555cdf0
Jhonny <gonzodzen@mail.ru>
2023-02-14 21:38:39 +0300

    Array plus array

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-function arrayMash (a1, a2, arr=[]) {
-   for(let i = 0; i < a1.length; i++){arr.push(a1[i]);arr.push(a2[i])}return arr
-}
-  console.log(arrayMash([1,2,3],['a','b','c'] ))
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((a,c)=>a+c,0)
+  }
