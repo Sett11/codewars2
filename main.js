9f597e3dfafb4c4c489976f60a6beb0cd0de445a
Jhonny <gonzodzen@mail.ru>
2023-02-15 01:05:23 +0300

    up AND down

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-function superStreetFighterSelection(fighters, position, moves){
-    let arr=[],vertCount=position[0],gorizCount=position[1]
-    for(let i = 0; i < moves.length; i++){
-        if(moves[i]==='up'&&vertCount!==0)vertCount--
-        if(moves[i]==='down'&&vertCount!==fighters.length-1)vertCount++
-        if(moves[i]==='left')gorizCount--
-        if(gorizCount<0)gorizCount=fighters[0].length-1
-        if(moves[i]==='right')gorizCount++
-        if(gorizCount>fighters[0].length-1)gorizCount=0
-        let tmp=1
-        if(moves[i]==='up'&&!fighters[vertCount][gorizCount])vertCount++
-        if(moves[i]==='down'&&!fighters[vertCount][gorizCount])vertCount--
-        if(moves[i]==='left'&&!fighters[vertCount][gorizCount]||moves[i]==='right'&&!fighters[vertCount][gorizCount])i--
-        while(tmp){arr.push(fighters[vertCount][gorizCount]);tmp--}
+function arrange(s) {
+    s=s.split` `
+    for(let i = 1; i < s.length-1; i++){
+        let tmp=''
+        if(i%2!==0&&s[i].length<s[i-1].length){
+            tmp=s[i]
+            s[i]=s[i-1]
+            s[i-1]=tmp
+            i-=1
+        }
+        if(i%2===0&&s[i].length>s[i+1].length){
+            tmp=s[i+1]
+            s[i+1]=s[i]
+            s[i]=tmp
+        }
+        if(i%2!==0&&s[i].length<s[i+1].length){
+            tmp=s[i]
+            s[i]=s[i+1]
+            s[i+1]=tmp
+            i-=1
+        }
     }
-    return arr.filter(e=>e)
+    return s.map((e,i)=>i%2!==0?e.toUpperCase():e.toLowerCase()).join` `
 }
-
-  console.log(superStreetFighterSelection(
-    [
-        [        "",     "Ryu",  "E.Honda",  "Cammy" ],
-        [  "Balrog",     "Ken",  "Chun Li",       "" ],
-        [    "Vega",        "", "Fei Long", "Balrog",],
-        [  "Blanka",   "Guile",         "", "Chun Li"],
-        [ "M.Bison", "Zangief",  "Dhalsim", "Sagat"  ],
-        [  "Deejay",   "Cammy",         "", "T.Hawk" ]
-    ], [0,3], ["left","left","down","right","right","right","right","down","left","left","left","left","down","right","right", "down","right","right","right","down","left","left","left","down","left","left","left"]))
+console.log(arrange("way the my wall them him"))
