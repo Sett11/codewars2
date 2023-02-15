ad438eae8ae72f3ff0da8c3858f7dec8d3789f6d
Jhonny <gonzodzen@mail.ru>
2023-02-15 21:53:09 +0300

    Compare section numbers

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function compareIntegers(a, b) {
-    if(a.match(/\-/g)&&b.match(/\-/g))return parseFloat(a)>parseFloat(b)?'greater':parseFloat(a)<parseFloat(b)?'less':'equal'
-      if(a.length<b.length)a='0'.repeat(b.length-a.length)+a
-      if(a.length>b.length)b='0'.repeat(a.length-b.length)+b
-      for(let i = 0; i < Math.max(a.length,b.length); i++){
-          if(a[i]<b[i])return 'less'
-          if(a[i]>b[i])return 'greater'
-      }return 'equal'
-  }
+function cmp(a, b) {
+  console.log(a, b)
+  a=a.split`.`.map(Number), b=b.split`.`.map(Number),c=0
+  if(a.length<b.length)c=b.length-a.length;while(c){a.push(0);c--}
+  if(b.length<a.length)c=a.length-b.length;while(c){b.push(0);c--}
+  for(let i = 0; i < Math.max(a.length,b.length); i++){if(a[i]>b[i])return 1;if(b[i]>a[i])return -1}
+  return 0
+}
+console.log(cmp('1.20', '1.5'));
