e1daec1b7eeb778018b906bea5d8e997e1bad642
Jhonny <gonzodzen@mail.ru>
2023-02-15 10:41:32 +0300

    First Variation on Caesar Cipher

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-function caesarEncode(p, s) {
-    s=s%26,smLett='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz',p=p.split``
-    for(let i = 0; i < p.length; i++){
-        if(p[i]===' ')s=(s+1)%26
-        if(smLett.includes(p[i]))p[i]=smLett[smLett.indexOf(p[i])+s]
+function movingShift(s, h) {
+    console.log(s,h)
+    const letters = 'abcdefghijklmnopqrstuvwxyz', lettersUp=letters.toUpperCase()
+    s=s.split``
+    for(let i = 0; i < s.length; i++){
+        if(!letters.includes(s[i])&&!lettersUp.includes(s[i]))continue
+        if(letters.includes(s[i]))s[i]=letters[(letters.indexOf(s[i])+h+i)%26]
+        if(lettersUp.includes(s[i]))s[i]=lettersUp[(lettersUp.indexOf(s[i])+h+i)%26]
     }
-    return p.join``
+    let chanc = Math.ceil(s.join``.length/5),arr=[]
+    for(let i = 0; i < s.length; i+=chanc){
+        arr.push(s.slice(i,i+chanc))
+    }
+    arr=arr.map(e=>e.join``)
+    if(arr.length===4)arr.push('')
+    return arr
 }
-console.log(caesarEncode("alea iacta est", 3))
+function demovingShift(a, h) {
+    console.log(a,h)
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.split``.reverse().join``, lettersUp=letters.toUpperCase()
+    a=a.join``.split``
+    for(let i = 0; i < a.length; i++){
+        if(!letters.includes(a[i])&&!lettersUp.includes(a[i]))continue
+        if(letters.includes(a[i]))a[i]=letters[(letters.indexOf(a[i])+h+i)%26]
+        if(lettersUp.includes(a[i]))a[i]=lettersUp[(lettersUp.indexOf(a[i])+h+i)%26]
+    }
+    return a.join``
+}
+
+console.log(demovingShift([ 'P feuzhqw!', ' yl rqgltc', 'i! mtr hhe', 'wlbt dcuc ', 'xi vhhz;' ],1))
