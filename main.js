cfac17a55e06e562c045c0c85cb2492756a75d28
Jhonny <gonzodzen@mail.ru>
2023-02-15 17:51:25 +0300

    Jungerstein's Math Training Room: 1. How many zeros are at the end of n!! ?

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function move_zeros(arrNum, isRight=true){
-    return isRight?arrNum.filter(e=>e!==0).concat(arrNum.filter(e=>e===0)):arrNum.filter(e=>e===0).concat(arrNum.filter(e=>e!==0))
-  }
+function countZeros(n) {
+    arr=[],c=n%2===0?2:1
+    while(c<=n){arr.push(c);c+=2}
+    const res=(arr.reduce((a,c)=>BigInt(a)*BigInt(c))+'').match(/0+$/g)
+    return res?res[0].length:0
+}
+  console.log(countZeros(30))
