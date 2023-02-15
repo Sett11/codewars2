36cd5a27108f2f07a00fc54db0bc4284022e92f6
Jhonny <gonzodzen@mail.ru>
2023-02-15 15:18:15 +0300

    Simple reversal game

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function solve(eq){
-   return eq.replace(/\D/g,' $& ').split` `.reverse().filter(e=>e!==' ').join``
+function solve(a,b){
+   arr=[],res=[],c=0,z=0
+   while(c<a){arr.push(c);c++}
+   while(z<arr.length-1){
+    arr=arr.slice(0,z).concat((arr.slice(z).reverse()))
+    res.push(arr),z++
+   }return res[res.length-1].indexOf(b)
 }
-console.log(solve("100*b/y"))
+console.log(solve(20,8))
