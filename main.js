7f63cccacf55397df54018ecc8454284aa1c8e9d
Jhonny <gonzodzen@mail.ru>
2023-02-15 03:08:53 +0300

    Caesar Cipher Encryption - Variation

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-class CaesarCipher{
-    constructor(n,smLett='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz',bgLett='ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'){
-        this.n=n
-        this.a=smLett
-        this.b=bgLett
+function caesarEncode(p, s) {
+    s=s%26,smLett='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz',p=p.split``
+    for(let i = 0; i < p.length; i++){
+        if(p[i]===' ')s=(s+1)%26
+        if(smLett.includes(p[i]))p[i]=smLett[smLett.indexOf(p[i])+s]
     }
-        encode(s){
-            return s.split``.map(e=>this.a.includes(e)?this.a[this.a.indexOf(e)+this.n]:this.b.includes(e)?this.b[this.b.indexOf(e)+this.n]:e).join``.toUpperCase()
-        }
-        decode(s){
-            return s.split``.map(e=>this.a.includes(e)?this.a[this.a.lastIndexOf(e)-this.n]:this.b.includes(e)?this.b[this.b.lastIndexOf(e)-this.n]:e).join``.toUpperCase()
-        }
-  }
-  console.log(new CaesarCipher(5).decode('BFKKQJX'))
+    return p.join``
+}
+console.log(caesarEncode("alea iacta est", 3))
