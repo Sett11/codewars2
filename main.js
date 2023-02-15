5b6e7ed83cfcae8a2bfe83920f91f64d3fa1c330
Jhonny <gonzodzen@mail.ru>
2023-02-15 17:35:33 +0300

    Move Zeros

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function moveZeros(a) {
-    const arr=[], zeroArr=[]
-    a.forEach(e=>e!==0?arr.push(e):zeroArr.push(e))
-    return arr.concat(zeroArr)
-}
-  console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
+function move_zeros(arrNum, isRight=true){
+    return isRight?arrNum.filter(e=>e!==0).concat(arrNum.filter(e=>e===0)):arrNum.filter(e=>e===0).concat(arrNum.filter(e=>e!==0))
+  }
