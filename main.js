9a9ebe19d1c304eba40caedec02af00e1cdce1ca
Jhonny <gonzodzen@mail.ru>
2023-02-15 22:03:25 +0300

    Maximum and minimum

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function cmp(a, b) {
-  console.log(a, b)
-  a=a.split`.`.map(Number), b=b.split`.`.map(Number),c=0
-  if(a.length<b.length)c=b.length-a.length;while(c){a.push(0);c--}
-  if(b.length<a.length)c=a.length-b.length;while(c){b.push(0);c--}
-  for(let i = 0; i < Math.max(a.length,b.length); i++){if(a[i]>b[i])return 1;if(b[i]>a[i])return -1}
-  return 0
+function max(){
+    res=[...arguments].flat(100)
+    return res.length===0?0:res.some(e=>+e!==+e)?NaN:+res.sort((a,b)=>b-a)[0]
 }
-console.log(cmp('1.20', '1.5'));
+function min(){
+    res=[...arguments].flat(100)
+    return res.length===0?0:res.some(e=>+e!==+e)?NaN:+res.sort((a,b)=>a-b)[0]
+}
+console.log(min(1,2,[3,[4]]))
