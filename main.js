15b38a17b27c9fd4e35ffa5a7b8f1abef83e6035
Jhonny <gonzodzen@mail.ru>
2023-02-16 01:09:56 +0300

    Prime Word

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function checkValidTrNumber(n) {
-    n=(n+'').split``.map(Number)
-    return n.length===11&&n[0]!==0&&((n[0]+n[2]+n[4]+n[6]+n[8])*7-(n[1]+n[3]+n[5]+n[7]))%10===n[9]&&n.slice(0,n.length-1).reduce((a,c)=>a+c,0)%10===n[10]
+function primeWord(a){
+    const F=(x)=>{
+        if(x<2)return false
+        for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return x>=2
+    }
+    return a.map(e=>e[0].split``.map(u=>u.charCodeAt()).map(z=>z+e[1]).some(v=>F(v))).map(e=>e?1:0)
 }
-  console.log(checkValidTrNumber(12762438338))
+console.log(primeWord([["Ava",55],["William",17]]))
