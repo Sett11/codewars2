07c2619ea5763f7fed005097dd9c375ca0dde491
Jhonny <gonzodzen@mail.ru>
2023-02-16 14:33:17 +0300

    Coding 3min : Special factor

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ sc=n=>{
   return a.filter(e=>n.includes(e.toString(2)))
 }
 console.log(sc(100))
+// function specialPrimes(n,m=251){
+//     const isPrime=(a,b=2)=>b*b>a?!0:a%b?isPrime(a,++b):!1
+//     const arr = []
+//     while(m<=n){tmp=(m+'').split``.map(Number);if(isPrime(m)&&!tmp.includes(0)&&tmp[0]*tmp[2]!==45&&[...new Set(tmp)].length===tmp.length&&tmp.reduce((a,c)=>a+c)%2===0)arr.push(m);m++}
+//     return arr
+// }
+// console.log(specialPrimes(457))
