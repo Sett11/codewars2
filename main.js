fff4adb23181d56524675003e0ca129263392d1d
Jhonny <gonzodzen@mail.ru>
2023-02-16 20:19:00 +0300

    Special Subsets of Primes

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-
-sc=s=>[...s].reduce((a,c,i)=>c==s[i+1]?a+2:a+7,1)-7
-
-  console.log(sc("-+-+-+"))
-  console.log(sc("---+++"))
-  console.log(sc("-+-+-----------"))
-  console.log(sc("-+-+-++++++++++"))
-
-// function specialPrimes(n,m=251){
-//     const isPrime=(a,b=2)=>b*b>a?!0:a%b?isPrime(a,++b):!1
-//     const arr = []
-//     while(m<=n){tmp=(m+'').split``.map(Number);if(isPrime(m)&&!tmp.includes(0)&&tmp[0]*tmp[2]!==45&&[...new Set(tmp)].length===tmp.length&&tmp.reduce((a,c)=>a+c)%2===0)arr.push(m);m++}
-//     return arr
-// }
-// console.log(specialPrimes(500))
+function specialPrimes(n){
+    const F=(a,b=2)=>b*b>a?!0:a%b?F(a,++b):!1;arr = [],res1=[],res2=[],res3=[],m=251
+    while(m<=n){tmp=(m+'').split``.map(Number);sum=tmp.reduce((a,c)=>a+c,0);if(F(m)&&!tmp.includes(0)&&tmp[0]*tmp[tmp.length-1]!==45&&[...new Set(tmp)].length===tmp.length)arr.push(m);m++}
+   arr.map(e=>e<1000&&(e+'').split``.map(Number).reduce((a,c)=>a+c,0)%4===0?e:e>1000&&e<10000&&(e+'').split``.map(Number).reduce((a,c)=>a+c,0)%4===0||(e+'').split``.map(Number).reduce((a,c)=>a+c,0)%25===0?e:e>10000&&e<100000&&(e+'').split``.map(Number).reduce((a,c)=>a+c,0)%4===0||(e+'').split``.map(Number).reduce((a,c)=>a+c,0)%25===0||(e+'').split``.map(Number).reduce((a,c)=>a+c,0)%225===0?e:'&').filter(e=>e!=='&').map(e=>e+''===(e+'').split``.map(Number).sort((a,b)=>a-b).join``?res1.push(e):e+''===(e+'').split``.map(Number).sort((a,b)=>b-a).join``?res2.push(e):res3.push(e))
+   return n<251?[[0,0,0],[0,0,0],[0,0,0]]:[res3,res1,res2].map(e=>[e[0],e[e.length-1],e.length])
+}
+console.log(specialPrimes(43419))
