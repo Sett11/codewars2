0c61e5e884cc5e4ba45ea1713bd7364f918f53ed
Jhonny <gonzodzen@mail.ru>
2023-02-16 23:00:01 +0300

    Which are in?

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-countSpecMult=(n, m)=> ~~(m/[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,157, 163, 167, 173, 179, 181, 191, 193, 197, 199].slice(0,n).reduce((a,c)=>a*c))
-console.log(countSpecMult(3, 200));
+inArray=(a,b)=>a.filter(e=>b.some(u=>u.match(e))).sort()
+console.log(inArray(["live", "strong", "arp"],["lively", "alive", "harp", "sharp", "armstrong"]))
