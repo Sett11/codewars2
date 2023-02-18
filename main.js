9db52321ff0cf20e5865bef02ed26e6a6823800f
Jhonny <gonzodzen@mail.ru>
2023-02-18 14:25:20 +0300

    What is type of variable?

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-class GroupByDifference {
-    constructor(n){
-        this.n=n.sort((a,b)=>a-b).map((e,i)=>[e,i]).sort((a,b)=>a[1]-b[1])
-    }
-    find(t){
-        const arr=[]
-        for(let i = 0; i < this.n.length; i++){
-            for(let j = 0; j<this.n.length; j++){
-                if(Math.abs(this.n[i][0]-this.n[j][0])<=t&&i!==j&&this.n[i][1]!==this.n[j][1]){
-                    arr.push(this.n[j])
-                }
-            }
-        }
-        arr.sort((a,b)=>a[1]-b[1])
-        for(let i = 0; i < arr.length-1; i++){
-            if(arr[i][1]===arr[i+1][1]){
-                arr[i]='&'
-            }
-        }
-        return arr.filter(e=>e!=='&').map(e=>e[0])
-    }
+function type(v) {
+    if(v===null)return 'null'
+    if(typeof v==='object'&&typeof Date.parse(v)==='number'&&Date.parse(v)===Date.parse(v))return 'date'
+    if(typeof v==='object'&&v.length===0)return 'array'
+    if(typeof v==='object')return 'object'
+    return typeof v
   }
-  console.log(new GroupByDifference([5, 32, 5, 1, 31, 70, 30, 8]).find(2))
+console.log(type([]))
+
