8d19359264af5cc42155bcfacd437271ad9eba20
Jhonny <gonzodzen@mail.ru>
2023-02-18 22:22:53 +0300

    Find the Mine!

--- main.js
+++ main.js
@@ -1,27 +1,2 @@
-const squares = n => {
-    arr=[],c=1
-    while(arr.length<n){arr.push(c**2);c++}
-    return arr
-}
-
-const range = (n, start, step) => {
-    arr=[]
-    for(let i=start;;i+=step){
-        arr.push(i)
-        if(arr.length===n)break
-    }
-    return arr
-}
-
-const random = (n, min, max) => {
-    arr=[]
-    while(arr.length<=n){arr.push(min-1 + Math.floor(Math.random()*(max+1-min)))}
-    return arr
-}
-
-const primes = n => {
-    let F=(x)=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true},arr=[],c=2
-    while(arr.length<n){if(F(c))arr.push(c);c++}return arr
-}
-
-console.log(random(4,5,10))
+mineLocation=f=>f.map((e,i)=>e.map((u,z)=>u===1?[i,z]:0).filter(a=>a!==0)).filter(e=>e.length!==0)[0][0]
+  console.log(mineLocation([ [1, 0, 0], [0, 0, 0], [0, 0, 0] ]))
