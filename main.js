d5f9d0f3714cb2e6e49e548ba74d25e555c7e4db
Jhonny <gonzodzen@mail.ru>
2023-02-18 21:17:45 +0300

    Filling an array (part 2)

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-String.prototype.characterCount = function (c) {
-    if(!c)return undefined
-    const total = Object.entries([...this].reduce((a,c)=>{
-        a[c]=(a[c]||0)+1
-        return a
-    },{}))
-    c=[...c]
-    for(let i=0;i<c.length;i++){
-        for(let j=0;j<total.length;j++){
-            if(c[i]===total[j][0])c[i]=total[j][1]
-        }
-    }return c.length===1?c.map(e=>typeof e==='string'?0:e)[0]:c.map(e=>typeof e==='string'?0:e)
+const squares = n => {
+    arr=[],c=1
+    while(arr.length<n){arr.push(c**2);c++}
+    return arr
 }
-  console.log('lol'.characterCount('l'))
+
+const range = (n, start, step) => {
+    arr=[]
+    for(let i=start;;i+=step){
+        arr.push(i)
+        if(arr.length===n)break
+    }
+    return arr
+}
+
+const random = (n, min, max) => {
+    arr=[]
+    while(arr.length<=n){arr.push(min-1 + Math.floor(Math.random()*(max+1-min)))}
+    return arr
+}
+
+const primes = n => {
+    let F=(x)=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true},arr=[],c=2
+    while(arr.length<n){if(F(c))arr.push(c);c++}return arr
+}
+
+console.log(random(4,5,10))
