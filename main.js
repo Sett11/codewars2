6b89b8a850b698f34f9fb6c470278ca91d806760
Jhonny <gonzodzen@mail.ru>
2023-02-19 00:18:05 +0300

    Matrix Transpose

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-const mirror = obj => {
-    res={}
-    for(let i in obj){
-        res[i]=[...i].reverse().join``
-    }return res
-}
-console.log(mirror({
-    abc: undefined,
-    arara: undefined,
-  }))
+function transpose(m) {
+    if(m.length===1)return m.flat().map(e=>[e])
+      arr=[],c=1,res=[]
+      for(let i=0;i<=m.length;i++){for(let j=0;j<m.length;j++){arr.push(m[j][i])}}for(let i=0;i<arr.length;i+=m.length){res.push(arr.slice(i,i+m.length))}
+      return res.map(e=>e.filter(e=>e!==undefined)).filter(e=>e&&e.length)
+  }
+console.log(transpose([ [ 1, 0, 0 ], [ 0, 1, 0 ], [ 0, 0, 1 ], [ 0, 1, 0 ], [ 1, 0, 0 ] ]))
