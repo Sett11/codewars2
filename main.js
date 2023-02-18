2809e14574efe7373f707de6f87061dcd855dd43
Jhonny <gonzodzen@mail.ru>
2023-02-18 14:54:15 +0300

    Shorten Me: Array Declaration II (undefined)

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-function type(v) {
-    if(v===null)return 'null'
-    if(typeof v==='object'&&typeof Date.parse(v)==='number'&&Date.parse(v)===Date.parse(v))return 'date'
-    if(typeof v==='object'&&v.length===0)return 'array'
-    if(typeof v==='object')return 'object'
-    return typeof v
-  }
-console.log(type([]))
+array=()=>[...typeof x]
 
+  console.log(array())
+
+//   [
+//     'array', 'boolean', 'function', 'number',
+//     'object', 'string', 'undefined'
+//   ];
