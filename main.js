a00c9c453f483f0788731d2dbaab2d64d07d9e8c
Jhonny <gonzodzen@mail.ru>
2023-02-19 19:59:39 +0300

    Concatenate sequential page numbers in an array

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function differentDigitsNumberSearch(a) {
- for(let i=0;i<a.length;i++){if([...new Set([...(a[i]+'')])].length===[...(a[i]+'')].length)return a[i]}return -1
+function paginate(a) {
+    a=a.slice().sort((a,b)=>a-b)
+    for(let i=0;i<a.length;i++){
+        if(a[i+1]-a[i]===1){
+            for(let j=i;j<a.length;j++){
+                if(a[j+1]-a[j]!==1){
+                    a=a.slice(0,i).concat([a.slice(i,j+1)].concat(a.slice(j+1)))
+                    break
+                }
+            }
+        }
+    }return a.map(e=>typeof e==='object'?[e[0],'-',e[e.length-1]]:e).flat().join`,`.replace(/,-,/g,'-').replace(/,/g,', ')
 }
-console.log(differentDigitsNumberSearch([10, 11, 12, 13]))
+console.log(paginate([10,12,13,14,15,16,17,20,22,23,27]))
