fb69186a838f9dc11ad5befe59ac95c403673aa9
Jhonny <gonzodzen@mail.ru>
2023-02-19 13:38:08 +0300

    Simple Fun #303: Prime Product

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-const uniqueDigitProducts=a=>[...new Set(a.map(e=>[...(e+'')].map(Number).reduce((a,c)=>a*c)))].length
-
-console.log(uniqueDigitProducts([100, 23, 42, 239, 22339, 9999999, 456, 78, 228, 1488]))
+function primeProduct(n){
+  if(n<4)return 0
+  const F=(x)=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true};x=n-1,arr=[]
+  while(x>2){x--;if(F(x)&&F(n-x))arr.push(x*(n-x))}return !arr.length?0:Math.max(...arr)
+}
+console.log(primeProduct(1001))
