f6d1097b6960fe9020aa797c3966bdc4221214d3
Jhonny <gonzodzen@mail.ru>
2023-02-19 13:25:13 +0300

    Simple Fun #91: Unique Digit Products

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-function mostFrequentDigitSum(n) {
-  const arr=[n],res=[]
-  while(!arr.includes(0)){n-=[...(n+'')].map(Number).reduce((a,c)=>a+c,0);arr.push(n)}
-  for(let i=0;i<arr.length-1;i++){res.push(Math.abs(arr[i+1]-arr[i]))}
-  return +Object.entries(res.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b[1]-a[1]||+b[0]- +a[0])[0][0]
-}
-console.log(mostFrequentDigitSum(17))
+const uniqueDigitProducts=a=>[...new Set(a.map(e=>[...(e+'')].map(Number).reduce((a,c)=>a*c)))].length
+
+console.log(uniqueDigitProducts([100, 23, 42, 239, 22339, 9999999, 456, 78, 228, 1488]))
