9f686f70e4265318da4b2292c9c4bdc2c9ca6111
Jhonny <gonzodzen@mail.ru>
2023-02-19 14:34:33 +0300

    Simple Fun #318: Sort String

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-function primeString(s) {
-  if(s.length===1)return true
-  for(let i=1;i<s.length/2+1;i++){
-    if(s===s.slice(0,i).repeat(Math.floor(s.length/s.slice(0,i).length)))return false
-  }return true
+function sortString(s){ 
+  let check=[...s.toLowerCase()].sort(),arr=[],str=[...s].map((e,i)=>!e.match(/[a-z]/g)&&!e.match(/[A-Z]/g)?arr.push([e,i]):null),res=[...s.replace(/[\W_]/g,'')].map(e=>[e,check.indexOf(e.toLowerCase())]).sort((a,b)=>a[1]-b[1]).map(e=>e[0]).join``
+  for(let i=0;i<arr.length;i++){res=res.slice(0,arr[i][1])+arr[i][0]+res.slice(arr[i][1])}return res
 }
-console.log(primeString('utdutdtdutd'))
-console.log(primeString('abba'))
+  console.log(sortString(" MkWD{RB=//k-^ J@,xH Vfi uAz+$ kV _[ }a!}%pSBwn !kKB (b  q PQF +}wS  .kfU r wFNEs#NsR UVMdG"))
