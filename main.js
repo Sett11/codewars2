a1c51273df0392481a2f925656eb4a2982ac47b1
Jhonny <gonzodzen@mail.ru>
2023-02-19 15:13:19 +0300

    Simple Fun #217: Sort By Guide

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-function digitDifferenceSort(a) {
-  return a.map((e,i)=>{
-    let tmp=[...(e+'')].map(Number).sort((a,b)=>b-a)
-    return [e,tmp[0]-tmp[tmp.length-1],i]
-  }).sort((a,b)=>a[1]-b[1]||b[2]-a[2]).map(e=>e[0])
+function sortByGuide(arr, g) {
+  check=[],arr=arr.map((e,i)=>{
+    e=[e,g[i],i]
+    if(e[1]===-1){
+      check.push(e)
+      return '&'
+    }
+    return e
+  }).filter(e=>e!=='&').sort((a,b)=>a[1]-b[1])
+  for(let i=0;i<check.length;i++){arr=arr.slice(0,check[i][2]).concat(check[i][0]).concat(arr.slice(check[i][2]))}return arr.map(e=>typeof e==='number'?e:e[0])
 }
-console.log(digitDifferenceSort([13098, 1308, 12398, 52433, 213, 424, 213, 243, 12213, 54234, 99487, 81892, 11111, 97897]))
+console.log(sortByGuide([700, 800, 400, 100, 900, 325], [2, -1, 1, -1, 3, -1]))
