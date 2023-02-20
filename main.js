c4aba22bdcf029fa52934402edf813fbdeadf228
Jhonny <gonzodzen@mail.ru>
2023-02-20 22:39:22 +0300

    Exclamation marks series #10: Remove some exclamation marks to keep the same number of exclamation marks at the beginning and end of each word

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const formatWords=w=>!w?'':w.filter(e=>e).join`, `.split``.reverse().join``.replace(/,/,'dna ').split``.reverse().join``.replace(/  /g,' ')
-
-console.log(formatWords(['ninja', '', 'ronin']))
+function remove(s){
+  return s.split` `.map(e=>[e.replace(/!/g,''),e.match(/!+/g)]).map(e=>{
+    if(!e[1]||e[1].length===1)e[1]=''
+    if(e[1].length>1)e[1]=e[1].sort((a,b)=>a.length-b.length)[0]
+    e=[e[1],e[0],e[1]]
+    return e.join``
+  }).join` `
+}
+console.log(remove("!!!!!!!!!asfwx!!!!!!! !!!!!!!ujyefx!!!! dweenq!!! aqjg !!!!!!!zuwfmyd!!! !!!bjug !!!!!!!!!!rcuura!!!! !!!!!!!afjqilr!!!!!!"))
+console.log(remove("!!!!Hi!! !!!!Hi !Hi!!!"))
