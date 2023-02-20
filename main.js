9c0667513da5f5c8a5d2b8bf86f17854fde9ae09
Jhonny <gonzodzen@mail.ru>
2023-02-20 18:32:15 +0300

    Treebeard #Determine Tree Age

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-const cutCancerCells=o=>{
-  o=[...o],o.push('&'),o.unshift('&')
-  for(let i=1;i<o.length-1;i++){
-    if(o[i]==='c')o[i]='&'
-    if(o[i]==='C'&&o[i-1]!==o[i-1].toUpperCase()&&o[i+1]!==o[i+1].toUpperCase())o[i-1]='&',o[i]='&',o[i+1]='&'
-    if(o[i]==='C'&&o[i-1]===o[i-1].toUpperCase()&&o[i+1]!==o[i+1].toUpperCase())o[i]='&',o[i+1]='&'
-    if(o[i]==='C'&&o[i-1]!==o[i-1].toUpperCase()&&o[i+1]===o[i+1].toUpperCase())o[i-1]='&',o[i]='&'
-  }return o.filter(e=>e!=='&').join``.replace(/C/g,'')
-}
+const treeAge=t=>JSON.stringify(t).replace(/\]/g,'').replace(/\,\[/g,'').length
 
-console.log(cutCancerCells('CAAC'))
-console.log(cutCancerCells('BCE'))
-console.log(cutCancerCells('acCb'))
-console.log(cutCancerCells('acb'))
-console.log(cutCancerCells('NcE'))
-console.log(cutCancerCells('HuccccCCmPHYccCcCCSMhClCCX'))
+console.log(treeAge([ [],[ [] ] ]))
+
+
+// const treeAge=(t,z=1)=> {
+//   if(t.length===0)return z
+//   return +t.map(e=>typeof e==='object'?treeAge(e,z+1):0).join``
+// }
