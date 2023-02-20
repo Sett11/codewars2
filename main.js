66610a92d2e4ddad32dd0e73892a516f318a7bb8
Jhonny <gonzodzen@mail.ru>
2023-02-20 16:51:27 +0300

    Perfect Square.

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-function Dictionary(words) {
-  this.words = words;
-}
-Dictionary.prototype.getMatchingWords = function(p) {
-  p=p.replace(/\?/g,'.')
-  return this.words.map(e=>e.match(p)&&e.length===p.length?e.match(p):0).map(e=>e?e[0]:e).filter(e=>e)
-}
-
-console.log(new Dictionary(['banana', 'apple', 'papaya', 'cherry']).getMatchingWords('?????'))
+const perfectSquare=s=>s.split`\n`.every((e,_,ar)=>e.length===ar.length&&[...e].every(u=>u==='.'))
+console.log(perfectSquare(".,\n.,"))
