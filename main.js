38b48a497c0e83250305379341d95e4c74e78a51
Jhonny <gonzodzen@mail.ru>
2023-02-20 18:59:11 +0300

    Find the First Number in Having a Certain Number of Divisors I

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-const treeAge=t=>JSON.stringify(t).replace(/\]/g,'').replace(/\,\[/g,'').length
-
-console.log(treeAge([ [],[ [] ] ]))
+function findMinNum(n,c=1) {
+  while(1){
+    let innerArr=[]
+    for(let i=1;i<c/2+1;i++){
+      if(c%i===0)innerArr.push(i)
+    }
+    if(innerArr.length<n-1)innerArr=[]
+    if(innerArr.length===n-1)return c
+    c++
+  }
+}
+console.log(findMinNum(10))
