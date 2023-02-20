fa59a3d990d1d1138b1eeedde00e1c2030ebec6e
Jhonny <gonzodzen@mail.ru>
2023-02-21 00:31:02 +0300

    Pair of gloves

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-function remove(s){
-  return s.split` `.map(e=>[e.replace(/!/g,''),e.match(/!+/g)]).map(e=>{
-    if(!e[1]||e[1].length===1)e[1]=''
-    if(e[1].length>1)e[1]=e[1].sort((a,b)=>a.length-b.length)[0]
-    e=[e[1],e[0],e[1]]
-    return e.join``
-  }).join` `
+function numberOfPairs(g){
+  g=g.slice().sort(),arr=[]
+  for(let i=0;i<g.length;i++){
+    if(g[i]===g[i+1]){
+      for(let j=i;j<g.length;j++){
+        if(g[j+1]!==g[i]){
+          arr.push(g.slice(i,j+1))
+          i=j
+          break
+        }
+      }
+    }
+  }return arr.map(e=>Math.floor(e.length/2)).reduce((a,c)=>a+c,0)
 }
-console.log(remove("!!!!!!!!!asfwx!!!!!!! !!!!!!!ujyefx!!!! dweenq!!! aqjg !!!!!!!zuwfmyd!!! !!!bjug !!!!!!!!!!rcuura!!!! !!!!!!!afjqilr!!!!!!"))
-console.log(remove("!!!!Hi!! !!!!Hi !Hi!!!"))
+console.log(numberOfPairs([
+  'Teal',   'Green',   'Aqua',   'Purple', 'Olive',
+  'Teal',   'Navy',    'Navy',   'Gray',   'Navy',
+  'White',  'Maroon',  'Lime',   'Silver', 'Purple',
+  'Gray',   'Green',   'Olive',  'Lime',   'Lime',
+  'Aqua',   'White',   'Purple', 'Black',  'Yellow',
+  'Maroon', 'Fuchsia', 'Black',  'Yellow', 'Maroon',
+  'Silver', 'Lime',    'Gray',   'Lime',   'Maroon',
+  'Gray',   'Yellow',  'Purple', 'Black',  'Fuchsia',
+  'Purple', 'Navy',    'Red',    'Navy',   'Purple',
+  'Silver', 'Green',   'Gray',   'Silver', 'Olive',
+  'Gray',   'White',   'Navy',   'Lime',   'Gray',
+  'Gray',   'Black',   'Teal',   'White',  'Teal',
+  'Lime'
+]))
+console.log(numberOfPairs([ 'Blue', 'Gray', 'Green', 'Aqua', 'Red', 'Black' ]))
