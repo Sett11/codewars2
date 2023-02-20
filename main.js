434ccd405036fa807cb9fce13c76a9cf77ec23cd
Jhonny <gonzodzen@mail.ru>
2023-02-20 18:32:32 +0300

    Treebeard #Determine Tree Age

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 const treeAge=t=>JSON.stringify(t).replace(/\]/g,'').replace(/\,\[/g,'').length
 
 console.log(treeAge([ [],[ [] ] ]))
-
-
-// const treeAge=(t,z=1)=> {
-//   if(t.length===0)return z
-//   return +t.map(e=>typeof e==='object'?treeAge(e,z+1):0).join``
-// }
