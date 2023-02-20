3b7cc29d26346813d900a8b8ad0e8cf5d92472dc
Jhonny <gonzodzen@mail.ru>
2023-02-20 17:58:32 +0300

    Cancer cells

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-const perfectSquare=s=>s.split`\n`.every((e,_,ar)=>e.length===ar.length&&[...e].every(u=>u==='.'))
-console.log(perfectSquare(".,\n.,"))
+const cutCancerCells=o=>{
+  o=[...o],o.push('&'),o.unshift('&')
+  for(let i=1;i<o.length-1;i++){
+    if(o[i]==='c')o[i]='&'
+    if(o[i]==='C'&&o[i-1]!==o[i-1].toUpperCase()&&o[i+1]!==o[i+1].toUpperCase())o[i-1]='&',o[i]='&',o[i+1]='&'
+    if(o[i]==='C'&&o[i-1]===o[i-1].toUpperCase()&&o[i+1]!==o[i+1].toUpperCase())o[i]='&',o[i+1]='&'
+    if(o[i]==='C'&&o[i-1]!==o[i-1].toUpperCase()&&o[i+1]===o[i+1].toUpperCase())o[i-1]='&',o[i]='&'
+  }return o.filter(e=>e!=='&').join``.replace(/C/g,'')
+}
+
+console.log(cutCancerCells('CAAC'))
+console.log(cutCancerCells('BCE'))
+console.log(cutCancerCells('acCb'))
+console.log(cutCancerCells('acb'))
+console.log(cutCancerCells('NcE'))
+console.log(cutCancerCells('HuccccCCmPHYccCcCCSMhClCCX'))
