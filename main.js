a1c9a3f63a3bbf0af3abefb8e4360dc1d2aff5f9
Jhonny <gonzodzen@mail.ru>
2023-02-21 18:19:13 +0300

    Chess Fun #10: Chess Encryption

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-function piecesValue(arr, s) {
-  return arr.map(e=>e.filter(u=>u!==' '&&u[0]===s[0]&&u[2]+u[3]!=='ki')).filter(e=>e.length).map(e=>e.length>1?e.join` `:e).join` `.split` `.map(e=>e.replace(/.-/,'')).map(e=>e==='queen'?9:e==='rook'?5:e==='pawn'?1:e==='bishop'?3:e==='knight'?3:0).reduce((a,c)=>a+c,0)
+function chessEncryption(m) {
+  const obj = {
+    a: "d3",
+    b: "d4",
+    c: "d5",
+    d: "d6",
+    e: "d7",
+    f: "e7",
+    g: "e6",
+    h: "e5",
+    i: "e4",
+    j: "e3",
+    k: "e2",
+    l: "f2",
+    m: "g2",
+    n: "h2",
+    o: "h1",
+    p: "g1",
+    q: "f1",
+    r: "e1",
+    s: "d1",
+    t: "c1",
+    u: "b1",
+    v: "a1",
+    w: "a2",
+    x: "b2",
+    y: "c2",
+    z: "d2",
+  }
+  return [...m].map(e=>obj[e]?obj[e]:e).join``
 }
-console.log(piecesValue([ [ ' ', ' ', 'b-king', ' ', ' ', ' ', ' ', ' ' ],
-[ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-[ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-[ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-[ 'w-king', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-[ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-[ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-[ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ] ],'white'))
+console.log(chessEncryption("go away"))
