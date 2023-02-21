05fd989d72c946fed85e0d339ee6360884dd7480
Jhonny <gonzodzen@mail.ru>
2023-02-22 00:42:26 +0300

    Simple frequency sort

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const findX=n=>n**2*(3*n-2)
+function solve(a){
+  const res=[], arr=Object.entries(a.slice().reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b[1]-a[1]||+a[0]- +b[0]).map(e=>{
+    for(let i=0;i<e[1];i++){res.push(+e[0])}
+  })
+  return res
+}
+console.log(solve([4,9,5,0,7,3,8,4,9,0]))
 
-console.log(findX(9))
+
