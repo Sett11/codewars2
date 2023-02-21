4f650a763a6f041326d6465b31a7d63fbc0e9f7c
Jhonny <gonzodzen@mail.ru>
2023-02-21 16:04:45 +0300

    T.T.T.18: Find out the prime part in a number

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const findUnknowNumber=(a,b,d,x,y,z,c=1)=>{
-  while(1){if(c%a===x&&c%b===y&&c%d===z)return c;c++}
+function findOutPrime(n){
+  const F=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true};arr=[],check=[...(n+'')]
+  if(F(n))return n
+  for(let i=0;i<check.length;i++){
+    for(let j=i;j<check.length;j++){
+    let tmp=check.slice(i,j+1).join``
+    if(F(+tmp))arr.push(tmp)
+    }
+  }return !arr.length?null:Math.max(...arr)
 }
-console.log(findUnknowNumber(3,5,7,1,2,3))
+console.log(findOutPrime(1052326572))
