7c8fa98ed9d88e449f0456c353ad92b9d2f92019
Jhonny <gonzodzen@mail.ru>
2023-02-22 01:42:43 +0300

    Sum The Array

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-Array.prototype.except = function(){
-  const keys=[];[...arguments].map(e=>typeof e!=='object'?[e]:e).forEach(e=>e.forEach(u=>keys.push(u)))
-  return this.slice().filter((e,i)=>!keys.includes(i))
+Array.prototype.sum=function(){
+  return this.reduce((a,c)=>a+c,0)
 }
-console.log(['a', 'b', 'c', 'd', 'e'].except([1,3]))
