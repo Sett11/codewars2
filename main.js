df535a17442b931a817ecdc7c39dedc311c7a264
Jhonny <gonzodzen@mail.ru>
2023-02-22 21:46:45 +0300

    Simple Fun #23: Square Digits Sequence

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function find(a,b,n){
-  n=+[...''+n].slice(-4).join``, str=`${a}${b}`
-  while(str.length<=n){
-    let tmp=+str[str.length-2]+ +str[str.length-1]
-    str+=tmp
-  }return +str[n]
+function squareDigitsSequence(a) {
+  let arr=[a],val=true
+  while(val){
+    let tmp=arr[arr.length-1].toString().split``.map(Number).map(e=>e**2).reduce((a,c)=>a+c,0)
+    arr.push(tmp)
+    if([...new Set(arr)].length<arr.length)val=false
+  }return arr.length
 }
-console.log(find(7,8,2483685788))
+console.log(squareDigitsSequence(103))
