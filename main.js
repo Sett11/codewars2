5a7de7b4158f33fe53544bfb1a7392026b1b2c18
Jhonny <gonzodzen@mail.ru>
2023-02-22 21:07:00 +0300

    Simple Fun #361: Who Is The Hero? I

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-function additionWithoutCarrying(a,b) {
- a=([...a+'']).map(Number),b=([...b+'']).map(Number),arr=[]
- if(a.length>b.length){
-  let tmp=a.length-b.length
-  while(tmp){b.unshift(0);tmp--}
- }
- if(b.length>a.length){
-  let tmp=b.length-a.length
-  while(tmp){a.unshift(0);tmp--}
- }
- for(let i=0;i<a.length;i++){arr.push(a[i]+b[i])}
- return +arr.map(e=>{
-  e=[...(e+'')]
-  return e[e.length-1]
- }).join``
+const whoIsTheHero=n=>{
+  let check=Object.values(n).reduce((a,c)=>a*c),arr=[],c=1
+  while(c<Math.sqrt(check)+1){
+    if(check%c===0&&!arr.includes(c)&&!arr.includes(check/c)){arr.push(c,check/c)};c++};arr=[...new Set(arr)]
+  return `Good-Bye, Hero ${Object.keys(n)[+[...(arr.length+'')].reverse()[0]]}! We'll take care of your children and your wife.`
 }
-console.log(additionWithoutCarrying(999,999))
+
+console.log(whoIsTheHero({Voile:1,John:2,Jerry:3,Mike:4,Abc:5,Def:6,Ghi:7,Jkl:8,Mno:9,Tom:10}))
+console.log(whoIsTheHero({John:1,Tom:2,Jerry:6,Mike:1,Abc:3,Def:1,Ghi:1,Jkl:1,Mno:1,Voile:1}))
+console.log(whoIsTheHero({John:1,Voile:1,Jerry:1,Mike:1,Abc:1,Def:1,Ghi:1,Jkl:1,Mno:1,Tom:1}))
