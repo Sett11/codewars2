1aafd3d528d3f391a19fc9dcd6c29f4964fa8e23
Jhonny <gonzodzen@mail.ru>
2023-02-24 01:23:29 +0300

    Character with longest consecutive repetition

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function isBeautifulString(s) {
-  if(!s.includes('a'))return false
-  const arr=Object.values([...s].sort().reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))
-  return arr.every((e,i,ar)=>ar.slice(i+1).every(u=>e>=u))
+function longestRepetition(s) {
+  if(!s||s===' ')return ['',0]
+  if(s.length===2||[...new Set(s)].length===s.length)return [s[0],1]
+  let arr=[],check=0
+  for(let i=0;i<s.length;i++){
+    if(s[i]===s[i+1]){
+      for(let j=i;j<s.length;j++){
+        if(s[j]!==s[i]){
+          arr.push(s.slice(i,j))
+          i=j
+        }
+        if(j===s.length-1&&s[j]===s[i])arr.push(s.slice(i,j+1))
+      }
+    }
+  }check=Math.max(...arr.map(e=>e.length)),arr=arr.filter(e=>e.length===check)[0]
+  return [arr[0],arr.length]
 }
- console.log(isBeautifulString("bacabcbdabcbbadaaaacedb"))
- console.log(isBeautifulString("bbc"))
+console.log(longestRepetition("ba"))
