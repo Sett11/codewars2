94732fb4f0eccfe89f8e37b1463133d0f44010b1
Jhonny <gonzodzen@mail.ru>
2023-02-23 13:36:43 +0300

    Regexp Basics - is it IPv4 address?

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-function numberAndIPaddress(s){
-  if(s.includes('.')){
-    s=s.split`.`.map(e=>(+e).toString(2))
-    .map(e=>{
-      if(e.length<8)e='0'.repeat(8-e.length)+e
-      return e
-  }).join``
-    return parseInt(s,2)+''
-  }
-  s=(+s).toString(2),arr=[]
-  if(s.length<32)s='0'.repeat(32-s.length)+s
-  for(let i=0;i<s.length;i+=8){arr.push(s.slice(i,i+8))}
-  return arr.map(e=>parseInt(+e,2)).join`.`
+String.prototype.ipv4Address=function(){
+  if(JSON.stringify(this).replace(/"/g,'').replace(/[\d\.]/g,'').length!==0)return false
+  if(JSON.stringify(this).replace(/"/g,'').replace(/[^\.]/g,' ').match(/\.\./g))return false
+  const res=JSON.stringify(this).replace(/"/g,'').split``
+  if(+res[0]!==+res[0]||+res.slice().reverse()[0]!==+res.slice().reverse()[0])return false
+  console.log(this)
+  const check=JSON.stringify(this).split`.`.map(e=>e.replace(/"/g,'')).map(e=>{
+    if(e.length>1&&e[0]==='0')e='&'
+    return e
+  }).filter(e=>e&&e!=='&')
+  return check.length===4&&check.every(e=>+e===+e&&+e<256&&+e>=0&&e.replace(/[^\s]/g,'').length===0)
 }
-console.log(numberAndIPaddress("10.0.3.193"))
-console.log(numberAndIPaddress("167969729"))
+console.log("127.0.0.1".ipv4Address())
+console.log(" 127.0.0.1 ".ipv4Address())
+console.log('10.20.030.40'.ipv4Address())
+console.log('..255.255'.ipv4Address())
+console.log("\n127.0.0.1".ipv4Address())
+console.log("127.0.0.1.".ipv4Address())
