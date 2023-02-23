70099bbc75488d9cc7a41a2d9f986e1387618fe7
Jhonny <gonzodzen@mail.ru>
2023-02-23 20:41:57 +0300

    Simple Fun #90: Rows Rearranging

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-function possibleBase(ux){
-  ex=ux.split`=`.map(e=>e.trim().replace(/ /g,'')).map((e,i)=>{
-    if(i===0){
-      e=[e.match(/\+|\-|\/|\*|%=\%/g)[0],e.replace(e.match(/\+|\-|\/|\*|%=\%/g)[0],' ').split` `]
-      e=[e[1][0],e[0],e[1][1]]
-    }return e
-  })
-  for(let i=2;i<=32;i++){
-    if(ux.match(/[a-z]/gi)&&i<11)continue
-    if(eval(`${parseInt(ex[0][0],i)}${ex[0][1]}${parseInt(ex[0][2],i)}`)===parseInt(ex[1],i))return i
-  }return -1
+function rowsRearranging(m) {
+  n=m.slice().sort((a,b)=>a[0]-b[0]),arr=[],res=[]
+  for(let i=0;i<n[0].length;i++){
+    for(let j=0;j<n.length;j++){
+      arr.push(n[j][i])
+    }
+  }for(let i=0;i<arr.length;i+=n.length){
+    res.push(arr.slice(i,i+n.length))
+  }
+  for(let i=0;i<res.length;i++){
+    for(let j=0;j<res[i].length-1;j++){
+      if(res[i][j]>=res[i][j+1])return false
+    }
+  }return true
 }
-console.log(possibleBase("49 * 43 = ib8"))
+
+console.log(rowsRearranging([
+  [6,4], 
+  [2,2], 
+  [4,3]]))
+console.log(rowsRearranging([
+  [1,3,1], 
+  [0,2,0], 
+  [1,7,2]]))
+console.log(rowsRearranging( [[56,67,-200],
+  [86,95,-173],
+  [71,87,-180],
+  [78,88,-175],
+  [63,72,-186],
+  [30,44,-220],
+  [48,58,-204],
+  [92,99,-164],
+  [59,70,-195],
+  [35,49,-215],
+  [69,81,-182],
+  [39,54,-206] ]))
