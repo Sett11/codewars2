16553d74395694a9ade1d095597b67cefa458beb
Jhonny <gonzodzen@mail.ru>
2023-02-23 19:49:09 +0300

    Simple Fun #355: Possible Base

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function buildPalindrome(s) {
-  if(s===[...s].reverse().join``)return s
-  for(let i=0;i<s.length;i++){
-    if(s+s.slice(0,i).split``.reverse().join``===(s+s.slice(0,i).split``.reverse().join``).split``.reverse().join``)return s+s.slice(0,i).split``.reverse().join``
-  }
+function possibleBase(ux){
+  ex=ux.split`=`.map(e=>e.trim().replace(/ /g,'')).map((e,i)=>{
+    if(i===0){
+      e=[e.match(/\+|\-|\/|\*|%=\%/g)[0],e.replace(e.match(/\+|\-|\/|\*|%=\%/g)[0],' ').split` `]
+      e=[e[1][0],e[0],e[1][1]]
+    }return e
+  })
+  for(let i=2;i<=32;i++){
+    if(ux.match(/[a-z]/gi)&&i<11)continue
+    if(eval(`${parseInt(ex[0][0],i)}${ex[0][1]}${parseInt(ex[0][2],i)}`)===parseInt(ex[1],i))return i
+  }return -1
 }
-console.log(buildPalindrome('ababab'))
+console.log(possibleBase("49 * 43 = ib8"))
