16d10bc216290ac33bbc7ed08dc86aa6cd0fbbf2
Jhonny <gonzodzen@mail.ru>
2023-02-23 15:18:26 +0300

    Simple Fun #110: Array Operations

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-function arrayConversion(arr,c=0) {
-  let F=(x,y)=>x+y,R=(x,y)=>x*y
-    for(let i=0;i<arr.length-1;i++){
-      if(c%2===0){
-        arr[i]=F(arr[i],arr[i+1])
-        arr.splice(i+1,1)
-      }
-      if(c%2!==0){
-        arr[i]=R(arr[i],arr[i+1])
-        arr.splice(i+1,1)
-      }
-    }
-  return arr.length===1?arr[0]:arrayConversion(arr,c+1)
-}
-console.log(arrayConversion([3, 3, 5, 5]))
+function arrayOperations(a, k) {
+  if(k<1)return a
+  if(k%2!==0)k=1
+  if(k%2===0)k=2
+  while(k){a=a.map((e,i,arr)=>Math.max(...arr)-e);k--}return a
+ }
+ console.log(arrayOperations([-4, 0, -1, 0],223))
