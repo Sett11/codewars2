183edb5669c7d9c4abeb4f9406d955decd73593c
Jhonny <gonzodzen@mail.ru>
2023-02-24 02:04:33 +0300

    A small difference

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-function longestRepetition(s) {
-  if(!s||s===' ')return ['',0]
-  if(s.length===2||[...new Set(s)].length===s.length)return [s[0],1]
-  let arr=[],check=0
-  for(let i=0;i<s.length;i++){
-    if(s[i]===s[i+1]){
-      for(let j=i;j<s.length;j++){
-        if(s[j]!==s[i]){
-          arr.push(s.slice(i,j))
-          i=j
-        }
-        if(j===s.length-1&&s[j]===s[i])arr.push(s.slice(i,j+1))
-      }
-    }
-  }check=Math.max(...arr.map(e=>e.length)),arr=arr.filter(e=>e.length===check)[0]
-  return [arr[0],arr.length]
+function oneCharDifference(s1, s2) {
+  if(s1.match(s2)||s2.match(s1)||s1===s2||s1.slice(1)===s2||s2.slice(1)===s1||s1.slice(0,s1.length-1)===s2||s2.slice(0,s2.length-1)===s1)return true
+  for(let i=0;i<s1.length;i++){
+    if(s1.slice(0,i)+s1.slice(i+1)===s2.slice(0,i)+s2.slice(i+1))return true
+    if(s2.slice(0,i)+s2.slice(i+1)===s1.slice(0,i)+s1.slice(i+1))return true
+    if(s1.slice(0,i)+s1.slice(i+1)===s2)return true
+    if(s2.slice(0,i)+s2.slice(i+1)===s1)return true
+  }
+  return false
 }
-console.log(longestRepetition("ba"))
+console.log(oneCharDifference('grant', 'grent'))
