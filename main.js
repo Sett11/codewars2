0a28d33f4be4885519e4008c5af75924d10d2079
Jhonny <gonzodzen@mail.ru>
2023-02-23 14:42:40 +0300

    Simple Fun #47: Stolen Lunch

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-String.prototype.whitespace=function(){
-  return JSON.stringify(this).replace(/[" ]/g,'').replace(/\\t/g,'').replace(/\\s/g,'').replace(/\\r/g,'').replace(/\\n/g,'').length===0
+function stolenLunch(n) {
+  return [...(n+'')].map(e=>{
+    if(!e.match(/\w/))return e
+    if(typeof +e==='number'&&+e===+e){
+      e=String.fromCharCode((+e+97))
+      return e
+    }
+    if(+e!==+e&&(parseInt(e,36)-10)<10){
+      e=parseInt(e,36)-10
+      return e
+    }return e
+  }).join``
 }
-
-console.log('\t\t n\t'.whitespace())
+console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja"))
+console.log(stolenLunch('0123456789'))
+console.log(stolenLunch('jihgfedcba'))
+console.log(stolenLunch("you won't know!!"))
