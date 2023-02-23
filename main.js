cca3fc772de5a660db5d2b1094faea3585d0ebf0
Jhonny <gonzodzen@mail.ru>
2023-02-23 16:22:26 +0300

    Simple Fun #65: Matrix Elements Sum

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function arrayOperations(a, k) {
-  if(k<1)return a
-  if(k%2!==0)k=1
-  if(k%2===0)k=2
-  while(k){a=a.map((e,i,arr)=>Math.max(...arr)-e);k--}return a
- }
- console.log(arrayOperations([-4, 0, -1, 0],223))
+function matrixElementsSum(m) {
+  m.push(Array(m[0].length).fill(0))
+  for(let i=0;i<m.length-1;i++){
+    for(let j=0;j<m[i].length;j++){
+      if(m[i][j]===0)m[i+1][j]=0
+    }
+  }return m.flat().reduce((a,c)=>a+c)
+}
+console.log(matrixElementsSum(
+  [[0]]))
