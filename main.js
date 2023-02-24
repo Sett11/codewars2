50d1476f7ffd54c92930bae821ae59e71a79a55a
Jhonny <gonzodzen@mail.ru>
2023-02-24 03:24:24 +0300

    Number of Digit d in m-based Number Converted to Base n

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-function oneCharDifference(s1, s2) {
-  if(s1.match(s2)||s2.match(s1)||s1===s2||s1.slice(1)===s2||s2.slice(1)===s1||s1.slice(0,s1.length-1)===s2||s2.slice(0,s2.length-1)===s1)return true
-  for(let i=0;i<s1.length;i++){
-    if(s1.slice(0,i)+s1.slice(i+1)===s2.slice(0,i)+s2.slice(i+1))return true
-    if(s2.slice(0,i)+s2.slice(i+1)===s1.slice(0,i)+s1.slice(i+1))return true
-    if(s1.slice(0,i)+s1.slice(i+1)===s2)return true
-    if(s2.slice(0,i)+s2.slice(i+1)===s1)return true
-  }
-  return false
-}
-console.log(oneCharDifference('grant', 'grent'))
+const countDigit=(n, d, base=10, fromBase=10)=>[...(parseInt(n,fromBase)).toString(base)].filter(e=>e===d).length
+
+console.log(countDigit("1100101110101", "d", 15, 2))
+console.log(countDigit("133", "3", 10, 10))
