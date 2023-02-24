78c40ed37e9905e5f2cddcdd4637c6ec029bae1f
Jhonny <gonzodzen@mail.ru>
2023-02-24 11:45:56 +0300

    Simple Fun #234: Euler's Totient Function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-Ackermann = function(m,n) {
-    if(m===0)return n+1
-    if(n===0&&m>0)return Ackermann(m-1,1)
-    if(m>0&&n>0)return Ackermann(m-1,Ackermann(m,n-1))
-}
-console.log(Ackermann(4,0))
+function eulersTotientFunction(n) {
+    let arr=Array(n).fill(1).map((e,i)=>[i+1,Array(i+1).fill(1).map((u,j)=>j+1).filter(z=>(i+1)%z===0)]),check=arr[arr.length-1]
+    return n===1?1:arr.slice(0,arr.length-1).filter(e=>e[1].every(u=>u===1?u:!check[1].includes(u))).length
+  }
+  console.log(eulersTotientFunction(49))
