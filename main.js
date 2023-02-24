5fb58498ad2d19eb8ec961a3d1909769a1ab7de3
Jhonny <gonzodzen@mail.ru>
2023-02-24 13:05:49 +0300

    Walk-up Stairs

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-const maxZeroSequence=a=>{
-    if(a.length&&a.reduce((a,c)=>a+c)===0||!a.length)return a
-    let arr=[]
-    for(let i=0;i<a.length;i++){
-        for(let j=i+1;j<a.length;j++){
-            if(a.slice(i,j).reduce((a,c)=>a+c)===0)arr.push(a.slice(i,j))
-            if(j===a.length-1&&a.slice(i,j+1).reduce((a,c)=>a+c)===0)arr.push(a.slice(i,j+1))
-        }
-    }arr=arr.map((e,i)=>[e,i])
-    return !arr.length&&a.includes(0)?[0]:!arr.length?[]:arr.sort((a,b)=>b[0].length-a[0].length||b[1]-a[1])[0][0]
+function stairs(n){
+    let str='',arr=[],c=1
+    while(arr.length<n){
+       if(c>9)c=0
+       arr.push(c)
+       str+=' '.repeat((n*4)-arr.length*4)+arr.join` `+' '+arr.slice().reverse().join` `+'\n'
+       c++
+    }return str.slice(0,str.length-1)
 }
-  console.log(maxZeroSequence([78,-77,-7,16,-9]))
-  console.log(maxZeroSequence([-44,-12,79,64,-46,68,33,34,-73,-66,30,21,-60,-56,66,50,-100,94,93,42,28,24,-54,-81,-8,40,92,25,-76,-28,-25,-27,71,-49,-33,87,34,-4,-90,-88,-46,43,73,54,86,-97,-64,84,48,45,-44,38]))
+console.log(stairs(10))
