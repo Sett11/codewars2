a781f2b8a51a8015ae050de7d416a31f80e1e1d5
Jhonny <gonzodzen@mail.ru>
2023-02-24 21:18:02 +0300

    Complete The Pattern #8 - Number Pyramid

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function pattern(n){
-    let center=[],arr=[],c=1,str=''
-    while(arr.length<n){
-        arr.push(' '.repeat(n-1)+c+'\n')
+    let str='',arr=[],c=1
+     while(arr.length<n){
+        arr.push(c)
         c++
-        if(c===10)c=0
-    }arr=arr.concat(arr.slice(0,arr.length-1).reverse()),center=arr.slice().join``.replace(/[^0-9]/g,'')
-    arr=arr.slice(0,Math.floor(arr.length/2)).concat([center+'\n']).concat(arr.slice(Math.ceil(arr.length/2))),str=arr.join``
-    return str
-}
-console.log(pattern(3))
+    if(c===10)c=0}
+     for(let i=0;i<arr.length;i++){
+        str+=' '.repeat((arr.length)-i-1)+arr.slice(0,i).join``+arr[i]+arr.slice(0,i).reverse().join``+' '.repeat(arr.length-i-1)+'\n'
+     }return str.slice(0,str.length-1)
+   }
+   console.log(pattern(3))
