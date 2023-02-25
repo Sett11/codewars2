f8c4a6eabb82489d63fed6974ccfe6cf32788bc7
Jhonny <gonzodzen@mail.ru>
2023-02-25 12:56:59 +0300

    Complete The Pattern #12

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 function pattern(n){
-    let str='',arr=[],c=1
-     while(arr.length<n){
-        arr.push(c)
-        c++
-    if(c===10)c=0}
-     for(let i=0;i<arr.length;i++){
-        str+=' '.repeat((arr.length)-i-1)+arr.slice(0,i).join``+arr[i]+arr.slice(0,i).reverse().join``+' '.repeat(arr.length-i-1)+'\n'
-     }return str.slice(0,str.length-1)
-   }
-   console.log(pattern(3))
+  if(n<1)return ''
+ let arr=Array(n-1).fill(1).map((_,i)=>i+1).map(e=>e%10),str='',str2='',s='',s2='',res=[],arr2=Array(n-1).fill(1).map((_,i)=>i+1)
+ for(let i=0;i<arr.length;i++){
+  str+=' '.repeat((arr2[i]-1)<0?0:(arr2[i]))+arr[i]+'\n'
+ }
+ for(let i=0;i<arr.length;i++){
+  s+=' '.repeat((arr2.slice().reverse()[i]*2)<0?0:(arr2.slice().reverse()[i]*2-1))+arr[i]+' '.repeat(arr2[i]-1)+'\n'
+ }
+ str2=(str+' '.repeat(n)+((+(str.replace(/[\s]/g,'').split``.reverse()[0])+1)%10)+str.split('\n').reverse().join`\n`).split`\n`,s2=(s+' '.repeat(n-1)+s.split`\n`.reverse().join`\n`).split`\n`
+ for(let i=0;i<s2.length;i++){
+  res.push(str2[i].slice(1)+s2[i])
+ }
+ return res.join`\n`
+}
+  console.log(pattern(15))
