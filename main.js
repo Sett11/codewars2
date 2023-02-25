0462525109cb1b3d13443ae20543dd5cbe03352a
Jhonny <gonzodzen@mail.ru>
2023-02-25 17:00:36 +0300

    Is it a Set?

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-const sumMix=x=>x.reduce((a,c)=>a+=+c,0)
-console.log(sumMix([9, 3, '7', '3']))
+function isValidSet(q,s,c,p) {
+  return [...new Set(q)].length!==2&&[...new Set(s)].length!==2&&[...new Set(c)].length!==2&&[...new Set(p)].length!==2
+}
+console.log(isValidSet([1,2,1], ["diamond","diamond","snake"], ["blue","red","red"], ["blank","blank","striped"]))
