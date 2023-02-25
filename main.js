7662b44f04629995f3def9f39311883b054c895e
Jhonny <gonzodzen@mail.ru>
2023-02-25 16:41:56 +0300

    Sum Mixed Array

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
-function pattern(n){
-  if(n<1)return ''
-  let a=Array(n).fill(1).map((_,i)=>(i+1)%10).reverse(),b=Array(n).fill(1).map((_,i)=>i+1).reverse(),str='',c=1
-  for(let i=0;i<a.length;i++){
-    if(i===0){
-      str+=a.slice(0,i).join``+(a[i]+'').repeat(b[i]+1)+'\n'
-    }else{
-      str+=a.slice(0,i).join``+(a[i]+'').repeat(b[i])+'\n'
-    }
-  }return str.slice(1,str.length-1)
-}
-console.log(pattern(17))
+const sumMix=x=>x.reduce((a,c)=>a+=+c,0)
+console.log(sumMix([9, 3, '7', '3']))
