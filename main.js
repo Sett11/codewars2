6e7a2fa1b7ec969cbb9087d17531cef58c75a35b
Jhonny <gonzodzen@mail.ru>
2023-02-25 15:28:52 +0300

    Complete The Pattern #13

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function pattern(n,x,y){
+function pattern(n,x){
   if(n<1)return ''
- let arr=Array(n-1).fill(1).map((_,i)=>i+1).map(e=>e%10),str='',str2='',s='',s2='',res=[],arr2=Array(n-1).fill(1).map((_,i)=>i+1),ar=[],finRes=[]
+ let arr=Array(n-1).fill(1).map((_,i)=>i+1).map(e=>e%10),str='',str2='',s='',s2='',res=[],arr2=Array(n-1).fill(1).map((_,i)=>i+1),ar=[]
  for(let i=0;i<arr.length;i++){
   str+=' '.repeat((arr2[i]-1)<0?0:(arr2[i]))+arr[i]+'\n'
  }
@@ -11,22 +11,13 @@ function pattern(n,x,y){
  for(let i=0;i<s2.length;i++){
   res.push(str2[i].slice(1)+s2[i])
  }
- if(!x&&!y)return res.join`\n`
+ if(!x)return res.join`\n`
   ar=res.slice()
   while(x>1){
     for(let i=0;i<res.length;i++){
         ar[i]+=res[i].slice(1)
     }
     x--
-  }
-  if(x&&!y)return ar.join`\n`
- if(x&&y){
-  finRes=ar.slice()
-  while(y>1){
-    finRes=finRes.concat(ar.slice(1))
-    y--
-  }
-  return finRes.join`\n`
- }
+  }return ar.join`\n`
 }
-console.log(pattern(7,2,3))
+console.log(pattern(7,3))
