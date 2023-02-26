fc774f21f287a596efe5e1920ba7f0758379859e
Jhonny <gonzodzen@mail.ru>
2023-02-26 18:35:58 +0300

    Implementing a Queue

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-function sameFactRev(n) {
-  if(n<=1000)return []
-  if(n>1000&&n<=2000)return [1089]
- a=[1089, 2178, 4356, 6534, 8712, 9801, 10989, 21978, 24024, 26208, 42042, 43956, 48048],z=0
- for(let i=0;i<a.length;i++){
-  if(a[i]>n){z=i;break}
- }return a.slice(0,z!==0?z:a.length)
+class Queue{
+  constructor(a=[]){
+    this.a=a
+  }
 }
-console.log(sameFactRev(50000))
+
+Queue.prototype.enqueue = function(i) {
+  this.a.push(i)
+}
+
+Queue.prototype.dequeue = function() {
+  return this.a.shift()
+}
+
+Queue.prototype.size = function() {
+  return this.a.length
+}
