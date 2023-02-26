98c7a7ae5fa3144534604f189add29f532f14ca9
Jhonny <gonzodzen@mail.ru>
2023-02-26 17:26:41 +0300

    Reverse a Number

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-digitsAverage=j=>{
-  j=[...j+''],a=[]
-  while(j.length>1){
-    for(i=0;i<j.length-1;i++)a.push(Math.ceil((+j[i]+ +j[i+1])/2))
-    j=a,a=[]
-  }return +j[0]
-}
-
-console.log(digitsAverage(345))
+reverseNumber=(n,m=[...n+''])=>m.includes('-')?+(m[0]+m.slice(1).reverse().join``):+m.reverse().join``
+console.log(reverseNumber(321))
