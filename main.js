55ead6b17fe26f5b5eb433f9e3bcb3d08ebddbd8
Jhonny <gonzodzen@mail.ru>
2023-02-26 13:38:21 +0300

    One line task: Square a number Without *, + or Math.pow()

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
-const createIterator = function(f,n){
-  return function(){
-    let x=f(...arguments)
-    for(let i=0;i<n-1;i++){
-      x=f(x)
-    }return x
-  }
- }
-const getDouble = function (n) {
-  return n + n;
-}
-const doubleIterator = createIterator(getDouble,2)
-console.log(doubleIterator(5))
+sq=x=>~~-(-.5-x/(1/x))
+console.log(sq(4.44))
