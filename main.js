e658de02a9a813e0134d65a14bbc03d0a45f0f66
Jhonny <gonzodzen@mail.ru>
2023-02-26 14:32:30 +0300

    Digits Average

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-sd=x=>+[...x+''].map(e=>e**2).join``
-console.log(sd(3212))
+digitsAverage=j=>{
+  j=[...j+''],a=[]
+  while(j.length>1){
+    for(i=0;i<j.length-1;i++)a.push(Math.ceil((+j[i]+ +j[i+1])/2))
+    j=a,a=[]
+  }return +j[0]
+}
+
+console.log(digitsAverage(345))
