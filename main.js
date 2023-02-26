2069bebc69a0ab6ed1250582d8c47d6ec7a15ecb
Jhonny <gonzodzen@mail.ru>
2023-02-26 21:08:59 +0300

    IP Validation

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function isAValidMessage(m,n=+m[m.length-1]){
-  if(typeof n==='number'&&n===n)return false
-  m=m.replace(/\d+/g,' $& ').split` `.filter(e=>e).map(e=>+e===+e?+e:e.length)
-  for(let i=0;i<m.length-1;i+=2){if(m[i]!==m[i+1])return false}return true
+function isValidIP(str) {
+  if(JSON.stringify(str).replace(/"/g,'').replace(/[\d\.]/g,'').length!==0)return false
+  if(JSON.stringify(str).replace(/"/g,'').replace(/[^\.]/g,' ').match(/\.\./g))return false
+  const res=JSON.stringify(str).replace(/"/g,'').split``
+  if(+res[0]!==+res[0]||+res.slice().reverse()[0]!==+res.slice().reverse()[0])return false
+  console.log(str)
+  const check=JSON.stringify(str).split`.`.map(e=>e.replace(/"/g,'')).map(e=>{
+    if(e.length>1&&e[0]==='0')e='&'
+    return e
+  }).filter(e=>e&&e!=='&')
+  return check.length===4&&check.every(e=>+e===+e&&+e<256&&+e>=0&&e.replace(/[^\s]/g,'').length===0)
 }
-console.log(isAValidMessage("4code13hellocodewars"))
+console.log(isValidIP('abc.def.ghi.jkl') )
