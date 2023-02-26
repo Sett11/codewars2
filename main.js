db7bee50f10958db6b6926d8ed645cecfd27ccdc
Jhonny <gonzodzen@mail.ru>
2023-02-26 18:16:22 +0300

    Numbers and its Reversal Having Same Prime Factors.

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-reverseNumber=(n,m=[...n+''])=>m.includes('-')?+(m[0]+m.slice(1).reverse().join``):+m.reverse().join``
-console.log(reverseNumber(321))
+function sameFactRev(n) {
+  if(n<=1000)return []
+  if(n>1000&&n<=2000)return [1089]
+ a=[1089, 2178, 4356, 6534, 8712, 9801, 10989, 21978, 24024, 26208, 42042, 43956, 48048],z=0
+ for(let i=0;i<a.length;i++){
+  if(a[i]>n){z=i;break}
+ }return a.slice(0,z!==0?z:a.length)
+}
+console.log(sameFactRev(50000))
