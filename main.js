0e7ed989447b626a51fcb169fe99e930ee002cd8
Jhonny <gonzodzen@mail.ru>
2023-02-27 22:59:29 +0300

    Remove consecutive duplicate words

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-removeDuplicateWords=s=>[...new Set(s.split` `)].join` `
+const removeConsecutiveDuplicates=s=>{
+  s=s.split` `
+  for(let i=0;i<s.length-1;i++){
+    if(s[i]===s[i+1]){s.splice(i,1);i--}
+  }return s.join` `
+}
 
-console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
+console.log(removeConsecutiveDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
