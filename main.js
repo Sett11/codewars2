dceaa58636c9d2f0159d71cc0be05d8ea4f11e54
Jhonny <gonzodzen@mail.ru>
2023-02-27 22:45:04 +0300

    Bracket Duplicates

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-onlyDuplicates=s=>[...s].filter(e=>s.indexOf(e)!==s.lastIndexOf(e)).join``
+function stringParse(s){
+  if(typeof s!=='string')return "Please enter a valid string"
+  const l='abcdefghijklmnopqrstuvwxyz',m=l.toUpperCase()
+  for(let i=0;i<l.length;i++){
+    let r=RegExp(`${l[i]}{3,}`,'g'),z=RegExp(`${m[i]}{3,}`,'g')
+    s=s.replace(r,e=>e.slice(0,2)+'['+e.slice(2)+']'),s=s.replace(z,e=>e.slice(0,2)+'['+e.slice(2)+']')
+  }return s
+}
 
-console.log(onlyDuplicates('abccdefee'))
+console.log(stringParse('"aaaabbcdefffffffg"'))
