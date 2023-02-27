8ce85b30f8793162ce56bf18d8cd21985dac96a5
Jhonny <gonzodzen@mail.ru>
2023-02-27 12:14:26 +0300

    Text align right

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-function evaluate(a){
-  b=a.join``.replace(/[\+\*]/g,' $& ').trim().split` `
-  if(!+b[0]||!+b[b.length-1])return undefined
-  for(let i=0;i<a.length-1;i++){if(+a[i]===+a[i]&&+a[i+1]===+a[i+1])return undefined}
-  while(a.includes('*')){
-    let c=a.indexOf('*'),tmp=a.slice(c-1,c+2)
-   a=a.slice(0,c-1).concat([tmp[0]*tmp[2]]).concat(a.slice(c+2))
-  }
-  return a.filter(e=>+e===+e).map(Number).reduce((a,c)=>a+= +c)
+function alignRight(t,w){
+  if(t.length<w)return ' '.repeat(w-t.length)+t
+  let res=[],arr=[],c=0
+  for(let i=0;i<t.length;i++){
+    if(t[i]===' ')arr.push(i+1)
+    if(i>=w){res.push(t.slice(0,arr[arr.length-1]));t=t.slice(arr[arr.length-1]),i=0}
+    if(t.length<=w)break
+  }res.push(t);res.forEach(e=>c=Math.max(c,e.trim().length))
+  return res.map(e=>e.trim()).map(e=>' '.repeat(c-e.length<1?0:c-e.length)+e).join('\n')
 }
-console.log(evaluate(['10', '+', '20', '*','3', '+', '30']))
-console.log(evaluate(['10', '+', '20', '*','3', '*']))
-console.log(evaluate(['10', '20', '*','3']))
+console.log(alignRight("Two lines, I am",10))
