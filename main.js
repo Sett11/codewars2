249ab83bd6d5add1111e7348d5ce324ce7488b01
Jhonny <gonzodzen@mail.ru>
2023-02-27 22:26:38 +0300

    Only Duplicates

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function polybius (t) {
-  t=[...t],arr=[[ 'A', 'B', 'C', 'D', 'E' ],[ 'F', 'G', 'H', [ 'I', 'J' ], 'K' ],[ 'L', 'M', 'N', 'O', 'P' ],[ 'Q', 'R', 'S', 'T', 'U' ],[ 'V', 'W', 'X', 'Y', 'Z' ]],s=[]
-  for(let i=0;i<t.length;i++){
-    if(!t[i].match(/[A-Z]/g))s.push(t[i])
-      arr.map((e,j)=>e.map((u,z)=>typeof u==='string'&&u===t[i]?s.push([j+1,z+1]):typeof u==='object'&&u[0]===t[i]||u[1]===t[i]?s.push([j+1,z+1]):null))
-  }return s.filter(e=>typeof e==='object'||e===' ').map(e=>typeof e==='object'?e.join``:e).join``
- }
- 
- console.log(polybius('POLYBIUS SQUARE CIPHER'))
- console.log(polybius('CODEWARS'))
+onlyDuplicates=s=>[...s].filter(e=>s.indexOf(e)!==s.lastIndexOf(e)).join``
+
+console.log(onlyDuplicates('abccdefee'))
