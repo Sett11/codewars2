c76237e9612d6e052a958fe62de679d2707917d5
Jhonny <gonzodzen@mail.ru>
2023-02-28 01:43:41 +0300

    Find numbers which are divisible by given number

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const largestArrangement=b=>+b.map(e=>e+'').sort((a,b)=>(b+a)-(a+b)).join(``)
-
-console.log(largestArrangement([8, 6, 590, 70]))
-console.log(largestArrangement([4, 50, 8, 145]))
-console.log(largestArrangement([4, 40, 7]))
-console.log(largestArrangement([6, 73, 79, 356, 7]))
+function divisibleBy(n, d){
+  return n.filter(e=>e%d===0)
+}
