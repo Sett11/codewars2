e3004a855ce52314c21fce98338a9590800f51a8
Jhonny <gonzodzen@mail.ru>
2023-02-27 20:11:24 +0300

    Simple Fun #111: Reverse Brackets

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function replaceDashesAsOne(s) {
-  s=s.replace(/- +-|--/g,'-')
-  return s.match(/--|- +-/)?replaceDashesAsOne(s):s
+const reverseParentheses=(s,r=/\(([a-zA-Z]+)\)/g,v=/\(.+\)/g)=>{
+  if(!s.match(r)&&!s.match(v))return s
+  if(s.match(r))return reverseParentheses(s=s.replace(r,e=>[...e].reverse().join``.replace(/\(|\)/g,'')),r,v)
+  if(!s.match(r))return reverseParentheses(s=s.replace(v,e=>[...e].reverse().join``.replace(/\(|\)/g,'')),r,v)
 }
-console.log(replaceDashesAsOne("we-are- - - code----warriors.-"))
-console.log(replaceDashesAsOne("a---b- - -c"))
-console.log(replaceDashesAsOne(" - - gmsvdz--  - -yfdttxusa" ))
+
+console.log(reverseParentheses("Code(Cha(lle)nge)"))
+console.log(reverseParentheses("a(bcdefghijkl(mno)p)q"))
+console.log(reverseParentheses("f(niT((xjH)iQSk)QuMD)Hfu"))
+console.log(reverseParentheses("Ivjo(sGw((IDCZ(GWmlBVaUkI)y)(AkwFLq(qNmKmzF)uqd)wrw)cK)qI"))
