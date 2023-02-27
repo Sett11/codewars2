4f4313fdf6890ae704cfc98000df4e1a133077e7
Jhonny <gonzodzen@mail.ru>
2023-02-27 22:48:25 +0300

    Remove duplication

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-function stringParse(s){
-  if(typeof s!=='string')return "Please enter a valid string"
-  const l='abcdefghijklmnopqrstuvwxyz',m=l.toUpperCase()
-  for(let i=0;i<l.length;i++){
-    let r=RegExp(`${l[i]}{3,}`,'g'),z=RegExp(`${m[i]}{3,}`,'g')
-    s=s.replace(r,e=>e.slice(0,2)+'['+e.slice(2)+']'),s=s.replace(z,e=>e.slice(0,2)+'['+e.slice(2)+']')
-  }return s
-}
-
-console.log(stringParse('"aaaabbcdefffffffg"'))
+removeDuplication=a=>a.filter(e=>a.indexOf(e)===a.lastIndexOf(e))
