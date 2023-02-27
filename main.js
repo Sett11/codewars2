8f21d939e2460681a0eb4ffa9cdb31d7c7495aa5
Jhonny <gonzodzen@mail.ru>
2023-02-27 21:39:09 +0300

    Polybius square cipher - encode

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const removeParentheses=(s,r=/\([^\(\)]*\)/g)=>!s.match(r)?s:removeParentheses(s=s.replace(r,''),r)
-
-console.log(removeParentheses("example(unwanted thing)example"))
-console.log(removeParentheses("(first group) (second group) (third group)"))
+function polybius (t) {
+  t=[...t],arr=[[ 'A', 'B', 'C', 'D', 'E' ],[ 'F', 'G', 'H', [ 'I', 'J' ], 'K' ],[ 'L', 'M', 'N', 'O', 'P' ],[ 'Q', 'R', 'S', 'T', 'U' ],[ 'V', 'W', 'X', 'Y', 'Z' ]],s=[]
+  for(let i=0;i<t.length;i++){
+    if(!t[i].match(/[A-Z]/g))s.push(t[i])
+      arr.map((e,j)=>e.map((u,z)=>typeof u==='string'&&u===t[i]?s.push([j+1,z+1]):typeof u==='object'&&u[0]===t[i]||u[1]===t[i]?s.push([j+1,z+1]):null))
+  }return s.filter(e=>typeof e==='object'||e===' ').map(e=>typeof e==='object'?e.join``:e).join``
+ }
+ 
+ console.log(polybius('POLYBIUS SQUARE CIPHER'))
+ console.log(polybius('CODEWARS'))
