df30a6bfbe39750c7505a8fe7371f96773b9e180
Jhonny <gonzodzen@mail.ru>
2023-02-27 23:27:05 +0300

    Calculate number of inversions in array

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-function toBinaryString(n,arr=[]){
-  let v=n<0
-  if(v)n*=-1
-  arr.unshift(n%2);n=Math.floor(n/2)
-  if(n)arr=toBinaryString(n,arr)
-  else{
-    return arr.join``
-  }return (v?'-':'')+arr
+function countInversions(a){
+  if(a.length<2)return 0
+  let c=0
+  for(let i=0;i<a.length;i++){
+    for(let j=i+1;j<a.length;j++){if(a[i]>a[j])c++}
+  }return c
 }
-
-console.log(toBinaryString(15))
+console.log(countInversions([4, 3, 2, 1]))
+console.log(countInversions([2,1,3]))
+console.log(countInversions([6,5,4,3,2,1]))
