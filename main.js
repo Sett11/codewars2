948f210f229ebeb77ad643048c817eec69f7ac0b
Jhonny <gonzodzen@mail.ru>
2023-02-28 22:42:41 +0300

    Padovan numbers

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-subtract=(a,b)=> BigInt(a)-BigInt(b)+''
-
-
-console.log(subtract('30','69'))
+function padovan(n) {
+  const arr=[1,1,1]
+  if(arr.length>=n+1)return arr[n]
+  for(let i=3;arr.length<n+1;i++){
+    arr.push(arr[i-2]+arr[i-3])
+  }return arr[n]
+}
+console.log(padovan(9))
