54123595e3e2e58a973dfe48ec4d6ad8b0100562
Jhonny <gonzodzen@mail.ru>
2023-02-28 20:28:01 +0300

    Reverse Odd Count

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-oddCount=n=>n%2!=0?(n-1)/2:n/2
-
-console.log(oddCount(15023))
+function reverseOddCount(a,b=a.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}),c=[]){
+ a=a.map(e=>b[e]%2!==0?c.push(e)&&'&':e),c=c.reverse(),z=0
+ while(z<c.length){
+  let tmp=a.indexOf('&')
+  a[tmp]=c[z]
+  z++
+ }
+ return a
+}
+console.log(reverseOddCount(['a', 'a', 'b', 'c', 'c', 'd']))
