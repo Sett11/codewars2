10c0cfa7182d029ad99748610c060eacee9cb602
Jhonny <gonzodzen@mail.ru>
2023-02-28 20:13:45 +0300

    Count Odd Numbers below n

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-addedChar=(s1,s2)=>{
-  let c1=Object.entries([...s1].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})),c2=Object.entries([...s2].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})),r=''
-  c2.forEach(e=>c1.forEach(u=>u[0]===e[0]&&u[1]+3===e[1]?r=u[0]:0))
-  if(!r)c2.forEach(e=>!c1.map(u=>u[0]).join``.includes(e[0])?r=e[0]:0)
-  return r
-}
-console.log(addedChar("hello","checlclo"))
-console.log(addedChar("aabbcc","aacccbbcc"))
+oddCount=n=>n%2!=0?(n-1)/2:n/2
+
+console.log(oddCount(15023))
