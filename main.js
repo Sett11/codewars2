d288b158b7ebe5dbce087fe66143752405fbaf97
Jhonny <gonzodzen@mail.ru>
2023-02-28 19:15:00 +0300

    Is there a sequence re-occuring in the list

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-function isMagical(x,a=[],b=[],c=[],z=2){
-  if([...new Set(x)].length!==x.length)return false
-  a[0]=x.slice(0,3),a[1]=x.slice(3,6),a[2]=x.slice(6)
-  while(z){
-    for(let i=0;i<a.length;i++){
-      let innerArr=[]
-      for(let j=0;j<a.length;j++){
-        if(z===2)innerArr.push(a[j][i])
-        if(i==j)c.push(a[i][j])
-      }
-      b.push(innerArr),innerArr=[]
-    }
-   if(z===2)a=a.map(e=>e.reverse())
-    z--
-  }
-  return [c.slice(0,a.length)].concat([c.slice(a.length)]).concat(b).filter(e=>e.length).concat(a).map(e=>e.reduce((a,c)=>a+c,0)).every(e=>e===15)
+isReoccuring=a=>{
+  let arr=[]
+  for(let i=0;i<a.length;i++){
+    if(arr.includes(a[i])&&arr[arr.length-1]!==a[i])return true
+    else arr.push(a[i])
+  }return false
 }
-console.log(isMagical([4, 9, 2, 3, 5, 7, 8, 1, 6]))
+
+console.log(isReoccuring([0,0,1,1,2,2,1,1]))
+console.log(isReoccuring([0,0,1,1,2,2]))
