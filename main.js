115342266b630b560008b7a8e3c022c0b5aaa6e9
Jhonny <gonzodzen@mail.ru>
2023-02-28 17:26:40 +0300

    Grid index

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-replaceCommon=(s,l,m=Object.entries([...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).filter(e=>e[0]!==' ').map(e=>[...e,s.indexOf(e[0])]).sort((a,b)=>b[1]-a[1]||a[2]-b[2])[0][0])=>s.replace(RegExp(m,'g'),l)
-
-console.log(replaceCommon('real talk bro', 'n'))
+function gridIndex(g,i) {
+  return i.map(e=>g.flat(2)[e-1]).join``
+}
