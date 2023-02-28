5643fba2876ccd795f4f39c212742a650d57e444
Jhonny <gonzodzen@mail.ru>
2023-03-01 01:27:01 +0300

    Concatenated Sum

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function charConcat(s){
-  let c1=s.slice(0,~~s.length/2),c2=[...s.slice(~~s.length/2)].reverse().join``,str=''
-  for(let i=0;i<c1.length;i++){
-    str+=c1[i]+c2[i]+(i+1)
-  }
-  return str
-}
-console.log(charConcat("CodeWars"))
+checkConcatenatedSum=(n,m)=>[...(n+'')].map(e=> n<0?-+e.repeat(m):+e.repeat(m)).filter(e=>e).reduce((a,c)=>a+c,0)===n
+
+console.log(checkConcatenatedSum(-2997,3))
