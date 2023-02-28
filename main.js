180ad693bafbe302f82b16a196b4442dc5441f71
Jhonny <gonzodzen@mail.ru>
2023-02-28 13:51:44 +0300

    Card game

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-divisors=(n,i=2,a=[])=>i>=n/2+1&&!a.length?`${n} is prime`:i>=n/2+1?a:n%i===0?a.push(i)&&divisors(n,i+1,a):divisors(n,i+1,a)
-
-console.log(divisors(36))
+function cardGame(c1,c2,t) {
+  if(c1===c2)return "Someone cheats."
+  if(c1==='joker'&&c2!=='joker')return 'The first card won.'
+  if(c1!=='joker'&&c2==='joker')return 'The second card won.'
+  if(c1[c1.length-1]===c2[c2.length-1]&&c1.slice(0,c1.length-1)!==c2.slice(0,c2.length-1))return `The ${deck.indexOf(c1)>deck.indexOf(c2)?'first':'second'} card won.`
+  if(c1[c1.length-1]!==c2[c2.length-1]&&!c1.match(t)&&!c2.match(t))return "Let us play again."
+  if(c1.match(t)&&!c2.match(t))return 'The first card won.'
+  if(!c1.match(t)&&c2.match(t))return 'The second card won.'
+}
