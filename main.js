2a80d0fbc95a51c4827360734721458959b201ec
Jhonny <gonzodzen@mail.ru>
2023-02-28 10:43:12 +0300

    Ziiiiip!

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-function solution(a){
-  for(let i=0;i<a.length-1;i++){
-    if(Math.abs(a[i]-a[i+1])!==1)continue
-    if(Math.abs(a[i]-a[i+1])===1&&Math.abs(a[i+1]-a[i+2])===1){
-      for(let j=i+1;j<a.length;j++){
-        if(Math.abs(a[j]-a[j+1])!==1){
-          a=a.slice(0,i).concat([a[i],'-',a[j]]).concat(a.slice(j+1))
-          break
-        }
-      }
-    }
-  }return a.join`,`.replace(/,-,/g,'-')
+function zipObject(k,v=[]) { 
+  if(!k)return {}
+  const obj={}
+  if(k.every(e=>typeof e==='string')){
+    k.forEach((e,i)=>obj[e]=v[i])
+    return obj
+  }
+  if(!v.length&&k.every(e=>typeof e==='object')){
+   k.map(e=>obj[[e][0][0]]=e[1])
+   return obj
+  }
 }
- console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+console.log(zipObject(['fred', 'barney'], [30, 40]))
+console.log(zipObject([['fred', 30], ['barney', 40]]))
+console.log(zipObject([['fred'], ['barney']]))
+console.log(zipObject())
