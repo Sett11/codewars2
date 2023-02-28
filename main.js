4295c18c3b546e5ee0c117409cb30b2001ff7976
Jhonny <gonzodzen@mail.ru>
2023-02-28 18:27:29 +0300

    Magic Square Validator

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function magicSquare(a,b=[],c=[],z=2){
-  if(!a||a.flat().some(e=>!e)||!a.length)return false
+function isMagical(x,a=[],b=[],c=[],z=2){
+  if([...new Set(x)].length!==x.length)return false
+  a[0]=x.slice(0,3),a[1]=x.slice(3,6),a[2]=x.slice(6)
   while(z){
     for(let i=0;i<a.length;i++){
       let innerArr=[]
@@ -12,10 +13,6 @@ function magicSquare(a,b=[],c=[],z=2){
    if(z===2)a=a.map(e=>e.reverse())
     z--
   }
-  return [c.slice(0,a.length)].concat([c.slice(a.length)]).concat(b).filter(e=>e.length).concat(a).map(e=>e.reduce((a,c)=>a+c,0)).every((e,i,v)=>e===v[0]&&v[0]!==0)
+  return [c.slice(0,a.length)].concat([c.slice(a.length)]).concat(b).filter(e=>e.length).concat(a).map(e=>e.reduce((a,c)=>a+c,0)).every(e=>e===15)
 }
-console.log(magicSquare([
-  [8, 1, 6],
-  [3, 5, 7],
-  [4, 9, 2]
-]))
+console.log(isMagical([4, 9, 2, 3, 5, 7, 8, 1, 6]))
