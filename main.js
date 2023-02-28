a93a2fca2becd5f745e902555675f8074ea6df48
Jhonny <gonzodzen@mail.ru>
2023-02-28 03:31:29 +0300

    Range Extraction

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-function range(s=0,c,x=1) {
-  console.log(s,c,x)
-  if(s==c)return []
-  let arr=[],y=!c?s:c
-  for(let i=!c?0:s;i<y;i+=x){
-    arr.push(i)
-    if(arr.length==c-s)break
-  }return arr
+function solution(a){
+  for(let i=0;i<a.length-1;i++){
+    if(Math.abs(a[i]-a[i+1])!==1)continue
+    if(Math.abs(a[i]-a[i+1])===1&&Math.abs(a[i+1]-a[i+2])===1){
+      for(let j=i+1;j<a.length;j++){
+        if(Math.abs(a[j]-a[j+1])!==1){
+          a=a.slice(0,i).concat([a[i],'-',a[j]]).concat(a.slice(j+1))
+          break
+        }
+      }
+    }
+  }return a.join`,`.replace(/,-,/g,'-')
 }
-
-console.log(range(1,5,0))
+ console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
