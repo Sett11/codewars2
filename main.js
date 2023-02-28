17f5a20e725b9fe46f9dbf19400135218745a4f5
Jhonny <gonzodzen@mail.ru>
2023-02-28 20:49:29 +0300

    MinMaxMin: Bounded Nums

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-function reverseOddCount(a,b=a.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}),c=[]){
- a=a.map(e=>b[e]%2!==0?c.push(e)&&'&':e),c=c.reverse(),z=0
- while(z<c.length){
-  let tmp=a.indexOf('&')
-  a[tmp]=c[z]
-  z++
- }
- return a
+minMinMax=a=>{
+  let x=Math.min(...a)+1
+  while(a.includes(x)){
+    x++
+  }return [Math.min(...a),x,Math.max(...a)]
 }
-console.log(reverseOddCount(['a', 'a', 'b', 'c', 'c', 'd']))
+
+console.log(minMinMax([-1, 4, 5, -23, 24]))
+console.log(minMinMax([1, 3, -3, -2, 8, -1]))
