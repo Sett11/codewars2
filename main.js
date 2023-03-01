05e356af75fbc10833370abdafce13654e9e1c00
Jhonny <gonzodzen@mail.ru>
2023-03-01 21:31:08 +0300

    Be Concise IV - Index of an element in an array

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-calculate=(s,c=s.split`+`.map(e=>e.replace(/ /g,'')))=>c.length===1?+c[0]:c.length===2?+c[0]+ +c[1]:+c[0]+ +c[1]+ +c[2]
-
-console.log(calculate("1+2+3"))
-console.log(calculate("1+2 3"))
-console.log(calculate("1 2 3"))
+let find=(a,e,c=a.indexOf(e))=>c>-1?c:'Not found'
