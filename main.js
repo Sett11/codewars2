667e2c93449d130edaa11522ebc06d5303e9b2c5
Jhonny <gonzodzen@mail.ru>
2023-03-01 21:22:56 +0300

    Calculator (Addition and Spaces) [Part 1]

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function solution(s) {
-  let a=[],b=[],c=[],d=[];s.split`,`.map(e=>e.split`_`).map(e=>e[0]==='fruit'?a.push(e[1]):e[0]==='meat'?b.push(e[1]):e[0]==='vegetable'?d.push(e[1]):c.push(e[1]))
-  return `fruit:${a.sort().join`,`}\nmeat:${b.sort().join`,`}\nother:${c.sort().join`,`}\nvegetable:${d.sort().join`,`}`
-}
-console.log(solution("fruit_banana,vegetable_carrot,fruit_apple,canned_sardines,drink_juice,fruit_orange"))
+calculate=(s,c=s.split`+`.map(e=>e.replace(/ /g,'')))=>c.length===1?+c[0]:c.length===2?+c[0]+ +c[1]:+c[0]+ +c[1]+ +c[2]
+
+console.log(calculate("1+2+3"))
+console.log(calculate("1+2 3"))
+console.log(calculate("1 2 3"))
