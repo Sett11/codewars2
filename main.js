bb6e8d7cef9673b7c7febe2622ede915fcc8a20e
Jhonny <gonzodzen@mail.ru>
2023-03-01 20:45:06 +0300

    Group Anagrams

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-twistedSum=n=>Array(n).fill(1).map((e,i)=>(i+1)<10?i+1:[...(i+1)+''].reduce((a,c)=>+a+ +c,0)).reduce((a,c)=>a+c)
-console.log(twistedSum(10))
+function groupAnagrams(w){
+  w=w.map(e=>[e,[...e].sort().join``]).sort((a,b)=>a[1].localeCompare(b[1])).map(e=>[e[0]])
+  for(let i=0;i<w.length-1;i++){
+    if([...w[i][0]].sort().join``===[...w[i+1][0]].sort().join``){
+      w[i]=w[i].concat([w[i+1][0]])
+      w.splice(i+1,1)
+      i--
+    }
+  }return w
+}
+console.log(groupAnagrams(["tsar", "rat", "tar", "star", "tars", "cheese"]))
