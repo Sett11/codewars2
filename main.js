9cc0f9b7c737689f2fc11029d8aeeeb5757bd4a7
Jhonny <gonzodzen@mail.ru>
2023-03-02 21:39:21 +0300

    Sub-array division

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-function f(s,a=[]){
-  if([...new Set(s)].length===1)return [s[0],s.length]
-  for(let i=1;i<s.length+1;i++){
-    for(let j=1;j<s.length/2+1;j++){
-      if(s.slice(0,i).repeat(j)===s)a.push([s.slice(0,i),j])
-    }
-  }return a.sort((a,b)=>b[1]-a[1])[0]
-}
-console.log(f('abcde'))
-console.log(f('aaaaa'))
+function solve(a,n) {
+    if(a.length===1&&a[0]%n!==0)return false
+    if(a.reduce((a,c)=>a+c,0)%n===0)return true
+      a=a.slice().sort((a,b)=>a-b),c=0,r=[],v=[],s=x=>a[x]
+      a.push(n+1)
+      for(let i=0;i<a.length;i++){
+          c+=a[i]
+          if(c>n){
+              let tmp=r.pop()
+              c=c-a[i]-a[tmp]
+              i=tmp
+          }else{
+              r.push(i)
+              if(c%n===0)v.push(r.map(s))
+          }
+      }
+      for(let i=0;i<a.length;i++){
+          for(let j=i+1;j<a.length;j++){
+              if((a[i]+a[j])%n===0)return true
+          }
+      }
+      return !!v.length
+  }
+console.log(solve([1,2,3,4,5],10))
+console.log(solve([ 8, 5, 3, 9 ],7))
