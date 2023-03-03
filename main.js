dfefb683af9fff1c8b92df39f3da7030b2c4a2ae
Jhonny <gonzodzen@mail.ru>
2023-03-03 20:07:48 +0300

    Simple Pig Latin

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-testIt=(a,b)=>[...a+''].reduce((a,c)=>a+ +c,0)*[...b+''].reduce((a,c)=>a+ +c,0)
+pigIt=s=>s.split` `.map(e=>{
+    if(!e.match(/[a-zA-Z]/g))return e
+    e=[e.replace(/[^a-zA-Z]/g,''),e.replace(/[a-z]/gi,'')]
+    e[0]=e[0].slice(1)+e[0].charAt(0)+'ay'+e[1]
+    return e[0]
+}).join` `
 
-console.log(testIt(123,45))
+console.log(pigIt('Pig, latin is cool'))
+console.log(pigIt('O tempora o mores !'))
