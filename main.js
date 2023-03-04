2d5e557bda98d3e17121d169ae6434c748362888
Jhonny <gonzodzen@mail.ru>
2023-03-04 11:16:44 +0300

    Advanced Pig Latin

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-pigIt=s=>s.split` `.map(e=>{
-    if(!e.match(/[a-zA-Z]/g))return e
-    e=[e.replace(/[^a-zA-Z]/g,''),e.replace(/[a-z]/gi,'')]
-    e[0]=e[0].slice(1)+e[0].charAt(0)+'ay'+e[1]
-    return e[0]
-}).join` `
+translate=s=>{
+    return s.split` `.map(e=>{
+        console.log(s)
+        if(e[0][0].match(/a|u|i|o|e/gi))return e.replace(/[^a-zA-Z]/g,'')+'way'+e.replace(/[a-zA-Z]/g,'')
+        e=[e.replace(/[^a-zA-Z]/g,''),e.replace(/[a-z]/gi,''),[]]
+        for(let i=0;i<e[0].length;i++){
+            if(e[0][i]===e[0][i].toUpperCase()){
+                e[2].push(i)
+            }
+        }
+            while(!e[0][0].match(/a|u|i|o|e/gi)){
+                e[0]=e[0].slice(1)+e[0].charAt(0)
+        }
+        e[0]=e[0].toLowerCase().split``.map((u,j)=>e[2].includes(j)?u.toUpperCase():u).join``+'ay'+e[1]
+        return e[0]
+    }).join` `
+}
 
-console.log(pigIt('Pig, latin is cool'))
-console.log(pigIt('O tempora o mores !'))
+console.log(translate('Hello everyone. Welcome to the code kata.'))
