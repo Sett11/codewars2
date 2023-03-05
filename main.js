2465276ccc6fe02232897af544e732caf2237411
Jhonny <gonzodzen@mail.ru>
2023-03-05 15:21:52 +0300

    Count Decimal Places

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-palindromize=n=>{
-    let z=[...n+''].join``,v=[...z].reverse().join``,c=0
-    if(z===v)return `${c} ${n}`
-    while(z!==v){
-        n+=+v
-        z=[...n+''].join``,v=[...z].reverse().join``,c+=1
-    }return `${c} ${n}`
-}
+decimalPlaces=(n,m=n+'',g=m.includes('.')?m.indexOf('.'):m.indexOf('e'),r=m.slice(g).replace(/\./g,''))=>!m.includes('.')&&!m.includes('e')?0:r.replace(/e-\d+/,e=>'0'.repeat(+e.replace(/\D/g,''))).length
 
-console.log(palindromize(750))
+
+  console.log(decimalPlaces(3.14))
+  console.log(decimalPlaces(-3.14e-21))
+  console.log(decimalPlaces(2.e-14))
+  console.log(decimalPlaces(NaN))
