6387bc478f762ca47423a7a135aca85ae05ee70c
Jhonny <gonzodzen@mail.ru>
2023-03-05 16:30:48 +0300

    COFFEE!

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-function treasureCode(s) {
-    s=s.replace(/\d+/g,'-$&-').split`-`.map(e=>+e===+e?+e:e).filter(e=>e),c=0,newS=s.filter(e=>typeof e==='number')
-    for(let i=0;i<Math.max(...newS);i++){
-        if(newS.every(e=>e%i===0))c=Math.max(c,i)
-    }return s.map(e=>typeof e==='number'?e/c:e).join``
-}
-  console.log(treasureCode("Y14U7I7P21"))
+coffee=s=>s.replace(/coffee/gi,e=>e.toUpperCase())
+
+  console.log(coffee( "Did I only have two cups of coffee this morning? I need more."))
