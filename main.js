bed5ee878129312694f36fbe02f70fe632a3c90e
Jhonny <gonzodzen@mail.ru>
2023-03-06 00:14:58 +0300

    Happy numbers

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-findSubarrMaxSum=(a,arr=[],b=[])=>{
-    if(a.every(e=>e<=0))return [[],0]
-    arr.push(a.reduce((a,c)=>a+c,0))
-    for(let i=0;i<a.length+1;i++){
-        for(let j=i;j<a.length+1;j++){
-            let tmp=a.slice(i,j),s=tmp.reduce((a,c)=>a+c,0)
-            if(s>=Math.max(...arr)){
-                arr.push(s)
-                b.push(tmp)
-            }
-        }
+function isHappy(n){
+    if(n===1)return true
+    const a=[n]
+    while(1){
+        n=[...n+''].map(e=>+(e**2)).reduce((a,c)=>a+c)
+        if(a.includes(n))return false
+        if(n===1)return true
+        a.push(n)
     }
-    b=b.filter(e=>e.reduce((a,c)=>a+c,0)===Math.max(...arr))
-    if(b.length>1)b=[b]
-    b.push(Math.max(...arr))
-    return b
-}
+  }
 
-console.log(findSubarrMaxSum([4, -1, 2, 1, -40, 1, 2, -1, 4]))
-console.log(findSubarrMaxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+  console.log(isHappy(1))
