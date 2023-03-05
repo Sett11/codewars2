ac96035851c7bfdd4c8bf0df13d094d4c1bc7024
Jhonny <gonzodzen@mail.ru>
2023-03-05 15:35:30 +0300

    Symbols counted

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-decimalPlaces=(n,m=n+'',g=m.includes('.')?m.indexOf('.'):m.indexOf('e'),r=m.slice(g).replace(/\./g,''))=>!m.includes('.')&&!m.includes('e')?0:r.replace(/e-\d+/,e=>'0'.repeat(+e.replace(/\D/g,''))).length
+transform=s=>Object.entries([...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).map(e=>e[1]>1?e[0]+e[1]:e[0]).join``
 
-
-  console.log(decimalPlaces(3.14))
-  console.log(decimalPlaces(-3.14e-21))
-  console.log(decimalPlaces(2.e-14))
-  console.log(decimalPlaces(NaN))
+console.log(transform('economics'))
