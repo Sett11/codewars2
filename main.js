5f513bf818cd1364f0ef4f8bb2599777fb6d5257
Jhonny <gonzodzen@mail.ru>
2023-03-06 00:19:09 +0300

    Happy Numbers

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-function isHappy(n){
-    if(n===1)return true
-    const a=[n]
-    while(1){
-        n=[...n+''].map(e=>+(e**2)).reduce((a,c)=>a+c)
-        if(a.includes(n))return false
+function happyNumbers(x){
+    const isHappy=n=>{
         if(n===1)return true
-        a.push(n)
-    }
-  }
+        const a=[n]
+        while(1){
+            n=[...n+''].map(e=>+(e**2)).reduce((a,c)=>a+c)
+            if(a.includes(n))return false
+            if(n===1)return true
+            a.push(n)
+        }
+      }
+  const arr=Array(x).fill(1).map((e,i)=>i+1)
+  return arr.filter(e=>isHappy(e))
+}
 
-  console.log(isHappy(1))
+  console.log(happyNumbers(100))
