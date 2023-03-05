7b35f0100ab7a50379fc80e1edc85eba14c53645
Jhonny <gonzodzen@mail.ru>
2023-03-05 16:40:29 +0300

    How much coffee do you need?

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-coffee=s=>s.replace(/coffee/gi,e=>e.toUpperCase())
-
-  console.log(coffee( "Did I only have two cups of coffee this morning? I need more."))
+function howMuchCoffee(e) {
+  let c=0
+  for(let i=0;i<e.length;i++){
+    if(e[i]==='cw')c+=1
+    if(e[i]==='CW')c+=2
+    if(e[i]==='cat')c+=1
+    if(e[i]==='CAT')c+=2
+    if(e[i]==='dog')c+=1
+    if(e[i]==='DOG')c+=2
+    if(e[i]==='movie')c+=1
+    if(e[i]==='MOVIE')c+=2
+  }
+  return c>3?'You need extra sleep':c
+}
+console.log(howMuchCoffee(['cw','CAT']))
