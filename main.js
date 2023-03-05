5e81ba35ae773ee8d1dd71f85a954882705dd041
Jhonny <gonzodzen@mail.ru>
2023-03-05 14:48:10 +0300

    Palindromic Numbers

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-convertPalindromes=n=>n.map(e=>[...e+''].join``===[...e+''].reverse().join``?1:0)
+palindromize=n=>{
+    let z=[...n+''].join``,v=[...z].reverse().join``,c=0
+    if(z===v)return `${c} ${n}`
+    while(z!==v){
+        n+=+v
+        z=[...n+''].join``,v=[...z].reverse().join``,c+=1
+    }return `${c} ${n}`
+}
+
+console.log(palindromize(750))
