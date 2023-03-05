2a6460a288b81e65460519a4c49976387e426e51
Jhonny <gonzodzen@mail.ru>
2023-03-05 17:24:43 +0300

    I need more speed!

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-partsSums=a=>{
-    let r=[0]
-    for(let i in a.reverse()){
-        r.push(a[i]+r[i])
-    }
-    return r.reverse()
+function reverse(a) {
+    for(let i=0,j=a.length-1; i<j; i++,j--){
+        tmp=a[j]
+        a[j]=a[i];
+        a[i]=tmp;
+    }return a
 }
 
-console.log(partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]))
+ console.log(reverse([1,2,3,4,5]))
