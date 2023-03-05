68e48befb1bb1be2829fbec474358889c1d792d4
Jhonny <gonzodzen@mail.ru>
2023-03-05 22:21:51 +0300

    Palindromes Below

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function addArrays(a1,a2) {
-    if(!a1.length&&!a2.length)return []
-    let a=a1.join``,b=a2.join``,r=[...((+a+ +b)+'')]
-    for(let i=0;i<r.length;i++){
-        if(r[i]==='-'){
-            r[i+1]=r[i]+r[i+1]
-            r.splice(i,1)
-        }
-    }
-    return r.map(e=>+e)
-  }
-  console.log(addArrays([3,2,6,6],[-7,2,2,8] ))
+Number.prototype.palindromeBelow = function(b,a=[]){
+    for(let i=1;i<+this.toString();i++){
+        let tmp=(i).toString(b)
+        if(tmp===[...tmp].reverse().join``)a.push(i)
+    }return a
+}
+
+console.log((15).palindromeBelow(10))
