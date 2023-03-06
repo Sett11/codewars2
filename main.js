16456861dd2067e1d781c525c603ed73fc891a90
Jhonny <gonzodzen@mail.ru>
2023-03-06 23:22:27 +0300

    So basic

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-function alphabetWar(c,s=[...('oo'+c+'oo')],l=0,r=0,left=['t','s','b','p','w'],right=['j','z','d','q','m']){
-  for(let i=2;i<s.length-2;i++){
-    if(s[i]===left[0]&&right.includes(s[i+1])&&s[i+1]!==right[0]&&s[i+2]!==right[0]){
-        s[i+1]=left[right.indexOf(s[i+1])]
-    }
-    if(s[i]===left[0]&&right.includes(s[i-1])&&s[i-1]!==right[0]&&s[i-2]!==right[0]){
-        s[i-1]=left[right.indexOf(s[i-1])]
-    }
-    if(s[i]===right[0]&&left.includes(s[i+1])&&s[i+1]!==left[0]&&s[i+2]!==left[0]){
-        s[i+1]=right[left.indexOf(s[i+1])]
-    }
-    if(s[i]===right[0]&&left.includes(s[i-1])&&s[i-1]!==left[0]&&s[i-2]!==left[0]){
-        s[i-1]=right[left.indexOf(s[i-1])]
-    }
-  }
- s.slice(2,-2).forEach(e=>left.includes(e)?l+=left.indexOf(e):right.includes(e)?r+=right.indexOf(e):0)
- return r>l?'Right side wins!':r<l?'Left side wins!':"Let's fight again!"
-}
+convertBase20ToDecimal=(i,r=parseInt(i,20))=>r===r?r:-1
+    
 
-console.log(alphabetWar('ztztztzs'))
-console.log(alphabetWar('zdqmwpbs'))
-console.log(alphabetWar('wololooooo'))
+
+  console.log(convertBase20ToDecimal('50'))
