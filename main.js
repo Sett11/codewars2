87bfa83248a94f686b3d15031697b92487748163
Jhonny <gonzodzen@mail.ru>
2023-03-06 20:19:14 +0300

    Snail

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-function commas(n,z=n+''){
-      n=(n).toFixed(3),m=n.replace(/\d+\./,''),n=[...(n.match(/\d+\./g)[0].replace(/\./,''))].reverse(),a=[]
-      if(z.slice(z.indexOf('.')).replace(/\./,'').length<3)m=m.replace(/0/g,'')
-      for(let i=0;i<n.length;i+=3){
-          a.push(n.slice(i,i+3))
-      }a=a.map(e=>e.reverse().join``).reverse().join`,`
-      let r=(z[0]==='-'?'-':'')+a+(z.includes('.')?'.'+m.replace(/-/g,''):'')
-      if(r[r.length-1]==='0'&&z.includes('.'))r=r.slice(0,r.length-1)
-      if(r[r.length-1]==='0'&&z.includes('.'))r=r.slice(0,r.length-1)
-      return r
-  }
+snail=(a,b=[])=>{
+    if(a.length<2){
+        if(typeof a[0]!=='number'){
+            b.push(...a[0])
+        }
+        if(typeof a[0]==='number'){
+            b.push(a[0])
+        }
+        return b.filter(e=>e)
+    }
+    if(typeof a[0]!=='number'&&a[0]){
+        b.push(...a.shift())
+    }
+    if(typeof a[0]==='number'){
+        b.push(a.shift())
+    }
+    for(let i=0;i<a.length-1;i++){
+        if(typeof a[i]!=='number'){
+            b.push(a[i].pop())
+        }
+        if(typeof a[i]==='number'){
+            b.push(a.pop())
+        }
+    }
+    if(typeof a[a.length-1]!=='number'&&a[a.length-1]){
+        b.push(...a.pop().reverse())
+    }
+    if(typeof a[a.length-1]==='number'){
+        b.push(...a.pop())
+    }
+    let innerArr=[]
+    for(let i=0;i<a.length;i++){
+        if(typeof a[i]!=='number'){
+            innerArr.push(a[i].shift())
+        }
+        if(typeof a[i]==='number'){
+            innerArr.push(a.shift())
+        }
+    }
+    if(innerArr.length){
+        b.push(...innerArr.reverse())
+    }
+    return snail(a,b)
+}
 
-  console.log(commas(100.2346))
-  console.log(commas(1000000000.23))
-  console.log(commas(-171481.70013760368))
+  console.log(snail([[1, 2, 3, 4, 5, 6], [20, 21, 22, 23, 24, 7], [19, 32, 33, 34, 25, 8], [18, 31, 36, 35, 26, 9], [17, 30, 29, 28, 27, 10], [16, 15, 14, 13, 12, 11]]))
