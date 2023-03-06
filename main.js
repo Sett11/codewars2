eaa8702427fec02af8ed67d93b1d341fb8e9b094
Jhonny <gonzodzen@mail.ru>
2023-03-06 23:14:37 +0300

    Alphabet war - Wo lo loooooo priests join the war

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-deNico=(t,m,a=[])=>{ 
-    let newT=[...t],k=[...t].map((e,i)=>[e,i+1]).sort((a,b)=>a[0].localeCompare(b[0])).map((e,i)=>[...e,i+1])
-    k.forEach(e=>newT[newT.indexOf(e[0])]=[e[1],e[2]])
-    for(let i=0;i<m.length;i+=k.length){
-                let tmp=m.slice(i,i+k.length)
-                if(tmp.length<k.length)tmp=tmp+' '.repeat(k.length-tmp.length)
-                a.push(tmp)
-            }
-    return a.map(e=>e.split``.map((e,j,v)=>{
-        e=[e,0]
-        for(let i=0;i<newT.length;i++){if(newT[i][1]===j+1)e[1]=newT[i][0]}
-        return e
-    })).map(e=>e.sort((a,b)=>a[1]-b[1])).map(e=>e.map(u=>u[0]).join``).join``.replace(/\s+$/g,'')
+function alphabetWar(c,s=[...('oo'+c+'oo')],l=0,r=0,left=['t','s','b','p','w'],right=['j','z','d','q','m']){
+  for(let i=2;i<s.length-2;i++){
+    if(s[i]===left[0]&&right.includes(s[i+1])&&s[i+1]!==right[0]&&s[i+2]!==right[0]){
+        s[i+1]=left[right.indexOf(s[i+1])]
+    }
+    if(s[i]===left[0]&&right.includes(s[i-1])&&s[i-1]!==right[0]&&s[i-2]!==right[0]){
+        s[i-1]=left[right.indexOf(s[i-1])]
+    }
+    if(s[i]===right[0]&&left.includes(s[i+1])&&s[i+1]!==left[0]&&s[i+2]!==left[0]){
+        s[i+1]=right[left.indexOf(s[i+1])]
+    }
+    if(s[i]===right[0]&&left.includes(s[i-1])&&s[i-1]!==left[0]&&s[i-2]!==left[0]){
+        s[i-1]=right[left.indexOf(s[i-1])]
+    }
+  }
+ s.slice(2,-2).forEach(e=>left.includes(e)?l+=left.indexOf(e):right.includes(e)?r+=right.indexOf(e):0)
+ return r>l?'Right side wins!':r<l?'Left side wins!':"Let's fight again!"
 }
 
-
-
-// nico=(t,m,a=[])=>{
-//     let newT=[...t],k=[...t].map((e,i)=>[e,i+1]).sort((a,b)=>a[0].localeCompare(b[0])).map((e,i)=>[...e,i+1])
-//     k.forEach(e=>newT[newT.indexOf(e[0])]=e[2])
-//     for(let i=0;i<m.length;i+=k.length){
-//         let tmp=m.slice(i,i+k.length)
-//         if(tmp.length<k.length)tmp=tmp+' '.repeat(k.length-tmp.length)
-//         a.push(tmp)
-//     }
-//     return a.map(e=>e.split``.map((u,i)=>[u,newT[i]])).map(e=>e.sort((a,b)=>a[1]-b[1])).map(e=>e.map(u=>u[0]).join``).join``
-// }
-
-console.log(deNico("crazy","cseerntiofarmit on  "))
+console.log(alphabetWar('ztztztzs'))
+console.log(alphabetWar('zdqmwpbs'))
+console.log(alphabetWar('wololooooo'))
