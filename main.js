8e085979ca80ee2b236085765897450e5882f3bf
Jhonny <gonzodzen@mail.ru>
2023-03-06 21:59:46 +0300

    Basic Nico variation

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-kaCokadekaMe=(w,s='ka'+w.replace(/[aioue]+/gi,e=>e+'ka'))=>w[w.length-1].match(/[aioue]/gi)?s.slice(0,s.length-2):s
+nico=(t,m,a=[])=>{
+    let newT=[...t],k=[...t].map((e,i)=>[e,i+1]).sort((a,b)=>a[0].localeCompare(b[0])).map((e,i)=>[...e,i+1])
+    k.forEach(e=>newT[newT.indexOf(e[0])]=e[2])
+    for(let i=0;i<m.length;i+=k.length){
+        let tmp=m.slice(i,i+k.length)
+        if(tmp.length<k.length)tmp=tmp+' '.repeat(k.length-tmp.length)
+        a.push(tmp)
+    }
+    return a.map(e=>e.split``.map((u,i)=>[u,newT[i]])).map(e=>e.sort((a,b)=>a[1]-b[1])).map(e=>e.map(u=>u[0]).join``).join``
+}
 
-console.log(kaCokadekaMe('maintenance'))
+console.log(nico("a", "message"))
