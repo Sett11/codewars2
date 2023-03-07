57695c773f96f23c109276ab11e8e735d463a1df
Jhonny <gonzodzen@mail.ru>
2023-03-07 12:09:00 +0300

    BitMath: Addition

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function getMichaelLastName(s,a=[]) {
-    s.split` `.forEach((e,i,v)=>e==='Michael'?a.push(v[i+1]):0)
-    return a.filter(e=>e.charAt(0)===e.charAt(0).toUpperCase()?e:0).map(e=>e.replace(/[^a-z]/gi,''))
+class BitMath {
+  static add(a, b) {
+    if(b===0)return a
+    let s=a^b,c=(a&b)<<1
+    return this.add(s,c)
+  }
 }
 
-  console.log(getMichaelLastName("Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?"))
+console.log(BitMath.add(5,5))
