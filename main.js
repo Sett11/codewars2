b4db7370f43e8bdee130fa3cfd8d87f96cb550d0
Jhonny <gonzodzen@mail.ru>
2023-03-07 21:37:21 +0300

    Return String As Sorted Blocks

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-function sortString(s,o,t=[...new Set(o)].map((e,i)=>[e,i]),r='') {
-   return [...s].map((e,i,v)=>{
-    for(let i=0;i<t.length;i++){
-      if(e===t[i][0])e=[e,t[i][1]]
+function blocks(s,a=[],b=[],c=[],r=[],z=0){
+  if(z===0){
+  s=s.replace(/[a-z]+|[A-Z]+|[0-9]+/g,e=>e.match(/[a-z]/g)?a.push(e):e.match(/[A-Z]+/g)?b.push(e):c.push(e))
+  a=a.join``.split``.sort(),b=b.join``.split``.sort(),c=c.join``.split``.map(Number).sort((a,b)=>a-b),t=[a,b,c]
+  s=''
+  }
+  for(let i=0;i<t.length;i++){
+    for(let j=0;j<t[i].length;j++){
+      if(!r.slice(r.lastIndexOf('-')).includes(t[i][j])){
+        r.push(t[i][j])
+        t[i][j]='&'
+      }
     }
-    if(e.length===2)return e
-    else r+=e
-  }).filter(e=>e).sort((a,b)=>a[1]-b[1]).map(e=>e[0]).join``+r
+  }
+  t=t.map(e=>e.filter(u=>u!=='&'))
+  if(t.every(e=>!e.length)){return r.join``}
+  else{
+    r.push('-')
+    return blocks(s,a,b,c,r,z+1)
+  }
 }
-console.log(sortString("banana","abn"))
+console.log(blocks('hbh420sUUW222IWOxndjn93cdop69NICEep832'))
