b86a48532e0f80214fe79be6cca4dc2c01bd3697
Jhonny <gonzodzen@mail.ru>
2023-03-07 20:15:38 +0300

    Frequency Analysis With Buckets

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-function add(a,b) {
-  if(+a<+b){
-    let tmp=a
-    a=b
-    b=tmp
-  }
-  if(a.length<b.length)a='0'.repeat(b.length-a.length)+a
-  if(a.length>b.length)b='0'.repeat(a.length-b.length)+b
-  a=[...a].map(Number),b=[...b].map(Number),s=''
-  for(let i=a.length-1;i>=0;i--){
-    let c=(a[i]+b[i])%10,r=(a[i]+b[i])>9?1:0
-    if(r)b[i-1]+=1
-    s+=c
-    if(i===0&&r)s+=r
-  }
-  return [...s].reverse().join``
+function bucketize(a,b=Object.entries(a.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))) {
+  return Array(a.length+1).fill(1).map((e,i)=>[i+1,[]]).map((e,j,v)=>{
+    for(let i=0;i<b.length;i++){
+      if(b[i][1]===j){
+        e[1].push(+b[i][0])
+      }
+    }
+    return e[1]
+  }).map(e=>!e.length?null:e)
 }
 
-console.log(add("888", "222"))
-console.log(add("123", "321"))
-console.log(add("1372", "69"))
-console.log(add('63829983432984289347293874', '90938498237058927340892374089'))
-
-//"91002328220491911630239667963"
+console.log(bucketize([1,2,3,4,4,5,5,5]))
