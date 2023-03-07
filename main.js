531437e67f9092f42fb2f5149a3a0aa6071a91b5
Jhonny <gonzodzen@mail.ru>
2023-03-07 15:08:35 +0300

    Grab CSV Columns

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-function sortArray(a,b=[],c=[]) {
-  a.forEach((e,i)=>e%2!==0?b.push(e):c.push(e)),b.sort((a,b)=>a-b),c.sort((a,b)=>b-a)
-  for(let i=0;i<a.length;i++){
-    if(a[i]%2!==0)a[i]=b.shift()
-    if(a[i]%2===0)a[i]=c.shift()
-  }
-  return a
+function csvColumns(c,i) {
+	c=c.split`\n`.map(e=>e.split`,`),i=i.filter(e=>e<=c[0].length-1)
+  return !i.length?'':c.map(e=>e.filter((e,j)=>i.includes(j)).join`,`).join`\n`
 }
 
-console.log(sortArray([1, 111, 11, 11, 2, 1, 5, 0]))
+console.log(csvColumns("1,2,3\n4,5,6",[0, 1]))
+console.log(csvColumns("a,b,c,d,e\n1,2,3,4,5\nf,g,h,i,j", [1, 3, 5, 7]))
