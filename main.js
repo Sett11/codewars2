84017a87b59c033c0f352bb25ac95145af3b484e
Jhonny <gonzodzen@mail.ru>
2023-03-07 23:40:19 +0300

    String average

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-function findSecretMessage(p) {
-  p=p.replace(/[^a-zA-Z ]/g,'').split` `.map(e=>e.toLowerCase()).filter((e,i,v)=>v.indexOf(e)!==v.lastIndexOf(e)),a=[],r=[]
-  for(let i=0;i<p.length;i++){
-    if(a.includes(p[i])&&!r.includes(p[i]))r.push(p[i])
-    a.push(p[i])
-  }
-  return r.join` `
+function averageString(s) {
+  const check=['zero','one','two','three','four','five','six','seven','eight','nine'],newS=s.split` `.map(e=>check.indexOf(e))
+  return newS.includes(-1)?'n/a':check[Math.floor(newS.reduce((a,c)=>a+c,0)/newS.length)]||'n/a'
 }
-console.log(findSecretMessage('asdf qwer zxcv. zxcv fdsa rewq. qazw asdf sxed. qwer crfv asdf.'))
+
+console.log(averageString(""))
