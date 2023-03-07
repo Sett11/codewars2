b295665952981150097af13da5ecada42947ac37
Jhonny <gonzodzen@mail.ru>
2023-03-07 22:10:03 +0300

    Matrix Addition

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-count=n=>n
+matrixAddition=(a,b,c=[],z=[])=>{
+  let arr1=a.flat(),arr2=b.flat()
+  for(let i=0;i<arr1.length;i++){c.push([arr1[i]+arr2[i]])}
+  for(let i=0;i<c.length;i+=a.length){z.push(c.slice(i,i+a.length))}
+  return z.map(e=>e.flat())
+}
+
+console.log(matrixAddition(
+  [[1, 2, 3],
+  [3, 2, 1],
+  [1, 1, 1] ],
+
+[ [2, 2, 1],
+  [3, 2, 3],
+  [1, 1, 3] ]))
