4fa199de3ec1edfa7da49808ff16a04dffa93e32
Jhonny <gonzodzen@mail.ru>
2023-03-07 17:06:38 +0300

    Simple Fun #218: Replace Words

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function fib(n,a=[0,1]) {
-  for(let i=0;a.length<=n;i++){
-    a.push(a[a.length-2]+a[a.length-1])
-  }
-  return a[n]
+function replaceWords(s) {
+ let n=s.split` `.slice().sort((a,b)=>a.length-b.length),m=s.split` `.slice().sort((a,b)=>b.length-a.length)
+ return s.split` `.map(e=>n.includes(e)?m[n.indexOf(e)]:'&').map((e,i)=>i===0?e.charAt(0).toUpperCase()+e.slice(1):e==='I'?e:e.toLowerCase()).join` `
 }
 
-console.log(fib(100))
+console.log(replaceWords("Pneumonoultramicroscopicsilicovolcanoconiosis is a dangerous disease"))
