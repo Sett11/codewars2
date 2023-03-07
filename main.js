fa7852bf312dc076623c092acba18b1f45949d7b
Jhonny <gonzodzen@mail.ru>
2023-03-07 16:06:22 +0300

    Don't give me five!

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-function sortCsvColumns(c,a=[],b=[]){
-  c=c.split`\n`.map(e=>e.split`;`)
-  for(let i=0;i<c[0].length;i++){
-    innerArr=[]
-    for(let j=0;j<c.length;j++){
-      innerArr.push(c[j][i])
-    }
-    a.push(innerArr)
-    innerArr=[]
-  }
-  a=a.sort((a,b)=>a[0].localeCompare(b[0]))
-  for(let i=0;i<a[0].length;i++){
-    innerArr=[]
-    for(let j=0;j<a.length;j++){
-      innerArr.push(a[j][i])
-    }
-    b.push(innerArr)
-    innerArr=[]
-  }
-  return b.map(e=>e.join`;`).join`\n`
+dontGiveMeFive=(s,e,a=[])=>{
+  while(s<=e){
+    if(![...s+''].includes('5'))a.push(s);s++}
+    return a.length
 }
 
-console.log(sortCsvColumns("IronMan;Thor;Captain America;Hulk\n"
-+ "arrogant;divine;honorably;angry\n"
-+ "armor;hammer;shield;greenhorn\n"
-+ "Tony;Thor;Steven;Bruce"))
+console.log(dontGiveMeFive(4,17))
