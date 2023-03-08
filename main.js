2334758c21781294bc4ff84fe4dd22790a17317f
Jhonny <gonzodzen@mail.ru>
2023-03-08 14:43:43 +0300

    Balance the arrays

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-ìsZeroBalanced=n=>{
-    if(n.length===1&&n[0]!==0||!n.length)return false
-    if(n.length===3&&n.includes(0)&&n.includes(1)&&n.includes(-1)||!n.length||n.length===1&&n[0]===0)return true
-    n.sort((a,b)=>a-b)
-    let a=n.slice(0,Math.floor(n.length/2)),b=n.slice(Math.floor(n.length/2)).reverse(),c=[]
-    for(let i=0;i<a.length;i++){c.push([a[i],b[i]]);a[i]='&';b[i]='&'}
-    a=a.filter(e=>e!=='&'),b=b.filter(e=>e!=='&')
-    if(b.length&&b[0]!==0)return false
-    return c.every(e=>e.reduce((a,c)=>a+c)===0)
+function balance (a1,a2){
+    const a=Object.values(a1.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a-b).join``,b=Object.values(a2.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a-b).join``
+    return a===b
   }
-console.log(ìsZeroBalanced([ -3, 2, 1, 3, -1, -2 ]))
+  console.log(balance(["a","b","c","d","e","f","g","g"],["h","h","i","j","k","l","m","m"]))
