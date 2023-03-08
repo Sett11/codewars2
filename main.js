972eefda8b73784a0d0823d4d9175f37ab00aa90
Jhonny <gonzodzen@mail.ru>
2023-03-08 11:04:51 +0300

    Consecutive Differences

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-function twosDifference(a,b=[]){
-    for(let i=0;i<a.length;i++){
-        for(let j=i+1;j<a.length;j++){
-            if(Math.abs(a[i]-a[j])===2){
-                b.push([a[i],a[j]])
-            }
-        }
-    }
-    return b.map(e=>e.sort((a,b)=>a-b)).sort((a,b)=>a[0]-b[0])
+differences=a=>{
+    while(a.length>1){a=a.map((e,i,v)=>Math.abs(e-v[i+1])).filter(e=>e===e)}
+    return a[0]
 }
 
-   console.log(twosDifference([4, 3, 1, 5, 6]))
+  console.log(differences([1, 5, 2, 7, 8, 9, 0]))
