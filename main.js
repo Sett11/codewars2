04c5cdecd6b916a6ee621fed5e25d37bae3f4554
Jhonny <gonzodzen@mail.ru>
2023-03-08 16:08:18 +0300

    Pack Some Chocolates

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-function balance (a1,a2){
-    const a=Object.values(a1.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a-b).join``,b=Object.values(a2.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a-b).join``
-    return a===b
+function makeChocolates(s,b,g) {
+    console.log(s,b,g)
+  if(2+(b*5)===g||2+5===g&&s&&b)return 1
+  if((s*2)+(b*5)<g||b===0&&s%2===0&&g%2!==0||g<5&&g%2!==0)return -1
+  if(g===b*5)return 0
+  if(g<b*5){
+      g%=5
+      while(g%2!==0){
+          g+=5
+      }
+      return g/2<=s?g/2:-1
   }
-  console.log(balance(["a","b","c","d","e","f","g","g"],["h","h","i","j","k","l","m","m"]))
+  if(g>b*5){
+      g-=b*5
+      while(g%2!==0){
+          g+=5
+      }
+      return g/2<=s?g/2:-1
+  }
+  if(g>b*5){
+      g-=(b*5)
+      if(g%2!==0)return -1
+  }
+}
+console.log(makeChocolates(3, 137, 241))
