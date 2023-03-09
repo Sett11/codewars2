46e7793915da457a1d31faa54e36e9c27697a35b
Jhonny <gonzodzen@mail.ru>
2023-03-09 10:44:51 +0300

    Array Info

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-abundantNumber=n=>{
-    const arr=[]
-    for(let i=1;i<n/2+1;i++){
-        if(n%i===0)arr.push(i)
-    }
-    return arr.reduce((a,c)=>a+c,0)>n
-}
+function arrayInfo(a){
+    return !a.length?'Nothing in the array!':[a.length,a.filter(e=>Number.isInteger(e)).length,a.filter(e=>!Number.isInteger(e)&&typeof e==='number').length,a.filter(e=>typeof e==='string'&&e!==' ').length,a.filter(e=>e===' ').length].map(e=>!e?[null]:[e])
+  }
 
-console.log(abundantNumber(18))
+  console.log(arrayInfo([1,2,3.33,4,5.01,'bass','kick',' ']))
