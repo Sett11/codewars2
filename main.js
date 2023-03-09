fd9e8eaeb031fd4bba722343b155d59d6e99bc33
Jhonny <gonzodzen@mail.ru>
2023-03-09 18:54:24 +0300

    Hamming Distance

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-function findHobbitsIn(a){
-  return a.map(e=>{
-    if(e.race){
-        if(e.race.toLowerCase().match(/hobbit/gi)||e.race.toLowerCase().match(/halfling/gi))return e
-    }
-    if(e.home){
-        if(e.home.toLowerCase().match(/hobbiton/gi)||e.home.toLowerCase().match(/shire/gi)||e.home.toLowerCase().match(/farthing/gi))return e
-    }
-    return '&'
-  }).filter(e=>e!=='&')
+function hamming(a,b,c=0) {
+	[...a].forEach((e,i)=>{if(e!==b[i])c++})
+    return c
 }
 
-console.log(findHobbitsIn([ { name: 'Loen', race: 'Hobbit', home: ' ' },
-{ name: 'Dwalin', race: 'Dwarve', home: 'the mountain' },
-{ name: 'Oen', race: null, home: 'hobbiton' },
-{ name: 'Joe', race: 'Human', home: 'Bree' } ]))
+console.log(hamming("espresso","espresso"))
