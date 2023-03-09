3823a78abbc564dc73d38843b6985fc7c8239bb9
Jhonny <gonzodzen@mail.ru>
2023-03-09 11:07:24 +0300

    Self-Descriptive Numbers

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-function amicableNumbers(a,b,c=0,d=0) {
-  for(let i=1;i<Math.max(a,b)/2+1;i++){
-    if(a%i===0)c+=i
-    if(b%i===0)d+=i
-  }
-  return a===d&&b===c
+function selfDescriptive(m,n=[...m+'']) {
+    return Object.values(n.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).join``===n.join``.replace(/0/g,'')
 }
 
-console.log(amicableNumbers(220,284))
+  console.log(selfDescriptive(21200))
+  console.log(selfDescriptive(42101000))
+  console.log(selfDescriptive(21230))
