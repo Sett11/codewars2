c3a33291f626cd5150c6a6c2b19058e5670bfe22
Jhonny <gonzodzen@mail.ru>
2023-03-09 14:24:38 +0300

    Simple array rotation

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-function rotate(b,w,n=w%b.length,a=b.slice()){
-    while(n!==0){
-        if(n>0){
-            a.unshift(a.pop())
-            n--
-        }
-        if(n<0){
-            a.push(a.shift())
-            n++
+solve=a=>{
+	let r=a.slice().sort((a,b)=>a-b),v=a.join``,c=a.length
+    if(r.join``===v)return 'A'
+    while(r.join``!==v){
+        r.unshift(r.pop())
+        if(r.join``===v)return 'RA'
+        c--
+        if(c===0&&r!==v){
+            c=a.length;break
         }
     }
-    return a
+    let z=a.slice().sort((a,b)=>b-a)
+    if(z.join``===v)return 'D'
+    while(z.join``!==v){
+        z.push(z.shift())
+        if(z.join``===v)return 'RD'
+    }
 }
-  console.log(rotate( [ 5, 1, 2, 3, 4 ],2))
-  console.log(rotate( [1, 2, 3, 4, 5],5))
+
+console.log(solve([1,2,3,4,5,7]))
+console.log(solve([5,9,8,7,6]))
+console.log(solve([
+    522, 748, 762, 787, 877,903, 990,   1, 111, 151,159, 193, 303, 421, 504
+  ]))
