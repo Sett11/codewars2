2be34cc6a30179ada161bd734f38b5bd60eef6ab
Jhonny <gonzodzen@mail.ru>
2023-03-09 23:01:19 +0300

    SMS Shortener

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-function verticalHistogramOf(s,c=0,a=[]){
-    if(!s.replace(/[^A-Z]/g,''))return ''
-   let r=Object.entries(s.replace(/[^A-Z]/g,'').split``.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a[0].localeCompare(b[0]))
-   r.forEach(e=>c=Math.max(e[1],c))
-   r=r.map(e=>(' '.repeat(c-e[1])+'*'.repeat(e[1])+e[0]).split``)
-   for(let i=0;i<r[0].length;i++){
-    let innerArr=[]
-    for(let j=0;j<r.length;j++){
-        innerArr.push(r[j][i])
+shortener=m=>{
+    if(!m.includes(' '))return m
+    if(m.length<=160)return m
+    while(m.length>160){
+      m=m.slice(0,m.lastIndexOf(' '))+m.charAt(m.lastIndexOf(' ')+1).toUpperCase()+m.slice(m.lastIndexOf(' ')+2)
+      if(!m.includes(' '))return m
     }
-    a.push(innerArr);innerArr=[]
-   }
-   return a.map((e,i,v)=>i!==v.length-1?e.slice(0,e.lastIndexOf('*')+1).join` `:e.join` `).join`\n`
-}
-console.log(verticalHistogramOf("AAABBC"))
-console.log(verticalHistogramOf("XXY YY ZZZ123ZZZ AAA BB C"))
-console.log(verticalHistogramOf("PSQh'' q PxSPP+qRO_QPQOQOP&OPPfSS[*S"))
+    return m
+  }
+
+console.log(shortener('No one expects the Spanish Inquisition! Our chief weapon is surprise, fear and surprise; two chief weapons, fear, surprise, and ruthless efficiency! And that will be it.'))
+console.log(shortener('SMS messages are limited to 160 characters. It tends to be irritating, especially when freshly written message is 164 characters long. SMS messages are limited to 160 characters. It tends to be irritating, especially when freshly written message is 164 characters long.'))
