88d907cdec1e8e8e7900c2abb65f560c3f8ef92f
Jhonny <gonzodzen@mail.ru>
2023-03-09 19:00:41 +0300

    Simple Fun #141: Hamming Distance

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function hamming(a,b,c=0) {
+function hammingDistance(z,v,a=(z).toString(2),b=(v).toString(2),c=0) {
+    if(a.length<b.length)a='0'.repeat(b.length-a.length)+a
+    if(b.length<a.length)b='0'.repeat(a.length-b.length)+b;
 	[...a].forEach((e,i)=>{if(e!==b[i])c++})
     return c
 }
 
-console.log(hamming("espresso","espresso"))
+console.log(hammingDistance(34013, 702))
