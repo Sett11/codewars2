b89b54c74eaaba465c3457340f4f65a079000790
Jhonny <gonzodzen@mail.ru>
2023-03-09 10:50:20 +0300

    DigitAll

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-function arrayInfo(a){
-    return !a.length?'Nothing in the array!':[a.length,a.filter(e=>Number.isInteger(e)).length,a.filter(e=>!Number.isInteger(e)&&typeof e==='number').length,a.filter(e=>typeof e==='string'&&e!==' ').length,a.filter(e=>e===' ').length].map(e=>!e?[null]:[e])
-  }
+digitAll=x=>typeof x!=='string'?'Invalid input !':x.replace(/[^\d]/g,'')
 
-  console.log(arrayInfo([1,2,3.33,4,5.01,'bass','kick',' ']))
+  console.log(digitAll("wai8, where are yo9u goin'?"))
