4c700f0c440121fd5a7d8b5c9c1fbabfdd889d21
Jhonny <gonzodzen@mail.ru>
2023-03-09 14:38:18 +0300

    Basics 08: Find next higher number with same Bits (1's)

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-solve=a=>{
-	let r=a.slice().sort((a,b)=>a-b),v=a.join``,c=a.length
-    if(r.join``===v)return 'A'
-    while(r.join``!==v){
-        r.unshift(r.pop())
-        if(r.join``===v)return 'RA'
-        c--
-        if(c===0&&r!==v){
-            c=a.length;break
-        }
-    }
-    let z=a.slice().sort((a,b)=>b-a)
-    if(z.join``===v)return 'D'
-    while(z.join``!==v){
-        z.push(z.shift())
-        if(z.join``===v)return 'RD'
+function nextHigher(m,n=[...(m).toString(2)].sort().join``,c=m+1) {
+    while(1){
+        let tmp=[...(c).toString(2)].sort().join``
+        if(tmp===n||tmp.slice(1)===n)return c
+        c++
     }
 }
 
-console.log(solve([1,2,3,4,5,7]))
-console.log(solve([5,9,8,7,6]))
-console.log(solve([
-    522, 748, 762, 787, 877,903, 990,   1, 111, 151,159, 193, 303, 421, 504
-  ]))
+  console.log(nextHigher(1))
