23ce4d1860b3d9367356081c01c2f6766069fd49
Jhonny <gonzodzen@mail.ru>
2023-03-09 10:59:39 +0300

    The Most Amicable of Numbers

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-digitAll=x=>typeof x!=='string'?'Invalid input !':x.replace(/[^\d]/g,'')
+function amicableNumbers(a,b,c=0,d=0) {
+  for(let i=1;i<Math.max(a,b)/2+1;i++){
+    if(a%i===0)c+=i
+    if(b%i===0)d+=i
+  }
+  return a===d&&b===c
+}
 
-  console.log(digitAll("wai8, where are yo9u goin'?"))
+console.log(amicableNumbers(220,284))
