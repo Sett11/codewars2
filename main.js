d84245ddeab7b6d732268c1a65074be43f211993
Jhonny <gonzodzen@mail.ru>
2023-03-10 23:38:38 +0300

    Backwards Read Primes

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-reverseWords=s=>s.split` `.reverse().join` `
+function backwardsPrime(s,c,a=[]){
+  const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
+  for(let i=s;i<=c;i++){
+    if(f(i)&&f(+[...i+''].reverse().join``)&&i!==+[...i+''].reverse().join``)a.push(i)
+  }
+  return a
+}
+
+console.log(backwardsPrime(9900, 10000))
