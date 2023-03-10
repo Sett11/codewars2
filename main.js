aa49dd27910f08a9393d15c46fce55211260dd58
Jhonny <gonzodzen@mail.ru>
2023-03-10 20:48:11 +0300

    Are we alternate?

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-absentVowel=(s,l='aeiou',c=l.length-1)=>!s.includes(l[c])?c:absentVowel(s,l,c-1)
-
-
-console.log(absentVowel("Bb Smith sent us six neatly arranged range bicycles"))
+function isAlt(s,l='aieou') {
+  for(let i=0;i<s.length-1;i++){
+    if(l.includes(s[i])&&l.includes(s[i+1])||!l.includes(s[i])&&!l.includes(s[i+1]))return false
+  }
+  return true
+}
+console.log(isAlt('apple'))
