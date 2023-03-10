c0368129977d8670497aa62027ea45f87a2af505
Jhonny <gonzodzen@mail.ru>
2023-03-10 23:22:32 +0300

    Reversed Words

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-function nextLetter(s) {
-    return [...s].map(e=>!e.match(/[a-zA-Z]/)?e:e===e.toUpperCase()?(e.charCodeAt()+1):(e.charCodeAt()+1)).map(e=>e>122?97+e-123:e<97&&e>90?65+e-91:e).map(e=>typeof e!=='number'?e:String.fromCharCode(e)).join``
-}
-console.log(nextLetter('What is your name?'))
+reverseWords=s=>s.split` `.reverse().join` `
