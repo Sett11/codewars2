fd1764308761148bb68484f1bbcacd986ecbe2b2
Jhonny <gonzodzen@mail.ru>
2023-03-10 21:35:51 +0300

    Vowels Back

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-function isAlt(s,l='aieou') {
-  for(let i=0;i<s.length-1;i++){
-    if(l.includes(s[i])&&l.includes(s[i+1])||!l.includes(s[i])&&!l.includes(s[i+1]))return false
+function vowelBack(s){
+  const a='abcdefghijklmnopqrstuvwxyz',l='aioue'
+   return [...s].map(e=>{
+    if(e.match(/[co]/ig))return [e,a.indexOf(e)-1]
+    if(e.match(/d/ig))return [e,a.indexOf(e)-3]
+    if(e.match(/e/ig))return [e,a.indexOf(e)-4]
+    if(l.includes(e)&&!e.match(/[eo]/ig))return [e,a.indexOf(e)-5]
+    if(!l.includes(e)&&!e.match(/[dc]/ig))return [e,a.indexOf(e)+9]
+ }).map(e=>{
+  if(e[1]>25)e[1]-=26
+  if(e[1]<0)e[1]=26- -e[1]
+  return e
+ }).map(e=>{
+  if(a[e[1]].match(/[code]/ig)){
+    return e[0]
   }
-  return true
+  else{
+    return a[e[1]]
+  }
+ }).join``
 }
-console.log(isAlt('apple'))
+console.log(vowelBack('testcase'))
+console.log(vowelBack('codewars'))
+console.log(vowelBack('exampletesthere'))
