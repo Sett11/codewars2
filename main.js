f6cb0b63b467152eeabb4627878a97a29a8e20eb
Jhonny <gonzodzen@mail.ru>
2023-03-10 22:24:23 +0300

    Modulus 11 - Check Digit

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-checkDigit=(n,i1,i2,d)=>(n+'').slice(Math.min(i1,i2),Math.max(i1,i2)+1).includes(d)
+function addCheckDigit(m,n=m) {
+  n=[...m.slice()].reverse().map(e=>[e,[]]),a='234567'.repeat(Math.floor(n.length/2))
+  for(let i=0;i<n.length;i++){
+    n[i][1].push(a[i])
+  }
+  const r=n.map(e=>e[0]*e[1][0]).reduce((a,c)=>a+c,0)%11
+  return r===0?m+'0':r===1?m+'X':m+((11-r)+'')
+}
 
-console.log(checkDigit(42793387075989, 6, 0, 8))
+console.log(addCheckDigit('036532'))
