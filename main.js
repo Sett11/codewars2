7991b4972ff6f85817ec643d4f8df77bf4d50420
Jhonny <gonzodzen@mail.ru>
2023-03-10 10:50:18 +0300

    Sum The Strings: ARRAY EDITION

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-function sepStr(s,a=[]){
-  s=s.split` `.map((e,i,v)=>(e+' '.repeat(Math.max(...v.map(u=>u.length))-e.length)).split``.map(z=>z===' '?'':z))
-  for(let i=0;i<s[0].length;i++){
-    let innerArr=[]
-    for(let j=0;j<s.length;j++){
-      innerArr.push(s[j][i])
-    }
-    a.push(innerArr);innerArr=[]
-  }
-  return a
-}
-console.log(sepStr("The Mitochondria is the powerhouse of the cell"))
+sumArr=(a,b)=>a.map((e,i)=>(+e+ +b[i])+'')
