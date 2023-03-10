729b3d343fd3ef3602726ba99bfde8a28849b9ec
Jhonny <gonzodzen@mail.ru>
2023-03-10 20:11:54 +0300

    Vowel Shifting

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-checkVowel=(s,p)=>p>=0&&p<s.length&&s[p].match(/a|e|u|o|i/gi)!==null
+vowelShift=(s,n,a=[])=>{
+  if(!s)return s
+  s=s.split``.map(e=>{
+    if(e.match(/a|e|u|i|o/gi)){
+      a.push(e)
+      return '&'
+    }
+    return e
+  })
+  while(n!==0){
+    if(n>0){
+      a.unshift(a.pop())
+      n--
+    }
+    if(n<0){
+      a.push(a.shift())
+      n++
+    }
+  }
+  return s.map(e=>e==='&'?a.shift():e).join``
+}
 
-console.log(checkVowel('cat', 1))
+console.log(vowelShift("This is a test!", 3))
