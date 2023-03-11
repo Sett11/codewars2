4a9cad11d465bc9d8bd1327ed336923ff97342d5
Jhonny <gonzodzen@mail.ru>
2023-03-11 20:09:04 +0300

    Remove method in Arrays

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-isFlush=c=>[...new Set(c.map(e=>e[e.length-1]).join``)].length===1
-
-console.log(isFlush([  "AD", "4S",  "7H", "KC", "5S" ]))
+Array.prototype.remove = function(i) {
+  if(typeof i!=='number'||i<0||i>this.length-1)return this
+  this.splice(i,1)
+  return this
+}
