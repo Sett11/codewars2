94a641aac15a87731d38765af208105c84483d6d
Jhonny <gonzodzen@mail.ru>
2023-03-11 21:14:33 +0300

    Part 1: Evil Programming Government Restrictions

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-generateHashtag=(s,c='#'+s.split` `.filter(e=>e).map(e=>e.charAt(0).toUpperCase()+e.slice(1)).join``)=>c.length>140||!s||!s.match(/[a-zA-Z0-9]/g)?false:c
-console.log(generateHashtag("   "))
+anarchy=()=>{
+  const r=new Set();r.add('a');one=r.size;r.add('b');two=r.size;r.add('c');three=r.size
+  return `I can write numbers like, ${one}, ${two}, ${three}.`
+}
+
+console.log(anarchy())
