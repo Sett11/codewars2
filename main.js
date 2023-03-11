451f8f243c7ef13d72779956e8fb4493c000170d
Jhonny <gonzodzen@mail.ru>
2023-03-11 12:17:33 +0300

    Divisible Ints

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-christmasTree=(h,a=Array(h).fill('*'))=>{
-  for(let i=0;i<a.length;i++){
-    a[i]=' '.repeat(a.length-i-1)+'*'.repeat(i)+a[i]+'*'.repeat(i)+' '.repeat(a.length-i-1)
+function getCount(m,n=[...m+''].map(Number),a=[]){
+  if(m===0)return 0
+  for(let i=0;i<n.length;i++){
+    let innerArr=[]
+    for(let j=i+1;j<n.length+1;j++){
+      innerArr.push(n.slice(i,j))
+    }
+    a.push(innerArr);innerArr=[]
   }
-  return a.join`\n`
+  return a.flat().filter(e=>e.length<n.length).map(e=>+e.join``).filter(e=>m%e===0).length
 }
-console.log(christmasTree(5))
+
+console.log(getCount(1111111111))
