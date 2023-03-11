b8383f6c479bc53fde154427d1fb6e2accd53e9b
Jhonny <gonzodzen@mail.ru>
2023-03-11 21:48:08 +0300

    Validate Credit Card Number

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function solution(){
-  return [...arguments].some((e,i,v)=>v.indexOf(e)!==v.lastIndexOf(e))
- }
- console.log(solution(1,2,3,6,5,6))
+function validate(m,n=[...m+''].map(Number)){
+  for(let i=n.length%2===0?0:1;i<n.length;i+=2){
+    let tmp=n[i]*2
+    if(tmp>9)n[i]=[...tmp+''].map(Number).reduce((a,c)=>a+c)
+    else n[i]=tmp
+  }
+  return n.reduce((a,c)=>a+c)%10===0
+}
+console.log(validate(2121))
