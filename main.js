3d1d9ecbdbbe906924b3717de25a7e765815a245
Jhonny <gonzodzen@mail.ru>
2023-03-11 17:24:44 +0300

    Is n divisible by x and y?

--- main.js
+++ main.js
@@ -1,3 +1,1 @@
-longest=(a,n)=>a.map((e,i)=>[e,i]).sort((a,b)=>b[0].length-a[0].length||a[1]-b[1])[n-1][0]
-
-console.log(longest(['aa', 'bb', 'cc', 'dd', 'eee', 'b', 'f', 'ff', 'hhh', 'gggg'],4))
+isDivisible=(n,x,y)=>n%x===0&&n%y===0
