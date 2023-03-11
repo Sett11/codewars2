b2564718026d01fd8fd99e5c274860db4fabd6f0
Jhonny <gonzodzen@mail.ru>
2023-03-11 16:17:44 +0300

    Mean Means

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-getAverage=a=>Math.floor(a.reduce((a,c)=>a+c,0)/a.length)
+function geo_mean(m,a){
+  const check=(m.length+1)*a,s=m.reduce((a,c)=>a+c,0),r=check-s;m.push(r)
+  return m.reduce((a,c)=>a*c)**(1/m.length)
+}
+console.log(geo_mean([2], 10))
