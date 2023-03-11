c15dcf8bd38325c47a7ad5edb52c4a077665a488
Jhonny <gonzodzen@mail.ru>
2023-03-11 16:04:29 +0300

    Get the mean of an array

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-function encryptNum(s,c=6) {
-  if(s.slice(0,3)!=='+39'&&s.slice(0,4)!=='0039'&&s[0]!=='1'||s.slice(0,2)==='11'||s.slice(0,3)==='111')return false
-  s=[...s].reverse().join``
-  while(c){
-    s=s.replace(/\d/,'X');c--
-  }
-  return [...s].reverse().join``
- }
- console.log(encryptNum("1-212-420-0202")) 
+getAverage=a=>Math.floor(a.reduce((a,c)=>a+c,0)/a.length)
