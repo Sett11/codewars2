c4b291b3849f3c4b21605dafe22f0f81cc3451b8
Jhonny <gonzodzen@mail.ru>
2023-03-11 17:20:16 +0300

    Find the Nth longest string in an Array

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function solve(n,c=n,s=n,a=[]){
-  const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
-  while(1){
-    if(f(c))return c
-    if(f(s))return s
-    c--,s++
-  }
-  }
-console.log(solve(35000))
-console.log(solve(350000))
+longest=(a,n)=>a.map((e,i)=>[e,i]).sort((a,b)=>b[0].length-a[0].length||a[1]-b[1])[n-1][0]
+
+console.log(longest(['aa', 'bb', 'cc', 'dd', 'eee', 'b', 'f', 'ff', 'hhh', 'gggg'],4))
