191479fa2575885ed9a01f562765b7ee2926629a
Jhonny <gonzodzen@mail.ru>
2023-03-11 20:33:14 +0300

    The Hashtag Generator

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-Array.prototype.remove = function(i) {
-  if(typeof i!=='number'||i<0||i>this.length-1)return this
-  this.splice(i,1)
-  return this
-}
+generateHashtag=(s,c='#'+s.split` `.filter(e=>e).map(e=>e.charAt(0).toUpperCase()+e.slice(1)).join``)=>c.length>140||!s||!s.match(/[a-zA-Z0-9]/g)?false:c
+console.log(generateHashtag("   "))
