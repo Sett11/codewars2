4ccda6869e506763b81b9ae4c980d9513dcb27ca
Jhonny <gonzodzen@mail.ru>
2023-03-11 17:45:31 +0300

    Say Me Please Operations

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function center (s,w,f=' ') {
-  return s.length>=w?s:f.repeat(Math.ceil((w-s.length)/2))+s+f.repeat(Math.floor((w-s.length)/2))
+function sayMeOperations(s,c='') {
+  const f=(x,y,z)=>x+y===z?'addition':x-y===z?'subtraction':x*y===z?'multiplication':'division'
+  s=s.split` `.map(Number)
+  for(let i=0;i<s.length-2;i++){
+    c+=f(s[i],s[i+1],s[i+2])+', '
+  }
+  return c.slice(0,c.length-2)
 }
-console.log(center("abc", 10, '_'))
+console.log(sayMeOperations("10 2 5 -3 -15 12"))
