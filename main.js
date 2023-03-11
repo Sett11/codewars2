069b11ebed4a760b0b7cf8bef9e7d042d77dec67
Jhonny <gonzodzen@mail.ru>
2023-03-11 17:29:15 +0300

    Wordsearch

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-isDivisible=(n,x,y)=>n%x===0&&n%y===0
+function wordSearch(w,t){
+  return t.split` `.map(e=>e.replace(/\W/g,'')).some(e=>e===w)
+}
+console.log(wordSearch('beautiful',"what makes the desert beautiful, said the little prince is that somewhere it hides a well"))
