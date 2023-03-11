c4a4020c45857c3e33fb02b3668044d20da80feb
Jhonny <gonzodzen@mail.ru>
2023-03-11 17:04:34 +0300

    Simple nearest prime

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function getMean(a, x, y) {
-  if(x<2||y<2||x>a.length||y>a.length)return -1
-  const one=a.slice(0,x),two=a.slice(-y)
-  return (one.reduce((a,c)=>a+c,0)/one.length+two.reduce((a,c)=>a+c,0)/two.length)/2
-}
-console.log(getMean([1,3,2,4],2,3))
+function solve(n,c=n,s=n,a=[]){
+  const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
+  while(1){
+    if(f(c))return c
+    if(f(s))return s
+    c--,s++
+  }
+  }
+console.log(solve(35000))
+console.log(solve(350000))
