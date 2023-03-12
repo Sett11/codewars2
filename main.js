a2b14aef97f29158d55ee4d0b2bae726aa0040c5
Jhonny <gonzodzen@mail.ru>
2023-03-13 00:23:44 +0300

    Convert PascalCase string into snake_case

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function notPrimes(a,b,c=[]){
-  const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
-  while(a<=b){
-    let tmp=[...a+''].map(Number)
-    if(tmp.every(e=>e===2||e===3||e===5||e===7)&&!f(a))c.push(a)
-    a++
-  }
-  return c
-}
+const toUnderscore=s=>typeof s!=='string'?`${s}`:s.charAt(0).toLowerCase()+s.slice(1).replace(/[A-Z]/g,e=>'_'+e.toLowerCase())
 
-console.log(notPrimes(999,2500))
+console.log(toUnderscore('TestController'))
