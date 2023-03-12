26da3572443905e955c3c7b92eeb45e83b1895c2
Jhonny <gonzodzen@mail.ru>
2023-03-12 20:41:18 +0300

    Burrows-Wheeler-Transformation

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-const consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
-const vowels = ['a','e','i','o','u'];
-
-function replaceLetters(s) {
-  return [...s].map(e=>{
-    if(e.match(/a|e|u|o|i/)){
-      for(let i=alphabet.indexOf(e);;i--){
-        if(i===-1)i=25
-        if(!alphabet[i].match(/a|e|u|o|i/))return alphabet[i]
-      }
-    }
-    for(let i=alphabet.indexOf(e);;i++){
-      if(i===26)i=0
-      if(alphabet[i].match(/a|e|u|o|i/))return alphabet[i]
-    }
-  }).join``
+function encode(s,a=[]){
+  if(!s)return ['','fack']
+  const r=[...s].map((a,i,v)=>(v.slice(i)+'&'+v.slice(0,i)).replace(/&/g,',').split`,`.filter(e=>e)).sort()
+  r.forEach(e=>a.push(e[e.length-1]))
+  return [a.join``,r.map(e=>e.join``).indexOf(s)]
+ }
+ 
+ function decode(s,z,c=[...s].sort(),a=[...s]) {
+   if(!s)return ''
+   for(let i=0;i<s.length-1;i++){
+     for(let j=0;j<s.length;j++){
+       a[j]+=c[j].charAt(i)
+       c[j]=a[j]
+     }
+     c.sort()
+   }
+   return c[z]
  }
 
-console.log(replaceLetters('abcdtuvwxyz'))
+console.log(encode("bananabar"))
+
+// ['a', 'b', 'a', 'r', 'b', 'a', 'n', 'a', 'n']
+// 1
+// : 
+// (9) ['a', 'n', 'a', 'b', 'a', 'r', 'b', 'a', 'n']
+// 2
+// : 
+// (9) ['a', 'n', 'a', 'n', 'a', 'b', 'a', 'r', 'b']
+// 3
+// : 
+// (9) ['a', 'r', 'b', 'a', 'n', 'a', 'n', 'a', 'b']
+// 4
+// : 
+// (9) ['b', 'a', 'n', 'a', 'n', 'a', 'b', 'a', 'r']
+// 5
+// : 
+// (9) ['b', 'a', 'r', 'b', 'a', 'n', 'a', 'n', 'a']
+// 6
+// : 
+// (9) ['n', 'a', 'b', 'a', 'r', 'b', 'a', 'n', 'a']
+// 7
+// : 
+// (9) ['n', 'a', 'n', 'a', 'b', 'a', 'r', 'b', 'a']
+// 8
+// : 
+// (9) ['r', 'b', 'a', 'n', 'a', 'n', 'a', 'b', 'a']
