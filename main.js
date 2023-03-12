6977c3f70c876ed151952c547144251f74915478
Jhonny <gonzodzen@mail.ru>
2023-03-12 18:58:29 +0300

    Roof Replacement

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-var replaceDots = function(str) {
-  return str.replace(/\./g, '-');
+function roofFix(f, r,a=[...f].map((e,i)=>e===' '?i+1:0).filter(e=>e),b=[...r].map((e,i)=>e==='\\'||e==='/'?i+1:0).filter(e=>e)){
+  return b.every(e=>a.includes(e))
 }
+console.log(roofFix("d  egl mfl    j  jce a     " , "/_/________/_______________"))
