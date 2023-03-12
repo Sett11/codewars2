523b6e775edd9af169fc075b743c2c9f52eb87b3
Jhonny <gonzodzen@mail.ru>
2023-03-12 19:19:26 +0300

    Replace letters

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function roofFix(f, r,a=[...f].map((e,i)=>e===' '?i+1:0).filter(e=>e),b=[...r].map((e,i)=>e==='\\'||e==='/'?i+1:0).filter(e=>e)){
-  return b.every(e=>a.includes(e))
-}
-console.log(roofFix("d  egl mfl    j  jce a     " , "/_/________/_______________"))
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+const vowels = ['a','e','i','o','u'];
+
+function replaceLetters(s) {
+  return [...s].map(e=>{
+    if(e.match(/a|e|u|o|i/)){
+      for(let i=alphabet.indexOf(e);;i--){
+        if(i===-1)i=25
+        if(!alphabet[i].match(/a|e|u|o|i/))return alphabet[i]
+      }
+    }
+    for(let i=alphabet.indexOf(e);;i++){
+      if(i===26)i=0
+      if(alphabet[i].match(/a|e|u|o|i/))return alphabet[i]
+    }
+  }).join``
+ }
+
+console.log(replaceLetters('abcdtuvwxyz'))
