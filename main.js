8f5c99f4ee6ecbc06b9ca01a15aa7cb529da7848
Jhonny <gonzodzen@mail.ru>
2023-03-14 00:30:17 +0300

    Error correction #1 - Hamming Code

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-function cleverSplit(s){
-  s=s.split` `
-  for(let i=0;i<s.length;i++){
-    if(s[i].includes('[')&&!s[i].includes(']')){
-      let tmp=s[i]
-      for(let j=i+1;j<s.length;j++){
-        tmp+=' '+s[j]
-        if(s[j].includes(']')){
-          s=s.slice(0,i).concat(tmp).concat(s.slice(j+1))
-          break
-        }
-      }
-    }
-  }return s
+function encode(t) {
+  return [...t].map(e=>{
+    e=[...(e.charCodeAt()).toString(2)]
+    while(e.length<8){e.unshift('0')}
+    return e.map(u=>u.repeat(3)).join``
+  }).join``
 }
 
-console.log(cleverSplit('!wguqh [!asaape] [!aaaamk] [cipck] !klapr ndxaa [!abya !zpae aidtf auzo] [!afqdn] !adha !uoaqf jhci jzeop abav yodyg [niyua] !apza [!fxraa !rayy !aagcae axlea !iyyk wazat raaa !pandg !mmsqg !rawgx] !poeaa !zsmrq !rchkc'))
+function decode(b,a=[],c=[]) {
+  for(let i=0;i<b.length;i+=3)a.push(b.slice(i,i+3))
+  a=a.map(e=>Object.entries([...e].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))).map(e=>{
+    if(e.length===1)return e[0][0]
+    return e.sort((a,b)=>b[1]-a[1])[0][0]
+  })
+  for(let i=0;i<a.length;i+=8) c.push(a.slice(i,i+8))
+  return c.map(e=>String.fromCharCode(parseInt(e.join``,2))).join``
+}
+
+console.log(encode('hey'))
+console.log(decode('000111111000111000000000000111111000000111000111000111111111111000000111'))
