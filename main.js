129de38b2a28ade7c640c5f03434266696759fa4
Jhonny <gonzodzen@mail.ru>
2023-03-15 01:00:23 +0300

    Length of missing array

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-function solve(s){
-  const res=Object.values([...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b-a)
-  if(res.length===2&&res[0]-res[1]>1&&res[1]!==1)return false
-  if(res.every(e=>e===1)||res.length===2&&res.includes(1))return true
-  for(let i=0;i<res.length;i++){
-    res[i]-=1
-    if(res.every((e,i,v)=>e===v[0]))return true
-    res[i]+=1
-  }
-  if([...new Set(res)].length===res.length)return false
-  if(res.includes(1)&&res.filter(e=>e!==1).length!==1)return true
-  return false
- }
+function getLengthOfMissingArray(b){
+  if(!b||!b.length||b.some(e=>!e||!e.length))return 0
+  let a=b.map(e=>e.length),c=Math.min(...a),z=Math.max(...a)
+  while(c<z){if(!a.includes(c))return c;c++}
+}
 
- console.log(solve('aabbccddd'))
- console.log(solve('cdefghijklmnopqrstuvwx'))
- console.log(solve('xzgxxgzg'))
+console.log(getLengthOfMissingArray([ [], [ 1, 2, 2 ] ]))
