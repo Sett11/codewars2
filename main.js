574a604e273ef4ff34ea437695a6bfccba8b7b2c
Jhonny <gonzodzen@mail.ru>
2023-03-14 07:42:41 +0300

    Typoglycemia Generator

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function parityBit(b) {
-    return b.split` `.map(e=>e.charAt(e.length-1)==='1'&&e.slice(0,e.length-1).replace(/0/g,'').length%2!==0||e.charAt(e.length-1)==='0'&&e.slice(0,e.length-1).replace(/0/g,'').length%2===0?e.slice(0,e.length-1):'error').join` `
+ScrambleWords=s=>{
+  return s.split` `.map(e=>{
+    e=[e,[]]
+    for(let i=0;i<e[0].length;i++)if(!e[0][i].match(/[a-zA-Z]/g))e[1].push(i)
+    e=[e[0].replace(/\W/g,''),e[0].replace(/\w/g,'').split``,e[1]]
+    if(e[0].length>3){
+        e[0]=e[0].charAt(0)+e[0].slice(1,e[0].length-1).split``.sort().join``+e[0].charAt(e[0].length-1)
+    }
+    if(e[2].length){
+         for(let i=0;i<e[2].length;i++){
+         e[0]=e[0].slice(0,e[2][i])+e[1][i]+e[0].slice(e[2][i])
+         }
+    }
+    return e[0]
+  }).join` `
 }
 
-console.log(parityBit("01011001 01101110 01100000 01010110 10001111 01100011"))
+console.log(ScrambleWords(`he's dice. lock-in. retribution subsequently you're. you're discombobulate discombobulate come. we'll`))
