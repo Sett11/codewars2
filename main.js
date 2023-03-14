293c98fb120a234097b87c0643663c4a0b9ba8bc
Jhonny <gonzodzen@mail.ru>
2023-03-14 20:33:37 +0300

    Sexy Primes <3

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-factorial=(n,c=1)=>n<0?null:n<1?1:n===1?c:factorial(n-1,c*n)
+const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
 
-console.log(factorial(5))
+sexy_prime=(x,y)=>Math.abs(x-y)===6&&f(x)&&f(y)
