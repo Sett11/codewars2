4ae0e38a82dc16e27cfda729507e9675610bed14
Jhonny <gonzodzen@mail.ru>
2023-03-14 22:16:05 +0300

    Name to Matrix

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-findOddDigits=(n,k,m=[...n+''].map(Number).filter(e=>e%2!==0).slice(0,k).join``)=>!m||k>m.length||k>[...n+''].length?0:m
+const matrixfy=(s,c=1,a=[])=>{
+  if(!s)return 'name must be at least one letter'
+  while(1){if(c*c>=s.length)break;c++}
+  s=s+'.'.repeat(c*c-s.length)
+  for(let i=0;i<s.length;i+=c)a.push(s.slice(i,i+c))
+  return a.map(e=>[...e])
+}
 
-console.log(findOddDigits(123456789111,5))
+console.log(matrixfy('Franklin'))
