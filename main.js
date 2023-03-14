43b268924b146823665fea373d130d54a111bc7d
Jhonny <gonzodzen@mail.ru>
2023-03-14 22:05:35 +0300

    Give me the k first digits. Odd digits.

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function onlyOddDigPrimes(n,a=[],c=2) {
-  const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
-  while(1){
-    t=[...c+''].map(Number)
-    if(f(c)&&t.every(e=>e%2!==0))a.push(c)
-    c++
-    if(a.some(e=>e>n))break
-  }
-  return [a.filter(e=>e<=n).length, a.includes(n)?a[a.indexOf(n)-1]:a[a.length-2],a[a.length-1]]
-}
-console.log(onlyOddDigPrimes(100))
+findOddDigits=(n,k,m=[...n+''].map(Number).filter(e=>e%2!==0).slice(0,k).join``)=>!m||k>m.length||k>[...n+''].length?0:m
+
+console.log(findOddDigits(123456789111,5))
