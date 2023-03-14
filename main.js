f2a4c1ba1917b7d671bee34ff77f3ee5294238de
Jhonny <gonzodzen@mail.ru>
2023-03-15 00:50:06 +0300

    String character frequency

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-function solution(m,n=m+''){
-  if(n.length<3||n.length>4)throw Error('should raise an exception')
-  return n.length===3?`${n[0]}:${n.slice(1)}`:`${n.slice(0,2)}:${n.slice(2)}`
-}
+function solve(s){
+  const res=Object.values([...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b-a)
+  if(res.length===2&&res[0]-res[1]>1&&res[1]!==1)return false
+  if(res.every(e=>e===1)||res.length===2&&res.includes(1))return true
+  for(let i=0;i<res.length;i++){
+    res[i]-=1
+    if(res.every((e,i,v)=>e===v[0]))return true
+    res[i]+=1
+  }
+  if([...new Set(res)].length===res.length)return false
+  if(res.includes(1)&&res.filter(e=>e!==1).length!==1)return true
+  return false
+ }
 
-console.log(solution(100))
+ console.log(solve('aabbccddd'))
+ console.log(solve('cdefghijklmnopqrstuvwx'))
+ console.log(solve('xzgxxgzg'))
