230ef6b79fdab1a692a662c4842541532179d234
Jhonny <gonzodzen@mail.ru>
2023-03-14 22:20:28 +0300

    Odds-Index

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const matrixfy=(s,c=1,a=[])=>{
-  if(!s)return 'name must be at least one letter'
-  while(1){if(c*c>=s.length)break;c++}
-  s=s+'.'.repeat(c*c-s.length)
-  for(let i=0;i<s.length;i+=c)a.push(s.slice(i,i+c))
-  return a.map(e=>[...e])
-}
-
-console.log(matrixfy('Franklin'))
+oddBall=a=>a.includes(a.indexOf('odd'))
