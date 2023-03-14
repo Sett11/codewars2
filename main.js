e0d4898dc948cdd1c68968169bb2220a3a3322fd
Jhonny <gonzodzen@mail.ru>
2023-03-14 20:40:05 +0300

    Bases Everywhere

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
+baseFinder=a=>[...new Set(a.join``)].length
 
-sexy_prime=(x,y)=>Math.abs(x-y)===6&&f(x)&&f(y)
+console.log(baseFinder([
+  '301', '302', '303',
+  '304', '305', '310',
+  '311', '312', '313',
+  '314'
+]))
