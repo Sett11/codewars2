1055f6711db38f9f99c09c3f01efc255b01cd5c8
Jhonny <gonzodzen@mail.ru>
2023-03-15 20:53:44 +0300

    String insert values

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-function customChristmasTree(c,n,a=[],z=Math.floor(n/3)){
-  while(c.length<n**2){c+=c.repeat(1)}
-  for(let i=1;i<=n;i++){
-    a.push(c.slice(0,i))
-    c=c.slice(i)
-  }
-  while(z){a.push('|');z--}
-  return a.map((e,i,v)=>{
-    e=e.split``.join` `
-    if(e.length<n*2-1)e=' '.repeat((n*2-1-e.length)/2)+e
-    return e
-  }).join`\n`
+format=(s,o)=>{
+  return s.replace(/\{/g,'(').replace(/\}/g,')').replace(/\(([^)^(]+)\)/g,e=>o[e.replace(/[\(\)]/g,'')]!==undefined?o[e.replace(/[\(\)]/g,'')]:`{${e.replace(/[\(\)]/g,'')}}`)
 }
 
-console.log(customChristmasTree("MUP",14))
+console.log(format('Hello {0} - {foobar} make me {2} {1} - I\'m full..', ['Jack', 'sandwiches', 0]))
