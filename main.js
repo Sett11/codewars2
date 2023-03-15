95dfabe73ca3ac46f8517b6ca1f2b64df32eb08b
Jhonny <gonzodzen@mail.ru>
2023-03-15 23:35:18 +0300

    Coprimes up to N

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-function insertAtIndexes(p,w,g,z=0){
-  for(let i=0;i<g.length;i++){
-    if(i===0){
-      p=p.slice(0,g[i])+w+p.slice(g[i])
-      z+=w.length
-    }
-    if(i!==0){
-      p=p.slice(0,g[i]+z)+w+p.slice(g[i]+z)
-      z+=w.length
-    }
-    }
-  return p
+function coprimes(n){
+  const c=Array(n-1).fill(1).map((e,i)=>i+1).filter(e=>n%e===0&&e!==1)
+  return Array(n-1).fill(1).map((e,i)=>i+1).filter(e=>c.every(u=>e%u!==0))
 }
-console.log(insertAtIndexes("I like codewars! It makes me happy."," really",[1,28]))
-console.log(insertAtIndexes("'I' write a wi said Phi","ll",[3,14,24]))
+console.log(coprimes(20))
