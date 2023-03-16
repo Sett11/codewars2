dc5f2aea6f94e8074623b49309d9a9538906559b
Jhonny <gonzodzen@mail.ru>
2023-03-16 23:53:08 +0300

    Ragbaby cipher

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
-ranks=b=>b.map(e=>b.slice().sort((a,b)=>b-a).indexOf(e)+1)
+function encode(t,k) {
+  const a=[...new Set(k+'abcdefghijklmnopqrstuvwxyz')]
+  t=[...t].map(e=>[e,[]])
+  for(let i=0,c=1;i<t.length;i++,c++){
+    if(t[i][0].match(/[a-z0-9]/i)){
+      t[i][1].push(c)
+    }else{
+      c=0
+      t[i][1].push(c)
+    }
+  }
+  return t.map(e=>e[0].match(/[a-z]/)?a[(a.indexOf(e[0])+e[1][0])%a.length]:e[0].match(/[A-Z]/)?a[(a.indexOf(e[0].toLowerCase())+e[1][0])%a.length].toUpperCase():e[0]).join``
+}
 
-console.log(ranks([5,2,3,5,5,4,9,8,0]))
+function decode(t,k) {
+  const a=[...new Set(k+'abcdefghijklmnopqrstuvwxyz')]
+  t=[...t].map(e=>[e,[]])
+  for(let i=0,c=1;i<t.length;i++,c++){
+    if(t[i][0].match(/[a-z]/i)){
+      t[i][1].push(c)
+    }else{
+      c=0
+      t[i][1].push(c)
+    }
+  }
+  return t
+  .map(e=>{
+    if(e[0].match(/[a-z]/)){
+      let r=(a.indexOf(e[0])-(e[1][0]%a.length))
+      if(r<0)r=a.length-Math.abs(r)
+      return a[r]
+    }
+    if(e[0].match(/[A-Z]/)){
+      let r=(a.indexOf(e[0].toLowerCase())-(e[1][0]%a.length))
+      if(r<0)r=a.length-Math.abs(r)
+      return a[r].toUpperCase()
+    }
+    return e[0]
+  }).join``
+}
+
+console.log(decode("TGBqhkMZxVMOQMMxlNKOOvdTSGmcommxoYa,V","mmpyhi"))
