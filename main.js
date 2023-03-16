af560ef226e61e5395fbe96bf95ad4fec7ee69f2
Jhonny <gonzodzen@mail.ru>
2023-03-16 21:04:44 +0300

    Pyramid Array

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-function fizzbuzzPlusPlus(n,w,a=[],c=1,s='') {
-  while(c){
-    for(let i=0;i<n.length;i++){
-      if(c%n[i]===0)s+=w[i]
-    }
-    if(s){
-      a.push(s)
-      s='';c++
-    }else{
-      a.push(c)
-      c++
-    }
-    if(a.includes(w.join``))return a
-  }
-}
+pyramid=n=>Array(n).fill(1).map((e,i)=>Array(i+1).fill(1))
 
-console.log(fizzbuzzPlusPlus([2,3,5],["fizz", "buzz", "bazz"]))
+
+console.log(pyramid(7))
