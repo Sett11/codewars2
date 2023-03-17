0b0c1187a9dcd13964f95ef34b987cea3f5bad19
Jhonny <gonzodzen@mail.ru>
2023-03-17 20:14:15 +0300

    Number Decoding

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-function woofDecoder(s){
-  const a='abcdefghijklmnopqrstuvwxyz'
-  s=s.replace(/[^wof\-\!]/gi,'')
-  if(!s.match(/[a-z]/i)||!s.match(/w/i)||!s.match(/o/i)||!s.match(/f/i))return "nothing to decode!"
-  s=s.split`!`.filter(e=>e).map(e=>e.split`-`.length-1)
-  return !s.length?"nothing to decode!":s.some(e=>e>=a.length)?"just barking!":s.map(e=>a[e]).join``
-}
-console.log(woofDecoder("Shhh! Shhh-shhh!"))
+function decode(n){
+    return n.replace(/98/g,',').split`,`.filter(e=>e).map((e,i)=>e.match(/[2-9]/g)?e.match(/\d{3}/g).map(u=>+u).map(z=>String.fromCharCode(z-4)).join``:i%2===0?String.fromCharCode(+e-4):parseInt(e,2)).filter(e=>e).join`, `
+  }
+
+console.log(decode('126101123105981000011100001011981211221161011131261111211201251201109810000009811198111'))
