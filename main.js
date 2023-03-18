888173541c97190ffe47493f3b183ffcafcd39a9
Jhonny <gonzodzen@mail.ru>
2023-03-18 15:06:56 +0300

    Bit calculator

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function sqrtApproximation(n){
-    if(n<2)return n
-    for(let i=0;i<=n;i++){
-        if(i*i===n)return i
-        if(i*i>n)return [i-1,i]
+function calculate(n1,n2){
+    const f=x=>{
+        let s=0,l=x.length,b=1
+        while(l--){s+=x[l]=='1'?b:0;b<<=1}
+        return s
     }
+    return f(n1)+f(n2)
 }
 
-console.log(sqrtApproximation(2))
+
+
+console.log(calculate('101','10'))
