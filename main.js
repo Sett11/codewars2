61691995cc1a97559d6e48db67e3baf28964c7fc
Jhonny <gonzodzen@mail.ru>
2023-03-18 10:15:17 +0300

    Anagram difference

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function decode(n){
-    return n.replace(/98/g,',').split`,`.filter(e=>e).map((e,i)=>e.match(/[2-9]/g)?e.match(/\d{3}/g).map(u=>+u).map(z=>String.fromCharCode(z-4)).join``:i%2===0?String.fromCharCode(+e-4):parseInt(e,2)).filter(e=>e).join`, `
-  }
-
-console.log(decode('126101123105981000011100001011981211221161011131261111211201251201109810000009811198111'))
+function anagramDifference(w1,w2,c=0){
+    let one=[...w1].filter(e=>w2.includes(e)),two=[...w2].filter(e=>w1.includes(e))
+    c=w1.length-one.length+(w2.length-two.length)
+    one=Object.entries(one.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a[0].localeCompare(b[0])).map(e=>e[1]),two=Object.entries(two.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a[0].localeCompare(b[0])).map(e=>e[1])
+    one.forEach((e,i)=>c+=Math.max(e,two[i])-Math.min(e,two[i]))
+    return c
+ }
+ 
+ 
+ console.log(anagramDifference('fqwfaiedilajlownvrn', 'kickeqbrtdhulhwxevn'))
