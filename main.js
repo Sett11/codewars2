290d0a324579cc708260f510f3c5fd0816183e2a
Jhonny <gonzodzen@mail.ru>
2023-03-18 14:52:47 +0300

    Sqrt approximation

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-sentenceCompression=s=>s.replace(/[\W\d]/g,'')
+function sqrtApproximation(n){
+    if(n<2)return n
+    for(let i=0;i<=n;i++){
+        if(i*i===n)return i
+        if(i*i>n)return [i-1,i]
+    }
+}
 
-console.log(sentenceCompression('This 111 is madness. Madness? This, is, SPARTA!'))
+console.log(sqrtApproximation(2))
