7036874f2b980cd782a4d63fbf4d9dc6a0e93580
Jhonny <gonzodzen@mail.ru>
2023-03-18 17:53:02 +0300

    Déjà vu Duplicates

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function calculate1RM(w,r) {
-    return r===0?0:r===1?w:Math.max(...[Math.round(w*(1+r/30)),Math.round(w*r**0.1),Math.round((100*w)/(101.3-(2.67123*r)))])
+function findDuplicates(a,b=[]) {
+   for(let i=0;i<a.length;i++){
+    for(let j=i+1;j<a.length;j++){
+        if(Object.entries(a[i]).join``===Object.entries(a[j]).join``){
+            b.push(a[j])
+            a.splice(j,1)
+        }
+    }
+   }
+   return b
 }
 
-console.log(calculate1RM(135,20))
+console.log(findDuplicates([  { FirstName: 'Johnatan', LastName: 'Joestar', UserName: 'JoJo' },
+ { FirstName: 'Dio', LastName: 'Brando', UserName: 'DIO' },
+ { FirstName: 'Joseph', LastName: 'Joestar', UserName: 'JoJo' },
+ {
+  FirstName: 'Kars',
+  LastName: '',
+  UserName: 'the ultimate being' },
+ { FirstName: 'Johnatan', LastName: 'Joestar', UserName: 'JoJo' } ]))
