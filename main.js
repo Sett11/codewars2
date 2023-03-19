d443f8c2289b2f7abeffe9334c44030f72bbbfed
Jhonny <gonzodzen@mail.ru>
2023-03-19 21:14:59 +0300

    Digit Recovery

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-function pattern(n,s='*\n',z=n,v=n,q=n){
-  if(!Number.isInteger(n)||n<0)return "check input"
-  if(n===0)return ''
-  if(n===1)return '  *'
-  if(n===4)return '     *\n   1 2 3\n  # # # #\n * * * * *'
-  while(n%3!==0){
-    z++
-    if(z%3===0){
-      while(1){
-        v--
-        if(v%3===0){
-          if(z-n>n-v)n=v
-          else n=z
-          break
+const alph = {"ZERO":0,"ONE":1,"TWO":2,"THREE":3,"FOUR":4,"FIVE":5,"SIX":6,"SEVEN":7,"EIGHT":8,"NINE":9}
+function recover(s,x=s,a=[]){
+  function permute(s) {
+    const F=(c,p)=>{
+     if(c.length===0)return [p]
+     let a=[]
+     for(let i=0;i<c.length;i++){
+       let one=c.slice(0,i),two=c.slice(i+1)
+       a=a.concat(F(one+two,c[i]+p))
+     }
+     return a
+    }
+    const obj={}
+    F(s,'').forEach(e=>obj[e]=true)
+    return Object.keys(obj)
+   }   
+  let n=Object.entries(alph).map(e=>{
+    e[0]=permute(e[0])
+    return e
+  })
+    for(let i=0;i<s.length;i++){
+      for(let j=i;j<s.length+1;j++){
+        let tmp=s.slice(i,j)
+        for(let z=0;z<n.length;z++){
+          if(n[z][0].some(v=>v===tmp)){
+            s+=' '+n[z][1]
+            i++
+          }
+        }
         }
       }
+      return s.replace(/\D/g,'')||'No digits found'
     }
-  }
-  if(n===3)return "    *\n  1 2 3\n # # # #"
-  let a=Array(n).fill(1).map((e,i)=>{
-    e=[...i+1+''].reverse()[0]
-    return +e
-  }),c=n/3,r='# '.repeat(4)+'\n',l='* '.repeat(5)+'\n',i=3
-  while(c){
-    s+=a.slice(0,i).join` `+'\n'+r+l
-    c--,i+=3,r='# '.repeat(3)+r,l='* '.repeat(3)+l
-  }
-  s=s.split`\n`.filter(e=>e)
-  while(s.length>q){
-    s.pop()
-  }
-  const check=Math.max(...s.map(u=>u.length))
-  return s.map(e=>e.length<=check?' '.repeat(Math.floor((check-e.trim().length))/2+1)+e.trim():0).join`\n`
-}
 
-console.log(pattern(25))
+console.log(recover('NEOTWONEINEIGHTOWSVEEN'))
+console.log(recover('TCLIETITNHTAZERONXUZERONSEVEN'))
