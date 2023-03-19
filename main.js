e660470760683fdc0b2e9ab55f65debd067b75dc
Jhonny <gonzodzen@mail.ru>
2023-03-19 11:35:48 +0300

    Find missing numbers

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function missingNo(a) {
-  for(let i=0;i<=100;i++)if(!a.includes(i))return i
+function findMissingNumbers(a,b=[]){
+  for(let i=Math.min(...a);i<=Math.max(...a);i++)if(!a.includes(i))b.push(i)
+  return b
 }
 
-console.log(missingNo([9, 45, 53, 10, 100, 30, 85, 72, 69, 93, 98, 27, 73, 82, 91, 60, 5, 79, 88, 18, 71, 36, 44, 22, 89, 40, 59, 80, 81, 67, 25, 54, 13, 64, 56, 39, 48, 92, 84, 94, 87, 90, 77, 63, 32, 68, 37, 96, 23, 0, 95, 1, 52, 78, 6, 57, 50, 2, 46, 19, 76, 47, 14, 4, 3, 29, 17, 11, 21, 24, 74, 65, 12, 83, 28, 41, 66, 7, 58, 55, 51, 43, 97, 42, 86, 49, 31, 20, 75, 70, 34, 33, 38, 8, 15, 62, 35, 61, 99, 16]))
+console.log(findMissingNumbers([-3,-2,1,4]))
