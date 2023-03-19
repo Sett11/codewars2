321cf86b7619d240a27ed66c13f7793eed9927c4
Jhonny <gonzodzen@mail.ru>
2023-03-19 18:51:35 +0300

    Interesting Pyramid

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-function emotional(s,e){
-   if(e==='Natural'){
-    return s.split` `.map((e,i,v)=>i===0?e.charAt(0).toUpperCase()+e.slice(1):v[i-1].match(/[\,\.+\!+\?\;+\:+]/)?e.charAt(0).toUpperCase()+e.slice(1):e).join` `
-   }
-   if(e==='Prim'){
-    return s.split` `.map(e=>e.charAt(0).toUpperCase()+e.slice(1)).join` `
-   }
-   if(e==='Joyful'){
-    return s.split` `.map(e=>e.length<2?e:e.replace(/[aioue]/gi,e=>e.repeat(5))).map((e,i,v)=>i===0?e.charAt(0).toUpperCase()+e.slice(1):v[i-1].match(/[\,\.+\!+\?\;+\:+]/)?e.charAt(0).toUpperCase()+e.slice(1):e).join` `
-   }
-   if(e==='Painful'){
-    return s.split` `.map((e,i,v)=>i===0?e.charAt(0).toUpperCase()+e.slice(1):v[i-1].match(/[\,\.+\!+\?\;+\:+]/)?e.charAt(0).toUpperCase()+e.slice(1):e).map((e,i,v)=>i===v.length-1?e:e.replace(/[\W_]/g,'')).join`...`
-   }
-   if(e==='Excited'||e==='Angry'){
-    return s.split` `.map(e=>e.length<2?e:e.replace(/[aioue]/gi,e=>e.repeat(5))).map(e=>(e.charAt(0).toUpperCase()+e.slice(1)).replace(/[^\w]/g,'!!!')).join` `
-   }
-   if(e==='Rage'||e==='Ecstasy'){
-    s=s.toUpperCase().split` `.map(e=>e.length<2?e:e.replace(/[aioue]/gi,e=>e.repeat(5)))
-   return s.join` `.replace(/[\,\.+\!+\?\;+\:+]/g,'&').split`&`.filter(e=>e).map(e=>e+'!'.repeat(e.replace(/ /g,'').length)).join``
-   }
-   return s
+function pattern(n,s='*\n',z=n,v=n,q=n){
+  if(!Number.isInteger(n)||n<0)return "check input"
+  if(n===0)return ''
+  if(n===1)return '  *'
+  if(n===4)return '     *\n   1 2 3\n  # # # #\n * * * * *'
+  while(n%3!==0){
+    z++
+    if(z%3===0){
+      while(1){
+        v--
+        if(v%3===0){
+          if(z-n>n-v)n=v
+          else n=z
+          break
+        }
+      }
+    }
+  }
+  if(n===3)return "    *\n  1 2 3\n # # # #"
+  let a=Array(n).fill(1).map((e,i)=>{
+    e=[...i+1+''].reverse()[0]
+    return +e
+  }),c=n/3,r='# '.repeat(4)+'\n',l='* '.repeat(5)+'\n',i=3
+  while(c){
+    s+=a.slice(0,i).join` `+'\n'+r+l
+    c--,i+=3,r='# '.repeat(3)+r,l='* '.repeat(3)+l
+  }
+  s=s.split`\n`.filter(e=>e)
+  while(s.length>q){
+    s.pop()
+  }
+  const check=Math.max(...s.map(u=>u.length))
+  return s.map(e=>e.length<=check?' '.repeat(Math.floor((check-e.trim().length))/2+1)+e.trim():0).join`\n`
 }
 
-console.log(emotional('go where you want to go; those who hurt, May you have enough happiness to make you sweet, Please send this message to those people who mean something to you,','Joyful'))
+console.log(pattern(25))
