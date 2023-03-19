73f9570a7fcf6a8be646e602077d8b92b80b4efa
Jhonny <gonzodzen@mail.ru>
2023-03-19 23:36:11 +0300

    ASCII Fun #2: Funny Dots

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-function x(n,c=2){
-  let a=Array(n).fill(1).map(e=>Array(n).fill(0))
-  while(c){
-    for(let i=0;i<a.length;i++){
-      for(let j=0;j<a[i].length;j++){
-        if(i===j)a[i][j]=1
-      }
-    }
-    a=a.reverse(),c--
-  }
-  return a.map(e=>e.map(u=>u===1?'■':'□').join``).join`\n`
+function dot(n,m,a=[]){
+  let chank='| '+'o'.repeat(n).split``.join` | `+' |',s=('+'+'-'.repeat(3)+'+').repeat(n).replace(/\+\+/g,'+')+'\n'
+  while(m){a.push(chank);m--}
+  return s+a.map(e=>e+'\n').join(s)+s.slice(0,s.length-1)
 }
 
-console.log(x(21))
+console.log(dot(5,10))
