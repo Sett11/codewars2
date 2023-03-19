82c5b7989afcae19c857c89081afb7f57f9fcd3d
Jhonny <gonzodzen@mail.ru>
2023-03-20 01:57:37 +0300

    Keyword Cipher Helper

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-function encrypter(s){
-  const a='abcdefghijklmnopqrstuvwxyz'
-    const rot13=s=>{
-    return [...s].map(e=>{
-      if(!e.match(/[a-zA-Z]/))return e
-      return e.charCodeAt()
-    }).map(e=>{
-      if(typeof e==='number'&&e>=65&&e<=90){
-        e-=13
-        if(e<65)e=90-(65-e)+1
-        return String.fromCharCode(e)
-      }
-      if(typeof e==='number'&&e>=97&&e<=122){
-        e-=13
-        if(e<97)e=122-(97-e)+1
-        return String.fromCharCode(e)
-      }
-      return e
-    })
+class KeywordCipher{
+  constructor(a,k){
+    this.a=a
+    this.k=k
+    this.r=[...new Set(k+a)]
   }
-  return rot13(s).map(e=>a.includes(e)?a[((a.length-a.indexOf(e)))-1]:e).join``
+  encode(s){
+    return [...s].map(e=>this.r[this.a.indexOf(e)]?this.r[this.a.indexOf(e)]:e).join``
+  }
+  decode(s){
+    return [...s].map(e=>this.a[this.r.indexOf(e)]?this.a[this.r.indexOf(e)]:e).join``
+  }
 }
 
-console.log(encrypter("welcome to the organization"))
+const abc = "abcdefghijklmnopqrstuvwxyz"
+const key = "keyword"
+const cipher = new KeywordCipher(abc, key)
+
+console.log(cipher.decode("key"))
