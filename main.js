9cbaf644e48babf91ce56cb08df78726a8006092
Jhonny <gonzodzen@mail.ru>
2023-03-20 23:07:55 +0300

    Update inventory in your smartphone store

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-isInteresting=(n,b,a=[n,n+1,n+2])=>{
-    for(let i=0;i<a.length;i++){
-        let tmp=[...a[i]+''].map(Number),dmb=tmp.map(e=>e===0?10:e),dmb2=tmp.slice()
-        if(tmp.length<3)continue
-        if(i===0){
-            if(a[i]===+tmp.slice().reverse().join``||b.includes(a[i])||tmp[0]!==0&&tmp.slice(1).every(e=>e===0)||[...new Set(tmp)].length===1)return 2
-            let arr=[]
-            for(let j=0;j<dmb.length-1;j++){
-                arr.push(Math.abs(dmb[j]-dmb[j+1]))
-            }
-            let f=arr.every(e=>e===1)
-            if(f&&+dmb.slice().sort((a,b)=>a-b).map(e=>e===10?0:e).join``===a[i]||arr.map(e=>e===9?1:e).every(e=>e===1)&&+dmb2.slice().sort((a,b)=>b-a).join``===a[i])return 2
+function updateInventory(c,n) {
+  return c.map((e,i,v)=>{
+    for(let i=0;i<n.length;i++){
+        if(e[1]===n[i][1]){
+            e.push(n[i][0])
+            n[i]='&'
         }
-        if(a[i]===+tmp.slice().reverse().join``||b.includes(a[i])||tmp[0]!==0&&tmp.slice(1).every(e=>e===0)||[...new Set(tmp)].length===1)return 1
-        let arr=[]
-        for(let j=0;j<dmb.length-1;j++){
-            arr.push(Math.abs(dmb[j]-dmb[j+1]))
-        }
-        let f=arr.every(e=>e===1)
-        if(f&&+dmb.slice().sort((a,b)=>a-b).map(e=>e===10?0:e).join``===a[i]||arr.map(e=>e===9?1:e).every(e=>e===1)&&+dmb2.slice().sort((a,b)=>b-a).join``===a[i])return 1
     }
-    return 0
+    return e
+  }).concat(n).filter(e=>e!=='&').sort((a,b)=>a[1].localeCompare(b[1])).map(e=>{
+    if(e[2])e[0]+=e[2]
+    return [e[0],e[1]]
+  })
 }
 
-console.log(isInteresting(3210, []))
+console.log(updateInventory([
+    [25, 'HTC'],
+    [1000, 'Nokia'],
+    [50, 'Samsung'],
+    [33, 'Sony'],
+    [10, 'Apple']
+  ],[
+    [5, 'LG'],
+    [10, 'Sony'],
+    [4, 'Samsung'],
+    [5, 'Apple']
+  ]))
