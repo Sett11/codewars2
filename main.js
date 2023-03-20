99fa0384084ddeb2fd8a445e808b916a2ba9f9e0
Jhonny <gonzodzen@mail.ru>
2023-03-21 00:30:31 +0300

    Simple Fun #305: Typist

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-function updateInventory(c,n) {
-  return c.map((e,i,v)=>{
-    for(let i=0;i<n.length;i++){
-        if(e[1]===n[i][1]){
-            e.push(n[i][0])
-            n[i]='&'
-        }
-    }
-    return e
-  }).concat(n).filter(e=>e!=='&').sort((a,b)=>a[1].localeCompare(b[1])).map(e=>{
-    if(e[2])e[0]+=e[2]
-    return [e[0],e[1]]
-  })
+function typist(s,c=0){
+    for(let i=0;i<s.length-1;i++)if(s[i]===s[i].toUpperCase()&&s[i+1]===s[i+1].toLowerCase()||s[i]===s[i].toLowerCase()&&s[i+1]===s[i+1].toUpperCase())c++
+    if(s[0]===s[0].toUpperCase())c++
+    return s.length+c
 }
 
-console.log(updateInventory([
-    [25, 'HTC'],
-    [1000, 'Nokia'],
-    [50, 'Samsung'],
-    [33, 'Sony'],
-    [10, 'Apple']
-  ],[
-    [5, 'LG'],
-    [10, 'Sony'],
-    [4, 'Samsung'],
-    [5, 'Apple']
-  ]))
+console.log(typist('AmericanRAILWAY'))
