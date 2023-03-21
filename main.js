3bd6f0d657a14e86dfef8281b978342087a58817
Jhonny <gonzodzen@mail.ru>
2023-03-22 00:07:12 +0300

    For whom the Bell tolls

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-function loopArr(a,d,s) {
-    while(s){
-        if(d==='left')a.push(a.shift())
-        if(d==='right')a.unshift(a.pop())
-        s--
-    }
-    return a
+function bell(n,a=[n],c=n) {
+    if(n===1)return [1]
+    if(n===2)return [2,2]
+    while(c>0){a.push(a[a.length-1]+(c-2));c-=2}
+    a.pop()
+    return n%2===0?a.concat(a.slice().reverse()):a.concat(a.slice(0,a.length-1).reverse())
 }
 
-console.log(loopArr([1, 5, 87, 45, 8, 8], 'left', 2))
+console.log(bell(3))
