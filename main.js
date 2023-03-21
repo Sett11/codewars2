b2696751249263eab1cd9dc1acec59a270b8db00
Jhonny <gonzodzen@mail.ru>
2023-03-22 01:19:23 +0300

    flatten()

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-sumIntervals=g=>{
-    return g.sort((a,b)=>a[0]-b[0]).reduce((a,c)=>{
-        if(a.length&&c[0]<=a[a.length-1][1])a[a.length-1][1]=Math.max(c[1],a[a.length-1][1])
-        else a.push(c)
-        return a
-    },[]).map(e=>e[1]-e[0]).reduce((a,c)=>a+c,0)
-}
+function flatten(...a) {
+    Array.prototype.f=function(d=1){
+        const r=[]
+        this.forEach(e=>{
+            if(Array.isArray(e)&&d>0)r.push(...e.f(d-1))
+            else r.push(e)
+        })
+        return r
+    }
+    return [...a].f(100)
+  }
 
-console.log(sumIntervals([
-    [0, 20],
-    [-100000000, 10],
-    [30, 40]
- ]))
+  console.log(flatten([[[[[1]]]]], ['a', [3,['b']]], null, 5, 'c'))
