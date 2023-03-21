f10b113e5d619d6d43580759b4717b3313596978
Jhonny <gonzodzen@mail.ru>
2023-03-22 00:26:51 +0300

    Union of Intervals

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function snakesOn(a) {
-    return [...new Set(a.map(e=>e.join``).join``)].filter(e=>e!=='_').length
+intervalInsert=(g,i)=>{
+    g.push(i)
+    g=g.sort((a,b)=>a[0]-b[0])
+    for(let i = 0; i < g.length-1; i++){
+      if(g[i][1]>=g[i+1][0]&&g[i][1]<=g[i+1][1]){
+        g[i][1] = g[i+1][1]
+        g = g.slice(0,i+1).concat(g.slice(i+2))
+        i--
+      }
+      else if(g[i][1]>g[i+1][1]){
+        g = g.slice(0,i+1).concat(g.slice(i+2))
+        i--
+      }
+    }
+    return g
 }
 
-  console.log(snakesOn([  
-    '__AAAAA__A'.split(''),
-    '__A___A__A'.split(''),
-    'K_____AAAA'.split(''),
-    'K_DDDD____'.split(''),
-    'K____DDDD_'.split('')
-  ]))
+console.log(intervalInsert([[0,2], [3, 6], [7, 7], [9, 12]], [1, 8]))
