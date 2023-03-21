ec69d8b77e723d1631f95a97bf3122d013d0f986
Jhonny <gonzodzen@mail.ru>
2023-03-21 23:09:50 +0300

    Loop Array

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-function combinePathsUri(...a){
-    const check=[...new Set(...a)]
-      if(![...a].length||!a[0]||check.length===1&&check[0]==='\\'||check[0]==='/'&&check.length===1)return '/'
-      let r=[...a].filter(e=>e).map(e=>e.replace(/\\/g,'/').replace(/ +/g,'').replace(/\/+/g,'/')).join`/`.trim().replace(/\/{2,}/g,'/')
-      if(r[0]!=='/')r='/'+r
-      while(!r[r.length-1].match(/[A-Za-z\.]/))r=r.slice(0,r.length-1)
-      return r
-  }
+function loopArr(a,d,s) {
+    while(s){
+        if(d==='left')a.push(a.shift())
+        if(d==='right')a.unshift(a.pop())
+        s--
+    }
+    return a
+}
 
-console.log(combinePathsUri( '/complex/path/with/slashes/inside/',
-'random',
-'part',
-'\\ / \\',
-'/complex/path/with/slashes/inside/' ))
-console.log(combinePathsUri(" .. ", "/complex/path/with/slashes/inside/", " . ", "\\complex\\path\\with\\back\\slashes\\inside\\"))
+console.log(loopArr([1, 5, 87, 45, 8, 8], 'left', 2))
