c396a04c6536ee574a0571b00a9b52af543b1f3e
Jhonny <gonzodzen@mail.ru>
2023-03-22 19:41:01 +0300

    Equalize the array!

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function string_evaluation(s,c){
-    s=[...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})
-    return c.map(e=>eval(e.replace(/[a-zA-Z\@\#\$\%\^\&\*\(\)\_\{\}\[\]]/g,e=>s[e]?s[e]:e)))
-}
+equalize=(a,c=a[0])=>a.map(e=>e-c===0?'+0':e-c>0?`+${e-c}`:`${e-c}`)
 
-  console.log(string_evaluation('abb', ['a>b','b==a','b<=a','b>a','b!=b','a==1','b==1']))
+console.log(equalize([20,30,35,10]))
