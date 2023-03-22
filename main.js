b5e85c956fa7fa7aba7490e19137becd2d232c27
Jhonny <gonzodzen@mail.ru>
2023-03-22 20:31:02 +0300

    Simplify the number!

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-equalize=(a,c=a[0])=>a.map(e=>e-c===0?'+0':e-c>0?`+${e-c}`:`${e-c}`)
+function simplify(n,a=[...n+''].map(Number).filter(e=>e),r=[]){
+    for(let i=0;i<a.length;i++){
+        if(n<10){
+            r.push(`${n}`)
+            break
+        }
+        for(let j=1;;j*=10){
+            if(a[i]*j===n){
+                r.push(`${a[i]}*${j}`)
+                n-=a[i]*j
+                break
+            }
+            if(a[i]*j>n){
+                r.push(`${a[i]}*${j/10}`)
+                n-=(a[i]*j/10)
+                break
+            }
+        }
+    }
+    return r.filter(e=>e!=='0').join`+`
+}
 
-console.log(equalize([20,30,35,10]))
+console.log(simplify(9090))
