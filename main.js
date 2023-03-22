33ba265ba658379346fd65749d9143d11a237e4c
Jhonny <gonzodzen@mail.ru>
2023-03-23 00:12:50 +0300

    Interweaving strings and removing digits

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-removeNoise=s=>s.replace(/[\%\$\&\#\@\º\ª\·\|\/\\]/g,'')
+function interweave(s1,s2,a=[]) {
+    for(let i=0;i<s1.length;i++)a.push(s1[i],s2[i])
+    return a.filter(e=>e).join``.replace(/\d/g,'').trim()
+}
+
+console.log(interweave("h3lo", "el4"))
