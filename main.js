dc1bfd36ca90e685e163edf90337e05fe3b3bb0e
Jhonny <gonzodzen@mail.ru>
2023-03-23 00:04:35 +0300

    Remove the noise from the string

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-function check(a){
-    a=a.map((e,i)=>e.map((u,j)=>u!=='*'?[i,j]:0).filter(e=>e).flat()).filter(e=>e.length)
-    return a.length===1||a[0][0]===a[1][0]||a[0][1]===a[1][1]||Math.abs(a[1][0]-a[0][0])===Math.abs(a[1][1]-a[0][1])?true:false
-}
-
-console.log(check([
-    [ '*', 'q', '*', '*', 'k' ],
-    [ '*', '*', '*', '*', '*' ],
-    [ '*', '*', '*', '*', '*' ],
-    [ '*', '*', '*', '*', '*' ],
-    [ '*', '*', '*', '*', '*' ]
-  ]))
+removeNoise=s=>s.replace(/[\%\$\&\#\@\º\ª\·\|\/\\]/g,'')
