10994b15fc17c6aa7ebdaa444dbb66d29d2a461a
Jhonny <gonzodzen@mail.ru>
2023-03-23 22:52:44 +0300

    Reverse and Invert

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-function invertedRanges(r,a=[]) {
-    if(!r.length)return [[0,100]]
-    r=r.sort((a,b)=>a[0]-b[0]).reduce((a,c)=>{
-      if(a.length&&c[0]<=a[a.length-1])a[a.length-1]=Math.max(c[1],a[a.length-1])
-      else a.push(c)
-      return a
-    },[])
-    if(r[0][0]>1)a.push([0,r[0][0]-1])
-    for(let i=0;i<r.length;i++){
-      if(i!==r.length-1&&Math.abs(r[i+1][0]-r[i][1])>1)a.push([r[i][1]+1,r[i+1][0]-1])
-      if(i===r.length-1&&r[r.length-1][1]<100)a.push([r[r.length-1][1]+1,100])
-    }
-    return a
-  }
+function reverseInvert(a){
+    return a.filter(e=>Number.isInteger(e)).map(e=>e>0?-[...e+''].map(Number).reverse().join``:e<0?+[...e+''].filter(u=>u!=='-').map(Number).reverse().join``:-0)
+}
 
-console.log(invertedRanges([ [ 40, 46 ], [ 48, 52 ], [ 62, 63 ] ]))
+  console.log(reverseInvert([1,12,'a',3.4,87,99.9,-42,50,5.6]))
