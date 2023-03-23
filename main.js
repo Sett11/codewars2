556406cd06303b7a633cba2329e8915abde48258
Jhonny <gonzodzen@mail.ru>
2023-03-24 00:12:34 +0300

    Strip Comments

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-extend=(...n)=>{
-    let r=[],o={},a=[...n].filter(e=>isObject(e)).map(e=>Object.entries(e).map(u=>r.push(u)))
-    r.forEach(e=>o[e[0]]===undefined?o[e[0]]=e[1]:null)
-    return o
+function solution(c,m){
+    c=c.replace(/\n/g,'|'),s=[...c]
+    for(let i=0;i<s.length;i++){
+        if(m.includes(s[i]))s=s.slice(0,i-1).concat(s.slice(s.indexOf('|',i)!==-1?s.indexOf('|'):s.length))
+    }
+    return s.join``.replace(/\|/g,'\n')
 }
 
-console.log(extend({ a: 1, b: 2, length: 6 }, [], 'nope', false, [Function], { c: 3, a: 3 }))
+console.log(solution("Q @b\nu\ne -e f g", ["@", "-"]))
