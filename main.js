f3b69140d482c018fb7b7c0dc81196cb1126408d
Jhonny <gonzodzen@mail.ru>
2023-03-23 18:00:10 +0300

    Vodka festival.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-diff=(a,b)=>[...new Set(a.filter(e=>!b.includes(e)).concat(b.filter(e=>!a.includes(e))))].sort()
-
-console.log(diff(["a","b","z","d","e","d"],["a","b", "j","j"]))
+function vodkaConsumption(s){
+    return s.map(e=>e.replace(/\d+/g,'')==='.l'?+e.replace(/[^\d.]/g,'')*1000:+e.replace(/\D/g,'')).reduce((a,c)=>a+c,0)+'ml'
+}
+console.log(vodkaConsumption([
+    '7097ml', '7729ml',
+    '11.76l', '7.08l',
+    '1.85l',  '9.01l',
+    '4136ml', '4638ml'
+  ]))  
+console.log(vodkaConsumption([
+    '7470ml', '379ml',
+    '2257ml', '15.67l',
+    '6633ml', '12.9l',
+    '3.46l'
+  ]))  
