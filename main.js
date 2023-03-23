07a76cb649925ef5e1cb1fd7210c3cd2419972c2
Jhonny <gonzodzen@mail.ru>
2023-03-23 18:22:03 +0300

    Harvest Festival

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-function vodkaConsumption(s){
-    return s.map(e=>e.replace(/\d+/g,'')==='.l'?+e.replace(/[^\d.]/g,'')*1000:+e.replace(/\D/g,'')).reduce((a,c)=>a+c,0)+'ml'
+function plant(s,w,f,t){
+ let r=('-'.repeat(w)+s.repeat(f)).repeat(w)
+ return t>19&&t<31?r:r.replace(/./g,e=>e===s?'':e)+s
 }
-console.log(vodkaConsumption([
-    '7097ml', '7729ml',
-    '11.76l', '7.08l',
-    '1.85l',  '9.01l',
-    '4136ml', '4638ml'
-  ]))  
-console.log(vodkaConsumption([
-    '7470ml', '379ml',
-    '2257ml', '15.67l',
-    '6633ml', '12.9l',
-    '3.46l'
-  ]))  
+
+console.log(plant("#", 10, 2, 15))
