a5d3964584ae45fecb7bb0c6bffaa97d6a4a820a
Jhonny <gonzodzen@mail.ru>
2023-03-24 13:12:00 +0300

    Shortest Distance to a Character

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-function whoWon(p,y,x,a=[]) {
-    return Object.entries(p).map(e=>{
-        e=[e[0],e[1].split`, `.map(u=>!u.match(/\d+/g)?10:+u).reduce((a,c)=>a+c,0)]
-        if(x.includes(e[0]))e[1]+=typeof y==='string'?10:y
-        if(e[1]>21){
-            a.push(e)
-            e='&'
-        }
-        return e
-    }).filter(e=>e!=='&').sort((a,b)=>b[1]-a[1]||a[0].localeCompare(b[0])).concat(a)
-  }
+function shortesttoChar(s,c,a=Array(s.length),z=-Infinity){
+    if(!s.includes(c)||!z||!c)return []
+    for(let i=0;i<s.length;++i){
+        if(s[i]===c)z=i
+        a[i]=i-z
+    }
+    z=Infinity
+    for(let i=s.length-1;i>=0;--i){
+        if(s[i]===c)z=i
+        a[i]=Math.min(a[i],z-i)
+    }
+    return a
+}
 
-  console.log(whoWon({"Ben":"9, 7", "Amy":"9, J", "Sam":"K, Q",},'K', ["Amy"]))
+console.log(shortesttoChar("lovecodewars",'e'))
+console.log(shortesttoChar("aaaabbbb",'b'))
+console.log(shortesttoChar("aabbaabb", "a"))
+
+
+// function diagonals(m,a=[]) {
+//     for(let i=0;i<m.length;i++){
+//         for(let j=0;j<m.length;j++){
+//             let tmp=[i,j]
+//             if(tmp.join``===tmp.reverse().join``)a.push(m[i][j])
+//         }
+//     }
+//     return a
+// }
+
+// console.log(diagonals([
+//     [ 1, 2, 3 ],
+//     [ 4, 5, 6 ],
+//     [ 7, 8, 9 ]
+//   ]))
