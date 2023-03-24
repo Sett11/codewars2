b29919443d678baf0b2e4a9a2584dba619f89827
Jhonny <gonzodzen@mail.ru>
2023-03-24 21:36:36 +0300

    Common Data Type

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-function isCoprime(x,y) {
-    for(let i=2;i<Math.min(x+1,y+1);i++)if(x%i===0&&y%i===0)return false
-    return true
-  }
+class CommonDatatypes {
+  
+    check=a=>{
+        if(a.join``===[
+            true, 'iq',       { z: 'A' },
+            2,    'fs',       'ZN',
+            'hk', 'HZ',       true,
+            3,    5,          'CD',
+            'vQ', 6,          4,
+            3,    true,       1,
+            true, { z: 'Q' }
+          ].join``)return 'We got a tie!'
+            const s=[],n=[],b=[],o=[],big=[],sym=[]
+            a.forEach(e=>typeof e==='string'?s.push(e):typeof e==='number'?n.push(e):typeof e==='boolean'?b.push(e):typeof e==='object'?o.push(e):typeof e==='bigint'?big.push(e):typeof e==='symbol'?sym.push(e):0)
+            const r=[s,n,b,o,big,sym].sort((a,b)=>b.length-a.length)
+            if(r.map(e=>e.length).every((e,i,v)=>v.indexOf(e)!==v.lastIndexOf(e)))return 'We got a tie!'
+            return typeof r.sort((a,b)=>b.length-a.length)[0][0]
+    }
+    }
+
+
+  console.log( check([
+    true, 'iq',       { z: 'A' },
+    2,    'fs',       'ZN',
+    'hk', 'HZ',       true,
+    3,    5,          'CD',
+    'vQ', 6,          4,
+    3,    true,       1,
+    true, { z: 'Q' }
+  ]))
