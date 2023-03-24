9820250d4eb9293b43df10ae06150b3a410c84be
Jhonny <gonzodzen@mail.ru>
2023-03-24 10:33:03 +0300

    Strip Url Params

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-shortenSpeech=s=>{	
-    return s.split` `.map(e=>{
-        if(e.length<4||!e.slice(3).match(/[aioue]/))return e
-        e=e.split``
-        for(let i=3;i<e.length;i++){
-            if(e[i].match(/[aioue]/))e=e.slice(0,i).join``+'.'
+function stripUrlParams(u,p=[]){
+    if(!u.match(/\?/))return u
+    let one=u.slice(0,u.indexOf('?')+1),two=u.slice(u.indexOf('?')+1).split`&`
+    two=two.map((e,j,v)=>{
+        for(let i=0;i<v.length;i++){
+            if(e.replace(/[^a-zA-Z]/g,'')===v[i].replace(/[^a-zA-Z]/g,'')&&i!==j){
+                v.splice(i,1)
+                i--
+            }
         }
         return e
-    }).join` `
+    }).filter(e=>e&&!p.some(z=>e.match(z))).join`&`
+    let res=one+two
+    if(res[res.length-1]==='?')res=res.slice(0,res.length-1)
+    return res
 }
 
-console.log(shortenSpeech("Hello, do you want a coffee ? "))
+  console.log(stripUrlParams('www.codewars.com?a=1&b=2',[]))
