c86ed290a927ed37fe50bc42ae32979bec61a40d
Jhonny <gonzodzen@mail.ru>
2023-03-24 20:37:42 +0300

    Spot the Differences

--- main.js
+++ main.js
@@ -1,34 +1,4 @@
-function shortesttoChar(s,c,a=Array(s.length),z=-Infinity){
-    if(!s.includes(c)||!z||!c)return []
-    for(let i=0;i<s.length;++i){
-        if(s[i]===c)z=i
-        a[i]=i-z
-    }
-    z=Infinity
-    for(let i=s.length-1;i>=0;--i){
-        if(s[i]===c)z=i
-        a[i]=Math.min(a[i],z-i)
-    }
+function spot(s1,s2,a=[]){
+    [...s1].forEach((e,i)=>e!==s2[i]?a.push(i):0)
     return a
-}
-
-console.log(shortesttoChar("lovecodewars",'e'))
-console.log(shortesttoChar("aaaabbbb",'b'))
-console.log(shortesttoChar("aabbaabb", "a"))
-
-
-// function diagonals(m,a=[]) {
-//     for(let i=0;i<m.length;i++){
-//         for(let j=0;j<m.length;j++){
-//             let tmp=[i,j]
-//             if(tmp.join``===tmp.reverse().join``)a.push(m[i][j])
-//         }
-//     }
-//     return a
-// }
-
-// console.log(diagonals([
-//     [ 1, 2, 3 ],
-//     [ 4, 5, 6 ],
-//     [ 7, 8, 9 ]
-//   ]))
+  }
