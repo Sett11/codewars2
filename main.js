10bf3f009fe6296fc7896ecb5dbdbfd16527e7b9
Jhonny <gonzodzen@mail.ru>
2023-03-25 19:47:59 +0300

    I'm everywhere!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-say=s=>c=>`${s} ${c}`
+i=w=>!w||w[0]==='I'||w[0]==='i'||!w[0].match(/[A-Z]/)?"Invalid word":w.replace(/[^aioue]/gi,'').length>=w.replace(/[aioue]/gi,'').length?"Invalid word":'i'+w
 
-console.log(say("Hello")("World"))
+
+console.log(i('Inspire'))
+console.log(i('Phone'))
