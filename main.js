00929936cc98a67bcc7972c5ec726fc933c24c2c
Jhonny <gonzodzen@mail.ru>
2023-03-26 13:18:44 +0300

    Simple Fun #147: Find The Missing Tree

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function missingTerm(n,l,a=[]) {
+function findTheMissingTree(t){
+  t=Object.entries(t.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))
   while(1){
-    let t=a.reduce((a,c)=>a+c,0)
-    if(t>l)return t-l
-    a.push(n)
-    n++
+    let tmp=t.shift()
+    if(t.every(e=>e[1]===t[0][1]))return +tmp[0]
+    t.push(tmp)
   }
 }
 
-console.log(missingTerm(1,4))
+console.log(findTheMissingTree([1, 1, 1, 1, 1, 1, 1, 22, 22, 22, 22, 22, 22, 22, 3, 3, 3, 3, 3, 3]))
