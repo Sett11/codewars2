5b257cde3bacb941232de4521e9aafd67e279c7d
Jhonny <gonzodzen@mail.ru>
2023-03-26 18:18:52 +0300

    Simple Fun #231: Kth Divisor

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-function greatestCommonPrimeDivisor(a,b){
- const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
- for(let i=Math.ceil(Math.max(a,b)/2)+1;i>1;i--)if(f(i)&&a%i===0&&b%i===0)return i
- return -1
+function kthDivisor(n,k,a=[],c=1){
+  while(c<=~~n/2){
+    if(n%c===0){a.push(c,n/c);c++}c++
+  }
+  return [...new Set(a)].sort((a,b)=>a-b)[k-1]||-1
 }
 
-console.log(greatestCommonPrimeDivisor(234,299))
+console.log(kthDivisor(16,5))
