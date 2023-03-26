2775a0e99f6d8a8694717b47ffe0d7921b26057f
Jhonny <gonzodzen@mail.ru>
2023-03-27 01:36:25 +0300

    Max span

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-maxedOut=(a,b=a.reduce((a,c)=>a+c**3,0))=>Number.isSafeInteger(b)?b:'You\'ve pushed me to the max!'
+const maxSpan=(a,c=0)=>{
+  a.forEach((e,i,v)=>c=Math.max(v.lastIndexOf(e)-v.indexOf(e),c))
+  return c+1
+}
 
-console.log(maxedOut([-530,230,5600,40,5,7200]))
-console.log(maxedOut([7600,32300,66700,22200,4554,323000,5555]))
+console.log(maxSpan([1, 4, 2, 1, 4, 1, 4, 9, 7, 7, 7]))
