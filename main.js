1f341749f271ddf0043cb452f8611dbc30a60095
Jhonny <gonzodzen@mail.ru>
2023-03-27 00:43:58 +0300

    Make Object Into Iterable Array

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-function max(a){
-  if(!a.length)return -Infinity
-  const b=[],r=a.pop()
-  if(a.every(e=>r>e)&&b.every(e=>r>e))return r
-  return max(a)
- }
- console.log(max([99,2,100,4,5]))
+function shapeTimesObjectIntoIterableArray(t,a=[{}],c=0){
+  if(!t)return []
+  a[c].days=[],t=Object.entries(t)
+  for(let i=0;i<t.length;i++){
+    if(!t[i][1][0])continue
+        if(!a[c].open){
+        a[c].open=t[i][1][0].open
+        a[c].close=t[i][1][0].close
+        a[c].days.push(t[i][0])
+      }
+      if(a[c].open===t[i][1][0].open&&!a[c].days.includes(t[i][0])){
+        a[c].days.push((t[i][0]))
+      }
+        if(t[i][1][0].open&&a[c].open!==t[i][1][0].open){
+          a.push({}),c+=1,i-=1,a[c].days=[]
+        }
+      }
+  return !a[c].days.length?[]:a.map(e=>{
+    e=Object.entries(e)
+    e.push(e.shift())
+    const o={}
+    e.forEach(u=>o[u[0]]=u[1])
+    return o
+  })
+}
+
+console.log(shapeTimesObjectIntoIterableArray({ monday: [ { open: '00:00', close: '23:00' } ],
+tuesday: [ { open: '00:00', close: '23:00' } ],
+wednesday: [],
+thursday: [ { open: '01:00', close: '22:00' } ],
+friday: [ { open: '01:00', close: '22:00' } ],
+saturday: [],
+sunday: [] }
+))
