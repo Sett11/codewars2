e06175d7bed95e62cd5379289417073409eed13c
Jhonny <gonzodzen@mail.ru>
2023-03-26 22:37:07 +0300

    Thinkful - List and Loop Drills: Lists of lists

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function cyclops(n) {
-  n=(n).toString(2)
-  return [...n.slice(0,~~n.length/2)].every(e=>e==='1')&&[...n.slice(Math.ceil(n.length/2))].every(e=>e==='1')&&n[Math.floor(n.length/2)]==='0'
-}
+processData=b=>b.reduce((a,c)=>a*(c[0]-c[1]),1)
 
-console.log(cyclops(5))
+console.log(processData([[2, 9], [2, 4], [7, 5]]))
