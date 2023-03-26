95fb4d824074177f3555c4044da7604fa5069c72
Jhonny <gonzodzen@mail.ru>
2023-03-26 14:22:50 +0300

    Calculate the function f(x) for a simple linear sequence (Easy)

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-function missingAlphabets(s,c=0,a=[],t=s){
-  const l='abcdefghijklmnopqrstuvwxyz'.split``
-  s=Object.entries([...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))
-  s.forEach(e=>c=(Math.max(e[1],c)));s.forEach(e=>e[1]!==c?a.push(e[0].repeat(c-e[1])):0);l.forEach(e=>!t.includes(e)?a.push(e.repeat(c)):0)
-  return a.sort().join``
+function getFunction(n,a=[],c=n[0]){
+  console.log(n)
+  if(n.every(e=>e===n[0]))return `f(x) = ${n[0]}`
+  for(let i=n.length-1;i>1;i--)a.push(n[i]-n[i-1])
+  if(!a.every(e=>e===a[0]))return "Non-linear sequence"
+  if(c<0&&a[0]<0)return `f(x) = -${Math.abs(a[0])===1?'':Math.abs(a[0])}x - ${Math.abs(c)}`
+  if(c<0)return `f(x) = ${Math.abs(a[0])===1?'':a[0]}x - ${Math.abs(c)}`
+  if(a[0]===1&&!c)return `f(x) = x`
+  if(c>0&&a[0]<0)return `f(x) = -${Math.abs(a[0])===1?'':Math.abs(a[0])}x + ${c}`
+  if(c>0)return `f(x) = ${Math.abs(a[0])===1?'':Math.abs(a[0])}x + ${c}`
+  return `f(x) = ${Math.abs(a[0])===1?'':a[0]}x`
 }
 
-console.log(missingAlphabets('codewars'))
+console.log(getFunction([0, 1, 2, 3, 4]))
+console.log(getFunction([0, 3, 6, 9, 12]))
+console.log(getFunction([ 3, -1, -5, -9, -13 ]))
