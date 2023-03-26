13768146950649cf98265dc20d8fbd71cba4e17e
Jhonny <gonzodzen@mail.ru>
2023-03-26 17:50:43 +0300

    Array element parity

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-greatestCommonFactor=(a,c=Math.min(...a))=>a.every(e=>e%c===0)?c:greatestCommonFactor(a,c-1)
+solve=a=>a.filter((e,i,v)=>!v.includes(-e))[0]
 
-console.log(greatestCommonFactor([46, 14, 20, 88]))
+console.log(solve([-110,110,-38,-38,-62,62,-38,-38,-38]))
+console.log(solve([1,-1,2,-2,3]))
