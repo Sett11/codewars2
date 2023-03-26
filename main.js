f63b5fd0264bd59b794833a15c9bd7c53cda531d
Jhonny <gonzodzen@mail.ru>
2023-03-26 14:50:57 +0300

    Infinite Sequences

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function getFunction(n,a=[],c=n[0]){
-  console.log(n)
-  if(n.every(e=>e===n[0]))return `f(x) = ${n[0]}`
-  for(let i=n.length-1;i>1;i--)a.push(n[i]-n[i-1])
-  if(!a.every(e=>e===a[0]))return "Non-linear sequence"
-  if(c<0&&a[0]<0)return `f(x) = -${Math.abs(a[0])===1?'':Math.abs(a[0])}x - ${Math.abs(c)}`
-  if(c<0)return `f(x) = ${Math.abs(a[0])===1?'':a[0]}x - ${Math.abs(c)}`
-  if(a[0]===1&&!c)return `f(x) = x`
-  if(c>0&&a[0]<0)return `f(x) = -${Math.abs(a[0])===1?'':Math.abs(a[0])}x + ${c}`
-  if(c>0)return `f(x) = ${Math.abs(a[0])===1?'':Math.abs(a[0])}x + ${c}`
-  return `f(x) = ${Math.abs(a[0])===1?'':a[0]}x`
+const sequence=(f)=>{
+  return {
+    take(n){
+      let a=[],c=0
+      while(a.length<n){a.push(f(c));c++}
+      return a
+    },
+    takeWhile(fn){
+      let a=[],c=0,z=0
+      while(z<200){if(fn(f(c)))a.push(f(c));c++,z++}
+      return a
+    }
+  }
 }
 
-console.log(getFunction([0, 1, 2, 3, 4]))
-console.log(getFunction([0, 3, 6, 9, 12]))
-console.log(getFunction([ 3, -1, -5, -9, -13 ]))
+console.log(sequence( n => n * 4 ).takeWhile( n => n < 20 ))
