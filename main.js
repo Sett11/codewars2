c5dcd626edd4bdb3505ccfa11f2f7412bc0ea2de
Jhonny <gonzodzen@mail.ru>
2023-03-26 11:12:27 +0300

    Simple Fun #374: Minimal Subtractor

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function finalAttackValue(x,n){
-  const f=(a,b)=>{const r=[];for(let i=1;i<Math.max(a,b);i++){if(a%i===0&&b%i===0)r.push(i)}return Math.max(...r)}
-  n.forEach(e=>e<=x?x+=e:x+=f(x,e))
-  return x
+function minimalSubtractor(n,a=[],i=0,z=n){
+      while(!a.filter(e=>e<=1).length){
+      n=(n-i)/2
+      a.push(n)
+    }
+     if(a.includes(1))return i
+    n=z,i+=1,a=[]
+    return minimalSubtractor(n,a,i,z)
 }
 
-console.log(finalAttackValue(50,[50,105,200]))
+console.log(minimalSubtractor(14))
