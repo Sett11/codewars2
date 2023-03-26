9fed448dc0d39a075049049e91ad9c93e442b995
Jhonny <gonzodzen@mail.ru>
2023-03-26 17:02:45 +0300

    Greatest Common Factor of an Array

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function findNum(n,a=[],c=0){
-  while(a.length<n){
-    c++
-    if(c<=10)a.push(c)
-    else{
-      for(let i=11;;i++){
-        if(!a.includes(i)&&[...i+''].every(e=>![...a[a.length-1]+''].includes(e))){
-          c=i;a.push(c);break
-        }
-      }
-    }
-  }
-  return a[n-1]
-}
+greatestCommonFactor=(a,c=Math.min(...a))=>a.every(e=>e%c===0)?c:greatestCommonFactor(a,c-1)
 
-  console.log(findNum(500))
+console.log(greatestCommonFactor([46, 14, 20, 88]))
