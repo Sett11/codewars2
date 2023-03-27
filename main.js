15a9b70603fddbdcca61a16020dbab4108dc8c2d
Jhonny <gonzodzen@mail.ru>
2023-03-27 22:19:40 +0300

    Min or Max of . . .

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function parameter(n,f=(x,y=[...x+''].map(Number))=>[y.reduce((a,c)=>a+c,0),y.reduce((a,c)=>a*c,1)]){
-  const r=f(n)
- for(let i=Math.max(...r);;i++)if(r.every(e=>i%e===0))return i
+function max(x){
+  if(!Array.isArray(x))x=[...x]
+  x=[...new Set(x)]
+  if(typeof x[0]==='number')return x.sort((a,b)=>b-a)[0]
+  if(typeof x[0]==='string')return x.sort((a,b)=>b.localeCompare(a))[0]
+  return x.sort((a,b)=>Date.parse(b)-Date.parse(a))[0]
+}
+function min(x){
+  if(!Array.isArray(x))x=[...x]
+  x=[...new Set(x)]
+  if(typeof x[0]==='number')return x.sort((a,b)=>a-b)[0]
+  if(typeof x[0]==='string')return x.sort((a,b)=>a.localeCompare(b))[0]
+  return x.sort((a,b)=>Date.parse(a)-Date.parse(b))[0]
 }
 
-console.log(parameter(123))
+console.log(max([new Date(2017,1,1), new Date(2015,2,2), new Date(2013,3,3)]))
