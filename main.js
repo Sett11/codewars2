5d002a28b68415a15d8c29a1ae1a90526b272ef9
Jhonny <gonzodzen@mail.ru>
2023-03-27 18:56:36 +0300

    Creating a Bitset, Part 1

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function dropWhile(b,f,a=b.slice()){
-  for(let i=0;i<a.length;i++){if(f(a[i])){a.splice(i,1);i--}else return a}
-  return []
+function byteToSet(n){
+	n=(n).toString(2)
+  n='0'.repeat(8-n.length)+n
+  return new Set([...n].map(Number).map((e,i)=>e?i:'&').filter(e=>e!=='&'))
 }
 
-console.log(dropWhile([2,4,6,8,1,2,5,4,3,2],e=>e%2===0))
+console.log(byteToSet(3))
+console.log(byteToSet(255))
