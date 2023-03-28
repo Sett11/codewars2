2e8581b1fe986c0d77c8ae671f72c9a0eccb6189
Jhonny <gonzodzen@mail.ru>
2023-03-29 00:49:56 +0300

    Dumb News: The Truth About CW Leaderboard !

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-function hallOfFame(a){
-  const f=x=>x.filter(e=>typeof e==='string').length
- a.sort((a,b)=>b[1]-a[1])
- for(let i=0;i<a.length-1;i++){
-  if(a[i][1]===a[i+1][1]){
-    a[i].push(a[i+1][0]);a.splice(i+1,1);i--
-  }
- }
- a=a.map(e=>e.filter(u=>typeof u==='number').concat(e.filter(u=>typeof u==='string').sort()))
- a.length=3
- if(f(a[0])===1&&f(a[1])>1)a[2]=[]
- if(f(a[0])>1){
-  a[2]=a[1];a[1]=[]
- }
- if(f(a[0])>2){
-  a[1]=[];a[2]=[]
- }
- while(a.length<3)a.push([])
- return a
+function checkUser(m,a='olzeasbtBg'.split``,n=[...m].map((e,i)=>+e===+e?a[+e]:e).join``.toLowerCase()){
+  const r=100*parseInt([...'mxyzptlk'].map(e=>n.includes(e)?1:0).join``,2)/255
+  return r<1?"This one's too clean, should be a bot.":r<50?"This one seems ok, may be there's still some humans around here...":r<90?"This one's probably an alien from 5th dim, be careful!":"Yeeerk!!! Mr Mxyztplk himself the imp of imps! Only Superman can beat him!"
 }
 
-console.log(hallOfFame( [["Aya",500], ["Ben",400], ["Clo",300], ["Dan",200], ["Eva",100]]))
-console.log(hallOfFame( [["Aya",500], ["Ben",400], ["Clo",300], ["Dan",300], ["Eva",100]]))
-console.log(hallOfFame( [["Aya",500], ["Ben",400], ["Clo",400], ["Dan",300], ["Eva",100]]))
-console.log(hallOfFame( [["Aya",500], ["Ben",500], ["Clo",400], ["Dan",300], ["Eva",100]]))
-console.log(hallOfFame( [["Aya",500], ["Ben",500], ["Clo",500], ["Dan",300], ["Eva",100]]))
+console.log(checkUser('Kltpzyxm'))
