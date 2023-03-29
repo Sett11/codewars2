839f865462b2623876d1a6fc0c6bd581b5559d8e
Jhonny <gonzodzen@mail.ru>
2023-03-29 23:28:46 +0300

    Words to Hex

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-function timeMath(t1,o,t2,a=[0,0,0]) {
-  t1=t1.split`:`,t2=t2.split`:`
-  a[2]=eval(t1[2]+o+t2[2])
-  if(a[2]===60){a[2]=0,a[1]+=1}
-  if(a[2]>60){a[2]=a[2]-60,a[1]+=1}
-  if(a[2]<0){a[2]=60+a[2],a[1]-=1}
-  a[1]+=eval(t1[1]+o+t2[1])
-  if(a[1]===60){a[1]=0,a[0]+=1}
-  if(a[1]>60){a[1]=a[1]-60,a[0]+=1}
-  if(a[1]<0){a[1]=60+a[1],a[0]-=1}
-  a[0]+=eval(t1[0]+o+t2[0])
-  if(a[0]>=24)a[0]=a[0]-24
-  if(a[0]<0)a[0]=24+a[0]
-  return a.map(e=>'0'.repeat(2-(e+'').length)+e+'').join`:`
+function wordsToHex(w,f=x=>x.map(e=>e.charCodeAt().toString(16)).join``){
+  return w.split` `.map(e=>{
+    if(e.length<3){
+      e=[...e]
+      e='#'+f(e)
+      return e+'0'.repeat(7-e.length)
+    }
+    return '#'+f([...e.slice(0,3)])
+  })
 }
 
-console.log(timeMath('01:24:31', '+', '22:35:28'))
+console.log(wordsToHex('Hello, my name is Gary and I like cheese.'))
