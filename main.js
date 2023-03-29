7815ba70121547d49500ffc5ea060e9e9f1be683
Jhonny <gonzodzen@mail.ru>
2023-03-30 00:31:20 +0300

    Two levels arrays

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const a=["1", "2", "3", "4", "5", "6", "7"],b=["a", "b", "c"]
+flattenTwoLevels=(a,f=x=>x.every(e=>!Array.isArray(e))?x:f(x.flat()))=>a.map(e=>Array.isArray(e)?f(e):e)
 
-function exchangeWith(a,b,l=b.length,c=0){
-   while(b.length)a.push(b.pop())
-   while(c<a.length-l)b.unshift(a.shift())
-}
 
-console.log(exchangeWith(a,b))
+console.log(flattenTwoLevels([1, [2, 3], [4, 5, [6, 7, 8], 9, 10, [11, [12, [13, 14], 15], 16], 17], 18]))
