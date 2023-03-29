9f4b2f0bf6721229ff3ae35332d958ac30aa0fc6
Jhonny <gonzodzen@mail.ru>
2023-03-30 00:50:47 +0300

    Largest Difference in Increasing Indexes

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-flattenTwoLevels=(a,f=x=>x.every(e=>!Array.isArray(e))?x:f(x.flat()))=>a.map(e=>Array.isArray(e)?f(e):e)
+largestDifference=(a,c=0)=>{
+  for(let i=0;i<a.length;i++){
+    for(let j=i+1;j<a.length;j++){
+      if(a[i]<=a[j])c=Math.max(c,j-i)
+    }
+  }
+  return c
+}
 
-
-console.log(flattenTwoLevels([1, [2, 3], [4, 5, [6, 7, 8], 9, 10, [11, [12, [13, 14], 15], 16], 17], 18]))
+console.log(largestDifference([9,4,1,10,3,4,0,-1,-2]))
