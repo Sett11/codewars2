0c099d902a3754b4680101ea004768c41fe02a56
Jhonny <gonzodzen@mail.ru>
2023-03-29 22:00:46 +0300

    Make A Window

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-middleMe=(n,x,y)=>n%2!==0?x:y.repeat(n/2)+x+y.repeat(n/2)
+function makeAWindow(n,s='|'+'.'.repeat(n)+'|'+'.'.repeat(n)+'|',c='-'.repeat(s.length),z=(s.slice(0,Math.floor(s.length/2))+'+'+s.slice(Math.ceil(s.length/2))).replace(/\./g,'-'),a=[],v=n){
+    while(v){
+      a.push(s+'\n');v--}a.unshift(c+'\n');a.push(z+'\n')
+      return a.concat(a.slice().reverse().slice(1)).map((e,i,v)=>i===v.length-1?e.replace(/[^\-]/,''):e).join``
+}
 
-
-console.log(middleMe(10,'*','#'))
+console.log(makeAWindow(10))
