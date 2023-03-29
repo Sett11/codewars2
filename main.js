8a026a360d785188ac46d00bdb4d4aedbcc450a9
Jhonny <gonzodzen@mail.ru>
2023-03-30 01:38:03 +0300

    Most consecutive 0's in a row [Code-golf]

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-function topScores(a,n) {
-  for(let i=0;i<a.length;i++){
-    for(let j=i+1;j<a.length;j++){
-      if(a[i][0]===a[j][0]){
-        a[i][1]=Math.max(a[i][1],a[j][1]);a[j]='&'
-      }
-    }
-  }
-  return n<1?[]:a.filter(e=>e!=='&').sort((a,b)=>b[1]-a[1]||a[0].localeCompare(b[0])).slice(0,n)
-}
+f=n=>Math.max(...(n+'').match(/0+/g).map(e=>e.length))
 
-console.log(topScores([
-  ["Bob", 100],
-  ["Jane", 120],
-  ["Alice", 10],
-  ["Bob", 110],
-  ["Bob", 10]
-],3))
+console.log(f(10002030000))
