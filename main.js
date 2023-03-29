ce825127d5a1356c59b79f41f356c6dc00bfee44
Jhonny <gonzodzen@mail.ru>
2023-03-30 01:31:44 +0300

    N High Scores

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-largestDifference=(a,c=0)=>{
+function topScores(a,n) {
   for(let i=0;i<a.length;i++){
     for(let j=i+1;j<a.length;j++){
-      if(a[i]<=a[j])c=Math.max(c,j-i)
+      if(a[i][0]===a[j][0]){
+        a[i][1]=Math.max(a[i][1],a[j][1]);a[j]='&'
+      }
     }
   }
-  return c
+  return n<1?[]:a.filter(e=>e!=='&').sort((a,b)=>b[1]-a[1]||a[0].localeCompare(b[0])).slice(0,n)
 }
 
-console.log(largestDifference([9,4,1,10,3,4,0,-1,-2]))
+console.log(topScores([
+  ["Bob", 100],
+  ["Jane", 120],
+  ["Alice", 10],
+  ["Bob", 110],
+  ["Bob", 10]
+],3))
