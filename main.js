194ff48484e3bc34be6dd79ee6f1f63b369e664b
Jhonny <gonzodzen@mail.ru>
2023-03-30 00:12:17 +0300

    Array Exchange

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function wordsToHex(w,f=x=>x.map(e=>e.charCodeAt().toString(16)).join``){
-  return w.split` `.map(e=>{
-    if(e.length<3){
-      e=[...e]
-      e='#'+f(e)
-      return e+'0'.repeat(7-e.length)
-    }
-    return '#'+f([...e.slice(0,3)])
-  })
+const a=["1", "2", "3", "4", "5", "6", "7"],b=["a", "b", "c"]
+
+function exchangeWith(a,b,l=b.length,c=0){
+   while(b.length)a.push(b.pop())
+   while(c<a.length-l)b.unshift(a.shift())
 }
 
-console.log(wordsToHex('Hello, my name is Gary and I like cheese.'))
+console.log(exchangeWith(a,b))
