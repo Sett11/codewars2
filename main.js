3a49b0cdf4c197192cbf3c202b949c22aff8fbc7
Jhonny <gonzodzen@mail.ru>
2023-03-29 23:10:09 +0300

    Time Math

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function every(a,s=1,g=0,r=[]){
-  for(let i=g;i<a.length;i+=s){
-    r.push(a[i])
-  }
-  return r
+function timeMath(t1,o,t2,a=[0,0,0]) {
+  t1=t1.split`:`,t2=t2.split`:`
+  a[2]=eval(t1[2]+o+t2[2])
+  if(a[2]===60){a[2]=0,a[1]+=1}
+  if(a[2]>60){a[2]=a[2]-60,a[1]+=1}
+  if(a[2]<0){a[2]=60+a[2],a[1]-=1}
+  a[1]+=eval(t1[1]+o+t2[1])
+  if(a[1]===60){a[1]=0,a[0]+=1}
+  if(a[1]>60){a[1]=a[1]-60,a[0]+=1}
+  if(a[1]<0){a[1]=60+a[1],a[0]-=1}
+  a[0]+=eval(t1[0]+o+t2[0])
+  if(a[0]>=24)a[0]=a[0]-24
+  if(a[0]<0)a[0]=24+a[0]
+  return a.map(e=>'0'.repeat(2-(e+'').length)+e+'').join`:`
 }
 
-console.log(every([0,1,2,3,4],2,2))
+console.log(timeMath('01:24:31', '+', '22:35:28'))
