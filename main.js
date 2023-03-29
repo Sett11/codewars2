1c964c3c51adf829ad4bc88759dd2bdb00e8b729
Jhonny <gonzodzen@mail.ru>
2023-03-29 22:23:02 +0300

    Fill in the gaps in my timesheet.

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function makeAWindow(n,s='|'+'.'.repeat(n)+'|'+'.'.repeat(n)+'|',c='-'.repeat(s.length),z=(s.slice(0,Math.floor(s.length/2))+'+'+s.slice(Math.ceil(s.length/2))).replace(/\./g,'-'),a=[],v=n){
-    while(v){
-      a.push(s+'\n');v--}a.unshift(c+'\n');a.push(z+'\n')
-      return a.concat(a.slice().reverse().slice(1)).map((e,i,v)=>i===v.length-1?e.replace(/[^\-]/,''):e).join``
+function fill_gaps(a) {
+	for(let i=0;i<a.length;i++){
+    if(a[i]!==null){
+      for(let j=i+1;j<a.length;j++){
+        if(a[j]!==null&&a[j]===a[i]){
+          a=a.slice(0,i).concat(a.slice(i,j).map(e=>e===null?a[i]:e)).concat(a.slice(j))
+          i=j-1;break
+        }
+        if(a[j]!==null&&a[j]!==a[i]){
+          i=j-1;break
+        }
+      }
+    }
+  }
+  return a
 }
 
-console.log(makeAWindow(10))
+console.log(fill_gaps([1,null,2,2,null,2]))
+console.log(fill_gaps(['codewars', null, null, 'codewars', 'real work', null, null, 'real work']))
+console.log(fill_gaps([1,null,1,null,1]))
