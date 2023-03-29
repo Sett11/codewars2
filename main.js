31cb81f01b4b5355d2d63e8f0d4f64c93a64f305
Jhonny <gonzodzen@mail.ru>
2023-03-29 22:29:42 +0300

    Every nth array element. (Basic)

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-function fill_gaps(a) {
-	for(let i=0;i<a.length;i++){
-    if(a[i]!==null){
-      for(let j=i+1;j<a.length;j++){
-        if(a[j]!==null&&a[j]===a[i]){
-          a=a.slice(0,i).concat(a.slice(i,j).map(e=>e===null?a[i]:e)).concat(a.slice(j))
-          i=j-1;break
-        }
-        if(a[j]!==null&&a[j]!==a[i]){
-          i=j-1;break
-        }
-      }
-    }
+function every(a,s=1,g=0,r=[]){
+  for(let i=g;i<a.length;i+=s){
+    r.push(a[i])
   }
-  return a
+  return r
 }
 
-console.log(fill_gaps([1,null,2,2,null,2]))
-console.log(fill_gaps(['codewars', null, null, 'codewars', 'real work', null, null, 'real work']))
-console.log(fill_gaps([1,null,1,null,1]))
+console.log(every([0,1,2,3,4],2,2))
