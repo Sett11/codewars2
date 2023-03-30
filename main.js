2e641c884bc126d42ebcdd3222d8c2b83d07f834
Jhonny <gonzodzen@mail.ru>
2023-03-30 20:33:34 +0300

    From..To..Series #7: from sentence to camelCase. Can you convert it?

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const toUnderScore=s=>{
-    let c=s.replace(/[A-Z]/g,'_$&').replace(/\d+/g,'_$&').replace(/__/g,'_')
-    if(s[0]!=='_'&&c[0]==='_')c=c.slice(1)
-    return c
+function toCamelCase(s,n){
+   s=s.split` `.map(e=>n===1?e.charAt(0).toUpperCase()+e.slice(1).toLowerCase():n===2?e.slice(0,-1).toLowerCase()+e.charAt(e.length-1).toUpperCase():e.charAt(0).toUpperCase()+e.slice(1,-1).toLowerCase()+e.charAt(e.length-1).toUpperCase()).join``
+   return s.charAt(0).toLowerCase()+s.slice(1,-1)+s.charAt(s.length-1).toLowerCase()
 }
-
-  console.log(toUnderScore('_IfATestStartAndEndsWithUnderscore_ItShouldBeTheSame_'))
+  
+  console.log(toCamelCase("hello world",2))
