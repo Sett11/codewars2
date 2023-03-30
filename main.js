a8c7eac14a0407700f08138b169d4ec63f975770
Jhonny <gonzodzen@mail.ru>
2023-03-30 23:27:06 +0300

    Capitalize first letter of a string

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-letterCheck=a=>[...a[1].toLowerCase()].every(e=>[...a[0].toLowerCase()].includes(e))
+String.prototype.capitalize=function(){
+    if(this[0].charCodeAt()<96)return this.toString()
+    return String.fromCharCode(this[0].charCodeAt()-32)+this.slice(1)
+}
+  
+console.log("Hello world".capitalize())  
