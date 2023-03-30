012112a699bc176d083d8b5906d7b5f15f93ee5c
Jhonny <gonzodzen@mail.ru>
2023-03-31 01:16:57 +0300

    Shuffle an Array

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-function countRepeats(s,c=0){
-    s=s.toLowerCase()
-      for(let i=0;i<s.length-1;i++){
-          if(s[i]===s[i+1]){
-              for(let j=i;j<s.length;j++){
-                  if(s[j]!==s[i]){
-                      c+=j-i-1;i=j-1;break
-                  }
-                  if(j===s.length-1&&s[j]===s[i]){
-                    c+=j-i>1?j-i-1:j-i;i=s.length-1
-                  }
-              }
-          }
-      }
-      return c===1176?1177:c
-  }
+shuffle=a=>a.map(e=>[e,Math.random()*a.length]).sort((a,b)=>a[1]-b[1]).map(e=>e[0])
 
-console.log(countRepeats("BA><?6?8559E>22BB695E7;;:6E?:>?C72D7C3<4:@B8>AB=8EEAD9D82;6>>"))
+
+console.log(shuffle([1,2,3,4]))
