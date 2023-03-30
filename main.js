7dd5904a79b2d6506fb0a10f5391f830b8059693
Jhonny <gonzodzen@mail.ru>
2023-03-30 23:20:12 +0300

    noobCode 03: CHECK THESE LETTERS... see if letters in "String 2" are present in "String 1"

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-function triangle(s,a=[[...s]],c=0,f=x=>parseInt(x,36)-9,r=[]){
-    if(s.length===1)return s
-    a[0]=a[0].map(e=>f(e))
-    while(1){
-        for(let i=0;i<a[c].length-1;i++){
-            r.push((a[c][i]+a[c][i+1])%26)
-        }
-        if(r.length===1){a.push(r);break}
-        else{a.push(r);r=[];c++}
-    }
-    res=a.map(e=>e.map(u=>String.fromCharCode(u+96)))[a.length-1][0]
-    return res==='`'?'z':res
-}
-
-console.log(triangle('codewars'))
-console.log(triangle('jlwrwczyjohse'))
+letterCheck=a=>[...a[1].toLowerCase()].every(e=>[...a[0].toLowerCase()].includes(e))
