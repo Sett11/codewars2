4e46488462474b0b838cc4d35dcb60c098164221
Jhonny <gonzodzen@mail.ru>
2023-03-30 19:25:41 +0300

    ThetaFormulation

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-squareOrSquareRoot=a=>a.map(e=>Number.isInteger(Math.sqrt(e))?Math.sqrt(e):e**2)
+function thetaFormula(a,b,c,r=[a,b,c],l=r.filter(e=>e)){
+  if(!l.length||l.length===2||r.every(e=>e==='?')||r.filter(e=>e==='?').length>1||r.filter(e=>e!=='?'&&typeof e!=='number').length)return null
+  if(r[0]==='?')return +(r[1]/r[2]).toFixed(3)
+  if(r[1]==='?')return +(r[0]*r[2]).toFixed(3)
+  if(r[2]==='?')return +(r[1]/r[0]).toFixed(3)
+  return +(r[0]).toFixed(3)===+(r[1]/r[2]).toFixed(3)
+}
 
-console.log(squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ]))
+console.log(thetaFormula(1.5707963267948966, 3.141592653589793, 2))
