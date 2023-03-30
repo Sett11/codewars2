7d35029628c7e98e43cae8c25f88b7bbc5fee42c
Jhonny <gonzodzen@mail.ru>
2023-03-30 23:54:42 +0300

    Emotional Sort ( ︶︿︶)

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-function triangle(s,a=[[...s]],f=(x,y)=>x===y?x:['R','G','B'].filter(e=>e!==x&&e!==y)[0],c=0){
-    if(s.length===1||new Set(s).size===1)return s[0]
-   while(1){
-    let r=[]
-    for(let i=0;i<a[c].length-1;i++)r.push(f(a[c][i],a[c][i+1]))
-    if(r.length===1){a.push(r);break}
-    else{a.push(r);r=[];c++}
-   }
-   return a[a.length-1][0]
+function sortEmotions(a,v,c=[':D',':)',':|',':(','T_T']){
+    return !a.length?[]:a.map(e=>[e,c.indexOf(e)]).sort((a,b)=>(v?a:b)[1]-(v?b:a)[1]).map(e=>e[0])
 }
 
-console.log(triangle('B'))
+console.log(sortEmotions([ ':)', 'T_T', ':)', ':D', ':D' ]))
