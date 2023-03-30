cd1537f3a4cb71f1eb2d42463bff8a51448d5859
Jhonny <gonzodzen@mail.ru>
2023-03-31 00:20:43 +0300

    From..To..Series #3: from char a to char b. Find a substring that contains most char c

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function charToAscii(s){
-    return !s.match(/[A-z]/)?null:[...s.replace(/[\W_]/g,'')].reduce((a,c)=>{a[c]=c.charCodeAt();return a},{})
+function findSub(s,a,b,c,r=[]){
+    for(let i=0;i<s.length;i++){
+        if(s[i]===a){
+            for(let j=i+1;j<s.length;j++){
+                if(s[j]===s[i]){
+                    i=j-1;break
+                }
+                if(s[j]===b){
+                    r.push(s.slice(i,j+1));i=j;break
+                }
+            }
+        }
+    }
+    r=r.map((e,i)=>[e,i,[...e].filter(u=>u===c).length]).sort((a,b)=>b[2]-a[2]||a[1]-b[1]).filter(e=>e[0].includes(c))
+    return r.length?r[0][0]:''
 }
 
-  console.log(charToAscii("ABaa _^"))
+  console.log(findSub("((( )))((  ))",'(',')',' '))
