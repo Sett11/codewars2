126adbcd082d3272f0d549acbec9cf2ddcd5cfde
Jhonny <gonzodzen@mail.ru>
2023-03-30 23:11:09 +0300

    Letter triangles

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-toBcd=(n,f=x=>(x).toString(2),s=n<0?'-':'')=>s+[...Math.abs(n)+''].map(e=>'0'.repeat(4-f(+e).length)+f(+e)).join` `
+function triangle(s,a=[[...s]],c=0,f=x=>parseInt(x,36)-9,r=[]){
+    if(s.length===1)return s
+    a[0]=a[0].map(e=>f(e))
+    while(1){
+        for(let i=0;i<a[c].length-1;i++){
+            r.push((a[c][i]+a[c][i+1])%26)
+        }
+        if(r.length===1){a.push(r);break}
+        else{a.push(r);r=[];c++}
+    }
+    res=a.map(e=>e.map(u=>String.fromCharCode(u+96)))[a.length-1][0]
+    return res==='`'?'z':res
+}
 
-console.log(toBcd(-10))
+console.log(triangle('codewars'))
+console.log(triangle('jlwrwczyjohse'))
