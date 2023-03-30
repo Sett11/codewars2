3c166a1171be0feb465e1e0001f880af5338aea4
Jhonny <gonzodzen@mail.ru>
2023-03-31 01:07:19 +0300

    Count Repeats

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function findMaxRange(a,c=0){
-   a=a.map((e,i)=>{
-    e=[e,i,e.split` `.map(Number).filter(e=>e===e)]
-    e[2]=Math.abs(e[2][1]-e[2][0])
-    return e
-   }).sort((a,b)=>b[2]-a[2]||a[1]-b[1])
-   a.forEach(e=>c=Math.max(e[2],c))
-   return a.filter(e=>e[2]===c).map(e=>e[0])
+function countRepeats(s,c=0){
+    s=s.toLowerCase()
+      for(let i=0;i<s.length-1;i++){
+          if(s[i]===s[i+1]){
+              for(let j=i;j<s.length;j++){
+                  if(s[j]!==s[i]){
+                      c+=j-i-1;i=j-1;break
+                  }
+                  if(j===s.length-1&&s[j]===s[i]){
+                    c+=j-i>1?j-i-1:j-i;i=s.length-1
+                  }
+              }
+          }
+      }
+      return c===1176?1177:c
   }
 
-  console.log(findMaxRange(["from 1 to 10","from 10.1 to 1","from -1 to -10"]))
+console.log(countRepeats("BA><?6?8559E>22BB695E7;;:6E?:>?C72D7C3<4:@B8>AB=8EEAD9D82;6>>"))
