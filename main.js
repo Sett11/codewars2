0bc774110a6e460de1868af2e7b12ac319e1726e
Jhonny <gonzodzen@mail.ru>
2023-03-30 23:38:56 +0300

    Coloured Triangles

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-String.prototype.capitalize=function(){
-    if(this[0].charCodeAt()<96)return this.toString()
-    return String.fromCharCode(this[0].charCodeAt()-32)+this.slice(1)
+function triangle(s,a=[[...s]],f=(x,y)=>x===y?x:['R','G','B'].filter(e=>e!==x&&e!==y)[0],c=0){
+    if(s.length===1||new Set(s).size===1)return s[0]
+   while(1){
+    let r=[]
+    for(let i=0;i<a[c].length-1;i++)r.push(f(a[c][i],a[c][i+1]))
+    if(r.length===1){a.push(r);break}
+    else{a.push(r);r=[];c++}
+   }
+   return a[a.length-1][0]
 }
-  
-console.log("Hello world".capitalize())  
+
+console.log(triangle('B'))
