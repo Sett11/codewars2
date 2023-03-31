aad5857bdcc213383565f0093a62fc2b13ba63cc
Jhonny <gonzodzen@mail.ru>
2023-03-31 10:54:37 +0300

    Adding values of arrays in a shifted way

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-function faroCount(n,a=Array(n).fill(1).map((e,i)=>e+i),c=1){
-  let f=(x,y=[],l=x.length/2)=>{for(let i=0;i<l;i++){y.push(x[i],x[i+l])}return y},r=f(a)
-  while(1){
-    if(a.join``===r.join``)break
-    r=f(r),c++
+function addingShifted(a,s,r=Array(a[0].length+(a.length-1)*s).fill(0)){
+  for(let i=0;i<a.length;i++){
+    for(let j=0;j<a[0].length;j++)r[j+i*s]+=a[i][j]
   }
-  return c
+  return r
 }
 
-console.log(faroCount(30))
+console.log(addingShifted([[1, 2, 3, 4, 5, 6], [7, 7, 7, -7, 7, 7],[1, 1, 1, 1, 1, 1]],3))
