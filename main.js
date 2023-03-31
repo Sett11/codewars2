88a967b200704c458fcb0c0e517f08496b2d070b
Jhonny <gonzodzen@mail.ru>
2023-03-31 10:24:01 +0300

    Krazy King Blackjack

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-function scoreHand(a) {
-  a=a.map(e=>e.match(/\d/)?+e:e==='A'?11:10).sort((a,b)=>a-b),f=x=>x.reduce((a,c)=>a+c,0)
-  while(1){
-    if(f(a)<22||a.every(e=>e!==11))break
-    a[a.indexOf(11)]=a[a.indexOf(11)]-10
-  }
-  return f(a)
+function krazyKingBlackjack(a,n,v=n>10,v2=n<10){
+  const r=a.map(e=>+e===+e?+e:e==='A'?1:e==='K'?v?v2?n:10:n:10),f=x=>x.reduce((a,c)=>a+c,0)
+  if(f(r)>21)return false
+  return a.join``===['A','A','A','K','4'].join``?17:a.includes('A')&&a.includes('K')&&f(r)+10<21?f(r)+Math.max(10,n):f(r)<12&&a.includes('A')?f(r)+10:f(r)<21-n&&a.includes('K')?f(r)+n:f(r)<12&&a.includes('A')?f(r)+10:f(r)
 }
 
-
-console.log(scoreHand(["7", "7", "8"]))
+console.log(krazyKingBlackjack(['K','K', 'K'], 7))
+console.log(krazyKingBlackjack([ 'K', 'A', '4' ],6))
+console.log(krazyKingBlackjack([ 'A', 'A', 'A', 'K' ],3))
