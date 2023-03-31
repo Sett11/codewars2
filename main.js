7d5e7f1491020ea9fc8ac43fc5cb345261546308
Jhonny <gonzodzen@mail.ru>
2023-03-31 16:09:07 +0300

    Checkered Board

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function calculate(n){
-  console.log(n)
-  if(!n.match(/[\+\-\/\*]/))return n
-  if(n==="(@~@@+@~@@@)*@~~~~")return "@@~@@@@@~~~~"
-  if(n==="@~~~~@+@~@@")return "@~~~@~@@@"
-  if(n==='@@@~@@@@@@@@@~@@*@@@@~~~')return '@~@@@@@~@@@@@@~@@@@@@@@~~~'
-  if(n==='@~@@@@@@@@@~@@@@*@@@@~@@@@@@~@@@@@@@@@*(@@@@~~~-@~@@@@@@~@@@@@@@)')return '@@~@~@~@@@@@@@@@~@@@@@@@@@~@@@@@@@~@@@~@@@@@@@@'
-  if(n==='@@~@@@@@~~*@@@@~@@~@@@@@@@@')return '@~~@@@@@@@~~~~'
-  n=eval(n.replace(/@+/g,_=>_.length).replace(/~~/g,'0').replace(/~/g,''))
-  const res=[...(n+'')].join``.replace(/0/g,'~~').replace(/\d/g,_=>'@'.repeat(_)+'~')
-  return res.slice(0,res.length-1).replace(/~~~/g,'~~').replace(/~~~~/g,'~~~')
- }
+function checkeredBoard(n,a='■',b='□',r=[],s=0){
+  if(n%2===0){b='■',a='□'}
+  let c=Array(n).fill(1).map((e,i)=>i%2===0?a:b).join` `
+  while(s<n){
+    if(s%2===0){r.push(c);s++}
+    r.push(c.split` `.map(e=>e===a?b:a).join` `),s++
+  }
+  r.length=n
+  return r.join`\n`
+}
+
+console.log(checkeredBoard(11))
