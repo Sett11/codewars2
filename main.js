6384df63d67112d3822af4569566c0c3904f410b
Jhonny <gonzodzen@mail.ru>
2023-03-31 16:52:22 +0300

    A for Apple

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-function checkeredBoard(n,a='■',b='□',r=[],s=0){
-  if(n%2===0){b='■',a='□'}
-  let c=Array(n).fill(1).map((e,i)=>i%2===0?a:b).join` `
-  while(s<n){
-    if(s%2===0){r.push(c);s++}
-    r.push(c.split` `.map(e=>e===a?b:a).join` `),s++
+function a(n){
+  if(n<4)return ''
+  if(n%2!==0)n-=1
+  const r=Array(n).fill('A').map((e,i)=>i!==0?e+' '.repeat(i*2-1)+e:e).map((e,i)=>' '.repeat(n-i-1)+e+' '.repeat(n-i-1))
+  for(let i=n/2-1;i<(r[n/2].length)-n/2-1;i++){
+    r[n/2]=[...r[n/2]]
+    r[n/2][i]='A'
   }
-  r.length=n
+  r[n/2].length=n,r[n/2]=r[n/2].join` `
+  let l=n/2-1
+  while(l){r[n/2]=r[n/2].replace(/ /,''),l--}
+  r[n/2]+=' '.repeat(n/2-1)
   return r.join`\n`
 }
 
-console.log(checkeredBoard(11))
+console.log(a(7))
