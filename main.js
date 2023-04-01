bd45b48804f59f156fb4235227db46bdd6ffcdb9
Jhonny <gonzodzen@mail.ru>
2023-04-01 14:37:41 +0300

    Sierpinski's Gasket

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-function GrεεκL33t(s){
- const o={'A':'α','B':'β','D':'δ','E':'ε','I':'ι','K':'κ','N':'η','O':'θ','P':'ρ','R':'π','T':'τ','U':'μ','V':'υ','W':'ω','X':'χ','Y':'γ'}
- return [...s.toUpperCase()].map(e=>o[e]?o[e]:e.toLowerCase()).join``
+function sierpinski(n,a=[]) {
+  if(!n)return 'L'
+  let c=`L\nL L`.split`\n`,l=c.length,z=0
+ while(n-1){
+  for(let i=0;i<l;i++){
+    if(!z){
+      c.push(c[i]+' '.repeat(l+1)+c[i])
+    }
+    else{
+      if(!i)c.push(c[i]+' '.repeat(c[c.length-1].length)+c[i])
+      else c.push(c[i]+' '.repeat(c[c.length-i-1].length-c[i].length+1)+c[i])
+      
+    }
+  }
+  n--;z++;l=c.length;c[c.length-1]=c[c.length-1].replace(/\s+/g,' ')
+ }
+  return c.join`\n`
 }
 
-console.log(GrεεκL33t('codewars'))
+console.log(sierpinski(4))
