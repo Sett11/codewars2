1b038fe1d1f0313969bba981c5e9b4d8dd25b88b
Jhonny <gonzodzen@mail.ru>
2023-04-02 17:20:25 +0300

    Nextel phone number validator

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-isValidMXPhoneNumber=s=>{
-  return s.replace(/[^\(\)]/g,'').length%2!==0||s.replace(/[\(\) \d]/g,'').length!==0||s.replace(/[\(\) ]/g,'').length!==8?NaN===NaN:/\(*56|55\)* *\d{2,2} *\d{2,2} *\d{2,2}/.test(s)
-}
+isValidNextelPhoneNumber=s=>!s.match(/\*/)||s.replace(/[\* ]/g,'').length!==9||s[0]==='*'||s[s.length-1]==='*'||s.replace(/[^\*]/g,'').length%2!==0||s.replace(/[\*\d ]/g,'').length!==0||s.match(/\*\*/)?1==2:1==1
 
 
 
 
 
-console.log(isValidMXPhoneNumber('56846552'))
+console.log(isValidNextelPhoneNumber('72 * 673639 * 2'))
