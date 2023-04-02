cebbe50dd8edd707f929a1927dda30b7befd9289
Jhonny <gonzodzen@mail.ru>
2023-04-02 20:24:07 +0300

    Survive The Seven Kingdoms

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-function phoneWords(s){
-  if(!s||[...new Set(s)].length===1&&s[0]==='1')return ''
-  if([...new Set(s)].length===1&&s[0]==='0')return ' '.repeat(s.length)
-  const a={2:['a','b','c'],3:['d','e','f'],4:['g','h','i'],5:['j','k','l'],6:['m','n','o'],7:['p','q','r','s'],8:['t','u','v'],9:['w','x','y','z'],0:[' ']}
-  s=s.replace(/1/g,' ').replace(/((\d)\2*)/g,e=>e[0]+e.length).match(/\d{2,2}/g).map(e=>[...e])
-  for(let i=0;i<s.length;i++){
-    if(a[s[i][0]].length<s[i][1]){
-      let t=s[i][1]
-      s[i][1]=a[s[i][0]].length
-      t-=s[i][1]
-      s=s.slice(0,i+1).concat([[s[i][0],t]]).concat(s.slice(i+1))
-    }
+const finalDestination=(c,d)=>{
+  const a=[
+    ["Deepwood Motte", "Shadow Tower", "Castle Black", "Eastwatch", "Bay of Seals"],
+    [ "The Stony Shore", "Torrhen's Square", "Winterfell", "The Dreadfort", "Karhold"],
+    [ "Flint's Finger", "Barrowtown", "Moat Cailin", "White Harbour", "Widow's Wat"],
+    [ "Pyke", "Seagard", "The Twins", "Sisterton", "The Fingers"],
+    [ "The Crag", "Riverrun", "Darry", "The Eyrie", "Gulltown"],
+    [ "Castamere", "Acorn Hall", "Harrenhal", "Maidenpool", "Dragonstone"],
+    [ "Lannisport", "Stoney Sept", "King's Landing", "Blackwater Bay", "Sharp Point"],
+    [ "High Garden", "Bitterbridge", "King's Wood", "Storm's End", "Evenfall"],
+    [ "Old Town", "Horn Hill", "Prince's Pass", "Planky Town", "Sunspear"]
+  ],r=d.split` `.map(e=>[e[0],+e[1]]),z=a.map((e,i)=>e.map((u,j)=>u===c?[i,j]:0).filter(e=>e)).filter(e=>e.length).flat(2)
+  for(let i=0;i<r.length;i++){
+    if(z[0]<0)return "The Wall blocks your way"
+    if(z[0]>a.length-1||z[1]<0||z[1]>a[0].length-1)return "You do not have a ship to cross this sea."
+    if(r[i][0]==='S')z[0]+=r[i][1]
+    if(r[i][0]==='N')z[0]-=r[i][1]
+    if(r[i][0]==='W')z[1]-=r[i][1]
+    if(r[i][0]==='E')z[1]+=r[i][1]
   }
-  return s.map(e=>a[e[0]][e[1]-1]).join``
+  return z[0]<0?"The Wall blocks your way":z[0]>a.length-1||z[1]<0||z[1]>a[0].length-1?"You do not have a ship to cross this sea.":a[z[0]][z[1]]
 }
-
-console.log(phoneWords("222555866363355855999559922208999333333333335566882"))
+console.log(finalDestination("King's Landing", "N7"))
