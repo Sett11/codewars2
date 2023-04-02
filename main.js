9c117045ef09ac2c7a2172e83a24b172c5779077
Jhonny <gonzodzen@mail.ru>
2023-04-02 20:49:23 +0300

    Lucky Sevens

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const finalDestination=(c,d)=>{
-  const a=[
-    ["Deepwood Motte", "Shadow Tower", "Castle Black", "Eastwatch", "Bay of Seals"],
-    [ "The Stony Shore", "Torrhen's Square", "Winterfell", "The Dreadfort", "Karhold"],
-    [ "Flint's Finger", "Barrowtown", "Moat Cailin", "White Harbour", "Widow's Wat"],
-    [ "Pyke", "Seagard", "The Twins", "Sisterton", "The Fingers"],
-    [ "The Crag", "Riverrun", "Darry", "The Eyrie", "Gulltown"],
-    [ "Castamere", "Acorn Hall", "Harrenhal", "Maidenpool", "Dragonstone"],
-    [ "Lannisport", "Stoney Sept", "King's Landing", "Blackwater Bay", "Sharp Point"],
-    [ "High Garden", "Bitterbridge", "King's Wood", "Storm's End", "Evenfall"],
-    [ "Old Town", "Horn Hill", "Prince's Pass", "Planky Town", "Sunspear"]
-  ],r=d.split` `.map(e=>[e[0],+e[1]]),z=a.map((e,i)=>e.map((u,j)=>u===c?[i,j]:0).filter(e=>e)).filter(e=>e.length).flat(2)
-  for(let i=0;i<r.length;i++){
-    if(z[0]<0)return "The Wall blocks your way"
-    if(z[0]>a.length-1||z[1]<0||z[1]>a[0].length-1)return "You do not have a ship to cross this sea."
-    if(r[i][0]==='S')z[0]+=r[i][1]
-    if(r[i][0]==='N')z[0]-=r[i][1]
-    if(r[i][0]==='W')z[1]-=r[i][1]
-    if(r[i][0]==='E')z[1]+=r[i][1]
+function luckySevens(a,r=[],c=Array(a[0].length).fill(0)){
+  a.push(c),a.unshift(c)
+  a=a.map(e=>{
+    e.push(0),e.unshift(0)
+    return e
+  })
+  for(let i=0;i<a.length;i++){
+    for(let j=0;j<a[i].length;j++){
+      if(a[i][j]===7)r.push([a[i][j-1],a[i][j+1],a[i-1][j],a[i+1][j]])
+    }
   }
-  return z[0]<0?"The Wall blocks your way":z[0]>a.length-1||z[1]<0||z[1]>a[0].length-1?"You do not have a ship to cross this sea.":a[z[0]][z[1]]
+  return r.map(e=>e.reduce((a,c)=>a+c,0)).filter(e=>Number.isInteger(Math.cbrt(e))).length
 }
-console.log(finalDestination("King's Landing", "N7"))
+
+console.log(luckySevens([
+  [ 186, 946, 992, 578, 286, 10, 295, 499, 369 ],
+  [ 408, 16, 347, 608, 827, 7, 7, 727, 858 ],
+  [ 5, 7, 4, 638, 409, 472, 817, 1, 531 ],
+  [ 792, 2, 179, 7, 133, 684, 0, 7, 0 ],
+  [ 764, 374, 962, 50, 46, 95, 474, 0, 460 ],
+  [ 165, 7, 133, 5, 7, 11, 825, 40, 626 ],
+  [ 240, 57, 954, 447, 2, 551, 1, 7, 17 ],
+  [ 7, 738, 7, 450, 37, 7, 120, 6, 7 ],
+  [ 7, 683, 902, 66, 402, 21, 870, 323, 233 ]
+]))
