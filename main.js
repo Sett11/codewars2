41180f4cd7ed48cf80663308a6e9bafad5eb9f45
Jhonny <gonzodzen@mail.ru>
2023-04-03 01:13:16 +0300

    Surrounding Primes for a value

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-function findMatchedByPattern(p){
-  return function(a){
-    if([...p].some(e=>![...a].includes(e)))return 1==2
-    a=[...a],v=[...p]
-    if([...new Set(p)].length===p.length)return [...new Set(a.filter(e=>v.includes(e)))].join``.match(p)?1==1:1==2
-    a=a.filter(e=>v.includes(e))
-    for(let i=0;i<a.length-1;i++){
-      if(v.indexOf(a[i])===v.lastIndexOf(a[i])&&a[i]===a[i+1]){
-        a.splice(i+1,1);i--
-      }
-    }
-    return a.join``.slice(0,p.length)===p
+function primeBefAft(n,c=n+1,x=n-1,a=[],b=[]) {
+  const f=x=>{if(x<2)return false;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return x>=2}
+  while(1){
+    if(f(c)){a.push(c);c++}
+    if(f(x)){b.push(x);x--}
+    c++,x--
+    if(a.length&&b.length)return [b[0],a[0]]
   }
 }
 
-const p=findMatchedByPattern('tit')
-
-console.log(p('multiplicity'))
+console.log(primeBefAft(97))
