adabd8defd7d175b597ae663280a60edad705adc
Jhonny <gonzodzen@mail.ru>
2023-04-03 01:20:32 +0300

    Not very secure

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function primeBefAft(n,c=n+1,x=n-1,a=[],b=[]) {
-  const f=x=>{if(x<2)return false;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return x>=2}
-  while(1){
-    if(f(c)){a.push(c);c++}
-    if(f(x)){b.push(x);x--}
-    c++,x--
-    if(a.length&&b.length)return [b[0],a[0]]
-  }
-}
+alphanumeric=s=>s&&!s.replace(/[A-Za-z0-9]/g,'')?1==1:1==2
 
-console.log(primeBefAft(97))
+console.log(alphanumeric('Mazinkaiser'))
