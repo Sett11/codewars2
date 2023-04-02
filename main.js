fb2bb93bb2c907892c9048b3fd9d1151bae28cf0
Jhonny <gonzodzen@mail.ru>
2023-04-03 00:52:04 +0300

    Get match by pattern

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
-function countDifferentMatrices(a){
-  const f=x=>{
-    let r=[]
-    for(let i=0;i<x[0].length;i++){
-      let iArr=[]
-      for(let j=0;j<x.length;j++)iArr.push(x[j][i])
-      r.push(iArr.reverse());iArr=[]
-    }
-    return r
-  }
-  a=a.map(e=>[[e.slice(0,2),e.slice(2)]])
-  for(let i=0;i<a.length;i++){
-    while(a[i].length<5){
-      a[i][0]=f(a[i][0])
-      a[i].push(a[i][0])
-    }
-  }
-  a=a.map(e=>e.slice(0,e.length-1).map(u=>u.map(z=>z.join``).join``))
-  for(let i=0;i<a.length;i++){
-    for(let j=i+1;j<a.length;j++){
-      if(a[j].some(e=>a[i].includes(e))){
-        a.splice(j,1);i--;break
+function findMatchedByPattern(p){
+  return function(a){
+    if([...p].some(e=>![...a].includes(e)))return 1==2
+    a=[...a],v=[...p]
+    if([...new Set(p)].length===p.length)return [...new Set(a.filter(e=>v.includes(e)))].join``.match(p)?1==1:1==2
+    a=a.filter(e=>v.includes(e))
+    for(let i=0;i<a.length-1;i++){
+      if(v.indexOf(a[i])===v.lastIndexOf(a[i])&&a[i]===a[i+1]){
+        a.splice(i+1,1);i--
       }
     }
+    return a.join``.slice(0,p.length)===p
   }
-  return a.length
 }
 
-console.log(countDifferentMatrices(
-  [[3, 1, 2, 3],
-          [3, 1, 2, 3],
-          [1, 3, 3, 2],
-          [3, 2, 1, 3]]))
-console.log(countDifferentMatrices(
-  [[1, 2, 2, 1],
-           [1, 1, 2, 2],
-           [2, 1, 1, 2],
-           [2, 1, 2, 1],
-           [1, 2, 1, 2]]))
+const p=findMatchedByPattern('tit')
+
+console.log(p('multiplicity'))
