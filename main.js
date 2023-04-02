4bc27f24ba2653c140903334f1b5acf9c0550db0
Jhonny <gonzodzen@mail.ru>
2023-04-02 15:53:03 +0300

    Creating a string for an array of objects from a set of words

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function lookAndSay(b,n,a=[b]){
-  while(a.length<=n)a.push(a[a.length-1].replace(/((\d)\2*)/g,e=>e.length+e[0]))
-  return a.slice(1)
+function wordsToObject(c,s=c.split` `,a=[]){
+  for(let i=0;i<s.length;i+=2)a.push([s[i],s[i+1]])
+  return '['+a.map(e=>`{name : '${e[0]}', id : '${e[1]}'}`).join`, `+']'
 }
 
-console.log(lookAndSay('1259',5))
+console.log(wordsToObject("red 1 yellow 2 black 3 white 4"))
