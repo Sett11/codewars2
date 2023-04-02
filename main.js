4e45ad9ecfbdebeb69236b8517f552e91cc69c27
Jhonny <gonzodzen@mail.ru>
2023-04-02 16:39:33 +0300

    Valid Phone Number

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-function formatNumber(n,t,a=[...n+'']){
-  for(let i=0;i<a.length;i++)t=t.replace(/\#/,a[i])
-  return t.match(/\#/)?"Invalid phone number":t
-}
+validPhoneNumber=s=>s.replace(/\d/g,'').length>4?NaN===NaN:/\(\d{3,3}\) \d{3,3}\-\d{4,4}/.test(s)
 
-console.log(formatNumber(8123706890, "+## ### ### ##-##"))
-console.log(formatNumber(790524790757777777, "+# ### ### ## ##"))
+
+console.log(validPhoneNumber("(123) 456-7890"))
