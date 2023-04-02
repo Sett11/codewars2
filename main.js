3d2335e75efe379f0aac2137b85d1ae33248bba3
Jhonny <gonzodzen@mail.ru>
2023-04-02 11:01:17 +0300

    Pete, the baker

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
-function podium(a,c=-1000,j=-1000,i=-1000){
-  if(Object.entries(a).every(e=>!e[1].length))return {}
-  const b=Object.entries(a).filter(e=>e[1].length).map(e=>[e[0],e[1].reduce((a,c)=>a+c,0)]).sort((a,b)=>b[1]-a[1])
-  b.forEach(e=>c=Math.max(c,e[1]))
-  b.filter(e=>e[1]!==c).forEach(e=>j=Math.max(j,e[1]))
-  b.filter(e=>e[1]!==c&&e[1]!==j).forEach(e=>i=Math.max(i,e[1]))
-  let r={
-    'gold':{
-      'score':c,
-      'players':b.filter(e=>e[1]===c).map(e=>e[0]).sort()
-    },
-    'silver':{
-      'score':j,
-      'players':b.filter(e=>e[1]===j).map(e=>e[0]).sort()
-    },
-    'bronze':{
-      'score':i,
-      'players':b.filter(e=>e[1]===i).map(e=>e[0]).sort()
-    }
-  }
-  if(r.gold.players&&r.gold.players.length>1){
-    r.bronze=r.silver;delete r.silver
-  }
-  if(r.silver&&r.silver.players.length>1){
-    delete r.bronze
-  }
-  if(r.silver&&r.bronze&&r.silver.score===r.bronze.score&&r.silver.players.join``===r.bronze.players.join``){
-    delete r.bronze
-  }
-  if(r.bronze&&!r.bronze.players.length)delete r.bronze
-  if(r.silver&&!r.silver.players.length)delete r.silver
-  if(r.gold.players.length>2){
-    r={'gold':r.gold}
-  }
-  return r
+function cakes(r,a,c=10000){
+  for(let i in r)c=Math.min(~~(a[i]/r[i]),c)
+  return c
 }
 
-console.log(podium({"Ayoub":[0,0,0,0,0,0],"Manel":[0,0,0],"David":[0,0,0,0,0,0,0],"Sandra":[0,0],"Keren":[0],"Nabil":[0,0,0,0,0,0,0,0],"Fatoumata":[0,0,0,0],"Aude":[0,0,0,0,0,0,0],"Souleymane":[0],"Inaya":[0,0,0,0,0,0,0,0],"Rafael":[0,0,0,0],"John":[0,0,0,0,0,0,0,0,0],"Valentine":[0,0],"Ian":[0,0,0,0,0,0,0],"Amandine":[0,0,0,0],"Paul":[0,0,0,0,0,0,0],"Marvin":[0,0,0],"Eliana":[0,0,0,0,0,0,0,0],"Selene":[0,0,0,0,0,0,0,0,0,0],"Charlie":[0,0,0,0],"Sue":[0,0,0,0,0,0,0,0,0,0]}))
+console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
+console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}))
