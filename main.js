ac4c97aa9f3726e4737bdfabb1b2ca49a95ad035
Jhonny <gonzodzen@mail.ru>
2023-04-02 12:27:33 +0300

    Look and say sequence generator

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-function getLines(n,a=['1']){
-  if(n<1||!n)return -1
+function lookAndSaySequence(b,n,a=[b],c=n){
   while(a.length<n)a.push(a[a.length-1].replace(/((\d)\2*)/g,e=>e.length+e[0]))
-  return a.join`,`
+  return a[c-1]
 }
 
-console.log(getLines(6))
+console.log(lookAndSaySequence('22',10))
