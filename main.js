c11863730b011b8f8504f6383f16d829877970de
Jhonny <gonzodzen@mail.ru>
2023-04-02 12:22:59 +0300

    The Look and Say sequence

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-function lookSay(n,a=[...n+''].map(Number),s=''){
-  for(let i=0;i<a.length;i++){
-    if(a[i]!==a[i+1])s+='1'+a[i]
-    else{
-      for(let j=i+1;j<=a.length;j++){
-      if(a[j]!==a[i]){
-        s+=(a.slice(i,j).join``.length+'')+a[i];i=j-1;break
-      }
-      }
-    }
-  }
-  return +s
+function getLines(n,a=['1']){
+  if(n<1||!n)return -1
+  while(a.length<n)a.push(a[a.length-1].replace(/((\d)\2*)/g,e=>e.length+e[0]))
+  return a.join`,`
 }
 
-console.log(lookSay(22322))
+console.log(getLines(6))
