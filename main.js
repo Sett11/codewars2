fbe72814600c06855d6f02fc5395297f67cfd70b
Jhonny <gonzodzen@mail.ru>
2023-04-02 17:05:43 +0300

    Mexican capital 's phone number validator

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-validPhoneNumber=s=>s.replace(/\d/g,'').length>4?NaN===NaN:/\(\d{3,3}\) \d{3,3}\-\d{4,4}/.test(s)
+isValidMXPhoneNumber=s=>{
+  return s.replace(/[^\(\)]/g,'').length%2!==0||s.replace(/[\(\) \d]/g,'').length!==0||s.replace(/[\(\) ]/g,'').length!==8?NaN===NaN:/\(*56|55\)* *\d{2,2} *\d{2,2} *\d{2,2}/.test(s)
+}
 
 
-console.log(validPhoneNumber("(123) 456-7890"))
+
+
+
+console.log(isValidMXPhoneNumber('56846552'))
