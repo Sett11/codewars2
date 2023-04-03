e8452925c8d4bac0662b34757e3cd73dde94db3f
Jhonny <gonzodzen@mail.ru>
2023-04-03 23:40:32 +0300

    Simple decrypt algo

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-function spoonerise(c,s=c.split` `){
-  if(s.length<2||s.length>3)return 'No spoons here'
-  s=s.map((e,i,v)=>{
-    e=[...e]
-    if(i===0||i===v.length-1){
-      for(let i=0;i<e.length;i++)if(e[i].match(/[aioue]/))e=[e.slice(0,i).join``,e.slice(i).join``]
-    }
-    return e
-  })
-  let one=s[s.length-1][0]+s[0][1],two=s[0][0]+s[s.length-1][1]
-  s[0]=one,s[s.length-1]=two
-  return s.map(e=>typeof e!=='string'?e.join``:e).join` `
+function decrypt(s,l='0'.repeat(26).split``){
+  s=[...s.replace(/[^a-z]/g,'')].sort().join``.replace(/((.)\2*)/g,e=>e[0]+e.length).match(/.{2,2}/g).map(e=>[l[parseInt(e[0],36)-10]=e[1]])
+  return l.join``
 }
 
-console.log(spoonerise("pack of lies"))
+console.log(decrypt('z$aaa#ccc%eee123456789'))
