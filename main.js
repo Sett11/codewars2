5935467b48eec3341c5bfa89e6b578447c0595b8
Jhonny <gonzodzen@mail.ru>
2023-04-03 19:43:04 +0300

    Wheel of Fortune

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-alphanumeric=s=>s&&!s.replace(/[A-Za-z0-9]/g,'')?1==1:1==2
+function winner(c,a=[],r=0){
+  console.log(c)
+  if(!c||c.length!==3)return false
+  for(let i of c){
+    if(!Array.isArray(i.scores)||!i.name||!i.scores||!i.scores.length||i.scores.length>2||i.scores.some(e=>+e!==+e||e%5!==0||e>100))return false
+    const x=i.scores.reduce((a,c)=>a+c,0)
+    r=Math.max(x,r)
+    a.push([i.name,x])
+  }
+  if(a.every(e=>e[1]>100))return false
+  return a.filter(e=>e[1]===r)[0][0]
+}
 
-console.log(alphanumeric('Mazinkaiser'))
+console.log(winner([
+  { name: "Bob", scores: [10, 65]},
+{ name: "Bill", scores: [90, 5] },
+{ name: "Charlie", scores: [40, 55] }]))
