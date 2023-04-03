6f295a87fdff8aef8ef68bd6f1c667baf1651eba
Jhonny <gonzodzen@mail.ru>
2023-04-04 01:50:23 +0300

    How many pages in a book?

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-function findRoutes(a,r=[]){
-  if(a.join``===[["Chicago", "Winnipeg"], ["Halifax", "Montreal"], ["Montreal", "Toronto"], ["Toronto", "Chicago"], ["Winnipeg", "Seattle"]].join``)return "Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle"
-  if(a.join``===[["Agra", "Tokyo"], ["Seoul", "Ljubljana"], ["Ljubljana", "Wroclaw"], ["Wroclaw", "Nashville"], ["Nashville", "Amsterdam"], ["Amsterdam", "Hull"], ["Hull", "Vancouver"], ["Vancouver", "Agra"], ["Tokyo", "Manila"]].join``)return 'Seoul, Ljubljana, Wroclaw, Nashville, Amsterdam, Hull, Vancouver, Agra, Tokyo, Manila'
-  if(a.join``===[["Calgary", "Fargo"], ["Spokane", "Toronto"], ["Winnipeg", "Montreal"], ["Toronto", "Calgary"], ["Fargo", "Winnipeg"]].join``)return 'Spokane, Toronto, Calgary, Fargo, Winnipeg, Montreal'
-  if(a.join``===[["BRA", "KSA"], ["USA", "BRA"], ["JPN", "PHL"], ["KSA", "UAE"], ["UAE", "JPN"]].join``)return 'USA, BRA, KSA, UAE, JPN, PHL'
-  if(a.join``===[["San Policarpo", "Oras"], ["Balangiga", "Lawaan"], ["Borongan", "Maydolong"], ["Jipapad", "Maslog"], ["Balangkayan", "Llorente"], ["Mercedes", "Guiuan"], ["Taft", "Sulat"], ["Sulat", "San Julian"], ["Arteche", "San Policarpo"], ["Oras", "Dolores"], ["Dolores", "Can-avid"], ["Can-avid", "Taft"], ["San Julian", "Borongan"], ["Maydolong", "Balangkayan"], ["Llorente", "Hernani"], ["Hernani", "General MacArthur"], ["General MacArthur", "Giporlos"], ["Giporlos", "Balangiga"], ["Lawaan", "Salcedo"], ["Salcedo", "Mercedes"], ["Maslog", "Arteche"]].join``)return 'Jipapad, Maslog, Arteche, San Policarpo, Oras, Dolores, Can-avid, Taft, Sulat, San Julian, Borongan, Maydolong, Balangkayan, Llorente, Hernani, General MacArthur, Giporlos, Balangiga, Lawaan, Salcedo, Mercedes, Guiuan' 
-  while(a.length){
-    let t=a[0][1]
-    r.push(a[0][0],t);a=a.slice(1)
-    for(let i=0;i<a.length;i++){
-      if(a[i][0]===t&&!r.includes(a[i][0])){
-        r.push(a[i][1]);t=a[i][1];a.splice(i,1)
-      }
-    }
+function amountOfPages(n,c=1,s=''){
+  while(1){
+    s+=c
+    if(s.length===n)return c
+    c++
   }
-  return [...new Set(r)].join`, `
 }
+
+console.log(amountOfPages(25))
