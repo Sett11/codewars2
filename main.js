eb83009905a53af768d9edcb20017693af615ce1
Jhonny <gonzodzen@mail.ru>
2023-04-04 02:20:54 +0300

    Goldbach's Conjecture

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function amountOfPages(n,c=1,s=''){
-  while(1){
-    s+=c
-    if(s.length===n)return c
-    c++
-  }
+checkGoldbach=(n,a=[],r=[],c=2,s=n-1)=>{
+  if(n%2!==0&&n>2||n<=2)return []
+ const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
+ while(1){
+  if(c===s)return []
+  if(a.length&&r.length&&a.some(e=>r.some(u=>u+e===n)))return a.map(e=>r.map(u=>u+e===n?[u,e]:0).filter(e=>e)).filter(e=>e.length).flat(2).reverse()
+  if(f(c))a.push(c)
+  if(f(s))r.push(s)
+  c++,s--
+ }
 }
 
-console.log(amountOfPages(25))
+console.log(checkGoldbach(2))
