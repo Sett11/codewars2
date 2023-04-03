186c821c2bef1603a45b6d2ae9a59f18845a71bf
Jhonny <gonzodzen@mail.ru>
2023-04-03 23:48:45 +0300

    Spy games - rebuild your decoder

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function decrypt(s,l='0'.repeat(26).split``){
-  s=[...s.replace(/[^a-z]/g,'')].sort().join``.replace(/((.)\2*)/g,e=>e[0]+e.length).match(/.{2,2}/g).map(e=>[l[parseInt(e[0],36)-10]=e[1]])
-  return l.join``
-}
+decrypt=s=>s.split` `.map(e=>String.fromCharCode((e.match(/\d/g).reduce((a,c)=>a+ +c,0)%27)+96)).join``.replace(/`/g,' ')
 
-console.log(decrypt('z$aaa#ccc%eee123456789'))
+console.log(decrypt("2@I51Rk 6@95I4R8k B23A1W6BZ O231I!W6BZ 3k&4E7h1"))
