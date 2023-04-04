8ebfdc1af977d5de8b003670f9c53ac8b8b1ce84
Jhonny <gonzodzen@mail.ru>
2023-04-04 22:03:11 +0300

    Frog jumping

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function separateLiquids(a,r=[],c=[]){
-  const o={'H':1.36,'W':1.0,'A':0.87,'O':0.80}
-  a.forEach(e=>e.forEach(u=>r.push([u,o[u]]))),r.sort((a,b)=>a[1]-b[1])
-  for(let i=0;i<r.length;i+=a[0].length)c.push(r.slice(i,i+a[0].length))
-  return c.map(e=>e.map(u=>u[0]))
+function solution(a,c=0,z=0,x=0){
+  while(1){
+    if(c>=a.length||c<0)return x
+    if(x===a.length)return -1
+    c+=a[z],z+=a[z],x++
+  }
 }
 
-console.log(separateLiquids([]))
+console.log(solution([-3]))
