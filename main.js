a2e27969fc7ec352865c0146dbcedbadf62d974f
Jhonny <gonzodzen@mail.ru>
2023-04-04 17:51:53 +0300

    Spelling Bee

--- main.js
+++ main.js
@@ -1,52 +1,17 @@
-class User{
-  constructor(a=-8,b=0){
-    this.rank=a
-    this.progress=b
+howManyBees=(a,r=[])=>{
+  if(!a||!a.length)return 0
+  for(let i=0;i<a[0].length;i++){
+    let iArr=[]
+    for(let j=0;j<a.length;j++)iArr.push(a[j][i])
+    r.push(iArr.join``);iArr=[]
   }
-  incProgress(n){
-    if(n<-8||n>8||!n)throw Error()
-    if(this.rank===8){
-      this.progress=0
-      return this
-    }
-    if(this.rank===8||n<this.rank-1){
-      if(n===-1&&this.rank===1)this.progress+=1
-      return this
-    }
-    if(n===this.rank)this.progress+=3
-    if(n===this.rank-1)this.progress+=1
-    if(n>this.rank){
-      if(this.rank<0&&n>0)n-=1
-      this.progress+=10*(n-this.rank)*(n-this.rank)
-    }
-      while(this.progress>=100){
-        this.rank+=1;this.progress-=100
-      }
-      if(!this.rank)this.rank=1
-      if(this.rank===8)this.progress=0
-    }
+  r=r.map(e=>[e.match(/bee/g),e.match(/eeb/g)].filter(u=>u)).filter(e=>e).flat().map(e=>e.length).reduce((a,c)=>a+c,0)
+  a=a.map(e=>[e.join``.match(/bee/g),e.join``.match(/eeb/g)].filter(u=>u)).filter(e=>e).flat().map(e=>e.length).reduce((a,c)=>a+c,0)
+  return a+r
 }
 
-const r=new User()
-r.incProgress(1)
-r.incProgress(1)
-r.incProgress(1)
-r.incProgress(1)
-r.incProgress(1)
-r.incProgress(2)
-r.incProgress(2)
-r.incProgress(-1)
-r.incProgress(3)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-r.incProgress(8)
-
-console.log(r.rank)
+console.log(howManyBees([
+  "bee.bee".split(''),
+  ".e..e..".split(''),
+  ".b..eeb".split('')
+]))
