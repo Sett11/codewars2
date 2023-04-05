84ce427ba805b77d66fe4b94af934ebff4a72440
Jhonny <gonzodzen@mail.ru>
2023-04-05 19:16:53 +0300

    Largest product in a series

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-function palindrome(n,f=y=>[...y+''],c=n-1,x=n+1){ 
-  if(typeof n!=='number'||n<0||!Number.isInteger(n))return 'Not valid'
-  if(f(n).join``===f(n).reverse().join``&&n>9)return n
-  while(1){
-    if(f(c).join``===f(c).reverse().join``&&f(c).length>1)return c
-    if(f(x).join``===f(x).reverse().join``&&f(x).length>1)return x
-    c--,x++
-  }
+greatestProduct=(s,c=0)=>{
+  for(let i=0;i<s.length-4;i++)c=Math.max(c,[...s.slice(i,i+5)].reduce((a,c)=>a* +c,1))
+  return c
 }
 
- console.log(palindrome(8))
+console.log(greatestProduct('92494737828244222221111111532909999'))
