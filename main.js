5fcc1f733077fa3614e7aa8d8f01550b7aa1b550
Jhonny <gonzodzen@mail.ru>
2023-04-05 18:43:39 +0300

    Numerical Palindrome #4

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-function palindrome(n,a=[...n+''],r=[]){
+function palindrome(n,f=y=>[...y+''],c=n-1,x=n+1){ 
   if(typeof n!=='number'||n<0||!Number.isInteger(n))return 'Not valid'
-  for(let i=0;i<a.length;i++){
-    for(let j=i+1;j<=a.length;j++){
-      let t=a.slice(i,j),q=t.slice(0,1).concat(t.slice(-1)),c=+t.join``
-      if(t.join``===t.reverse().join``&&t.length>1&&q.join``!=='00'&&!r.includes(c))r.push(c)
-    }
+  if(f(n).join``===f(n).reverse().join``&&n>9)return n
+  while(1){
+    if(f(c).join``===f(c).reverse().join``&&f(c).length>1)return c
+    if(f(x).join``===f(x).reverse().join``&&f(x).length>1)return x
+    c--,x++
   }
-  return !r.length?"No palindromes found":r.sort((a,b)=>a-b)
- }
+}
 
- console.log(palindrome(1294))
+ console.log(palindrome(8))
