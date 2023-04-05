e31bb80a6eec28eb9aa97275881e9d3c822bd881
Jhonny <gonzodzen@mail.ru>
2023-04-05 18:22:40 +0300

    Numerical Palindrome #3.5

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 function palindrome(n,a=[...n+''],r=[]){
-  if(a.length===1)return !1
   if(typeof n!=='number'||n<0||!Number.isInteger(n))return 'Not valid'
   for(let i=0;i<a.length;i++){
     for(let j=i+1;j<=a.length;j++){
-      let t=a.slice(i,j)
-      if(t.join``===t.reverse().join``&&t.length>1)r.push(t)
+      let t=a.slice(i,j),q=t.slice(0,1).concat(t.slice(-1)),c=+t.join``
+      if(t.join``===t.reverse().join``&&t.length>1&&q.join``!=='00'&&!r.includes(c))r.push(c)
     }
   }
-  return r.length
+  return !r.length?"No palindromes found":r.sort((a,b)=>a-b)
  }
 
- console.log(palindrome(141221001))
+ console.log(palindrome(1294))
