247d62aa0ce1e0a4835a97342eb2587dbc88111a
Jhonny <gonzodzen@mail.ru>
2023-04-05 23:33:03 +0300

    Rectangle letter juggling

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-function solve(s) {
-  if(!s.includes('[backspace]'))return s
-  s=s.replace(/\[backspace\]\*\d+/g,e=>e.replace(/[^\[\]a-z]/g,'').repeat(+e.replace(/\D/g,'')))
-  return !s.includes('[backspace]')?s:solve(s.replace(/.?\[backspace\]/,''))
+function cipherText(c,s=c.replace(/[^a-z]/gi,'').toLowerCase(),l=c?s.length:0,a=1,b=1,q=[],r=[]){
+  if(!l)return ''
+  while(1){if(a*b>=l)break;a++,b++}
+  for(let i=0;i<s.length;i+=b)q.push(s.slice(i,i+b))
+  q=q.map(e=>e.length<b?e+' '.repeat(b-e.length):e)
+  for(let i=0;i<q[0].length;i++){
+    let iArr=[]
+    for(let j=0;j<q.length;j++)iArr.push(q[j][i])
+    r.push(iArr.join``),iArr=[]
+  }
+  return r.join` `
 }
 
-console.log(solve('[backspace]5nani'))
+console.log(cipherText('Dolores wouldn\'t have eaten the meal if she had known what it actually was.'))
