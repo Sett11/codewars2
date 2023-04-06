9ec4ba7a65dd147897e904f97352414c3a229aff
Jhonny <gonzodzen@mail.ru>
2023-04-07 00:28:53 +0300

    Seven "ate" nine!

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-const o=['A','2','3','4','5','6','7','8','9','T','J','Q','K']
-const w=['c','d','h','s']
-function encode(a){
-  return a.map(e=>{
-    e=[...e]
-    let t=w.indexOf(e[1])
-    e=o.indexOf(e[0])
-    while(t){e+=13;t--}
-    return e
-  }).sort((a,b)=>a-b)
-}
-function decode(a){
-  return a.map(e=>{
-    let t=1
-    while(1){
-      if(e/t<13)break
-      t++
+function hungrySeven(a){
+  for(let i=0;i<a.length-2;i++){
+    let t=a.slice(i,i+3)
+    if(t.join``==='789'){
+      t.push(t.shift())
+      a=a.slice(0,i).concat(t).concat(a.slice(i+3))
+      i-=2
     }
-    return [o[e%13],w[t-1],e]
-  }).sort((a,b)=>a[2]-b[2]).map(e=>[e[0],e[1]].join``)
+  }
+  return a
 }
 
-console.log(decode([0, 51, 30, 22, 2]))
+  console.log(hungrySeven([7,7,7,8,9]))
