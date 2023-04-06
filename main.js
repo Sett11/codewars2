39d1be6ae68dcb85adb2b76041310e2911e5a28e
Jhonny <gonzodzen@mail.ru>
2023-04-06 22:48:09 +0300

    Tongues

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function goodVsEvil(a,b){
-  const f=(x,y)=>y===0?x:y===1?x*2:y===2||y===3?x*3:y===4?x*4:x*10
-  const r=(x,y)=>y===0?x:y===1||y===2||y===3?x*2:y===4?x*3:y===5?x*5:x*10
-  const z=a.split` `.reduce((a,c,i)=>a+f(+c,i),0),v=b.split` `.reduce((a,c,i)=>a+r(+c,i),0),s='Battle Result: '
-  return z<v?s+'Evil eradicates all trace of Good':z>v?s+'Good triumphs over Evil':s+'No victor on this battle field'
+function tongues(s){
+  const v='aiyeou',z='bkxznhdcwgpvjqtsrlmf'
+  return [...s].map(e=>{
+    e=e===e.toUpperCase()&&(z+v).includes(e.toLowerCase())?[e.toLowerCase(),1]:[e,0]
+    e[0]=v.includes(e[0])?v[(v.indexOf(e[0])+3)%v.length]:z.includes(e[0])?z[(z.indexOf(e[0])+10)%z.length]:e[0]
+    e[0]=e[1]?e[0].toUpperCase():e[0]
+    return e[0]
+  }).join``
 }
 
-console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'))
+console.log(tongues('Tim oh nsa nowa gid ecc fiir wat ni liwa ni nsa eor ig nsaod liytndu.'))
