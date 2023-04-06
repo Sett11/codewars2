8d62de9abfd6703376d319712803669b2df0130c
Jhonny <gonzodzen@mail.ru>
2023-04-06 23:02:19 +0300

    Four/Seven

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function tongues(s){
-  const v='aiyeou',z='bkxznhdcwgpvjqtsrlmf'
-  return [...s].map(e=>{
-    e=e===e.toUpperCase()&&(z+v).includes(e.toLowerCase())?[e.toLowerCase(),1]:[e,0]
-    e[0]=v.includes(e[0])?v[(v.indexOf(e[0])+3)%v.length]:z.includes(e[0])?z[(z.indexOf(e[0])+10)%z.length]:e[0]
-    e[0]=e[1]?e[0].toUpperCase():e[0]
-    return e[0]
-  }).join``
-}
+fourSeven=(n,o={4:7,7:4})=>o[n]||0
 
-console.log(tongues('Tim oh nsa nowa gid ecc fiir wat ni liwa ni nsa eor ig nsaod liytndu.'))
+console.log(fourSeven(5))
