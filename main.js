51c1ffabaabc8cced39fc8eed01bf15abf7a3e00
Jhonny <gonzodzen@mail.ru>
2023-04-06 20:09:03 +0300

    Too Big To Sum?

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-function battle(a,b){
-  for(let i=0;i<Math.min(a.length,b.length);i++){
-    if(a[i][0]>=b[i][1]&&b[i][0]>=a[i][1]){a[i]=0,b[i]=0}
-    if(a[i][0]>=b[i][1])b[i]=0
-    if(b[i][0]>=a[i][1])a[i]=0
-  }
-  return {'player1':a.filter(e=>e),'player2':b.filter(e=>e)}
+function certainSum(a,b){
+  return Number.isSafeInteger(a)&&Number.isSafeInteger(b)&&Number.isSafeInteger(a+b)
 }
 
-console.log(battle(
-  [[1, 1], [2, 1], [2, 2], [5, 5]],
-  [[1, 2], [1, 2], [3, 3]]))
+console.log(certainSum(2,2))
