e084589bbc55a82cd76d091a21c4beb063149d14
Jhonny <gonzodzen@mail.ru>
2023-04-06 22:23:22 +0300

    Good vs Evil

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-function add(a,b) {
-  if(+a<+b){
-    let tmp=a
-    a=b
-    b=tmp
-  }
-  if(a.length<b.length)a='0'.repeat(b.length-a.length)+a
-  if(a.length>b.length)b='0'.repeat(a.length-b.length)+b
-  a=[...a].map(Number),b=[...b].map(Number),s=''
-  for(let i=a.length-1;i>=0;i--){
-    let c=(a[i]+b[i])%10,r=(a[i]+b[i])>9?1:0
-    if(r)b[i-1]+=1
-    s+=c
-    if(i===0&&r)s+=r
-  }
-  return [...s].reverse().join``
+function goodVsEvil(a,b){
+  const f=(x,y)=>y===0?x:y===1?x*2:y===2||y===3?x*3:y===4?x*4:x*10
+  const r=(x,y)=>y===0?x:y===1||y===2||y===3?x*2:y===4?x*3:y===5?x*5:x*10
+  const z=a.split` `.reduce((a,c,i)=>a+f(+c,i),0),v=b.split` `.reduce((a,c,i)=>a+r(+c,i),0),s='Battle Result: '
+  return z<v?s+'Evil eradicates all trace of Good':z>v?s+'Good triumphs over Evil':s+'No victor on this battle field'
 }
 
-function sum(...a){
-  let b=[...a].map(e=>typeof e!=='string'?e+'':e).map(e=>{
-    while(e[0]==='0')e=e.slice(1)
-    return e
-  })
-  if(!b.length)return '0'
-  if(b.length%2!==0)b.push('0')
-  for(let i=0;i<b.length;i++){
-    let t=b.splice(i,2)
-    b.unshift(add(...t))
-  }
-  return b.length>1?sum(...b):b[0]
-}
-
-console.log(sum('12', '000003'))
+console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'))
