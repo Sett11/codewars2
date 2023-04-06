deac883146da736f84d5069f93885cfa483c31ac
Jhonny <gonzodzen@mail.ru>
2023-04-06 23:24:57 +0300

    Poker cards reducer

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-fourSeven=(n,o={4:7,7:4})=>o[n]||0
+function reduceCards(a){
+  const o=['A','1','2','3','4','5','6','7','8','9','T','J','Q','K'],f=x=>x.sort((a,b)=>a-b)
+  return !Array.isArray(a)?null:!a.length?[]:typeof a[0]==='string'?f(a.map(e=>o.indexOf(e[0]))).map(e=>o[e]):f(a.map(e=>e%13))
+}
 
-console.log(fourSeven(5))
+console.log(reduceCards([51, 7, 24, 22, 50, 0]))
