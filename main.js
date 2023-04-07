62ae20c1dcbc82c55da21ae4a1d4e284d048a86b
Jhonny <gonzodzen@mail.ru>
2023-04-07 15:48:31 +0300

    Guess The Gifts!

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function sortStringsByVowels(s){
-    return s.map(e=>[e,Math.max(...(e.match(/[aioue]+/gi)||[]).map(u=>u.length),0)]).sort((a,b)=>b[1]-a[1]).map(e=>e[0])
+function guessGifts(w,p,a=[]){
+    w=w.map(e=>[e.name,[e.size,e.clatters,e.weight].join``]),p=p.map(e=>[e.size,e.clatters,e.weight].join``)
+    for(let i=0;i<p.length;i++){
+        for(let j=0;j<w.length;j++)if(p[i]===w[j][1])a.push(w[j][0])
+    }
+    return [...new Set(a)]
 }
 
-console.log(sortStringsByVowels(["AIBRH","","YOUNG","GREEEN"]))
+console.log(guessGifts( [
+    {name: "mini puzzle", size: "small", clatters: "yes", weight: "light"},
+    {name: "toy car", size: "medium", clatters: "a bit", weight: "medium"},
+    {name: "card game", size: "small", clatters: "no", weight: "light"}
+  ],
+  [
+    {size: "medium", clatters: "a bit", weight: "medium"},
+    {size: "small", clatters: "yes", weight: "light"}
+  ]))
