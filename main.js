b1e5ce9ca1ddcbd00453613b576c52be5cdbde84
Jhonny <gonzodzen@mail.ru>
2023-04-07 08:22:48 +0300

    Wave Sorting

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-onesCounter=a=>(a.join``.match(/1+/g)||[]).map(e=>e.length)
+function waveSort(a){
+  for(let i=0;i<a.length;i++){
+    if(i%2===0){
+      for(let j=i+1;j<a.length;j++){
+        if(a[j]>=a[i]){
+          let t=a[i]
+          a[i]=a[j]
+          a[j]=t
+          break
+        }
+      }
+    }
+    if(i%2!==0){
+      for(let j=i+1;j<a.length;j++){
+        if(a[j]<=a[i]){
+          let t=a[i]
+          a[i]=a[j]
+          a[j]=t
+          break
+        }
+      }
+    }
+  }
+  return a
+}
 
-console.log(onesCounter([1, 1, 1, 0, 1]))
+console.log(waveSort([1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4]))
