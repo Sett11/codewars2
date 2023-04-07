d07bec3ced5bfefd176ab31f353d2ce230ff6cbc
Jhonny <gonzodzen@mail.ru>
2023-04-07 11:30:13 +0300

    Kingdoms Ep4: The Dancing Witch

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function sumFibs(n,a=[0,1,1]){
-  while(a.length<=n)a.push(a[a.length-1]+a[a.length-2])
-  return a.reduce((a,c)=>a+=(c%2===0)?c:0,0)
+function figureOut(a,r=[]){
+  for(let i=1;i<a.length;i++){
+    for(let j=0;j<a[i].length;j++){
+      r.push([a[i][j],Math.abs(a[i].indexOf(a[i][j])-a[i-1].indexOf(a[i][j]))])
+    }
+  }
+  r=r.filter(e=>e[1]>1&&e[0]!==' ')
+  return !r.length?null:r[0][0]
 }
 
-console.log(sumFibs(10))
+console.log(figureOut(["cb feg da", "bcf ge ad", "cbfg ea d", "cfb gae d", "fcbga e d", "fbcag  ed", "bfac ged ", "baf cgde "]))
