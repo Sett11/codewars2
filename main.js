b0d9387f789505dedefc57ef685a3e50996ff8df
Jhonny <gonzodzen@mail.ru>
2023-04-07 22:28:27 +0300

    Interleaving Arrays

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-sortme=a=>{
-  return a.sort((a,b)=>+a.split`-`[1]-+b.split`-`[1]||a.split`-`[0].localeCompare(b.split`-`[0]))
+function interleave(...a){
+    let t=[...a],r=[]
+    for(let i=0;i<Math.max(...t.map(e=>e.length));i++){
+        let q=[]
+        for(let j=0;j<t.length;j++){
+            q.push(t[j][i])
+        }
+        r.push(q),q=[]
+    }
+    return r.flat(1).map(e=>e===undefined?null:e)
 }
 
-console.log(sortme(['aeb-1305', 'site-1305', 'play-1215', 'web-1304', 'site-1304', 'beb-1305']))
+  console.log(interleave([1, 2, 3], [4, 5, 6], [7, 8, 9]))
