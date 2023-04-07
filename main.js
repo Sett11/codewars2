bc15078f5a7fe3538644077a97097d365e90c1da
Jhonny <gonzodzen@mail.ru>
2023-04-07 08:04:53 +0300

    Counter of neighbor ones

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function assembleString(b,a=b.map(e=>[...e])){
-  if(!a.length)return ''
-  let t=a[0]
-  for(let i=1;i<a.length;i++){
-    for(let j=0;j<a[i].length;j++){
-      if(t[j]==='*'&&a[i][j]!=='*')t[j]=a[i][j]
-    }
-  }
-  return t.join``.replace(/\*/g,'#')
-}
+onesCounter=a=>(a.join``.match(/1+/g)||[]).map(e=>e.length)
 
-console.log(assembleString([
-  "a*c**",
-  "**cd*",
-  "a*cd*"
-]))
+console.log(onesCounter([1, 1, 1, 0, 1]))
