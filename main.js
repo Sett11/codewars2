3ba38072eae78dd2bf3860097b7467d76bae86a3
Jhonny <gonzodzen@mail.ru>
2023-04-07 11:07:05 +0300

    SumFibs

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-function oddity(n,a=new Set()){
-  const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
-  if(f(n))return 'even'
-  for(let i=1;i<Math.sqrt(n)+1;i++){
-    if(n%i===0){
-      a.add(i),a.add(n/i)
-    }
-  }
-  return a.size%2===0?'even':'odd'
+function sumFibs(n,a=[0,1,1]){
+  while(a.length<=n)a.push(a[a.length-1]+a[a.length-2])
+  return a.reduce((a,c)=>a+=(c%2===0)?c:0,0)
 }
 
-console.log(oddity(100))
+console.log(sumFibs(10))
