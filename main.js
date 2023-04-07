653e830e12f969989399a90060b673603078d714
Jhonny <gonzodzen@mail.ru>
2023-04-07 15:22:22 +0300

    Maximum value in nested arrays

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function pattern(r,c,s,a=[]){
-    for(let i=0;i<s.length;i+=c)a.push(s.slice(i,i+c))
-    while(a.length<r)a.push(Array(c).fill(' ').join``)
-    let q='+'+'--- '.repeat(c).split` `.join`+`
-    a=a.map(e=>e.length<c?e+' '.repeat(c-e.length):e).map(e=>(q+'fack'+'| '+[...e].join` | `+' |').split`fack`.join`\n`+'\n'),a.push(q)
-    return a.join``
+function findNestedMaximum(a){
+  return Math.max(...a.flat(100))
 }
 
-console.log(pattern(6,7,"ww#_Qx*JlSOnxwZO][%LhQ#.&a&=z*xQT}" ))
+console.log(findNestedMaximum([4, -3, [10], [[-5, 6], 18]]))
