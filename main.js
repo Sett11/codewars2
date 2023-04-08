96263dae4e3d0f64d7371896c7c54e4656fc1bd0
Jhonny <gonzodzen@mail.ru>
2023-04-08 13:30:19 +0300

    Breaking search bad

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-function splitAndAdd(a,r) {
-   if(a.length===1||r===0)return a
-   let b=a.slice(0,Math.floor(a.length/2)),c=a.slice(Math.floor(a.length/2))
-   while(b.length<c.length)b.unshift(0)
-   b=b.map((e,i)=>e+c[i])
-   return splitAndAdd(b,r-1)
-}
+const TITLES=[
+    'The Big Bang Theory',
+    'How I Met Your Mother',
+    'Dexter',
+    'Breaking Bad',
+    'Doctor Who',
+    'The Hobbit',
+    'Pacific Rim',
+    'Pulp Fiction',
+    'The Avengers',
+    'Shining'
+  ] 
 
-console.log(splitAndAdd([1,2,3,4,5], 2))
+search=c=>TITLES.filter(e=>e.match(RegExp(`${c}`,'gi')))
+
+console.log(search('ho'))
