e5afd0b78ec8827b7b40b2c2942828dfcddef492
Jhonny <gonzodzen@mail.ru>
2023-04-08 13:10:26 +0300

    Write a function to sort a given string into alphabetical order

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-function sort(s,a=[],b=[]){
-    s=s.replace(/[^a-z ]/gi,'').split` `
-    s.forEach(e=>e[0]===e[0].toUpperCase()?b.push(e):a.push(e))
-    a.sort((a,b)=>a.localeCompare(b)),b.sort((a,b)=>b.localeCompare(a))
-    return a.concat(b).join` `
+function sort(w,a=[],b=[],c=[],s=[...w]){
+    s.forEach(e=>+e===+e?a.push(e):+e!==+e&&!e.match(/[a-z]/i)?b.push(e):c.push(e))
+    a=a.filter(e=>e!==' '&&e!=='\n'),c=c.map(e=>e===e.toUpperCase()?[e.toLowerCase(),1]:[e,0])
+    return c.sort((a,b)=>a[0].localeCompare(b[0])||b[1]-a[1]).map(e=>e[1]?e[0].toUpperCase():e[0]).concat(a.sort((a,b)=>+a- +b)).concat(b.sort()).join``
 }
 
-console.log(sort("Land of the Old Thirteen! Massachusetts land! land of Vermont and Connecticut!"))
+console.log(sort(`() => {
+
+    let text = '',
+        _stringlen  = _string.length,
+        _txtlen     = ~~(Math.random()*100);
+  
+    for(let i=0; i < _txtlen; i++ ) {
+  
+      text += _string[~~(Math.random() * _stringlen)];
+    }
+    
+    return text;
+  }`))
