322748cebc5ec4353e1892a39b1bb93225dc4bee
Jhonny <gonzodzen@mail.ru>
2023-04-08 12:26:39 +0300

    Exclusive presentations

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-normIndex=(a,g)=>g>=0?a[g%a.length]:a.slice().reverse()[Math.abs(g)%a.length-1]||a[0]
-
-console.log(normIndex([0, 1, 2, 3, 4, 5, 6, 7, 8, 9],-16))
+function presentationAgenda(a,r=[]){
+    for(let i=0;i<a.length;i++){
+        for(let j=i+1;j<a.length;j++){
+            for(let z=0;z<Math.max(a[i].dest.length,a[j].dest.length);z++){
+                if(a[i].dest.includes(a[j].dest[z]))r.push(a[j].dest[z])
+            }
+        }
+    }
+    return a.map(e=>{
+        e.dest=e.dest.map(u=>r.includes(u)?0:u).filter(z=>z)
+        return e
+    }).filter(e=>e.dest.length)
+}
+  console.log(presentationAgenda([ {'person': 'John', 'dest': ['Ahmedabad', 'Baghdad', 'Delhi', 'Dhaka']},
+  {'person': 'Mickeal Angelo', 'dest': ['Ahmedabad', 'Baghdad', 'Delhi', 'Hong Kong', 'Istanbul', 'Jakarta', 'Mumbai', 'Santiago', 'Singapore']},
+  {'person': 'Gandalf', 'dest': ['Chennai', 'Hong Kong']}
+]))
