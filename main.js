8f2af863fd7362391f430dae02b3553a6522f5cf
Jhonny <gonzodzen@mail.ru>
2023-04-08 11:29:44 +0300

    Will the present fit?

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-function solve(s){
-    if(s===[...s].reverse().join``)return 'OK'
-    for(let i=0;i<=s.length;i++){
-        let t=s.slice(0,i)+s.slice(i+1)
-        if(t===[...t].reverse().join``)return "remove one"
-    }
-    return "not possible"
+function willFit(a,b){
+  const f=x=>x.sort((a,b)=>a-b)
+  a=f(a),b=f(b)
+  for(let i=0;i<a.length;i++)if(a[i]>b[i]-2)return !1
+  return !0
 }
 
-console.log(solve("abbaab"))
+console.log(willFit([10, 2, 4], [6, 4, 12]))
