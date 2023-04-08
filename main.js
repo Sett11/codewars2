13b96613f5b088c7f10145e73be3fa23bdb6d8b0
Jhonny <gonzodzen@mail.ru>
2023-04-08 12:40:54 +0300

    Sort sentence pseudo-alphabetically

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-function presentationAgenda(a,r=[]){
-    for(let i=0;i<a.length;i++){
-        for(let j=i+1;j<a.length;j++){
-            for(let z=0;z<Math.max(a[i].dest.length,a[j].dest.length);z++){
-                if(a[i].dest.includes(a[j].dest[z]))r.push(a[j].dest[z])
-            }
-        }
-    }
-    return a.map(e=>{
-        e.dest=e.dest.map(u=>r.includes(u)?0:u).filter(z=>z)
-        return e
-    }).filter(e=>e.dest.length)
+function sort(s,a=[],b=[]){
+    s=s.replace(/[^a-z ]/gi,'').split` `
+    s.forEach(e=>e[0]===e[0].toUpperCase()?b.push(e):a.push(e))
+    a.sort((a,b)=>a.localeCompare(b)),b.sort((a,b)=>b.localeCompare(a))
+    return a.concat(b).join` `
 }
-  console.log(presentationAgenda([ {'person': 'John', 'dest': ['Ahmedabad', 'Baghdad', 'Delhi', 'Dhaka']},
-  {'person': 'Mickeal Angelo', 'dest': ['Ahmedabad', 'Baghdad', 'Delhi', 'Hong Kong', 'Istanbul', 'Jakarta', 'Mumbai', 'Santiago', 'Singapore']},
-  {'person': 'Gandalf', 'dest': ['Chennai', 'Hong Kong']}
-]))
+
+console.log(sort("Land of the Old Thirteen! Massachusetts land! land of Vermont and Connecticut!"))
