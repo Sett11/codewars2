910dd661f5b7c5b74fe78624b40c328145e88205
Jhonny <gonzodzen@mail.ru>
2023-04-10 01:10:51 +0300

    I'm longer than you!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-sumNestedNumbers=(a,f=(x,c=1)=>x.map(e=>!Array.isArray(e)?e**c:f(e,c+1)))=>f(a).flat(10).reduce((a,c)=>a+c,0)
+function longer(s) {
+    return s.split` `.map(e=>e[0]===e[0].toUpperCase()?[e,1,e.length]:[e,0,e.length]).sort((a,b)=>a[2]-b[2]||b[1]-a[1]||a[0].localeCompare(b[0])).map(e=>e[0]).join` `
+}
 
-console.log(sumNestedNumbers([1, [2], 3, [4, [5]]]))
+  console.log(longer("hello Hello"))
