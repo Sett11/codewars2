5cf5fb794c9e9ded51637a41df1e9bf9f5646f25
Jhonny <gonzodzen@mail.ru>
2023-04-09 20:59:00 +0300

    Triple trouble

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-const createIterator=a=>{
-  return {
-    index:0,
-    next(){
-        const r={value:a[this.index],done:this.index>a.length-1?!0:!1}
-        if(this.index!==a.length)this.index+=1
-        return r
+function tripledouble(n,m,a=[...n+''].join``,b=[...m+''].join``,c=[],d=[]){
+    for(let i=0;i<a.length;i++){
+        let t=a.slice(i,i+3)
+        if([...new Set(t)].length===1&&t.length===3)c.push([a[i],t])
     }
-  }
+    for(let i=0;i<b.length;i++){
+        let t=b.slice(i,i+2)
+        if([...new Set(t)].length===1&&t.length===2)d.push([b[i],t])
+    }
+    return c.some(e=>d.some(u=>u[0]===e[0]))?1:0
 }
 
-const iterator = createIterator(['One', , 'Two']);
-console.log(iterator.next())
-console.log(iterator.next())
-console.log(iterator.next())
-console.log(iterator.next())
-console.log(iterator.next())
-
-console.log(iterator.index)
+  console.log(tripledouble(10560002, 100))
