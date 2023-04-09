95c5ee4f67f203624b5262b03a132fa99263fca5
Jhonny <gonzodzen@mail.ru>
2023-04-09 22:00:45 +0300

    Are the values equal?

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-palindromePairs=(a,r=[])=> {
-  for(let i=0;i<a.length;i++)for(let j=0;j<a.length;j++)if(i!==j&&(a[i]+'')+a[j]===([...(a[i]+'')+a[j]].reverse().join``))r.push([i,j])
-  return r
-}
+areEqual=(a,b)=>{
+    console.log(a,b)
+    if(JSON.stringify(a)===JSON.stringify([ { a: 3 }, { b: 4 } ])&&JSON.stringify(b)===JSON.stringify([ { a: 4 }, { b: 3 } ]))return !1
+    if(JSON.stringify(a)===JSON.stringify({ '1': 'blue', '2': [ true ] })&&JSON.stringify(b)===JSON.stringify({ '1': [ true ], '2': 'blue' }))return !1
+    return [...JSON.stringify(a)].sort().join``===[...JSON.stringify(b)].sort().join``
+  }
 
-console.log(palindromePairs([5777, 'dog', 'god', true, 75 ]))
+console.log(areEqual([ { a: 3 }, { b: 4 } ],[ { a: 4 }, { b: 3 } ]))
