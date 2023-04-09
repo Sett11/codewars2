3b96e237485b6639f0073beeeaada3d139f39240
Jhonny <gonzodzen@mail.ru>
2023-04-09 16:04:21 +0300

    Square Matrix Multiplication

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-getCountedNucleotides=g=>[...g.toUpperCase()].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{A:0,C:0,G:0,T:0})
-
-console.log(getCountedNucleotides('aCCggT'))
+function matrixMultiplication(a,b,l=0,r=[]){
+    if(a[0].length!==b.length)return -1
+    for(let i=0;i<a.length;i++){
+      let iArr=[],z=0
+      while(l<(b[0].length*a.length/2)*2){
+        let c=0
+        for(let j=0;j<a[i].length;j++){
+          c+=a[i][j]*b[j][z]
+        }
+        iArr.push(c);l++;z++
+      }
+      r.push(iArr);iArr=[];l=0
+    }
+    return r.map(e=>e.filter(u=>u===u))
+  }
