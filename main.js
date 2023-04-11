61d6ab44c4d3f421f812d00adb4f1d769869df97
Jhonny <gonzodzen@mail.ru>
2023-04-11 22:18:15 +0300

    Count number of zeros from 1 to N

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-function longer(s) {
-    return s.split` `.map(e=>e[0]===e[0].toUpperCase()?[e,1,e.length]:[e,0,e.length]).sort((a,b)=>a[2]-b[2]||b[1]-a[1]||a[0].localeCompare(b[0])).map(e=>e[0]).join` `
-}
+countZeros=n=>Array(n).fill(1).map((e,i)=>i+e).join``.replace(/[^0]/g,'').length
 
-  console.log(longer("hello Hello"))
+
+console.log(countZeros(200))
