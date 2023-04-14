9a33d9297ad28ff02a7a453927b4e36c8f9406d9
Jhonny <gonzodzen@mail.ru>
2023-04-14 16:11:24 +0300

    Set Reducer

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-const DATA = [
-        [0,    15,       "Very severely underweight"],
-        [15,   16,       "Severely underweight"],
-        [16,   18.5,     "Underweight"],
-        [18.5, 25,       "Normal (healthy weight)"],
-        [25,   30,       "Overweight"],
-        [30,   35,       "Obese Class I (Moderately obese)"],
-        [35,   40,       "Obese Class II (Severely obese)"],
-        [40,   45,       "Obese Class III (Very severely obese)"],
-        [45,   50,       "Obese Class IV (Morbidly obese)"],
-        [50,   60,       "Obese Class V (Super obese)"],
-        [60,   Infinity, "Obese Class VI (Hyper obese)"],
-    ]
-
-function calculateBmi(w,v,h=v/100,c=(w/(h*h)).toFixed(1)){
-    return {value:c,category:DATA.filter(e=>e[0]<=+c&&e[1]>+c)[0][2]}
+function setReducer(a,r=[]){
+    for(let i=0;i<a.length;i++){
+        if(a[i]!==a[i+1])r.push(1)
+        else{
+            for(let j=i;j,a.length;j++){
+                if(a[j]!==a[j+1]){
+                    r.push(a.slice(i,j+1).length)
+                    i=j;break
+                }
+            }
+        }
+    }
+    return r.length==1?r[0]:setReducer(r,[])
 }
 
-console.log(calculateBmi(65,175))
+  console.log(setReducer([1, 7, 0, 6, 1, 9, 0, 7, 1, 6, 0, 9, 0]))
