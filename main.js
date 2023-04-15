e3b778d317d16cb5405accf33be85c9b09584a9f
Jhonny <gonzodzen@mail.ru>
2023-04-15 14:06:54 +0300

    RuplesJS #1: N Times Do

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function orderType(b,a=b.map(e=>typeof e==='number'?[...e+''].length:e.length)){
-    return new Set(a).size===1?'Constant':a.slice().sort((a,b)=>a-b).join``===a.join``?'Increasing':a.slice().sort((a,b)=>b-a).join``===a.join``?'Decreasing':'Unsorted'
+Number.prototype.times=function(f){
+    if(!f)throw Error()
+    let n=Math.round(+JSON.stringify(this))
+    for(let i=0;i<n;i++)f()
 }
 
-console.log(orderType([[1, 23, 456, 78910], ["abcdef", "ghijklmno", "pqrstuvwxy"], [[1, 23, 456, 78910, ["abcdef", "ghijklmno", "pqrstuvwxy"]], 1234]]))
+console.log((10).times(function() {
+    console.log('hello');
+  }))
