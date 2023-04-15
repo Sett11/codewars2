344f5e9798be3c98b6e4904025798c88f2f5b43e
Jhonny <gonzodzen@mail.ru>
2023-04-15 14:26:08 +0300

    RuplesJS #2: String Delete

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-Number.prototype.times=function(f){
-    if(!f)throw Error()
-    let n=Math.round(+JSON.stringify(this))
-    for(let i=0;i<n;i++)f()
+String.prototype.delete=function(...a){
+    s=[...a].filter(e=>typeof e==='string'||typeof e==='object').map(e=>typeof e=='string'?new RegExp(`${e}`,'g'):e),c=this
+    while(s.length)c=c.replace(s.shift(),'')
+    return String(c)
 }
 
-console.log((10).times(function() {
-    console.log('hello');
-  }))
+console.log("Hello World123!".delete(123))
