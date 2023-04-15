61ee4cdd28b0bf2d8ce03a592ec68201586de072
Jhonny <gonzodzen@mail.ru>
2023-04-15 19:27:10 +0300

    Summarize ranges

--- main.js
+++ main.js
@@ -1,43 +1,16 @@
-class Range{
-    constructor(a){
-        if(!a.includes('..')||typeof a!=='string')throw Error()
-        this.a=a
-        this.b=this.a.split`..`
-        this.c=this.b[0]
-        this.d=this.b[1]
-        if(this.c===this.d||+this.c>=+this.d||this.c.match(/[A-z]/)&&!this.d.match(/[A-z]/)||!this.c.match(/[A-z]/)&&this.d.match(/[A-z]/))throw Error()
-        this.arr=[]
-        if(this.c.match(/[0-9]/)){
-            this.x=+this.c,this.y=+this.d
-            while(this.x<=this.y){
-                this.arr.push(this.x)
-                this.x+=1
+function summaryRanges(a){
+    a=[...new Set(a)]
+    for(let i=0;i<a.length-1;i++){
+        if(a[i]+1===a[i+1]){
+            for(let j=i;j<a.length;j++){
+                if(a[j]+1!==a[j+1]){
+                    let t=a.slice(i,j+1)
+                    a=a.slice(0,i).concat([t[0],t[t.length-1]].join`->`).concat(a.slice(j+1))
+                    break
+                }
             }
         }
-        if(this.c.match(/[A-z]/)){
-            if((this.c).charCodeAt()>=(this.d).charCodeAt())throw Error()
-            this.arr=Array((this.d).charCodeAt()).fill(1).map((e,i)=>i+(this.c).charCodeAt()).filter(e=>e>=(this.c).charCodeAt()&&e<=(this.d).charCodeAt()).map(e=>String.fromCharCode(e)).filter(e=>e.match(/[A-Za-z]/))
-        }
-        this.length=this.arr.length
-        this.min=typeof this.arr[0]==='number'?Math.min(...this.arr):String.fromCharCode(this.arr.map(e=>(e).charCodeAt()).slice().sort((a,b)=>a-b)[0])
-        this.max=typeof this.arr[0]==='number'?Math.max(...this.arr):String.fromCharCode(this.arr.map(e=>(e).charCodeAt()).slice().sort((a,b)=>b-a)[0])
     }
-    each(f){
-        for(let i=0;i<this.arr.length;i++){
-            f(this.arr[i])
-        }
-    }
-    includes(n){
-        return this.arr.includes(n)
-    }
-    toArray(){
-        return this.arr
-    }
-    equals(s){
-        return s===this.a
-    }
+    return a.map(e=>typeof e!=='string'?e+'':e)
 }
-
-const r=new Range('A..Z')
-
-console.log(r.toArray())
+console.log(summaryRanges([-2,0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 9, 9, 10, 12]))
