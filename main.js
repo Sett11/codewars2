8e3387dd1f44417e7f310151eb68138f3c5614d2
Jhonny <gonzodzen@mail.ru>
2023-04-16 16:00:21 +0300

    Longest Repeated DNA Motif

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function luxhouse(a){
-    return a.map((e,i,v)=>Math.max(...v.slice(i+1))===e?1:e>Math.max(...v.slice(i+1))?0:Math.max(...v.slice(i+1))-e+1)
+function longestMotif(s,a=[]){
+    for(let i=0;i<s.length;i++){
+        for(let j=i+1;j<s.length;j++){
+            let t=s.slice(i,j)
+            if(s.slice(j).match(t)){
+                a.push(t)
+            }
+        }
+    }
+    return [...new Set(a.filter(e=>e.length===Math.max(...a.map(u=>u.length))))]
 }
 
-console.log(luxhouse([1, 2, 3, 1, 2]))
-console.log(luxhouse([1,1,1]))
+console.log(longestMotif('CGGGTGGAGTCCGTCCCATGAAAGAGACGGTATAGCAAGCGCAACGAAGCGCGATATAATCCTTTTTCTTCCTTTTC'))
