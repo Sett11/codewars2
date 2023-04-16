4bf8068ff4b6a88dffa27cdcd8e179aa66f004cd
Jhonny <gonzodzen@mail.ru>
2023-04-16 13:36:30 +0300

    Simple Fun #200: Luxurious House

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-function summaryRanges(a){
-    a=[...new Set(a)]
-    for(let i=0;i<a.length-1;i++){
-        if(a[i]+1===a[i+1]){
-            for(let j=i;j<a.length;j++){
-                if(a[j]+1!==a[j+1]){
-                    let t=a.slice(i,j+1)
-                    a=a.slice(0,i).concat([t[0],t[t.length-1]].join`->`).concat(a.slice(j+1))
-                    break
-                }
-            }
-        }
-    }
-    return a.map(e=>typeof e!=='string'?e+'':e)
+function luxhouse(a){
+    return a.map((e,i,v)=>Math.max(...v.slice(i+1))===e?1:e>Math.max(...v.slice(i+1))?0:Math.max(...v.slice(i+1))-e+1)
 }
-console.log(summaryRanges([-2,0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 9, 9, 10, 12]))
+
+console.log(luxhouse([1, 2, 3, 1, 2]))
+console.log(luxhouse([1,1,1]))
