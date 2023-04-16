9e52810a5cd1cf7b9c4fad5cc773ece076884c42
Jhonny <gonzodzen@mail.ru>
2023-04-16 19:12:24 +0300

    Countdown - Longest Word

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-function longestMotif(s,a=[]){
-    for(let i=0;i<s.length;i++){
-        for(let j=i+1;j<s.length;j++){
-            let t=s.slice(i,j)
-            if(s.slice(j).match(t)){
-                a.push(t)
-            }
+function longestWord(s){
+    console.log(s)
+      let a=words.filter(e=>[...e].every(u=>s.includes(u))).sort((a,b)=>b.length-a.length)
+      let c=Object.entries([...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))
+      a=a.map(e=>{
+        let t=c.filter(u=>e.match(u[0])).sort((a,b)=>a[0].localeCompare(b[0]))
+        let r=Object.entries([...e].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>a[0].localeCompare(b[0]))
+        for(let i=0;i<r.length;i++){
+          if(r[i]>t[i])return 0
         }
-    }
-    return [...new Set(a.filter(e=>e.length===Math.max(...a.map(u=>u.length))))]
-}
+        return e
+      }).filter(e=>e)
+    a=a.filter(e=>e.length===a[0].length)
+    return a.length?a.sort():null
+  }
 
-console.log(longestMotif('CGGGTGGAGTCCGTCCCATGAAAGAGACGGTATAGCAAGCGCAACGAAGCGCGATATAATCCTTTTTCTTCCTTTTC'))
+console.log(longestWord('ALI'))
