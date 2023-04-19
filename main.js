000444da34c17e45863073e7eac092338acec5fd
Jhonny <gonzodzen@mail.ru>
2023-04-19 21:47:43 +0300

    Urban Dictionary

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-Array.prototype.map=function(f){
-  const a=[]
-  for(let i=0;i<this.length;i++){
-    a.push(f(this[i]))
+class WordDictionary{
+  constructor(a=[]){
+    this.arr=a
   }
-  return a
+  addWord(s){
+    this.arr.push(s)
+    this.arr.sort((a,b)=>a.length-b.length)
+  }
+  search(s){
+    for(let i=0;i<this.arr.length;i++){
+      if(this.arr[i].length>s.length)return false
+      if(RegExp(`${s}`).test(this.arr[i])&&s.length===this.arr[i].length)return true
+    }
+    return false
+  }
 }
+
+const r= new WordDictionary()
+r.addWord('a');
+r.addWord('at');
+r.addWord('ate');
+r.addWord('ear');
+
+console.log(r.search('e.'))
