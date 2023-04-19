ba30268e12a594ac03b21234c67e24a5c534394c
Jhonny <gonzodzen@mail.ru>
2023-04-19 23:49:29 +0300

    Pad Left and Right

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-class WordDictionary{
-  constructor(a=[]){
-    this.arr=a
-  }
-  addWord(s){
-    this.arr.push(s)
-    this.arr.sort((a,b)=>a.length-b.length)
-  }
-  search(s){
-    for(let i=0;i<this.arr.length;i++){
-      if(this.arr[i].length>s.length)return false
-      if(RegExp(`${s}`).test(this.arr[i])&&s.length===this.arr[i].length)return true
-    }
-    return false
-  }
+String.prototype.padLeft = function(ch, n) {
+  return this.padStart(n,ch)
+}
+String.prototype.padRight = function(ch, n) {
+  return [...[...this].reverse().join``.padStart(n,ch)].reverse().join``
 }
 
-const r= new WordDictionary()
-r.addWord('a');
-r.addWord('at');
-r.addWord('ate');
-r.addWord('ear');
-
-console.log(r.search('e.'))
+console.log('2'.padRight('0',4))
