fe842508a6d1426e1f7e6d89ded69257b48af4ae
Jhonny <gonzodzen@mail.ru>
2023-04-20 20:33:48 +0300

    PaginationHelper

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-String.prototype.padLeft = function(ch, n) {
-  return this.padStart(n,ch)
-}
-String.prototype.padRight = function(ch, n) {
-  return [...[...this].reverse().join``.padStart(n,ch)].reverse().join``
+class PaginationHelper {
+	constructor(a,n) {
+	this.a=a
+  this.n=n
+  this.arr=[]
+  for(let i=0;i<this.a.length;i+=this.n)this.arr.push(this.a.slice(i,i+this.n))
+	}
+	itemCount=()=>this.a.length
+	pageCount=()=>this.arr.length
+	pageItemCount=n=>this.arr[n]?this.arr[n].length:-1
+	pageIndex=n=>n<0||n>=this.a.length||!this.a.length?-1:n<this.n?0:~~(n/this.n)
 }
 
-console.log('2'.padRight('0',4))
+const r=new PaginationHelper([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],4)
+
+console.log(r.pageIndex(17))
