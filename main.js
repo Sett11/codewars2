9ebf5ec692df9c3ba0a4924ff98770cf5d95cad9
Jhonny <gonzodzen@mail.ru>
2023-04-21 22:31:35 +0300

    #9 Matrices: Adding diagonal products

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-function solve(n){
-  a=Array(n).fill(1).map((e,i)=>e+i),j=1,r=[]
-  r.push(a.shift())
-  while(j<a.length){
-    r.push(a[0])
-    if(j===1)a=a.filter((e,i)=>i%2!==0)
-    if(j!==1)a=a.filter((e,i)=>i%j!==0)
-    j=a[0]
+const f=(w,c,r=[])=>{
+  [n,m]=[w.length,w[0].length]
+  while(c){
+    for(let i=0;i<n;i++){
+      let c=i,inArr=[],j=0
+      while(j<m&&c>=0){
+        inArr.push(w[c][j])
+        c-=1
+        j+=1
+      }
+      r.push(inArr.reverse())
+      inArr=[]
+    }
+    c--
+    w=w.map(e=>e.reverse()).reverse()
   }
-  return r.concat(a).reduce((a,c)=>a+c,0)
+  r.pop()
+  return r
 }
 
-console.log(solve(100))
+const s=x=>x.reduce((a,c)=>a+c.reduce((x,y)=>x*y,1),0)
+
+function sumProdDiags(a){
+  const one=f(a,2),two=f(a.map(e=>e.reverse()),2)
+  return s(two)-s(one)
+}
+
+console.log(sumProdDiags([
+  [ 1,  4, 7,  6,  5],
+  [-3,  2, 8,  1,  3],
+  [ 6,  2, 9,  7, -4],
+  [ 1, -2, 4, -2,  6],
+  [ 3,  2, 2, -4,  7]]))
