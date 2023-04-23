eac6b261c4a425e1ab356be2ac0f2c4cddeb350d
Jhonny <gonzodzen@mail.ru>
2023-04-23 20:13:54 +0300

    Find Cracker.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const maximumGap = function(n,c=0){
-    n=n.slice().sort((a,b)=>a-b)
-    for(let i=0;i<n.length-1;i++)c=Math.max(c,n[i+1]-n[i])
-    return c
+function findHack(a) {
+  return a.slice().map(e=>{
+    if(e[2].length>=5&&e[2].every(u=>u==='A'||u==='B'))e[2].push(20)
+    e[2]=e[2].map(u=>u==='A'?30:u==='B'?20:u==='C'?10:u==='D'?5:u===20?20:0).reduce((a,c)=>a+c,0)
+    return e
+  }).filter(e=>e[1]!==e[2]||e[1]>200).map(e=>e[0])
 }
-
-console.log(maximumGap([3,6,9,1]))
