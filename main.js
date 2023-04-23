96c045bd4f63b08ef0745a7296b133841c757755
Jhonny <gonzodzen@mail.ru>
2023-04-23 13:03:50 +0300

    Maximum Gap

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
-const f=(w,c,r=[])=>{
-  [n,m]=[w.length,w[0].length]
-  while(c){
-    for(let i=0;i<n;i++){
-      let c=i,inArr=[],j=0
-      while(j<m&&c>=0){
-        inArr.push(w[c][j])
-        c-=1
-        j+=1
-      }
-      r.push(inArr.reverse())
-      inArr=[]
-    }
-    c--
-    w=w.map(e=>e.reverse()).reverse()
-  }
-  r.pop()
-  return r
+const maximumGap = function(n,c=0){
+    n=n.slice().sort((a,b)=>a-b)
+    for(let i=0;i<n.length-1;i++)c=Math.max(c,n[i+1]-n[i])
+    return c
 }
 
-const s=x=>x.reduce((a,c)=>a+c.reduce((x,y)=>x*y,1),0)
-
-function sumProdDiags(a){
-  const one=f(a,2),two=f(a.map(e=>e.reverse()),2)
-  return s(two)-s(one)
-}
-
-console.log(sumProdDiags([
-  [ 1,  4, 7,  6,  5],
-  [-3,  2, 8,  1,  3],
-  [ 6,  2, 9,  7, -4],
-  [ 1, -2, 4, -2,  6],
-  [ 3,  2, 2, -4,  7]]))
+console.log(maximumGap([3,6,9,1]))
