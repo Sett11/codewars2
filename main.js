a11cdbd369da9a41fb036b21adf319b6b495dfc5
Jhonny <gonzodzen@mail.ru>
2023-04-24 15:57:09 +0300

    Simple Fun #191: Sum Of Regular Numbers

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-function sixColumnEncryption(s,a=[],r=[]){
-  for(let i=0;i<s.length;i+=6)a.push(s.slice(i,i+6).replace(/ /g,'.'))
-  a=a.map(e=>e+'.'.repeat(6-e.length))
-  for(let i=0;i<6;i++){
-    let c=''
-    for(let j=0;j<a.length;j++)c+=a[j][i]
-    r.push(c)
-    c=''
+function sumOfRegularNumbers(a,r=[]){
+  a.push(0)
+  for(let i=0;i<a.length;i++){
+    let t=a[i+1]-a[i],y=a[i+2]-a[i+1],inArr=[]
+    if(t===y){
+      for(let j=i;j<a.length-1;j++){
+        let x=a[j+1]-a[j]
+        inArr.push(a[j])
+        if(x!==t){
+          i=j-1
+          break
+        }
+      }
+    }
+    if(inArr.length>2)r.push(inArr.reduce((a,c)=>a+c,0))
+    inArr=[]
   }
-  return r.join` `
+  return r.reduce((a,c)=>a+c,0)
 }
-
-console.log(sixColumnEncryption("Attack at noon or we are done for"))
+console.log(sumOfRegularNumbers([1, 2, 3, 4, 5]))
