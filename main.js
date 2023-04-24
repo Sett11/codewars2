fb3fcdc8bd59c93a2f64500b971b29651c73fb6b
Jhonny <gonzodzen@mail.ru>
2023-04-24 14:50:33 +0300

    Simple Fun #188: Slogans

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-class Cons{
-    constructor(head,tail){
-    this.head = head;
-    this.tail = tail;
+function slogans(p,r,a=[]){
+  for(let i=r.length;i>=0;i--){
+    if(p.match(r)){
+      a.push(r)
+      break
     }
-}
-function toArray(list) {
-    if(list){
-        var more = list.tail;
-        return [list.head].concat(more? toArray(more) : []);
+    let t=r.slice(0,i)
+    if(p.match(t)){
+      a.push(t)
+      r=r.replace(t,'')
+      i=r.length
+      t=''
     }
-    return [];
+  }
+  return a.length
 }
-Cons.prototype.toArray = function(){ return toArray(this); };
 
-Cons.fromArray=function(a){
-    let t=null
-    while(a.length){
-        t=new Cons(a.pop(),t)
-    }
-    return t
-}
-  
- Cons.prototype.filter=function(p){
-    let t=this.tail&&this.tail.filter(p)
-    return p(this.head)?new Cons(this.head,t):t
-  }
-  
-  Cons.prototype.map=function(m){
-    return new Cons(m(this.head),this.tail&&this.tail.map(m))
-  }
-  const r=new Cons()
-  const x=r.fromArray([ 1, 2, 3, 4, 5 ])
-  console.log(map(x,e=>e**2))
-  
+console.log(slogans("glorytoukraine","ukraineaineainee"))
