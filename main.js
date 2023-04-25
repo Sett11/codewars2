021b98b336074739ffd2cb5340603d7ae73221e6
Jhonny <gonzodzen@mail.ru>
2023-04-25 15:47:39 +0300

    Benford's Law

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function noOrder(s){
-  s=s.replace(/ /g,'').replace(/\-\-/g,'+')
-  if(s.match(/\d+%0|\d+\/0/))return null
-  if(!s.match(/[^\d]/))return Math.floor(+s)
-  if(s.replace(/[\d\.]/g,'').length===1||s.replace(/[\d\.]/g,'').length===2&&s[0]==='-')return Math.floor(eval(s.replace(/\^/g,'**')))
-  let c=0
-  for(let i=1;i<s.length;i++){
-    if(!s[i].match(/[\.\d]/))c++
-    if(c===2){
-      s=Math.floor(eval(s.slice(0,i).replace(/\^/g,'**')))+s.slice(i)
-      break
-    }
-  }
-  return noOrder(s)
-}
+benford=a=>Array(9).fill(0).map((e,i)=>a.map(e=>(e+'').replace(/[^1-9]/g,'')[0]).filter(z=>z===i+1+'').length/a.length)
 
-console.log(noOrder("7 *  3 - 3/  10  0"))
-console.log(noOrder("0.037037037037037035-39+82-35/19+61-61+11/98+31*83-91*2*61-86-92-61/96/27/51"))
-console.log(noOrder('111.22222222222221+38'))
+console.log(benford([ 1.32, 57.8, 0.009, 111.73 ]))
