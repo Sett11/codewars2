ec5bcd1d00a285dd4a5a32b43df15383066780b5
Jhonny <gonzodzen@mail.ru>
2023-04-25 12:29:33 +0300

    No Order of Operations

--- main.js
+++ main.js
@@ -1,36 +1,19 @@
-function findDuplicatePhoneNumbers(a){
-  const o={'ABC':2,'DEF':3,'GHI':4,'JKL':5,'MNO':6,'PRS':7,'TUV':8,'WXY':9}
-  a=a.map(e=>{
-    e=[...e.replace(/[^A-z0-9]/g,'').toUpperCase()]
-    e=e.map(u=>{
-      for(let i in o){
-        if(i.match(u))u=o[i]
-      }
-      return u
-    }).join``
-    return [e.slice(0,3)+'-'+e.slice(3),1]
-  }).sort()
-  for(let i=0;i<a.length;i++){
-    for(let j=i+1;j<a.length;j++){
-      if(a[j][0]===a[i][0]){
-        a[i][1]++
-        a[j][0]=0
-      }
+function noOrder(s){
+  s=s.replace(/ /g,'').replace(/\-\-/g,'+')
+  if(s.match(/\d+%0|\d+\/0/))return null
+  if(!s.match(/[^\d]/))return Math.floor(+s)
+  if(s.replace(/[\d\.]/g,'').length===1||s.replace(/[\d\.]/g,'').length===2&&s[0]==='-')return Math.floor(eval(s.replace(/\^/g,'**')))
+  let c=0
+  for(let i=1;i<s.length;i++){
+    if(!s[i].match(/[\.\d]/))c++
+    if(c===2){
+      s=Math.floor(eval(s.slice(0,i).replace(/\^/g,'**')))+s.slice(i)
+      break
     }
   }
-  return a.filter(e=>e[0]&&e[1]>1).map(e=>`${e[0]}:${e[1]}`)
+  return noOrder(s)
 }
 
-console.log(findDuplicatePhoneNumbers([
-"4873279",
-"ITS-EASY",
-"888-4567",
-"3-10-10-10",
-"888-GLOP",
-"TUT-GLOP",
-"967-11-11",
-"310-GINO",
-"F101010",
-"888-1200",
-"-4-8-7-3-2-7-9-",
-"487-3279"]))
+console.log(noOrder("7 *  3 - 3/  10  0"))
+console.log(noOrder("0.037037037037037035-39+82-35/19+61-61+11/98+31*83-91*2*61-86-92-61/96/27/51"))
+console.log(noOrder('111.22222222222221+38'))
