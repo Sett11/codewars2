d6809c5990397e22ca8181d507c7c8c53ac67813
Jhonny <gonzodzen@mail.ru>
2023-04-26 21:40:41 +0300

    X marks the spot!

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-function getMostFrequent(a){
-  return a.temperature.map(e=>e.map((u,j)=>[u,e.filter(z=>z===u).length,j]).sort((a,b)=>b[1]-a[1]||b[2]-a[2]).filter((a,b,c)=>a[1]===c[0][1]&&a[2]===c[0][2])).map(e=>e[0][0])
-}
+x=n=>Array(n).fill(Array(n).fill(0)).map((e,i)=>e.map((u,j)=>i===j||i+j===n-1?1:u))
 
-console.log(getMostFrequent( {
-    "temperature": [
-      [15,17,19,21,21,21,20,16],
-      [16,17,22,22,22,22,20,16],
-      [12,17,19,20,20,20,20,18],
-      [14,15,19,19,20,22,18,17],
-      [15,17,24,24,24,20,20,20]
-    ]
-  }))
+
+console.log(x(6))
