6a41aef1f09746bd73b17e52c0deffd66643fe9f
Jhonny <gonzodzen@mail.ru>
2023-04-26 23:27:50 +0300

    Valid string

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-x=n=>Array(n).fill(Array(n).fill(0)).map((e,i)=>e.map((u,j)=>i===j||i+j===n-1?1:u))
+validWord=(d,w)=>{
+  const f=(x,y)=>{
+    for(let i=y.length;i>0;i--){
+      if(x.includes(y))return ''
+      if(x.includes(y.slice(i))){
+        y=y.slice(0,i)
+        i=y.length
+      }
+    }
+    return y
+  }
+    const r=(x,y)=>{
+      for(let i=0;i<y.length;i++){
+        if(x.includes(y))return ''
+        if(x.includes(y.slice(0,i))){
+          y=y.slice(i)
+          i=0
+        }
+      }
+      return y
+    }
+    const t=(x,y)=>{
+      for(let i=y.length;i>0;i--){
+        if(x.includes(y))return ''
+        if(x.includes(y.slice(0,i))){
+          y=y.slice(i)
+          i=y.length
+        }
+      }
+      return y
+    }
+  const one=f(d,w),two=r(d,w),three=t(d,w)
+  return !three||!one||!two||d.includes(one)||d.includes(two)||d.includes(three)
+}
 
-
-console.log(x(6))
+console.log(validWord(['wcxr','yi','r','y','cwf','wyf','pyf'],'wcxryirycwfwyfpyf'))
+console.log(validWord(['code', 'wars'], 'code'))
+console.log(validWord(['a','ujzg','rq','b','m','aj','rq','tzma','db','jubq','t'],'aujzgrqbmaj'))
+console.log(validWord( ['xfsk','xf','i',"qxy",'qi','eg','yi','iw','i','igt','jxqo','r','lfi','fln','iln','ojiv','q','yi','j','f','egr','lf','esk'],'xfskxfiqxyqiegyiiw'))
