dde47d18e2cd359d3d18211829af38a693521aa5
Jhonny <gonzodzen@mail.ru>
2023-04-26 11:33:26 +0300

    Roman Numerals Decoder

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function sort(a){
-  for(let i=0;i<a.length;i++){
-    for(let j=i+1;j<a.length;j++){
-        if(a[i]>a[j]){
-            let t=a[i]
-            a[i]=a[j]
-            a[j]=t
-        }
-    }
+function solution(r){
+  const o={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+  r=[...r].map(e=>o[e]);c=0;r.push(0)
+  for(let i=0;i<r.length-1;i++){
+    if(r[i+1]<=r[i])c+=r[i]
+    else c-=r[i]
   }
-  return a
+  return c
 }
 
-console.log(sort(["abc", "adc", "acc"]))
+console.log(solution('DXLVII'))
+console.log(solution('MDCLXVI'))
