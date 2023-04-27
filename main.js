285c8b6c9a0c059c2c62b20a069f57b4239abf34
Jhonny <gonzodzen@mail.ru>
2023-04-27 23:04:44 +0300

    Fat Fingers

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-class VigenèreCipher{
-  constructor(k,a){
-    this.k=[...k]
-    this.a=[...a]
-  }
-  encode(s){
-    return [...s].map((e,i)=>!this.a.includes(e)?e:this.a[(this.a.indexOf(this.k[i%this.k.length])+this.a.indexOf(e))%this.a.length]).join``
-  }
-  decode(s){
-    return [...s].map((e,i)=>{
-      if(!this.a.includes(e))return e
-      let r=(this.a.indexOf(e)-this.a.indexOf(this.k[i%this.k.length]))%this.a.length
-      if(r<0)r+=this.a.length
-      return this.a[r]
-    }).join``
+const fatFingers=s=>{
+console.log(s)
+if(!s)return s
+if([...new Set(s.toLowerCase())].length===1&&s[0].match(/a/i))return ''
+s=[...s]
+for(let i=0;i<s.length;i++){
+  if(s[i]==='a'||s[i]==='A'){
+    for(let j=i+1;j<s.length+1;j++){
+      if(s[j]==='a'||s[j]==='A'&&j<s.length-1){
+        s=s.slice(0,i).concat(s.slice(i+1,j).map(e=>e===e.toUpperCase()?e.toLowerCase():e.toUpperCase())).concat(s.slice(j+1))
+        i=j-1
+        break
+      }
+      if(j===s.length-1&&s[j]=='A'||s[j]=='a'){
+        s=s.slice(0,i).concat(s.slice(i+1,j).map(e=>e===e.toUpperCase()?e.toLowerCase():e.toUpperCase()))
+        i=j
+        break
+      }
+      if(j===s.length){
+        s=s.slice(0,i).concat(s.slice(i+1,j+1).map(e=>e===e.toUpperCase()?e.toLowerCase():e.toUpperCase()))
+        i=j
+        break
+      }
+    }
   }
 }
+s=s.join``
+while(s.includes('A'))s=s.slice(0,s.lastIndexOf('A'))+[...s.slice(s.lastIndexOf('A')+1)].map(e=>e===e.toUpperCase()?e.toLowerCase():e.toUpperCase()).join``
+while(s.includes('a'))s=s.slice(0,s.lastIndexOf('a'))+[...s.slice(s.lastIndexOf('a')+1)].map(e=>e===e.toUpperCase()?e.toLowerCase():e.toUpperCase()).join``
+return s
+}
 
-const r=new VigenèreCipher('password','abcdefghijklmnopqrstuvwxyz')
-
-console.log(r.encode('codewars'))
-console.log(r.decode('rovwsoiv'))
-
-const y=new VigenèreCipher('カタカナ','アイウエオァィゥェォカキクケコサシスセソタチツッテトナニヌネノハヒフヘホマミムメモヤャユュヨョラリルレロワヲンー')
-
-console.log(y.decode('ドオカセガヨゴザキアニ'))
+console.log(fatFingers('3wDEBAZteA'))
+console.log(fatFingers('MaAAU'))
+console.log(fatFingers('8YK3AAAAACxA'))
+//console.log(fatFingers("The end of the institution, maintenance, and administration of government, is to secure the existence of the body politic, to protect it, and to furnish the individuals who compose it with the power of enjoying in safety and tranquillity their natural rights, and the blessings of life: and whenever these great objects are not obtained, the people have a right to alter the government, and to take measures necessary for their safety, prosperity and happiness."))
