56a4d50dee9acc0d5652485b00039a765108b76b
Jhonny <gonzodzen@mail.ru>
2023-04-27 17:26:02 +0300

    Vigenère Cipher Helper

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
-const IterativeRotationCipher={};
-
-IterativeRotationCipher.encode=function(n,s){
-  const f=(n,s,v=n)=>{
-    if(!v)return n+' '+s
-    sp=[...s].map((e,i)=>e===' '?i:'').filter(e=>e)
-    s=s.replace(/ /g,'')
-    s=s.slice(-(n%s.length))+s.slice(0,-(n%s.length))
-    for(let i=0;i<sp.length;i++)s=s.slice(0,sp[i])+' '+s.slice(sp[i])
-    return f(n,s.split` `.map(e=>e.slice(-(n%e.length))+e.slice(0,-(n%e.length))).join` `,v-1)
+class VigenèreCipher{
+  constructor(k,a){
+    this.k=[...k]
+    this.a=[...a]
   }
-  return f(n,s)
+  encode(s){
+    return [...s].map((e,i)=>!this.a.includes(e)?e:this.a[(this.a.indexOf(this.k[i%this.k.length])+this.a.indexOf(e))%this.a.length]).join``
+  }
+  decode(s){
+    return [...s].map((e,i)=>{
+      if(!this.a.includes(e))return e
+      let r=(this.a.indexOf(e)-this.a.indexOf(this.k[i%this.k.length]))%this.a.length
+      if(r<0)r+=this.a.length
+      return this.a[r]
+    }).join``
+  }
 }
 
-IterativeRotationCipher.decode=function(s){
-  n=0
-  s=s.replace(/^\d+ /,e=>n=+e).replace(n,'')
-  const f=(n,s,v=n)=>{
-    if(!v)return s
-    s=s.split` `.map(e=>e.slice(n%e.length)+e.slice(0,n%e.length)).join` `
-    sp=[...s].map((e,i)=>e===' '?i:'').filter(e=>e)
-    s=s.replace(/ /g,'')
-    s=s.slice(n%s.length)+s.slice(0,n%s.length)
-    for(let i=0;i<sp.length;i++)s=s.slice(0,sp[i])+' '+s.slice(sp[i])
-    return f(n,s,v-1)
-  }
-  return f(n,s)
-}
+const r=new VigenèreCipher('password','abcdefghijklmnopqrstuvwxyz')
 
-console.log(IterativeRotationCipher.encode(14,`True evil is a mundane bureaucracy.`))
-console.log(IterativeRotationCipher.decode('14 daue ilev is a munbune Traurecracy.'))
+console.log(r.encode('codewars'))
+console.log(r.decode('rovwsoiv'))
+
+const y=new VigenèreCipher('カタカナ','アイウエオァィゥェォカキクケコサシスセソタチツッテトナニヌネノハヒフヘホマミムメモヤャユュヨョラリルレロワヲンー')
+
+console.log(y.decode('ドオカセガヨゴザキアニ'))
