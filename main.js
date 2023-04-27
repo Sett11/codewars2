5d76baf8d4781457fa89fc34205db146b2727e50
Jhonny <gonzodzen@mail.ru>
2023-04-28 01:53:28 +0300

    Remember

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function findMissing(a,r=[]){
-  for(let i=1;i<a.length;i++)r.push(a[i]/a[i-1])
-  c=+Object.entries(r.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b[1]-a[1])[0][0]
-  for(let i=0;i<a.length;i++)if(a[i]*c!==a[i+1])return a[i]*c
+function remember(s){
+  let a=[],c=[],r=[...s]
+  for(let i=0;i<r.length;i++){
+    if(a.includes(r[i])&&c.includes(r[i]))continue
+    if(a.includes(r[i]))c.push(r[i])
+    else a.push(r[i])
+  }
+  return c
 }
 
-console.log(findMissing([1,4,16,256]))
+console.log(remember("limbojackassin the garden"))
