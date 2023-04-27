fc005e0664876e4ef6b6e1503d75a217aaf6d3a9
Jhonny <gonzodzen@mail.ru>
2023-04-27 14:27:17 +0300

    Iterative Rotation Cipher

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-function runningAverage(){
-  const a=[]
-  return x=>{
-    a.push(x)
-    return Math.round(a.reduce((a,c)=>a+c,0)/a.length*100)/100
+const IterativeRotationCipher={};
+
+IterativeRotationCipher.encode=function(n,s){
+  const f=(n,s,v=n)=>{
+    if(!v)return n+' '+s
+    sp=[...s].map((e,i)=>e===' '?i:'').filter(e=>e)
+    s=s.replace(/ /g,'')
+    s=s.slice(-(n%s.length))+s.slice(0,-(n%s.length))
+    for(let i=0;i<sp.length;i++)s=s.slice(0,sp[i])+' '+s.slice(sp[i])
+    return f(n,s.split` `.map(e=>e.slice(-(n%e.length))+e.slice(0,-(n%e.length))).join` `,v-1)
   }
+  return f(n,s)
 }
 
-const r=runningAverage()
+IterativeRotationCipher.decode=function(s){
+  n=0
+  s=s.replace(/^\d+ /,e=>n=+e).replace(n,'')
+  const f=(n,s,v=n)=>{
+    if(!v)return s
+    s=s.split` `.map(e=>e.slice(n%e.length)+e.slice(0,n%e.length)).join` `
+    sp=[...s].map((e,i)=>e===' '?i:'').filter(e=>e)
+    s=s.replace(/ /g,'')
+    s=s.slice(n%s.length)+s.slice(0,n%s.length)
+    for(let i=0;i<sp.length;i++)s=s.slice(0,sp[i])+' '+s.slice(sp[i])
+    return f(n,s,v-1)
+  }
+  return f(n,s)
+}
 
-console.log(r(10))
-console.log(r(12))
+console.log(IterativeRotationCipher.encode(14,`True evil is a mundane bureaucracy.`))
+console.log(IterativeRotationCipher.decode('14 daue ilev is a munbune Traurecracy.'))
