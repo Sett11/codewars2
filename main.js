efd446e7679bb72e01e5898be63e08e9464a9d14
Jhonny <gonzodzen@mail.ru>
2023-04-27 09:08:09 +0300

    Get root property name

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function manhattanDistance(a,b){
-  return Math.abs(a[0]-b[0])+Math.abs(a[1]-b[1])
+function getRootProperty(o,v){
+ for(let i in o){
+  if(JSON.stringify(o[i]).replace(/\d+/g,' $& ').replace(/[^\d ]/g,'').split` `.filter(e=>e).map(Number).includes(v))return i
+ }
+  return null
 }
+
+console.log(getRootProperty({
+  "one": {
+      "nest1": {
+          "val1": [9, 34, 92, 100]
+       }
+  },
+  "2f7": {
+      "n1": [10, 92, 53, 71],
+      "n2": [82, 34, 6, 19]
+  }
+},10))
