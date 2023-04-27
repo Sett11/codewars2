b34389a959334c0a65cc061898c944441557047c
Jhonny <gonzodzen@mail.ru>
2023-04-27 13:06:56 +0300

    Running Average

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-function getRootProperty(o,v){
- for(let i in o){
-  if(JSON.stringify(o[i]).replace(/\d+/g,' $& ').replace(/[^\d ]/g,'').split` `.filter(e=>e).map(Number).includes(v))return i
- }
-  return null
+function runningAverage(){
+  const a=[]
+  return x=>{
+    a.push(x)
+    return Math.round(a.reduce((a,c)=>a+c,0)/a.length*100)/100
+  }
 }
 
-console.log(getRootProperty({
-  "one": {
-      "nest1": {
-          "val1": [9, 34, 92, 100]
-       }
-  },
-  "2f7": {
-      "n1": [10, 92, 53, 71],
-      "n2": [82, 34, 6, 19]
-  }
-},10))
+const r=runningAverage()
+
+console.log(r(10))
+console.log(r(12))
