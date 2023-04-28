1f26daaed67d2d20c52e2fc7c8ed3b76e32beaa2
Jhonny <gonzodzen@mail.ru>
2023-04-28 17:48:09 +0300

    Hamster me

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function findNum(s){
-  return ['zero','one','two','three','four','five','six','seven','eight','nine','ten'].map((e,i)=>s.toLowerCase().replace(/[^a-z]/g,'').match(e)?i:'').filter(e=>e)[0]||0
+function hamsterMe(c,m,a=[...'abcdefghijklmnopqrstuvwxyz']){
+ c=[...new Set(c)].sort(),r=[],g=0
+ while(c.length){
+  g=a.indexOf(c.shift())
+  iArr=[]
+  for(let i=g;c[0]!==a[i];i++){
+    iArr.push(a.splice(i,1)[0])
+    i--
+  }
+  r.push(iArr),iArr=[]
+ }
+ if(a.length)r[r.length-1].push(...a)
+ m=[...m]
+ for(let i=0;i<m.length;i++){
+  for(let j=0;j<r.length;j++){
+    if(r[j].includes(m[i]))m[i]=r[j][0]+(r[j].indexOf(m[i])+1)
+  }
+ }
+ return m.join``
 }
 
-console.log(findNum("To avoid the calf, I veered sharply to the left."))
-console.log(findNum("The robins love hiding amongst the smooth reeds."))
-console.log(findNum("It's always worth looking after your friends, even if they've upset you."))
+console.log(hamsterMe("hamster", "abcdefghijklmnopqrstuvwxyz"))
