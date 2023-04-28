519cd4b36f44a2104d9a638c23dc5affce44b252
Jhonny <gonzodzen@mail.ru>
2023-04-28 18:06:27 +0300

    String tops

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-function hamsterMe(c,m,a=[...'abcdefghijklmnopqrstuvwxyz']){
- c=[...new Set(c)].sort(),r=[],g=0
- while(c.length){
-  g=a.indexOf(c.shift())
-  iArr=[]
-  for(let i=g;c[0]!==a[i];i++){
-    iArr.push(a.splice(i,1)[0])
-    i--
-  }
-  r.push(iArr),iArr=[]
- }
- if(a.length)r[r.length-1].push(...a)
- m=[...m]
- for(let i=0;i<m.length;i++){
-  for(let j=0;j<r.length;j++){
-    if(r[j].includes(m[i]))m[i]=r[j][0]+(r[j].indexOf(m[i])+1)
-  }
- }
- return m.join``
+function tops(s,a=[]){
+  for(let i=1,j=2;i<s.length;i+=j*2+1,j+=2)a.push(s[i])
+  return a.reverse().join``
 }
 
-console.log(hamsterMe("hamster", "abcdefghijklmnopqrstuvwxyz"))
+console.log(tops('abcdefghijklmnopqrstuvwxyz12345'))
