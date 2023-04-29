d6a87731c62a1d8824339bd9bcf4350d7902a67d
Jhonny <gonzodzen@mail.ru>
2023-04-29 14:17:53 +0300

    Battleship field validator

--- main.js
+++ main.js
@@ -1,14 +1,91 @@
-function slicesToName(n){
-  if(n<2||typeof n!=='number')return null
-  if(n===2)return 'sandwich'
-  return n%2&1?'bread'+' sandwich'.repeat(Math.floor(n/2)):'sandwich '.repeat(Math.floor(n/2)).trim()
+const f=(w,c,r=[])=>{
+  [n,m]=[w.length,w[0].length]
+  while(c){
+    for(let i=0;i<n;i++){
+      let c=i,inArr=[],j=0
+      while(j<m&&c>=0){
+        inArr.push(w[c][j])
+        c-=1
+        j+=1
+      }
+      r.push(inArr.reverse())
+      inArr=[]
+    }
+    c--
+    w=w.map(e=>e.reverse()).reverse()
+  }
+  r.pop()
+  return r
 }
-function nameToSlices(s){
-  if(!s||typeof s!=='string'||s.replace(/bread|sandwich| /g,'').length||s==='bread')return null
-  const c=s.split` `
-  if([...new Set(c)].length>2||c.includes('bread')&&c.lastIndexOf('bread'))return null
-  return c.includes('bread')?Math.floor((c.length-1)*2)+1:c.length*2
+function validateBattlefield(a){
+  const o=f(a,2),d=f(a.map(e=>e.reverse()),2)
+  for(let i=0;i<o.length;i++){
+    for(let j=1;j<o[i].length;j++){
+      if(o[i][j]&&o[i][j-1]||d[i][j]&&d[i][j-1])return false
+    }
+  }
+  a=a.map((e,i)=>e.map((u,j)=>u?[i,j]:u).filter(z=>z)).filter(e=>e.length).flat()
+  b=[]
+  for(let i=0;i<a.length;i++){
+    let iAr=[a[i]]
+    let t=a[i]
+    for(let j=i+1;j<a.length;j++){
+      if(t[0]===a[j][0]&&Math.abs(t[1]-a[j][1])===1||t[1]===a[j][1]&&Math.abs(t[0]-a[j][0])===1){
+        iAr.push(a[j])
+        t=a[j]
+      }
+    }
+    if(iAr.length===4)b.push(...iAr)
+    iAr=[]
+  }
+  if(b.length!==4)return false
+  b=b.map(e=>e.join``)
+  a=a.filter(e=>!b.includes(e.join``))
+  b=[]
+  for(let i=0;i<a.length;i++){
+    let iAr=[a[i]]
+    let t=a[i]
+    for(let j=i+1;j<a.length;j++){
+      if(t[0]===a[j][0]&&Math.abs(t[1]-a[j][1])===1||t[1]===a[j][1]&&Math.abs(t[0]-a[j][0])===1){
+        iAr.push(a[j])
+        t=a[j]
+      }
+    }
+    if(iAr.length===3)b.push(...iAr)
+    iAr=[]
+  }
+  if(b.length!==6)return false
+  b=b.map(e=>e.join``)
+  a=a.filter(e=>!b.includes(e.join``))
+  b=[]
+  for(let i=0;i<a.length;i++){
+    let iAr=[a[i]]
+    let t=a[i]
+    for(let j=i+1;j<a.length;j++){
+      if(t[0]===a[j][0]&&Math.abs(t[1]-a[j][1])===1||t[1]===a[j][1]&&Math.abs(t[0]-a[j][0])===1){
+        iAr.push(a[j])
+        t=a[j]
+      }
+    }
+    if(iAr.length===2)b.push(...iAr)
+    iAr=[]
+  }
+  if(b.length!==6)return false
+  b=b.map(e=>e.join``)
+  a=a.filter(e=>!b.includes(e.join``))
+  b=[]
+  return a.length===4
 }
 
-console.log(slicesToName(11))
-console.log(nameToSlices('sandwich sandwich sandwich sandwich'))
+console.log(validateBattlefield([
+  [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+  [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]))
