6a458ea3ef8c182fdac19c6c00aa332384808997
Jhonny <gonzodzen@mail.ru>
2023-04-30 16:18:50 +0300

    Coding 3min : Balance Attraction

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-function isHappy(n,p,c=1){
-  if(n===42&&p===2&&c)return [42, 20, 4, 16, 37, 58, 89, 145, 42]
-  if(n===58&&p===2&&c)return [58, 89, 145, 42, 20, 4, 16, 37, 58]
-  if(n===94101&&p===4&&c)return [2178, 6514, 2178]
-  if(n===7120&&p===3&&c)return [352, 160, 217, 352]
-  if(n===8663||n===8814&&p===2&&c)return [145, 42, 20, 4, 16, 37, 58, 89, 145]
-  if(n===41970&&c)return [89, 145, 42, 20, 4, 16, 37, 58, 89]
-  if(n===50229&&p===4&&c)return [6514, 2178, 6514]
-  if(n===19168&&p===3&&c)return [1459, 919, 1459]
-  let a=[...[...n+''].reduce((a,c)=>a+=(c**p),0)+''],r=[]
-  while(1){
-    let t=a.reduce((a,c)=>a+=(c**p),0)
-    if(r.includes(1)||r.includes(t))break
-    r.push(t)
-    a=[...t+'']
-  }
-  let b=r[r.length-1]
-  r.unshift(b)
-  return !c&&r[0]===1?[1]:!c?r:isHappy(b,p,c-1)
+function sc(s,a){
+  s=s.replace(/(.)\1*/g,' $& ').split` `
+  while(a.length){
+    t=a.shift(),m=100
+    s.forEach(e=>e[0]==t?m=Math.min(m,e.length):0)
+      for(i=0;i<=s.length;i++){
+        if(i==s.length){
+          s.push(t)
+          break
+        }
+        if(s[i][0]==t&&s[i].length==m){
+          s[i]+=t
+          break
+        }
+      }
+    }
+  return s.join``
 }
 
-console.log(isHappy(7,2))
+console.log(sc("x",["x","x","x"]))
+console.log(sc("abababab",["a","a","a","a","b","b","b","b"]))
+console.log(sc('mmilllccckkkiiiqqwwttllllllpppcccccccccpppppppppxxvss'  ,['i','i','p','m','i','k','c','i']))
