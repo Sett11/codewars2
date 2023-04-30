75c9a488b5c7814eadf856f70fabd07118d19036
Jhonny <gonzodzen@mail.ru>
2023-04-30 17:21:24 +0300

    Shortest code : Balance Attraction

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-function sc(s,a){
-  s=s.replace(/(.)\1*/g,' $& ').split` `
-  while(a.length){
-    t=a.shift(),m=100
-    s.forEach(e=>e[0]==t?m=Math.min(m,e.length):0)
-      for(i=0;i<=s.length;i++){
-        if(i==s.length){
-          s.push(t)
-          break
-        }
-        if(s[i][0]==t&&s[i].length==m){
-          s[i]+=t
-          break
-        }
-      }
-    }
-  return s.join``
+function sc(c,a){
+  s=c.match(/(.)\1*/g).map(e=>[e[0],e.length])
+  a.forEach(e=>{
+    v=s.reduce((a,c)=>c[0]!=e||a[1]<=c[1]?a:c, [e,NaN])
+    v[1]++
+    if(!v[1])s.push([e,1])
+  })
+  return s.map(([e,i])=>e.repeat(i)).join``
 }
 
+console.log(sc('ab',[ 'c' ]))
 console.log(sc("x",["x","x","x"]))
 console.log(sc("abababab",["a","a","a","a","b","b","b","b"]))
 console.log(sc('mmilllccckkkiiiqqwwttllllllpppcccccccccpppppppppxxvss'  ,['i','i','p','m','i','k','c','i']))
