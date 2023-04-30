64c1c4119d81ddd05e4e5a393d7f9d280c737389
Jhonny <gonzodzen@mail.ru>
2023-04-30 18:22:12 +0300

    Shortest Code : Find the murderer

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-function sc(c,a){
-  s=c.match(/(.)\1*/g).map(e=>[e[0],e.length])
-  a.forEach(e=>{
-    v=s.reduce((a,c)=>c[0]!=e||a[1]<=c[1]?a:c, [e,NaN])
-    v[1]++
-    if(!v[1])s.push([e,1])
-  })
-  return s.map(([e,i])=>e.repeat(i)).join``
-}
+sc=(s,c,f=RegExp(`^${s.replace(/~/g,'.')}`,'i'))=>c.filter(e=>f.test(e)).join`,`
 
-console.log(sc('ab',[ 'c' ]))
-console.log(sc("x",["x","x","x"]))
-console.log(sc("abababab",["a","a","a","a","b","b","b","b"]))
-console.log(sc('mmilllccckkkiiiqqwwttllllllpppcccccccccpppppppppxxvss'  ,['i','i','p','m','i','k','c','i']))
+
+console.log(sc('p~t~r',['Bernard Deltheil','R Steell','Peter Gone','Peter Reeves','Roland Scorsini']))
