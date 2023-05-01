c889d8a498d3001bd182868db92176b6a9ad06bb
Jhonny <gonzodzen@mail.ru>
2023-05-01 21:19:35 +0300

    Linux history and `!` command. Series#2 The `!n` command

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-bangBang=(s,r=s.split('\n').reverse().map(e=>e.trim()))=>r[0].slice(r[0].indexOf(' ')).trim()
+ bangN=(n,h)=>((h.split`\n`[n-1]||'').split(/\d+(.*)/)[1]||'').trim``||`!${n}: event not found`
 
-
-console.log(bangBang("   1  cd /pub\n  2  more beer\n  3  lost\n  4  ls \n  5  touch me \n  6  chmod 000 me \n  7  history\n  8  more me"))
+console.log(bangN(13,"   1  cd /pub\n  2  more beer\n  3  lost\n  4  ls\n  5  touch me \n  6  chmod 000 me \n  7  more me\n  8  history"))
