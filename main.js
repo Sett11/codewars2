c884a8912bdf0d9793f47aa3a738f22ce613366a
Jhonny <gonzodzen@mail.ru>
2023-05-01 22:21:10 +0300

    Linux history and `!` command. Series#5 The `!?string` command

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-bangStartString=(s,h)=>h.split`\n`.reverse().map(e=>e.split(/\d+(.*)/)[1].trim()).filter(e=>e.match(RegExp(`^${s}`)))[0]||`!${s}: event not found`
+bangContainString=(s,h)=>((h.split`\n`.reverse().filter(e=>e.match(s))[0]||'').split(/\d+(.*)/)[1]||'').trim()||`!${s}: event not found`
 
-console.log(bangStartString('me',"   1  cd /pub\n  2  more beer\n  3  lost\n  4  ls\n  5  touch me\n  6  chmod 000 me\n  7  more me\n  8  history"))
+console.log(bangContainString('me',"   1  cd /pub\n  2  more beer\n  3  lost\n  4  ls\n  5  touch me\n  6  chmod 000 me\n  7  more me\n  8  history"))
