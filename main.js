37cb8d297931954af9449b2349b09c0bb2a94c48
Jhonny <gonzodzen@mail.ru>
2023-05-01 13:34:56 +0300

    Unflatten a list (Easy)

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-function lemmingBattle(n,g,b){
-    const f=x=>x.sort((a,b)=>b-a),r=x=>x.filter(e=>e>0),q=x=>x.join` `;f(g),f(b)
-    while(g.length&&b.length){
-        for(let i=0;i<n;i++){
-            if(g[i]&&b[i]){
-                let t=b[i]
-                b[i]-=g[i]
-                g[i]-=t
-            }
+function unflatten(a,r=[]){
+    for(let i=0;i<a.length;i++){
+        if(a[i]<3)r.push(a[i])
+        else{
+            r.push(a.slice(i,i+a[i]))
+            i+=a[i]-1
         }
-        g=r(f(g)),b=r(f(b))
     }
-    const l=g.length,c=b.length
-    return !l&&!c?'Green and Blue died':!l?`Blue wins: ${q(f(b))}`:`Green wins: ${q(f(g))}`
+    return r
 }
 
-console.log(lemmingBattle(5, [10], [10]))
-console.log(lemmingBattle(2, [20, 10], [10, 10, 15]))
-console.log(lemmingBattle(3, [50, 40, 30, 40, 50], [50, 30, 30, 20, 60]))
+console.log(unflatten([1,4,5,2,1,2,4,5,2,6,2,3,3]))
