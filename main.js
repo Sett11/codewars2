59bba3d0c3e62779b6565003dc4cf6b85400dea8
Jhonny <gonzodzen@mail.ru>
2023-05-03 01:59:46 +0300

    Address Book by State

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-camelize=s=>s.replace(/[^a-z0-9]/gi,' ').split` `.map(e=>e.charAt(0).toUpperCase()+e.slice(1).toLowerCase()).join``
+function byState(s){
+    o={'AZ': 'Arizona','CA': 'California','ID': 'Idaho','IN': 'Indiana','MA': 'Massachusetts','OK': 'Oklahoma','PA': 'Pennsylvania','VA': 'Virginia'}
+    const r=s.split`\n`.filter(e=>e).map(e=>[o[e.slice(-2)],e.slice(0,e.length-2)+o[e.slice(-2)]])
+    for(let i=0;i<r.length;i++){
+        for(let j=i+1;j<r.length;j++){
+            if(r[i][0]===r[j][0]){
+                r[i].push(r[j][1])
+                r.splice(j,1)
+                j--
+            }
+        }
+    }
+    return r.map(e=>[e[0],...e.slice(1).sort()].join`\r\n..... `.replace(/,/g,'')).sort().join`\r\n `
+}
 
-
-console.log(camelize('.BQlf:,Bu'))
-console.log(camelize('"t8k0tL C8yRddnuU4GZky3Y w: t7I'))
+console.log(byState(`John Daggett, 341 King Road, Plymouth MA
+Alice Ford, 22 East Broadway, Richmond VA
+Orville Thomas, 11345 Oak Bridge Road, Tulsa OK
+Terry Kalkas, 402 Lans Road, Beaver Falls PA
+Eric Adams, 20 Post Road, Sudbury MA
+Hubert Sims, 328A Brook Road, Roanoke MA
+Amy Wilde, 334 Bayshore Pkwy, Mountain View CA
+Sal Carpenter, 73 6th Street, Boston MA`))
