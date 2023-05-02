a6941337b056383d54a1286a7ea5ca02a5f30542
Jhonny <gonzodzen@mail.ru>
2023-05-02 20:34:14 +0300

    Closest pair of points

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
-function findPosition(s,m){
-    console.log(s)
-    console.log(1)
-    console.log(m)
-    if(s===`Pikachu
-  He
-  llo`&&m===`He
-  llo`)return { lineStart: 1, indexStart: +0, indexEnd: 3, lineEnd: 2 }
-    if(s===`Pikachu
-  He
-  lloooo`&&m===`He
-  lloooo`)return { lineStart: 1, indexStart: +0, indexEnd: 6, lineEnd: 2 }
-    if(s===`Pikachu
-     He
-  lloooo`&&m===`He
-  lloooo`)return { lineStart: 1, indexStart: 3, indexEnd: 6, lineEnd: 2 }
-    if(s==='World, Hello'&&m==='Hello')return { lineStart: +0, indexStart: 7, indexEnd: 12, lineEnd: +0 }
-   if(s===m)return {lineStart: 0, indexStart: 0, indexEnd: s.length, lineEnd: 0}
-   if(!s.match(m))return {lineStart: 0, indexStart: 0, indexEnd: 0, lineEnd: 0}
-   c=s.replace(/[\n\r]/g,' ').split` `.map((e,i)=>[e,i]).filter(e=>e[0].match(m)).map(e=>[e,e[0].split``,RegExp(m).exec(e[0]),[]]).map(e=>{
-    for(let i=0;i<e[1].length;i++){
-      for(let j=i;j<e[1].length+1;j++){
-        if(e[1].slice(i,j).join``==m)e[3].push([i,j])
-      }
-    }return e
-   })
-   return {lineStart: !s.match(/[\n\r]/g)?0:c[0][0][1], indexStart: c[0][3][0][0], indexEnd: c[0][3][0][1], lineEnd: !s.match(/[\n\r]/g)?0:c[0][0][1]}
-  }
+function closestPair(a,r=[],c=10000000){
+    for(let i=0;i<a.length;i++){
+        for(let j=i+1;j<a.length;j++){
+            let s=Math.abs(a[j][0]-a[i][0])+Math.abs(a[j][1]-a[i][1])
+            c=Math.min(c,s)
+            r.push([s,[a[i],a[j]]])
+        }
+    }
+    return r.filter(e=>e[0]==c)[0][1]
+}
+
+console.log(closestPair([
+    [ 2000, 2000 ],
+    [ 2000, 8000 ],
+    [ 5000, 5000 ],
+    [ 6000, 3000 ],
+    [ 6000, 7000 ],
+    [ 7000, 4000 ],
+    [ 7000, 9000 ]
+  ]))
