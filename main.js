77b81d3a0f284efd0127207181459767a70e1f80
Jhonny <gonzodzen@mail.ru>
2023-05-03 01:39:25 +0300

    Arabian String

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-fireAndFury=(s,r=s.match(/FURY|FIRE/g))=>{
-    if(!r||s.match(/[^EFIRUY]/))return 'Fake tweet.'
-    r=r.map(e=>[e,0])
-    for(let i=0;i<r.length;i++){
-        for(let j=i+1;j<r.length;j++){
-            if(r[i][0]===r[j][0]){
-                r[i][1]++
-                r.splice(j,1)
-                j--
-            }
-            else break
-        }
-    }
-    return r.map(e=>e[0]==='FIRE'?`You ${'and you '.repeat(e[1])}are fired!`:`I am ${'really '.repeat(e[1])}furious.`).join` `
-}
+camelize=s=>s.replace(/[^a-z0-9]/gi,' ').split` `.map(e=>e.charAt(0).toUpperCase()+e.slice(1).toLowerCase()).join``
 
-  console.log(fireAndFury('FURYYYFIREYYFIRE'))
-  console.log(fireAndFury('FIREYYFURYYFURYYFURRYFIRE'))
-  console.log(fireAndFury('FYRYFIRUFIRUFURE'))
+
+console.log(camelize('.BQlf:,Bu'))
+console.log(camelize('"t8k0tL C8yRddnuU4GZky3Y w: t7I'))
