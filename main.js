adf103663e085a29820e14757bde169f3cc297ba
Jhonny <gonzodzen@mail.ru>
2023-05-03 19:47:21 +0300

    ACME factory needs your help

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-Array.prototype.mapMany=function(fn){
-  const f=x=>{
-    x=x.reduce((a,c)=>Array.isArray(c)?a.concat(c):a.concat([c]),[])
-    if(x.some(e=>Array.isArray(e)))return f(x)
-    return x
-  }
-  return f(this.slice().map(fn))
+function factory(a){
+  const f=x=>x.map(e=>typeof e==='function'?e():e).reduce((a,c)=>a.concat(Array.isArray(c)?f(c):[c]),[])
+  const r=f(a).sort((a,b)=>a.priority-b.priority).map(e=>e.item),z=[]
+  for(let i=0;i<r.length;i+=10)z.push(r.slice(i,i+10))
+  return z
 }
-
-  console.log((['1', 2, 3, [4, ['4', 5, '5', [6]], 7, [8], 'abc']].mapMany(num => num)))
+console.log(factory([
+  [{item: 'item 3', priority: 3}, {item: 'item 1', priority: 1}],
+  [{item: 'item 2', priority: 2}]
+]))
+console.log( factory([
+  function() {return [
+    {item: 'item 3', priority: 3}, {item: 'item 2', priority: 2}
+  ]},
+  [{item: 'item 1', priority: 1}],
+]))
+console.log( factory([
+  [{item: 'wrench', priority: 1}, {item: 'socket set', priority: 10}],
+  function () { return [{item: 'cake', priority: 8}, {item: 'glue', priority: 5}] },
+]))
