d2b0a8f72129424a41463522fc10ab6c99bcec19
Jhonny <gonzodzen@mail.ru>
2023-05-04 00:27:10 +0300

    Holiday IV - Leg Room

--- main.js
+++ main.js
@@ -1,48 +1,6 @@
-function conquerIsland(m,a=[],r=[],z=[],x=100){
-  m.forEach((e,i)=>e.forEach((u,j)=>u==='p'?z.push([i,j]):u==='u'?a.push([i,j]):u==='m'?r.push([i,j]):0)) 
-  z.forEach(e=>(a.length?a:r).forEach(u=>x=Math.min(Math.abs(e[0]-u[0])+Math.abs(e[1]-u[1]),x)))
-  return a.length===1?a:!a.length&&r.length===1?r:!a.length&&!r.length?[]:(a.length?a:r).filter(u=>z.some(e=>Math.abs(e[0]-u[0])+Math.abs(e[1]-u[1])===x)).sort((a,b)=>a[0]-b[0]||a[1]-b[1])
-}
+legRoom=(a,b,v=b.match(/00/),s=b.replace(/[aeiou]/gi,'').length*2,n=~~(a/100*55)/100*15)=>v?'Jackpot!':s<=n?'ouch':s>n+6?'super comfy':'comfortable'
 
-console.log(conquerIsland([
-  [
-    '~', '~', '~',
-    '~', '~', '~',
-    '~', '~'
-  ],
-  [
-    '~', '~', 'p',
-    '~', '~', '~',
-    '~', '~'
-  ],
-  [
-    '~', '~', '~',
-    '~', '~', 'p',
-    '~', '~'
-  ],
-  [
-    '~', '~', '~',
-    '~', '~', '~',
-    '~', '~'
-  ],
-  [
-    '~', '~', '~',
-    '~', '~', '~',
-    '~', '~'
-  ],
-  [
-    '~', 'm', '~',
-    '~', '~', '~',
-    '~', 'm'
-  ],
-  [
-    '~', '~', '~',
-    '~', '~', '~',
-    '~', '~'
-  ],
-  [
-    '~', '~', '~',
-    '~', 'm', '~',
-    '~', '~'
-  ]
-]))
+
+console.log(legRoom(108,'ifbjejaigghkg'))
+console.log(legRoom(192, 'aelm'))
+console.log(legRoom(168, 'alljhfmd'))
