25635abedc0e41b5e543c06fe132e31decdaf624
Jhonny <gonzodzen@mail.ru>
2023-05-03 11:00:01 +0300

    Verify if it's valid (n x n) Magic Square with custom rules

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-function count(a){
-    for(let i in a){
-        if(!a[i].state)throw Error()
+isValid=(m,n,a=[])=>{
+  const s=x=>x.reduce((a,c)=>a+c,0)
+  const f=x=>{
+    let r=[],w=[],z=[],c=2
+    for(let i=0;i<x[0].length;i++){
+      iAr=[]
+      for(let j=0;j<x.length;j++)iAr.push(x[j][i])
+      r.push(iAr),iAr=[]
     }
-    return Object.entries(a.reduce((a,c)=>{a[c.state]=(a[c.state]||0)+1;return a},{})).map(e=>{return {'state':e[0],'count':e[1]}})
+    while(c){
+      for(let i=0;i<x.length;i++)for(let j=0;j<x[i].length;j++)if(i===j)w.push(x[i][j])
+      z.push(w),w=[],x=x.map(e=>e.reverse()),c--
+    }
+    return r.concat(z)
+  }
+  m.flat().sort((a,b)=>a-b).forEach((e,i,v)=>a.push(v[i+1]-e))
+  return a.slice(0,-1).every(e=>e===n)&&f(m).concat(m).every((e,i,v)=>s(e)===s(v[0]))
 }
 
-  console.log(count([
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'AK' },
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'AR' },
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'OR' },
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'CA' },
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'AK' },
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'OR' },
-    { house: 1, street: 'Binary St.', city: 'Zero Value', state: 'OR' }
-  ]
-  ))
+console.log(isValid([
+                    [22, 1, 16],
+                    [7, 13, 19],
+                    [10, 25, 4],
+                ],
+                3))
