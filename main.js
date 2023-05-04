a08804ceceb11d1ef855f5bd432337e18b779195
Jhonny <gonzodzen@mail.ru>
2023-05-05 01:43:07 +0300

    Mnemonic Lookup

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function send(s){
-  return [...s].map(e=>((e).charCodeAt()).toString(2)).map(e=>'0'.repeat(7-e.length)+e).join``.match(/0+|1+\1*/g).map(e=>+e[0]?'0 '+'0'.repeat(e.length):'00 '+'0'.repeat(e.length)).join` `
+mnemonicLookup=(g,w)=>{
+  return !this[String(g)]?'There is no mnemonic for this.':!this[String(g)][w]?`You're onto something, but still quite far off.`:this[String(g)][w]
 }
-function receive(s,a=s.split` `,r=[]){
-  for(let i=0;i<a.length;i+=2)a[i]=='0'?r.push('1'.repeat(a[i+1].length)):r.push('0'.repeat(a[i+1].length))
-  return r.join``.match(/.{7,7}/g).map(e=>String.fromCharCode(parseInt(e,2))).join``
-}
-
-console.log(receive("0 0 00 0000 0 000 00 0000 0 00"))
-console.log(send("%"))
