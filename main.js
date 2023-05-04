46bc0f6caf987bae4d26c6f0f860748af40db4f6
Jhonny <gonzodzen@mail.ru>
2023-05-04 20:31:47 +0300

    T.T.T.42: Loners Are Disgraceful（孤独的人是可耻的）

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
-duplicated=(a,k)=>a.filter((e,i,v)=>v.some((u,j)=>i!==j&&k.every(z=>e[z]===u[z])))
-
-console.log(duplicated([
-  { x: 1, y: 1 },
-  { x: 2, y: 2 },
-  { x: 1, z: 1 },
-  { x: 1, y: 1, z: 3 },
-],['x','y']))
+function eliminateLonely(s){
+  const o={
+    '{':'{}',
+    '}':'{}',
+    '[':'[]',
+    ']':'[]',
+    '<':'<>',
+    '>':'<>',
+    'b':'bd',
+    'd':'bd',
+    'p':'pq',
+    'q':'pq',
+    '(':'()',
+    ')':'()',
+    'e':'e9',
+    '9':'e9',
+    '\\':'\\/',
+    '\/':'\\/',
+    'n':'m',
+    'v':'w',
+    'V':'W',
+    'o':'8',
+    '3':'8'
+  }
+  return [...s].map((e,i,v)=>{
+    if(e.match(/[Wwm8]/))return e
+    if(e===v[i+1]&&!o[e]||e===v[i-1]&&!o[e]||[e+v[i+1],v[i-1]+e].some(u=>Object.values(o).includes(u)))return e
+    if(o[e])return o[e]
+    return e+e
+  }).join``
+ }
+ console.log(eliminateLonely("1A3uT6C{jb-\\/"))
+ console.log(eliminateLonely('eliminateLonely'))
+ console.log(eliminateLonely("][_Jy-A][[]8" ))
