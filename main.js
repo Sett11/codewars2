d5606e193dbdf6e8d30d0e4ce64e1e2285696139
Jhonny <gonzodzen@mail.ru>
2023-05-05 14:47:15 +0300

    Crazy programmer 01: find SuperMan

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const a='zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA'
-function encode(s){
-  return [...s].map(e=>a.includes(e)?(a.indexOf(e)+1).toString(2).replace(/\d/g,e=>+e?'0':'1')+2:e).join``
-}
-function decode(s){
-  return s.split` `.map(e=>e.split`2`.filter(c=>c).map(u=>u.replace(/\d+/g,z=>z[0].match(/\d/)?a[parseInt(z.replace(/\d/g,x=>+x?0:1),2)-1]:z)).join``).join` `
+function findSuperMan(c,s='superman',f=x=>x.replace(/[^superman]/g,'')){
+  const r=c.toLowerCase().split` `.filter(e=>!e.match(s)&&![...s].some((u,i,v)=>e.match(u+v[i+1]))&&![...s].reverse().some((u,i,v)=>e.match(u+v[i+1]))).join` `
+  return f(r).match(s)||f(r).match([...s].reverse().join``)?'Hi, SuperMan!':'Are you crazy?'
 }
 
-console.log(encode('JXZONB|vY wzX%CLtC* Wxd  HxMAXkHRhy _f_ue!vp* cp r-^ pSxz!'))
-console.log(encode('e,p_F+O WsS _WnzD}(*!EI^o NgMrsVwku( wx_xWA Y(n XmFAL&.'))
-console.log(decode('0100102010012000020000200112 0112001120110200002010002!'))
-console.log(decode('010012,01002_0100002+0110012 000012011120111012 _00001200102020011102}(*!00111120100112^00112 01100020101120101112011020111200000201120111120012( 0112002_0020000120010112 000112(00102 00010200012010000200101120101102&.'))
+
+console.log(findSuperMan('jfc szzw qudzgh pz geddxyrq mqlvgc ya oddn'))
