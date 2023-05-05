586002ea503ab9fe3ee12d352382ea8d9a207ba3
Jhonny <gonzodzen@mail.ru>
2023-05-05 17:04:32 +0300

    Coding 3min: Trypophobia

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-sc=s=>[...s].filter(e=>s.includes(e==e.toLowerCase()?e.toUpperCase():e.toLowerCase())).join``
+sc=(a,f=x=>x.reduce((a,c)=>a.concat(Array.isArray(c)?f(c):[c]),[]),r=x=>Object.entries(x.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b[1]-a[1]).filter((e,i,v)=>e[1]==v[0][1]).map(e=>+e[0]),z=r(f(a)))=>a.map(e=>e.filter(u=>!z.includes(u)))
 
-console.log(sc('monkeyDONKEY'))
+console.log(sc([[3,4,4,5],[3,4,4,5],[3,4,4,5],[3,4,4,5]]))
