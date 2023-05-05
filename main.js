0244fa5d238c2276bbe46f371658835dcb2b47f6
Jhonny <gonzodzen@mail.ru>
2023-05-05 12:14:53 +0300

    T.T.T.59: The language from the distant planet

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-function reverseNumber(s,m=s[0]==='-'?'-':'',n=s.replace(/\D/g,''),q=''){
- const f=(y,x=[...y].map(Number))=>x.slice().sort((a,b)=>a-b).join``===y||x.slice().sort((a,b)=>b-a).join``===y
- for(let i=0;i<=n.length;i++){
-  let t=n.slice(0,i)
-  if(i===n.length&&f(t)){
-    q+=[...t].reverse().join``
-    break
-  }
-  if(!f(t)){
-    q+=[...t.slice(0,-1)].reverse().join``
-    n=n.slice(i-1)
-    i=0
-  }
- }
- while(!+q[0])q=q.slice(1)
- return m+q
+const a='zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA'
+function encode(s){
+  return [...s].map(e=>a.includes(e)?(a.indexOf(e)+1).toString(2).replace(/\d/g,e=>+e?'0':'1')+2:e).join``
+}
+function decode(s){
+  return s.split` `.map(e=>e.split`2`.filter(c=>c).map(u=>u.replace(/\d+/g,z=>z[0].match(/\d/)?a[parseInt(z.replace(/\d/g,x=>+x?0:1),2)-1]:z)).join``).join` `
 }
 
-console.log(reverseNumber('-520025'))
-console.log(reverseNumber('-244969809067815868079677501805063264427988149853'))
+console.log(encode('JXZONB|vY wzX%CLtC* Wxd  HxMAXkHRhy _f_ue!vp* cp r-^ pSxz!'))
+console.log(encode('e,p_F+O WsS _WnzD}(*!EI^o NgMrsVwku( wx_xWA Y(n XmFAL&.'))
+console.log(decode('0100102010012000020000200112 0112001120110200002010002!'))
+console.log(decode('010012,01002_0100002+0110012 000012011120111012 _00001200102020011102}(*!00111120100112^00112 01100020101120101112011020111200000201120111120012( 0112002_0020000120010112 000112(00102 00010200012010000200101120101102&.'))
