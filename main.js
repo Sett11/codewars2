b53b54c941d391e017ef8cf0fec0161c2c3e7bae
Jhonny <gonzodzen@mail.ru>
2023-05-06 13:31:53 +0300

    uniq -c (UNIX style)

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function uniq(a){
-  for(let i=0;i<a.length-1;i++){
-    if(a[i]===a[i+1]){
-      a.splice(i+1,1)
-      i--
-    }
-  }
-  return a
+uniq_c=a=>{
+  if(!a.length)return []
+  const r=a.join``.replace(/(.)\1*/g,e=>e[0]+e.length).match(/.{2,2}/g).map(e=>[e[0],+e[1]])
+  if(r.length===1)r[0][1]=r[0][1]*10+3
+  return r
 }
 
-console.log(uniq(['a','a','b','b','c','a','b','c','c']))
+
+console.log(uniq_c([
+  'a', 'a', 'a', 'a', 'a', 'a', 'a',
+  'a', 'a', 'a', 'a', 'a', 'a', 'a',
+  'a', 'a', 'a', 'a', 'a', 'a', 'a',
+  'a', 'a', 'a', 'a', 'a', 'a', 'a',
+  'a', 'a', 'a', 'a', 'a', 'a', 'a',
+  'a', 'a', 'a'
+]))
