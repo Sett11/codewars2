047311f01165d81fbf90f80b98ce201dfa90af24
Jhonny <gonzodzen@mail.ru>
2023-05-07 14:02:00 +0300

    Trifid Cipher Encoder

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
-function catchThief(s,n=0){
-  for(let i=0;i<s.length;i++){
-    if(s[i].match(/\d/)){
-      let x=(i-+s[i]),y=i+ (+s[i]+1),t=s.slice(x<0?0:x,y),tt=t.replace(/X/g,'#')
-      n+=t.replace(/[^X]/g,'').length
-      s=s.slice(0,x<0?0:x)+tt+s.slice(y)
-    }
-  }
-  return n
+const f=x=>{
+  x=[...x]
+  let r=[],rr=[]
+  for(let i=0;i<x.length;i+=3)r.push(x.slice(i,i+3))
+  for(let i=0;i<r.length;i+=3)rr.push(r.slice(i,i+3))
+  return rr
 }
 
-console.log(catchThief('X5X#3X###XXXX##1#X1X'))
-console.log(catchThief('X1X#2X#XX'))
-console.log(catchThief('#82XXX9XX#X92#X1##61X44XXX2##X'))
-console.log(catchThief('X#X1#X9XX'))
-console.log(catchThief('3X29#4X9X####XX15#9##XX#XX7##XXX58XX#2X'))
+const r=x=>{
+  let a=[],c=0
+    for(let i=0;i<x[c].length;i++){
+      let iAr=[]
+      for(let j=0;j<x.length;j++)iAr.push(x[j][i])
+      a.push(iAr),iAr=[]
+    }
+  return a
+}
+
+const o=(x,y,a=[],c=y)=>{
+  for(let i=0;i<x.length;i++){
+    while(c*3!=x[i].length)c-=1
+    let t=[]
+    for(let j=0;j<x[i].length;j+=c)t.push(x[i].slice(j,j+c))
+    a.push(t),t=[],c=y
+  }
+  return a
+}
+
+function trifidEncode(k,p,d,a=[]){
+  let q=f(k)
+  d=[...d].map(e=>{
+    q.forEach((u,i)=>u.forEach((z,j)=>z.forEach((x,y)=>x===e?e=[i,j,y]:0)))
+    return e
+  })
+  for(let i=0;i<d.length;i+=p)a.push(d.slice(i,i+p))
+  return a.map(e=>r(e).map(u=>u.join``).join``.match(/.{3,3}/g).map(z=>[...z].map(Number))).map(e=>e.map(u=>q[u[0]][u[1]][u[2]]).join``).join``
+}
+
+function trifidDecode(k,p,d,a=[],w=[]){
+  let q=f(k)
+  d=[...d].map(e=>{
+    q.forEach((u,i)=>u.forEach((z,j)=>z.forEach((x,y)=>x===e?e=[i,j,y]:0)))
+    return e
+  })
+  for(let i=0;i<d.length;i+=p)a.push(d.slice(i,i+p))
+  a=a.map(e=>[...e.map(u=>u.join``).join``].map(Number))
+  for(let i=0;i<a.length;i++){
+    let t=[]
+    for(let j=0;j<a[i].length;j+=p)t.push(a[i].slice(j,j+p))
+    w.push(t),t=[]
+  }
+  w=w.map(e=>e.map(u=>u.join``).join``.match(/.{3,3}/g).join``.split``.map(Number))
+  return o(w,p).map(e=>r(e)).map(e=>e.map(u=>q[u[0]][u[1]][u[2]]).join``).join``
+}
+
+console.log(trifidEncode("EPSDUCVWYM+ZLKXNBTFGORIJHAQ",5,"DEFENDTHEEASTWALLOFTHECASTLE"))
+console.log(trifidDecode('EPSDUCVWYM+ZLKXNBTFGORIJHAQ', 7, 'RSQLPNPQBLB'))
