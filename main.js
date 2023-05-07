e248717bcd0593119027942ec8693f8459157d4a
Jhonny <gonzodzen@mail.ru>
2023-05-07 17:48:29 +0300

    Character Counter and Bars Graph

--- main.js
+++ main.js
@@ -1,40 +1,7 @@
-class Scytale{
-  static f(x,y,a=[]){
-    x=[...x]
-    for(let i=0;i<x.length;i+=y)a.push(x.slice(i,i+y))
-    return a.map(e=>{
-      while(e.length<=y)e.push(' ')
-      return e
-    })
-  }
-  static r(x,a=[]){
-    for(let i=0;i<x[0].length;i++){
-      let t=[]
-      for(let j=0;j<x.length;j++)t.push(x[j][i])
-      a.push(t),t=[]
-    }
-    return a
-  }
-  static Decode(s,n){
-    console.log(1)
-    if(s==='CodeWarsScytaleKata'&&n===6)return 'CWSaa oaclt dryea estK'
-    if(s==='ARDgWgIc'&&n===7)return 'ARDgWgIc'
-    if(s==='UWg Wy XUZ cx'&&n===8)return 'UWg Wy XUZ cx'
-    if(!s)return ''
-    return this.r(this.f(s,n)).map(e=>e.join``).join``.trim()
-  }
-  static Encode(s,n,c=s.length){
-    console.log(2)
-    console.log(s,n)
-    if(s==='CodeWarsScytaleKata'&&n===6)return 'CWSaa oaclt dryea estK'
-    if(s==='ARDgWgIc'&&n===7)return 'ARDgWgIc'
-    if(s==='UWg Wy XUZ cx'&&n===8)return 'UWg Wy XUZ cx'
-    if(!s)return ''
-    while(c%n!==0)c++
-    return this.r(this.f(s,c/n)).map(e=>e.join``).join``.trim()
-  }
+function countCharsBarGraph(s,w){
+  return Object.entries([...s.toLowerCase().replace(/[^A-Za-z]/g,'')].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).sort((a,b)=>b[1]-a[1]||a[0].localeCompare(b[0])).map((e,i,v)=>!i?[...e,'#'.repeat(w)]:[...e,'#'.repeat(~~(e[1]/v[0][1]*w))]).map(e=>[e[0],e[2]].join`:`).join`\n`
 }
 
-console.log(Scytale.Encode('CodeWarsScytaleKata',6))
-
-console.log(Scytale.Encode('cGZ PVZU 6', 6))
+console.log(countCharsBarGraph('codeWARs:ssscdo....:::eocodeawrRRssSww:aaAA',37))
+console.log(1)
+console.log('s:#####################################\na:###############################\no:#####################\nr:#####################\nw:#####################\nc:###############\nd:###############\ne:###############')
