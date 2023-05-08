85b86920180164da2b034e869f8b0674a890c9a0
Jhonny <gonzodzen@mail.ru>
2023-05-08 15:46:10 +0300

    Second Variation on Caesar Cipher

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-resolver=(g,s,b=[...s].reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))=>{
-  const r=[...g].map((e,i)=>{
-    if(e===s[i]){
-      b[e]-=1
-      return '&'
+const a='abcdefghijklmnopqrstuvwxyz',b='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+function encodeStr(s,h,q=[]){
+  let r=s.split` `.map(e=>[...e].map(u=>!u.match(/[a-z]/i)?u:a.includes(u)?a[(a.indexOf(u)+h)%a.length]:b[(b.indexOf(u)+h)%b.length]).join``).join` `
+  r=s[0].toLowerCase()+a[a.indexOf(r[0].toLowerCase())]+r
+  let l=Math.ceil(r.length/5)
+  for(let i=0;i<r.length;i+=l)q.push(r.slice(i,i+l))
+  return q
+}
+function decode(w,o=w[0][0]+w[0][1],h=a.indexOf(o[1])-a.indexOf(o[0])){
+  w=w.join``.slice(2)
+  return w.split` `.map(e=>[...e].map(u=>{
+    if(!u.match(/[a-z]/i))return u
+    if(a.includes(u)){
+      u=(a.indexOf(u)-h)%a.length
+      return u<0?a[u+a.length-1]:a[u]
     }
-   return e
-  })
-  return r.map(e=>{
-    if(e==='&')return 'g'
-    if(b[e]){
-      b[e]-=1
-      return 'y'
+    if(b.includes(u)){
+      u=(b.indexOf(u)-h)%b.length
+      return u<0?b[u+b.length-1]:b[u]
     }
-    return 'b'
-  }).join``
+  }).join``).join` `
 }
 
-console.log(resolver( 'wooer','ivory'))
+console.log(encodeStr("I should have known that you would have a perfect answer for me!!!",1))
+console.log(decode([
+  'ihH gzud roqdzc lx cqdz',
+  'lr tmcdq xntq edds; Sqd',
+  'zc rneskx adbztrd xnt s',
+  'qdzc nm lx cqdzlr. Vhkk',
+  'hzl A Xdzsr (1865-1939)'
+]))
