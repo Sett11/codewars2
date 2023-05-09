28bcd867e1a3c6a5c298e62e1d5c23d42969f67b
Jhonny <gonzodzen@mail.ru>
2023-05-09 16:28:25 +0300

    How many elephants can the spider web hold?

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-function fillWater(a,c=0){
- a=a.map(e=>{
-  let t=e.slice(0,6)
-  if(t==='robber'){
-    let z=e
-    while(e.length<30)e+='water'
-    e=e.slice(0,30)
-    c+=e.length-z.length
+function breakTheWeb(s,w,a=[],c=0,t=1000,r=[]){
+  const f=x=>x.reduce((a,c)=>a+c,0)
+  while(f(r.flat())<s){
+    a.push(t)
+    c++
+    if(f(a)>s){
+      a.pop()
+      r.push(a)
+      break
+    }
+    if(f(a)===s){
+      r.push(a)
+      break
+    }
+    if(c===w){
+      c=0,w--,t+=1000,r.push(a),a=[]
+    }
+    if(!w)break
   }
-  return e
- })
- a.push(`${c} litre${c>1?'s':''}`)
- return a
+  r=r.flat()
+  return f(r)>s?r.length-1:r.length
 }
-console.log(fillWater(["robber-A","robber-123","robber-xyz","stone","grass","tortoise",""]))
+console.log(breakTheWeb(532000,5))
