5cdfa3fd6b7c7c1037d3fd124d1c6686049cc21e
Jhonny <gonzodzen@mail.ru>
2023-05-09 13:36:54 +0300

    The Lamp: Revisited

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-f=x=>{
-  for(let i=0;i<x.length;i++)x[i][i]='&'
-  return x.map(e=>e.slice(0,e.indexOf('&'))).filter(e=>e.length)
+class Lamp{
+  constructor(c,o=false){
+    this.color=c
+    this.on=o
+  }
+  state(){
+    return `The lamp is ${this.on?'on':'off'}.`
+  }
+  toggleSwitch(){
+    this.on=(this.on?false:true)
+  }
 }
-upperTriangular=(m,q=f(m))=>q.every(e=>e.every(u=>!u))
-lowerTriangular=(m,q=f(m.map(e=>e.reverse()).reverse()))=>q.every(e=>e.every(u=>!u))
-
-console.log(upperTriangular([
-  [1,1,1],
-  [0,1,1],
-  [0,0,1]]))
-console.log(lowerTriangular([
-  [1,0,0],
-  [1,1,0],
-  [1,1,1]]))
