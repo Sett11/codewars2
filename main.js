243f1ac19c75ebabb85f921bf2c95b904209f5a2
Jhonny <gonzodzen@mail.ru>
2023-05-09 14:44:41 +0300

    myjinxin's Fairy tales #003 : Ali Baba and the Forty Thieves II

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-class Lamp{
-  constructor(c,o=false){
-    this.color=c
-    this.on=o
-  }
-  state(){
-    return `The lamp is ${this.on?'on':'off'}.`
-  }
-  toggleSwitch(){
-    this.on=(this.on?false:true)
-  }
+function sameMark(s,a,c=[...s].map((e,i)=>[e,i]).filter(e=>!e[0].match(/[a-z]/i))){
+ return a.map(e=>{
+  for(let i=0;i<c.length;i++)e=e.slice(0,c[i][1])+c[i][0]+e.slice(c[i][1])
+  return e
+ })
 }
+console.log(sameMark("A-li--Bab---a",["JohnSmith","MichaelJackson","JamesBond"]))
