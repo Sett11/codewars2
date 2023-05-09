f5cbb594a48bf0b2030c2eee05d209a999dc06d9
Jhonny <gonzodzen@mail.ru>
2023-05-09 16:45:41 +0300

    One Line Task: Remove Zeros

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-function breakTheWeb(s,w,a=[],c=0,t=1000,r=[]){
-  const f=x=>x.reduce((a,c)=>a+c,0)
-  while(f(r.flat())<s){
-    a.push(t)
-    c++
-    if(f(a)>s){
-      a.pop()
-      r.push(a)
-      break
-    }
-    if(f(a)===s){
-      r.push(a)
-      break
-    }
-    if(c===w){
-      c=0,w--,t+=1000,r.push(a),a=[]
-    }
-    if(!w)break
-  }
-  r=r.flat()
-  return f(r)>s?r.length-1:r.length
-}
-console.log(breakTheWeb(532000,5))
+removeZeros=a=>eval('['+/[1-9].*[1-9]/.exec(a)+']')
+
+console.log(removeZeros([0, 9, 5, 0, 0, 0, 0, 2, 0, 0]))
