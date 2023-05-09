957a1d28e31c16e8a488a7f353254539bee7f1de
Jhonny <gonzodzen@mail.ru>
2023-05-09 20:50:48 +0300

    Abundant Array

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-searchDisable=l=>{
-  const f=x=>{if(x<2)return 1==2;if(x===2)return 1==1;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return 1==2}return 1==1}
-  const r=e=>f(+e)&&e.length===4&&e[2]==='2'||e[2]==='3'
-  return l.slice(0,4)==='2031'?'no match continue':Object.entries(l.split` `.reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{})).filter(e=>r(e[0])&&e[1]>3).map(e=>e[1]).reduce((a,c)=>a+c,0)>50?'match disable bot':'no match continue'
+function abundance(n,a=[],c=12){
+  const f=(x,a=[])=>{
+    for(let i=1;i<x/2+1;i++)if(x%i===0)a.push(i)
+    return a
+  }
+  while(a.length<n){
+    let t=f(c).reduce((a,c)=>a+c,0)
+    if(t>c)a.push([c,t-c])
+    c++
+  }
+  return a.sort((a,b)=>a[1]-b[1]||a[0]-b[0]).map(e=>e[0])
 }
 
-
-console.log(searchDisable('2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031 2031'))
+console.log(abundance(51))
