db5a6a5c0dcdf4c652d353e4c0e7c585dcdf610e
Jhonny <gonzodzen@mail.ru>
2023-05-10 19:38:51 +0300

    Automaton (Part 2)

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-eqAll=a=>{
-  if(Array.isArray(a))return a.every(e=>e===a[0])
-  if(typeof a==='string'||typeof a==='number')return [...a+''].every((e,i,v)=>e===v[0])
-  if(typeof a==='object'&&a.size!==undefined)return a.size<2
-  if(JSON.stringify(a)==='{"listOf":"aaa"}'||JSON.stringify(a)==='{"left":{"left":{},"leaf":0,"right":{"left":{},"leaf":0,"right":{}}},"leaf":0,"right":{"left":{},"leaf":0,"right":{}}}'||JSON.stringify(a)==='{}'||'{"listOf":"000"}'===JSON.stringify(a)||'{"listOf":""}'===JSON.stringify(a))return true
-  if(JSON.stringify(a)==='"00"'||JSON.stringify(a)==='{"0":0,"1":0,"2":0,"length":0,"listOf":"123"}'||JSON.stringify(a)==='{"listOf":"lots of zeroes, a one, and infinite zeroes"}'||JSON.stringify(a)==='{"listOf":"012.."}'||JSON.stringify(a)==='{"listOf":"abc"}'||JSON.stringify(a)==='{"listOf":"123"}'||JSON.stringify(a)==='{"left":{"left":{},"leaf":1,"right":{"left":{},"leaf":2,"right":{}}},"leaf":0,"right":{"left":{},"leaf":3,"right":{}}}')return false
+function automaton(e){
+	if (typeof e==="string") {
+		return e.replace(/[^aeiuo]/ig, "").length
+	} else if(typeof e==="number") {
+		if(e<2||!Number.isInteger(e))return false
+    if(e===2)return true
+		for (var i = 2; i < Math.sqrt(e)+1; i++) if (e % i===0) return false;
+		return true
+	} else if(typeof e==="object"){
+		var a={}
+		for (var k in e)a[e[k]+'']=k
+		return a
+	}
 }
-
-console.log(eqAll('aaa'))
