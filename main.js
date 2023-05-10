1fa9fe84ffbff46dcdf7101ff2f75e95b9dc72bd
Jhonny <gonzodzen@mail.ru>
2023-05-10 10:51:36 +0300

    An 'orrible expression on 'is face

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-function shouldIBeTired(d){
-  return d.filter(e=>e[1]==='Drive').map(e=>e[0].split`-`.map(u=>u.split`:`.map(Number))).map(e=>(e[1][0]-e[0][0])*60+(e[1][1]-e[0][1])).reduce((a,c)=>a+c,0)/60<9
-}
-console.log(shouldIBeTired([ ["7:00-10:30","Drive"],
-["10:30-10:45","Rest"],
-["10:45-11:45","Drive"],
-["11:45-12:15","Rest"],
-["12:15-16:45","Drive"],
-["16:45-20:15","Work"]]))
+const cockney = /[a-z]+/ig
+const replacement = e=>e.replace(/^h/i,'`')
+
+console.log("How's it hanghing?".replace(cockney,replacement))
