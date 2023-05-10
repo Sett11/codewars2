d137d8f6172a7ddb586c2fa4226af779ca30eb5f
Jhonny <gonzodzen@mail.ru>
2023-05-10 17:13:00 +0300

    Are all elements equal?

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-divide2357=(a,f=e=>e%2===0?f(e/2):e%3===0?f(e/3):e%5===0?f(e/5):e%7===0?f(e/7):e)=>a.map(e=>f(e))
+Object.defineProperty(String.prototype,"eqAll",{value:function eqAll(){
+  return new Set(this).size<2
+},enumerable:false})
 
-console.log(divide2357([1,2,3,4,5,6]))
+Object.defineProperty(Array.prototype,"eqAll",{value:function eqAll(){
+  return this.every((e,i,v)=>e===v[0])
+},enumerable:false})
