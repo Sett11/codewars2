3d2fcbe02a0fb3a0d6fe43712a36fecc506d6867
Jhonny <gonzodzen@mail.ru>
2023-05-11 00:16:55 +0300

    1 Two 3 Four 5!

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-base64toBase10=(s,l='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/')=>[...s].reverse().map((e,i)=>l.indexOf(e)*(64**i)).reduce((a,c)=>a+c,0)
+conv=(s,o={'0':'zero','1':'one','2':'two','3':'three','4':'four','5':'five','6':'six','7':'seven','8':'eight','9':'nine'})=>{
+  const r=(x,y,z,a=[])=>{
+    let r=(o[x].repeat(y)).slice(0,y)
+    for(let i=0;i<r.length;i+=o[x].length)a.push(r.slice(i,i+o[x].length))
+     if(z)a=a.map((e,i)=>i%2===0?e.toLowerCase():e.toUpperCase()).join``
+     if(!z)a=a.map((e,i)=>i%2===0?e.toUpperCase():e.toLowerCase()).join``
+     return a
+  }
+  const f=(x,y)=>[...x].map((e,i)=>y&&+e%2===0?r(e,i+1,y):!y&&+e%2!==0?r(e,i+1,y):e).join``
+  return f(s,s.length%2==0)
+}
 
-
-console.log(base64toBase10('WIN'))
+console.log(conv('1101'))
+console.log(conv('54563'))
+console.log(conv('47309534'))
+console.log(conv('15795379351687'))
