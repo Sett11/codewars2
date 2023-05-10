b331b3c2e38d46b8688052b2a57ba46dbb92bd73
Jhonny <gonzodzen@mail.ru>
2023-05-10 09:39:45 +0300

    Alternating Loops

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-add=(a,b)=>!b?a:add(a^b,(a&b)<<1)
+function combine(...a){
+  const c=[...a],r=[]
+  for(let i=0;i<Math.max(...c.map(e=>e.length));i++)for(let j=0;j<c.length;j++)if(a[j][i]!==undefined)r.push(a[j][i])
+  return r
+}
 
-console.log(add(5,7))
+console.log(combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8]))
