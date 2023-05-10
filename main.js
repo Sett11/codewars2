8587d01a47ddf246d87a0eed85892ec1eb161a02
Jhonny <gonzodzen@mail.ru>
2023-05-11 00:52:08 +0300

    Berserk rater: CG Vs. Clang

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-conv=(s,o={'0':'zero','1':'one','2':'two','3':'three','4':'four','5':'five','6':'six','7':'seven','8':'eight','9':'nine'})=>{
-  const r=(x,y,z,a=[])=>{
-    let r=(o[x].repeat(y)).slice(0,y)
-    for(let i=0;i<r.length;i+=o[x].length)a.push(r.slice(i,i+o[x].length))
-     if(z)a=a.map((e,i)=>i%2===0?e.toLowerCase():e.toUpperCase()).join``
-     if(!z)a=a.map((e,i)=>i%2===0?e.toUpperCase():e.toLowerCase()).join``
-     return a
-  }
-  const f=(x,y)=>[...x].map((e,i)=>y&&+e%2===0?r(e,i+1,y):!y&&+e%2!==0?r(e,i+1,y):e).join``
-  return f(s,s.length%2==0)
-}
+berserkRater=(a,r=a.reduce((a,c)=>c.match(/clang/i)?a+=5:c.match(/cg/i)?a-=2:a-=1,0))=>r<0?'worstest episode ever':r>10?'bestest episode ever':r+''
 
-console.log(conv('1101'))
-console.log(conv('54563'))
-console.log(conv('47309534'))
-console.log(conv('15795379351687'))
+console.log(berserkRater(["is this the CG from a P2 game?","Hell, no! Even the CG in the Dreamcast game was more fluid than this!","Well, at least Gatsu does his clang even against a mere rabbit", "Hey, Cosette was not in this part of the story!", "Ops, everybody dead again! Well, how boring..."]))
