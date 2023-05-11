a1436a29a0010c9175eb3e2c595b2c6c99a3d8c3
Jhonny <gonzodzen@mail.ru>
2023-05-11 21:15:58 +0300

    The Great Digital Conflict

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-isbnConverter=(s,n='978',c=[...n+s.slice(0,-2).replace(/[^\d]/g,'')].map((e,i)=>i%2===0?+e:+e*3).reduce((a,c)=>a+c,0)%10)=>n+'-'+s.slice(0,-1)+(!c?c:10-c)
+function battleCodes(l,n,f=x=>x.filter(e=>e>0)){
+    l=[...l].map(e=>parseInt(e,36)-9),n=[...n+''].map(Number)
+    if(!l.length||!n.length)return 'Peace'
+    while(l.length&&n.length){
+        let t=n[0],p=l[l.length-1]
+        l[l.length-1]-=t
+        l[l.length-2]-=t
+        n[0]-=p
+        l=f(l),n=f(n)
+    }
+    return l.length?l.map(e=>String.fromCharCode(e+96)).join``:n.length?n.join``:'Draw'
+}
 
-
-console.log(isbnConverter('1-85326-158-0'))
+console.log(battleCodes('abc', '123'))
