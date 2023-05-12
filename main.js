93b6288b6ab30b0333f093814e2bba9293e16c58
Jhonny <gonzodzen@mail.ru>
2023-05-12 22:53:38 +0300

    Deep Freeze

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function makeLooper(s){
-    let i=0
-    return function(){
-        let t=s[i%s.length]
-        i+=1
-        return t
-    }
+Object.deepFreeze=function(o){
+     Object.freeze(o)
+     for(let i in o){
+        if(typeof o[i]==='object'){
+            return Object.deepFreeze(o[i])
+        }
+     }
 }
-abc = makeLooper('abc')
-console.log(abc())
-console.log(abc())
-console.log(abc())
-console.log(abc())
+  const a={ b: [42, { x: 9 }] }
+  Object.deepFreeze(a)
+  a.b=42
+  a.b[1]=42
+  console.log(a.b)
