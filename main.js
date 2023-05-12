b52fc857777201d955436ec1976fd05622d78e14
Jhonny <gonzodzen@mail.ru>
2023-05-13 00:13:15 +0300

    Defend your castle

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-Object.deepFreeze=function(o){
-     Object.freeze(o)
-     for(let i in o){
-        if(typeof o[i]==='object'){
-            return Object.deepFreeze(o[i])
-        }
-     }
+function defendable(a,d){
+    const f=x=>Object.entries(x).map(e=>e[0]==='archer'?e[1]*8:e[0]==='footSoldier'?e[1]*10:e[0]==='cavalry'?e[1]*14:e[0]==='artillery'?e[1]*30:e[1]).filter(e=>+e===+e).reduce((a,c)=>a+c,0)
+    let one=f(a),two=f(d),m=1
+    if(a.strategy==='siege'){
+        if(d.resources==='low')m*=0.5
+        if(d.resources==='medium')m*=0.75
+    }
+    if(d.reinforcements===null)m*=0.9
+    if(d.reinforcements){
+        if(d.reinforcements.inTime)m*=1.5
+        if(!d.reinforcements.inTime)m*=0.75
+    }
+    two*=m
+    if(d.reinforcements&&d.reinforcements.inTime)two+=f(d.reinforcements)
+    return one<two
 }
-  const a={ b: [42, { x: 9 }] }
-  Object.deepFreeze(a)
-  a.b=42
-  a.b[1]=42
-  console.log(a.b)
+
+console.log(defendable({
+	armyHp: 500,
+	archer: 13,
+	footSoldier: 25,
+	cavalry: 20,
+	artillery: 2,
+	strategy: "siege"
+},{
+    armyHp: 300,
+      archer: 50,
+      footSoldier: 30,
+      artillery: 10,
+      resources: "low",
+    reinforcements: {
+      battleValue: 500,
+      inTime: true
+    }
+  }))
