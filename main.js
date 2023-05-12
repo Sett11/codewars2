e28e42eca5ee3f3e5ce2e304a7280de3ba35b8a0
Jhonny <gonzodzen@mail.ru>
2023-05-13 00:50:41 +0300

    Implementing the Array Object

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
-function defendable(a,d){
-    const f=x=>Object.entries(x).map(e=>e[0]==='archer'?e[1]*8:e[0]==='footSoldier'?e[1]*10:e[0]==='cavalry'?e[1]*14:e[0]==='artillery'?e[1]*30:e[1]).filter(e=>+e===+e).reduce((a,c)=>a+c,0)
-    let one=f(a),two=f(d),m=1
-    if(a.strategy==='siege'){
-        if(d.resources==='low')m*=0.5
-        if(d.resources==='medium')m*=0.75
+class List{
+    constructor(...a){
+        this.arr=[...a]
+        this.length=this.arr.length
+        for(let i=0;i<this.arr.length;i++){
+            this[i]=this.arr[i]
+        }
+        return this
     }
-    if(d.reinforcements===null)m*=0.9
-    if(d.reinforcements){
-        if(d.reinforcements.inTime)m*=1.5
-        if(!d.reinforcements.inTime)m*=0.75
+    push(...v){
+        this.arr.push(...v)
+        for(let i=0;i<this.arr.length;i++)this[i]=this.arr[i]
+        this.length=this.arr.length
+        return this.arr.length
     }
-    two*=m
-    if(d.reinforcements&&d.reinforcements.inTime)two+=f(d.reinforcements)
-    return one<two
+    pop(){
+       delete this[this.arr.length-1]
+       let t=this.arr.pop()
+       this.length=this.arr.length
+       return t
+    }
+    shift(){
+        delete this[0]
+        let t=this.arr.shift()
+        this.length=this.arr.length
+        return t
+    }
+    unshift(...v){
+        this.arr.unshift(...v)
+        for(let i=0;i<this.arr.length;i++)this[i]=this.arr[i]
+        this.length=this.arr.length
+        return this.arr.length
+    }
+    filter(f){
+        return this.arr.filter(f) 
+    }
 }
 
-console.log(defendable({
-	armyHp: 500,
-	archer: 13,
-	footSoldier: 25,
-	cavalry: 20,
-	artillery: 2,
-	strategy: "siege"
-},{
-    armyHp: 300,
-      archer: 50,
-      footSoldier: 30,
-      artillery: 10,
-      resources: "low",
-    reinforcements: {
-      battleValue: 500,
-      inTime: true
-    }
-  }))
+const r=new List(1,2,3,4)
+
+console.log(r.push(10))
+console.log(r.length)
