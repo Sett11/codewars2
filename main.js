662190888c23b6d4c1f8224bce6cebfe0b302291
Jhonny <gonzodzen@mail.ru>
2023-05-14 13:21:35 +0300

    Help Suzuki purchase his Tofu!

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function spiralPermutations(s,a=[s]){
-  while(1){
-    let t=[...a[a.length-1]],c=''
-    while(t.length){
-      c+=t.pop()||''
-      c+=t.shift()||''
-    }
-    if(c===s)break
-    a.push(c)
-  }
-  return a
+buyTofu=(c,b)=>{
+  b=b.split` `
+  let x=b.filter(e=>e==='mon').length,y=b.filter(e=>e==='monme').length,z=y*60+x,r=[x,y,z],w=~~(c/60)
+  if(w>y)w=y
+  c-=w*60
+  if(z<c||x<c)return 'leaving the market'
+  r.push(c+w)
+  return r
 }
 
-console.log(spiralPermutations('ABCDE'))
+console.log(buyTofu(124,'mon mon mon mon mon apple mon mon mon mon mon mon mon monme mon mon monme mon mon mon mon cloth monme mon mon mon mon mon mon mon mon cloth mon mon monme mon mon mon mon monme mon mon mon mon mon mon mon mon mon mon mon mon mon'))
+console.log(buyTofu(124,'mon mon mon mon mon apple mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon monme mon mon mon mon cloth mon mon mon mon mon mon mon mon mon cloth mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon mon'))
