fcd443153506e767f3990347451d60f74b26eb0a
Jhonny <gonzodzen@mail.ru>
2023-05-15 15:04:27 +0300

    2 Arrays 1 Sort

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function getMostProfitFromStockQuotes(a,t=[],s=0){
-  for(let i=0;i<a.length;i++){
-    if(a[i]!==Math.max(...a.slice(i)))t.push(a[i])
-    else{
-      t.forEach(e=>s+=a[i]-e)
-      t=[]
-    }
-  }
-  return s
+function linkedSort(a,b,c,w=a.slice(),q=c?a.sort(c):a.sort(),r=[]){
+  z=b.map((e,i)=>[e,w[i]])
+  q.forEach(e=>z.forEach(u=>u[1]===e?r.push(u[0]):0))
+  b.splice(0,z.length,...[...new Set(r)])
+  return a
 }
 
-console.log(getMostProfitFromStockQuotes([ 1, 6, 5, 10, 8, 7 ]))
+console.log(linkedSort([1,6,5,0],['house', 'car','pen','jeans'],(a,b)=>a-b))
