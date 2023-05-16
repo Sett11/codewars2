3ad7e82a620365ea2d1a0f02350fc79e6b5aa317
Jhonny <gonzodzen@mail.ru>
2023-05-16 20:16:02 +0300

    Happy Coding : a Spy On the Train

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-isFloat=(n,f=x=>x.reduce((a,c)=>a.concat(!c?[c]:Array.isArray(c)?f(c):typeof c==='object'?f(Object.values(c)):[c]),[]),r=x=>parseFloat(x))=>{  
-    return f([n]).some(e=>!Number.isInteger(r(e))&&r(e)===r(e)&&(e+'').replace(/[^\.]/g,'').length===1&&!(e+'').match(/[^0-9\. \-]/))
+function lengthOfRailway(s,c=0,v=0){
+  s=s.replace(/呜呜呜|哐当/g,' $& ').split` `.filter(e=>e==='哐当'||e==='呜呜呜')
+  for(let i=0;i<s.length;i++){
+    if(s[i]==='呜呜呜')v+=1
+    if(s[i]==='哐当')c+=v%2==0?10:20
   }
+  return c
+}
 
-console.log(isFloat({a:[{b:[[1.1]]},1]}))
-console.log(isFloat(1.1))
-console.log(isFloat('1.1'))
-console.log(isFloat(/1.1/))
-console.log(isFloat('=1.1'))
-console.log(isFloat(null))
+console.log(lengthOfRailway("呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当"))
+console.log(lengthOfRailway("呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当"))
+console.log(lengthOfRailway("呜呜呜哐当哐当面包饮料矿泉水了啊，哐当桶面火腿肠茶叶蛋了啊哐当哐当呜呜呜哐当哐当哐当北京站到了，下车的旅客请带好您的行李，准备下车哐当哐当"))
