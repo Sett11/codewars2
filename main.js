35d07c59bb789dfabfe3c152ad9e78db8bfddce4
Jhonny <gonzodzen@mail.ru>
2023-05-16 23:01:40 +0300

    WOW-signal!

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function lengthOfRailway(s,c=0,v=0){
-  s=s.replace(/呜呜呜|哐当/g,' $& ').split` `.filter(e=>e==='哐当'||e==='呜呜呜')
-  for(let i=0;i<s.length;i++){
-    if(s[i]==='呜呜呜')v+=1
-    if(s[i]==='哐当')c+=v%2==0?10:20
-  }
-  return c
+function WOWdecoder(s,r=s.split` `.filter(e=>e&&e.match(/[7-9A-F]/)&&!e.match(/[0-6J-Z]/))){
+  return [...s].some(e=>parseInt(e,36)-10>5)||r.length>3&&r.every(e=>e.length<6)||s.match(/[J-Z]/)?'blocked jam attempt':!r.length?'no signals found':r.join` `
 }
 
-console.log(lengthOfRailway("呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当"))
-console.log(lengthOfRailway("呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当呜呜呜哐当哐当哐当哐当哐当"))
-console.log(lengthOfRailway("呜呜呜哐当哐当面包饮料矿泉水了啊，哐当桶面火腿肠茶叶蛋了啊哐当哐当呜呜呜哐当哐当哐当北京站到了，下车的旅客请带好您的行李，准备下车哐当哐当"))
+console.log(WOWdecoder(" 1  2  1 221 3 ABC99F   23 BBC   "))
+console.log(WOWdecoder("AFC 1  633 1 22 899 AAA9  1  2 C 99"))
+console.log(WOWdecoder("  445 295 12 23 6654 1  11 1   3"))
+console.log(WOWdecoder("   1   1  3   2 2      3   4   2 1     H    3  1 2   "))
