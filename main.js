035e493c99fa9eb9c14fd24350e7d1bae4608076
Jhonny <gonzodzen@mail.ru>
2023-05-18 00:57:21 +0300

    One is the loneliest number

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-function solve(a,c=0,s=0){
-  a=a.sort((a,b)=>b-a)
-    while(s!==a[0]){
-      [x,y,z]=a
-      if(z)z--
-      else y--
-      c++,x--
-      if(y>x)a=[y,x,z]
-      else a=[x,y,z]
-      s=x+y+z
-    }
-  return c
+function loneliest(m,n=[...(m+'')].map(Number),c=100){
+    n=n.map((e,i,v)=>[e,(v.slice((i-e)<0?0:i-e,i).concat(v.slice(i+1,i+e+1))).reduce((a,c)=>a+c,0)])
+    n.forEach(e=>c=Math.min(c,e[1]))
+    return n.some(e=>e[0]===1&&e[1]===c)
 }
+
+console.log(loneliest(34315))
