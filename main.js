a8ead084beb24c920caba52a3a83adc00a6d7cf9
Jhonny <gonzodzen@mail.ru>
2023-05-19 01:16:16 +0300

    Regex Tic Tac Toe Win Checker

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-function solveTTT(b,a=[],d=[],x=2){
-  b=b.map((e,i)=>[e,i])
+function regexTicTacToeWinChecker(c,b=[...c],a=[],d=[],x=2){
   for(let i=0;i<b.length;i+=3)a.push(b.slice(i,i+3))
-  let c=a[0].map((e,i)=>a.map((u,j,v)=>u[i]))
+  let w=a[0].map((e,i)=>a.map(u=>u[i]))
   while(x){
     let t=[]
     for(let i=0;i<a.length;i++)t.push(a[i][i])
     d.push(t),a=a.map(e=>e.reverse()),x--
   }
-  let r=(a.concat(c).concat(d)).filter(e=>e.flat().filter(u=>u==='X').length===2&&!e.flat().includes('O'))
-  if(r.length){
-    return r.flat().filter(e=>!e[0])[0][1]
-  }
-  return a.flat().find(e=>!e[0])[1]
+  return (a.concat(w).concat(d)).some(e=>e.every((u,i,v)=>u===v[0]&&u!='-'))
 }
 
-console.log(solveTTT(['X', 'X', 'O', 'O', 'X', '', '', '', '']))
-console.log(solveTTT(['', '', '', 'O', '', '', 'X', '', '']))
+console.log(regexTicTacToeWinChecker("O--OO-XXX"))
+console.log(regexTicTacToeWinChecker("OXO--XXO-"))
