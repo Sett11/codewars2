218e9243b47b437a2822e52aabd3025176ce064e
Jhonny <gonzodzen@mail.ru>
2023-05-19 00:03:50 +0300

    Connected blocks

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function loneliest(m,n=[...(m+'')].map(Number),c=100){
-    n=n.map((e,i,v)=>[e,(v.slice((i-e)<0?0:i-e,i).concat(v.slice(i+1,i+e+1))).reduce((a,c)=>a+c,0)])
-    n.forEach(e=>c=Math.min(c,e[1]))
-    return n.some(e=>e[0]===1&&e[1]===c)
-}
+function solution(s,a=[]){
+    if(!s||s.match(/[^0-9,]/))return 0
+    if(s.length===1)return 1
+    let m=[...new Set(s.split`,`)].filter(e=>e.length===2).map(e=>[+e[0],+e[1]]).sort((a,b)=>a[0]-b[0]||a[1]-b[1])
+    for(let i=0;i<m.length;i++){
+      let t=[m[i]]
+      for(let j=0;j<m.length;j++){
+        if(t.some(e=>e[0]===m[j][0]&&Math.abs(m[j][1]-e[1])===1||e[1]===m[j][1]&&Math.abs(m[j][0]-e[0])===1)&&m[j][0]!==undefined){
+          t.push(m[j])
+          m[j]=[]
+          j=0
+        }
+      }
+      if(t[0][0]!==undefined)a.push(t)
+    }
+    return Math.max(...a.map(e=>[...new Set(e)].length))
+  }
 
-console.log(loneliest(34315))
+console.log(solution('18,00,95,40,36,26,57,48,54,65,76,87,97,47,00'))
+console.log(solution('18,00,95,40,36,26,57,48,54,65,76,87,97,47,00,46'))
