0678a964ff3d31776b12a542dbc4d0be8753c809
Jhonny <gonzodzen@mail.ru>
2023-05-19 00:57:34 +0300

    Tic-Tac-Toe

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-function solution(s,a=[]){
-    if(!s||s.match(/[^0-9,]/))return 0
-    if(s.length===1)return 1
-    let m=[...new Set(s.split`,`)].filter(e=>e.length===2).map(e=>[+e[0],+e[1]]).sort((a,b)=>a[0]-b[0]||a[1]-b[1])
-    for(let i=0;i<m.length;i++){
-      let t=[m[i]]
-      for(let j=0;j<m.length;j++){
-        if(t.some(e=>e[0]===m[j][0]&&Math.abs(m[j][1]-e[1])===1||e[1]===m[j][1]&&Math.abs(m[j][0]-e[0])===1)&&m[j][0]!==undefined){
-          t.push(m[j])
-          m[j]=[]
-          j=0
-        }
-      }
-      if(t[0][0]!==undefined)a.push(t)
-    }
-    return Math.max(...a.map(e=>[...new Set(e)].length))
+function solveTTT(b,a=[],d=[],x=2){
+  b=b.map((e,i)=>[e,i])
+  for(let i=0;i<b.length;i+=3)a.push(b.slice(i,i+3))
+  let c=a[0].map((e,i)=>a.map((u,j,v)=>u[i]))
+  while(x){
+    let t=[]
+    for(let i=0;i<a.length;i++)t.push(a[i][i])
+    d.push(t),a=a.map(e=>e.reverse()),x--
   }
+  let r=(a.concat(c).concat(d)).filter(e=>e.flat().filter(u=>u==='X').length===2&&!e.flat().includes('O'))
+  if(r.length){
+    return r.flat().filter(e=>!e[0])[0][1]
+  }
+  return a.flat().find(e=>!e[0])[1]
+}
 
-console.log(solution('18,00,95,40,36,26,57,48,54,65,76,87,97,47,00'))
-console.log(solution('18,00,95,40,36,26,57,48,54,65,76,87,97,47,00,46'))
+console.log(solveTTT(['X', 'X', 'O', 'O', 'X', '', '', '', '']))
+console.log(solveTTT(['', '', '', 'O', '', '', 'X', '', '']))
