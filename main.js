2b97995d9cbc4bc8a5e5e2d1f9a674834f66e330
Jhonny <gonzodzen@mail.ru>
2023-05-19 15:16:18 +0300

    Bingo!

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-function getCard(){
-  const a=[[1,15,'B'],[16,30,'I'],[31,45,'N'],[46,60,'G'],[61,75,'O']],r=[]
-  for(let i=0;i<a.length;i++){
+function bingo(c,n,o=c[0].map((e,i)=>c.map(u=>u[i])).map(e=>e.slice(1).map(u=>e[0]+u)),v=c[0].map((e,i)=>o.map(u=>u[i])).filter(e=>e),z=2,w=[]){
+  const f=x=>x.reduce((a,c)=>a.concat(Array.isArray(c)?f(c):[c]),[])
+  while(z){
     let t=[]
-    while(t.length<(a[i][2]==='N'?4:5)){
-      let n=a[i][2]+(a[i][0]+Math.floor((Math.random()*15)))
-      if(!t.includes(n))t.push(n)
-    }
-    r.push(...t)
+    for(let i=0;i<o.length;i++)t.push(o[i][i])
+    w.push(t),o=o.reverse(),z--
   }
-  return r
+  const r=(o.concat(v).concat(w)).map(e=>e.includes('NFREE SPACE')?e.slice(0,2).concat(e.slice(3)):e)
+  n=n.filter(e=>f(r).includes(e))
+  return r.some(e=>e.every(u=>n.includes(u)))
 }
 
-console.log(getCard())
+console.log(bingo([
+  ['B', 'I', 'N', 'G', 'O'],
+  [1, 16, 31, 46, 61],
+  [3, 18, 33, 48, 63],
+  [5, 20, 'FREE SPACE', 50, 65],
+  [7, 22, 37, 52, 67],
+  [9, 24, 39, 54, 69]
+],['B1', 'I16', 'N31', 'G46', 'O61']))
+console.log(bingo([
+  ['B', 'I', 'N', 'G', 'O'],
+  [1, 16, 31, 46, 61],
+  [3, 18, 33, 48, 63],
+  [5, 20, 'FREE SPACE', 50, 65],
+  [7, 22, 37, 52, 67],
+  [9, 24, 39, 54, 69]
+],['B1', 'I16', 'N31', 'G46', 'O63', 'O61']))
