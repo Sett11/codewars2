57a4a92c87a3feaac6dec926b3c7c2d9965c5ef5
Jhonny <gonzodzen@mail.ru>
2023-05-19 13:19:35 +0300

    Longest Strict Bouncy Subarray

--- main.js
+++ main.js
@@ -1,42 +1,26 @@
-class Vector{
-  constructor(a){
-    this.a=a
-    return this
+function longestBouncyList(a,r=[]){
+  if(new Set(a).size===1)return [a[0]]
+  const f=x=>{for(let i=0;i<x.length+1;i++){if(x[i-1]>=x[i]&&x[i+1]<=x[i]||x[i-1]<=x[i]&&x[i+1]>=x[i])return false}return true}
+  for(let i=0;i<a.length;i++){
+    for(let j=0;j<a.length+1;j++){
+      if(!f(a.slice(i,j))||j===a.length+1&&f(a.slice(i,j))){
+        r.push(a.slice(i,j-1))
+        break
+      }
+    }
   }
-  add(x,a=[]){
-    if(x.a.length!==this.a.length)throw Error()
-    this.a.forEach((e,i)=>a.push(e+x.a[i]))
-    return new Vector(a)
-  }
-  subtract(x,a=[]){
-    if(x.a.length!==this.a.length)throw Error()
-    this.a.forEach((e,i)=>a.push(e-x.a[i]))
-    return new Vector(a)
-  }
-  dot(x){
-    if(x.a.length!==this.a.length)throw Error()
-    return this.a.reduce((a,c,i)=>a+=(c*x.a[i]),0)
-  }
-  norm(){
-    return Math.sqrt(this.a.reduce((a,c)=>a+(c**2)))
-  }
-  toString(){
-    return `(${this.a.join`,`})`
-  }
-  equals(x){
-    for(let i=0;i<this.a.length;i++)if(this.a[i]!==x.a[i])return false
-    return true
-  }
+  return r.sort((a,b)=>b.length-a.length)[0]
 }
-
-const a = new Vector([1, 2, 3])
-const b = new Vector([3, 4, 5])
-const c = new Vector([5, 6, 7, 8])
-
-console.log(a.add(b))
-console.log(a.subtract(b))
-console.log(a.dot(b))
-console.log(a.norm())
-console.log(a.toString())
-console.log(a.equals(a))
-console.log(a.add(b).equals(new Vector([4, 6, 8])))
+console.log(longestBouncyList([7,9,6,10,5,11,10,12,13,4,2,5,1,6,4,8]))
+console.log(longestBouncyList([1,2,3,4,5,6]))
+// console.log(longestBouncyList([1,1,1,1,1,1]))
+// console.log(longestBouncyList([
+//   -11,  10,  6,  7,  3, -1,
+//    -3,  -3, 15, -4, -5, -1,
+//    -7, -16
+// ]))
+// console.log(longestBouncyList([
+//   -10, -6, 7,   7,  -7, -11,
+//     3,  3, 6, -12, -14,  14,
+//    14,  7
+// ]))
