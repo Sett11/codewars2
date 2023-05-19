7a9e09f6d518ee037283c4a0e01b866f2e806a07
Jhonny <gonzodzen@mail.ru>
2023-05-19 15:37:14 +0300

    #8: Treasure Map

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-function bingo(c,n,o=c[0].map((e,i)=>c.map(u=>u[i])).map(e=>e.slice(1).map(u=>e[0]+u)),v=c[0].map((e,i)=>o.map(u=>u[i])).filter(e=>e),z=2,w=[]){
-  const f=x=>x.reduce((a,c)=>a.concat(Array.isArray(c)?f(c):[c]),[])
-  while(z){
-    let t=[]
-    for(let i=0;i<o.length;i++)t.push(o[i][i])
-    w.push(t),o=o.reverse(),z--
+function findX(m,x=[~~(m.length/2),~~(m[0].length/2)],q=[]){
+  let [a,b]=x
+  while(1){
+    let c=m[a][b]
+    if(q.includes([c,a,b].join``))return null
+    q.push([c,a,b].join``)
+    if(c==='X')return [a,b]
+    if(c.match('N'))a-=+c.slice(1)
+    if(c.match('S'))a+=+c.slice(1)
+    if(c.match('W'))b-=+c.slice(1)
+    if(c.match('E'))b+=+c.slice(1)
+    if(a>=m.length||a<0||b>=m[0].length||b<0)return null
   }
-  const r=(o.concat(v).concat(w)).map(e=>e.includes('NFREE SPACE')?e.slice(0,2).concat(e.slice(3)):e)
-  n=n.filter(e=>f(r).includes(e))
-  return r.some(e=>e.every(u=>n.includes(u)))
 }
 
-console.log(bingo([
-  ['B', 'I', 'N', 'G', 'O'],
-  [1, 16, 31, 46, 61],
-  [3, 18, 33, 48, 63],
-  [5, 20, 'FREE SPACE', 50, 65],
-  [7, 22, 37, 52, 67],
-  [9, 24, 39, 54, 69]
-],['B1', 'I16', 'N31', 'G46', 'O61']))
-console.log(bingo([
-  ['B', 'I', 'N', 'G', 'O'],
-  [1, 16, 31, 46, 61],
-  [3, 18, 33, 48, 63],
-  [5, 20, 'FREE SPACE', 50, 65],
-  [7, 22, 37, 52, 67],
-  [9, 24, 39, 54, 69]
-],['B1', 'I16', 'N31', 'G46', 'O63', 'O61']))
+console.log(findX([
+  ["X", "W3", "E2", "S3", "S4"],
+  ["S1", "E1", "N1", "S2", "W1"],
+  ["S2", "E1", "N2", "S1", "N3"],
+  ["N1", "X", "S2", "E1", "W4"],
+  ["X", "E3", "X", "N2", "W4"]
+]))
+console.log(findX([
+  ["X", "W3", "E2", "S3", "S4"],
+  ["S1", "E1", "N1", "S2", "W1"],
+  ["S2", "E1", "N4", "S1", "N3"],
+  ["N1", "X", "S2", "E1", "W4"],
+  ["X", "E3", "X", "N2", "W4"]
+]))
