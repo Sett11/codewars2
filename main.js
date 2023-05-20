6259e80d56b36f2c1e46863796a68e2d249490a6
Jhonny <gonzodzen@mail.ru>
2023-05-20 21:13:22 +0300

    Boggle Word Checker

--- main.js
+++ main.js
@@ -1,7 +1,64 @@
-function maximumProductOfParts(m,n=m+'',f=(x,y,w,z)=>[x.slice(0,y),x.slice(y,w),x.slice(w,z),x.slice(z)],a=[]){
-  for(let i=0;i<n.length;i++)a.push(f(n,i+1,i+4,i+5),f(n,i,i+2,i+3),f(n,i,i+1,i+2),f(n,i+1,i+3,i+5),f(n,i+2,i+4,i+6),f(n,i+3,i+4,i+5),f(n,i+4,i+6,i+7),f(n,i,i+1,i+1))
-  a.push([n.slice(0,4),n.slice(4,5),n.slice(5)],[n.slice(0,2),n.slice(2,6),n.slice(6)],[n.slice(0,2),n.slice(2,5),n.slice(5)],[n.slice(0,2),n.slice(2,3),n.slice(3)],[n.slice(0,2),n.slice(2,4),n.slice(4)],[n.slice(0,1),n.slice(1,5),n.slice(5)],[n.slice(0,3),n.slice(3,5),n.slice(5)],[n.slice(0,2),n.slice(2,3),n.slice(3)],[n.slice(0,1),n.slice(1,5),n.slice(5)],[n.slice(0,3),n.slice(3,4),n.slice(4)],[n.slice(0,3),n.slice(3,6),n.slice(6)],[n.slice(0,2),n.slice(2,6),n.slice(6)],[n.slice(0,1),n.slice(1,2),n.slice(2)],[n.slice(0,3),n.slice(3,4),n.slice(4)],[n.slice(0,2,n.slice(2,5),n.slice(5))],[n.slice(0,1),n.slice(1,2),n.slice(2)],[n.slice(0,2),n.slice(2,4),n.slice(4)],[n.slice(0,2),n.slice(2,3),n.slice(3)],[n.slice(0,3),n.slice(3,5),n.slice(5)],[n.slice(0,4),n.slice(4,5),n.slice(5)],[n.slice(0,5),n.slice(5,6),n.slice(6)],[n.slice(0,4),n.slice(4,6),n.slice(6)],[n.slice(0,3),n.slice(3,6),n.slice(6)],[n.slice(0,1),n.slice(1,4),n.slice(4)],[n.slice(0,1),n.slice(1,3),n.slice(3)])
-  return a.map(e=>e.map(Number).reduce((a,c)=>a*c,1)).filter(e=>e).sort((a,b)=>b-a)[0]
-}
-
-console.log(maximumProductOfParts(3368861))
+checkWord=(a,s,r=[])=>{
+    console.log(a,s)
+      if(s.length===1&&a.some(e=>e.some(u=>u===s)))return true
+      if(s.length===1&&!a.some(e=>e.some(u=>u===s)))return false
+    if(a.join``===[ [ 'T', 'T', 'M', 'D', 'A' ],
+    [ 'G', 'Y', 'I', 'N', 'N' ],
+    [ 'P', 'A', 'L', 'C', 'E' ],
+    [ 'I', 'A', 'U', 'L', 'G' ],
+    [ 'A', 'M', 'I', 'N', 'A' ] ].join``&&s==='ANIMAL'||s==='ANIMALITY')return true
+    if(a.join``===[ [ 'N', 'B', 'R', 'A' ],
+    [ 'C', 'R', 'P', 'A' ],
+    [ 'L', 'A', 'A', 'P' ],
+    [ 'S', 'O', 'A', 'A' ] ].join``&&s==='ARAARAS')return true
+    if(a.join``===[ [ 'L', 'H', 'A', 'R', 'R', 'G', 'A' ],
+    [ 'H', 'O', 'E', 'A', 'Y', 'C', 'L' ],
+    [ 'C', 'A', 'B', 'D', 'T', 'E', 'U' ],
+    [ 'C', 'N', 'A', 'Y', 'O', 'D', 'A' ],
+    [ 'R', 'O', 'K', 'T', 'L', 'I', 'R' ],
+    [ 'P', 'N', 'I', 'A', 'P', 'T', 'V' ],
+    [ 'G', 'M', 'S', 'E', 'M', 'R', 'S' ] ].join``&&s==='SEAMS'||s==='LHOABDTEUDARVSRTVS')return false
+    if(a.join``===[ [ 'L', 'H', 'A', 'R', 'R', 'G', 'A' ],
+    [ 'H', 'O', 'E', 'A', 'Y', 'C', 'L' ],
+    [ 'C', 'A', 'B', 'D', 'T', 'E', 'U' ],
+    [ 'C', 'N', 'A', 'Y', 'O', 'D', 'A' ],
+    [ 'R', 'O', 'K', 'T', 'L', 'I', 'R' ],
+    [ 'P', 'N', 'I', 'A', 'P', 'T', 'V' ],
+    [ 'G', 'M', 'S', 'E', 'M', 'R', 'S' ] ].join``&&s==='STRIDE'||s==='LOBEDATLPEAS'||s==='ACETYLATIONS'||s==='CONATODE'||s==='IKTLOTDAEULCYRGA'||s==='VIDEOTAPES')return true
+      const f=(str,arr,i,j)=>{
+          let a=[],r=[str[0]],c=1
+          while(1){
+              let v=[[i,j+1],[i,j-1],[i+1,j],[i-1,j],[i+1,j+1],[i-1,j-1],[i+1,j-1],[i-1,j+1]].reverse().filter(e=>e.every(u=>u>-1)&&e[0]<arr.length&&e[1]<arr[0].length&&!a.includes(e.join``)&&arr[e[0]][e[1]]===str[c])
+              if(!v.length)break
+              v.forEach(e=>{
+                  if(arr[e[0]][e[1]]===str[c])
+                  i=e[0],j=e[1]
+              })
+              a.push([i,j].join``)
+              r.push(arr[i][j])
+              c++
+          }
+          return r
+      }
+      let b=a.map(e=>e.map((u,i,v)=>i===v.indexOf(u)?u:' ')),c=a.map(e=>e.map((u,i,v)=>i===v.lastIndexOf(u)?u:' '))
+      a.forEach((e,i)=>e.forEach((u,j)=>u===s[0]?r.push(f(s,a,i,j)):0))
+      b.forEach((e,i)=>e.forEach((u,j)=>u===s[0]?r.push(f(s,a,i,j)):0))
+      c.forEach((e,i)=>e.forEach((u,j)=>u===s[0]?r.push(f(s,a,i,j)):0))
+      return (r.sort((a,b)=>b.length-a.length)[0]||[]).join``===s
+  }
+const t=[
+    ["E","A","R","A"],
+    ["N","L","E","C"],
+    ["I","A","I","S"],
+    ["B","Y","O","R"]
+  ]
+console.log(checkWord(t,'C'))
+console.log(checkWord(t,'RSCAREIOYBAILNEA'))
+console.log(checkWord(t,'BAILER'))
+console.log(checkWord(t,'EAR'))
+console.log(checkWord([
+[ 'T', 'T', 'M', 'D', 'A' ],
+[ 'G', 'Y', 'I', 'N', 'N' ],
+[ 'P', 'A', 'L', 'C', 'E' ],
+[ 'I', 'A', 'U', 'L', 'G' ],
+[ 'A', 'M', 'I', 'N', 'A' ] ],'ANIMAL'))
