871563ecbfae74798d71e83ec5bbf96b883af96c
Jhonny <gonzodzen@mail.ru>
2023-05-20 14:18:57 +0300

    Largest Cross Sum

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
-function findX(m,x=[~~(m.length/2),~~(m[0].length/2)],q=[]){
-  let [a,b]=x
-  while(1){
-    let c=m[a][b]
-    if(q.includes([c,a,b].join``))return null
-    q.push([c,a,b].join``)
-    if(c==='X')return [a,b]
-    if(c.match('N'))a-=+c.slice(1)
-    if(c.match('S'))a+=+c.slice(1)
-    if(c.match('W'))b-=+c.slice(1)
-    if(c.match('E'))b+=+c.slice(1)
-    if(a>=m.length||a<0||b>=m[0].length||b<0)return null
+largestCrossSum=(a,m=[])=>{
+  for(let i=0;i<a.length;i++){
+    for(let j=0;j<a[i].length;j++){
+      let t=a[i].slice(0,j).concat(a[i].slice(j+1)),iAr=[]
+      for(let c=0;c<a.length;c++)iAr.push(a[c][j])
+      m.push(t.concat(iAr))
+    }
   }
+  return Math.max(...m.map(e=>e.reduce((a,c)=>a+c,0)))
 }
 
-console.log(findX([
-  ["X", "W3", "E2", "S3", "S4"],
-  ["S1", "E1", "N1", "S2", "W1"],
-  ["S2", "E1", "N2", "S1", "N3"],
-  ["N1", "X", "S2", "E1", "W4"],
-  ["X", "E3", "X", "N2", "W4"]
-]))
-console.log(findX([
-  ["X", "W3", "E2", "S3", "S4"],
-  ["S1", "E1", "N1", "S2", "W1"],
-  ["S2", "E1", "N4", "S1", "N3"],
-  ["N1", "X", "S2", "E1", "W4"],
-  ["X", "E3", "X", "N2", "W4"]
-]))
+console.log(largestCrossSum([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]]))
+console.log(largestCrossSum([
+    [ 3, 2, 3 ],
+    [ 2, 8, 7 ],
+    [ 9, 3, 5 ],
+    [ 1, 5, 7 ] ]))
