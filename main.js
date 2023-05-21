55bedc298fb8e0339e6734c294cb68740f5528eb
Jhonny <gonzodzen@mail.ru>
2023-05-21 14:21:18 +0300

    Good words

--- main.js
+++ main.js
@@ -1,64 +1,20 @@
-checkWord=(a,s,r=[])=>{
-    console.log(a,s)
-      if(s.length===1&&a.some(e=>e.some(u=>u===s)))return true
-      if(s.length===1&&!a.some(e=>e.some(u=>u===s)))return false
-    if(a.join``===[ [ 'T', 'T', 'M', 'D', 'A' ],
-    [ 'G', 'Y', 'I', 'N', 'N' ],
-    [ 'P', 'A', 'L', 'C', 'E' ],
-    [ 'I', 'A', 'U', 'L', 'G' ],
-    [ 'A', 'M', 'I', 'N', 'A' ] ].join``&&s==='ANIMAL'||s==='ANIMALITY')return true
-    if(a.join``===[ [ 'N', 'B', 'R', 'A' ],
-    [ 'C', 'R', 'P', 'A' ],
-    [ 'L', 'A', 'A', 'P' ],
-    [ 'S', 'O', 'A', 'A' ] ].join``&&s==='ARAARAS')return true
-    if(a.join``===[ [ 'L', 'H', 'A', 'R', 'R', 'G', 'A' ],
-    [ 'H', 'O', 'E', 'A', 'Y', 'C', 'L' ],
-    [ 'C', 'A', 'B', 'D', 'T', 'E', 'U' ],
-    [ 'C', 'N', 'A', 'Y', 'O', 'D', 'A' ],
-    [ 'R', 'O', 'K', 'T', 'L', 'I', 'R' ],
-    [ 'P', 'N', 'I', 'A', 'P', 'T', 'V' ],
-    [ 'G', 'M', 'S', 'E', 'M', 'R', 'S' ] ].join``&&s==='SEAMS'||s==='LHOABDTEUDARVSRTVS')return false
-    if(a.join``===[ [ 'L', 'H', 'A', 'R', 'R', 'G', 'A' ],
-    [ 'H', 'O', 'E', 'A', 'Y', 'C', 'L' ],
-    [ 'C', 'A', 'B', 'D', 'T', 'E', 'U' ],
-    [ 'C', 'N', 'A', 'Y', 'O', 'D', 'A' ],
-    [ 'R', 'O', 'K', 'T', 'L', 'I', 'R' ],
-    [ 'P', 'N', 'I', 'A', 'P', 'T', 'V' ],
-    [ 'G', 'M', 'S', 'E', 'M', 'R', 'S' ] ].join``&&s==='STRIDE'||s==='LOBEDATLPEAS'||s==='ACETYLATIONS'||s==='CONATODE'||s==='IKTLOTDAEULCYRGA'||s==='VIDEOTAPES')return true
-      const f=(str,arr,i,j)=>{
-          let a=[],r=[str[0]],c=1
-          while(1){
-              let v=[[i,j+1],[i,j-1],[i+1,j],[i-1,j],[i+1,j+1],[i-1,j-1],[i+1,j-1],[i-1,j+1]].reverse().filter(e=>e.every(u=>u>-1)&&e[0]<arr.length&&e[1]<arr[0].length&&!a.includes(e.join``)&&arr[e[0]][e[1]]===str[c])
-              if(!v.length)break
-              v.forEach(e=>{
-                  if(arr[e[0]][e[1]]===str[c])
-                  i=e[0],j=e[1]
-              })
-              a.push([i,j].join``)
-              r.push(arr[i][j])
-              c++
-          }
-          return r
-      }
-      let b=a.map(e=>e.map((u,i,v)=>i===v.indexOf(u)?u:' ')),c=a.map(e=>e.map((u,i,v)=>i===v.lastIndexOf(u)?u:' '))
-      a.forEach((e,i)=>e.forEach((u,j)=>u===s[0]?r.push(f(s,a,i,j)):0))
-      b.forEach((e,i)=>e.forEach((u,j)=>u===s[0]?r.push(f(s,a,i,j)):0))
-      c.forEach((e,i)=>e.forEach((u,j)=>u===s[0]?r.push(f(s,a,i,j)):0))
-      return (r.sort((a,b)=>b.length-a.length)[0]||[]).join``===s
-  }
-const t=[
-    ["E","A","R","A"],
-    ["N","L","E","C"],
-    ["I","A","I","S"],
-    ["B","Y","O","R"]
-  ]
-console.log(checkWord(t,'C'))
-console.log(checkWord(t,'RSCAREIOYBAILNEA'))
-console.log(checkWord(t,'BAILER'))
-console.log(checkWord(t,'EAR'))
-console.log(checkWord([
-[ 'T', 'T', 'M', 'D', 'A' ],
-[ 'G', 'Y', 'I', 'N', 'N' ],
-[ 'P', 'A', 'L', 'C', 'E' ],
-[ 'I', 'A', 'U', 'L', 'G' ],
-[ 'A', 'M', 'I', 'N', 'A' ] ],'ANIMAL'))
+function good(c,s=c.toUpperCase()){
+    const o=[['1','2ABC','3DEF'],['4GHI','5JKL','6MNO'],['7PQRS','8TUV','9WXYZ'],['*','0 ','#']]
+    for(let i=0;i<s.length-1;i++){
+        let t=-1
+        for(let j=0;j<o.length;j++){
+            for(let k=0;k<o[j].length;k++){
+                if(o[j][k].indexOf(s[i])!==-1){
+                    t=[j,k]
+                    break
+                }
+            }
+        }
+        let v=o[t[0]].concat(o.map(e=>e.filter((u,j)=>j===t[1]))).flat().join``
+        if(!v.includes(s[i+1]))return false
+    }
+    return true
+}
+
+console.log(good('Jako#One#Zone'))
+console.log(good('Jake'))
