98ccc3d65bf3fb1f0f193bd5f3436a24cc33e0e6
Jhonny <gonzodzen@mail.ru>
2023-05-21 16:41:02 +0300

    Mixbonacci

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-function good(c,s=c.toUpperCase()){
-    const o=[['1','2ABC','3DEF'],['4GHI','5JKL','6MNO'],['7PQRS','8TUV','9WXYZ'],['*','0 ','#']]
-    for(let i=0;i<s.length-1;i++){
-        let t=-1
-        for(let j=0;j<o.length;j++){
-            for(let k=0;k<o[j].length;k++){
-                if(o[j][k].indexOf(s[i])!==-1){
-                    t=[j,k]
-                    break
-                }
-            }
-        }
-        let v=o[t[0]].concat(o.map(e=>e.filter((u,j)=>j===t[1]))).flat().join``
-        if(!v.includes(s[i+1]))return false
+function mixbonacci(p,l,r=[],x={}){
+    if(!p.length||!l)return []
+  const o={
+    'fib':(n,a=[0, 1, 1, 2, 3])=>{
+        for(let i=0;a.length<=n;i++)a.push(a[a.length-1]+a[a.length-2])
+        return a
+    },
+    'jac':(n,a=[0, 1, 1, 3, 5])=>{
+        for(let i=0;a.length<=n;i++)a.push(a[a.length-1]+2*a[a.length-2])
+        return a
+    },
+    'pad':(n,a=[1, 0, 0, 1, 0])=>{
+        for(let i=0;a.length<=n;i++)a.push(a[a.length-2]+a[a.length-3])
+        return a
+    },
+    'pel':(n,a=[0, 1, 2, 5, 12])=>{
+        for(let i=0;a.length<=n;i++)a.push(2*a[a.length-1]+a[a.length-2])
+        return a
+    },
+    'tet':(n,a=[0, 0, 0, 1, 1])=>{
+        for(let i=0;a.length<=n;i++)a.push(a[a.length-1]+a[a.length-2]+a[a.length-3]+a[a.length-4])
+        return a
+    },
+    'tri':(n,a=[0, 0, 1, 1, 2])=>{
+        for(let i=0;a.length<=n;i++)a.push(a[a.length-1]+a[a.length-2]+a[a.length-3])
+        return a
     }
-    return true
+  }
+  let q=p.map(e=>[e,o[e](l)])
+  for(let i=0;i<q.length;i++)x[q[i][0]]=q[i][1]
+  for(let i=0;r.length<l;i++)r.push(x[p[i%p.length]].shift())
+  return r
 }
 
-console.log(good('Jako#One#Zone'))
-console.log(good('Jake'))
+console.log(mixbonacci(['tri', 'pel', 'fib', 'pad', 'tri', 'pad', 'tri' ], 10))
