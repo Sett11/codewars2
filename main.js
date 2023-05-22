0a944f57167dc943ac04242aca495d77febb3290
Jhonny <gonzodzen@mail.ru>
2023-05-22 17:23:54 +0300

    5command - Esoteric programming #1

--- main.js
+++ main.js
@@ -4,31 +4,29 @@ class Output {
       this.debug = debug;
     }
   }
-  class Interpreter{
+  class FiveCommands{
     constructor(){}
-    read(s){
-      s=[...s]
-      let a=[],b=[],c=0,v=0
-      for(let i=0;i<s.length;i++){
-        if(s[i]==='a')c++
-        if(s[i]==='b')c--
-        if(s[i]==='c')a.push(v?String.fromCharCode(c):c)
-        if(s[i]==='d')c=c>0?-c:Math.abs(c)
-        if(s[i]==='r')c=Math.floor(Math.random()*(c+1))
-        if(s[i]==='n')c=0
-        if(s[i]==='$')v=v?0:1
-        if(s[i]==='l'){
-            c++
-            s.splice(i,1)
-            i=0
+    read(s,d,a=[0],r='',db=[]){
+      for(let i=0,j=0;i<s.length;i++){
+        if(s[i]==='+'){
+            if(a[j+1]===undefined)a.push(0)
+            j++
         }
-        if(s[i]===';')b.push(`${c}->${String.fromCharCode(c)}`)
+        if(s[i]==='-'){
+            if(a[j-1]===undefined)a.unshift(0)
+            if(j)j--
+        }
+        if(s[i]==='^')a[j]++
+        if(s[i]==='v')a[j]--
+        if(s[i]==='*')r+=a[j]
       }
-      return new Output(a.join``,b)
+      for(let i=0;i<a.length;i++)db.push(i+'->'+a[i])
+      return new Output(r,d?db:[])
     }
-}
+  }
 
-const r=new Interpreter()
+const r=new FiveCommands()
 
-console.log(r.read('aaaaaaaaaaaaaaaaaaaaaaablddaaaacaaaa'))
-console.log()
+console.log(r.read('^*',true))
+console.log(r.read('^^*',true))
+console.log(r.read('+^^^-^v*+*',true))
