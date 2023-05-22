eed66aedfe16ea325192a04dc2d885f6a373c668
Jhonny <gonzodzen@mail.ru>
2023-05-22 18:18:31 +0300

    "this" is an other solution

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-class NamedOne{
+class OnceNamedOne{
     constructor(a,b){
     this.firstName=a
     this.lastName=b
+    this.fullName=a+' '+b
+    return Object.freeze(this)
     }
-    get fullName(){return `${this.firstName} ${this.lastName}`}
-    set fullName(s){if(s.includes(' '))[this.firstName,this.lastName]=s.split` ` }
 }
-const n=new NamedOne('John', 'Doe')
 
-console.log(n.fullName)
+const n=new OnceNamedOne('John', 'Doe')
+n.firstName='hhh'
+
+console.log(n.firstName)
