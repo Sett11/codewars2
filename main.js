d777b84a63f7677be1c849c82fa519610bd8668e
Jhonny <gonzodzen@mail.ru>
2023-05-23 01:09:56 +0300

    Array.isArray

--- main.js
+++ main.js
@@ -1,7 +1,75 @@
-function checkAvailability(a,s){
-    a=a.map(e=>e.map(u=>u.split`:`.map(z=>+z))),s=s.split`:`.map(e=>+e)
-    for(let i=0;i<a.length;i++)if(a[i][0][0]<s[0]&&a[i][1][0]>=s[0]&&a[i][1][1]>s[1])return a[i][1].join`:`
-    return 1==1
+isArray=a=>{
+    return !a?false:typeof a==='object'&&a.length!==undefined&&a.push!==undefined&&JSON.stringify(a)[0]==='['
 }
 
-console.log(checkAvailability([["09:30", "10:15"], ["12:20", "15:50"]], "10:00"))
+  console.log(isArray([]))
+
+
+// const pathFinder=(b,a=b.split`\n`.map(e=>[...e]),i=0,j=0,q=[],w=[[i,j].join`&`])=>{
+//     if((a[0].concat(a.map((e,i)=>e[e.length-1]))).every(e=>e==='.')||(a[a.length-1].concat(a.map(e=>e[0]))).every(e=>e==='.'))return true
+//     a[0][0]='&'
+//     a=a.reverse().map(e=>e.reverse())
+//     while(1){
+//         if(a[i][j]=='&')return true
+//         let r=[[i,j+1],[i+1,j],[i-1,j],[i,j-1]].filter(e=>e[0]>-1&&e[1]>-1&&e[0]<a.length&&e[1]<a[0].length&&a[e[0]][e[1]]!=='W'&&!w.includes(e.join`&`))
+//         if(!r.length){
+//             let t=(q.pop()||[])
+//             i=t[0],j=t[1]
+//             w.push([i,j].join`&`)
+//         }
+//         if(!r.length&&!q.length)return false
+//         if(r.length){
+//             i=r[0][0],j=r[0][1]
+//             w.push([i,j].join`&`)
+//         }
+//         if(r.length>1)q.push(...r.slice(1))
+//     }
+// }
+
+// console.log(pathFinder(
+// `.....WW..W
+// ..W.......
+// .W........
+// ..WWW....W
+// W..W...W..
+// W..W.W....
+// .W.......W
+// .W...W....
+// .....W.W.W
+// .WWW..W...`))
+// console.log(pathFinder(`......
+// ....W.
+// ..WWW.
+// ...W.W
+// ......
+// W..W..`))
+// console.log(pathFinder(
+// `.W.
+// .W.
+// W..`))
+// console.log(pathFinder(
+// `...W.WWW..
+// W..WW.WW..
+// W........W
+// WW.W..W.W.
+// ..W...W...
+// W......W.W
+// ...WWW....
+// W.........
+// W.........
+// .W........`))
+
+// console.log(pathFinder(
+// `.W...W...W...
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// .W.W.W.W.W.W.
+// ...W...W...W.`))
