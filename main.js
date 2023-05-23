dc9850abaa8d59248ff1fd37e3ec308ae622a5b9
Jhonny <gonzodzen@mail.ru>
2023-05-24 00:37:53 +0300

    Another one down—the Survival of the Fittest!

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-reverseCase=s=>s.replace(/(.)\1*/g,e=>e.length<2?e:e[0]>{}?e.toUpperCase():e.toLowerCase())
+removeSmallest=f=(n,b,a=b.slice())=>n<1?a:n>a.length?[]:a.splice(a.indexOf(Math.min(...a)),1)&&f(n-1,b,a)
 
-console.log(reverseCase('puzzles'))
+
+console.log(removeSmallest(3,[5,3,2,1,4]))
