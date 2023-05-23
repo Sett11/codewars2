7954103b3e4dbec5250bd3f7af5f296f14205a8b
Jhonny <gonzodzen@mail.ru>
2023-05-23 16:50:36 +0300

    Brian's Brain

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-biggestNum=(a,b,r=[a,b].map(e=>BigInt(e.replace(/^\-+/g,e=>e.length%2===0?'+':'-'))),q=r[0]>r[1]?r[0]:r[1])=>q+''
-
-console.log(biggestNum('0222','----0003333'))
+function brain(x){
+    let q=x
+    return {
+        nextGen:function(a=q,r=[]){
+            for(let i=0;i<a.length;i++)for(let j=0;j<a[i].length;j++)r.push([a[i][j],[i,j],[[i,j+1],[i,j-1],[i-1,j],[i+1,j],[i+1,j+1],[i-1,j-1],[i+1,j-1],[i-1,j+1]].filter(e=>e[0]>-1&&e[0]<a.length&&e[1]>-1&&e[1]<a[i].length).map(e=>a[e[0]][e[1]])])
+            r=r.map(e=>e[0]===0&&e[2].filter(u=>u===2).length===2?[e[1],2]:e[0]===2?[e[1],1]:e[0]===1?[e[1],0]:0).filter(e=>e)
+            for(let i=0;i<r.length;i++)a[r[i][0][0]][r[i][0][1]]=r[i][1]
+            },
+            grid:q
+    }
+}
+const r=brain([
+    [ 1, 2, 2 ],
+    [ 0, 1, 0 ]])
+r.nextGen()
+r.nextGen()
+console.log(r.grid)
