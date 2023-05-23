a80385d7e60f627d5c7acde7bac657d0e5062b0f
Jhonny <gonzodzen@mail.ru>
2023-05-23 23:13:24 +0300

    Min value of each n elements -- Simple version

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-function friendFind(a,q=JSON.stringify(['blue','red','blue'].sort()),c=0){
-    for(let i=0;i<a.length;i++){
-        if(a[i]==='red'){
-            let t=[a.slice(i-1,i+2),a.slice(i-2,i+1),a.slice(i,i+3)].filter(e=>JSON.stringify(e.sort())===q)
-            if(t.length){
-                c++
-                a[i]='&'
-            }
-        }
-    }
-    return c
+minValue=(a,n,r=[])=>{
+    for(i=-1;++i<=a.length-n;)r.push(Math.min(...a.slice(i,i+n)))
+    return r
 }
 
-console.log(friendFind(['blue','red','blue','blue','red','blue','red']))
-console.log(friendFind(['blue','blue','red','red','blue','green']))
-console.log(friendFind(['red','blue','blue','red']))
+  console.log(minValue([1,2,3,10,-5],2))
