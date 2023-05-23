a0b26eaf1821e5f925d1e8eb2d0484b3d9f9d3c8
Jhonny <gonzodzen@mail.ru>
2023-05-23 23:28:10 +0300

    Last 3 digits of m^n

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-minValue=(a,n,r=[])=>{
-    for(i=-1;++i<=a.length-n;)r.push(Math.min(...a.slice(i,i+n)))
-    return r
-}
+lastThreeDigits=(m,n,f=x=>BigInt(x))=>(f(m)**f(n)+'').slice(-3)
 
-  console.log(minValue([1,2,3,10,-5],2))
+console.log(lastThreeDigits(22,33))
