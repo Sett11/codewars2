eda6b3980271aac4a0886a4d82131c7854b43bc4
Jhonny <gonzodzen@mail.ru>
2023-05-24 21:21:39 +0300

    Collaboration Station

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-pizzaRewards=(c,m,n)=>Object.entries(c).filter(e=>e[1].filter(u=>u>=n).length>=m).map(e=>e[0])
+splitMessage=(m,c)=>
+    !c||c<1?null:Array(c).fill(!m?'':m).map((e,i)=>{
+        e=[...e]
+        for(let j=i;j<e.length;j+=c)e[j]=[e[j]]
+        return e.map(u=>typeof u==='string'?'-':u).join``
+    })
 
 
-console.log(pizzaRewards({ 'John Doe': [ 22, 30, 11, 17, 15, 52, 27, 12 ],
-'Jane Doe': [ 5, 17, 30, 33, 40, 22, 26, 10, 11, 45 ] },5,20))
+console.log(splitMessage('I think you are all sexy',3))
