49d854c784f3f68db3cb90de81dcc7a5ca9732a5
Jhonny <gonzodzen@mail.ru>
2023-05-24 23:42:34 +0300

    Airport Arrivals/Departures - #1

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-const alf='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-function encode(c){
-    if(!c)return ''
-    let a=[...c.toUpperCase()],ind=a.findIndex(e=>e.match(/[a-z]/i)),q=a.slice(0,ind).join``
-    a=a.slice(ind)
-    return q+a.map((e,i)=>{
-        if(!e.match(/[a-z]/i)||!i)return e
-        let t=a.slice(0,i).join``.replace(/[^a-z]/gi,'')
-        return alf[(((alf.indexOf(e)+1)+(alf.indexOf(t[t.length-1].toUpperCase())+1))%alf.length)-1]
-    }).join``
-}
-function decode(s){
-    if(!s)return ''
-    const a=[...s.toUpperCase()]
-    for(let i=0;++i<a.length;){
-        if(!a[i].match(/[a-z]/i))continue
-        let t=a.slice(0,i).join``.replace(/[^a-z]/gi,'')
-        t=(alf.indexOf(a[i])-alf.indexOf(t[t.length-1]))-1
-        a[i]=alf[t<0?t+alf.length:t]
+const ALPHABET='ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789'
+flapDisplay=(a,b,w=x=>JSON.parse(JSON.stringify(x)))=>{
+    const l=w(a),r=w(b)
+    const f=(x,y,a=[])=>{
+        x=[...x]
+        while(y.length){
+            let q=y.shift()
+            x=x.map(e=>ALPHABET[(ALPHABET.indexOf(e)+q)%ALPHABET.length])
+            a.push(x.shift())
+        }
+        return a
     }
-    return a.join``
+    return l.map((e,i)=>f(e,r[i]).join``)
 }
 
-console.log(encode('The quick brown fox jumps over the lazy dog. '))
-console.log(decode('HM1QA'))
+console.log(flapDisplay(["CAT","HELLO "],[[1,13,27],[15,49,50,48,43,13]]))
