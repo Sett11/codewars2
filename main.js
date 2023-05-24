b49add85894ef8cefca797716365e9166709bf72
Jhonny <gonzodzen@mail.ru>
2023-05-24 23:07:59 +0300

    Paul Cipher & Kevin Arnold

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-splitMessage=(m,c)=>
-    !c||c<1?null:Array(c).fill(!m?'':m).map((e,i)=>{
-        e=[...e]
-        for(let j=i;j<e.length;j+=c)e[j]=[e[j]]
-        return e.map(u=>typeof u==='string'?'-':u).join``
-    })
+const alf='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+function encode(c){
+    if(!c)return ''
+    let a=[...c.toUpperCase()],ind=a.findIndex(e=>e.match(/[a-z]/i)),q=a.slice(0,ind).join``
+    a=a.slice(ind)
+    return q+a.map((e,i)=>{
+        if(!e.match(/[a-z]/i)||!i)return e
+        let t=a.slice(0,i).join``.replace(/[^a-z]/gi,'')
+        return alf[(((alf.indexOf(e)+1)+(alf.indexOf(t[t.length-1].toUpperCase())+1))%alf.length)-1]
+    }).join``
+}
+function decode(s){
+    if(!s)return ''
+    const a=[...s.toUpperCase()]
+    for(let i=0;++i<a.length;){
+        if(!a[i].match(/[a-z]/i))continue
+        let t=a.slice(0,i).join``.replace(/[^a-z]/gi,'')
+        t=(alf.indexOf(a[i])-alf.indexOf(t[t.length-1]))-1
+        a[i]=alf[t<0?t+alf.length:t]
+    }
+    return a.join``
+}
 
-
-console.log(splitMessage('I think you are all sexy',3))
+console.log(encode('The quick brown fox jumps over the lazy dog. '))
+console.log(decode('HM1QA'))
