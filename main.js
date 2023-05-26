0ce4654c87fb2f6d540a6f8501a644530311f02a
Jhonny <gonzodzen@mail.ru>
2023-05-26 15:38:15 +0300

    Route Calculator

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-function openingTimes(s,f=x=>x.replace(/300/,'30')){
-    const o=Object.entries({'Monday':[[08,00],[20,00]],'Tuesday':[[08,00],[20,00]],'Wednesday':[[08,00],[20,00]],'Thursday':[[08,00],[20,00]],'Friday':[[08,00],[20,00]],'Saturday':[[10,00],[18,00]],'Sunday':[[12,00],[16,30]]})
-    s=(s.charAt(0).toUpperCase()+s.slice(1).toLowerCase()).split` `.map((e,i)=>i?e.split`:`.map(Number):e)
-    if(s[1][0]>23||s[1][0]<0||s[1][1]<0||s[1][1]>59)return 'Invalid time!'
-    for(let i=-1;++i<o.length;){
-        if(s[0]===o[i][0]){
-            let t=o[i][1],v=s[1],w=o[(i+1)%o.length][1]
-            if(v[0]<t[0][0])return f(`Library opens: today ${(t[0][0]<10?'0':'')+t[0].join`:`+0}`)
-            if(v[0]>=t[0][0]&&v[0]<t[1][0])return f(`Library closes at ${t[1].join`:`+0}`)
-            if(v[0]>t[1][0]||v[0]===t[1][0]&&v[1]>=t[1][1])return f(`Library opens: ${o[(i+1)%o.length][0]} ${(w[0][0]<10?'0':'')+o[(i+1)%o.length][1][0].join`:`+0}`)
-            return [t,v]
-        }
+function calculate(s,r=[]){
+    if(s.match(/[^\.0123456789\+\-\*\$]/))return '400: Bad request'
+    s=s.replace(/[\$\*\-\+]/g,' $& ').split` `.map(e=>+e===+e?+e:e)
+    while(s.includes('$')){
+        let ind=s.indexOf('$'),t=s.slice(ind-1,ind+2)
+        s=s.slice(0,ind-1).concat([t[0]/t[2]]).concat(s.slice(ind+2))
     }
-    return 'Invalid time!'
+    while(s.includes('*')){
+        let ind=s.indexOf('*'),t=s.slice(ind-1,ind+2)
+        s=s.slice(0,ind-1).concat([t[0]*t[2]]).concat(s.slice(ind+2))
+    }
+    while(s.includes('-')){
+        let ind=s.indexOf('-'),t=s.slice(ind-1,ind+2)
+        s=s.slice(0,ind-1).concat([t[0]-t[2]]).concat(s.slice(ind+2))
+    }
+    while(s.includes('+')){
+        let ind=s.indexOf('+'),t=s.slice(ind-1,ind+2)
+        s=s.slice(0,ind-1).concat([t[0]+t[2]]).concat(s.slice(ind+2))
+    }
+    return s[0]
 }
 
-console.log(openingTimes("Monday 09:30"))
-console.log(openingTimes("Saturday 00:00"))
-console.log(openingTimes("Tuesday 20:00"))
-console.log(openingTimes('Wednesday 23:12'))
-console.log(openingTimes("MoNDay 07:59"))
+console.log(calculate('5*6$2+5-10'))
+console.log(calculate('1*1*1*1*1*1$1$1$1$1+1-1+9-1'))
+console.log(calculate('1000$2.5$5+5-5+6$6'))
+console.log(calculate('1+1-1'))
