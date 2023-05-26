85d490fdc8cad66b92edcadda49fbad3eed49f23
Jhonny <gonzodzen@mail.ru>
2023-05-26 13:43:39 +0300

    Library Opening Times

--- main.js
+++ main.js
@@ -1,43 +1,21 @@
-function isItPossible(s,c=s.replace(/[^X0]/g,''),t=[],q=[]){
-    console.log(s)
-    s=s.match(/.{3,3}/g).map(e=>[...e])
-    let r=s[0].map((e,i)=>s.map(u=>u[i])).concat(s)
-    for(let i=-1;++i<s.length;)t.push(s[i][i]),q.push(s[i][s[i].length-1-i])
-    r.push(t,q)
-    let z=r.filter(e=>e.every(u=>u===e[0])&&e[0]!=='_')
-    if(c==='X'||!c)return true
-    if(c==='0')return false
-    if(z.length>2)return false
-    if(z.length===2&&z[0][0]!==z[1][0]||z.length===2&&z[0][0]!=='X')return false
-    if(z.length===1&&z[0][0]==='0'&&c.replace(/0/g,'').length===c.replace(/X/g,'').length)return true
-    if(z.length===1&&z[0][0]==='0'&&c.replace(/0/g,'').length!==c.replace(/X/g,'').length)return false
-    if(z.length===1&&z[0][0]==='X'&&c.replace(/0/g,'').length===c.replace(/X/g,'').length)return false
-    if(!z.length&&c.replace(/0/g,'').length===c.replace(/X/g,'').length)return true
-    return c.replace(/0/g,'').length-c.replace(/X/g,'').length===1
+function openingTimes(s,f=x=>x.replace(/300/,'30')){
+    const o=Object.entries({'Monday':[[08,00],[20,00]],'Tuesday':[[08,00],[20,00]],'Wednesday':[[08,00],[20,00]],'Thursday':[[08,00],[20,00]],'Friday':[[08,00],[20,00]],'Saturday':[[10,00],[18,00]],'Sunday':[[12,00],[16,30]]})
+    s=(s.charAt(0).toUpperCase()+s.slice(1).toLowerCase()).split` `.map((e,i)=>i?e.split`:`.map(Number):e)
+    if(s[1][0]>23||s[1][0]<0||s[1][1]<0||s[1][1]>59)return 'Invalid time!'
+    for(let i=-1;++i<o.length;){
+        if(s[0]===o[i][0]){
+            let t=o[i][1],v=s[1],w=o[(i+1)%o.length][1]
+            if(v[0]<t[0][0])return f(`Library opens: today ${(t[0][0]<10?'0':'')+t[0].join`:`+0}`)
+            if(v[0]>=t[0][0]&&v[0]<t[1][0])return f(`Library closes at ${t[1].join`:`+0}`)
+            if(v[0]>t[1][0]||v[0]===t[1][0]&&v[1]>=t[1][1])return f(`Library opens: ${o[(i+1)%o.length][0]} ${(w[0][0]<10?'0':'')+o[(i+1)%o.length][1][0].join`:`+0}`)
+            return [t,v]
+        }
+    }
+    return 'Invalid time!'
 }
 
-console.log(isItPossible(
-"0XX"+
-"XX0"+
-"00X"))
-console.log(isItPossible("XXX"+
-"0_0"+
-"___"))
-console.log(isItPossible("__0"+
-"___"+
-"___"))
-console.log(isItPossible('0X00X0__X'))
-console.log(isItPossible('XX_X_X000'))
-console.log(isItPossible(
-`X00
-0X0
-XXX`))
-console.log(isItPossible(
-`X_0
-0X0
-XX0`))
-console.log(
-isItPossible(
-`0X_
-0X0
-_X_`))
+console.log(openingTimes("Monday 09:30"))
+console.log(openingTimes("Saturday 00:00"))
+console.log(openingTimes("Tuesday 20:00"))
+console.log(openingTimes('Wednesday 23:12'))
+console.log(openingTimes("MoNDay 07:59"))
