7bb6e43a8091defaa7f317a40597b8c14a27c1ff
Jhonny <gonzodzen@mail.ru>
2023-05-26 21:52:53 +0300

    How many feelings?

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-theJanitor=s=>Array(26).fill(1).map((_,i)=>String.fromCharCode(i+97)).map(e=>!s.includes(e)?0:s.indexOf(e)===s.lastIndexOf(e)?1:s.lastIndexOf(e)-s.indexOf(e)+1)
+function countFeelings(s,a){
+    const r=a.map(e=>[e,Object.values([...s].filter(u=>e.includes(u)).reduce((a,c)=>{a[c]=(a[c]||0)+1;return a},{}))]).filter(e=>e[0].length===e[1].length).length
+    return `${r} feeling${r===1?'':'s'}.`
+}
 
-
-console.log(theJanitor('rkuhsxtflzvytbtwxyarsglibmhfmmikyolfmopbtkzxezjahq'))
+console.log(countFeelings('yliausoenvjw',['anger', 'awe', 'joy', 'love', 'grief']))
