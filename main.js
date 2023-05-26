7a30c589d87fa84923e9e2fce03632f1fc70f0d1
Jhonny <gonzodzen@mail.ru>
2023-05-26 20:35:58 +0300

    Simple Fun #265: The Janitor And His Mop

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-searchNames=a=>a.filter((_,i,v,c=(v[i-1]||''))=>c[0]==='.'||c[c.length-1]==='.')
+theJanitor=s=>Array(26).fill(1).map((_,i)=>String.fromCharCode(i+97)).map(e=>!s.includes(e)?0:s.indexOf(e)===s.lastIndexOf(e)?1:s.lastIndexOf(e)-s.indexOf(e)+1)
 
-console.log(searchNames(["foo", "foo@bar.com", "bar", "bar@foo.com", ".foo", "food@bar.com"]))
+
+console.log(theJanitor('rkuhsxtflzvytbtwxyarsglibmhfmmikyolfmopbtkzxezjahq'))
