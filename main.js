ea5f0df7f12d81349ac2ce727dfe019121fbf8a6
Jhonny <gonzodzen@mail.ru>
2023-05-26 17:52:42 +0300

    Javascript filter - 2

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-function coinsNeeded(c,ar,a=ar.sort((a,b)=>a-b),s=0){
-    for(let i=a.length-1;c;i--){
-      while(c>=a[i]){
-          let t=Math.floor(c/a[i])
-          c-=t*a[i]
-          s+=t
-      }
-    }
-    return s
-  }
-
-console.log(coinsNeeded(1232,[7,3,1,27]))
-console.log(coinsNeeded(6561797970922670,[576,48,24,4,1,192,12]))
+roomMates=(r,f,c=(f-1)*6)=>r.slice(c,c+6).filter(e=>e)
+  
+  console.log(roomMates([ "Jill", "Jackson", "Jan", "Eve", "", "John", "Jimmy", "Tom", "", "Duke" ],2))
