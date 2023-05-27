0cca99304767413fd91d599943134811b9933236
Jhonny <gonzodzen@mail.ru>
2023-05-27 17:34:12 +0300

    Tick Toward

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function isDivisibleBy6(s,f=x=>BigInt(x),a=[]){
-  for(let i=-1;++i<10;)a.push(s.replace(/\*/,i))
-  return a.filter(e=>f(e)%f(6)===f(0))
+function tickToward(s,e,f=(x,y)=>x<y?x+=1:x>y?x-=1:x,a=[s]){
+  while(a[a.length-1].join``!==e.join``){
+    let t=a[a.length-1]
+    a.push([f(t[0],e[0]),f(t[1],e[1])])
+  }
+  return a
 }
 
-console.log(isDivisibleBy6("1*0"))
-console.log(isDivisibleBy6("34234*2"))
-console.log(isDivisibleBy6("1234567890123456789012345678*0"))
+console.log(tickToward([5,1],[5,-2]))
