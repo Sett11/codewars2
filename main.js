2a51813983d7acfcbf2e7b5a4634e1821bdfb2fa
Jhonny <gonzodzen@mail.ru>
2023-05-27 10:02:42 +0300

    Old Mobile Display

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function oragoo(s){
-    if(!s||!s.match(/[aiouey]/))return s
-    if(!s.match(/[aioue]/)&&s.includes('y'))return s.replace(/y/,'oragy')
-    if(s[0].match(/[aioeu]/))return 'orag'+s
-    return s.replace(/^[^aioue]+/g,e=>e+'orag')
+function mobileDisplay(n,p){
+    n=n<20?20:n,p=p<30?30:p,c=~~(n/100*p),w='*'.repeat(n),a=Array(c-2).fill('*'+' '.repeat(n-2)+'*')
+    a.push(w),a.unshift(w),a[Math.floor(a.length/2)-1]='*'+' '.repeat(Math.floor(n/2)-5)+'CodeWars *',a[a.length-2]='* '+'Menu'+' '+'Contacts *'
+    while(a[Math.floor(a.length/2)-1].length<n)a[Math.floor(a.length/2)-1]=a[Math.floor(a.length/2)-1].replace(/s/,'$& ')
+    while(a[a.length-2].length<n)a[a.length-2]=a[a.length-2].replace(/C/,' $&')
+    return a.join`\n`
 }
 
-console.log(oragoo('hi'))
+console.log(mobileDisplay(27,41))
