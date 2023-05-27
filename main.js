4c5481a363e7509412155b17e2032d7ffd727ffe
Jhonny <gonzodzen@mail.ru>
2023-05-27 21:28:45 +0300

    Same Array?

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-function cipherFromPlaintext(p,e,a=[...new Set(p.replace(/[^a-z]/gi,'').toLowerCase())],l='abcdefghijklmnopqrstuvwxyz',o={},c={}){
-  for(let i=-1;++i<l.length;)o[l[i]]=a[i]
-  o=Object.entries(o).map(e=>e.reverse())
-  for(let i=-1;++i<o.length;)c[o[i][0]]=o[i][1]
-  return [...e].map(e=>c[e]?c[e]:e).join``
+function same(a,b,f=x=>x.sort((a,b)=>a-b),r=x=>x.sort((a,b)=>a[0]-b[0]||a[1]-b[1])){
+  if(a.length!==b.length)return !1
+  for(let i=-1;++i<a.length;)f(a[i]),f(b[i])
+  return JSON.stringify(r(a))===JSON.stringify(r(b))
 }
 
-console.log(cipherFromPlaintext('Regardless highhandedly vacuously activated goodness crud hey less some much stark courageous ouch grunted and angelfish much crud fancifully cobra because before contrary the following admonishingly hey less bawdy insect wherever dragonfly swung more lobster and far clear narrowly immaculate amazing jeez staunchly under and jealous alas pert less incorrect cat drove greedily more that oversold goodness goodness more blandly steady yet or and abominable ridiculously flinched one the hence a one that because much frog but cagily much a tardily so rapid returned bucolically that that and condescending beheld a a misunderstood a quixotic factual.', 'jd ftib iu qowk looa.'))
+console.log(same([[2,5], [3,6]], [[5,2], [3,6]]))
