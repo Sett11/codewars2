7d2c86937d2806946bded5179f5c75df5d539a94
Jhonny <gonzodzen@mail.ru>
2023-05-27 20:37:28 +0300

    Random Substitution Cipher

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-const GIFTS={
-  '1': 'Toy Soldier',
-  '2': 'Wooden Train',
-  '4': 'Hoop',
-  '8': 'Chess Board',
-  '16': 'Horse',
-  '32': 'Teddy',
-  '64': 'Lego',
-  '128': 'Football',
-  '256': 'Doll',
-  '512': "Rubik's Cube"
-}
-function gifts(n,q=Object.keys(GIFTS).map(e=>+e).reverse(),a=[]){
-  for(let i=-1;++i<q.length;)if(n>=q[i])n-=q[i],a.push(GIFTS[q[i]])
-  return a.sort()
+function randomSub() {
+    let a='abcdefghijklmnopqrstuvwxyz',b=[...a],o={}
+    for(let i=-1;++i<b.length;){
+      let t=b[i],r=~~(Math.random()*b.length)
+      b[i]=b[r],b[r]=t
+    }
+    for(let i=-1;++i<a.length;)o[a[i]]=b[i]
+    return o
 }
 
-console.log(gifts(160))
+console.log(randomSub())
