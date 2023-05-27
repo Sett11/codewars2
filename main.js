7f24c8999d31b6796fbc8e1fa0fcb9f711badf47
Jhonny <gonzodzen@mail.ru>
2023-05-27 18:42:46 +0300

    Santa's Missing Gift List

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function getWinner(a,o={}){
-  for(let i=-1;++i<a.length;){
-    if(o[a[i]])o[a[i]]+=1
-    else o[a[i]]=1
-    if(o[a[i]]>a.length/2)return a[i]
-  }
-  return null
+const GIFTS={
+  '1': 'Toy Soldier',
+  '2': 'Wooden Train',
+  '4': 'Hoop',
+  '8': 'Chess Board',
+  '16': 'Horse',
+  '32': 'Teddy',
+  '64': 'Lego',
+  '128': 'Football',
+  '256': 'Doll',
+  '512': "Rubik's Cube"
 }
-console.log(getWinner(["A", "A", "A", "B", "B", "B", "A"]))
+function gifts(n,q=Object.keys(GIFTS).map(e=>+e).reverse(),a=[]){
+  for(let i=-1;++i<q.length;)if(n>=q[i])n-=q[i],a.push(GIFTS[q[i]])
+  return a.sort()
+}
+
+console.log(gifts(160))
