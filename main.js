74f379c996c1c0d0fc4d90dad9ae2c796d8079ce
Jhonny <gonzodzen@mail.ru>
2023-05-28 20:40:41 +0300

    Dreidel dreidel

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-function willThereEverBeARainbow(s,c='REDORANGEYELLOWGREENBLUEINDIGOVIOLET',f=x=>Object.entries([...x].reduce((a,c)=>(a[c]=a[c]+1||1,a),{})).sort((a,b)=>a[0].localeCompare(b[0]))){
-    if(s.length<c.length)return 'Too short for a rainbow!'
-    s=s.replace(/[^REDOANGYLWBUIVT]/gi,'')
-    const ch=f(c),w=f(s)
-    if(ch.length!==w.length)return 'No rainbow to be found!'
-    if(ch.every((e,i)=>e[0]===w[i][0]&&w[i][1]>=e[1]*2))return 'Double rainbow all the way!'
-    for(let i=-1;++i<ch.length;){
-        if(!w[i]||ch[i][0]!==w[i][0]||ch[i][1]>w[i][1])return 'No rainbow to be found!'
+function gamble(r,m,p){
+    for(let i=-1;++i<r.length;){
+        if(r[i]==='Gimel')m+=p,p-=p
+        if(r[i]==='Hei')m+=p/2,p-=p/2
+        if(r[i]==='Shin')m-=1,p+=1
     }
-    return 'Look! A rainbow!'
+    return ~~m
 }
 
-console.log(willThereEverBeARainbow('REDORANGEYELLOWGREENBLUEINDIGOVIOLETREDORANGEYELLOWGREENBLUEINDIGOVIOLET'))
+console.log(gamble(['Hei', 'Shin'], 10, 20))
