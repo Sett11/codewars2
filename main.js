6554b03993a1ca07cc791fcc9940f38d508f77cc
Jhonny <gonzodzen@mail.ru>
2023-05-29 23:15:50 +0300

    Formatting a number as price

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-function flip(d,a){
-    if(d==='R')return a.map(e=>e.sort((a,b)=>a-b))
-    if(d==='L')return a.map(e=>e.sort((a,b)=>b-a))
-    const f=(x,r=[])=>{
-        for(let i=-1;++i<x[0].length;){
-            let t=[]
-            for(let j=-1;++j<x.length;)t.push(x[j][i])
-            r.push(t)
-        }
-        return r
-    }
-    return d==='U'?f(f(a).map(e=>e.sort((a,b)=>b-a))):f(f(a).map(e=>e.sort((a,b)=>a-b)))
+numberToPrice=(...z)=>{
+  let n=z+'',s=''
+  if(z.length>1||typeof z[0]!=='number')return 'NaN'
+  if(!n.match(/\./))n+='.00'
+  if(n.slice(n.indexOf('.')+1).length<2)n+='0'
+  if(n[0]==='-')s='-',n=n.slice(1)
+  let a=n.slice(0,n.indexOf('.')),b=n.slice(n.indexOf('.')+1)
+  b=b.slice(0,2)
+  a=[...a].reverse().join``,r=[]
+  for(let i=0;i<a.length;i+=3)r.push(a.slice(i,i+3))
+  return s+[...r.join`,`].reverse().join``+'.'+b
 }
 
-console.log(flip('U',[[1, 3, 2], [4, 5, 1], [6, 5, 3], [7, 2, 9]]))
+console.log(numberToPrice(13253.5123))
+console.log(numberToPrice(-13253.5123))
+console.log(numberToPrice(13253))
+console.log(numberToPrice(13253.1))
+console.log(numberToPrice(1000000.5))
+console.log(numberToPrice(5.1,2))
