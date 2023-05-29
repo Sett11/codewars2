12ff3c43601e462fe0b7c5a3724e265241f37111
Jhonny <gonzodzen@mail.ru>
2023-05-29 17:30:12 +0300

    Memoized Fibonacci

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-class Primes{
-    static first(x){
-        return r.slice(0,x)
+fibonacci=n=>{
+    const f=x=>{
+        if(!x)return [0,1]
+        if(x===1)return [1,1]
+        const [a,b]=f(Math.floor(x/2)),p=a*(2*b-a),q=b*b+a*a
+        return !(x%2)?[p,q]:[q,p+q]
     }
+    return n>=0?f(n)[0]:!(n%2)?-f(-n)[0]:f(-n)[0]
 }
+console.log(fibonacci(50))
