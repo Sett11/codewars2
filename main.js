049a7b9c1d6c36b41c3b6cafaefe69d265000054
Jhonny <gonzodzen@mail.ru>
2023-05-31 21:05:13 +0300

    groupBy

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-Array.prototype.groupBy=function groupBy(f=e=>e){
-    let a=this.map(e=>[f(e),e]).sort((a,b)=>a[0]-b[0]),r=[],o={}
-    for(let i=-1;++i<a.length;)if(a[i][0]!==(a[i+1]||[])[0])r.push(a.slice(0,i+1)),a=a.slice(i+1),i=-1
-    r.forEach(e=>o[e[0][0]]=e.map(u=>u[1]))
-    return o
+let secondArg='',firstArg=''
+Array.prototype.groupBy=function groupBy(f=(v,w)=>v===w){
+  console.log(this,f.toString())
+    const a=[]
+    for(let i=-1;++i<this.length;){
+        for(let j=i-1;++j<=this.length;){
+            if(!f((this[j]===undefined?'':this[j]),(this[j+1]===undefined?'':this[j+1]))||j===this.length)a.push(this.slice(i,j+1)),i=j+1
+        }
+    }
+    return a.filter(e=>e.length)
 }
+Object.defineProperty(Array.prototype,"groupBy",{enumerable:false})
 
 console.log([1,2,3,2,4,1,5,1,6].groupBy())
 console.log([1,2,3,2,4,1,5,1,6].groupBy(function(val) { return val % 3;} ))
