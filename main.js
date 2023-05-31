dbeb56e3364c7166f103d3c0b1e6c6cf10a1a4b7
Jhonny <gonzodzen@mail.ru>
2023-05-31 18:23:35 +0300

    Survivors Ep.4

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-function door(s,a=[...s]){
-  for(let i=-1,c=0,v=0,t=0,f=x=>x>5?5:x<0?0:x;++i<a.length;){
-    if(a[i]==='P')v=v?0:1
-    if(a[i]==='O')t=t?0:1,v=v?0:v
-    if(v)c++
-    if(t)c--
-    a[i]=f(c)
-  }
-  a=a.join``
-  return a==='12345555555432'?'12345554345555':a==='12345555555555'?'12345554321000':a==='0000012345555555555555'?'0000012345554333321000':a==='123455555555555432'?'123455543233334555':a
+function survivors(n,a){
+    return n.map((e,j)=>{
+      if(e+a[j].reduce((a,c)=>a+c,0)<=a[j].length)return '&'
+        let t=[]
+        for(let i=-1;++i<a[j].length;){
+            if(e<1)break
+            e-=1,e+=a[j][i],t.push(a[j][i])
+        }
+        return t.length===a[j].length?j:'&'
+    }).filter(e=>e!=='&')
 }
-
-console.log(door('..P...O.....'))
-console.log(door('P.P.P....'))
-console.log(door('P......P......'))
-console.log(door('P.P..'))
+console.log(survivors([3, 2, 1, 0],[[1, 0, 0], [0, 2, 0, 0], [0, 9], [8, 8]]))
