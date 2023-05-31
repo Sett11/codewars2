2f6f20d2638493d53c4a2faa9f075a0d8c94177c
Jhonny <gonzodzen@mail.ru>
2023-06-01 00:08:32 +0300

    Row of the odd triangle

--- main.js
+++ main.js
@@ -1,39 +1,9 @@
-function funnelOut(b,a=b.reverse(),s=''){
-    if(a.length===1)return a[0][0]
-    while(!a.every(e=>e.every(u=>!u))){
-        s+=a[0][0],a[0][0]=''
-        for(let i=-1;++i<a.length-1;){
-            for(let j=-1;++j<a[i].length;){
-                if(!a[i][j]){
-                   let t=[[a[i+1][j],[i+1,j]],[a[i+1][j+1],[i+1,j+1]]].sort((a,b)=>a[0].localeCompare(b[0])).filter(e=>e[0])[0]
-                   if(t)a[i][j]=t[0],a[t[1][0]][t[1][1]]=''
-                }
-            }
-        }
-    }
-  return s==='SQNJEFAKGBcdelmfnOLHCgoRPMIDhptuvwxqiyrjabksz'?'SQNJEFAKGBOLHCRPMIDcdefglmnohptkbsuvwxqiyrjaz':s
+function oddRow(n,r=[],c=1){
+    for(let i=0;++i<n;)c+=i*2
+    r=[c]
+    for(let i=0;++i<n;)r.push(c+=2)
+    return r
 }
 
-console.log(funnelOut([
-    [
-      'a', 'b', 'c',
-      'd', 'e', 'f',
-      'g', 'h', 'i'
-    ],
-    [
-      'j', 'k', 'l',
-      'm', 'n', 'o',
-      'p', 'q'
-    ],
-    [
-      'r', 's', 't',
-      'u', 'v', 'w',
-      'x'
-    ],
-    [ 'y', 'z', 'A', 'B', 'C', 'D' ],
-    [ 'E', 'F', 'G', 'H', 'I' ],
-    [ 'J', 'K', 'L', 'M' ],
-    [ 'N', 'O', 'P' ],
-    [ 'Q', 'R' ],
-    [ 'S' ]
-  ]))
+console.log(oddRow(2))
+console.log(oddRow(141360))
