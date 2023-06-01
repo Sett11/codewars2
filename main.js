26a7a193ec801d8d6447bcff479df06a2d171633
Jhonny <gonzodzen@mail.ru>
2023-06-01 14:48:31 +0300

    Give The Biggest Prime Factor And The Biggest Divisor Of A Number

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-const f=(n,x=n,i=2,a=[])=>{
-    while(i*i<=x){
-        while(x%i===0)x/=i,a.push(i)
-        i++
-    }
-    if(x!==1&&x!==n)a.push(x)
-    return a
-}
 const r=(x,a=[])=>{
-    for(let i=0;++i<=Math.sqrt(x);)if(!(x%i))a.push(i,x/i)
+    for(let i=1;++i<=Math.sqrt(x);)if(!(x%i))a.push(i,x/i)
     return a
 }
-const dsMultofPfs=(a,b,q=[])=>{
-    for(let i=a-1;++i<=b;)if(eval(r(i).join`+`)%eval(f(i).join`+`)===0)q.push(i)
-    return q
+const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<=Math.sqrt(x);i++){if(x%i===0)return false}return true}
+function bigPrimefacDiv(n){
+    if(!Number.isInteger(n)&&t.slice(t.indexOf('.')+1).some(e=>e!=='0'))return 'The number has a decimal part.'
+    n=Math.abs(n),t=r(n)
+    if(f(n))return []
+    return [Math.max(...t.filter(e=>f(e))),Math.max(...t)]
 }
-
-console.log(dsMultofPfs(20,120))
+console.log(bigPrimefacDiv(1969))
+console.log(bigPrimefacDiv(100))
+console.log(bigPrimefacDiv(372748824))
