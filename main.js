c4e4069df3b2ffb132a667d4ab9090c3958b81c2
Jhonny <gonzodzen@mail.ru>
2023-06-01 09:51:43 +0300

    Steps in k-primes

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-function simplestProblem(a){
-    return a[0].map((_,i)=>a.map(u=>u[i]).map((u,k)=>[u,Math.min(...a[k]),Math.max(...a[k]),i])).filter(e=>e.every(u=>u[0]!==u[2])&&e.filter(u=>u[0]===u[1]).length>a.length/2).map(e=>(e||[]).map(u=>u[3])[0])
+const f=(x,i=2,j=0)=>{
+    while(i*i<=x){
+        while(x%i===0)x/=i,j++
+        i++
+    }
+    return x===1?j:j+1
+}
+function  kprimesStep(k,h,s,e) {
+    return Array(e-s+1).fill(0).map((_,i)=>s+i).filter(e=>f(e)===k).filter((e,_,v)=>v.includes(e+h)).map(e=>[e,e+h])
 }
 
-console.log(simplestProblem([
-    [1,1,1,2],
-    [5,90,21,40],
-    [10,10,9,10],
-    [3,4,3,5]
-    ]))
-
-console.log(simplestProblem([
-    [1,1,1,100],
-    [4,4,4,100],
-    [5,5,5,100],
-    [7,7,7,100]
-    ]))
-
-console.log(simplestProblem([
-    [1,2,1,1],
-    [4,3,3,3],
-    [5,5,6,5],
-    [7,7,8,8]
-    ]))
+console.log(kprimesStep(2, 2, 0, 50))
+console.log(kprimesStep(6, 8, 2627371, 2627581))
