f1450b33281950eea5dba509065b4876b3894d91
Jhonny <gonzodzen@mail.ru>
2023-06-01 22:03:55 +0300

    Raise Me to The Third Power, Search My Divisors... .....Could You Believe that?

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-function genfib(a=[0,1],c=0){
-    return function(){
-        if(c<2)return a[c++]
-        a.push(a[a.length-1]+a[a.length-2])
-        return a[a.length-1]
+class M{
+    constructor(a=[6]){
+        this.a=a
+    }
+    f(x,r=[]){
+        for(let i=0;++i<=Math.sqrt(x);)if(!(x%i))r.push(i,x/i)
+        return [...new Set(r)]
+    }
+    c(n){
+        if(this.a.length>=n)return this.a[n-1]
+        let c=this.a[this.a.length-1]+1
+        while(this.a.length<n){
+            if((c**3)%eval(this.f(c).join`+`)===0)this.a.push(c)
+            c++
+        }
+        return this.a[n-1]
     }
 }
-const f=genfib()
-console.log(f())
-console.log(f())
-console.log(f())
-console.log(f())
-console.log(f())
-console.log(f())
-console.log(f())
-console.log(f())
+const z=new M()
+intCubeSumDiv=n=>z.c(n)
+
+
+console.log(intCubeSumDiv(7))
+console.log(intCubeSumDiv(4))
+console.log(intCubeSumDiv(5))
