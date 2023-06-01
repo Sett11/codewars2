49cb8148622c11997963dd619fe6e2a28c8d20f9
Jhonny <gonzodzen@mail.ru>
2023-06-01 20:40:14 +0300

    Fibonacci Generator

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-class F{
-    constructor(a=[0,1]){
-        this.a=a
-    }
-    c(){
-        this.a.push(this.a[this.a.length-1]+this.a[this.a.length-2])
-        return this.a[this.a.length-1]
+function genfib(a=[0,1],c=0){
+    return function(){
+        if(c<2)return a[c++]
+        a.push(a[a.length-1]+a[a.length-2])
+        return a[a.length-1]
     }
 }
-const r=new F()
-function *fibonacci(){
-    yield 0
-    yield 1
-    while(1)yield r.c()
-}
-const fib=fibonacci()
-console.log(fib.next())
-console.log(fib.next())
-console.log(fib.next())
-console.log(fib.next())
-console.log(fib.next())
-console.log(fib.next().value)
+const f=genfib()
+console.log(f())
+console.log(f())
+console.log(f())
+console.log(f())
+console.log(f())
+console.log(f())
+console.log(f())
+console.log(f())
