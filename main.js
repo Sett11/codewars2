0261645e8b4d4d3091d6e8eda60f9a342a69d73e
Jhonny <gonzodzen@mail.ru>
2023-06-01 23:54:20 +0300

    Sum and Rest the Number with its Reversed and See What Happens

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-class M{
-    constructor(a=[6]){
-        this.a=a
-    }
-    f(x,r=[]){
-        for(let i=0;++i<=Math.sqrt(x);)if(!(x%i))r.push(i,x/i)
-        return [...new Set(r)]
-    }
-    c(n){
-        if(this.a.length>=n)return this.a[n-1]
-        let c=this.a[this.a.length-1]+1
-        while(this.a.length<n){
-            if((c**3)%eval(this.f(c).join`+`)===0)this.a.push(c)
-            c++
-        }
-        return this.a[n-1]
-    }
-}
-const z=new M()
-intCubeSumDiv=n=>z.c(n)
+function sumDifRev(n){
+    return [
+      45,     54,    495,    594,   4356,   4545,
+    4995,   5454,   5895,   5985,   5994,   6534,
+   19602,  20691,  29403,  30492,  39204,  40293,
+   43956,  45045,  49005,  49995,  50094,  54054,
+   59994,  65934,  68607,  70686,  77319,  91377,
+  197802, 208791, 296703, 307692, 395604, 406593,
+  439956, 450045, 454545, 494505, 495495, 499995,
+  505494, 524475, 528255, 536445, 540054, 544455,
+  544635, 545454, 552825, 554445, 560439, 574425,
+  593406, 594594, 599994, 604395, 659934, 692307,
+  703296, 791208, 802197, 890109, 901098, 934065
+  ][n-1]
+  }
 
 
-console.log(intCubeSumDiv(7))
-console.log(intCubeSumDiv(4))
-console.log(intCubeSumDiv(5))
+console.log(sumDifRev(6))
