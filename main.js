a610c595cbd3f726f4b541ba7f34953010aa5f2d
Jhonny <gonzodzen@mail.ru>
2023-06-01 14:02:41 +0300

    Highest number with two prime factors

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ const f=(n,x=n,i=2,a=[])=>{
     if(x!==1&&x!==n)a.push(x)
     return a
 }
-const r=x=>{if(x<2)return !1;if(x===2)return !0;for(let i=1;++i<=Math.sqrt(x);){if(!(x%i))return !1}return !0}
-const multPrimefactorSum=(s,e)=>Array(e-s+1).fill(0).map((e,i)=>i+s).filter(e=>!r(e)&&!(e%eval(f(e).join`+`)))
+const highestBiPrimeFac=(a,b,e,y=f(e))=>{
+    if([...new Set(y)].join``===[a,b].join``)return [e,y.filter(e=>e===a).length,y.filter(e=>e===b).length]
+    for(let i=e;i--;){
+        let t=f(i)
+        if([...new Set(t)].join``===[a,b].join``)return [i,t.filter(e=>e===a).length,t.filter(e=>e===b).length]
+    }
+}
 
-console.log(multPrimefactorSum(1,60))
+console.log(highestBiPrimeFac(2,3,50))
+console.log(highestBiPrimeFac(5,11,1000))
+console.log(highestBiPrimeFac(5,7,5000))
+console.log(highestBiPrimeFac(83, 367, 28263924862181))
+console.log(highestBiPrimeFac(7, 677, 22458121))
