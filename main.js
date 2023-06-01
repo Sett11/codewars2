fe249f5605270753953c22f3b3ee2e5d8a123ac1
Jhonny <gonzodzen@mail.ru>
2023-06-01 20:21:31 +0300

    Fibonacci Generator Function

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function* generator(a,b=0){
-  while(1)yield(`${a} x ${++b} = ${a*b}`)
+class F{
+    constructor(a=[0,1]){
+        this.a=a
+    }
+    c(){
+        this.a.push(this.a[this.a.length-1]+this.a[this.a.length-2])
+        return this.a[this.a.length-1]
+    }
 }
-const r=generator(1)
-console.log(r.next().value)
-console.log(r.next().value)
-console.log(r.next().value)
+const r=new F()
+function *fibonacci(){
+    yield 0
+    yield 1
+    while(1)yield r.c()
+}
+const fib=fibonacci()
+console.log(fib.next())
+console.log(fib.next())
+console.log(fib.next())
+console.log(fib.next())
+console.log(fib.next())
+console.log(fib.next().value)
