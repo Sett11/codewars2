4b60c696c96c65a0f66c21721a6c480db88a93fb
Jhonny <gonzodzen@mail.ru>
2023-06-02 17:12:09 +0300

    VaccineNation

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function unionJack(x,n=Math.ceil(x<7?7:x),q=Math.floor(n/2),p=!(n&1)?q-1:NaN){
-    return typeof x!=='number'?!1:Array(n).fill('').map((e,i,_)=>Array(n).fill('-').map((u,j,_)=>i===j||j+i===n-1||i===q||j===q||i===p||j===p?'X':u).join``).join`\n`
+const o={fiveInOne : ['8 weeks', '12 weeks', '16 weeks'],
+pneumococcal : ['8 weeks', '16 weeks'],
+rotavirus : ['8 weeks', '12 weeks'],
+meningitisB : ['8 weeks', '16 weeks', '12 months'],
+hibMenC : ['12 months'],
+measlesMumpsRubella : ['12 months', '40 months'],
+fluVaccine : ['september','october','november'],
+preSchoolBooster : ['40 months']}
+
+function vaccineList(a,s,m,r=[]){
+  for(let i in o)for(let j=-1;++j<o[i].length;)if(o[i][j]===a||o[i][j]===s||o[i][j]===m)r.push(i)
+  return [...new Set(r)].map(e=>e==='fluVaccine'?'offer fluVaccine':e).sort()
 }
 
-console.log(unionJack(4.5))
+console.log(vaccineList('12 weeks','up-to-date','december'))
+console.log(vaccineList('12 months','16 weeks','june'))
+console.log(vaccineList('40 months','12 months','october'))
