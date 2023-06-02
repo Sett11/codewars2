4f98a85e9e060c5ca610d01bbdca6aa1adc6ca31
Jhonny <gonzodzen@mail.ru>
2023-06-02 22:33:42 +0300

    Scrabble best word

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-fizzBuzzFactory=a=>
-   n=>{
-    for(let i=a.length;--i>=0;)if(!(n%a[i][0]))return a[i][1]
-    return n+''
-  }
+getBestWord=(p,w)=>w.map((e,i)=>[eval([...e].map(u=>p[u.charCodeAt()-65]).join`+`),e.length,i]).sort((a,b)=>b[0]-a[0]||a[1]-b[1]||a[2]-b[2])[0][2]
 
-const r=fizzBuzzFactory([[2, "Foo"], [4, "Bar"], [6, "FooBar"]])
-console.log(r(12))
+
+console.log(getBestWord([1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10],["WHO","IS","THE","BEST","OF","US"]))
+console.log(getBestWord([1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10],["NOQ","TXAY","S","OM","ESFT","CJUKQ","QL","QO","ASTK","Y"]))
+console.log(getBestWord([1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10],["JGPCWVWFW","JXHNKBJJG"]))
