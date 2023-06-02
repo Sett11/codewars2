f299d4b999a2b91a350ac2883efe5b32ed5e34b7
Jhonny <gonzodzen@mail.ru>
2023-06-02 16:48:38 +0300

    The Union Jack

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-function myCrib(n,t=7+6*(n-1),l=t-((n*2+1)+(n*2+1))){
-    const h=Array(n*2).fill('').map((e,i,_)=>{
-        e=' '.repeat(n*2-i-1)+'/_\\'+' '.repeat(n*2-i-1)
-        while(e.length<t)e=e.replace(/\//,'$&_')
-        return e
-    })
-    h.unshift(' '.repeat(n*2)+'_'.repeat(n*2+1)+' '.repeat(n*2))
-    const b=Array(n*2).fill('').map((e,i,v)=>{
-        e='| |'
-        while(e.length<t)e=e.replace(/\| /,'$& ')
-        if(i===v.length-1){
-            e=e.replace(/ /g,'_'),e=e.slice(0,n*2)+'|'+'_'.repeat(l)+'|'+e.slice(n*2+l+2)
-        }
-        if(i===n-1)e='|'+' '.repeat(n*2)+'_'.repeat(l)+' '.repeat(n*2)+'|'
-        if(i>=n&&i!==v.length-1)e=e.slice(0,n*2)+'|'+' '.repeat(l)+'|'+e.slice(n*2+l+2)
-        return e
-    })
-    return h.concat(b).join`\n`
+function unionJack(x,n=Math.ceil(x<7?7:x),q=Math.floor(n/2),p=!(n&1)?q-1:NaN){
+    return typeof x!=='number'?!1:Array(n).fill('').map((e,i,_)=>Array(n).fill('-').map((u,j,_)=>i===j||j+i===n-1||i===q||j===q||i===p||j===p?'X':u).join``).join`\n`
 }
 
-console.log(myCrib(2))
+console.log(unionJack(4.5))
