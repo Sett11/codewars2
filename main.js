ea44d4424fdaa2d4b0efcd366e496d10982e8689
Jhonny <gonzodzen@mail.ru>
2023-06-02 21:56:10 +0300

    Walter's miraculous FizzBuzz factory

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-const o={fiveInOne : ['8 weeks', '12 weeks', '16 weeks'],
-pneumococcal : ['8 weeks', '16 weeks'],
-rotavirus : ['8 weeks', '12 weeks'],
-meningitisB : ['8 weeks', '16 weeks', '12 months'],
-hibMenC : ['12 months'],
-measlesMumpsRubella : ['12 months', '40 months'],
-fluVaccine : ['september','october','november'],
-preSchoolBooster : ['40 months']}
+fizzBuzzFactory=a=>
+   n=>{
+    for(let i=a.length;--i>=0;)if(!(n%a[i][0]))return a[i][1]
+    return n+''
+  }
 
-function vaccineList(a,s,m,r=[]){
-  for(let i in o)for(let j=-1;++j<o[i].length;)if(o[i][j]===a||o[i][j]===s||o[i][j]===m)r.push(i)
-  return [...new Set(r)].map(e=>e==='fluVaccine'?'offer fluVaccine':e).sort()
-}
-
-console.log(vaccineList('12 weeks','up-to-date','december'))
-console.log(vaccineList('12 months','16 weeks','june'))
-console.log(vaccineList('40 months','12 months','october'))
+const r=fizzBuzzFactory([[2, "Foo"], [4, "Bar"], [6, "FooBar"]])
+console.log(r(12))
