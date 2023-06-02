7eaabf5099efa822846eeab1f9d629a1b6eb9dcc
Jhonny <gonzodzen@mail.ru>
2023-06-02 23:02:18 +0300

    UN-usual Sort

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-getBestWord=(p,w)=>w.map((e,i)=>[eval([...e].map(u=>p[u.charCodeAt()-65]).join`+`),e.length,i]).sort((a,b)=>b[0]-a[0]||a[1]-b[1]||a[2]-b[2])[0][2]
+function unusualSort(w,a=[],b=[],c=[]){
+  w.forEach(e=>typeof e==='string'&&+e!==+e?a.push(e):typeof e==='string'&&+e===+e?b.push(e):c.push(e))
+  return a.sort().concat(c.concat(b).sort())
+}
 
-
-console.log(getBestWord([1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10],["WHO","IS","THE","BEST","OF","US"]))
-console.log(getBestWord([1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10],["NOQ","TXAY","S","OM","ESFT","CJUKQ","QL","QO","ASTK","Y"]))
-console.log(getBestWord([1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10],["JGPCWVWFW","JXHNKBJJG"]))
+console.log(unusualSort([3,"3","2",2,"2","1",1,"a","b","c"]))
+console.log(unusualSort(["a","Z","B"]))
+console.log(unusualSort([1,2,3,'1','2','3']))
