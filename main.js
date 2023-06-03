050fc0bd39ba104ddfea3208c0db56191575942b
Jhonny <gonzodzen@mail.ru>
2023-06-03 14:44:33 +0300

    Around Fibonacci: chunks and counts

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-const f=x=>Object.values(x).reduce((a,c)=>a.concat(typeof c==='string'?[c]:f(Object.values(c))),[])
-const q=x=>{
-  for(let i=-1;++i<x.length;){
-    let v=Math.floor(Math.random()*x.length),t=x[i]
-    x[i]=x[v],x[v]=t
-  }
-  return x
+aroundFib=n=>{
+  let a=BigInt(0),b=BigInt(1),c=BigInt(0)
+  while(n>0){n--,c=b,b+=a,a=c;}
+  let s=a+'',r=Object.entries([...s].reduce((a,c)=>(a[c]=a[c]+1||1,a),{})).map((e,i)=>[e[0],e[1],i]).sort((a,b)=>b[1]-a[1]||a[2]-b[2])[0],v=''
+  for(let i=0;i<s.length;i+=25)v=s.slice(i,i+25)
+  return `Last chunk ${v}; Max is ${r[1]} for digit ${r[0]}`
 }
-Object.prototype.random=function(){
-  const w=this.toRandomArray()[0]
-  return typeof w==='object'?Object.values(w)[0]:w
-}
-Object.prototype.toRandomArray=function(){
-  const r=Object.values(Object.values(this))
-  r[0]=typeof r[0]==='object'?Object.values(r[0])[0]:r[0]
-	return r.length===1?r.filter(e=>e):q(f(this))
-}
-const o={ w: 'a', q: 'b', e: { w: { q: 'c' } } }
-const r={ a: 1 }
-console.log(o.random())
-console.log(o.toRandomArray())
-console.log(r.random())
-console.log(r.toRandomArray())
+
+console.log(aroundFib(666))
