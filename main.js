fb46ea7ceac3a4581db7311d657344dd0677de47
Jhonny <gonzodzen@mail.ru>
2023-06-03 09:45:28 +0300

    Sequence generator

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-const f=x=>{
-  for(let i=-1;++i<x.length/2;){
-    let v=Math.floor(Math.random()*x.length),t=x[i]
-    x[i]=x[v],x[v]=t
-  }
-  return x
-}
-class Randomizer{
-  constructor(s=0,e=9){
-    this.s=s
-    this.e=e
-    this.n=f(Array(e-s+1).fill(1).map((_,i)=>i+s))
-  }
-  next(){
-  if(this.n.length===0)throw new Error('No more numbers')
-  let t=Math.floor(Math.random()*this.n.length)
-	return this.n.splice(t,1)[0]
-  }
-  sequence(s){
-	if (s===undefined)s=this.n.length
-	if(s>this.n.length||!s)throw new Error('Too many numbers')
-	return this.n.splice(0,s)
+function* sequenceGen(...a){
+  const f=x=>x===undefined?1:x
+  let c=0
+  while(c<a.length)yield a[c++]
+  while(1){
+    a.push(a.slice(-c).reduce((a,c)=>a+c,0))
+    yield f(a[a.length-1])
   }
 }
-const r=new Randomizer(0,9)
-console.log(r.next())
-console.log(r.sequence(2))
-console.log(r)
+const fib=sequenceGen(0,1)
+console.log(fib.next().value)
+console.log(fib.next().value)
+console.log(fib.next().value)
+console.log(fib.next().value)
+console.log(fib.next().value)
+console.log(fib.next().value)
+console.log(fib.next().value)
