2c403a8fe5f3c2ef231beadade2eb9314370416d
Jhonny <gonzodzen@mail.ru>
2023-06-05 00:00:09 +0300

    Minesweeper

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-const f=(x,r=[0])=>{
-  a=0,b=1,c=0
-  while(a<x){
-    c=b,b+=a,a=c
-    r.push(a)
+const board=s=>{
+  if(s.some((e,i,v)=>e.length!==v[0].length))throw Error('Error rows length')
+  let c=[...s[0].slice(1,-1)+s[s.length-1].slice(1,-1)],w=s.map((e,i,v)=>i&&i!==v.length-1?e.charAt(0)+e.charAt(e.length-1):0).filter(e=>e)
+  if(c.some(e=>e!=='-')||c.length!==(s[0].length-2)*2||w.some(e=>e!=='||'))throw Error('Border is not closed')
+  if(s.join``.match(/[^ \-\+\*\|]/))throw Error('Invalid characters')
+  s=s.map(e=>[...e])
+  for(let i=0;++i<s.length-1;){
+    for(let j=-1;++j<s[i].length;){
+      if(s[i][j]==='*'||s[i][j]==='|')continue
+      let t=[[i,j+1],[i,j-1],[i+1,j],[i-1,j],[i+1,j+1],[i-1,j-1],[i+1,j-1],[i-1,j+1]].filter(e=>e[0]>-1&&e[1]>-1&&e[1]<s[i].length)
+      t=t.filter(e=>s[e[0]][e[1]]==='*')
+      if(t.length)s[i][j]=t.length
+    }
   }
-  return r
-}
-function zeckendorf(n,q=x=>eval(x.join`+`)){
-  const r=f(n).filter(e=>e<n).reverse(),a=[r[0]]
-  for(let i=0;++i<r.length;){
-    a.push(r[i])
-    let t=q(a)
-    if(t>n)a.pop()
-    if(t===n)return a
-  }
-  return n===1?[1]:!n?[]:null
+  return s.map(e=>e.join``)
 }
 
-console.log(zeckendorf(1))
+console.log(board(["+------+",
+"| *  * |",
+"|  *   |",
+"|    * |",
+"|   * *|",
+"| *  * |",
+"|      |",
+"+------+"]))
