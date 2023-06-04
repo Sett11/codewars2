39d5c3e0b69f255d21ce7a3d5cad3989e2e183fd
Jhonny <gonzodzen@mail.ru>
2023-06-04 13:22:43 +0300

    Zeckendorf representation

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-calculateWinners=(s,a,r=s.toLowerCase().split`\n`.map((e,i)=>[e.slice(e.indexOf('p'),-1).replace(/~/g,'--').length,a[i]]).sort((a,b)=>a[0]-b[0]).map(e=>e[1]))=>`GOLD: ${r[0]}, SILVER: ${r[1]}, BRONZE: ${r[2]}`
+const f=(x,r=[0])=>{
+  a=0,b=1,c=0
+  while(a<x){
+    c=b,b+=a,a=c
+    r.push(a)
+  }
+  return r
+}
+function zeckendorf(n,q=x=>eval(x.join`+`)){
+  const r=f(n).filter(e=>e<n).reverse(),a=[r[0]]
+  for(let i=0;++i<r.length;){
+    a.push(r[i])
+    let t=q(a)
+    if(t>n)a.pop()
+    if(t===n)return a
+  }
+  return n===1?[1]:!n?[]:null
+}
 
-console.log(calculateWinners(`|----p---~---------|
-|----p---~~--------|
-|----p---~~~-------|`,["Derek", "Francis", "Bob"]))
+console.log(zeckendorf(1))
