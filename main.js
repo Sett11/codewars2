18e4ea5e162819db401b6d9de5d5f52a7f37231f
Jhonny <gonzodzen@mail.ru>
2023-06-05 16:11:20 +0300

    Node Mania

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-function CaesarCryptoEncode(s,h,a='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',f=x=>x.toLowerCase()){
-  return !s||!s.match(/[a-z0-9]/i)?'':[...s].map(e=>{
-    if(!a.includes(f(e)))return e
-    e=[(a.indexOf(f(e))+h)%a.length,e<{}]
-    if(e[0]<0)e[0]+=a.length
-    e[0]=a[e[0]]
-    return e[1]&&e[0].toUpperCase()===e[0]?e[0].toLowerCase():e[1]?e[0].toUpperCase():e[0]
-  }).join``
+class Node {
+  next;  
+  constructor(item) {
+    this.data = item;
+    this.next = null;
+  }
+  toString() {
+    if (!this.data) return "";
+    let str = this.data;
+    let n = this.next;
+    while (n != null) {
+      str += " -> " + n.data;
+      n = n.next;
+    }
+    return str;
+  }
+  static build(values) { 
+    if (!values || !values.length) return null;
+    let head, tail
+    
+    if (values.length) head = tail = new Node(values[0]);
+    
+    for (let i = 1; i < values.length; i++) {
+      tail.next = new Node(values[i]);
+      tail = tail.next;
+    }
+    return head;
+  }
 }
 
-console.log(CaesarCryptoEncode("Hw wx, Euxwh?",-3))
+function searchKFromEnd(l,k,a=[]){
+  while(l&&l.data){
+    a.push(l.data)
+    l=l.next
+  }
+  return k>a.length?null:a[a.length-k]
+}
+
+console.log(searchKFromEnd(Node.build([5,2,3,1]),3))
