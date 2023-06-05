865a3cc21b07b05cd33e4a42dc5a84a3c5130cd7
Jhonny <gonzodzen@mail.ru>
2023-06-06 00:07:52 +0300

    Cumulative Triangle

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
-const f=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
-const q=x=>+[...x+''].slice(0,2).join``
-const w=x=>[...x+''].reverse().slice(0,2).join``
 class M{
-  constructor(){
-    this.a=[1176]
-  }
-  c(a,b){
-    let v=this.a.findIndex(e=>e>a)
-    if(v!==-1){
-      let t=this.a.findIndex(e=>e>b)
-      if(t!==-1)return this.a.slice(v,t).length
+  constructor(){this.a=[[1],[2,3],[4,5,6],[7,8,9,10]]}
+  c(n){
+    while(this.a.length<n){
+      let v=this.a[this.a.length-1],t=v[v.length-1],q=Array(v.length+1).fill(1).map((e,i)=>e+i+t)
+      this.a.push(q)
     }
-    let i=this.a[this.a.length-1]+1
-    while(i<b){
-      if(f(q(i))&&f(q(i*i))&&w(i)===w(i*i))this.a.push(i)
-      i++
-    }
-    let x=this.a.findIndex(e=>e>a)
-    return this.a.slice(x<0?0:x).length
+    return eval(this.a[n-1].join`+`)
   }
 }
 const r=new M()
-const solve=(a,b)=>r.c(a,b)
+cumulativeTriangle=n=>r.c(n)
 
-console.log(solve(2,1200))
-console.log(solve(2,1000000))
-console.log(solve(2,100000))
+console.log(cumulativeTriangle(10100))
