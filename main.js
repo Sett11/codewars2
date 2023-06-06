954ea994f35ad9cb4ed7be8bee78e2118ef33224
Jhonny <gonzodzen@mail.ru>
2023-06-06 09:07:05 +0300

    Required Data II (Easy One)

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-class M{
-  constructor(){this.a=[[1],[2,3],[4,5,6],[7,8,9,10]]}
-  c(n){
-    while(this.a.length<n){
-      let v=this.a[this.a.length-1],t=v[v.length-1],q=Array(v.length+1).fill(1).map((e,i)=>e+i+t)
-      this.a.push(q)
-    }
-    return eval(this.a[n-1].join`+`)
-  }
+function givenNthValue(a,k,c,s=c.toLowerCase()){
+  return k<0||+k!==+k||!Number.isInteger(k)?"Incorrect value for k":!['min','max'].includes(s)?"Valid entries: 'max' or 'min'":a.some(e=>!Number.isInteger(e))?"Invalid entry list":!a.length?"No values in the array":s==='min'?[...new Set(a.sort((a,b)=>a-b))][k-1]||'No way':[...new Set(a.sort((a,b)=>b-a))][k-1]||'No way'
 }
-const r=new M()
-cumulativeTriangle=n=>r.c(n)
 
-console.log(cumulativeTriangle(10100))
+console.log(givenNthValue([3, 3, -1, 10, 6, 8, -5, 4, 22, 31, 34, - 16, -16, 8 , 8],6,'max'))
