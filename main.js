53ee68d135abf14ef94f0ca12c0bb41a80ffd1a2
Jhonny <gonzodzen@mail.ru>
2023-06-07 00:31:09 +0300

    Vending Machine

--- main.js
+++ main.js
@@ -1,40 +1,34 @@
 class VendingMachine{
-  constructor(coins){
-    this.coins=coins
+  constructor(a,b){
+    this.a=a
+    this.b=b
   }
-  vending(p,c){
-  if(JSON.stringify(c)===JSON.stringify({8:2})&&p===9){
-    this.coins['3']-=2,this.coins['8']+=2
-    return { '3': 2 }
-  }
-    if(JSON.stringify(c)===JSON.stringify({20:1})&&p===1&&JSON.stringify(this.coins)===JSON.stringify({20:0, 9:2, 5:2, 1:19})){
-    this.coins={1:18,5:2,9:0,20:1}
-    return {1:1,9:2}
-  }
-   if(JSON.stringify(c)===JSON.stringify({20:1})&&p===1){
-    this.coins['5']-=2,this.coins['9']-=1,this.coins['20']+=1
-    return {5:2,9:1}
-  }
-  let o={},s=0
-  if(Object.keys(c).every(e=>this.coins[e]===undefined)||eval(Object.entries(c).map(e=>+e[0]*e[1]).join`+`)<p)return c
-  for(let i in c){
-    if(this.coins[i]!==undefined)this.coins[i]+=c[i],s+=c[i]*i,c[i]=0
-  }
-  s-=p
-  if(!s)return {}
-  let a=Object.entries(this.coins).reverse()
-  for(let i in c)if(c[i])o[i]=c[i]
-    for(let i=-1;++i<a.length;){
-      while(s>=a[i][0]&&a[i][1]){
-        s-=a[i][0]
-        if(o[a[i][0]]===undefined)o[a[i][0]]=0
-        if(o[a[i][0]]!==undefined)o[a[i][0]]+=1
-        a[i][1]-=1
-        this.coins[a[i][0]]-=1
-      }
+  vend(s,i){
+    let c=this.a.map((e,i)=>[e,i]).filter(e=>e[0].code.toLowerCase()===s.toLowerCase())[0]
+    if(!c)return `Invalid selection! : Money in vending machine = ${(this.b).toFixed(2)}`
+    if(c[0].price>i)return "Not enough money!"
+    if(!c[0].quantity)return `${c[0].name}: Out of stock!`
+    if(i===c[0].price){
+      this.b=+(this.b+ +i).toFixed(2)
+      this.a[c[1]].quantity-=1
+      return `Vending ${c[0].name}`
     }
-    return o
+    if(i>c[0].price){
+      this.b=+(this.b+ +i).toFixed(2)
+      let t=(i-c[0].price).toFixed(2)
+      this.b=+(this.b- +t).toFixed(2)
+      this.a[c[1]].quantity-=1
+      return `Vending ${c[0].name} with ${t} change.`
+    }
   }
 }
-const vm=new VendingMachine({1:0, 3:2, 5:1, 8:0})
-console.log(vm.vending(9, {8:2}))
+const r=new VendingMachine([{name:"Smarties", code:"A01", quantity:10, price:0.60},
+{name:"Caramac Bar", code:"A02", quantity:5, price:0.60},
+{name:"Dairy Milk", code:"A03", quantity:1, price:0.65},
+{name:"Freddo", code:"A04", quantity:1, price:0.25}],10.00)
+
+console.log(r.vend("A01",0.60))
+console.log(r.vend("A01",10.0))
+console.log(r.vend("Z01",0.41))
+console.log(r.vend("A02",0.40))
+console.log(r.vend("B06",4.60))
