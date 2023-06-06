2effcdeb02df37c061f8212b9fed77bb3abc4cff
Jhonny <gonzodzen@mail.ru>
2023-06-06 15:06:12 +0300

    String Pyramid

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-pascalsTriangle=x=(d,a=[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]],f=x=>x.flat())=>{
-  if(d<a.length)return f(a.slice(0,d))
-  if(a.length===d)return f(a)
-  let t=[1]
-  for(let i=-1;++i<a[a.length-1].length;)t.push((a[a.length-1][i]+a[a.length-1][i+1])||1)
-  a.push(t)
-  return x(d,a,f)
+function watchPyramidFromTheSide(s,n=-1,r=[]){
+  if(!s)return s
+  for(let i=s.length;--i>=0;)r.push(s[i].repeat(n+=2))
+  return r.map(e=>' '.repeat((n-e.length)/2)+e+' '.repeat((n-e.length)/2)).join`\n`
+}
+function watchPyramidFromAbove(s){
+  if(!s)return s
+  let n=s.length*2-1
+  let a=s[0].repeat(n*n).match(RegExp(`.{${n},${n}}`,'g')).map(e=>[...e]),c=s.slice(1),k=1
+  for(let q=0;q<c.length;q++){
+    for(let i=k;i<a.length-k;i++)for(let j=k;j<a[i].length-k;j++)a[i][j]=c[q]
+    k++
+  }
+  return a.map(e=>e.join``).join`\n`
+}
+function countVisibleCharactersOfThePyramid(s){
+  if(!s)return -1
+  return watchPyramidFromAbove(s).split`\n`.join``.length
+}
+function countAllCharactersOfThePyramid(s){
+  if(!s)return -1
+  let n=s.length*2-1,c=0
+  while(n>0)c+=n*n,n-=2
+  return c
 }
 
-console.log(pascalsTriangle(4))
+console.log(watchPyramidFromTheSide('*#'))
+console.log(watchPyramidFromAbove('*#'))
+console.log(countVisibleCharactersOfThePyramid('*#'))
+console.log(countAllCharactersOfThePyramid("*#"))
