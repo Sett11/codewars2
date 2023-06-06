69d10c544d8a31d49b04741e995c109ae4ec2ad9
Jhonny <gonzodzen@mail.ru>
2023-06-06 13:55:10 +0300

    Pascal's Triangle

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function pascal(d,a=[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]){
-  if(d<a.length)return a.slice(0,d)
-  if(a.length===d)return a
+pascalsTriangle=x=(d,a=[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]],f=x=>x.flat())=>{
+  if(d<a.length)return f(a.slice(0,d))
+  if(a.length===d)return f(a)
   let t=[1]
   for(let i=-1;++i<a[a.length-1].length;)t.push((a[a.length-1][i]+a[a.length-1][i+1])||1)
   a.push(t)
-  return pascal(d,a)
+  return x(d,a,f)
 }
 
-console.log(pascal(123))
+console.log(pascalsTriangle(4))
