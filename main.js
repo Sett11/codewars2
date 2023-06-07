f0e6a5351c6c7f9371921a4a19b554d8ac2e1704
Jhonny <gonzodzen@mail.ru>
2023-06-07 23:16:50 +0300

    Prime reversion

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-const f=x=>{if(x===2)return !1;for(let i=1;++i<=Math.sqrt(x);){if(!(x%i))return i}return !1}
-function primeAnt(n){
-  let a=[2],c=2,i=0
-  while(n){
-    let t=f(a[i])
-    if(!t)a.push(++c),i++,n--
-    else a[i]/=t,a[i-1]+=t,i--,n--
+const f=x=>{if(x<2)return !1;if(x===2)return !0;for(let i=1;++i<=Math.sqrt(x);){if(!(x%i))return !1}return !0}
+class M{
+  constructor(a=[]){
+    this.a=a
   }
-  return i
+  c(x,y){
+    const z=this.a.findIndex(e=>e>=x),v=this.a.findIndex(e=>e>y)
+    const arr=this.a.slice(z===-1?0:z,v===-1?this.a.length:v).filter(e=>e<y),r=[]
+    for(let i=-1;++i<arr.length;)for(let j=i-1;++j<arr.length;)if(f(eval([...(arr[i]*arr[j])+''].join`+`)))r.push([arr[i],arr[j]])
+    return r.length
+  }
+}
+const r=new M(Array(10000).fill(1).map((e,i)=>e+i).filter(f))
+function solve(a,b){   
+  return r.c(a,b)
 }
 
-console.log(primeAnt(12))
+console.log(solve(0,200))
