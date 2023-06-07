00f5aafbdf68d7784e1c8c9f731a8a2867d1e700
Jhonny <gonzodzen@mail.ru>
2023-06-07 15:11:13 +0300

    ■□ Pattern □■ : Wave

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-function pascal(d,a=[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]){
-  if(d<a.length)return a.slice(0,d)
-  if(a.length===d)return a
-  let t=[1]
-  for(let i=-1;++i<a[a.length-1].length;)t.push((a[a.length-1][i]+a[a.length-1][i+1])||1)
-  a.push(t)
-  return pascal(d,a)
-}
-function generateDiagonal(n,p,a=pascal(n===1e2?n*2:(p*p)).slice(n)){
-  return !p?[]:a[0].map((_,i)=>a.map(u=>u[n]))[0].slice(0,p)
+function draw(a,n=Math.max(...a)){
+  const r=a.map(e=>[...'■'.repeat(e)+'□'.repeat(n-e)].reverse())
+  return r[0].map((_,i)=>r.map(u=>u[i]).join``).join`\n`
 }
 
-console.log(generateDiagonal(100,6))
+console.log(draw([1,2,3,3,2,1,1,2,3,4,5,6,7]))
