f2b21c9c459df70da5cdc544982b7c21a8a3c6e2
Jhonny <gonzodzen@mail.ru>
2023-06-08 00:00:19 +0300

    Consecutive k-Primes

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-const f=x=>{for(let i=1;++i<=Math.sqrt(x);){if(!(x%i))return !1}return !0}
-class M{
-  constructor(a=[2,3,5,7,11,13,17,31,37]){this.a=a}
-  c(n){
-    let t=this.a[this.a.length-1]+2
-    while(this.a.length<=n){
-      if(f(t)&&f(+[...t+''].reverse().join``))this.a.push(t)
-      t+=2
-    }
-    return this.a[n]
+const f=(n,i=2,a=[])=>{
+  while(i<=n*n){
+    while(!(n%i))n/=i,a.push(i)
+    i++
   }
+  return a.length
 }
-const r=new M()
-reversiblePrime=n=>r.c(n)
+function consecKprimes(k,a,c=0){
+  a=a.map(e=>f(e))
+  for(let i=-1;++i<a.length-1;)if(a[i]===k&&a[i]===a[i+1])c++
+  return c
+}
 
-console.log(reversiblePrime(100))
+console.log(consecKprimes(2, [10110, 10102, 10097, 10113, 10123, 10109, 10118, 10119, 10117, 10115, 10101, 10121, 10122]))
