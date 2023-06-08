f97bbd3990a71ef1996552a92786acad3daef4db
Jhonny <gonzodzen@mail.ru>
2023-06-08 22:40:06 +0300

    Playing with Sandpiles

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
-const f=(x,y,r={})=>{
-  for(let i=-1;++i<y.length;)r[y[i]]=x.filter(e=>e.slice(0,e.indexOf('.'))===y[i]).map(e=>e.slice(e.indexOf('.')+1))
-  for(let i in r){
-    if(r[i].some(e=>e.includes('.')))r[i]=f(r[i],v(r[i]))
+const f=(x,r=[])=>{
+  let a=x.map((e,i)=>e.map((u,j)=>u>3?[i,j]:'').filter(z=>z)).filter(e=>e.length).flat()
+  for(let i=-1;++i<a.length;){
+    let [k,j]=a[i],t=[[k,j+1],[k,j-1],[k+1,j],[k-1,j]].filter(e=>e[0]>-1&&e[1]>-1&&e[0]<x.length&&e[1]<x[0].length)
+    r.push([a[i],t])
   }
-  return r
+  for(let i=-1;++i<r.length;){
+    let c=0
+    for(let j=-1;++j<r[i][1].length;)x[r[i][0][0]][r[i][0][1]]-=1,x[r[i][1][j][0]][r[i][1][j][1]]+=1,c++
+    x[r[i][0][0]][r[i][0][1]]-=(4-c)
+  }
+  return x.every(e=>e.every(u=>u<4))?x:f(x)
 }
-const v=x=>[...new Set(x.map(e=>e.slice(0,e.indexOf('.'))))]
-const z=x=>{
-  for(let i in x){
-    for(let j in x[i]){
-      if(j.match('=')){
-        let t=x[i][j][0].replace(/=/,' ').split` `
-        x[i][t[0]]=decodeURIComponent(t[1])
-        delete x[i][j]
-        break
-      }
-        if(Array.isArray(x[i][j])){
-          let o={}
-          x[i][j].forEach(e=>{
-          e=e.split`=`
-          o[e[0]]=decodeURIComponent(e[1])
-      })
-      x[i][j]=o
-      }
-    }
+class Sandpile {
+  constructor(a='000\n000\n000'){
+    this.a=f(a.split`\n`.map(e=>[...e].map(Number)))
   }
-  return x
-}
-function convertQueryToMap(q){
-  if(!q)return {}
-  if((q.match(/&/g)||'').length<2){
-    let o={}
-    q.split`&`.map(e=>e.split`=`).map(u=>o[u[0]]=decodeURIComponent(u[1]))
-    return o
+  add(b){
+    let t=f(this.a.map((e,i)=>e.map((u,j)=>u+b.a[i][j])))
+    return new Sandpile(t.map(e=>e.join``).join`\n`)
   }
-  let a=q.split`&`,w=v(a),newA=f(a,w)
-  return z(newA)
+  toString(){
+    return this.a.map(e=>e.join``).join`\n`
+  }
 }
 
-console.log(convertQueryToMap('user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue'))
-console.log(convertQueryToMap('jdlps.pkdww.vcnjp=wwijk&jdlps.pkdww.ncrpp=wqbdl&jdlps.qprww.itrwg=qbnwd&jdlps.qprww.krfxd=cgsis&tgrnc.lcnrb.lgmgh=mckrj&tgrnc.lcnrb.jnrjw=tnkhh&tgrnc.dvnph.ilipj=nfrwt&tgrnc.dvnph.lpsdq=mllri'))
-console.log(convertQueryToMap('a=1%202'))
-console.log(convertQueryToMap('a=1&b=2'))
-console.log(convertQueryToMap(''))
+let sp1 = new Sandpile('111\n111\n111');
+    let sp2 = new Sandpile('111\n111\n111');
+    let sp3 = new Sandpile('010\n101\n100');
+    let sp4 = sp1.add(sp2).add(sp3)
+
+  console.log(sp4.toString())
