228090cfd97ea8704dd20eb9f8e69840cdd0a4ef
Jhonny <gonzodzen@mail.ru>
2023-06-08 10:03:29 +0300

    Building a mini search engine

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-class M{
-  constructor(){this.a=[1,1,2,3,3,4,5,5,6,6,6,8,8,8,10,9,10,11,11,12,12,12,12]}
-  c(n){
-    while(this.a.length<n){
-      let t=this.a.length
-      this.a.push(this.a[t-this.a[t-1]]+this.a[t-this.a[t-2]])
-    }
-    return this.a.slice(0,n)
-  }
+function searchEng(s,c,n,ns){
+  if(c.match(/[^a-z0-9]/gi)||(ns||'').match(/[^a-z0-9]/gi)||!s)return 'No valid input'
+  s=s.replace(/\W| /g,'&$&&').split`&`
+  if(n===1)return {matches:s.filter(e=>e===c).length,newString:''}
+  return {matches:s.filter(e=>e===c).length,newString:s.map(e=>e===c?ns:e).join``}
 }
-const r=new M()
-lengthSupUK=(n,k)=>r.c(n).filter(e=>e>=k).length
-comp=(n,k=0)=>r.c(n).forEach((e,i,v)=>e<v[i-1]?k++:0)|k
 
-console.log(lengthSupUK(50,25))
-console.log(lengthSupUK(3332, 973))
-console.log(lengthSupUK(2941, 862))
-console.log(lengthSupUK(3177, 573))
-console.log(lengthSupUK(1745, 645))
-
-console.log(comp(74626))
-console.log(comp(71749))
+console.log(searchEng('OMG, today is Monday!','day', 1))
+console.log(searchEng('Today is my day!','day', 2, 'birthday'))
