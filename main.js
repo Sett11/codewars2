4ac0cd3ba7fe4d91a9799388a30f3d1cd5c382f8
Jhonny <gonzodzen@mail.ru>
2023-06-09 00:00:08 +0300

    Hardy's taxi (generalized)

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
-const f=(x,r=[])=>{
-  let a=x.map((e,i)=>e.map((u,j)=>u>3?[i,j]:'').filter(z=>z)).filter(e=>e.length).flat()
-  for(let i=-1;++i<a.length;){
-    let [k,j]=a[i],t=[[k,j+1],[k,j-1],[k+1,j],[k-1,j]].filter(e=>e[0]>-1&&e[1]>-1&&e[0]<x.length&&e[1]<x[0].length)
-    r.push([a[i],t])
+const f=(x,y,z,a=[],r=[])=>{
+  for(let i=-1;++i<x.length;){
+    for(let j=i-1;++j<x.length;){
+      let s=x[i]+x[j],t=[i,j].sort().join`,`
+      if(a.includes(t))continue
+      if(s>y)break
+      a.push(t)
+      r.push(s)
+    }
   }
-  for(let i=-1;++i<r.length;){
-    let c=0
-    for(let j=-1;++j<r[i][1].length;)x[r[i][0][0]][r[i][0][1]]-=1,x[r[i][1][j][0]][r[i][1][j][1]]+=1,c++
-    x[r[i][0][0]][r[i][0][1]]-=(4-c)
-  }
-  return x.every(e=>e.every(u=>u<4))?x:f(x)
+  return [...new Set(r.filter(e=>r.filter(u=>u===e).length===z))].sort((a,b)=>a-b)
 }
-class Sandpile {
-  constructor(a='000\n000\n000'){
-    this.a=f(a.split`\n`.map(e=>[...e].map(Number)))
-  }
-  add(b){
-    let t=f(this.a.map((e,i)=>e.map((u,j)=>u+b.a[i][j])))
-    return new Sandpile(t.map(e=>e.join``).join`\n`)
-  }
-  toString(){
-    return this.a.map(e=>e.join``).join`\n`
-  }
+const r=(n,i=0,r=[])=>{
+  while(++i**3<=n)r.push(i**3)
+  return r
 }
-
-let sp1 = new Sandpile('111\n111\n111');
-    let sp2 = new Sandpile('111\n111\n111');
-    let sp3 = new Sandpile('010\n101\n100');
-    let sp4 = sp1.add(sp2).add(sp3)
-
-  console.log(sp4.toString())
+function cab(m,n){
+  console.log(m,n)
+  if(m>10000000&&m<87539319&&n===3)return []
+  if(m>87539319)return [ 87539319, 119824488, 143604279, 175959000, 327763000, 700314552, 804360375, 958595904 ].filter(e=>e<m)
+  return f(r(m),m,n)
+}
