1c44e16b3b8e973ca2a36e0b22a094224ffd5098
Jhonny <gonzodzen@mail.ru>
2023-06-09 00:15:15 +0300

    How the Grinch Almost Ended Christmas

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-const f=(x,y,z,a=[],r=[])=>{
-  for(let i=-1;++i<x.length;){
-    for(let j=i-1;++j<x.length;){
-      let s=x[i]+x[j],t=[i,j].sort().join`,`
-      if(a.includes(t))continue
-      if(s>y)break
-      a.push(t)
-      r.push(s)
-    }
-  }
-  return [...new Set(r.filter(e=>r.filter(u=>u===e).length===z))].sort((a,b)=>a-b)
+function prioritizeMissiles(m){
+  return m.sort((a,b)=>b.speed/b.distance-a.speed/a.distance).map(e=>e.name)
 }
-const r=(n,i=0,r=[])=>{
-  while(++i**3<=n)r.push(i**3)
-  return r
-}
-function cab(m,n){
-  console.log(m,n)
-  if(m>10000000&&m<87539319&&n===3)return []
-  if(m>87539319)return [ 87539319, 119824488, 143604279, 175959000, 327763000, 700314552, 804360375, 958595904 ].filter(e=>e<m)
-  return f(r(m),m,n)
-}
+
+console.log(prioritizeMissiles([
+  {name: 'VIXEN_V1.1', distance: 500, speed: 4},
+  {name: 'COMET_V4', distance: 1000, speed: 500},
+  {name: 'CUPID_V5', distance: 100, speed: 1} ]))
