b9638de3b5a0b73f761968ec68f1590e40f23048
Jhonny <gonzodzen@mail.ru>
2023-06-09 21:21:54 +0300

    Prime number decompositions

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-function compare(a,b){
-  if(a==='.x .y'&&b==='.foo.bar')return b
-  const f=(x,y)=>x.replace(RegExp(`[^${y}]`,'g'),'').length
-  const r=x=>x.split`.`.filter(e=>e&&e!==' ').length
-  const q=x=>x.match(/[a-z]/i)
-  if(!q(a))return b
-  if(!q(b))return a
-  if(f(a,'#')>f(b,'#'))return a
-  if(f(a,'#')<f(b,'#'))return b
-  if(f(a,'.')>f(b,'.'))return a
-  if(f(a,'.')<f(b,'.'))return b
-  if(f(a,' ')>f(b,' '))return a
-  if(f(a,' ')<f(b,' '))return b
-  if(r(a)>r(b))return a
-  return b
+const f=(n,i=2,a=[])=>{
+  while(i<=n*n){
+    while(!(n%i))n/=i,a.push(i)
+    i++
+  }
+  return a
 }
+getAllPrimeFactors=n=>n===1?[1]:f(n)
+getUniquePrimeFactorsWithCount=(n,c=f(n),t=[...new Set(c)])=>n===1?[[1],[1]]:[t,t.map(e=>c.filter(u=>u===e).length)]
+getUniquePrimeFactorsWithProducts=(n,c=f(n),t=[...new Set(c)])=>n===1?[1]:[t.map(e=>e**c.filter(u=>u===e).length)].flat()
 
-console.log(compare('.x .y', '.foo.bar'))
+console.log(getAllPrimeFactors(100))
+console.log(getUniquePrimeFactorsWithCount(100))
+console.log(getUniquePrimeFactorsWithProducts(100))
