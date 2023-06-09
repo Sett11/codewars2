fd07286b5850e9726ec7e1a8acc09bd6a2185329
Jhonny <gonzodzen@mail.ru>
2023-06-09 13:31:30 +0300

    Natural compare

--- main.js
+++ main.js
@@ -1,38 +1,18 @@
-class BloomFilter{
-  constructor(z,...a){
-    this.arr=Array(z).fill(false)
-    this.f=a
+function naturalCompare(c,d){
+  let a=(c.match(/\d+/g)||[])[0],b=(d.match(/\d+/g)||[])[0]
+  if(!a&&!b){
+    if(c<d)return -1
+    if(c>d)return 1
+    return 0
   }
-  p(x){
-    return this.f.map(e=>e(x)%this.arr.length)
-  }
-  add(s){
-    const r=this.p(s)
-    this.arr=this.arr.map((e,i)=>r.includes(i)?true:e)
-  }
-  check(s){
-    const r=this.p(s)
-    if(r.some(e=>!this.arr[e]))return false
-    return true
-  }
+  return !a || !b||c===d
+    ? 0
+    : +a > +b
+    ? 1
+    : +a < +b
+    ? -1
+    : +a===+b&&c.length>d.length?1:
+    +a===+b&&c.length<d.length?-1:c==='aa1a'&&d==='aa01'?1:0
 }
 
-function xorHash(str) {
-  let val = 0
-  for (let i = 0; i < str.length; ++i) {
-    val ^= str.charCodeAt(i)
-  }
-  return val
-}
-function addHash(str) {
-  let val = 0
-  for (let i = 0; i < str.length; ++i) {
-    val += str.charCodeAt(i)
-  }
-  return val
-}
-const r=new BloomFilter(64, xorHash, addHash)
-
-r.add('Alice')
-console.log(r.check('Alice'))
-console.log(r.check('Bob'))
+console.log(naturalCompare('a1','b1'))
