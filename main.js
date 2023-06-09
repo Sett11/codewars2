5badb9bb5283bab943c2ef6422d26b620b8a9e6e
Jhonny <gonzodzen@mail.ru>
2023-06-09 13:17:17 +0300

    Definitely Naughty or Probably Nice?

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-const groceries={
-  'Orange Juice': { price: 1.5, discount: 10, bogof: false },
-  Chocolate: { price: 2, discount: 0, bogof: true },
-  Sweetcorn: { price: 4, discount: 20, bogof: true },
-  Apples: { price: 6, discount: 0, bogof: false },
-  Pears: { price: 2, discount: 50, bogof: false }
+class BloomFilter{
+  constructor(z,...a){
+    this.arr=Array(z).fill(false)
+    this.f=a
+  }
+  p(x){
+    return this.f.map(e=>e(x)%this.arr.length)
+  }
+  add(s){
+    const r=this.p(s)
+    this.arr=this.arr.map((e,i)=>r.includes(i)?true:e)
+  }
+  check(s){
+    const r=this.p(s)
+    if(r.some(e=>!this.arr[e]))return false
+    return true
+  }
 }
 
-function shoppingListCost(a){
-  return +a.map(e=>{
-    let t=groceries[e[0]].price*e[1]
-    if(groceries[e[0]].bogof)t-=groceries[e[0]].price*(Math.floor(e[1]/2))
-    t=t-(t/100)*groceries[e[0]].discount
-    return t
-  }).reduce((a,c)=>a+c,0).toFixed(2)
+function xorHash(str) {
+  let val = 0
+  for (let i = 0; i < str.length; ++i) {
+    val ^= str.charCodeAt(i)
+  }
+  return val
 }
+function addHash(str) {
+  let val = 0
+  for (let i = 0; i < str.length; ++i) {
+    val += str.charCodeAt(i)
+  }
+  return val
+}
+const r=new BloomFilter(64, xorHash, addHash)
 
-console.log(shoppingListCost([["Chocolate", 3],["Apples", 8],["Orange Juice", 15],["Pears",1]]))
-console.log(shoppingListCost([["Sweetcorn", 12],["Pears", 6],["Apples", 5]]))
+r.add('Alice')
+console.log(r.check('Alice'))
+console.log(r.check('Bob'))
