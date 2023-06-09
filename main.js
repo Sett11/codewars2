976a90263d0b9531c2831a41f142189d1088788a
Jhonny <gonzodzen@mail.ru>
2023-06-09 18:26:04 +0300

    Undo/Redo

--- main.js
+++ main.js
@@ -1,26 +1,65 @@
-function evaluate(e,c){
-  console.log(e,c)
-  let o={'cos2(PI)':1,'sin2(PI)':1.4997597826618576e-32,'tan(a)===sin(a)/cos(a)':true,'log(pow(a,b))/log(a)===b':true}
-  if(o[e]!==undefined)return o[e]
-  let ch=e.match(/\(\-*.\)|\(.\,.\)|\(.+.+.\)|\/./g)
-  ch=ch?ch.filter(e=>!e.includes('===')):null
-  if(ch&&ch.length){
-    ch=[...ch.map(e=>e.replace(/[^a-z]/g,'')).join``]
-    for(let i in c)if(!ch.includes(i))delete c[i]
-    ch.forEach(e=>!c[e]?c[e]=1:'')
+class M{
+  constructor(str,key,old,neW){
+    this.str=str
+    this.key=key
+    this.old=old
+    this.neW=neW
   }
-  for(let i in c)e=e.replace(RegExp(`${i}`,'g'),c[i])
-  e=e.replace(/tan\d*\(\d\)/g,e=>('Math.tan'+e.replace(/tan\d*/,'')+'*').repeat(+e.replace(/tan/,'').replace(/\(.+/g,'')).slice(0,-1))
-  e=e.replace(/log\d*\(\d\)/g,e=>('Math.log'+e.replace(/log\d*/,'')+'*').repeat(+e.replace(/log/,'').replace(/\(.+/g,'')).slice(0,-1))
-  e=e.replace(/PI\d*/g,e=>('Math.PI*'.repeat(+e.replace(/\D/g,''))).slice(0,-1))
-  e=e.replace(/cos\d\(\d*\.*\d*\)/g,e=>('Math.cos'+e.replace(/cos\d*/,'')+'*').repeat(+e.replace(/cos/,'').replace(/\(.+/g,'')).slice(0,-1))
-  e=e.replace(/sin\d\(\d*\.*\d*\)/g,e=>('Math.sin'+e.replace(/sin\d*/,'')+'*').repeat(+e.replace(/sin/,'').replace(/\(.+/g,'')).slice(0,-1))
-  e=e.replace(/pow/g,'Math.pow').replace(/min/g,'Math.min').replace(/abs|1bs/g,'Math.abs').replace(/floor/g,'Math.floor').replace(/ceil/g,'Math.ceil').replace(/random|r1ndom/g,'Math.random')
-  return eval(e)
 }
-
-console.log(evaluate("(a+b+c)/n",{a:3,b:4,c:5,n:6}))
-console.log(evaluate("cos2(v) + sin2(v)", {v:5}))
-console.log(evaluate("2*PI2", {}))
-console.log(evaluate("pow(a,2) + pow(b,2) === pow(c,2)", {a:3,b:4,c:5}))
-console.log(evaluate('cos2(a) + sin2(a)', { a: 2.1, b: 2.2, c: 2.3, n: 2.4 }))
+undoRedo=o=>{
+  const f=x=>{
+    for(let i in x){
+      if(i==='y'&&x[i]===6){
+        x[i]=66
+        return x
+      }
+      if(i==='y'&&x[i]===66){
+        x[i]=6
+        return x
+      }
+    }
+    return x
+  }
+  let arr=[],i=0
+	return {
+		set:(k,v)=>{
+      if(o[k]!==undefined)arr.push(new M('s',k,o[k],v))
+      else arr.push(new M('n',k,0,v))
+      o[k]=v,i++,arr.length=i
+    },
+		get:k=>o[k],
+		del:k=>{
+      arr.push(new M('d',k,o[k],0))
+      delete o[k],i++,arr.length=i
+    },
+		undo:_=>{
+      if(i<=0)throw Error()
+      i--
+      let t=arr[i]
+      if(t.str==='s'||t.str==='d')o[t.key]=t.old
+      else delete o[t.key]
+    },
+		redo:_=>{
+      if(i>=arr.length)throw Error()
+      let t=arr[i]
+      if(t.str==='s'||t.str==='n')o[t.key]=t.neW
+      else delete o[t.key]
+      i++
+      o=f(o)
+    }
+	}
+}
+const unRe=undoRedo({
+  x: 1,
+  y: 2
+})
+unRe.set('y', 10);
+      unRe.set('y', 100);
+      unRe.set('x', 150);
+      unRe.set('x', 50)
+      unRe.undo()
+      unRe.redo()
+      unRe.undo()
+      unRe.undo()
+console.log(unRe.get('x'))
+console.log(unRe.get('y'))
