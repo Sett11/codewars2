10c543394e2388398330632d42b3cb24a7d5d592
Jhonny <gonzodzen@mail.ru>
2023-06-10 00:01:13 +0300

    Prime Number Generator :Simple version

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const f=(n,i=2,a=[])=>{
-  while(i<=n*n){
-    while(!(n%i))n/=i,a.push(i)
-    i++
+const f=x=>{for(let i=1;++i<=Math.sqrt(x);){if(!(x%i))return !1}return !0}
+function *r(){
+  yield 2
+  let c=3
+  while(1){
+    if(f(c))yield c
+    c++
   }
-  return a
 }
-getAllPrimeFactors=n=>n===1?[1]:f(n)
-getUniquePrimeFactorsWithCount=(n,c=f(n),t=[...new Set(c)])=>n===1?[[1],[1]]:[t,t.map(e=>c.filter(u=>u===e).length)]
-getUniquePrimeFactorsWithProducts=(n,c=f(n),t=[...new Set(c)])=>n===1?[1]:[t.map(e=>e**c.filter(u=>u===e).length)].flat()
-
-console.log(getAllPrimeFactors(100))
-console.log(getUniquePrimeFactorsWithCount(100))
-console.log(getUniquePrimeFactorsWithProducts(100))
+primeGen=r()
+const r=primeGen()
+console.log(r.next())
+console.log(r.next())
+console.log(r.next())
+console.log(r.next())
