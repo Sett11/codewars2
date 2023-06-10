faa0da3869cc5ad570667e6dafbb7ce3a726748f
Jhonny <gonzodzen@mail.ru>
2023-06-10 19:39:41 +0300

    Once

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-function clamp(n,m,a,r){
-  if(a===undefined){
-    if(n>m)return m
-    if(n<0)return 0
-    else return n
-  }
-  if(a!==undefined&&r===undefined){
-    if(n<m)return m
-    if(n>a)return a
-    else return n
-  }
-  if(a!==undefined&&r!==undefined){
-    if(n<m||n>a){
-      if(typeof r==='function')return r(n,m,a)
-      else return r
+once=f=>{
+  const a=[]
+  return function(...x){
+    if(!a.includes(f.toString())){
+      a.push(f.toString())
+      return f(...x)
     }
-    return n
   }
 }
-console.log(clamp(2,5,10,'ggg'))
-console.log(clamp(-5,2,12,function(n,a,b){ return n<a? b : a }))
+
+logOnce=once(e=>e)
+console.log(logOnce(1))
+console.log(logOnce(2))
+console.log(logOnce(5))
