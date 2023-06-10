8da0752137362d5185b6971e0fd8bbee2246638c
Jhonny <gonzodzen@mail.ru>
2023-06-10 22:09:58 +0300

    Cycle Detection: greedy algorithm

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const a='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const decryptPassword=s=>s.slice(0,s.lastIndexOf('-')).split`-`.map(e=>a[e]).join``
-
-console.log(decryptPassword("44-19-4-21-4-13-Seentv"))
+cycle=(x,t=[])=>{
+  for(let i=-1;++i<x.length;){
+    if(t.includes(x[i]))return [x.indexOf(x[i]),x.indexOf(x[i])+t.length]
+    else t.push(x[i])
+  }
+  return []
+}
+console.log(cycle([1,2,3,1,2,3]))
