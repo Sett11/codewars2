c4511c127ca89bfb9d31d0575daec3575ffb0c1f
Jhonny <gonzodzen@mail.ru>
2023-06-12 01:20:42 +0300

    Common Bit Twiddles

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const countChange=x=(n,a)=>n<0||!a.length?0:!n?1:x(n-a[0],a)+x(n,a.slice(1))
+isEven=n=>!!((n&1)^1)
+isOdd=n=>!!((n&1)^0)
+halfAndFloor=n=>n>>1
+isPowerOfTwo=n=>!(n&(n-1))
+nthPowerOfTwo=n=>2<<(n-1)||1
+truncate=n=>~~n
+abs=(n,m=n>>31)=>(n+m)^m
 
-console.log(countChange(100,[5, 2, 3]))
+console.log(nthPowerOfTwo(4))
+console.log(nthPowerOfTwo(5))
+console.log(nthPowerOfTwo(3))
