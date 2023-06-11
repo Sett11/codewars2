6b247c19e60fbf30fb2f0497d67992d7a5c13dc3
Jhonny <gonzodzen@mail.ru>
2023-06-12 00:18:22 +0300

    Counting Change Combinations

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-let q=[]
-const f=(a,j,n,r)=>{
-    if(r<0)return
-    if(!r){
-        let t=[]
-        for(let i=-1;++i<j;)t.push(a[i])
-        q.push(t)
-        return
-    }
-    let p=!j?1:a[j-1]
-    for(let i=p-1;++i<=n;)a[j]=i,f(a,j+1,n,r-i)
-    return q
-}
-const combos=n=>{
-    let r=f(Array(n).fill(0),0,n,n)
-    q=[]
-    return r
-}
+const countChange=x=(n,a)=>n<0||!a.length?0:!n?1:x(n-a[0],a)+x(n,a.slice(1))
 
-console.log(combos(5))
-console.log(combos(3))
+console.log(countChange(100,[5, 2, 3]))
