e4e657fdce9a2190f18ed4eb1dca92febd3bbcb0
Jhonny <gonzodzen@mail.ru>
2023-06-12 13:08:43 +0300

    Custom Setters and Getters

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function portion(a,j,n,r=[],f=x=>Math.abs(x),z=n){
-    if(j>-1)for(let i=j;n>0;i++)r.push(a[i]),n--
-    else for(let i=a.length-f(j)-1;n>0;i--)r.unshift(a[i]),n--
-    return r.length<z||!a[f(j)]||r.some(e=>e===undefined)?-1:r
-}
-console.log(portion([1,2,3,4],-1,2))
-console.log(portion([1,2,3,4],1,2))
+class Archiver{
+    constructor(){this.a=[]}
+    set temperature(x){
+        let o={date:new Date(),val:x}
+        this.a.push(o)
+    }
+    get temperature(){
+        return this.a[this.a.length-1].val
+    }
+    getArchive(){
+      return this.a
+    }
+  }
+  const arc = new Archiver()
+  arc.temperature=33
+  arc.temperature=23
+  console.log(arc.getArchive())
+  console.log(arc.temperature)
