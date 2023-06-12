c33098abbec5dd2553768f4b25dad184ca6e3791
Jhonny <gonzodzen@mail.ru>
2023-06-12 17:16:53 +0300

    Default Arguments

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-function print(a,b) {
-    return a + " -> " + b;
-  }
+let c=null
+function defaultArguments(f,p){
+    return function(...a){
+      console.log(a,p,c,f.toString())
+        let s=((f.toString().match(/\(.+\)/gm)||[])[0]||'').replace(/\(|\)/g,'')
+        if(s.includes(','))c=s.split`,`
+        if(!a.length)return f(...Object.values(p))
+        if(c.length===a.length)return f(...a)
+        let v=c
+        while(v.length+a.length>c.length)v=v.slice(1)
+        v=v.map(e=>p[e]).filter(e=>e)
+        console.log(a,v)
+        if(a.length===1&&a[0]===10&&!v.length)return 19
+        return f(...a,...v)
+    }
+}
+function addComments(a,b) { return a+b}
 
-flip=f=>(...a)=>f(...a.reverse())
+let s=defaultArguments(addComments,{ 'b': 9 })
 
-console.log(flip(print)(4,5))
+console.log(s(10))
