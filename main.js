f3acc289d05fa9c00135eb90561c04eed0d307b5
Jhonny <gonzodzen@mail.ru>
2023-06-12 17:47:45 +0300

    Argument Mapper

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-let c=null
-function defaultArguments(f,p){
-    return function(...a){
-      console.log(a,p,c,f.toString())
-        let s=((f.toString().match(/\(.+\)/gm)||[])[0]||'').replace(/\(|\)/g,'')
-        if(s.includes(','))c=s.split`,`
-        if(!a.length)return f(...Object.values(p))
-        if(c.length===a.length)return f(...a)
-        let v=c
-        while(v.length+a.length>c.length)v=v.slice(1)
-        v=v.map(e=>p[e]).filter(e=>e)
-        console.log(a,v)
-        if(a.length===1&&a[0]===10&&!v.length)return 19
-        return f(...a,...v)
-    }
+function createArgumentMap(f,...a){
+    const m=new Map(),r=(f.toString().match(/\(.+\)/g)||[]).map(e=>e.replace(/\(|\)/g,'').split`,`).flat()
+    r.forEach((e,i)=>m[e]=a[i])
+    return m
 }
-function addComments(a,b) { return a+b}
 
-let s=defaultArguments(addComments,{ 'b': 9 })
-
-console.log(s(10))
+console.log(createArgumentMap(function(a1){},'a1 argvalue'))
