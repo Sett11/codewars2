3e75437345db8b3d25b5aa48f5c53099355e15b8
Jhonny <gonzodzen@mail.ru>
2023-06-12 08:33:00 +0300

    Binary multiplication.

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-class UriBuilder{
-    constructor(s){
-      this.s=s
-      this.q=s
-      this.params={}
-    }
-    build(){
-        this.s=this.q
-        let c=[]
-        for(let i in this.params)c.push(i+'='+this.params[i])
-        this.s+=encodeURI(c.join`&`)
-        return this.s.replace(/a=1a=2/,'a=2').replace(/a=1b=1/,'b=1').replace(/a=1b=a%20b/,'b=a%20b')
-    }
+function binMul(m,n,a=[],b=[],t=0){
+    if(m<n)t=m,m=n,n=t
+    a.push(m),b.push(n)
+    while(m>0)a.push(m=~~(m/2)),b.push(n*=2)
+    return b.filter((e,i)=>a[i]&1&&e).reverse()
 }
 
-let builder = new UriBuilder('http://www.codewars.com')
-builder.params.a=2
-builder.params.a=3
-builder.build()
-console.log(builder.s)
+console.log(binMul(1954, 6252))
