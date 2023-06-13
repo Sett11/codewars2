4827f99db9492164f7b5519a87fb556b7a9b7866
Jhonny <gonzodzen@mail.ru>
2023-06-13 09:16:52 +0300

    IPv4 Parser

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
-const au=`;,/?:@&=+$#abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.!~*'()`
-const auc=`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.!~*'()`
-const URICodec={}
-URICodec.valid=s=>typeof s==='string'&&[...s+''].filter(e=>e!=='%').every(e=>au.includes(e)||auc.includes(e))&&(s.match(/%.{2,2}/g)||[]).every((e,i,v,t=String.fromCharCode(parseInt(e.slice(1),16)))=>au.includes(t)||auc.includes(t)||e.slice(1)==='0F')
-URICodec.encode=s=>{
-    return [...s].map(e=>{
-        if(au.includes(e))return e
-        e=(e.charCodeAt()).toString(16).toUpperCase()
-        if(e.length<2)e='0'+e
-        return '%'+e
-    }).join``
-}
-URICodec.encode.component=s=>{
-    return [...s].map(e=>{
-        if(auc.includes(e))return e
-        e=(e.charCodeAt()).toString(16).toUpperCase()
-        if(e.length<2)e='0'+e
-        return '%'+e
-    }).join``
-}
-URICodec.decode=s=>{
-    const a=';,/?:@&=+$#'
-    return s.replace(/%.{2,2}/g,(e,i,v,t=String.fromCharCode(parseInt(e.slice(1),16)))=>a.includes(t)?e:t)
-}
-URICodec.decode.component=s=>{
-    return s.replace(/%.{2,2}/g,(e,i,v,t=String.fromCharCode(parseInt(e.slice(1),16)))=>t)
+function ipv4Parser(p,m){
+    const f=x=>x.split`.`,r=(a,b,s='')=>{for(let i=-1;++i<a.length;){s+=+a[i]&+b[i]}return s},w=x=>x.join`.`
+    const q=f(p).map((e,i)=>{
+        e=[(+e).toString(2),(+f(m)[i]).toString(2)].map(u=>u.length<8?u.padStart(8,'0'):u)
+        return parseInt(r(e[0],e[1]),2)
+    })
+    return [w(q),f(p).map((e,i)=>e^q[i]).join`.`]
 }
 
-console.log(URICodec.decode.component(`%23`))
-console.log(URICodec.decode.component(";,/?:@&=+$-_.!~*'()#"))
-console.log(URICodec.valid("bad%escaped-char"))
+console.log(ipv4Parser('65.196.188.53' , '255.255.240.0'))
