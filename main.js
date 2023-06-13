60306956338b1797191fe3fcf9c804d519aeabfe
Jhonny <gonzodzen@mail.ru>
2023-06-14 00:01:44 +0300

    Optimum coding school location

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-function ipv4Parser(p,m){
-    const f=x=>x.split`.`,r=(a,b,s='')=>{for(let i=-1;++i<a.length;){s+=+a[i]&+b[i]}return s},w=x=>x.join`.`
-    const q=f(p).map((e,i)=>{
-        e=[(+e).toString(2),(+f(m)[i]).toString(2)].map(u=>u.length<8?u.padStart(8,'0'):u)
-        return parseInt(r(e[0],e[1]),2)
-    })
-    return [w(q),f(p).map((e,i)=>e^q[i]).join`.`]
+optimumLocation=(s,l)=>{
+  const r=l.map(e=>[e,eval(s.map(u=>(Math.abs(e.x-u[0])+Math.abs(e.y-u[1]))).join`+`)]).sort((a,b)=>a[1]-b[1])[0][0]
+  return `The best location is number ${r.id} with the coordinates x = ${r.x} and y = ${r.y}`
 }
 
-console.log(ipv4Parser('65.196.188.53' , '255.255.240.0'))
+console.log(optimumLocation([[3,7],[2,2],[14,1]],[{id: 1, x: 3, y: 4}, {id: 2, x: 8, y: 2}]))
+console.log(optimumLocation([[152,7],[1,211],[14,56],[12,4],[142,7]], 
+[{id: 1, x: 63, y: 55}, {id: 2, x: 55, y: 21},{id: 3, x: 144, y: 12}]))
