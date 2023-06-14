47d85e28c6c536b2eada9188e4201fcc8888e626
Jhonny <gonzodzen@mail.ru>
2023-06-14 22:53:34 +0300

    T.T.T.#4: Key of door

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-let puzzle=(n,x)=>n<<x
+function findKey(ns,a=ns.map(e=>[...e+''])){
+  return a[0].map((_,i)=>Object.entries(a.map(u=>u[i]).reduce((a,c)=>(a[c]=a[c]+1||1,a),{})).sort((a,b)=>a[1]-b[1])[0][0]).join``
+}
 
-console.log(puzzle(3,4))
+console.log(findKey([153456,123406,124456,323456,123458,123756]))
