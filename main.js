ed51ae8ac48d3466f902b4ec38788004a3590faf
Jhonny <gonzodzen@mail.ru>
2023-06-15 20:58:47 +0300

    Simple Fun #201: Cards And Pero

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-function p(d,a=[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]){
-  if(d<a.length)return a.slice(0,d)
-  if(a.length===d)return a
-  let t=[1]
-  for(let i=-1;++i<a[a.length-1].length;)t.push((a[a.length-1][i]+a[a.length-1][i+1])||1)
-  a.push(t)
-  return p(d,a)
-}
-function pascalMatrix(n){
-  return p(Math.floor(n/2)+1).map(e=>e.join`,0,`.split`,`.map(Number)).map((e,i,v)=>{
-    while(e.length<v[v.length-1].length)e.push(0),e.unshift(0)
-    return e
-  })
+function cardsAndPero(s,a=s.match(/.{3,3}/g),c='PKHT'){
+  if(a.length>[...new Set(a)].length)return [-1,-1,-1,-1]
+  a=a.map(e=>e[0]).reduce((a,c)=>(a[c]=a[c]+1||1,a),{})
+  for(let i=-1;++i<c.length;)if(!a[c[i]])a[c[i]]=0
+  return [13-a['P'],13-a['K'],13-a['H'],13-a['T']]
 }
 
-console.log(pascalMatrix(15))
+console.log(cardsAndPero('P05P01P02P03P13P09P11P07K01P12K03K02K13K12K10K08H03H02H13H12H10H08T01T03T02T13T12T10T08P04K07H02T07H06T11K11T05K05H05H11'))
+console.log(cardsAndPero('P01K02P03P11K09K10P13P10'))
+console.log(cardsAndPero('P10K10H10T01'))
+console.log(cardsAndPero('P04H13T04K10H02K02K12H07H11H03T13T11T01K13'))
