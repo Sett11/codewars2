69af7a63faa1086f9e52ae8eaef463a495dab705
Jhonny <gonzodzen@mail.ru>
2023-06-15 23:40:43 +0300

    Sloppy Censorship

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-function combineStrings(...a){
-  a=a.map(e=>[...e])
-  return !a.length?'':a[a.map((e,i)=>[e,i]).sort((a,b)=>b[0].length-a[0].length)[0][1]].map((e,i)=>a.map(u=>u[i]).join``).join``
+function censor(s){
+  return s.split` `.map(e=>e[0]<{}?[...e].map(u=>u<{}&&u.match(/[a-z]/i)?u:'_').join``:e).join` `
 }
 
-console.log(combineStrings())
+console.log(censor("Gertrude WENT overboard on the Sailor Jerry..."))
