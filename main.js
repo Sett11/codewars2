45a12762e3f6e5d9a276a4a09ac0ccdd59250954
Jhonny <gonzodzen@mail.ru>
2023-06-15 10:09:38 +0300

    T.T.T.30: Mr. smile67 has no WLAN

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-let c=null
-const CodeTracker={
-  init:s=>{
-    return {
-      add:x=>{
-        c=x
-        return x
-      },
-      commit:_=>c
+function restFood(a,x,r=x.slice()){
+  for(let i=-1;++i<a.length;){
+    if(a[i]==='German team offense'){
+      let t=['Beer','Nut','Nut','Nut'],c=r.slice()
+      while(t.length){
+        let j=c.indexOf(t.shift())
+        if(j===-1)return r
+        c.splice(j,1)
+      }
+      r=c
+    }
+    if(a[i]==='German team scored'){
+      let t=['Beer','Meat','Meat'],c=r.slice()
+      while(t.length){
+        let j=c.indexOf(t.shift())
+        if(j===-1)return r
+        c.splice(j,1)
+      }
+      r=c
+    }
+    if(a[i]==='Brazil team scored'){
+      let t=['Nut','Nut','Nut','Nut','Nut'],c=r.slice()
+      while(t.length){
+        let j=c.indexOf(t.shift())
+        if(j===-1)return r
+        c.splice(j,1)
+      }
+      r=c
     }
   }
+  return r
 }
-const r=CodeTracker.init('name')
-console.log(r.add('data'))
-console.log(r.commit())
+console.log(restFood([
+  "German team offense",
+  "German team scored",
+  "Brazil team scored",
+  "German team scored",
+  "German team scored",
+  "German team scored"
+  ],[
+"Beer","Nut","Nut","Nut","Meat","Nut",
+"Nut","Nut","Beer","Nut","Meat","Beer",
+"Nut","Meat","Beer","Nut","Meat"]))
