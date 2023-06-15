939c50c5e529b37122d36c24c40c549a836da965
Jhonny <gonzodzen@mail.ru>
2023-06-15 21:15:27 +0300

    Simple Fun #180: Repeat Adjacent

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-function cardsAndPero(s,a=s.match(/.{3,3}/g),c='PKHT'){
-  if(a.length>[...new Set(a)].length)return [-1,-1,-1,-1]
-  a=a.map(e=>e[0]).reduce((a,c)=>(a[c]=a[c]+1||1,a),{})
-  for(let i=-1;++i<c.length;)if(!a[c[i]])a[c[i]]=0
-  return [13-a['P'],13-a['K'],13-a['H'],13-a['T']]
+function repeatAdjacent(s,f=x=>new Set(x).size,r=[]){
+  s=s.replace(/(.)\1+/g,' $& ').split` `.filter(e=>e)
+  for(let i=-1;++i<s.length;){
+    if(f(s[i])===1&&s[i].length>1){
+      for(let j=i;++j<=s.length;){
+        if(f(s[j])!==1||s[j].length===1){
+          let t=s.slice(i,j)
+          if(t.length>1)r.push(t)
+          i=j
+          break
+        }
+      }
+    }
+  }
+  return r.length
 }
 
-console.log(cardsAndPero('P05P01P02P03P13P09P11P07K01P12K03K02K13K12K10K08H03H02H13H12H10H08T01T03T02T13T12T10T08P04K07H02T07H06T11K11T05K05H05H11'))
-console.log(cardsAndPero('P01K02P03P11K09K10P13P10'))
-console.log(cardsAndPero('P10K10H10T01'))
-console.log(cardsAndPero('P04H13T04K10H02K02K12H07H11H03T13T11T01K13'))
+console.log(repeatAdjacent('ccccoodeffffiiighhhhhhhhhhttttttts'))
+console.log(repeatAdjacent('ccccoooooooooooooooooooooooddee'))
+console.log(repeatAdjacent('gztxxxxxggggggggggggsssssssbbbbbeeeeeeehhhmmmmmmmitttttttlllllhkppppp'))
