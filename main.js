dfbd4cde27276ace79ffd55ba463dc2f7420edb9
Jhonny <gonzodzen@mail.ru>
2023-06-15 23:08:08 +0300

    Kaprekar's Routine

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function extendPinocchioNose(s){
-  return s.split` `.some((e,i,v)=>(v[i+1]||'').match(e))
+function kaprekar(m,c=0,n=+(m+'').padEnd(4,'0')){
+  return new Set(n+'').size===1||c>7?-1:n===6174?c:kaprekar(+[...n+''].map(Number).sort((a,b)=>b-a).join``- +[...n+''].map(Number).sort((a,b)=>a-b).join``,c+1)
 }
 
-console.log(extendPinocchioNose("Im ver very good student,i have great grades."))
+console.log(kaprekar(12))
