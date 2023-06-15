3aaa55ae1857046fb7b77bf327ed97be6265b3fe
Jhonny <gonzodzen@mail.ru>
2023-06-15 22:39:07 +0300

    Pinocchio's polygraph.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function sumAccounts(s){
-  return s.match(/\-*\$\d+/g).map(e=>+e.replace(/\$/,'')).reduce((a,c)=>a+c,0)
+function extendPinocchioNose(s){
+  return s.split` `.some((e,i,v)=>(v[i+1]||'').match(e))
 }
 
-console.log(sumAccounts("Gal-Sal's Greengrocer -$200. Kesh Village Store, as of last week $400; promised to pay soon. Sumer's Giant 100X supermarket -$600. Sukalgir's veg delivery service -$200. Eridu 10 a Day store: $300. Isin's Greens Greens Greens health store: -$100. My mate Abu: $0. Alulim the First King of Eridu -$1000."))
+console.log(extendPinocchioNose("Im ver very good student,i have great grades."))
