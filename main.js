87f47a910315f2587889d98b31f268675e9e967a
Jhonny <gonzodzen@mail.ru>
2023-06-16 00:31:46 +0300

    So Easy: Arrange students' seats

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function censor(s){
-  return s.split` `.map(e=>e[0]<{}?[...e].map(u=>u<{}&&u.match(/[a-z]/i)?u:'_').join``:e).join` `
+function arrangeSeats(a,r=x=>x.sort((a,b)=>a.height-b.height||a.name.localeCompare(b.name)),m=[],f=[],q=[],c=1){
+  a.forEach(e=>e.sex==='M'?m.push(e):f.push(e))
+  r(m),r(f)
+  while(m.length){
+    let o={desk:c++,seat1:m.shift().name,seat2:f.shift().name}
+    q.push(o)
+  }
+  return q
 }
 
-console.log(censor("Gertrude WENT overboard on the Sailor Jerry..."))
+console.log(arrangeSeats([{name:"John",height:170,sex:"M"},{name:"Tom",height:175,sex:"M"},{name:"Jack",height:180,sex:"M"},{name:"Jone",height:175,sex:"F"},{name:"Anne",height:175,sex:"F"},{name:"Alice",height:178,sex:"F"}]))
