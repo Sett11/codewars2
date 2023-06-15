81394c8c108c6aa733060056726d79b0f3a6577f
Jhonny <gonzodzen@mail.ru>
2023-06-15 13:20:16 +0300

    When Sigma1 Function Has Equals Values For an Integer and Its Reversed One

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-Array.prototype.isSparse = function() {
-  return this.length&&this.length!==this.filter(_=>true).length
+const r=x=>x.reduce((a,c)=>a+c,0)
+const f=(x,a=[])=>{
+  for(let i=0;++i<=Math.sqrt(x);)if(!(x%i))a.push(i,x/i)
+  return r(a)
 }
+function equalSigma1(n,c=826,a=[528,825]){
+  while(c<=n){
+    let t=+[...c+''].reverse().join``
+    if(f(c)===f(t)&&c!==t)a.push(c,t)
+    c++
+  }
+  return r([...new Set(a.filter(e=>e<=n))])
+}
+
+console.log(equalSigma1(1561))
