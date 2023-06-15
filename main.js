ccb15b9abb43cd23a9e25ae783dc0848181f8c3a
Jhonny <gonzodzen@mail.ru>
2023-06-15 20:33:18 +0300

    Simple Fun #214: Pascal Matrix

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function segmentSumsMatrix(a){
-  return Array(a.length).fill(Array(a.length).fill(0)).map((e,i)=>e.map((u,j)=>a.slice(Math.min(i,j),Math.max(i,j)+1).reduce((a,c)=>a+c,0)))
+function p(d,a=[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]){
+  if(d<a.length)return a.slice(0,d)
+  if(a.length===d)return a
+  let t=[1]
+  for(let i=-1;++i<a[a.length-1].length;)t.push((a[a.length-1][i]+a[a.length-1][i+1])||1)
+  a.push(t)
+  return p(d,a)
+}
+function pascalMatrix(n){
+  return p(Math.floor(n/2)+1).map(e=>e.join`,0,`.split`,`.map(Number)).map((e,i,v)=>{
+    while(e.length<v[v.length-1].length)e.push(0),e.unshift(0)
+    return e
+  })
 }
 
-console.log(segmentSumsMatrix([1,2,3]))
+console.log(pascalMatrix(15))
