f1b22360b4d54543423e8e6c3af163bee3f543a8
Jhonny <gonzodzen@mail.ru>
2023-06-16 16:35:33 +0300

    Lychrel numbers

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function alg196(n,c=0){
-  const f=x=>[...x+''].join``===[...x+''].reverse().join``
-  n+=+[...n+''].reverse().join``
+function howManyIterationsNeeded(n,m=261,c=1){
+  const f=x=>[...x+''].join``===[...x+''].reverse().join``,r=x=>x+ +[...x+''].reverse().join``
+  n=r(n)
   while(!f(n)){
-    if(c>=261)return -1
-    n+=+[...n+''].reverse().join``,c++
+    if(c>=m)return null
+    n=r(n),c++
   }
-  return n
+  return c
 }
 
-console.log(alg196(196))
+console.log(howManyIterationsNeeded(12,8))
