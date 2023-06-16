81d2349089635943e74b3efaf9e251cf120bf180
Jhonny <gonzodzen@mail.ru>
2023-06-16 11:49:44 +0300

    Swedish Personal Number

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-function furthestDistance(a,k,m=-1){
-  for(let i=-1;++i<a.length;)for(let j=a.length;--j>i;)if(Math.abs(a[i]-a[j])>=k)m=Math.max(Math.abs(j-i),m)
-  return m
+function validatePersonalNumber(s){
+  return /^(1|2)(\d)(\d)(\d)(0|1)(\d)([0-3])(\d)(\-|\+)*(\d)(\d)(\d)(\d)$/.test(s)&&s.slice(4,6)!=='00'&&s.slice(4,6)<13&&s.slice(6,8)!=='00'&&+s.slice(0,4)<2017&&+s.slice(0,4)>1899&&s.replace(/\d/g,'').length<2&&+s.slice(6,8)<32
 }
 
-console.log(furthestDistance([8,7,1,9],5))
-console.log(furthestDistance([1,2,3,4],1))
-console.log(furthestDistance([3,4,1,2],2))
-console.log(furthestDistance([8,7,1,9],100))
+console.log(validatePersonalNumber("19001013-0044"))
+console.log(validatePersonalNumber("19420503-4566"))
