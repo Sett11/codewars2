615c1095a72628aca61c5f58639e419958ae04e1
Jhonny <gonzodzen@mail.ru>
2023-06-16 11:21:54 +0300

    The furthest distance of index

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function isTouhouTitle(s,a=['of','in','at','to','and','a','an','the'],t=s.slice(s.indexOf(' ')+1).split` `.map(e=>!e.match(/[a-z]/gi)?e:e.replace(/[^a-z0-9]/gi,''))){
-  return [...s.slice(0,5)].every(e=>(e).charCodeAt()>19000&&(e).charCodeAt()<46000)&&s.slice(0,2)==='東方'&&s.slice(0,s.indexOf('～')-1).length===5&&s.slice(5,8)==='　～ '&&s[s.length-1]==='.'&&t.filter(e=>!a.includes(e)).every(e=>e[0].match(/[A-Z]/))&&t.map(e=>[e,e.toLowerCase()]).filter(e=>a.includes(e[1])).every(e=>e[0]===e[1])&&t.every(e=>!e.match(/\d/))
+function furthestDistance(a,k,m=-1){
+  for(let i=-1;++i<a.length;)for(let j=a.length;--j>i;)if(Math.abs(a[i]-a[j])>=k)m=Math.max(Math.abs(j-i),m)
+  return m
 }
 
-console.log(isTouhouTitle("東方鞩鱽羧　～ Fashion Limit."))
+console.log(furthestDistance([8,7,1,9],5))
+console.log(furthestDistance([1,2,3,4],1))
+console.log(furthestDistance([3,4,1,2],2))
+console.log(furthestDistance([8,7,1,9],100))
