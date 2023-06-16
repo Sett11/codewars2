46ba9031f49547713e8a712a6077421a8ce009af
Jhonny <gonzodzen@mail.ru>
2023-06-16 10:37:56 +0300

    Unicode Madness: 東方名確認　～ Eastern Title Validator.

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-function liquidMixing(d,f=x=>x.sort((a,b)=>a-b),q=[],r=[]){
-  while(d.length){
-    q.unshift(d.shift()),f(q)
-    if(q.length&1)r.push(q[Math.floor(q.length/2)])
-    else r.push((q[q.length/2-1]+q[q.length/2])/2)
-  }
-  return r
+function isTouhouTitle(s,a=['of','in','at','to','and','a','an','the'],t=s.slice(s.indexOf(' ')+1).split` `.map(e=>!e.match(/[a-z]/gi)?e:e.replace(/[^a-z0-9]/gi,''))){
+  return [...s.slice(0,5)].every(e=>(e).charCodeAt()>19000&&(e).charCodeAt()<46000)&&s.slice(0,2)==='東方'&&s.slice(0,s.indexOf('～')-1).length===5&&s.slice(5,8)==='　～ '&&s[s.length-1]==='.'&&t.filter(e=>!a.includes(e)).every(e=>e[0].match(/[A-Z]/))&&t.map(e=>[e,e.toLowerCase()]).filter(e=>a.includes(e[1])).every(e=>e[0]===e[1])&&t.every(e=>!e.match(/\d/))
 }
 
-console.log(liquidMixing([10, 20, 8, 12, 6]))
+console.log(isTouhouTitle("東方鞩鱽羧　～ Fashion Limit."))
