a81955b15981349c81c339ab4f79f23ce548e1d2
Jhonny <gonzodzen@mail.ru>
2023-06-17 15:33:44 +0300

    Get your steppin' on son

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-function stress(s,ss=s.replace(/\d/g,''),sss=+s.replace(/\D/g,'')){
-  const a=['ic','sion','tion','ious'],b=['ty','al','ise','ate']
-  return !a.includes(ss)&&!b.includes(ss)||sss<3||sss>11?'invalid word':a.includes(ss)?(sss-1)+'pen':(sss-2)+'ante-pen'
+const f=x=>x.reduce((a,c)=>a.concat(Array.isArray(c)?f(c):[c]),[])
+function wordStep(s){
+  let a=s.split` `.map(e=>[...e]),b=a.filter((e,i)=>!(i&1)),c=a.filter((e,i)=>i&1),r=Array(c.map(e=>e.join``).join``.length-c.length+1).fill(Array(b.map(e=>e.join``).join``.length-b.length+1).fill(' ')).map(e=>JSON.parse(JSON.stringify(e)))
+  a=a.map((e,i,v)=>i&1&&i!==v.length-1?e.slice(1,-1):i&1&&i===v.length-1?e.slice(1):e)
+  for(let i=-1;++i<a.length;){
+    if(!(i&1)){
+      let j=r.findIndex(e=>e.every(u=>u===' ')),q=Math.max(...f(r.map(e=>e.map((u,i)=>u!==' '?i:0))))
+      j=j===-1?0:j,q=q===-1?0:q
+      for(let k=0;k<a[i].length;k++)r[j][q++]=a[i][k]
+    }
+    else{
+      let j=r.findIndex(e=>e.every(u=>u===' ')),q=Math.max(...f(r.map(e=>e.map((u,i)=>u!==' '?i:0))))
+      for(let k=-1;++k<a[i].length;)r[j++][q]=a[i][k]
+    }
+  }
+  return r
 }
 
-console.log(stress('10sion'))
+console.log(wordStep('CODEWARS SNAIL LAKE EEK'))
+console.log(wordStep('SNAKES SHOE EFFORT TRUMP POTATO'))
