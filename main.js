954351e727adc7f8ad3fc26ef6c6050937c58335
Jhonny <gonzodzen@mail.ru>
2023-06-17 10:55:41 +0300

    Throw from list - Error Handling #3

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-function howManyIterationsNeeded(n,m=261,c=1){
-  const f=x=>[...x+''].join``===[...x+''].reverse().join``,r=x=>x+ +[...x+''].reverse().join``
-  n=r(n)
-  while(!f(n)){
-    if(c>=m)return null
-    n=r(n),c++
-  }
-  return c
+function validate(u,p){
+  u=u.trim(),p=p.trim()
+  if(u.length>12)throw ERRORS['usernameTooLong'](u)
+  if(u.match(/[\(\)\{\}\[\]\|\;\:\'\"\/\?\.\,\<\>\~\-\=\+\*\&\^\%\$\@\!]/))throw ERRORS['usernameInvalidCharacters'](u)
+  if(!u||u.length<1)throw ERRORS['usernameTooShort'](u)
+  if(p.length>24)throw ERRORS['passwordTooLong'](p)
+  if(!p||p.length<8)throw ERRORS['passwordTooShort'](p)
+  if(p.match(/[\(\)\{\}\[\]\|\;\:\'\"\/\?\.\,\<\>\~\-\=\+\*\&\^\%\$\@\!]/))throw ERRORS['passwordInvalidCharacters'](p)
+  if(!p.match(/[A-Z]/))throw ERRORS['passwordNoCapital'](p)
+  if(!p.match(/\d/))throw ERRORS['passwordNoNumber'](p)
+  if(p.match(u))throw ERRORS['passwordContainsUsername'](p)
+  return true
 }
-
-console.log(howManyIterationsNeeded(12,8))
