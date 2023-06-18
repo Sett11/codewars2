f3009eb971a17b17d4cd374c18eba3ba7d7c1cdf
Jhonny <gonzodzen@mail.ru>
2023-06-18 14:57:16 +0300

    Midpoint inception

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-function reversiRow(a,r=Array(8).fill('.')){
-  for(let i=-1;++i<a.length;){
-    r[a[i]]=i&1?'O':'*'
-    let k,t
-    for(let j=a[i];++j<r.length;){
-      if(r[j]===r[a[i]]){
-        k=j
-        break
-      }
-    }
-    for(let j=a[i];--j>-1;){
-      if(r[j]===r[a[i]]){
-        t=j
-        break
-      }
-    }
-    if(k!==undefined){
-      let x=r.slice(a[i],k+1)
-      if(!x.includes('.')&&x[0]===x[x.length-1]&&x.some(e=>e!==x[0])){
-        r=r.slice(0,a[i]).concat(x.map(e=>x[0])).concat(r.slice(k+1))
-      }
-    }
-    if(t!==undefined){
-      let x=r.slice(t,a[i]+1)
-      if(!x.includes('.')&&x[0]===x[x.length-1]&&x.some(e=>e!==x[0])){
-        r=r.slice(0,t).concat(x.map(e=>x[0])).concat(r.slice(a[i]+1))
-      }
-    }
+const f=(s,c='')=>{
+  if(s.length<3)return c
+  let ns=[...s],t,v
+  if(ns.length&1){
+    t=ns.slice(0,~~(ns.length/2)),v=ns.slice(~~(ns.length/2)+1)
+    c+=ns[~~(ns.length/2)]
+    c+=f(t)+f(v)
   }
-  return r.join``
+  else{
+    t=ns.slice(0,~~(ns.length/2)-1),v=ns.slice(~~(ns.length/2)+1)
+    c+=ns.slice(~~(ns.length/2)-1,~~(ns.length/2)+1).join``
+    c+=f(t)+f(v)
+  }
+  return c
 }
+const midPoint=x=>x.split` `.map(e=>f(e)).join` `
 
-console.log(reversiRow([3,4,5]))
-console.log(reversiRow([4,6,7,5]))
-console.log(reversiRow([ 3, 2, 7, 1, 0 ]))
+
+console.log(midPoint('a ab abc abcd abcde abcdef abcdefg abcdefgh abcdefghi abcdefghij'))
