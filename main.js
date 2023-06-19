79db17ff564f43286562e07f1ad87cdbf6ca58a8
Jhonny <gonzodzen@mail.ru>
2023-06-19 15:27:24 +0300

    (Dis)member functions

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-function mapEmUp(a,b){
-  for(let i=-1;++i<a.length;){
-    for(let j=-1;++j<=b.length;){
-        if(j===b.length){
-            a[i]=-1
-            break
-        }
-        if(b[j][0](a[i])){
-            a[i]=b[j][1](a[i])
-            break
-        }
-    }
-  }
-  return a
+function dismember(f){
+  return f.toString().match(/\(.*\)/g).map(e=>e.replace(/\(|\)/g,''))[0].split`,`.filter(e=>e).map(e=>e.trim())
 }
-console.log(mapEmUp([1,2,3,4,5],[
-    [ function (i) { return i>4; },
-      function (i) { return i; }],
-    [function (i) { return i; },
-      function (i) { return "x"; }]
-  ]))
+
+console.log(dismember(function(z,b){return z+b}))
+console.log(dismember(function(){return 0}))
