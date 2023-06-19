46e74118dd3712c31d427c2cdce76cf6558687c2
Jhonny <gonzodzen@mail.ru>
2023-06-19 15:11:52 +0300

    Programmed mapper

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-makeLazy=(...a)=>_=>a[0](...a.slice(1))
-
-function add(a,b){
-    return a + b;
+function mapEmUp(a,b){
+  for(let i=-1;++i<a.length;){
+    for(let j=-1;++j<=b.length;){
+        if(j===b.length){
+            a[i]=-1
+            break
+        }
+        if(b[j][0](a[i])){
+            a[i]=b[j][1](a[i])
+            break
+        }
+    }
+  }
+  return a
 }
-let lazy_value = makeLazy(add, 2, 3)
-
-console.log(lazy_value())
+console.log(mapEmUp([1,2,3,4,5],[
+    [ function (i) { return i>4; },
+      function (i) { return i; }],
+    [function (i) { return i; },
+      function (i) { return "x"; }]
+  ]))
