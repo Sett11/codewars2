64ccd494ed35f1c0090c820691bafa48291215a7
Jhonny <gonzodzen@mail.ru>
2023-06-19 22:07:22 +0300

    Down Arrow With Numbers

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function dismember(f){
-  return f.toString().match(/\(.*\)/g).map(e=>e.replace(/\(|\)/g,''))[0].split`,`.filter(e=>e).map(e=>e.trim())
+function getADownArrowOf(n,r=[],a=[],i=1){
+  while(i<=n)r.push(a.join``+i%10+a.slice().reverse().join``),a.push(((i++)%10))
+  return r.reverse().map((e,i,v)=>{
+    let t=Math.max(...v.map(u=>u.length))
+    while(e.length<t)e=e.replace(e,' $& ')
+    while(e[e.length-1]===' ')e=e.slice(0,-1)
+    return e
+  }).join`\n`
 }
 
-console.log(dismember(function(z,b){return z+b}))
-console.log(dismember(function(){return 0}))
+console.log(getADownArrowOf(10))
