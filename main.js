1a3319b8473896e9500ca16e993c7e66cc27923c
Jhonny <gonzodzen@mail.ru>
2023-06-20 13:39:35 +0300

    All Star Code Challenge #31

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-hypotenuse=(a,b)=>Math.sqrt(a**2+b**2)
-leg=(c,a)=>Math.sqrt(c**2-a**2)
+function helpJesse(a){
+  return a.map(e=>`Pour ${e.amount} mL of ${e.solution} into a ${e.instrument}`+(e.note?` (${e.note})`:''))
+}
 
-console.log(hypotenuse(3,4))
-console.log(leg(5,3))
+console.log(helpJesse([{
+  solution : "Sodium Chloride",
+  amount: 5,
+  instrument: "Graduated Cylinder"   
+},
+{
+  solution : "Hydrochloric Acid",
+  amount: 250,
+  instrument: "Boiling Flask"   
+},
+{
+  solution : "Water",
+  amount: 100,
+  instrument: "Erlenmeyer Flask",
+  note: "Do NOT mess this step up, Jesse!"
+}]))
