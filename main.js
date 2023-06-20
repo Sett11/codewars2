b10663f97dfe4e2b32ed55c4165a16f71943ae2f
Jhonny <gonzodzen@mail.ru>
2023-06-20 13:20:17 +0300

    All Star Code Challenge #7

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const translate=s=>{
-  return s.length<2?s:!s[0].match(/a|u|o|e|i/)?s.slice(1)+s[0]+'ay':s+'ay'
+function compute(n,a=[],c=1){
+  if(!(n&1)||n<3)return 0
+  while(c<n)a.push(c+=2)
+  return eval('1+'+ a.map(e=>`(1/${e}**2)`).join`+`)
 }
 
-console.log(translate('billy'))
+console.log(compute(5))
