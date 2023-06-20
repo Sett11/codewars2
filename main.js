f15f8c19d6a058d9c4d293ef9232c9a9e9c52bba
Jhonny <gonzodzen@mail.ru>
2023-06-20 09:58:52 +0300

    Simple Fun #226: Airline Seating

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-function getADownArrowOf(n,r=[],a=[],i=1){
-  while(i<=n)r.push(a.join``+i%10+a.slice().reverse().join``),a.push(((i++)%10))
-  return r.reverse().map((e,i,v)=>{
-    let t=Math.max(...v.map(u=>u.length))
-    while(e.length<t)e=e.replace(e,' $& ')
-    while(e[e.length-1]===' ')e=e.slice(0,-1)
-    return e
-  }).join`\n`
+function airlineSeating(a,p,s){
+  return a.map(e=>{
+    if(p&&e==='f'){
+      p--
+      return 'p'
+    }
+    if(!p&&s&&e==='c'){
+      s--
+      return 's'
+    }
+    if(!p&&s&&a.filter(u=>u==='c').length<s){
+      s--
+      return 's'
+    }
+    return ' '
+  })
 }
 
-console.log(getADownArrowOf(10))
+console.log(airlineSeating(["f",  "f",  "f",  "c",  "c"],1,1))
+console.log(airlineSeating(["f",  "f",  "f",  "c",  "c",  "c"],2,5))
