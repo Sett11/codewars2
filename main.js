6532b515d89b57fdc085fa7b01aa123adb159e22
Jhonny <gonzodzen@mail.ru>
2023-07-27 21:46:16 +0300

    Balanced Braces (with non-brace characters)

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-squareRoot=x=>+(x**.5).toFixed(5)
+function isBalanced(q,s=q.replace(/[^\{\}\[\]\(\)]/g,''),c=0){
+    while(s.match(/\{\}|\(\)|\[\]/)){
+        if(s.match(/\(\)/))s=s.replace(/\(\)/,''),c++
+        if(s.match(/\{\}/))s=s.replace(/\{\}/,''),c++
+        if(s.match(/\[\]/))s=s.replace(/\[\]/,''),c++
+    }
+    return !s?[!0,c,0]:[!1,c,s.length]
+}
 
-console.log(squareRoot(39))
+console.log(isBalanced("(([()]))"))
+console.log(isBalanced("([{}])((){}[{}])"))
+console.log(isBalanced("((())"))
+console.log(isBalanced("((()]))"))
