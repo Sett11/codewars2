d4f9cb64e2da36553758a6dab6b7ea1d189c059d
Jhonny <gonzodzen@mail.ru>
2023-07-29 14:16:51 +0300

    Double value every next call

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-function isBalanced(q,s=q.replace(/[^\{\}\[\]\(\)]/g,''),c=0){
-    while(s.match(/\{\}|\(\)|\[\]/)){
-        if(s.match(/\(\)/))s=s.replace(/\(\)/,''),c++
-        if(s.match(/\{\}/))s=s.replace(/\{\}/,''),c++
-        if(s.match(/\[\]/))s=s.replace(/\[\]/,''),c++
-    }
-    return !s?[!0,c,0]:[!1,c,s.length]
+let n=1
+class Class{
+  static getNumber(){
+    n*=2
+    return n/2
+  }
 }
 
-console.log(isBalanced("(([()]))"))
-console.log(isBalanced("([{}])((){}[{}])"))
-console.log(isBalanced("((())"))
-console.log(isBalanced("((()]))"))
+console.log(Class.getNumber())
+console.log(Class.getNumber())
+console.log(Class.getNumber())
