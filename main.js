09886e0739179088e267af237154f39d36419962
Jhonny <gonzodzen@mail.ru>
2023-08-13 14:53:53 +0300

    Large Factorials

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function iccanobif(n) {
-  let a=0,b=1,r=[]
-  while(r.length<n){
-    [a,b]=[a+b,a]
-    r.push(a)
+const s=(a,b)=>{
+  let c=j=0
+  while(c||j<a.length){
+    const p=c+((j<a.length)?b*a[j]:0)
+    a[j]=p%10,c=Math.floor(p/10)
+    j+=1
   }
-  return r.reverse()
 }
+const f=x=>{
+  const a=[1]
+  for(let i=2;i<=x;++i)s(a,i)
+  return a
+}
+const factorial=n=>f(n).reverse``.join``
 
-console.log(iccanobif(64))
+console.log(factorial(1123))
