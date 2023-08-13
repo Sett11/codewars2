1070f26052ed24149e308ffce783af7828176fc2
Jhonny <gonzodzen@mail.ru>
2023-08-13 21:24:54 +0300

    Validate Hexadecimal

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-const s=(a,b)=>{
-  let c=j=0
-  while(c||j<a.length){
-    const p=c+((j<a.length)?b*a[j]:0)
-    a[j]=p%10,c=Math.floor(p/10)
-    j+=1
-  }
+function isHex(s){
+	return typeof parseInt(s,16)=='number'&&(s.length==3||s.length==6)&&!s.match(/[g-z]/i)
 }
-const f=x=>{
-  const a=[1]
-  for(let i=2;i<=x;++i)s(a,i)
-  return a
-}
-const factorial=n=>f(n).reverse``.join``
 
-console.log(factorial(1123))
+console.log(isHex('ff00ff'))
+console.log(isHex('b0Ff'))
