cb5661f28f9c294886f891219ee97722532fe409
Jhonny <gonzodzen@mail.ru>
2023-09-05 22:19:00 +0300

    Sorting Arrays

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-twins=a=>a.every(e=>a.filter(u=>e==u).length==2)
+sortArray=(a,b,o={},c=a.map((e,i)=>o[e[0]]=i))=>b.map(e=>[o[e[0]],e]).sort((a,b)=>a[0]-b[0]).map(e=>e[1])
 
-console.log(twins([1,2,3,1,2,3]))
+console.log(sortArray(['giraffe', 'orangutan', 'impala', 'elephant', 'rhino'],['rattlesnake', 'eagle', 'geko', 'iguana', 'octopus']))
