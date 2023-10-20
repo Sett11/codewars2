4a468e10e9052060e882230f401380237679068b
Jhonny <gonzodzen@mail.ru>
2023-10-20 15:47:24 +0300

    Rap-Name Generator

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-f=s=>s<{}
+function rapNameGen(s){
+  const o={'0':'ero','1':'ne','2':'wo','3':'hree','4':'our','5':'ive','6':'ix','7':'even','8':'ight','9':'ine'}
+  const a=Math.round((+s[0]+ +s[1])/2),b=Math.round([...s.slice(6)].reduce((a,c)=>+a + +c)/2)
+  return `${a+o[a]} ${b+o[b]}`
+}
 
-console.log(f('A'))
-console.log(f('a'))
+console.log(rapNameGen('01.01.01'))
