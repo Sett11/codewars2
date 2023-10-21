d1dbc1fbb85df79018f4f89ac4b0b044c130e65d
Jhonny <gonzodzen@mail.ru>
2023-10-21 13:29:07 +0300

    Find the maximal product

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function search(n,r) {
-  return !!JSON.stringify(r).match(n)
+const findMaxProduct=a=>{
+  let m=-Infinity
+  for(let i=-1;++i<a.length;){
+    m=Math.max(m,a.slice(i).filter((_,j)=>!(j%(i+1))).reduce((a,c)=>a*c))
+  }
+  return m
 }
+
+console.log(findMaxProduct([4, 6, 8, 11, 13, 5, 7, 9]))
