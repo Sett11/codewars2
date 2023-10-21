d075c26730f4ebed8333236e527b34b8841b0ac9
Jhonny <gonzodzen@mail.ru>
2023-10-21 08:46:23 +0300

    Binary Tree Search (not BST)

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function wordscore(s) {
-  const o={"a" : 1, "b" : 3,"c" : 3,"d" : 2, "e" : 1, "f" : 4,"g" : 2,"h" : 4,"i" : 1,"j" : 8,"k" : 5,"l" : 1,"m" : 3,"n" : 1,"o" : 1,"p" : 3,"q" : 10,"r" : 1,"s" : 1,"t" : 1,"u" : 1,"v" : 4, "w" : 4,"x" : 8,"y" : 4,"z" : 10}
-  return [...s].map(e=>o[e]).reduce((a,c)=>a+c)*s.length+(s.length>6?50:0)
+function search(n,r) {
+  return !!JSON.stringify(r).match(n)
 }
-
-console.log(wordscore('deceive'))
