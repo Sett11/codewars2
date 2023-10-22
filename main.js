c603f91d53101ec490e5840581c3ab8f7b4af7f5
Jhonny <gonzodzen@mail.ru>
2023-10-22 21:31:21 +0300

    What rank is this card?

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const gameOfFives=(a,b,f=x=>x.filter(e=>e===5).length)=>f(a)===f(b)?"Drinks All Round! Free Beers on Bjorg!":"Uh Oh! Bjorg's a donut! No beer for anyone!"
+function rank(c) {
+  let r=+c.replace(/\D/g,''),o={'T':10,'J':11,'Q':12,'K':13,'A':14}
+  if(c.match(/\d/))return r>1&&r<15?r:0
+  return o[c[0]]||0
+}
 
-console.log(gameOfFives([5,7,3,5,7,1,5,8,1,5,6,1],[5,1,5,6,6,6,5,5]))
+console.log(rank('Th'))
+console.log(rank('6i'))
