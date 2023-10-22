1c0a6424c0cd2477b5355513965524a6e036ce8a
Jhonny <gonzodzen@mail.ru>
2023-10-22 22:30:41 +0300

    refactoring the statement

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function rank(c) {
-  let r=+c.replace(/\D/g,''),o={'T':10,'J':11,'Q':12,'K':13,'A':14}
-  if(c.match(/\d/))return r>1&&r<15?r:0
-  return o[c[0]]||0
-}
+const refactoryString=s=>s.replace(/\d+/g,e=>String.fromCharCode(+e+97))
 
-console.log(rank('Th'))
-console.log(rank('6i'))
+console.log(refactoryString('str8ng is he17e'))
