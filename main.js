5f5772abfcd92d2a749cc5212aafe7a517f52fe4
Jhonny <gonzodzen@mail.ru>
2023-10-22 10:39:20 +0300

    5 without alphanums!!!

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const matrixSquareUp = (n) => {
-  const a = Array(n).fill(1).map((_, i) => (n - i)+''),r = []
-  for (let i = n + 1; --i; ) {
-    let t = Array(n - i).fill("x")
-    r.unshift(t.concat(a.slice(t.length)))
-  }
-  return r
-}
+_=()=>+(!!{})+(!!{})+(!!{})+(!!{})+(!!{})
 
-console.log(matrixSquareUp(4));
+console.log(_())
