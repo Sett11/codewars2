8ec8ad0ea83d8870a2e2fa85e88fb78cc10bad6c
Jhonny <gonzodzen@mail.ru>
2023-10-22 18:14:48 +0300

    Harry and the Mathematician's Chicken

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-function cookingTime(w){
-  let n=Math.ceil((w*1000/450*20+20)/5)*5,m=n/60,k=n%60
-  return !w?'There is no chicken!':n<60?`${n} mins`:`${Math.floor(m)} hr${m>=2?'s':''} ${k?k:''} ${k?'mins':''}`.trim()
+function chickenOptions(n){
+  const check=x=>{
+    let a=b=c=x
+    while(a>0||b>0||c>0){
+      if([a,b,c].some(e=>(e%3===0||e%6===0||e%10===0)&&e>0))return true
+      a-=3,b-=6,c-=10
+    }
+    return false
+  }
+  return Array(n+1).fill(0).map((e,i)=>e+i).filter(e=>check(e)||e%3===0||e%6===0||e%10===0)
 }
 
-console.log(cookingTime(2.2))
-console.log(cookingTime(0.3))
-console.log(cookingTime(1))
+
+console.log(chickenOptions(21))
