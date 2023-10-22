e0522e2a450b35b474c2346497c0d002263d7ebe
Jhonny <gonzodzen@mail.ru>
2023-10-22 13:52:20 +0300

    Selection in elementary school

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-function romanize(s) {
-  const o={'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'e','ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n','о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'kh','ц':'ts','ч':'ch','ш':'sh','щ':'shch','ъ':'ie','ы':'y','ь':'','э':'e','ю':'iu','я':'ia'}
-  return s.toLowerCase().split` `.map(e=>{
-    e=e.replace(/./g,e=>o[e])
-    return e.charAt(0).toUpperCase()+e.slice(1)
-  }).join` `
+function schoolSelection(a) {
+  const o={
+    'Kindergarten': 0,
+    '1st grade': 0,
+    '2nd grade': 0, 
+    '3rd grade': 0, 
+    '4th grade': 0
+  }
+  for(let i in a){
+    if(a[i]==5)o['Kindergarten']++
+    if(a[i]==6)o['1st grade']++
+    if(a[i]==7)o['2nd grade']++
+    if(a[i]==8)o['3rd grade']++
+    if(a[i]==9)o['4th grade']++
+  }
+  return o
 }
 
-console.log(romanize('Иван Иванович'))
-console.log(romanize('Никита Смирнов'))
+console.log(schoolSelection([5, 7, 4, 9, 10, 5, 15, 9, 5]))
