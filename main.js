04190e15f527c433d7973890e4a89bf16b9b5260
Jhonny <gonzodzen@mail.ru>
2023-10-22 15:12:56 +0300

    A twisted way to select an element from an array.

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function schoolSelection(a) {
-  const o={
-    'Kindergarten': 0,
-    '1st grade': 0,
-    '2nd grade': 0, 
-    '3rd grade': 0, 
-    '4th grade': 0
-  }
-  for(let i in a){
-    if(a[i]==5)o['Kindergarten']++
-    if(a[i]==6)o['1st grade']++
-    if(a[i]==7)o['2nd grade']++
-    if(a[i]==8)o['3rd grade']++
-    if(a[i]==9)o['4th grade']++
-  }
-  return o
-}
+select=(a,b)=>b.filter(e=>e[0]===a.sort().reverse()[2][2])[0]||'Nothing here'
 
-console.log(schoolSelection([5, 7, 4, 9, 10, 5, 15, 9, 5]))
+console.log(select(['banana','rose','orange','apple'], ['carrot','nectarines','cucumber','ananas']))
