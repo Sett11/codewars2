3b4ab7266a01fce2cab60027a3d94c66e7af9c9a
Jhonny <gonzodzen@mail.ru>
2023-10-22 18:18:36 +0300

    Javascript Mathematician

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-function chickenOptions(n){
-  const check=x=>{
-    let a=b=c=x
-    while(a>0||b>0||c>0){
-      if([a,b,c].some(e=>(e%3===0||e%6===0||e%10===0)&&e>0))return true
-      a-=3,b-=6,c-=10
-    }
-    return false
-  }
-  return Array(n+1).fill(0).map((e,i)=>e+i).filter(e=>check(e)||e%3===0||e%6===0||e%10===0)
-}
+const calculate=(...a)=>(...b)=>a.concat(b).reduce((e,c)=>e+c,0)
 
 
-console.log(chickenOptions(21))
+console.log(calculate(21)(1))
