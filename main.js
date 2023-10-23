e4676f7891a3ce040c318cbd360a9d2e197a6c22
Jhonny <gonzodzen@mail.ru>
2023-10-23 12:48:27 +0300

    Format With

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-function formatDataValue(n) {
-    let a=b=c=0
-    while(n>=1000000){
-          a+=1
-          n-=1000000
-      }
-    while(n>=1000){
-          b+=1
-          n-=1000
-      }
-    if(n>=1)c=n
-    return `${a?a+'tb':''} ${b?b+'gb':''} ${c?c+'mb':''}`.toUpperCase().trim()||'0MB'
-  }
+String.prototype.formatWith = function (...a) {
+    let i=0,s=this.replace(/{/g,'&').replace(/}/g,'@')
+    while(a.length)s=s.replace(new RegExp(`&${i++}@`,'g'),a.shift())
+    return s.replace(/&/g,'{').replace(/@/g,'}')
+}
 
-console.log(formatDataValue(1234567))
-console.log(formatDataValue(1000000))
+console.log('{0} + {0} = {1}'.formatWith('1','2'))
+console.log('Testing {1}'.formatWith('kata'))
