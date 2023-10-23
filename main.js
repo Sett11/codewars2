ff86a3846fc007f170495d066504d5236d959173
Jhonny <gonzodzen@mail.ru>
2023-10-23 22:39:52 +0300

    Trim method

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-evenCheck=n=>!(n&1)
+String.prototype.trim = function(c=' '){
+    return this.replace(new RegExp(`^${c}+|${c}+$`,'gi'),'')
+}
+
+console.log("LLLHello Worldlll".trim("l"))
+console.log("   Hello World                 ".trim())
