a48a508f26fe9c452b1f298c9d746d79ec097210
Jhonny <gonzodzen@mail.ru>
2023-10-23 21:55:48 +0300

    One Line: Even Check

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-function bulbMaze(s){
-  for(let i=-1;++i<s.length;){
-    if(s[i+1]==='x')return false
-    s=s.replace(/./g,e=>e=='o'?'x':e=='x'?'o':e)
-  }
-  return true
-}
-
-console.log(bulbMaze("xo oxox"))
+evenCheck=n=>!(n&1)
