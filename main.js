0b399d49c8ed82f791da3ae536b56761ed70fda7
Jhonny <gonzodzen@mail.ru>
2023-10-23 18:33:09 +0300

    Decipher the Message

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function reverseMessage(s) {
-    return s.split` `.map(e=>[...e.toLowerCase()].reverse().join``.replace(/./,u=>u.toUpperCase())).reverse().join` `
+function decipher(s){
+    const a='abcdefghijklmnopqrstuvwxyz'
+    return s.split` `.map(e=>e.replace(/./g,u=>{
+        let v=u.toLowerCase()
+        let t=a.includes(v)?a[(a.indexOf(v)+7)%a.length]:v
+        return u===u.toUpperCase()?t.toUpperCase():t.toLowerCase()
+    })).join` `
 }
 
-console.log(reverseMessage('This is an example of a Reversed Message!'))
+console.log(decipher('lvahhe bl lh uhkbgz b vtg\'m uxebxox maxkx tkx lh ftgr ahnkl exym'))
+console.log(decipher('HFZ FTR RHNK WKXLL BL LH VNMX'))
