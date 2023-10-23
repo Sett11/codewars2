8cb1d62feca0c035132aa97e5977d71b1ef70c4f
Jhonny <gonzodzen@mail.ru>
2023-10-23 23:02:40 +0300

    Padding

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-String.prototype.trim = function(c=' '){
-    return this.replace(new RegExp(`^${c}+|${c}+$`,'gi'),'')
-}
-
-console.log("LLLHello Worldlll".trim("l"))
-console.log("   Hello World                 ".trim())
+p=_=>[_,_,_].join``
