9d7d0d2e69bfea8830e13522dd8332ac82a3fbdc
Jhonny <gonzodzen@mail.ru>
2023-10-23 10:59:34 +0300

    Format data value

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-getElement=f=(a,i)=>!i.length?a:f(a[i.shift()],i)
+function formatDataValue(n) {
+    let a=b=c=0
+    while(n>=1000000){
+          a+=1
+          n-=1000000
+      }
+    while(n>=1000){
+          b+=1
+          n-=1000
+      }
+    if(n>=1)c=n
+    return `${a?a+'tb':''} ${b?b+'gb':''} ${c?c+'mb':''}`.toUpperCase().trim()||'0MB'
+  }
 
-console.log(getElement([[1, 2], [3, 4], [5, 6]], [0,0]))
-console.log(getElement([[[ 1, 2, 3]]], [ 0, 0, 1 ]))
+console.log(formatDataValue(1234567))
+console.log(formatDataValue(1000000))
