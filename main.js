3c9f3b980bdd289d536f975c7eaa0f02ef25f9b1
Jhonny <gonzodzen@mail.ru>
2023-10-23 21:52:58 +0300

    Simple Fun #347: Bulb Maze I

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-function ensureEven(n) {
-    if(!(n&1)&&Number.isInteger(n))return n
-    if(!Math.floor(n)||!Math.ceil(n))return 0
-    let a=Array(100).fill(1).map((e,i)=>[e+i,-(e+i)]).find(e=>e[0]%2==0&&(e[0]>n||e[1]>n)).filter(e=>e>n||e<n)
-    return n>0?a.filter(e=>e>0)[0]:a.filter(e=>e<0)[0]
+function bulbMaze(s){
+  for(let i=-1;++i<s.length;){
+    if(s[i+1]==='x')return false
+    s=s.replace(/./g,e=>e=='o'?'x':e=='x'?'o':e)
+  }
+  return true
 }
 
-console.log(ensureEven(1.346))
-console.log(ensureEven(-1))
-console.log(ensureEven(.5))
-console.log(ensureEven(18264952))
+console.log(bulbMaze("xo oxox"))
