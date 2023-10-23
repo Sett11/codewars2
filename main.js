22de05d36a01911e77a30f7722e22e8d387efc91
Jhonny <gonzodzen@mail.ru>
2023-10-23 18:08:54 +0300

    Reversed Message

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function calorie(a){
-    return `${a[0]}´s daily calorie requirement is ${(Math.round((a[4]*10+6.25*a[3]-5*a[2]+(a[1]==='m'?5:-161))*{"little activity":1.2,"moderately active":1.55,"very active":1.7,"extremely active":1.9}[a[5]]*100)/100).toFixed(2)} kcal.`
+function reverseMessage(s) {
+    return s.split` `.map(e=>[...e.toLowerCase()].reverse().join``.replace(/./,u=>u.toUpperCase())).reverse().join` `
 }
 
-console.log(calorie(["Sandra", "f", 17, 165, 61, "extremely active"]))
+console.log(reverseMessage('This is an example of a Reversed Message!'))
