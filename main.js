fd8f6b59852ce13e54d244b4f2bae9cac3f6a3f3
Jhonny <gonzodzen@mail.ru>
2023-10-24 22:12:26 +0300

    Playing with Sets : Equal or Not ?

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function inter(s1,s2){
-  [a,b]=[[...s1],[...s2]]
-  return new Set(a.filter(e=>b.includes(e)).concat(b.filter(e=>a.includes(e))))
+function areEqual(s1, s2){
+  return [...s1].every(e=>s2.has(e))&&[...s2].every(e=>s1.has(e))
 }
 
-console.log(inter(new Set([1,2]),new Set([1,2,3])))
+function notEqual(s1, s2){
+  return !areEqual(s1,s2)
+}
+
+
+console.log(areEqual(new Set(['1','2']),new Set([1,2])))
