1ac1919889882c7b32cf888a648857ff64d6fe90
Jhonny <gonzodzen@mail.ru>
2023-10-24 14:37:22 +0300

    The average length

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-swap = ary => {
-    [ary[0],ary[1]]=[ary[1],ary[0]]
-  }
+function averageLength(a) { 
+  let n=Math.round(a.map(e=>e.length).reduce((e,c)=>e+c,0)/a.length)
+  return a.map(e=>e.repeat(n).slice(0,n))
+}
+
+console.log(averageLength(['aa', 'bb', 'ddd', 'eee']))
