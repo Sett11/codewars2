1bcf6bf353393726e9bdc0bd903112760aedb94d
Jhonny <gonzodzen@mail.ru>
2023-10-25 00:34:03 +0300

    Simple Fun #225: Closest Neighbor

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function flipper(a){
-    return a.map(e=>{
-        if(e.length>1)return e.slice(0,e.length-1).toLowerCase()+e.charAt(e.length-1).toUpperCase()
-        return e
-    }).reverse().join` `
-  }
+function closestNeighbor(n,a,b,c) {
+    let i=n-1,j=n+1,r=[],z=[],q=[a,b,c],v=w=1
+    while(v||w){
+        if(q.every(e=>i%e===0)&&!r.includes(i))r.push(i),v=0
+        if(q.every(e=>j%e===0)&&!z.includes(j))z.push(j),w=0
+        i--,j++
+    }
+    return r.concat(z)
+}
+
+console.log(closestNeighbor(20,2,3,4))
