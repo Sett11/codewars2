fb187d548534800bd232c04b810b411ac2abf44f
Jhonny <gonzodzen@mail.ru>
2023-10-24 21:44:27 +0300

    Playing with Sets : Union

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function winnerOfTrick(a,b){
-    const r=['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-    a=a.map((e,i)=>[r.indexOf(e[0]),b[i]]).sort((a,b)=>a[0]-b[0]).filter((e,_,v)=>e[0]===v[0][0])
-    return a.length>1?`Tie! (${a.map(e=>e[1]).join`,`})`:a[0][1]+' wins'
-}
-
-console.log(winnerOfTrick(["2D", "KH", "QH"],["mZoIs", "GgbA", "S'Lg"]))
-console.log(winnerOfTrick(["4D", "2H", "4S"],["GgbA", "mZoIs", "S'Lg"]))
+function union(s1,s2){
+    return new Set([...s1].concat([...s2]))
+  }
