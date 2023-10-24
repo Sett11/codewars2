cfc0fe623ff6de63a805d0aec6b0071e7969a155
Jhonny <gonzodzen@mail.ru>
2023-10-25 00:49:42 +0300

    Simple Fun #154: Zero And One

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-function closestNeighbor(n,a,b,c) {
-    let i=n-1,j=n+1,r=[],z=[],q=[a,b,c],v=w=1
-    while(v||w){
-        if(q.every(e=>i%e===0)&&!r.includes(i))r.push(i),v=0
-        if(q.every(e=>j%e===0)&&!z.includes(j))z.push(j),w=0
-        i--,j++
-    }
-    return r.concat(z)
+function zeroAndOne(s) {
+    while(s.match(/01|10/))s=s.replace(/01|10/,'  ')
+    return s.replace(/\s/g,'').length
 }
 
-console.log(closestNeighbor(20,2,3,4))
+console.log(zeroAndOne('110100'))
