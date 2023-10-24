c39106eeb88667ab18a2d5b0c59cffa41e8821d3
Jhonny <gonzodzen@mail.ru>
2023-10-24 14:43:05 +0300

    That unites us

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-function averageLength(a) { 
-  let n=Math.round(a.map(e=>e.length).reduce((e,c)=>e+c,0)/a.length)
-  return a.map(e=>e.repeat(n).slice(0,n))
+function thatUnitesUs(a,b,n) {
+    return [...new Set(a.concat(b))].sort().slice(0,n)
 }
 
-console.log(averageLength(['aa', 'bb', 'ddd', 'eee']))
+console.log(thatUnitesUs(['a', 'z', 'c'], ['b', 'f', 'z'], 3))
