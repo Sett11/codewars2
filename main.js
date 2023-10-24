0893fc17a50aedf3c869d87576aac9ac5a5bcf88
Jhonny <gonzodzen@mail.ru>
2023-10-24 07:39:06 +0300

    Bitty Words

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-arrMultiply=a=>(+a[0]*+a[1])+''
+function bittyWords(w,b){
+    const a=w.split` `
+    const n=[...(b).toString(2).padStart(a.length, '0')]
+    return a.filter((_,i)=>+n[i]).join` `
+}
+
+console.log(bittyWords('So long, and thanks for all the fish', 42))
