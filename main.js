a14bd65bf0c6c116bbe52967eb0502eb1b72ee07
Jhonny <gonzodzen@mail.ru>
2023-10-24 09:03:39 +0300

    Swap two numbers

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-function swapTwo(arr, a, b) {
-    const r=[],q=[],w=arr.slice()
-    arr.forEach((e,i)=>e===a?r.push(i):e===b?q.push(i):null)
-    w[r[0]]=b,w[q[q.length-1]]=a
-    return w
+swap = ary => {
+    [ary[0],ary[1]]=[ary[1],ary[0]]
   }
-
-console.log(swapTwo([1,2,3,4],2,4))
-console.log(swapTwo([1,2,3,4].reverse(),2,4))
