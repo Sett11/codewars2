c9dac766bc23b0b86c0de76dacc9cf12d44fc04e
Jhonny <gonzodzen@mail.ru>
2023-10-24 20:24:39 +0300

    Get Planet Name By ID

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-function toInitials(s){
-    return s.replace(/\b(.)/g,'$&.').match(/([A-Z])\./g).join` `
-}
+function getPlanetName(id){
+    let name=''
+    switch(id){
+      case 1:
+        return 'Mercury'
+      case 2:
+        return 'Venus'
+      case 3:
+        return 'Earth'
+      case 4:
+        return 'Mars'
+      case 5:
+        return 'Jupiter'
+      case 6:
+        return 'Saturn'
+      case 7:
+        return 'Uranus'
+      case 8:
+        return 'Neptune'
+    }
+  }
 
-console.log(toInitials("Tom Araya"))
+  console.log(getPlanetName(2))
