1c66ddddf2024e9df99f2238d8625882cf4ef142
Jhonny <gonzodzen@mail.ru>
2023-10-24 21:54:29 +0300

    Playing with Sets : Intersection

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function union(s1,s2){
-    return new Set([...s1].concat([...s2]))
-  }
+function inter(s1,s2){
+  [a,b]=[[...s1],[...s2]]
+  return new Set(a.filter(e=>b.includes(e)).concat(b.filter(e=>a.includes(e))))
+}
+
+console.log(inter(new Set([1,2]),new Set([1,2,3])))
