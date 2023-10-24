8e1d40ab4baa3a6ae6f493d0052e1d8beb09f4fa
Jhonny <gonzodzen@mail.ru>
2023-10-24 22:19:18 +0300

    Playing with Sets : Symmetric difference

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function diff(s1, s2){
-    return new Set([...s1].filter(e=>!s2.has(e)))
+function symDiff(s1, s2){
+    return new Set([...s1].filter(e=>!s2.has(e)).concat([...s2].filter(e=>!s1.has(e))))
 }
 
-console.log(diff(new Set([1,2]),new Set([2,3])))
+console.log(symDiff(new Set([1,2]),new Set([2,3])))
