ed5ce3098ee47c9ca7f16c7918ecb3227b15c11b
Jhonny <gonzodzen@mail.ru>
2023-12-08 15:28:12 +0300

    Prime Streaming (PG-13)

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-lenR=x=(a,c=0)=>typeof a!='object'?5:!a.length?c:a.pop()&&x(a,c+1)
+const e=n=>{
+    const r=Array(n+1).fill(true)
+    r[0]=r[1]=false
+    for(let i=1;++i<Math.floor(n**.5+1);){
+      if(r[i]){
+        for(let j=i*2-i;(j+=i)<n;)r[j]=false
+      }
+    }
+    const res=[]
+     r.forEach((u,i)=>u?res.push(i):null)
+     return res
+  }
+  
+  const a=e(15486042)
+  
+  class Primes {
+    static *stream() {
+      for(let i=0;i<a.length;i++){
+        yield a[i]
+      }
+    }
+  }
