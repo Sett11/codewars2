25e61de0c6fac34c915f092b26e89f2db99d9772
Jhonny <gonzodzen@mail.ru>
2023-12-08 18:05:42 +0300

    Find primes in range (complexity matters)

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 class bitVector{
-    constructor(size){
-        this.store = Array(~~(size/31)+1).fill(0)
-        for (var i=3; i<~~Math.sqrt(size); i+=2) if (!this.isSet(i))
-        for (var j=i*i, k=i<<1; j<size; j+=k) this.setBit(j)
+  constructor(size){
+      this.store = Array(~~(size/31)+1).fill(0)
+      for (var i=3; i<~~Math.sqrt(size); i+=2) if (!this.isSet(i))
+      for (var j=i*i, k=i<<1; j<size; j+=k) this.setBit(j)
     }
-    setBit=function(bit){this.store[~~(bit/31)]|=1<<bit%31}
-    isSet=function(bit){return (this.store[~~(bit/31)]&1<<bit%31)>0}
+  setBit(bit){this.store[~~(bit/31)]|=1<<bit%31}
+  isSet(bit){return (this.store[~~(bit/31)]&1<<bit%31)>0}
+}
+
+
+
+
+MAX_PRIME = 400000000
+sieve = new bitVector(MAX_PRIME)
+class Primes {
+  static *stream() {
+      for(let i=3;;i+=2)if(!sieve.isSet(i))yield i
   }
- 
-  
-  MAX_PRIME = 500000000
-  sieve = new bitVector(MAX_PRIME)
-  class Primes {
-    static *stream() {
-      yield 2
-        for(let i=3;;i+=2)if(!sieve.isSet(i))yield i
-    }
+}
+
+const primeList=n=>{
+  let a=2,r=[]
+  const p=Primes.stream()
+  while(a<n){
+    r.push(a)
+    a=p.next().value
   }
+  return r
+}
+
+
+  console.log(primeList(100))
+  console.log(primeList(1000))
