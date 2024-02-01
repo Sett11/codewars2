8d7277f43f566d4b8eed635718234adf355e8508
Jhonny <gonzodzen@mail.ru>
2024-02-02 00:00:33 +0300

    One Line Task: GCD Function

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-function scratch(a){
-  return a.map(e=>{
-    let [b,c,d,g]=[...e.split` `]
-    let t=[...new Set([b,c,d])]
-    return t.length==1||(t.length==2&&t.includes('###'))?+g===+g?+g:10000:0
-  }).reduce((a,c)=>a+c,0)
-}
+GCDMeansGreatestCommonDivisorOrHighestCommonFactor=x=(a,b)=>!b?a:x(b,a%b)
 
-console.log(scratch([
-"horse tiger snake ###",
-"ox ### ### ###",
-"### ### ox 10000",
-"### ### pig ###",
-"### rabbit ### 10000",
-"monkey snake snake 50"]))
+console.log(GCDMeansGreatestCommonDivisorOrHighestCommonFactor(15,18))
