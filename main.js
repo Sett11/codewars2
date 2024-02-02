3ff49055018177476af768791a1a15f752f0debb
Jhonny <gonzodzen@mail.ru>
2024-02-02 11:47:53 +0300

    Dan's great power generator

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-GCDMeansGreatestCommonDivisorOrHighestCommonFactor=x=(a,b)=>!b?a:x(b,a%b)
+function danspower(n,p){
+    let r=n**p,k=r%10
+    return r%2==0?r:k<5?r-k:r-k+10
+  }
 
-console.log(GCDMeansGreatestCommonDivisorOrHighestCommonFactor(15,18))
+  
+  console.log(danspower(7,2))
