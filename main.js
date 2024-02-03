89945719ffc96654589ca6ef0b0c18eb7fac894c
Jhonny <gonzodzen@mail.ru>
2024-02-04 00:30:33 +0300

    Sort rectangles and circles by area

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-function danspower(n,p){
-    let r=n**p,k=r%10
-    return r%2==0?r:k<5?r-k:r-k+10
-  }
+function sortByArea(a){
+  return a.map(e=>+e==+e?+(Math.PI*(e**2)).toFixed(2):+(e[0]*e[1]).toFixed(2)).sort((a,b)=>a-b)
+}
 
-  
-  console.log(danspower(7,2))
+console.log(sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]]))
