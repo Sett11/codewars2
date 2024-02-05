bff9bb34376efb76d97eff7961a7c9a19bdf6d8d
Jhonny <gonzodzen@mail.ru>
2024-02-06 00:01:19 +0300

    Push a hash/an object into array

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-function sortByArea(a){
-  return a.map(e=>+e==+e?+(Math.PI*(e**2)).toFixed(2):+(e[0]*e[1]).toFixed(2)).sort((a,b)=>a-b)
-}
-
-console.log(sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]]))
+items = []
+items.push({a: "b", c: "d"})
