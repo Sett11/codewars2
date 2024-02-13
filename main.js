fbe851f2452275123a365939ba0c8cca4088be40
Jhonny <gonzodzen@mail.ru>
2024-02-13 17:06:45 +0300

    Array of all primes up to number N

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-name = 'The Window';
-
-var alpha = {
-    name : 'My Alpha',
-    getNameFunc : function() {
-        return function() {
-            return this.name;
-        }.bind(alpha)
+const primeArray=n=>{
+  n+=1
+  let a=Array(n).fill(true),r=[]
+  a[0]=a[1]=false
+  for(let i=1;++i<n**.5+1;){
+    if(a[i]){
+      for(let j=2*i;j<n;j+=i){
+        a[j]=false
+      }
     }
-};
+  }
+  a.forEach((e,i)=>e?r.push(i):null)
+  return r
+}
+
+console.log(primeArray(2))
