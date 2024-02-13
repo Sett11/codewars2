9dda8dc07e472b83882026b3ceb98ee253e8858c
Jhonny <gonzodzen@mail.ru>
2024-02-13 17:24:42 +0300

    Check if a number is a part of an arbitrary fibonacci sequence

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-const primeArray=n=>{
-  n+=1
-  let a=Array(n).fill(true),r=[]
-  a[0]=a[1]=false
-  for(let i=1;++i<n**.5+1;){
-    if(a[i]){
-      for(let j=2*i;j<n;j+=i){
-        a[j]=false
-      }
-    }
-  }
-  a.forEach((e,i)=>e?r.push(i):null)
-  return r
+function isFibo(a,b,f){
+  while(a<f)[b,a]=[a+b,b]
+  return a==f
 }
 
-console.log(primeArray(2))
+console.log(isFibo(4,5,23))
