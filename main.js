65637c15e349876d8305291db643a4f62ddd1887
Jhonny <gonzodzen@mail.ru>
2024-02-14 00:15:16 +0300

    Fac Recursion (Pest control - find the bugs and fix them)

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const diffBig2=(a,f=a=>a.splice(a.indexOf(Math.max(...a)),1)[0])=>f(a)-f(a)
-
-console.log(diffBig2([8,3,1]))
+function facRecursion(n){
+  return n<0?0:n<=1?1:n*facRecursion(n-1)
+}
+  
