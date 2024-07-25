9de744459b9f8d593b4d743f90989d6a3cdfcd80
Jhonny <gonzodzen@mail.ru>
2024-07-26 00:11:49 +0300

    Implementing a Queue - Performance Version

--- main.js
+++ main.js
@@ -1,17 +1,59 @@
-function allLeavesAtSameLevel(h) {
-  let d={},i=0
-  function f(x){
-    if(!x)return
-    i++
-    if(!d[i])d[i]=[]
-    d[i].push(x.getLeft()||x.getRight()?'Not leaf':'Leaf')
-    f(x.getLeft())
-    f(x.getRight())
-    i--
+class Node{
+  constructor(val,next,previous){
+    this.val=val
+    this.next=next
+    this.previous=previous
   }
-  f(h)
-  for(let i in d)if(d[i].includes('Leaf')&&d[i].some(e=>e!=='Leaf'))return false
-  return true
 }
 
-console.log(allLeavesAtSameLevel())
+class Queue{
+  constructor(head,tail,lenght=0){
+    this.head=head
+    this.tail=tail
+    this.lenght=lenght
+  }
+
+  size(){
+    return this.lenght
+  }
+  enqueue(v){
+    if(!this.tail){
+      this.head=this.tail=new Node(v)
+      this.lenght++
+      return
+    }
+    else{
+      let h=new Node(v)
+      if(this.tail&&this.tail===this.head){
+        this.tail.previous=this.tail
+        this.head.next=this.tail=h
+    }
+      else{
+      h.previous=this.tail
+      this.tail.next=this.tail=h
+      }
+    }
+      this.lenght++
+  }
+  dequeue(){
+    if(this.head){
+      let v=this.head.val
+      if(this.head===this.tail){
+        this.head=this.tail=undefined
+      }
+      else{
+        this.head=this.head.next
+      }
+      this.lenght--
+      return v
+    }
+    }
+}
+
+Q=new Queue()
+
+Q.enqueue(1)
+// Q.enqueue(2)
+// Q.enqueue(3)
+
+console.log(Q.dequeue(),Q.size())
