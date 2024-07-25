cccfea87e25ee32c7c716e53c480d4ec974cfc58
Jhonny <gonzodzen@mail.ru>
2024-07-26 00:42:11 +0300

    Fun with trees: lists edition

--- main.js
+++ main.js
@@ -1,59 +1,49 @@
-class Node{
-  constructor(val,next,previous){
-    this.val=val
-    this.next=next
-    this.previous=previous
+function TreeNode(value, left, right) {
+  this.value = value;
+  this.left = left;
+  this.right = right;
+};
+
+function ListNode(data, next = null) {
+  this.data = data;
+  this.next = next;
+};
+
+function flatten(h){
+  if(h===null)return ListNode()
+  let r=new Set()
+  const f=x=>{
+    if(!x)return
+    let y=x.value
+    while(y)r.add(y.data),y=y.next
+    f(x.left)
+    f(x.right)
   }
+  f(h)
+  r=[...r].sort((a,b)=>a-b)
+  let head=c=new ListNode(),n=r.length
+  for(let i=-1;++i<n;){
+    c.data=r[i]
+    if(i!==n-1){
+      c.next=new ListNode()
+      c=c.next
+    }
+  }
+  return head
 }
 
-class Queue{
-  constructor(head,tail,lenght=0){
-    this.head=head
-    this.tail=tail
-    this.lenght=lenght
-  }
-
-  size(){
-    return this.lenght
-  }
-  enqueue(v){
-    if(!this.tail){
-      this.head=this.tail=new Node(v)
-      this.lenght++
-      return
-    }
-    else{
-      let h=new Node(v)
-      if(this.tail&&this.tail===this.head){
-        this.tail.previous=this.tail
-        this.head.next=this.tail=h
-    }
-      else{
-      h.previous=this.tail
-      this.tail.next=this.tail=h
-      }
-    }
-      this.lenght++
-  }
-  dequeue(){
-    if(this.head){
-      let v=this.head.val
-      if(this.head===this.tail){
-        this.head=this.tail=undefined
-      }
-      else{
-        this.head=this.head.next
-      }
-      this.lenght--
-      return v
-    }
-    }
+const f=h=>{
+  let r=[]
+  while(h)r.push(h.data),h=h.next
+  return r
 }
 
-Q=new Queue()
+let l1 = new ListNode(17, new ListNode(1));
+let l2 = new ListNode(3);
+let l3 = new ListNode(1);
+let l4 = new ListNode(2);
+let l5 = new ListNode(16);
+let l6 = new ListNode(7, new ListNode(3));
+let root = new TreeNode(l1, new TreeNode(l2, new TreeNode(l4)), new TreeNode(l3, new TreeNode(l5), new TreeNode(l6)));
 
-Q.enqueue(1)
-// Q.enqueue(2)
-// Q.enqueue(3)
-
-console.log(Q.dequeue(),Q.size())
+console.log(f(flatten(null)))
