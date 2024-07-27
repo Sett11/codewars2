10d2591d92ac4efee547cd373f06c60075b51698
Jhonny <gonzodzen@mail.ru>
2024-07-28 01:18:05 +0300

    Balance the tree

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
-function findCenterPoints(m){
-  let a=m.split`\n`.map(e=>e.split` `),r=[],c=Infinity
-  a.forEach((e,i)=>e.forEach((u,j)=>u!=='.'?r.push([i,j]):0))
-  let n=r.length
-  for (let i=-1;++i<n;){
-    let s=0
-    for(let j=-1;++j<n;){
-      s+=(Math.abs(r[i][0]-r[j][0])**2+Math.abs(r[i][1]-r[j][1])**2)**.5
-    }
-    s=s.toFixed(6)
-    c=Math.min(c,s)
-    r[i].push(s)
-  }
-  return r.filter(e=>e[2]==c).map(e=>a[e[0]][e[1]]).sort()
+function Node(value, left, right){
+  this.value = value,
+  this.left = left;
+  this.right = right;
 }
 
-console.log(findCenterPoints(`. . . . . . . . . . .
-. I . . . . . . . . .
-. . . . . . . . . . .
-. . . . . . . . . . .
-. . . . . . . . . . .
-. . E . . . . . . . .
-. . . . . . . . . . .
-. . . G F . . . . . .
-. . . . A . . . . . .
-. . . . . D . . . . .
-H . . . . . . . C . B
-. . . . . . . . . . .
-. . . . . . . . . . .`))
-console.log(findCenterPoints(`A . . . . B
-. . . . . .
-. . . F . .
-. . E . . .
-. . . . . .
-D . . . . C`))
+function isBalanced(h){
+  let d={},r=[],i=0
+  const f=x=>{
+    if(!x)return
+    i++
+    if(!d[i])d[i]=[]
+    d[i].push(x.left||x.right?'not leaf':'leaf')
+    f(x.left)
+    f(x.right)
+    i--
+  }
+  f(h)
+  for(let i in d)if(d[i].includes('leaf'))r.push(i)
+  return Math.max(...r)-Math.min(...r)<2
+}
+
+console.log(isBalanced({"value":"a","left":{"value":"b","left":{"value":"e","left":{"value":"g","left":{"value":"h"}}},"right":null},"right":{"value":"c","left":{"value":"f"},"right":null}}))
