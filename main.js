12a4c0f18983f7c209f357b5b5ccec8b154e6f55
Jhonny <gonzodzen@mail.ru>
2024-07-28 00:01:22 +0300

    Find the "center" point

--- main.js
+++ main.js
@@ -1,49 +1,35 @@
-function TreeNode(value, left, right) {
-  this.value = value;
-  this.left = left;
-  this.right = right;
-};
-
-function ListNode(data, next = null) {
-  this.data = data;
-  this.next = next;
-};
-
-function flatten(h){
-  if(h===null)return ListNode()
-  let r=new Set()
-  const f=x=>{
-    if(!x)return
-    let y=x.value
-    while(y)r.add(y.data),y=y.next
-    f(x.left)
-    f(x.right)
-  }
-  f(h)
-  r=[...r].sort((a,b)=>a-b)
-  let head=c=new ListNode(),n=r.length
-  for(let i=-1;++i<n;){
-    c.data=r[i]
-    if(i!==n-1){
-      c.next=new ListNode()
-      c=c.next
+function findCenterPoints(m){
+  let a=m.split`\n`.map(e=>e.split` `),r=[],c=Infinity
+  a.forEach((e,i)=>e.forEach((u,j)=>u!=='.'?r.push([i,j]):0))
+  let n=r.length
+  for (let i=-1;++i<n;){
+    let s=0
+    for(let j=-1;++j<n;){
+      s+=(Math.abs(r[i][0]-r[j][0])**2+Math.abs(r[i][1]-r[j][1])**2)**.5
     }
+    s=s.toFixed(6)
+    c=Math.min(c,s)
+    r[i].push(s)
   }
-  return head
+  return r.filter(e=>e[2]==c).map(e=>a[e[0]][e[1]]).sort()
 }
 
-const f=h=>{
-  let r=[]
-  while(h)r.push(h.data),h=h.next
-  return r
-}
-
-let l1 = new ListNode(17, new ListNode(1));
-let l2 = new ListNode(3);
-let l3 = new ListNode(1);
-let l4 = new ListNode(2);
-let l5 = new ListNode(16);
-let l6 = new ListNode(7, new ListNode(3));
-let root = new TreeNode(l1, new TreeNode(l2, new TreeNode(l4)), new TreeNode(l3, new TreeNode(l5), new TreeNode(l6)));
-
-console.log(f(flatten(null)))
+console.log(findCenterPoints(`. . . . . . . . . . .
+. I . . . . . . . . .
+. . . . . . . . . . .
+. . . . . . . . . . .
+. . . . . . . . . . .
+. . E . . . . . . . .
+. . . . . . . . . . .
+. . . G F . . . . . .
+. . . . A . . . . . .
+. . . . . D . . . . .
+H . . . . . . . C . B
+. . . . . . . . . . .
+. . . . . . . . . . .`))
+console.log(findCenterPoints(`A . . . . B
+. . . . . .
+. . . F . .
+. . E . . .
+. . . . . .
+D . . . . C`))
