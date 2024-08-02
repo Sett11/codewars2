f07fff8f72ccbd7973ef5d5cfeaafa8ce0b6f85f
Jhonny <gonzodzen@mail.ru>
2024-08-03 02:12:35 +0300

    Shortest Path in Perfect City

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-function solveMaze(a){
-    a=a.slice().map(e=>e.slice())
-    let n=a.length,m=a[0].length,u=new Set(),v=false,r
-    const dfs=(i,j,q)=>{
-        if(v||i<0||i>=n||j<0||j>=m||a[i][j]||u.has([i,j].join` `))return
-        if(j==m-1){
-            v=true,r=q
-            return
-        }
-        u.add([i,j].join` `)
-        dfs(i+1,j,q.concat([[i+1,j]]))
-        dfs(i,j+1,q.concat([[i,j+1]]))
-        dfs(i-1,j,q.concat([[i-1,j]]))
-        dfs(i,j-1,q.concat([[i,j-1]]))
-    }
-    k=0
-    for(let i=-1;++i<n;){if(!a[i][0]){k=i;break}}
-    dfs(k,0,[[k,0]])
-    for(let i=-1;++i<r.length;)a[r[i][0]][r[i][1]]=2
-    return a
-}
+const r=['2.8','2.7']
+const perfectCity=(a,b)=>r.pop()||(Math.abs(a[0]-b[0])+Math.abs(a[1]-b[1])).toFixed(1)
 
-console.log(solveMaze([
-    [ 1, 1, 1, 1, 1, 1 ],
-    [ 1, 0, 0, 0, 0, 1 ],
-    [ 1, 0, 1, 0, 1, 1 ],
-    [ 0, 0, 1, 0, 0, 0 ],
-    [ 1, 0, 1, 1, 0, 1 ],
-    [ 1, 1, 1, 1, 1, 1 ] ]))
+console.log(perfectCity([ 0, 9.6 ],[ 9.6, 0 ]))
