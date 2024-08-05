ebd523c11b4f407453db642f94f10079b927a53d
Jhonny <gonzodzen@mail.ru>
2024-08-06 00:04:15 +0300

    Valid Binary Christmas Tree

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
-const r=['2.8','2.7']
-const perfectCity=(a,b)=>r.pop()||(Math.abs(a[0]-b[0])+Math.abs(a[1]-b[1])).toFixed(1)
+function isValidTree(h,v=true){
+    if(h.ornament!=='star')return false
+    const f=x=>{
+        if(!v||!x)return
+        if(x&&!x.left&&!x.right){
+            if(x.color!=='blue'){
+                v=false
+                return
+            }
+        }
+        if(x&&(x.left||x.right)){
+            if(x.color!=='red'){
+            v=false
+            return
+            }
+        }
+        f(x.left)
+        f(x.right)
+    }
+    f(h.left)
+    f(h.right)
+    return v
+}
 
-console.log(perfectCity([ 0, 9.6 ],[ 9.6, 0 ]))
+console.log(isValidTree({
+    ornament: 'star',
+    color: 'yellow',
+    left: {
+      ornament: 'candy cane',
+      color: 'blue'
+    },
+    right: {
+      ornament: 'stocking',
+      color: 'blue'
+    }
+  }))
+
+console.log(isValidTree({
+    ornament: 'star',
+    color: 'yellow',
+    right: {
+      ornament: 'stocking',
+      color: 'red'
+    }
+  }))
