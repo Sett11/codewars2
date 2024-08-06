5e2191f9028d50fd752b40232a6fb145bbfde7e9
Jhonny <gonzodzen@mail.ru>
2024-08-07 00:09:59 +0300

    Tree Depth

--- main.js
+++ main.js
@@ -1,45 +1,8 @@
-function isValidTree(h,v=true){
-    if(h.ornament!=='star')return false
-    const f=x=>{
-        if(!v||!x)return
-        if(x&&!x.left&&!x.right){
-            if(x.color!=='blue'){
-                v=false
-                return
-            }
-        }
-        if(x&&(x.left||x.right)){
-            if(x.color!=='red'){
-            v=false
-            return
-            }
-        }
-        f(x.left)
-        f(x.right)
-    }
-    f(h.left)
-    f(h.right)
-    return v
+function recordDepth(h,d=0){
+  if(!h||typeof h!=='object'||Array.isArray(h))return null
+  h.depth=d
+  for(let i in h)if(typeof h[i]=='object')recordDepth(h[i],d+1)
+  return h
 }
 
-console.log(isValidTree({
-    ornament: 'star',
-    color: 'yellow',
-    left: {
-      ornament: 'candy cane',
-      color: 'blue'
-    },
-    right: {
-      ornament: 'stocking',
-      color: 'blue'
-    }
-  }))
-
-console.log(isValidTree({
-    ornament: 'star',
-    color: 'yellow',
-    right: {
-      ornament: 'stocking',
-      color: 'red'
-    }
-  }))
+console.log(recordDepth({ a: 1, b: 2, c: { d: { e: 3 } } }))
