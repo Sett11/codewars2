5677eb503627bedafc3e393c118eca4f71ff47a4
Jhonny <gonzodzen@mail.ru>
2024-08-12 00:01:44 +0300

    Who let the dogs out?

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function squeakyClean(arr) {
-  return arr.filter(e=>e)
+var BARK ='woof woof';
+var SLEEP='zzzzzzzzz....';
+
+function dog_bark_by_default(b=true){
+  return b?BARK:SLEEP
+}
+
+function dog_bark_only_if_told_so(b=false){
+  return b?BARK:SLEEP
+}
+
+function dog_dont_bark_by_default(b=true){
+  return b?SLEEP:BARK
+}
+
+function dog_dont_bark_only_if_told_so(b=false){
+  return b?SLEEP:BARK
 }
