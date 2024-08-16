80707cc68ca892e3d3b8fd2a2734ce4a08ffb33f
Jhonny <gonzodzen@mail.ru>
2024-08-17 02:49:33 +0300

    Deep and universal search by object

--- main.js
+++ main.js
@@ -1,42 +1,35 @@
-function Relational(value) {
-  this.value=value
-  this.v=[this.value]
-}
-Relational.prototype = {
-  passed: function(){
-    let s=this.v.join` `
-    console.log(s)
-    if(s==='10 !==5  !==5 ')return false
-    if(s==='10 > 5 > 2')return true
-    return eval(this.v.join` `)
-  },
-  
-  equals: function(x) {
-    this.v.push(`===${x} `)
-    return this
-  },
-  notEquals: function(x) {
-    this.v.push(`!==${x} `)
-    return this
-  },
-  
-  lessThan: function(x) {
-    this.v.push(`< ${x}`)
-    return this
-  },
-  lessThanOrEqualTo: function(x) {
-    this.v.push(`<= ${x}`)
-    return this
-  },
-  
-  greaterThan: function(x) {
-    this.v.push(`> ${x}`)
-    return this
-  },
-  greaterThanOrEqualTo: function(x) {
-    this.v.push(`>= ${x}`)
-    return this
+const findItems = (obj,...args) => {
+  const res={}
+  const f=(x,s)=>{
+    if(!x&&args.includes(x))res[s]=x
+    if(typeof x=='object')for(let i in x)f(x[i],s+(Array.isArray(x)?`[${i}]`:'.'+i))
+    else for(let i=-1;++i<args.length;)if((typeof args[i]=='function'&&args[i](x))||x===args[i])res[s]=x
   }
+  f(obj,'tree')
+  return res
 }
 
-console.log(new Relational(10).greaterThan(5).greaterThan(2).passed())
+const tree = {
+  a: 4,
+  b: {
+    c: 5,
+    f: 1,
+    p: "ww",
+    falseValue: false,
+    undefinedValue: undefined,
+    t: {
+      y: 90,
+      i: {
+        s: "qwe",
+        a: 90,
+        aString: "90",
+        nullValue: null,
+      },
+      p: "wwww",
+      emptyStringValue: "",
+      asd: [123, 34, 90],
+    },
+  },
+};
+
+console.log(findItems(tree,Boolean))
