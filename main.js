6a0d4a69819c83c0780d489c3d0d91e4f6fa7dcf
Jhonny <gonzodzen@mail.ru>
2024-08-17 11:43:04 +0300

    Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-function solution(d,r){
-  if(typeof d!=='object')return d
-  const f=(x,y)=>{
-    if(typeof x[y]==='object'){
-    for(let i in x[y]){
-      if(typeof x[y][i]==='object')f(x[y],i)
-      if(i==='dynamic'){
-        x=x[y][i]
-        delete x[y][i]
-        x[y][r]=x
-      }
-      if(x[y][i]==='dynamic')x[y][i]=r
-    }
-  }
-    if(x[y]==='dynamic'){
-      x[y]=r
-    }
-    else return
-  }
-  for(let i in d)f(d,i)
-  return d
+function firstToLast(s,c){
+  let i=s.indexOf(c)
+  return i==-1?-1:s.lastIndexOf(c)-i
 }
-
-console.log(solution({
-  foo: "dynamic",
-  bar: {
-    baz: ["dynamic"]
-  },
-  dynamic: true
-},'static'))
