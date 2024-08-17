255c1cdf9ae182eec2dee163c9de90a6acc36eea
Jhonny <gonzodzen@mail.ru>
2024-08-18 02:16:41 +0300

    Anything to integer

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function firstToLast(s,c){
-  let i=s.indexOf(c)
-  return i==-1?-1:s.lastIndexOf(c)-i
+function toInteger(n){
+  if(Array.isArray(n))return n.reduce((a,c)=>a+c,0)||0
+  if(typeof n==='boolean')return +n
+  if((typeof n!=='number'&&typeof n!=='string')||n===Infinity||n===-Infinity||Number.isNaN(n))return 0
+  let s=String(+n).split`.`
+  return +s[0]
 }
+
+console.log(toInteger([]))
+console.log(toInteger(80085e-1))
