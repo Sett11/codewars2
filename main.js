de58b3cbcd0c48adb685890141844970f55aa7a0
Jhonny <gonzodzen@mail.ru>
2024-08-25 03:18:47 +0300

    Binary Search Tree using Arrays

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-generateMarkdowns=(m,t,u)=>m=='link'?`[${t}](${u})`:m=='img'?`![${t}](${u})`:'```'+`${u}\n`+t+'\n```'
+function toBSTArray(arr){
+    let a=arr.slice()
+	if (a.length===0)return []
+    for(let i=-1;++i<a.length;){
+        if((a[2*i+1]!==undefined&&a[i]<a[2*i+1])){
+            t=a[2*i+1]
+            a[i*2+1]=a[i]
+            a[i]=t
+        }
+        if((a[2*i+2]!==undefined&&a[i]>a[2*i+2])){
+            t=a[i*2+2]
+            a[i*2+2]=a[i]
+            a[i]=t
+        }
+    }
+    return isBSTArray(a)?a:toBSTArray(a)
+}
 
-console.log(generateMarkdowns('link','hyperlink','https://en.wikipedia.org/wiki/Hyperlink'))
-console.log(generateMarkdowns('img','this should be an image','https://github.com/codewars/gna.jpg'))
-console.log(generateMarkdowns('code',"function generateMarkdowns(markdown,text,urlOrLanguage) {\n// write your code here\n};",'javascript'))
+function isBSTArray(a){
+	if (a.length===0)return true
+    for(let i=-1;++i<a.length;){
+        if(!((a[2*i+1]===undefined||a[i]>=a[2*i+1])&&(a[2*i+2]===undefined||a[i]<=a[2*i+2])))return false
+    }
+	return true
+}
+
+function toArray(a){
+	if (!isBSTArray(a))throw new Error('The parameter must be a BSTArray')
+	return a.filter(e=>e).sort((a,b)=>a-b)
+}
+
+let array=[2, 4, 3, 5, 6]
+let bst = toBSTArray(array)
+console.log(isBSTArray(array))
