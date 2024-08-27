f09aa166bd54636926728810b96897a276aee1b7
Jhonny <gonzodzen@mail.ru>
2024-08-28 00:25:17 +0300

    How fast can the burglar steal all the diamonds?

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-function addProperty(obj, prop, value) {
-    if(prop in obj)throw Error()
-    obj[prop]=value
-  }
+diamondBurglar=a=>a.map(e=>e.replace(/(.)\1*/g,u=>u[0]==='.'?'':' '+Math.ceil(u.length/2)+' ').split` `).flat().reduce((a,c)=>a+ +c,0)
+
+console.log(diamondBurglar(['.*...*...*', '..***..*..', '...**.*...', '.***.**..*']))
