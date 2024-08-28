ab12f99e2978a2c77ae20d4d36f8cc1077f61ea8
Jhonny <gonzodzen@mail.ru>
2024-08-29 00:43:41 +0300

    Simplify Unique

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-Number.prototype.times = function (f) {
-  let i=0
-  while(i<this)f(i++)
+let allowed="0132456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ -",cache={}
+function simunq(s){
+  s=s.replace(/./g,e=>allowed.includes(e)?e:'-')
+  if(s in cache){
+    cache[s]+=1
+    let res=`${s}-${cache[s]}`
+    cache[res]=1
+    return res
+  }
+  cache[s]=1
+  return s
 }
+simunq.reset = function() {
+  cache={}
+}
+
+console.log(simunq('blah'))
+console.log(simunq('blah'))
+console.log(simunq('blah-2'))
