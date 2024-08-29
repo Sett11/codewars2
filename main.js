6a68c220e7b97c70077844f670b5fb5e6e179ff2
Jhonny <gonzodzen@mail.ru>
2024-08-30 01:00:34 +0300

    Max Headroom and JavaScript style

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-let allowed="0132456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ -",cache={}
-function simunq(s){
-  s=s.replace(/./g,e=>allowed.includes(e)?e:'-')
-  if(s in cache){
-    cache[s]+=1
-    let res=`${s}-${cache[s]}`
-    cache[res]=1
-    return res
+function getMax1(){
+  return {
+   name: 'Max Headroom'
   }
-  cache[s]=1
-  return s
-}
-simunq.reset = function() {
-  cache={}
 }
 
-console.log(simunq('blah'))
-console.log(simunq('blah'))
-console.log(simunq('blah-2'))
+function getMax2()
+{
+  return {
+    name: 'Max Headroom'
+  }
+}
