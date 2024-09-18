b32669f75b04cbda7fff2074c3ef68bdaa1f3360
Jhonny <gonzodzen@mail.ru>
2024-09-18 14:05:30 +0300

    Javascript from the Inside #2: Filter

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-function memo(fn){
-  const mem=new Map()
-  return arg=>{
-    if(!mem.has(arg))mem.set(arg,fn(arg))
-    return mem.get(arg)
+Array.prototype.filter=function(f,t){
+  if(t)f=f.bind(t)
+  const a=[],q=this,n=q.length
+  for(let i=-1;++i<n;){
+    if(i in q){
+      if(f(q[i],i,q))a.push(q[i])
+    }
   }
+  return a
 }
+
+console.log([1, 2, 3, 4, 5].filter(function(x) {return (x + this) % 2 == 0; }))
