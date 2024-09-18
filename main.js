b65b1a97c83de2499420ad76734236afbb98b59a
Jhonny <gonzodzen@mail.ru>
2024-09-18 14:43:51 +0300

    SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-Array.prototype.map=function(f,t){
-  if(t)f=f.bind(t)
-  const out=[],p=this,n=p.length
-  for(let i=-1;++i<n;)if(i in p)out.push(f(p[i],i,p))
-  if(!out.length&&n)return Array(n)
-  if(out.length<n)return Array(n-1).concat(out)
-  return out
+class Labrador {
+  constructor(n, a, g, m) {
+    this.name = n;
+    this.age = a;
+    this.gender = g;
+    this.species = "Labrador";
+    this.legs = 4;
+    this.size = "Large";
+    this.master = m;
+    this.loyal = true;
+  }
 }
-
-console.log(new Array(2).concat([null]).map(function(x) { return 1; }))
