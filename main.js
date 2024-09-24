2cf081591ac01c15c86f6b77b331106ad87ed282
Jhonny <gonzodzen@mail.ru>
2024-09-25 00:15:59 +0300

    Hard Time Bomb

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function partialKeys(obj){
-   return new Proxy(obj,{get:function(o,c){
-      let r=Object.keys(o).filter(e=>e.startsWith(c)).sort()
-      if(r.length)return o[r[0]]
-   }})
-}
-
-let o=partialKeys({'abcd':1})
-console.log(o['ab'])
+for(let i in this){
+   if (i.match(/boom\d/)){
+     Bomb.CutTheWire(this[i])
+   }
+ }
