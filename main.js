666a645c35bcd1ab325df21d142ecaaccc75733f
Jhonny <gonzodzen@mail.ru>
2024-09-25 02:38:39 +0300

    Move-To-Front Encoding

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+function encode(alf,seq){
+   let a=[...alf],r=[]
+   for(let i=-1;++i<seq.length;){
+      let g=a.indexOf(seq[i])
+      if(g)a.unshift(a.splice(g,1)[0])
+      r.push(g)
+   }
+   return r[0]!=-1?r:null
+}
 
+function decode(alf,seq){
+   let a=[...alf],r=seq.slice()
+   for(let i=-1;++i<seq.length;){
+      r[i]=a[seq[i]]
+      if(seq[i])a.unshift(a.splice(seq[i],1)[0])
+   }
+   return typeof alf==='string'?r.join``||null:r
+}
 
-// const a=[]
-// for(let i in this){
-//   a.push(i)
-// }
-// CutTheWire(this[a.pop()])
+console.log(encode("abc", "acacac"))
+console.log(decode("abc", [0,1,1,1,1,1]))
