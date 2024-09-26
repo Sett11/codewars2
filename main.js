d32f283b51515069c81fd9e2e5d39cbe2dda2f8c
Jhonny <gonzodzen@mail.ru>
2024-09-27 00:15:57 +0300

    Arbitrary word wrapping

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-Function.prototype.wrap=function(func){
-   let x=this
-   return function(a,b){
-      return func(x,a,b)
-   }
-}
+// 
 
-function speak(name){
-   return "Hello " + name;
-}
+// function wordWrap(s){
+//    let n=s.length,a=[]
+//    for(let i=0;i<n;){
+//       if(i+25<n&&s[i+25]!==' '&&s[i+24]!==' ')a.push(s.slice(i,i+=24)+'-')
+//       else a.push(s.slice(i,i+=25))
+//    }
+//    return a.join`\n`
+// }
 
-speak = speak.wrap(function(original, yourName, myName){
-   let greeting = original(yourName);
-   return greeting + ", my name is " + myName;
-})
-
-let greeting = speak("Mary", "Kate");
-
-console.log(greeting)
+// console.log(wordWrap('HNxu4xsbM Uk 5O4 j  Hi 6 4 Ol9V   wgBNdG61YoQZMOPrt BeFFbMrzKg3VB IjM MyKh wjhE 3'))
+// console.log(wordWrap('1234567890123456789012345 1234567890'))
+// console.log(wordWrap('The quick brown fox jumped over the lazy developer.'))
