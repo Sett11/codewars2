99669d529b262255eba0e3dc7175bcc8887b6ffe
Jhonny <gonzodzen@mail.ru>
2024-09-27 00:15:29 +0300

    Wrapped Function

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-function encode(alf,seq){
-   let a=[...alf],r=[]
-   for(let i=-1;++i<seq.length;){
-      let g=a.indexOf(seq[i])
-      if(g)a.unshift(a.splice(g,1)[0])
-      r.push(g)
+Function.prototype.wrap=function(func){
+   let x=this
+   return function(a,b){
+      return func(x,a,b)
    }
-   return r[0]!=-1?r:null
 }
 
-function decode(alf,seq){
-   let a=[...alf],r=seq.slice()
-   for(let i=-1;++i<seq.length;){
-      r[i]=a[seq[i]]
-      if(seq[i])a.unshift(a.splice(seq[i],1)[0])
-   }
-   return typeof alf==='string'?r.join``||null:r
+function speak(name){
+   return "Hello " + name;
 }
 
-console.log(encode("abc", "acacac"))
-console.log(decode("abc", [0,1,1,1,1,1]))
+speak = speak.wrap(function(original, yourName, myName){
+   let greeting = original(yourName);
+   return greeting + ", my name is " + myName;
+})
+
+let greeting = speak("Mary", "Kate");
+
+console.log(greeting)
