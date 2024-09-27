92ae97493b6adf5f2ed36fe314c21f5a2d4e65f1
Jhonny <gonzodzen@mail.ru>
2024-09-28 02:47:07 +0300

    Iterate function

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-// 
+function *fack(val,i,f,n){
+    if(n!==undefined){
+        while(n--){
+            yield val
+            val=f(val,i)
+            i++
+        }
+    }
+    else{
+        while(1){
+            yield val
+            val=f(val,i)
+            i++
+        }
+    }
+}
 
-// function wordWrap(s){
-//    let n=s.length,a=[]
-//    for(let i=0;i<n;){
-//       if(i+25<n&&s[i+25]!==' '&&s[i+24]!==' ')a.push(s.slice(i,i+=24)+'-')
-//       else a.push(s.slice(i,i+=25))
-//    }
-//    return a.join`\n`
-// }
+Object.prototype.iterate=function(f,n){
+    return fack(this.valueOf(),0,f,n)
+}
 
-// console.log(wordWrap('HNxu4xsbM Uk 5O4 j  Hi 6 4 Ol9V   wgBNdG61YoQZMOPrt BeFFbMrzKg3VB IjM MyKh wjhE 3'))
-// console.log(wordWrap('1234567890123456789012345 1234567890'))
-// console.log(wordWrap('The quick brown fox jumped over the lazy developer.'))
+Object.defineProperty(Object.prototype,'iterate',{enumerable: false,
+    configurable: false,
+    writable: false,
+  })
+
+console.log(Array.from((1).iterate(x=>x+2,3)))
