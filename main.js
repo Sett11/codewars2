bbdf98909d3843074163694f60a69a2651e5a02e
Jhonny <gonzodzen@mail.ru>
2024-10-04 00:09:57 +0300

    Hex class

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-function minSteps(a){
-  let n=a.length
-  let sum=a.reduce((a,c)=>a+c,0),s=sum/n
-  let r=a.map(e=>Math.abs(e-s)/2)
-  if(s%1||r.some(e=>e%1))return -1
-  return r.reduce((a,c)=>a+c)/2
-}
+class Hex{
+    constructor(v){this.v=v}
+    static parse(s){return parseInt(s,16)}
+    valueOf(){return this.v}
+    toString(){return '0x'+((this.v).toString(16)).toUpperCase()}
+    toJSON(){return this.toString()}
+    plus(x){return new Hex(this.v+x)}
+    minus(x){return new Hex(this.v-x)}
+  }
 
-console.log(minSteps([7,15,9,5]))
-console.log(minSteps([ 1944, 1952, 1988, 1940, 1956 ]))
-console.log(minSteps([526,512,502,498,502,508] ))
+F=new Hex(255)
+console.log(F+0)
