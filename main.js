71220a04e2ff6a7c8f062d674726864b1a361977
Jhonny <gonzodzen@mail.ru>
2024-10-04 13:01:29 +0300

    Advanced Events

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-class Event {
-  constructor(arr=[]){
-      this.arr=arr
+function Event(){
+  let store=[]
+	this.subscribe=function(...args){
+    args.filter(e=>typeof e==='function').forEach(e=>store.push(e))
   }
-  subscribe(fn){
-      this.arr.push(fn)
-  }
-  unsubscribe(f){
-      if(this.arr.includes(f)){
-          this.arr = this.arr.filter(el=>el!==f)
+  this.unsubscribe=function(...args){
+    for(let i=-1;++i<args.length;){
+      for(let j=store.length;--j>=0;){
+        if(store[j]==args[i]){
+          store.splice(j,1)
+          break
+        }
       }
+    }
   }
-  emit(...args){
-      return this.arr.map(el=>el(...args))
+  this.emit=function(...arg){
+    store.slice().forEach(e=>e.call(this,...arg))
   }
 }
 
+function l(arr) { arr.push('l'); }
+function o(arr) { arr.push('o'); }
 
+var e = new Event(),
+    bucket = [];
+
+e.subscribe(l, o, l);
+e.emit(bucket)
+
+console.log(bucket)
+
+e.unsubscribe(o, l);
+bucket = [];
+
+e.emit(bucket);
+
+console.log(bucket)
