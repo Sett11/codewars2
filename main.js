36c5d5a4838abfbae28f7a1f1ecd372034a05cfe
Jhonny <gonzodzen@mail.ru>
2024-10-06 22:55:36 +0300

    Write JavaScript's 'call' function using apply.

--- main.js
+++ main.js
@@ -1,91 +1,24 @@
-class ParkingLot {
-  constructor(size) {
-    this.a=Array(size).fill(0)
-    this.bikes=new Set()
-    this.cars=new Set()
-    this.vans=new Set()
-  }
-  park(s){
-    if(s instanceof Bike){
-      if(this.size<1)return false
-      for(let i=-1;++i<this.a.length;){
-        if(this.a[i]===0){
-          this.a[i]=s.license
-          this.bikes.add(s.license)
-          return true
-        }
-      }
-    }
-    if(s instanceof Car){
-      for(let i=-1;++i<this.a.length;){
-        if(this.a[i]===0&&this.a[i+1]===0){
-          this.a[i]=this.a[i+1]=s.license
-          this.cars.add(s.license)
-          return true
-        }
-      }
-    }
-    if(s instanceof Van){
-      for(let i=-1;++i<this.a.length;){
-        if(this.a[i]===0&&this.a[i+1]===0&&this.a[i+2]===0){
-          this.a[i]=this.a[i+1]=this.a[i+2]=s.license
-          this.vans.add(s.license)
-          return true
-        }
-      }
-    }
-    return false
-  }
-  retrieve(s){
-    if(this.bikes.has(s)){
-      for(let i=-1;++i<this.a.length;){
-        if(this.a[i]===s){
-          this.a[i]=0
-          this.bikes.delete(s)
-          return true
-        }
-      }
-    }
-    if(this.cars.has(s)){
-      for(let i=-1;++i<this.a.length;){
-        if(this.a[i]===s&&this.a[i+1]===s){
-          this.a[i]=this.a[i+1]=0
-          this.cars.delete(s)
-          return true
-        }
-      }
-    }
-    if(this.vans.has(s)){
-      for(let i=-1;++i<this.a.length;){
-        if(this.a[i]===s&&this.a[i+1]===s&&this.a[i+2]===s){
-          this.a[i]=this.a[i+1]=this.a[i+2]=0
-          this.vans.delete(s)
-          return true
-        }
-      }
-    }
-    return false
-  }
+Function.prototype.call = function(t,...a) {
+  return this.apply(t, a)
 }
 
-class Bike{
-  constructor(license){
-    this.license=license
-  }
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
 }
 
-class Car{
-  constructor(license){
-    this.license=license
-  }
-}
-class Van{
-  constructor(license){
-    this.license=license
-  }
+function Food(name, price) {
+  Product.call(this, name, price);
+  this.category = 'food';
 }
 
-p=new ParkingLot(6)
+function Toy(name, price) {
+  Product.call(this, name, price);
+  this.category = 'toy';
+}
 
-console.log(p.park(new Van('B1')))
-console.log(p.retrieve('B1'))
+const cheese = new Food('feta', 5);
+const fun = new Toy('robot', 40);
+
+console.log(cheese.price)
+console.log(fun.name)
