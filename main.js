65c4686e4943a0fa70847a9b190733fe386f309e
Jhonny <gonzodzen@mail.ru>
2024-10-10 02:18:35 +0300

    GPS Navigation

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
-function checkTrip(s,e,d){
-  let v='Impossible',u=new Set()
-  const dfs=x=>{
-    if(u.has(v)||v==='Possible')return
-    if(x===e){
-      v='Possible'
-      return
-    }
-    u.add(x)
-    for(let i in d[x])if(!u.has(d[x][i]))dfs(d[x][i])
+function createGraph(a){
+  const graph={},notDirGraph={}
+  for(let i in a){
+    if(!graph[a[i].from])graph[a[i].from]={}
+    if(!notDirGraph[a[i].from])notDirGraph[a[i].from]={}
+    if(!notDirGraph[a[i].to])notDirGraph[a[i].to]={}
+    graph[a[i].from][a[i].to]=a[i].drivingTime
+    notDirGraph[a[i].from][a[i].to]=a[i].drivingTime
+    notDirGraph[a[i].to][a[i].from]=a[i].drivingTime
   }
-  dfs(s)
-  return v
+  return [graph,notDirGraph]
 }
 
-const links = {"ADL" : ["MEL"], "MEL" : ["ADL", "SYD"],  "SYD" : ["BRI"], "BRI" : [] }
-console.log(checkTrip('SYD','ADL',links))
+function dijkstra(g,s){
+  const weight={},deq=[s]
+  weight[s]=0
+  while(deq.length){
+    let v=deq.pop()
+    for(let i in g[v]){
+      let t=weight[v]+g[v][i]
+      if(weight[i]===undefined||weight[i]>t){
+        weight[i]=t
+        deq.push(i)
+      }
+    }
+  }
+  return weight
+}
+
+function findPath(d,g,s,e){
+  const path=[e]
+  while(!path.includes(s)){
+    let t=path[path.length-1],n=d[t]
+    let r=Object.entries(g[t]).filter(e=>n-e[1]===d[e[0]])
+    if(!r.length)break
+    path.push(r[0][0])
+  }
+  return path.reverse().map(e=>+e)
+}
+
+function navigate(n,a,s,e){
+  const g=createGraph(a),dg=dijkstra(g[0],s)
+  if(dg[e]===undefined)return null
+  return findPath(dg,g[1],s,e)
+}
+
+console.log(navigate(5,[
+  {from: 0, to: 1, drivingTime: 5},
+  {from: 0, to: 2, drivingTime: 10},
+  {from: 1, to: 2, drivingTime: 10},
+  {from: 1, to: 3, drivingTime: 2},
+  {from: 2, to: 3, drivingTime: 2},
+  {from: 2, to: 4, drivingTime: 5},
+  {from: 3, to: 2, drivingTime: 2},
+  {from: 3, to: 4, drivingTime: 10}
+],0,4))
