79e7e0329000e9893f518d90f71d2a03ac617058
Jhonny <gonzodzen@mail.ru>
2024-10-11 00:00:21 +0300

    Approximate derivative

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-function search(d){
-  let v=false
-  const f=(o,x,s)=>{
-    if(v)return
-    for(let i in o[x]){
-      if(typeof o[x][i]==='string'){
-      v=s+'/'+i
-      return
-    }
-    else f(o[x],i,s+'/'+i)}
-  }
-  for(let i in d)f(d,i,i)
-  if(!v)throw Error()
-  return v
+function deriv(f){
+  const dx=.0001
+  return x=>(f(x+dx)-f(x))/dx
 }
-
-console.log(search({
-  'New folder': {
-    'New folder': {}
-  },
-  'New folder (1)': {
-    'New folder': {
-      'funnyjoke.txt': 'lol i pranked you!!!'
-    }
-  },
-  'New folder (2)': {}
-}))
