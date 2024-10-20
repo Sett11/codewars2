13d23b19d79cdcfcd956feca53ed6944b9051a39
Jhonny <gonzodzen@mail.ru>
2024-10-21 00:04:37 +0300

    Strings, strings, strings (Easy)

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-arrayLowerCase=a=>a.map(e=>typeof e==='string'?e.toLowerCase():e)
+Object.prototype.toString=function(){
+    return /./.exec(this.valueOf()).input
+}
+Number.prototype.toString=function(){
+    return /./.exec(this.valueOf()).input
+}
+Boolean.prototype.toString=function(){
+    return /./.exec(this.valueOf()).input
+}
+Array.prototype.toString=function(){
+    return JSON.stringify(this.valueOf())
+}
 
-
-// Strings, strings, strings (Easy)
-// Object.prototype.toString=function(){
-//     return /./.exec(this.valueOf()).input
-// }
-// Number.prototype.toString=function(){
-//     return /./.exec(this.valueOf()).input
-// }
-// Boolean.prototype.toString=function(){
-//     return /./.exec(this.valueOf()).input
-// }
-// Array.prototype.toString=function(){
-//     return JSON.stringify(this.valueOf())
-// }
-
-// console.log([1,2].toString())
+console.log([1,2].toString())
