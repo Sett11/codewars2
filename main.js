6ac724f9f2866b06a41499ed542805579aba3293
Jhonny <gonzodzen@mail.ru>
2024-10-25 00:01:11 +0300

    One Line Task: Squirrel And Tree

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-function halfIt(n){
-    if(typeof n!='number')return 0
-    return Number((n+'').replace(/\d/g,e=>Math.round(e/2)))
-}
-
-console.log(halfIt(-2.22))
+squirrel=(h,H,S)=>+Math.hypot(H,H*S/h).toFixed(4)
