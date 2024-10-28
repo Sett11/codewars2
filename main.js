91d02c2a66403b146232d8dfa1b63b699fe4286f
Jhonny <gonzodzen@mail.ru>
2024-10-29 00:08:54 +0300

    Convert a Number to money!!

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-squirrel=(h,H,S)=>+Math.hypot(H,H*S/h).toFixed(4)
+numberToMoney=n=>{
+  [a,b]=(n+'').split`.`,r=[]
+  b=b.slice(0,2).replace(/0+$/g,'')
+  a=[...a].reverse()
+  for(let i=0;i<a.length;i+=3)r.unshift(a.slice(i,i+3).reverse().join``)
+  return r.join`,`+(b?'.'+b:'')
+}
+
+console.log(numberToMoney(95876.0095294565))
+console.log(numberToMoney(1500.342626))
