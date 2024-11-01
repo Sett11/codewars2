24c9fc160339591c3288d7663c7b59a5aad8d80d
Jhonny <gonzodzen@mail.ru>
2024-11-02 01:58:58 +0300

    Sorted list

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-const f=n=>{
-    let a='abcdefghijklmnopqrstuvwxyz',r=''
-    while(n){
-        r=a[n%26]+r
-        n=~~(n/26)
+class SortedList{
+    constructor(a=[]){
+        this.a=a
+        this.length=this.a.length
     }
-    return r
+    bs(n){
+        let l=0,r=this.length-1
+        while(r-l>1){
+            let m=~~((r+l)/2)
+            if(this.a[m]<n)l=m
+            else r=m
+        }
+        return l+1
+    }
+
+    add(n){
+        if(n<=this.a[0])this.a.unshift(n)
+        else if(n>=this.a[this.length-1])this.a.push(n)
+        else this.a.splice(this.bs(n),0,n)
+        this.length+=1
+    }
+    get(i){
+        return this.a[i]
+    }
 }
 
-const conversion=a=>{
-    let m=0,b=a.map(e=>{
-        e=f(e)
-        m=Math.max(m,e.length)
-        return e
-    }).map(e=>e.padStart(m,'a')).join``
-    return f(m-1)+b
-}
-
-console.log(conversion([22, 48, 50, 40, 77, 48, 82, 14, 73, 58]))
+let a=Array(100).fill(1).map((e,i)=>e+i)
+let s=new SortedList(a)
+console.log(s.length)
+s.add(34)
+console.log(s.length)
+console.log(s.get(33))
