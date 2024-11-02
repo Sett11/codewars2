828e676b81e8709baa37f335eb2dc2562b48be34
Jhonny <gonzodzen@mail.ru>
2024-11-02 11:36:50 +0300

    multiFilter

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function sum(n){
-      return n===undefined?0:function(x){
-        return x===undefined?n:sum(x+n)
-      }
+multiFilter=(...a)=>f=x=>a.every(e=>e(x))
+
+function isEven(el){
+    return el % 2 === 0;
+}
+  
+function isGTten(el){
+   return el > 10;
 }
 
-console.log(sum(5)(3)(9)())
+console.log([1,2,3,4,10,11,12,20,21,22].filter(multiFilter(isEven, isGTten)))
