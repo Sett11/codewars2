90784661da20e309b2ef8ef8a1d3ec5cc5511d79
Jhonny <gonzodzen@mail.ru>
2024-11-02 11:29:30 +0300

    Sum #1

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-Array.prototype.valueOf=function(){
-    return (this.length?this:[0]).reduce((a,c)=>a+c)
+function sum(n){
+      return n===undefined?0:function(x){
+        return x===undefined?n:sum(x+n)
+      }
 }
 
-console.log([1,2,3]+4)
+console.log(sum(5)(3)())
