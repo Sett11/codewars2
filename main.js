c66ae34f2cb275d8d5c46125cb79abd944fffb11
Jhonny <gonzodzen@mail.ru>
2024-11-02 12:03:05 +0300

    Ho Ho Ho with Functions!

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-pipeline=(...a)=>{
-    if(!a.length)return
-    let x=a.shift()
-    return !a.length?x:a.reduce((acc,curr)=>acc=curr(acc),x)
-}
+ho=(...a)=>!a.length?'Ho!': 'Ho '+a.join``
 
-compose=(...a)=>{
-    if(!a.length)return
-    return function(x){
-        if(!x)return
-        a.reverse().forEach(e=>x=e(x))
-        return x
-    }
-}
-
-var greet    = function(name){ return "hi: " + name; };
-var exclaim  = function(statement){ return statement.toUpperCase() + "!"; };
-var welcome = compose(greet, exclaim);
-
-console.log(pipeline(42,x=>-x))
-console.log(pipeline())
-
-console.log(welcome('moe'))
+console.log(ho(ho(ho())))
