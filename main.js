b317cdc56f34f13d1ed0f5bb5fb6737b0c3e6399
Jhonny <gonzodzen@mail.ru>
2024-11-09 00:09:44 +0300

    Making Change: Part 2

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-sc=(m,n)=>[...Array(n)].reduce(a=>a.map((e,i)=>e.map((e,j)=>/V/.test(`${a[i][j-1]}${a[i][j+1]}${a[i-1]&&a[i-1][j]}${a[i+1]&&a[i+1][j]}`)?'V':e)),m)
+class Currency {
+    constructor(a){
+      this.a=a
+    }
+    numWays(n,a=this.a){
+      return n<0||!a.length?0:!n?1:this.numWays(n-a[0],a)+this.numWays(n,a.slice(1))
+    }
+    minChange(n,a=this.a){
+      let r=Array(n+1).fill(0).map((e,i)=>i?Number.MAX_VALUE:e)
+      for(let i=0;++i<=n;){
+        for(let j=-1;++j<a.length;){
+            if(a[j]<=i){
+                let x=r[i-a[j]]
+                if(x+1<r[i])r[i]=x+1
+            }
+        }
+      }
+      return r[n]
+    }
+  }
+
+const s=new Currency([100, 50, 25, 10, 5, 1])
+
+console.log(s.minChange(194))
