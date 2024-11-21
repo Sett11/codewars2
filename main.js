0842bb2082eb37af7644fa3d4de082e4f10752a7
Jhonny <gonzodzen@mail.ru>
2024-11-22 00:35:01 +0300

    Generate An Array of Unique Strings

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-countIncreasingSequences=(n,m,f=n=>n<=1?1:n*f(n-1))=>n<=m?f(m)/(f(n)*f(m-n)):0
+function uniqStrings(n,v,q){
+    for(let i=q[0];++i<q[1];)q.push(i)
+    let k=n-~~(n*v)
+    const a=Array(26).fill(1).map((_,i)=>String.fromCharCode(i+97)).concat(Array(10).fill(0).map((_,i)=>i)).concat(['_']),s=`\`'"~!@#$%^&*()[]{}:;<>?|\/`,r=[]
+    while(r.length<n){
+        let cs=q[~~(Math.random()*q.length)],c=''
+        if(k){
+            c+=s[~~(Math.random()*s.length)]
+            k--
+        }
+        while(c.length<cs)c+=a[~~(Math.random()*a.length)]
+        r.push(c)
+    }
+    return r
+}
 
-console.log(countIncreasingSequences(3,5))
+console.log(uniqStrings(4,0.25,[3, 5]))
+console.log(uniqStrings(3,.1,[4, 6]))
