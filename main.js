716bf6a0a2053884f8d3ea15e94da5f571454839
Jhonny <gonzodzen@mail.ru>
2024-11-25 00:05:02 +0300

    Sorting the Rabbit Hole

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-toASCIINumber=(a,b)=>a[c='charCodeAt']()+''+b[c]()^0
+function deepSort(a,v){
+    const f=x=>typeof x=='number'?x:x.reduce((a,c)=>a+(typeof c=='number'?c:f(c)))
+    a.sort((a,b)=>v?f(a)-f(b):f(b)-f(a))
+    for(let i of a)if(Array.isArray(i))deepSort(i.sort((a,b)=>v?f(a)-f(b):f(b)-f(a)),v)
+    return a
+ }
 
-console.log(toASCIINumber("A","B"))
+ console.log(deepSort([1, [2, 3, [4, 5, [9, 11], 1, 8], 6], [20, 7, 8]]))
+ console.log(deepSort([1, [2, 3, [4, 5, [9, 11], 1, 8], 6]]))
