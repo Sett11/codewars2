f4feca6f8c2864358e3110f24e0bf18a58dbe5d5
Jhonny <gonzodzen@mail.ru>
2024-11-30 00:07:12 +0300

    #Hashtag

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-function deepSort(a,v){
-    const f=x=>typeof x=='number'?x:x.reduce((a,c)=>a+(typeof c=='number'?c:f(c)))
-    a.sort((a,b)=>v?f(a)-f(b):f(b)-f(a))
-    for(let i of a)if(Array.isArray(i))deepSort(i.sort((a,b)=>v?f(a)-f(b):f(b)-f(a)),v)
-    return a
- }
+const getHashtags=s=>{
+    if(typeof s!=='string')return []
+    let r=(' '+s+' ').replace(/#+/g,'#').match(/\s#[A-z]+\s/g)
+    return r?[r[0].trim().slice(1)]:[]
+}
 
- console.log(deepSort([1, [2, 3, [4, 5, [9, 11], 1, 8], 6], [20, 7, 8]]))
- console.log(deepSort([1, [2, 3, [4, 5, [9, 11], 1, 8], 6]]))
+console.log(getHashtags("Hello #world"))
+console.log(getHashtags("too ##many tags"))
+console.log(getHashtags("#blue#red#yellow#green"))
+console.log(getHashtags("#lol #sorryNotSorry #heya #coolbeans"))
