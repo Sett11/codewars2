80e3348d3c89c2dd8c3ca7e8cd091f27ef24cd83
Jhonny <gonzodzen@mail.ru>
2024-12-01 00:02:20 +0300

    Word Segmentation

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-const getHashtags=s=>{
-    if(typeof s!=='string')return []
-    let r=(' '+s+' ').replace(/#+/g,'#').match(/\s#[A-z]+\s/g)
-    return r?[r[0].trim().slice(1)]:[]
+function segmentify(d,s,c=s){
+    const u=new Set(d),r=[],m=Math.max(...d.map(e=>e.length))+2
+    const f=s=>{
+        if(!s)return
+        for(let i=m;--i>=0;){
+            t=s.slice(0,i)
+            if(c&&u.has(t)){
+                r.push(t)
+                c=c.slice(i)
+                if(u.has(c)){
+                    r.push(c)
+                    c=''
+                    return
+                }
+                f(c)
+            }
+        }
+    }
+    f(s)
+    if(c)return 'IMPOSSIBLE'
+    for(let i=-1;++i<r.length;)for(let j=-1;++j<r[i].length;)if(u.has(r[i].slice(0,j))&&u.has(r[i].slice(j)))return 'AMBIGUOUS'
+    return r.join` `
 }
 
-console.log(getHashtags("Hello #world"))
-console.log(getHashtags("too ##many tags"))
-console.log(getHashtags("#blue#red#yellow#green"))
-console.log(getHashtags("#lol #sorryNotSorry #heya #coolbeans"))
+console.log(segmentify([
+    'd',     'nxfo',
+    'z',     'q',
+    'fxs',   'qb',
+    'smbft', 'ym'
+  ],'nxfoznxfozzsmbftqbfxsfxsfxszqzqbnxfo'))
+console.log(segmentify([ "quick", "jumped", "brown", "fox", "the", "jumped", "lazy" ],'thequickbrownfoxjumpedoverthelazydog'))
+console.log(segmentify([ "weather", "the", "how", "is" ],"howistheweather"))
+console.log(segmentify([ "examples", "over", "haul", "overhaul", "the" ],"overhaultheexamples"))
