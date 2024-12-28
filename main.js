3f350bf63d89adcc8294d2ccf0d3f6e072a6ef49
Jhonny <gonzodzen@mail.ru>
2024-12-29 00:02:02 +0300

    Grasshopper - Terminal Game Turn Function

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var Person = function(){
-    var person = {
-      _name: "Leroy",
-      _friends: [],
-      fillFriends(f) {
-        for(let i of f)this._friends.push(i)
-      }
-    }
-    return person
+function doTurn () {
+ rollDice(),move(),combat(),getCoins(),buyHealth(),printStatus()
 }
-
-let p=Person()
-p.fillFriends(['bob','john'])
