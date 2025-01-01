c037dec5238a109f40d563926be9cd56a590c96f
Jhonny <gonzodzen@mail.ru>
2025-01-02 00:20:18 +0300

    Shorten Me: Array Declaration IV (1-30)

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-function thirstyRobot(arr){
-  let a=arr.map(e=>e<=8?'b':e>15?'l':'w'),l=a.includes('l'),b=a.length-a.slice().reverse().indexOf('b'),w=a.indexOf('w')
-  if(b>a.length)b=-1
-  return (((w>-1&&b>=-1)&&b<=w)||(w==-1&&b==-1)||(w==-1&&b!=-1))?!l?'Fine':'Wonky robot':!l?'Oh dear':'Poor robot'
-}
-
-console.log(thirstyRobot([ 62.89, 8, 8, 4, 13 ]))
+array=_=>[...Array(30)].map((_,i)=>++i)
