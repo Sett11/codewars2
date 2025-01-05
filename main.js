54df5723bbb4da9c337695f666be8dfc1c6de526
Jhonny <gonzodzen@mail.ru>
2025-01-06 00:00:47 +0300

    Where's Wally

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-Object.prototype.description="TODO: GIVE ME A DESCRIPTION"
+function wheresWally(s){
+    return (' '+s).search(/ Wally\b/)
+}
+
+console.log(wheresWally(''))
+console.log(wheresWally('Wally'))
+console.log(wheresWally('.Wally'))
+console.log(wheresWally("Where's Wally"))
