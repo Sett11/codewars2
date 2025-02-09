47d519515f863c3ef9ee1fae452c5933f41b998d
Jhonny <gonzodzen@mail.ru>
2025-02-10 00:00:36 +0300

    Training JS #27: methods of arrayObject---filter()

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-tailAndHead=arr=>arr.reduce((a,c,i,v)=>a*((+((r=(c+''))[r.length-1])+(+(v[i+1]+'')[0]))||1),1)
+function countGrade(a){
+    const f=(a,s,l)=>a.filter(e=>e>=s&&e<l).length
+    return {S:a.filter(e=>e==100).length,A:f(a,90,100),B:f(a,80,90),C:f(a,60,80),D:f(a,0,60),X:a.filter(e=>e==-1).length}
+  }
 
-console.log(tailAndHead([123,456,789,12,34,56,78]))
-console.log(tailAndHead([1,2,3,4,5]))
+console.log(countGrade([50,60,70,80,90,100]))
