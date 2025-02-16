f329a3e5f6832929190c2b76863655b018a63718
Jhonny <gonzodzen@mail.ru>
2025-02-17 00:00:35 +0300

    Thinking & Testing : I hate big and small

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-function d01(a,b) {
-    return !(a-b)
-}
+testit=(arr,k=0)=>+(arr.reduce((a,c)=>a+(c==Math.max(...arr)||c==Math.min(...arr)?0:++k&&c),0)/k).toFixed(2)||0
+
+console.log(testit([6,7,19,15,16]))
+console.log(testit([3,8,14,19,3,18,15,7,4,19]))
