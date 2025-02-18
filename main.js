6bcd83c9a7eb7325edfd6d6dec388af4f9e849b0
Jhonny <gonzodzen@mail.ru>
2025-02-19 00:06:07 +0300

    Thinking & Testing #38: What's the "?"

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-testit=(arr,k=0)=>+(arr.reduce((a,c)=>a+(c==Math.max(...arr)||c==Math.min(...arr)?0:++k&&c),0)/k).toFixed(2)||0
+function testIt(a){
+  const f=x=>x.reduce((a,c)=>{
+    a[c]=(a[c]+1)||1
+    return a
+  },{}),b=a.filter(e=>e!=='?').map(e=>e.split` `)
+  let [x,y]=[Object.entries(f(b.map(e=>e[0]))),Object.entries(f(b.map(e=>e[1])))]
+  let m1=Math.min(...x.map(e=>e[1])),m2=Math.min(...y.map(e=>e[1]))
+  return `${x.filter(e=>e[1]==m1).map(e=>e[0])[0]} ${y.filter(e=>e[1]==m2).map(e=>e[0])[0]}`
+}
 
-console.log(testit([6,7,19,15,16]))
-console.log(testit([3,8,14,19,3,18,15,7,4,19]))
+console.log(testIt(["Red Square","Red Circle","Green Triangle","Green Circle","?","Green Square"]))
+console.log(testIt(["Red Square","Red Circle","Green Triangle","Green Circle","Blue Circle","?","Blue Triangle","Blue Square","Green Square"]))
