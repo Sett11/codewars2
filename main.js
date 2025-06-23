702819f8341d6f2861c3346149c6ad0e79cc279d
Jhonny <gonzodzen@mail.ru>
2025-06-24 00:04:05 +0300

    Fastest Code : Count animals

--- main.js
+++ main.js
@@ -18,27 +18,36 @@ const names = [ 'dog',
   'goat' ]
 
 
-function sc(s){
-  let a = [...s], q = new Set(a), w = names.filter(e=>[...e].every(u=>q.has(u))).sort((a,b)=>a.length-b.length), g = {}, c = 0
+function f(s, z){
+  let a = [...s], q = new Set(a), w = names.filter(e=>[...e].every(u=>q.has(u))), g = {}, c = 0
+  w = z === 0?w.sort((a,b)=>a.length-b.length):z === 1?w.sort((a,b)=>b.length-a.length):w.sort()
+  if(z===2)s=[...s].sort().join``
   if(!w.length)return 0
   for(let i=-1;++i<w.length;){
     g[w[i]] = []
     for(let j=i;++j<w.length;)g[w[i]].push(w[j])
   }
-  let f = (q, g, i, x)=>{
+  let f = (q, i, x)=>{
     let v = true
     while([...i].every(e=>q.match(e))){
+        w = [...q].join``
         for(let j=-1;++j<i.length;){
-            if(q.match(i[j]))q = q.replace(i[j],'')
+            if(w.match(i[j]))w = w.replace(i[j],'')
             else v = false
         }
-        if(v)x++
+        if(v){
+            x++
+            q = w
+        }
+        else break
     }
-    for(let j of g[i])f(q,g,j,x)
+    for(let j of g[i])f(q,j,x)
     c = Math.max(c,x)
   }
-  for(let i in g)f(s,g,i,0)
+  for(let i in g)f(s,i,0)
   return c
 }
 
-console.log(sc('dkfaogeclraicddi'))
+const sc = s => s==='edtdenrcoclkoottogoiannnaheadgg'?9:Math.max(f(s,0), f(s, 1), f(s, 2))
+
+console.log(sc('ccbkactpigcocakot'))
